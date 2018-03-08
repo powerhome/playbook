cc2ec7af91b8f8d6a2636942a9427c1856038c1e
Ben Langfeld <ben@langfeld.me>
2018-03-08 14:14:59 -0300

    Log properly inside a container

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,10 @@ module Uix
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
+    # Log to stdout because we're in a container
+    logger = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 end
