48632a71ca100ed6842f040def31b4187cfe176f
Markie Arnold <markiearnold@gmail.com>
2018-07-16 11:37:14 -0500

    Update snippet shortcode and preview

--- app/helpers/redcarpet_helper.rb
+++ app/helpers/redcarpet_helper.rb
@@ -33,6 +33,56 @@ module RedcarpetHelper
 end
 
 class HTMLBlockCode < Redcarpet::Render::HTML
+  def getId(string)
+    string.gsub(/id="(.*?)"/) do
+      @id = $1
+    end
+  end
+
+  def getHeight(string)
+    @height = 160
+    string.gsub(/height="(.*?)"/) do
+      @height = $1
+    end
+  end
+
+  def getPreview(string)
+    @preview = true
+    string.gsub(/preview="(.*?)"/) do
+      @preview = $1
+      if @preview == "false"
+        @preview = false
+      elsif @preview == "true"
+        @preview = true
+      end
+    end
+  end
+
+  def getExpand(string)
+    @expand = false
+    string.gsub(/expand="(.*?)"/) do
+      if $1.present?
+        @expand = $1
+        if @expand == "false"
+          @expand = false
+        else
+          @expand = true
+        end
+      end
+    end
+  end
+
+  def getLinks(string)
+    @links = ["view", "copy", "preview"]
+    string.gsub(/links="(.*?)"/) do
+      @links = $1.split(",").map{|i| i.downcase.strip}
+    end
+  end
+
+  def getContent(id)
+    @content = Page.where(id: id).first
+  end
+
   def header(title, level)
     if level == 2
       @headers ||= []
@@ -45,7 +95,7 @@ class HTMLBlockCode < Redcarpet::Render::HTML
         end
       end
       @headers << permalink
-      %(\n<h#{level} id=\"#{permalink}\"><a name="#{permalink}" class="markdown-header-anchor anchor" href="##{permalink}"><span class="fa fa-link anchor-icon"></span></a>#{title}</h#{level}>\n)
+      %(\n<h#{level} id=\"#{permalink}\"><a name="#{permalink}" class="markdown-header-anchor anchor" href="##{permalink}"><span class="far fa-link anchor-icon"></span></a>#{title}</h#{level}>\n)
     else
       %(\n<h#{level}>#{title}</h#{level}>\n)
     end
@@ -53,79 +103,67 @@ class HTMLBlockCode < Redcarpet::Render::HTML
 
   def playbook_snippet(full_document)
     full_document.gsub! /\[snippet (.*)\]/ do
-      @random_id = Random.rand(40)
       @string = $1
-      @default_height = '160'
-      @default_show = true
-      @default_expand = false
-      @attr = ['', @default_height, @default_show]
+      @random_id = Random.rand(100)
+      @iframe = %(\n\n)
+      @code = %(\n\n)
+      @image_preview = %(\n\n)
+      @action_buttons = %(\n\n)
+      @view_link = %(\n\n)
+      @copy_link = %(\n\n)
+      @preview_link = %(\n\n)
 
-      # Set id from attributes
-      @string.gsub(/id="(.*?)"/) do
-        @attr[0] = $1
-      end
+      getId(@string)
 
-      # Set height from attributes
-      @string.gsub(/height="(.*?)"/) do
-        if $1
-          @attr[1] = $1
-        else
-          @attr[1] = @default_height
-        end
-      end
+      if !@id.nil?
+        permalink = "snippet-#{@id}-#{@random_id}"
 
-      @string.gsub(/preview="(.*?)"/) do
-        if $1
-          if $1 == "false"
-            @attr[2] = false
+        getContent(@id)
+        getHeight(@string)
+        getPreview(@string)
+        getExpand(@string)
+        getLinks(@string)
+
+        if @content.present?
+          @snippet_encoded = (CGI.escapeHTML @content.body_markdown).html_safe
+
+          # Show iframe preview
+          if @preview == true
+            @iframe = %(\n<iframe onload="snippetFrameLoad('snippet#{@id}#{@random_id}')" id="snippet#{@id}#{@random_id}" scrolling="no" src="/snippet/#{@id}" width="100%" height="#{@height}"></iframe>\n)
           else
-            @attr[2] = $1
+            @iframe = %(\n<iframe onload="snippetFrameLoad('snippet#{@id}#{@random_id}')" class="snippet-hidden" id="snippet#{@id}#{@random_id}" scrolling="no" src="/snippet/#{@id}" width="0" height="0"></iframe>\n)
           end
-        else
-          @attr[2] = @default_show
-        end
-      end
 
-      @string.gsub(/expand="(.*?)"/) do
-        if $1
-          if $1 == "true" || $1 == true
-            @attr[3] = true
+          # Show image if preview is URL
+          if @preview != false && @preview != true
+            @iframe = %(\n<iframe onload="snippetFrameLoad('snippet#{@id}#{@random_id}')" class="snippet-hidden" id="snippet#{@id}#{@random_id}" scrolling="no" src="/snippet/#{@id}" width="0" height="0"></iframe>\n)
+            @image_preview = %(\n<div class="snippet-preview-image"><a href="#{Rails.application.routes.url_helpers.preview_snippet_path(@id)}" target="_blank" class="uix-component-link preview-image-link m-4 p-0"><img src="#{@preview}" /></a></div>\n)
+          end
+
+          # Show links
+          if @links.include? 'view'
+            @view_link = %(\n<a onclick="toggleSnippet(event, 'snippet#{@id}#{@random_id}')" class="uix-component-link toggle"><i class="far fa-eye mr-1"></i> View code</a>\n)
+          end
+          if @links.include? 'copy'
+            @copy_link = %(\n<a onclick="copySnippet(event, 'snippet#{@id}#{@random_id}')" id="copy-snippet#{@id}#{@random_id}" class="uix-component-link copy hidden"><i id="copy-icon" class="far fa-clone mr-1"></i><i id="copied-icon" class="far fa-check mr-1 hidden"></i> <span id="copy-text">Copy snippet</span></a>\n)
+          end
+          if @links.include? 'preview'
+            @preview_link = %(\n<a href="#{Rails.application.routes.url_helpers.preview_snippet_path(@id)}" target="_blank" class="uix-component-link preview-window"><i class="far fa-external-link mr-1"></i> Preview</a>\n)
+          end
+
+          # Auto expand or collapse
+          if @expand == true && !@expand.nil?
+            @code = %(\n<pre class="mx-4 toggle-snippet-preview line-numbers language-markup shown"><code>#{@snippet_encoded}</code></pre>\n)
+            @view_link = %(\n<a onclick="toggleSnippet(event, 'snippet#{@id}#{@random_id}')" class="uix-component-link toggle"><i class="far fa-eye-slash mr-1"></i> Hide code</a>\n)
           else
-            @attr[3] = false
+            @code = %(\n<pre class="mx-4 toggle-snippet-preview line-numbers language-markup"><code>#{@snippet_encoded}</code></pre>\n)
           end
-        else
-          @attr[3] = @default_expand
+
+          # Define action buttons based on options
+          @action_buttons = %(\n<div class="snippet-actions pb-4">#{@view_link}#{@copy_link}#{@preview_link}</div>\n)
+
+          %(\n<div class="uix-component-frame uix-snippet-frame"><a name="#{permalink}" class="markdown-snippet-anchor anchor" href="##{permalink}"><span class="far fa-link anchor-icon"></span></a>#{@iframe}#{@image_preview}#{@action_buttons}#{@code}</div>\n)
         end
-      end
-
-      @snippet_content = Page.where(id: @attr[0]).first
-
-      if @snippet_content.present?
-        @snippet_encoded = (CGI.escapeHTML @snippet_content.body_markdown).html_safe
-        @iframe = %(\n\n)
-        @image_preview = %(\n\n)
-        @action_buttons = %(\n\n)
-        @action_buttons = %(\n<div class="snippet-actions pb-4"><a onclick="toggleSnippet(event, 'snippet#{@random_id}')" class="uix-component-link toggle">View code</a><a onclick="copySnippet(event, 'snippet#{@random_id}')" class="uix-component-link copy">Copy snippet</a></div>\n)
-
-        if @attr[2] == true
-          @iframe = %(\n<iframe id="snippet#{@random_id}" scrolling="no" src="/snippet/#{@attr[0]}" width="100%" height="#{@attr[1]}"></iframe>\n)
-        else
-          @iframe = %(\n<iframe class="snippet-hidden" id="snippet#{@random_id}" scrolling="no" src="/snippet/#{@attr[0]}" width="0" height="0"></iframe>\n)
-        end
-
-        if @attr[2] != true && @attr[2] != false
-          @image_preview = %(\n<div class="snippet-preview-image"><a href="#{Rails.application.routes.url_helpers.preview_snippet_path(@attr[0])}" target="_blank" class="uix-component-link preview-image-link m-4 p-0"><img src="#{@attr[2]}" /></a></div>\n)
-          @action_buttons = %(\n<div class="snippet-actions pb-4"><a onclick="toggleSnippet(event, 'snippet#{@random_id}')" class="uix-component-link toggle">View code</a><a onclick="copySnippet(event, 'snippet#{@random_id}')" class="uix-component-link copy">Copy snippet</a><a href="#{Rails.application.routes.url_helpers.preview_snippet_path(@attr[0])}" target="_blank" class="uix-component-link preview-window">Preview</a></div>\n)
-        end
-
-        if @attr[3] == true
-          @code = %(\n<pre class="mx-4 toggle-snippet-preview line-numbers language-markup shown"><code>#{@snippet_encoded}</code></pre>\n)
-          @action_buttons = %(\n<div class="snippet-actions pb-4"><a onclick="copySnippet(event, 'snippet#{@random_id}')" class="uix-component-link copy">Copy snippet</a></div>\n)
-        else
-          @code = %(\n<pre class="mx-4 toggle-snippet-preview line-numbers language-markup"><code>#{@snippet_encoded}</code></pre>\n)
-        end
-
-        %(\n<div class="uix-component-frame uix-snippet-frame">#{@iframe}#{@image_preview}#{@action_buttons}#{@code}</div>\n)
       end
 
     end
