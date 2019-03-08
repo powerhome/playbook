6d8e5789af0e7cb8b995cbe4b2463dbb80beb4d9
Markie Arnold <markiearnold@gmail.com>
2019-03-08 09:04:31 -0600

    Image update

--- app/pb_kits/playbook/pb_image/docs/_default_image.jsx
+++ app/pb_kits/playbook/pb_image/docs/_default_image.jsx
@@ -4,7 +4,7 @@ import Image from "../_image.jsx"
 function DefaultImage() {
   return (
     <div>
-      <Image url="https://images.unsplash.com/photo-1465588042420-47a53c2d0320?ixlib=rb-1.2.1&auto=format&fit=crop&w=2595&q=80" />
+      <Image url="https://unsplash.it/500/400/?image=634" />
     </div>
   )
 }
