7a103b8a095a61aed201e9240194353493301946
Garett Arrowood <garettarrowood@gmail.com>
2019-05-24 14:38:45 -0400

    Correct (new) Gem spec_paths

--- lib/playbook/engine.rb
+++ lib/playbook/engine.rb
@@ -12,9 +12,9 @@ module Playbook
 
     config.sass.load_paths ||= []
     config.assets.paths ||= []
-    config.assets.paths << "#{Gem.loaded_specs['playbook'].full_gem_path}/fonts"
-    config.sass.load_paths << "#{Gem.loaded_specs['playbook'].full_gem_path}/sass-mixins"
-    config.sass.load_paths << "#{Gem.loaded_specs['playbook'].full_gem_path}/components"
+    config.assets.paths << "#{Gem.loaded_specs['playbook_ui'].full_gem_path}/fonts"
+    config.sass.load_paths << "#{Gem.loaded_specs['playbook_ui'].full_gem_path}/sass-mixins"
+    config.sass.load_paths << "#{Gem.loaded_specs['playbook_ui'].full_gem_path}/components"
 
     initializer "webpacker.proxy" do |app|
         insert_middleware = begin
