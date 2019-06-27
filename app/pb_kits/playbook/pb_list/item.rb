184772ec3345da513b2af0a4c6ab6898886a8bdd
Jonathan Zazula <djonyriz@gmail.com>
2019-06-27 11:23:19 -0400

    list kit updates- making list items a block & removing the bottom border on the last child of dark lists

--- app/pb_kits/playbook/pb_list/item.rb
+++ app/pb_kits/playbook/pb_list/item.rb
@@ -4,16 +4,19 @@ module Playbook
       PROPS = [:configured_classname,
           :configured_data,
           :configured_id,
-          :configured_text].freeze
+          :configured_text,
+          :block].freeze
 
       def initialize(classname: default_configuration,
                    data: default_configuration,
                    id: default_configuration,
-                   text: default_configuration)
+                   text: default_configuration,
+                  &block)
         self.configured_classname = classname
         self.configured_data = data
         self.configured_id = id
         self.configured_text = text
+        self.block = block_given? ? block : nil
       end
 
       def text
@@ -26,6 +29,10 @@ module Playbook
 
       def to_partial_path
         "pb_list/item"
+      end
+
+      def yield(context:)
+        context.capture(&block)
       end
 
     private
