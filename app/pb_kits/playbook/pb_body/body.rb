8905e00c489d3ff22627e4c156682430d4212e3f
Ramon Ruiz <30808035+rruiz85@users.noreply.github.com>
2019-06-27 13:55:31 -0400

    Markie Code review

--- app/pb_kits/playbook/pb_body/body.rb
+++ app/pb_kits/playbook/pb_body/body.rb
@@ -7,7 +7,9 @@ module Playbook
           :configured_data,
           :configured_id,
           :configured_tag,
-          :configured_text].freeze
+          :configured_text,
+          :block].freeze,
+
 
       def initialize(classname: default_configuration,
                    color: default_configuration,
@@ -15,7 +17,8 @@ module Playbook
                    data: default_configuration,
                    id: default_configuration,
                    tag: default_configuration,
-                   text: default_configuration)
+                   text: default_configuration,
+                   &block)
         self.configured_classname = classname
         self.configured_color = color
         self.configured_dark = dark
@@ -23,6 +26,7 @@ module Playbook
         self.configured_id = id
         self.configured_tag = tag
         self.configured_text = text
+        self.block = block_given? ? block : nil
       end
 
       def color
@@ -61,6 +65,10 @@ module Playbook
 
       def to_partial_path
         "pb_body/body"
+      end
+
+      def yield(context:)
+        context.capture(&block)
       end
 
     private
