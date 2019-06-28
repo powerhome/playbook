80ae65226e433bf4b1cf75dfe8ffd90297bbf51f
Jonathan Zazula <djonyriz@gmail.com>
2019-06-28 09:11:15 -0400

    added mixin file to list

--- app/pb_kits/playbook/pb_list/item.rb
+++ app/pb_kits/playbook/pb_list/item.rb
@@ -4,27 +4,17 @@ module Playbook
       PROPS = [:configured_classname,
           :configured_data,
           :configured_id,
-          :configured_text,
           :block].freeze
 
       def initialize(classname: default_configuration,
                    data: default_configuration,
                    id: default_configuration,
-                   text: default_configuration,
                   &block)
         self.configured_classname = classname
         self.configured_data = data
         self.configured_id = id
         self.configured_text = text
         self.block = block_given? ? block : nil
-      end
-
-      def text
-        if configured_text == default_configuration
-          "List item"
-        else
-          configured_text
-        end
       end
 
       def to_partial_path
