e4ff1b89bb542997e59f45d7ee3b45717f27d89a
Jasper Furniss <jasper.furniss@powerhrg.com>
2019-07-17 16:03:09 -0400

    Latest Try

--- app/pb_kits/playbook/pb_charts/pbLineGraph.js
+++ app/pb_kits/playbook/pb_charts/pbLineGraph.js
@@ -3,9 +3,10 @@ import { applyGlobalStyles } from './global.js';
 
 const applyLineGraphStyles = function(highchart){
     applyGlobalStyles(highchart);
+    highchart.plotOptions.
     highchart.plotOptions.series.marker.enabled = true;
     highchart.plotOptions.line.marker.enabled = true;
-    highchart.plotOptions.series.marker.fillColor = "white";
+    highchart.plotOptions.series.marker.fillColor = "black";
     highchart.plotOptions.series.marker.lineWidth = 2;
     highchart.isHighlightMode = true;
 }
