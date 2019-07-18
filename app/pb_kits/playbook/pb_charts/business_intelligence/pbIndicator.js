c1624a97a5a6cf7fb0799f5053474bf9f1775f08
Jasper Furniss <jasper.furniss@powerhrg.com>
2019-07-18 13:44:18 -0400

    Fixed Typography Variable

--- app/pb_kits/playbook/pb_charts/business_intelligence/pbIndicator.js
+++ app/pb_kits/playbook/pb_charts/business_intelligence/pbIndicator.js
@@ -6,38 +6,38 @@ const pbIndicator = {
     fontFamily: typography.font_family_base,
     color: colors.primary,
     fontSizes: {
-      big: typography.font_large,
-      medium: typography.font_base,
-      small: typography.font_small,
-      micro: typography.font_small
+      big: typography.text_large,
+      medium: typography.text_base,
+      small: typography.text_small,
+      micro: typography.text_small
     }
   },
   secondaryTitle: {
     fontFamily: typography.font_family_base,
     fontSizes: {
-      big: typography.font_large,
-      medium: typography.font_base,
-      small: typography.font_small,
-      micro: typography.font_small
+      big: typography.text_large,
+      medium: typography.text_base,
+      small: typography.text_small,
+      micro: typography.text_small
     }
   },
   value: {
     fontFamily: typography.font_family_base,
     color: colors.charcoal,
     fontSizes: {
-      big: typography.font_jumbo,
-      medium: typography.font_large,
-      small: typography.font_base,
-      micro: typography.font_small
+      big: typography.text_jumbo,
+      medium: typography.text_large,
+      small: typography.text_base,
+      micro: typography.text_small
     }
   },
   secondaryValue: {
     fontFamily: typography.font_family_base,
     fontSizes: {
-      big: typography.font_large,
-      medium: typography.font_base,
-      small: typography.font_small,
-      micro: typography.font_small
+      big: typography.text_large,
+      medium: typography.text_base,
+      small: typography.text_small,
+      micro: typography.text_small
     }
   }
 };
