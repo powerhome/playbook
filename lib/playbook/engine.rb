4d38121383dca4f84cebd4b58bdf1e34613824e9
Markie Arnold <markiearnold@gmail.com>
2019-07-19 20:40:05 -0500

    Resolve Layout/BlockAlignment offenses

--- lib/playbook/engine.rb
+++ lib/playbook/engine.rb
@@ -18,18 +18,18 @@ module Playbook
     config.sass.load_paths << "#{Gem.loaded_specs['playbook_ui'].full_gem_path}/components"
 
     initializer "webpacker.proxy" do |app|
-        insert_middleware = begin
-                            Playbook.webpacker.config.dev_server.present?
-                          rescue
-                            nil
-                          end
-        next unless insert_middleware
+      insert_middleware = begin
+                          Playbook.webpacker.config.dev_server.present?
+                        rescue
+                          nil
+                        end
+      next unless insert_middleware
 
-        app.middleware.insert_before(
-          0, Webpacker::DevServerProxy,
-          ssl_verify_none: true,
-          webpacker: Playbook.webpacker
-        )
-      end
+      app.middleware.insert_before(
+        0, Webpacker::DevServerProxy,
+        ssl_verify_none: true,
+        webpacker: Playbook.webpacker
+      )
+    end
   end
 end
