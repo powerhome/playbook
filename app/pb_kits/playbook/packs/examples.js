aac7c9498114a0c50ee8e9040e2dc4ca3589b87d
Markie Arnold <markiearnold@gmail.com>
2019-07-21 12:48:54 -0500

    Add time kit

--- app/pb_kits/playbook/packs/examples.js
+++ app/pb_kits/playbook/packs/examples.js
@@ -47,3 +47,6 @@ WebpackerReact.setup (Icon);
 
 import * as DashboardValue from "pb_dashboard_value/docs";
 WebpackerReact.setup (DashboardValue);
+
+import * as Time from "pb_time/docs";
+WebpackerReact.setup (Time);
---
+++ app/pb_kits/playbook/pb_time/time.rb
@@ -0,0 +1,123 @@
+# frozen_string_literal: true
+
+module Playbook
+  module PbTime
+    class Time < Playbook::PbKit::Base
+      PROPS = %i[configured_classname
+                 configured_data
+                 configured_id
+                 configured_size
+                 configured_timestamp
+                 configured_timezone].freeze
+
+      def initialize(classname: default_configuration,
+                     data: default_configuration,
+                     id: default_configuration,
+                     size: default_configuration,
+                     timestamp: default_configuration,
+                     timezone: default_configuration)
+        self.configured_classname = classname
+        self.configured_data = data
+        self.configured_id = id
+        self.configured_size = size
+        self.configured_timestamp = timestamp
+        self.configured_timezone = timezone
+      end
+
+      def display_value
+        size == "lg" ? display_value_lg : display_value_sm
+      end
+
+      def kit_class
+        kit_options = [
+          "pb_time",
+          size,
+        ]
+        kit_options.join("_")
+      end
+
+      def to_partial_path
+        "pb_time/time"
+      end
+
+    private
+
+      def timezone_value
+        default_value(configured_timezone, "EST").upcase
+      end
+
+      def size
+        size_options = %w[lg sm]
+        one_of_value(configured_size, size_options, "sm")
+      end
+
+      def convert_to_timestamp(ts)
+        ts.is_a?(String) ? DateTime.parse(ts) : nil
+      end
+
+      def hour
+        if is_set? configured_timestamp
+          timestamp = convert_to_timestamp(configured_timestamp)
+          timestamp.strftime("%l")
+        end
+      end
+
+      def minutes
+        if is_set? configured_timestamp
+          timestamp = convert_to_timestamp(configured_timestamp)
+          timestamp.strftime("%M")
+        end
+      end
+
+      def timezone
+        if is_set? configured_timestamp
+          "<span class='pb__time_timezone'>#{timezone_value}</span>".html_safe
+        end
+      end
+
+      def meridian
+        if is_set? configured_timestamp
+          timestamp = convert_to_timestamp(configured_timestamp)
+          timestamp.strftime("%P")[0,1]
+        end
+      end
+
+      def icon
+        pb_icon = Playbook::PbIcon::Icon.new(icon: "calendar", fixed_width: true)
+        ApplicationController.renderer.render(partial: pb_icon, as: :object)
+      end
+
+      def format_time_string
+        format_time = "#{hour}:#{minutes}#{meridian}"
+      end
+
+      def text
+        "<span>#{format_time_string}</span> #{timezone}".html_safe if is_set? configured_timestamp
+      end
+
+      def display_value_sm
+        if is_set? configured_timestamp
+          pb_value = Playbook::PbBody::Body.new(color: "default") do
+            icon +
+              text
+          end
+          ApplicationController.renderer.render(partial: pb_value, as: :object)
+        end
+      end
+
+      def display_value_lg
+        if is_set? configured_timestamp
+          pb_value_lg = Playbook::PbTitle::Title.new(size: 3, text: "#{format_time_string} #{timezone}".html_safe)
+          ApplicationController.renderer.render(partial: pb_value_lg, as: :object)
+        end
+      end
+
+      DEFAULT = Object.new
+      private_constant :DEFAULT
+      def default_configuration
+        DEFAULT
+      end
+      attr_accessor(*PROPS)
+    end
+  end
+end
