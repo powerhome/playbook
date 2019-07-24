05800149629517213b6e222c47233920dca464d5
Jasper Furniss <jasper.furniss@powerhrg.com>
2019-07-24 14:36:35 -0400

    Set Relative Path for Playbook Packs

--- app/pb_kits/playbook/packs/pb_caption.js
+++ app/pb_kits/playbook/packs/pb_caption.js
@@ -1,4 +1,4 @@
-import Caption from "pb_caption/_caption.jsx";
+import Caption from "../pb_caption/_caption.jsx";
 import WebpackerReact from "webpacker-react";
 
 WebpackerReact.setup({ Caption });
