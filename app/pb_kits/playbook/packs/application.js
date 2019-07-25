1453906925414120a531ed7d7c79f2687e6fb2df
Jasper Furniss <jasper.furniss@powerhrg.com>
2019-07-25 10:41:07 -0400

    Latest Restructuring

--- app/pb_kits/playbook/packs/application.js
+++ app/pb_kits/playbook/packs/application.js
@@ -1,14 +1,18 @@
-import "../../../../fonts/fontawesome.js"
-import "../../../../fonts/regular.js"
+import "../../../../fonts/fontawesome.js";
+import "../../../../fonts/regular.js";
 import "../pb_dashboard/pbChartPlugin.js";
 import "./main.scss";
 import "./kits.js";
 
 // Move to separate file
-$(document).on('click', '[data-toggle]', function(e){
+$(document).on("click", "[data-toggle]", function(e) {
   e.preventDefault();
-  var kit_container = $(this).closest('.pb--doc');
-  var toggle_target = $(this).data('toggle');
-  $(kit_container).find('[data-action="toggle"]').hide();
-  $(kit_container).find('[data-togglable="'+toggle_target+'"]').show();
+  var kit_container = $(this).closest(".pb--doc");
+  var toggle_target = $(this).data("toggle");
+  $(kit_container)
+    .find('[data-action="toggle"]')
+    .hide();
+  $(kit_container)
+    .find('[data-togglable="' + toggle_target + '"]')
+    .show();
 });
