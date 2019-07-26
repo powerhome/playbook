bb9f36e661a6aea8969654b780d76d127f988d23
Markie Arnold <markiearnold@gmail.com>
2019-07-26 11:21:34 -0500

    KIT: Pill (#85)
    * Add pill kit
    
    * Move private method cleanup
    
    * Use standard typography sizes for playbook 2.0

--- app/pb_kits/playbook/packs/examples.js
+++ app/pb_kits/playbook/packs/examples.js
@@ -51,6 +51,9 @@ WebpackerReact.setup (Icon);
 import * as DashboardValue from "pb_dashboard_value/docs";
 WebpackerReact.setup (DashboardValue);
 
+import * as Pill from "pb_pill/docs";
+WebpackerReact.setup (Pill);
+
 import * as UserBadge from "pb_user_badge/docs";
 WebpackerReact.setup (UserBadge);
 
