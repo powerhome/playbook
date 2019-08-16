ac90c64dbe1ec7d932ffa270a8231599a40469d7
Markie Arnold <markiearnold@gmail.com>
2019-08-16 14:08:47 -0400

    Toggle Kit

--- app/pb_kits/playbook/pb_toggle/toggle.rb
+++ app/pb_kits/playbook/pb_toggle/toggle.rb
@@ -5,19 +5,19 @@ module Playbook
 					:configured_data,
 					:configured_id,
           :configured_size,
-          :configured_state
+          :configured_checked
         ].freeze
 
       def initialize(classname: default_configuration,
 							data: default_configuration,
 							id: default_configuration,
               size: default_configuration,
-              state: default_configuration)
+              checked: default_configuration)
         self.configured_classname = classname
 				self.configured_data = data
 				self.configured_id = id
         self.configured_size = size
-        self.configured_state = state
+        self.configured_checked = checked
       end
 
       def size
@@ -25,19 +25,19 @@ module Playbook
         one_of_value(configured_size, size_options, "md")
       end
 
-      def state
-        true_value(configured_state, "checked='true'", "")
+      def checked
+        true_value(configured_checked, "checked='true'", "")
       end
 
-      def state_class
-        state ? "on" : "off"
+      def checked_class
+        checked ? "on" : "off"
       end
 
       def kit_class
         toggle_options = [
           "pb_toggle",
           size,
-          state_class
+          checked_class
         ]
         toggle_options.join("_")
       end
