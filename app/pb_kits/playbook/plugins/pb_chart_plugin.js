d2133839f285e50d27b8f8447f0c94a7c7562cdb
Stephen Marshall <stephen.marshall@powerhrg.com>
2019-08-30 13:50:44 -0500

    pbChartPlugin refactor for exporting

--- app/pb_kits/playbook/plugins/pb_chart_plugin.js
+++ app/pb_kits/playbook/plugins/pb_chart_plugin.js
@@ -1,123 +1,94 @@
-import { highchartsTheme } from '../pb_dashboard/pbChartsLightTheme.js';
-
-if (typeof(pbChart) === "undefined") {
-
-  (function(root, factory) {
-    const plugin_name = 'pbChart';
-
-    if (typeof define === 'function' && define.amd) {
-      define([], factory(plugin_name));
-    } else if (typeof exports === 'object') {
-      module.exports = factory(plugin_name);
-    } else {
-      root[plugin_name] = factory(plugin_name);
-    }
-  }((window || module || {}), function(plugin_name) {
-    'use strict';
-
-    const plugin = {};
-
-    const defaults = {
-      callbackInitializeBefore: () => {},
-      callbackInitializeAfter: () => {},
-      callbackRefreshBefore: () => {},
-      callbackRefreshAfter: () => {},
-      callbackDestroyBefore: () => {},
-      callbackDestroyAfter: () => {},
-      property: 'Value'
-    };
-
-    function Plugin(element, options) {
-      plugin.this = this;
-      plugin.name = plugin_name;
-      plugin.element = element;
-      plugin.defaults = defaults;
-      plugin.options = options;
-      plugin.settings = extendDefaults(defaults, options);
-      plugin.this.initialize();
-    }
-
-
-    const extendDefaults = (defaults, options) => {
-      for (let property in options) {
-        if (options.hasOwnProperty(property)) {
-          defaults[property] = options[property];
-        }
-      }
-
-      return defaults;
-    };
-
-
-    Plugin.prototype = {
-      initialize: (silent = false) => {
-        plugin.this.destroySilently();
-
-        if (!silent) {
-          plugin.settings.callbackInitializeBefore.call();
-        }
-
-        Highcharts.setOptions(highchartsTheme);
-        Highcharts.chart(plugin.defaults.id, {
-          title: {
-            text: plugin.defaults.title
-          },
-          chart: {
-            type: plugin.defaults.type
-          },
-          subtitle: {
-            text: plugin.defaults.subtitle
-          },
-          yAxis: {
-            title: {
-              text: plugin.defaults.axisTitle
-            }
-          },
-          plotOptions: {
-            series: {
-              pointStart: plugin.defaults.pointStart
-            }
-          },
-          series: plugin.defaults.chartData,
-          credits: false,
-        });
-
-        if (!silent) {
-          plugin.settings.callbackInitializeAfter.call();
-        }
-      },
-
-      refresh: (silent = false) => {
-        if (!silent) {
-          plugin.settings.callbackRefreshBefore.call();
-        }
-
-        plugin.this.destroy(silent);
-        plugin.this.initialize(silent);
-        if (!silent) {
-          plugin.settings.callbackRefreshAfter.call();
-        }
-      },
-
-      destroy: (silent = false) => {
-        if (!silent) {
-          plugin.settings.callbackDestroyBefore.call();
-        }
-        if (!silent) {
-          plugin.settings.callbackDestroyAfter.call();
-        }
-      },
-
-      refreshSilently: () => {
-        plugin.this.refresh(true);
-      },
-
-      destroySilently: () => {
-        plugin.this.destroy(true);
-      }
-    };
-
-    return Plugin;
-  }))
-
+if(typeof(Highcharts) !== 'object') {
+  const script = document.createElement('script')
+  script.src   = 'https://code.highcharts.com/highcharts.js'
+  document.body.appendChild(script)
 }
+
+import { highchartsTheme } from '../pb_dashboard/pbChartsLightTheme.js'
+
+class pbChart {
+  defaults = {
+    callbackInitializeBefore: () => {},
+    callbackInitializeAfter: () => {},
+    callbackRefreshBefore: () => {},
+    callbackRefreshAfter: () => {},
+    callbackDestroyBefore: () => {},
+    callbackDestroyAfter: () => {},
+    property: 'Value'
+  }
+
+  extendDefaults(defaults, options) {
+    for (let property in options) {
+      if (options.hasOwnProperty(property)) {
+        defaults[property] = options[property]
+      }
+    }
+    return defaults
+  }
+
+  constructor(element, options) {
+    this.element = element
+    this.options = options
+    this.settings = this.extendDefaults(this.defaults, options)
+    this.setupChart()
+  }
+
+  setupChart() {
+    Highcharts.setOptions(highchartsTheme)
+
+    Highcharts.chart(this.defaults.id, {
+      title: {
+        text: this.defaults.title
+      },
+      chart: {
+        type: this.defaults.type
+      },
+      subtitle: {
+        text: this.defaults.subtitle
+      },
+      yAxis: {
+        title: {
+          text: this.defaults.axisTitle
+        }
+      },
+      plotOptions: {
+        series: {
+          pointStart: this.defaults.pointStart
+        }
+      },
+      series: this.defaults.chartData,
+      credits: false,
+    })
+  }
+
+  refresh(silent = false) {
+    if (!silent) {
+      this.settings.callbackRefreshBefore.call()
+    }
+
+    this.this.destroy(silent)
+    this.this.initialize(silent)
+    if (!silent) {
+      this.settings.callbackRefreshAfter.call()
+    }
+  }
+
+  destroy(silent = false) {
+    if (!silent) {
+      this.settings.callbackDestroyBefore.call()
+    }
+    if (!silent) {
+      this.settings.callbackDestroyAfter.call()
+    }
+  }
+
+  refreshSilently() {
+    this.this.refresh(true)
+  }
+
+  destroySilently() {
+    this.this.destroy(true)
+  }
+}
+
+export default pbChart
