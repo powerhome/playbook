7db2383735decd3a6fbcfd665882ed7d00b83db0
Stephen Marshall <stephen.marshall@powerhrg.com>
2019-09-11 11:15:55 -0500

    Enable Body kit

--- app/pb_kits/playbook/pb_body/docs/_body_dark.jsx
+++ app/pb_kits/playbook/pb_body/docs/_body_dark.jsx
@@ -7,6 +7,9 @@ function BodyDark() {
       <Body dark text="I am a body kit (Default)" />
       <Body dark color="light" text="I am a body kit (Light)" />
       <Body dark color="lighter" text="I am a body kit (Lighter)" />
+
+      <Body dark status="negative" text="I am a body kit (Status: negative)" />
+      <Body dark status="positive" text="I am a body kit (Status: positive)" />
     </div>
   )
 }
