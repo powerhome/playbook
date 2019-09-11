a6b338ecbeaab24dbd97e629e0baf7aef1462ac5
Stephen Marshall <stephen.marshall@powerhrg.com>
2019-09-10 21:52:24 -0500

    Enable BarGraph React kit

--- app/pb_kits/playbook/pb_bar_graph/_bar_graph.jsx
+++ app/pb_kits/playbook/pb_bar_graph/_bar_graph.jsx
@@ -1,21 +1,61 @@
-import React from 'react';
-import PropTypes from "prop-types";
+/* @flow */
 
-const propTypes = {
-  className: PropTypes.string,
-  id: PropTypes.string
-};
+import React from 'react'
 
-class BarGraph extends React.Component {
+import pbChart from "../plugins/pb_chart_plugin.js"
+
+type BarGraphProps = {
+  axisTitle: String,
+  chartData: Array<{
+    name: String,
+    data: Array<Number>,
+  }>,
+  className?: String,
+  id: Number,
+  pointStart: Number,
+  subTitle?: String,
+  title: String,
+}
+
+export default class BarGraph extends React.Component<BarGraphProps> {
+  static defaultProps = {
+    className: 'pb_bar_graph',
+    type: 'column',
+  }
+
+  componentDidMount() {
+    const {
+      axisTitle,
+      className,
+      chartData,
+      id,
+      pointStart,
+      subTitle,
+      title,
+      type,
+    } = this.props
+
+    new pbChart(`.${className}`, {
+      axisTitle: axisTitle,
+      chartData: chartData,
+      id: id,
+      pointStart: pointStart,
+      subtitle: subTitle,
+      type,
+      title: title,
+    })
+  }
+
+  props: BarGraphProps
+
   render() {
+    const { className, id } = this.props
+
     return (
-      <div className="pb_bar_graph">
-        <div id="highcharts-5nzaf00-0" />
-      </div>
+      <div
+          className={className}
+          id={id}
+      />
     )
   }
 }
-
-BarGraph.propTypes = propTypes;
-
-export default BarGraph;
--- app/pb_kits/playbook/pb_bar_graph/barGraphSettings.js
+++ app/pb_kits/playbook/pb_bar_graph/barGraphSettings.js
@@ -1,24 +1,24 @@
-import commonSettings from "../pb_dashboard/commonSettings.js";
+import commonSettings from "../pb_dashboard/commonSettings.js"
 
 const sizeColumns = function(highchart) {
-    let column = highchart.plotOptions.column;
-    let series = highchart.plotOptions.series;
+    let column = highchart.plotOptions.column
+    let series = highchart.plotOptions.series
     
-    column.borderRadius = 0;
-    column.pointPadding = 0.3;
-    column.groupPadding = 0.2;
+    column.borderRadius = 0
+    column.pointPadding = 0.3
+    column.groupPadding = 0.2
 
-    series.borderWidth = 0;
-    series.shadow = false;
-    series.pointPadding = 0.3;
-    series.groupPadding = 0.2;
-    series.borderWidth = 0;
-    series.shadow = false;
-};
+    series.borderWidth = 0
+    series.shadow = false
+    series.pointPadding = 0.3
+    series.groupPadding = 0.2
+    series.borderWidth = 0
+    series.shadow = false
+}
 
 const pbBarGraph = function(highchart) {
-  commonSettings(highchart);
-  sizeColumns(highchart);
-};
+  commonSettings(highchart)
+  sizeColumns(highchart)
+}
 
-export default pbBarGraph;
+export default pbBarGraph
