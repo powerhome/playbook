b69845d25fd49f7fcb5f178559cfff8b27c9d37a
Stephen Marshall <stephen.marshall@powerhrg.com>
2019-09-17 10:08:18 -0500

    Enable Currency React kit

--- app/pb_kits/playbook/pb_currency/_currency.jsx
+++ app/pb_kits/playbook/pb_currency/_currency.jsx
@@ -1,21 +1,70 @@
-import React from 'react';
-import PropTypes from "prop-types";
+/* @flow */
 
-const propTypes = {
-  className: PropTypes.string,
-  id: PropTypes.string
-};
+import React from 'react'
+import classnames from 'classnames'
 
-class Currency extends React.Component {
-  render() {
-    return (
-      <div className="pb_currency">
-        <span>{`CURRENCY CONTENT`}</span>
-      </div>
-    )
-  }
+import Body from '../pb_body/_body.jsx'
+import Caption from '../pb_caption/_caption.jsx'
+import Title from '../pb_title/_title.jsx'
+
+type CurrencyProps = {
+  align?: 'left' | 'center' | 'right',
+  className: String,
+  currencySymbol: '$',
+  label?: String,
+  separator?: '.' | ',',
+  size?: 'sm' | 'lg',
+  value?: String,
+  unit?: '00',
 }
 
-Currency.propTypes = propTypes;
+const kitCSS = ({align='left'}: CurrencyProps) => {
+  let css = 'pb_currency_kit'
+  css += `_${align}`
+  return css
+}
 
-export default Currency;
+const bodyCSS = ({}: CurrencyProps) => {
+  let css = ''
+  return css
+}
+
+const symbolCSS = (currencySymbol) => {
+  return classnames({
+    dollar_sign: currencySymbol === '$',
+  })
+}
+
+const sizes = {
+  lg: 1,
+  sm: 2,
+}
+
+const Currency = (props: CurrencyProps) => {
+  const {
+    currencySymbol='$',
+    label='',
+    separator='.',
+    size='sm',
+    units='00',
+    value,
+  } = props
+
+  return (
+    <div className={kitCSS(props)}>
+      <Caption>{label}</Caption>
+      <div className='pb_currency_wrapper'>
+        <Body className={symbolCSS(currencySymbol)}>{currencySymbol}</Body>
+        <Title
+            className='pb_currency_value'
+            size={sizes[size]}
+        >
+          {`${value}${separator}`}
+        </Title>
+        <Body className='unit'>{units}</Body>
+      </div>
+    </div>
+  )
+}
+
+export default Currency
--- app/pb_kits/playbook/pb_currency/docs/index.js
+++ app/pb_kits/playbook/pb_currency/docs/index.js
@@ -1,1 +1,2 @@
 export {default as CurrencyDefault} from './_currency_default.jsx';
+export {default as CurrencyLarge} from './_currency_large.jsx';
