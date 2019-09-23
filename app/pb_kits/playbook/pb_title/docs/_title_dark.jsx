d2250da2c3bceec30387cdfb2165d4f89a152f05
Markie Arnold <markiearnold@gmail.com>
2019-09-23 11:04:14 -0400

    Update _title_dark.jsx

--- app/pb_kits/playbook/pb_title/docs/_title_dark.jsx
+++ app/pb_kits/playbook/pb_title/docs/_title_dark.jsx
@@ -4,10 +4,10 @@ import Title from "../_title.jsx"
 function TitleDark() {
   return (
     <div>
-      <Title dark size={4} tag="h4" text="Title 1" />
-      <Title dark size={3} tag="h3" text="Title 2" />
-      <Title dark size={2} tag="h2" text="Title 3" />
-      <Title dark size={1} tag="h1" text="Title 4" />
+      <Title dark size={4} tag="h4" text="Title 4" />
+      <Title dark size={3} tag="h3" text="Title 3" />
+      <Title dark size={2} tag="h2" text="Title 2" />
+      <Title dark size={1} tag="h1" text="Title 1" />
     </div>
   )
 }
