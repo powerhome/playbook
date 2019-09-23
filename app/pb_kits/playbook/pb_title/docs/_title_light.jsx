e298242821d130f533826e8d8690512e9681702c
Markie Arnold <markiearnold@gmail.com>
2019-09-23 11:04:31 -0400

    Update _title_light.jsx

--- app/pb_kits/playbook/pb_title/docs/_title_light.jsx
+++ app/pb_kits/playbook/pb_title/docs/_title_light.jsx
@@ -5,10 +5,10 @@ function TitleLight() {
   return (
     <div>
       <Title text="Default Title"/><br/>
-      <Title size={4} tag="h4" text="Title 1" />
-      <Title size={3} tag="h3" text="Title 2" />
-      <Title size={2} tag="h2" text="Title 3" />
-      <Title size={1} tag="h1" text="Title 4" />
+      <Title size={4} tag="h4" text="Title 4" />
+      <Title size={3} tag="h3" text="Title 3" />
+      <Title size={2} tag="h2" text="Title 2" />
+      <Title size={1} tag="h1" text="Title 1" />
     </div>
   )
 }
