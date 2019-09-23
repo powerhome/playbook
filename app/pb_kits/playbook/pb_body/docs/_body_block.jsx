50d9ad5f17888e4b85814ed6d0619c2686b2cc28
Markie Arnold <markiearnold@gmail.com>
2019-09-23 15:43:53 -0400

    Resolves gh-265 by adding optional block or text prop

---
+++ app/pb_kits/playbook/pb_body/docs/_body_block.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Body from "../_body.jsx"
+
+function BodyBlock() {
+  return (
+    <div>
+      <Body>
+        I am a body kit
+      </Body>
+    </div>
+  )
+}
+
+export default BodyBlock;
--- app/pb_kits/playbook/pb_body/docs/index.js
+++ app/pb_kits/playbook/pb_body/docs/index.js
@@ -1,2 +1,3 @@
 export {default as BodyDark} from './_body_dark.jsx';
 export {default as BodyLight} from './_body_light.jsx';
+export {default as BodyBlock} from './_body_block.jsx';
