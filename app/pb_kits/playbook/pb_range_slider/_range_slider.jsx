ce9bfe4cead06717f2ed37ca6cfbf156ceaf106f
Diego Borges <drborges.cic@gmail.com>
2019-09-23 23:53:04 -0400

    Add React version

--- app/pb_kits/playbook/pb_range_slider/_range_slider.jsx
+++ app/pb_kits/playbook/pb_range_slider/_range_slider.jsx
@@ -2,6 +2,7 @@
 /*eslint-disable react/no-multi-comp, flowtype/space-before-type-colon */
 
 import React from 'react'
+import classnames from 'classnames'
 
 type RangeSliderProps = {
   className?: String,
@@ -11,13 +12,37 @@ type RangeSliderProps = {
   min?: String,
   step?: String,
   value?: String,
-  
+
 }
 
-const RangeSlider = ({ className, data, id, max, min, step, value }: RangeSliderProps) => (
-  <div>
-    {`kit content`}
-  </div>
-)
+const RangeSlider = ({
+  className,
+  data = {},
+  id,
+  max = 100,
+  min = 0,
+  onChange,
+  step = 1,
+  value,
+}: RangeSliderProps) => {
+  const dataProps = {}
+
+  for (let key in data) {
+    dataProps[`data-${key}`] = data[key]
+  }
+
+  return (
+    <div {...dataProps} id={id} className={classnames("pb_range_slider", className)}>
+      <input
+          max={max}
+          min={min}
+          step={step}
+          type="range"
+          value={value}
+          onChange={onChange}
+      />
+    </div>
+  )
+}
 
 export default RangeSlider
--- app/pb_kits/playbook/pb_range_slider/docs/_range_slider_default.jsx
+++ app/pb_kits/playbook/pb_range_slider/docs/_range_slider_default.jsx
@@ -1,10 +1,21 @@
 import React from "react"
 import RangeSlider from "../_range_slider.jsx"
 
-function RangeSliderDefault() {
+const RangeSliderDefault = () => {
   return (
     <div>
-      <RangeSlider />
+      <RangeSlider id={123} />
+
+      <br /><br />
+
+      <RangeSlider
+          className="ml-5"
+          data={{ a: 123, b: 321}}
+          max={200}
+          min={10}
+          onChange={e => console.log("on change triggered!")}
+          step={10}
+      />
     </div>
   )
 }
