35554636a17140e64dd8dfcfd0c91abc1473187c
Katie Weber <k.weber0893@gmail.com>
2019-09-24 16:09:38 -0400

    WIP - woooo the rails kit works

--- app/pb_kits/playbook/pb_owner_project/owner_project.rb
+++ app/pb_kits/playbook/pb_owner_project/owner_project.rb
@@ -3,25 +3,31 @@
 module Playbook
   module PbOwnerProject
     class OwnerProject < Playbook::PbKit::Base
+      include ActionView::Helpers::TagHelper
+      include ActionView::Context
+
       PROPS = %i[configured_classname
                  configured_data
                  configured_id
                  configured_project_number
                  configured_name
-                 configured_date].freeze
+                 configured_date
+                 configured_link].freeze
 
       def initialize(classname: default_configuration,
                      data: default_configuration,
                      id: default_configuration,
                      project_number: default_configuration,
                      name: default_configuration,
-                     date: default_configuration)
+                     date: default_configuration,
+                     link: default_configuration)
         self.configured_classname = classname
         self.configured_data = data
         self.configured_id = id
         self.configured_project_number = project_number
         self.configured_name = name
         self.configured_date = date
+        self.configured_link = link
       end
 
       def to_partial_path
@@ -39,6 +45,18 @@ module Playbook
       def date
         if is_set? configured_date
           configured_date.strftime("%m/%d")
+        end
+      end
+
+      def display_title
+        if is_set? configured_link
+          content_tag(:a, href: configured_link, class: "pb_owner_project_link_title") do
+            title
+          end
+        else
+          content_tag(:span) do
+            title
+          end
         end
       end
 
