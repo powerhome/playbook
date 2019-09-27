1f5a4a2681c0c28979aceec049f213287bd3c345
Garett Arrowood <garettarrowood@gmail.com>
2019-09-27 09:44:38 -0400

    Remove db directories

--- db/migrate/20171025051859_add_page_ref_to_page_tags.rb
+++
@@ -1,5 +0,0 @@
-class AddPageRefToPageTags < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :page_tags, :page, foreign_key: true
-  end
-end
--- db/migrate/20180328145144_drop_friendly_id_slugs.rb
+++
@@ -1,6 +0,0 @@
-class DropFriendlyIdSlugs < ActiveRecord::Migration[5.1]
-  def change
-
-    drop_table :friendly_id_slugs, force: :cascade
-  end
-end
