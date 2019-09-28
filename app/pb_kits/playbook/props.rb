95001d9548e56eb019c5107d2051537efaffcd6e
Garett Arrowood <garettarrowood@gmail.com>
2019-09-28 16:36:58 -0400

    Getting constants to load for example

--- app/pb_kits/playbook/props.rb
+++ app/pb_kits/playbook/props.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "active_support/concern"
+
 module Playbook
   module Props
     extend ActiveSupport::Concern
---
+++ spec/pb_kits/playbook/props/base_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Playbook
+  module Props
+    describe Base do
+      it "getting test suite to load" do
+        # byebug
+      end
+    end
+  end
+end
