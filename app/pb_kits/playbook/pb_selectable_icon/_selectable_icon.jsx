d63798e68245358e6cddc5ee51b764d549d13079
christina tai <christina.tai@powerhrg.com>
2019-09-30 16:31:17 -0400

    React part of Selectable Icon Kit Created

--- app/pb_kits/playbook/pb_selectable_icon/_selectable_icon.jsx
+++ app/pb_kits/playbook/pb_selectable_icon/_selectable_icon.jsx
@@ -1,21 +1,52 @@
 /* @flow */
-/*eslint-disable react/no-multi-comp, flowtype/space-before-type-colon */
 
 import React from 'react'
+import {
+  Title
+} from '../'
 
 type SelectableIconProps = {
   className?: String,
-  data?: String,
-  id?: String,
-  label?: String,
-  select?: Boolean,
-  
+  tag: String,
+  label: String,
+  checked?: Boolean,
+  icon: String,
+  id: String,
+  name: String,
+  value: String
 }
 
-const SelectableIcon = ({ className, data, id, label, select }: SelectableIconProps) => (
-  <div>
-    {`kit content`}
-  </div>
-)
+const SelectableIcon = ({
+  icon,
+  className='',
+  tag='label',
+  label='',
+  checked=false,
+  id='',
+  name='',
+  value='',
+}: SelectableIconProps) => {
+
+  const Tag = `${tag}`
+  const iconClass = `far fa-${icon} fa-2x`
+  const kitClass = `pb_selectable_icon_kit`
+
+  return (
+    <Tag
+    className={kitClass}>
+      <div className="pb_selectable_icon_wrapper">
+      <input type="checkbox" defaultChecked={checked}/>
+        <div className="pb_selectable_icon_kit_icon">
+          <i className={iconClass}></i>
+        </div>
+        <Title
+          className="pb_selectable_icon_kit_title"
+          size={4}
+          text={label}
+          />
+      </div>
+    </Tag>
+  )
+}
 
 export default SelectableIcon
--- app/pb_kits/playbook/pb_selectable_icon/docs/_selectable_icon_default.jsx
+++ app/pb_kits/playbook/pb_selectable_icon/docs/_selectable_icon_default.jsx
@@ -4,7 +4,18 @@ import SelectableIcon from "../_selectable_icon.jsx"
 function SelectableIconDefault() {
   return (
     <div>
-      <SelectableIcon />
+      <SelectableIcon
+        icon="cog"
+        label="Title"
+      />
+
+      <br/><br/>
+
+      <SelectableIcon
+        icon="calendar"
+        label="Title"
+        checked="true"
+      />
     </div>
   )
 }
