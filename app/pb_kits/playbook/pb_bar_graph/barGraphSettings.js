a5d5f9d4e0bf5af835294d5461ef5fab6ffdb245
Jasper Furniss <jasper.furniss@powerhrg.com>
2019-10-07 15:26:23 -0400

    Styles Added for Text Labels on Line and Bar Graphs

--- app/pb_kits/playbook/pb_bar_graph/barGraphSettings.js
+++ app/pb_kits/playbook/pb_bar_graph/barGraphSettings.js
@@ -1,35 +1,31 @@
-import commonSettings from "../pb_dashboard/commonSettings.js"
-import colors from "../tokens/_colors.scss"
+import commonSettings from "../pb_dashboard/commonSettings.js";
 import typography from "../tokens/_typography.scss";
 
 const sizeColumns = function(highchart) {
-    let column = highchart.plotOptions.column
-    let series = highchart.plotOptions.series
-    
-    column.borderRadius = 0
-    column.pointPadding = 0.3
-    column.groupPadding = 0.2
+  let column = highchart.plotOptions.column;
+  let series = highchart.plotOptions.series;
 
-    series.borderWidth = 0
-    series.shadow = false
-    series.pointPadding = 0.3
-    series.groupPadding = 0.2
-}
+  column.borderRadius = 0;
+  column.pointPadding = 0.3;
+  column.groupPadding = 0.2;
 
-const colorDataLabels = (highchart) => {
-    let column = highchart.plotOptions.column
-    let series = highchart.plotOptions.series
+  series.borderWidth = 0;
+  series.shadow = false;
+  series.pointPadding = 0.3;
+  series.groupPadding = 0.2;
+};
 
-    series.dataLabels.color = colors.slate
-    series.dataLabels.style.fontFamily = typography.font_family_base;
-    series.dataLabels.style.fontSize = typography.font_small;
-    
-}
+const styleDataLabels = highchart => {
+  let series = highchart.plotOptions.series;
+
+  series.dataLabels.style.fontFamily = typography.font_family_base;
+  series.dataLabels.style.fontSize = typography.text_small;
+};
 
 const barGraphSettings = function(highchart) {
-  commonSettings(highchart)
-  sizeColumns(highchart)
-  colorDataLabels(highchart)
-}
+  commonSettings(highchart);
+  sizeColumns(highchart);
+  styleDataLabels(highchart);
+};
 
-export default barGraphSettings
+export default barGraphSettings;
--- app/pb_kits/playbook/pb_line_graph/lineGraphSettings.js
+++ app/pb_kits/playbook/pb_line_graph/lineGraphSettings.js
@@ -1,20 +1,29 @@
-import commonSettings from "../pb_dashboard/commonSettings.js"
+import commonSettings from "../pb_dashboard/commonSettings.js";
+import typography from "../tokens/_typography.scss";
 
 const markerStyles = highchart => {
-  highchart.plotOptions.line.marker.enabled = true
-  highchart.plotOptions.series.marker.enabled = true
-  highchart.plotOptions.series.marker.fillColor = "white"
-  highchart.plotOptions.series.marker.lineWidth = 2
-}
+  highchart.plotOptions.line.marker.enabled = true;
+  highchart.plotOptions.series.marker.enabled = true;
+  highchart.plotOptions.series.marker.fillColor = "white";
+  highchart.plotOptions.series.marker.lineWidth = 2;
+};
 
 const fixTooltipStyles = () => {
-  document.getElementById("main-view").setAttribute("style", "position: static !important")
-}
+  document.getElementById("main-view").setAttribute("style", "position: static !important");
+};
+
+const styleDataLabels = highchart => {
+  let series = highchart.plotOptions.series;
+
+  series.dataLabels.style.fontFamily = typography.font_family_base;
+  series.dataLabels.style.fontSize = typography.text_small;
+};
 
 const pbLineGraph = highchart => {
-  commonSettings(highchart)
-  markerStyles(highchart)
-  fixTooltipStyles()
-}
+  commonSettings(highchart);
+  styleDataLabels(highchart);
+  markerStyles(highchart);
+  fixTooltipStyles();
+};
 
-export default pbLineGraph
+export default pbLineGraph;
