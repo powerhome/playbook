d6079b4deb1a764bee62b0758336ac41b625f0df
Garett Arrowood <garettarrowood@gmail.com>
2019-10-08 08:24:54 -0400

    Reconfigure spec_helper & test assertions

--- spec/pb_kits/playbook/kits/checkbox_spec.rb
+++ spec/pb_kits/playbook/kits/checkbox_spec.rb
@@ -2,29 +2,25 @@
 
 require_relative "../../../../app/pb_kits/playbook/pb_checkbox/checkbox"
 
-module Playbook
-  module PbCheckbox
-    describe Checkbox do
-      subject { Checkbox }
+RSpec.describe Playbook::PbCheckbox::Checkbox do
+  subject { Playbook::PbCheckbox::Checkbox }
 
-      it { is_expected.to define_partial }
+  it { is_expected.to define_partial }
 
-      it { is_expected.to define_boolean_prop(:dark).with_default(false) }
-      it { is_expected.to define_prop(:text) }
-      it { is_expected.to define_prop(:value) }
-      it { is_expected.to define_prop(:name) }
-      it { is_expected.to define_boolean_prop(:checked).with_default(false) }
-      it { is_expected.to define_boolean_prop(:icon).with_default(false) }
+  it { is_expected.to define_boolean_prop(:dark).with_default(false) }
+  it { is_expected.to define_prop(:text) }
+  it { is_expected.to define_prop(:value) }
+  it { is_expected.to define_prop(:name) }
+  it { is_expected.to define_boolean_prop(:checked).with_default(false) }
+  it { is_expected.to define_boolean_prop(:icon).with_default(false) }
 
-      describe "#classname" do
-        it "returns namespaced class name", :aggregate_failures do
-          expect(Checkbox.new({}).classname).to eq "pb_checkbox_kit_off"
-          expect(Checkbox.new(dark: true).classname).to eq "pb_checkbox_kit_dark_off"
-          expect(Checkbox.new(checked: true).classname).to eq "pb_checkbox_kit_on"
-          expect(Checkbox.new(dark: true, checked: true).classname).to eq "pb_checkbox_kit_dark_on"
-          expect(Checkbox.new(classname: "additional_class").classname).to eq "pb_checkbox_kit_off additional_class"
-        end
-      end
+  describe "#classname" do
+    it "returns namespaced class name", :aggregate_failures do
+      expect(subject.new({}).classname).to eq "pb_checkbox_kit_off"
+      expect(subject.new(dark: true).classname).to eq "pb_checkbox_kit_dark_off"
+      expect(subject.new(checked: true).classname).to eq "pb_checkbox_kit_on"
+      expect(subject.new(dark: true, checked: true).classname).to eq "pb_checkbox_kit_dark_on"
+      expect(subject.new(classname: "additional_class").classname).to eq "pb_checkbox_kit_off additional_class"
     end
   end
 end
--- spec/pb_kits/playbook/props/enum_spec.rb
+++ spec/pb_kits/playbook/props/enum_spec.rb
@@ -1,28 +1,24 @@
 # frozen_string_literal: true
 
-module Playbook
-  module Props
-    describe Enum do
-      describe "#validate" do
-        it "returns true given value supplied upon initialization", :aggregate_failures do
-          enum = Enum.new(values: [1, "two", :three])
+RSpec.describe Playbook::Props::Enum do
+  describe "#validate" do
+    it "returns true given value supplied upon initialization", :aggregate_failures do
+      enum = Playbook::Props::Enum.new(values: [1, "two", :three])
 
-          expect(enum.validate(1)).to eq true
-          expect(enum.validate("two")).to eq true
-          expect(enum.validate(:three)).to eq true
-        end
+      expect(enum.validate(1)).to eq true
+      expect(enum.validate("two")).to eq true
+      expect(enum.validate(:three)).to eq true
+    end
 
-        it "returns false given anything else", :aggregate_failures do
-          enum = Enum.new(values: [])
+    it "returns false given anything else", :aggregate_failures do
+      enum = Playbook::Props::Enum.new(values: [])
 
-          expect(enum.validate(true)).to eq false
-          expect(enum.validate(:foo)).to eq false
-          expect(enum.validate(1)).to eq false
-          expect(enum.validate({})).to eq false
-          expect(enum.validate([])).to eq false
-          expect(enum.validate(nil)).to eq false
-        end
-      end
+      expect(enum.validate(true)).to eq false
+      expect(enum.validate(:foo)).to eq false
+      expect(enum.validate(1)).to eq false
+      expect(enum.validate({})).to eq false
+      expect(enum.validate([])).to eq false
+      expect(enum.validate(nil)).to eq false
     end
   end
 end
