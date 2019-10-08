70d3bf8de995a0adaca1c1aa14c1d7fa22525a6c
Garett Arrowood <garettarrowood@gmail.com>
2019-10-08 07:32:53 -0400

    Invoke #describe on Rspec, & use full class constant in spec

--- spec/pb_kits/playbook/kits/badge_spec.rb
+++ spec/pb_kits/playbook/kits/badge_spec.rb
@@ -2,27 +2,22 @@
 
 require_relative "../../../../app/pb_kits/playbook/pb_badge/badge"
 
-module Playbook
-  module PbBadge
-    describe Badge do
-      subject { Badge }
+RSpec.describe Playbook::PbBadge::Badge do
+  subject { Playbook::PbBadge::Badge }
 
-      it { is_expected.to define_partial }
+  it { is_expected.to define_partial }
 
-      it { is_expected.to define_prop(:text) }
-      it { is_expected.to define_boolean_prop(:rounded).with_default(false) }
-      it { is_expected.to define_enum_prop(:variant).with_default("neutral") }
+  it { is_expected.to define_prop(:text) }
+  it { is_expected.to define_boolean_prop(:rounded).with_default(false) }
+  it { is_expected.to define_enum_prop(:variant).with_default("neutral") }
 
-
-      describe "#classname" do
-        it "returns namespaced class name", :aggregate_failures do
-          expect(Badge.new({}).classname).to eq "pb_badge_kit_neutral"
-          expect(Badge.new(variant: "warning").classname).to eq "pb_badge_kit_warning"
-          expect(Badge.new(rounded: true).classname).to eq "pb_badge_kit_neutral_rounded"
-          expect(Badge.new(variant: "error", rounded: true).classname).to eq "pb_badge_kit_error_rounded"
-          expect(Badge.new(classname: "additional_class").classname).to eq "pb_badge_kit_neutral additional_class"
-        end
-      end
+  describe "#classname" do
+    it "returns namespaced class name", :aggregate_failures do
+      expect(subject.new({}).classname).to eq "pb_badge_kit_neutral"
+      expect(subject.new(variant: "warning").classname).to eq "pb_badge_kit_warning"
+      expect(subject.new(rounded: true).classname).to eq "pb_badge_kit_neutral_rounded"
+      expect(subject.new(variant: "error", rounded: true).classname).to eq "pb_badge_kit_error_rounded"
+      expect(subject.new(classname: "additional_class").classname).to eq "pb_badge_kit_neutral additional_class"
     end
   end
 end
