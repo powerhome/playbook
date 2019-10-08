45d68fd10f1d7173709bb94aa5015d92556e0942
Leila Weiss <leila.weiss@powerhrg.com>
2019-10-08 13:03:04 -0400

    Change values to widths

--- app/pb_kits/playbook/pb_distribution_bar/_distribution_bar.jsx
+++ app/pb_kits/playbook/pb_distribution_bar/_distribution_bar.jsx
@@ -8,39 +8,37 @@ type DistributionBarProps = {
   data?: String,
   id?: String,
   size?: 'lg' | 'sm',
-  values?: Array<Number>,
+  widths?: Array<Number>,
 }
 
-const normalizeCharacters = (values) => {
-  return values.map( value => {
-    return parseInt(value.toString().replace(/[^0-9.]/gi, ''))
+const normalizeCharacters = (widths) => {
+  return widths.map(width => {
+    return parseInt(width.toString().replace(/[^0-9.]/gi, ''))
   })
 }
 
 const barValues = (normalizedValues) => {
   let arrSum = value => value.reduce((a,b) => (a + b), 0)
-  let valueSum = arrSum(normalizedValues)
+  let widthSum = arrSum(normalizedValues)
   return normalizedValues.map((value,i) => {
     return(
       <div
           key={i}
-          className={`pb_distribution_value`}
-          style={{width:`${value*100/valueSum}%`}}
+          className={`pb_distribution_width`}
+          style={{width:`${value*100/widthSum}%`}}
       />
     )
   })
 }
-
-
 
 const DistributionBar = ({
     className,
     data,
     id,
     size='lg',
-    values=[1]
+    widths=[1]
   }: DistributionBarProps) => {
-    const normalizedValues = normalizeCharacters(values)
+    const normalizedValues = normalizeCharacters(widths)
 
     return(
       <div className={`pb_distribution_bar_${size}`}>
