93993f910cd175ff3868efb2c4c7fd02e8ed37bf
Leila Weiss <leila.weiss@powerhrg.com>
2019-10-08 08:46:07 -0400

    Create distrubution bar spec file

---
+++ spec/pb_kits/playbook/kits/distribution_bar_spec.rb
@@ -0,0 +1,7 @@
+# # frozen_string_literal: true
+
+# require_relative "../../../../app/pb_kits/playbook/pb_distribution_bar/distribution_bar"
+
+# RSpec.describe Playbook::PbDistributionBar::DistributionBar do
+#   subject { Playbook::PbDistributionBar::DistributionBar }
+
