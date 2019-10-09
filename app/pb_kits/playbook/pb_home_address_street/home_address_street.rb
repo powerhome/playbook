836fe4720abe124a83ae4b6e23301473d4aa94d8
Kevin Skilton <kevin.skilton@powerhrg.com>
2019-10-09 14:59:13 -0400

    Initial move to modernize

--- app/pb_kits/playbook/pb_home_address_street/home_address_street.rb
+++ app/pb_kits/playbook/pb_home_address_street/home_address_street.rb
@@ -2,79 +2,28 @@
 
 module Playbook
   module PbHomeAddressStreet
-    class HomeAddressStreet < Playbook::PbKit::Base
-      PROPS = %i[configured_address
-                 configured_city
-                 configured_classname
-                 configured_dark
-                 configured_data
-                 configured_home_id
-                 configured_house_style
-                 configured_id
-                 configured_state
-                 configured_zipcode].freeze
+    class HomeAddressStreet
+      include Playbook::Props
 
-      def initialize(address: default_configuration,
-                     city: default_configuration,
-                     classname: default_configuration,
-                     dark: default_configuration,
-                     data: default_configuration,
-                     home_id: default_configuration,
-                     house_style: default_configuration,
-                     id: default_configuration,
-                     state: default_configuration,
-                     zipcode: default_configuration)
-        self.configured_address = address
-        self.configured_city = city
-        self.configured_classname = classname
-        self.configured_dark = dark
-        self.configured_data = data
-        self.configured_home_id = home_id
-        self.configured_house_style = house_style
-        self.configured_state = state
-        self.configured_zipcode = zipcode
-      end
+      partial "pb_home_address_street/home_address_street"
 
-      def address
-        default_value(configured_address, "")
-      end
+      prop :address
+      prop :city
+      prop :home_id, type: Playbook::Props::Number
+      prop :house_style
+      prop :state
+      prop :zipcode
+      prop :dark, type: Playbook::Props::Boolean, default: false
 
-      def city
-        default_value(configured_city, "")
-      end
-
-      def dark
-        true_value(configured_dark, true, false)
-      end
-
-      def home_id
-        default_value(configured_home_id, "")
-      end
-
-      def house_style
-        default_value(configured_house_style, "")
-      end
-
-      def state
-        default_value(configured_state, "")
-      end
-
-      def zipcode
-        default_value(configured_zipcode, "")
-      end
-
-      def to_partial_path
-        "pb_home_address_street/home_address_street"
+      def classname
+        generate_classname("pb_home_address_street_kit", dark_class)
       end
 
     private
 
-      DEFAULT = Object.new
-      private_constant :DEFAULT
-      def default_configuration
-        DEFAULT
+      def dark_class
+        dark ? "dark" : nil
       end
-      attr_accessor(*PROPS)
     end
   end
 end
