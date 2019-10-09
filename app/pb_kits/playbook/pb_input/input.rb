3b9ec6e2b04851faade38c9bc9ec17c7ca65ad49
Kyle Fadigan <kfadigan@yahoo.com>
2019-10-09 15:52:40 -0400

    Turn old props into new syntax

--- app/pb_kits/playbook/pb_input/input.rb
+++ app/pb_kits/playbook/pb_input/input.rb
@@ -7,56 +7,11 @@ module Playbook
 
       partial "pb_input/input"
 
-        PROPS = %i[
-                   configured_name
-                   configured_label
-                   configured_placeholder
-                   configured_type
-                   configured_value].freeze
-
-        def label
-          if configured_label == default_configuration
-            ""
-          else
-            configured_label
-          end
-        end
-
-        def name
-          if configured_name == default_configuration
-            ""
-          else
-            configured_name
-          end
-        end
-
-        def placeholder
-          if configured_placeholder == default_configuration
-            ""
-          else
-            configured_placeholder
-          end
-        end
-
-        def type
-          if configured_type == default_configuration
-            "text"
-          else
-            configured_type
-          end
-        end
-
-        def value
-          if configured_value == default_configuration
-            ""
-          else
-            configured_value
-          end
-        end
-
-        def to_partial_path
-          "pb_input/input"
-        end
+        prop :label
+        prop :name
+        prop :placeholder
+        prop :value
+        prop :type, default: "text"
       end
     end
   end
