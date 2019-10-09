ac705d86b513e1b19ff091e9147ba7e3b56e51e5
Stephen Marshall <stephen.marshall@powerhrg.com>
2019-10-09 11:26:33 -0500

    Update Sisense Highcharts Styles and Bug Fixes (#319)
    * Update series linegraph point colors
    
    * Fix tooltip offset bug
    
    * Adjusted colors and text for Column Chart for readability
    
    * Styles Added for Text Labels on Line and Bar Graphs
    
    * Fix lineColor column chart bug

--- app/pb_kits/playbook/pb_bar_graph/barGraphSettings.js
+++ app/pb_kits/playbook/pb_bar_graph/barGraphSettings.js
@@ -1,24 +1,31 @@
-import commonSettings from "../pb_dashboard/commonSettings.js"
+import commonSettings from "../pb_dashboard/commonSettings.js";
+import typography from "../tokens/_typography.scss";
 
 const sizeColumns = function(highchart) {
-    let column = highchart.plotOptions.column
-    let series = highchart.plotOptions.series
-    
-    column.borderRadius = 0
-    column.pointPadding = 0.3
-    column.groupPadding = 0.2
+  let column = highchart.plotOptions.column;
+  let series = highchart.plotOptions.series;
 
-    series.borderWidth = 0
-    series.shadow = false
-    series.pointPadding = 0.3
-    series.groupPadding = 0.2
-    series.borderWidth = 0
-    series.shadow = false
-}
+  column.borderRadius = 0;
+  column.pointPadding = 0.3;
+  column.groupPadding = 0.2;
 
-const pbBarGraph = function(highchart) {
-  commonSettings(highchart)
-  sizeColumns(highchart)
-}
+  series.borderWidth = 0;
+  series.shadow = false;
+  series.pointPadding = 0.3;
+  series.groupPadding = 0.2;
+};
 
-export default pbBarGraph
+const styleDataLabels = highchart => {
+  let series = highchart.plotOptions.series;
+
+  series.dataLabels.style.fontFamily = typography.font_family_base;
+  series.dataLabels.style.fontSize = typography.text_small;
+};
+
+const barGraphSettings = function(highchart) {
+  commonSettings(highchart);
+  sizeColumns(highchart);
+  styleDataLabels(highchart);
+};
+
+export default barGraphSettings;
