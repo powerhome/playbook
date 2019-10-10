e1508b55d343ab45b62a5612e9450655e29361e8
Jasper Furniss <jasper.furniss@powerhrg.com>
2019-10-10 14:28:34 -0400

    Latest WIP

--- app/pb_kits/playbook/pb_currency/currency.rb
+++ app/pb_kits/playbook/pb_currency/currency.rb
@@ -2,85 +2,49 @@
 
 module Playbook
   module PbCurrency
-    class Currency < Playbook::PbKit::Base
+    class Currency
+      include Playbook::Props
       include ActionView::Helpers::NumberHelper
-      PROPS = %i[configured_align
-                 configured_classname
-                 configured_data
-                 configured_id
-                 configured_label
-                 configured_value
-                 configured_size].freeze
+     
+      prop :align, type: Playbook::Props::Enum,
+                   values: %w[left center right],
+                   default: "left"
+      prop :label, type: Playbook::PbCaption::Caption.new(text: label)
+      prop :amount, required: true
+      prop :size, type: Playbook::Props::Enum,
+                  values: %w[lg sm],
+                  default: "sm"
 
-      def initialize(align: default_configuration,
-                     classname: default_configuration,
-                     data: default_configuration,
-                     id: default_configuration,
-                     label: default_configuration,
-                     value: default_configuration,
-                     size: default_configuration)
-        self.configured_align = align
-        self.configured_classname = classname
-        self.configured_data = data
-        self.configured_id = id
-        self.configured_label = label
-        self.configured_value = value
-        self.configured_size = size
+      def classname
+        generate_classname("pb_currency_kit")
       end
 
-      def align
-        align_options = %w[left center right]
-        one_of_value(configured_align, align_options, "left")
-      end
+      # def align
+      #   align_options = %w[left center right]
+      #   one_of_value(configured_align, align_options, "left")
+      # end
 
-      def label
-        if is_set? configured_label
-          pb_label = Playbook::PbCaption::Caption.new(text: configured_label)
-          ApplicationController.renderer.render(partial: pb_label, as: :object)
+      # def label
+      #   if is_set? configured_label
+      #     pb_label = Playbook::PbCaption::Caption.new(text: configured_label)
+      #     ApplicationController.renderer.render(partial: pb_label, as: :object)
+      #   end
+      # end
+
+      # def size
+      #   size_options = %w[lg sm]
+      #   one_of_value(configured_size, size_options, "sm")
+      # end
+
+      # def value_size
+      #   size == "lg" ? 1 : 2
+      # end
+
+      def amount
+        if is_set? configured_amount
+          display_amount = formatted_amount[1...-2]
+          display_amount
         end
-      end
-
-      def size
-        size_options = %w[lg sm]
-        one_of_value(configured_size, size_options, "sm")
-      end
-
-      def value_size
-        size == "lg" ? 1 : 2
-      end
-
-      def formatted_value
-        number_to_currency(configured_value)
-      end
-
-      def value
-        if is_set? configured_value
-          display_value = formatted_value[1...-2]
-          pb_title = Playbook::PbTitle::Title.new(size: value_size, text: display_value, classname: "pb_currency_value")
-          ApplicationController.renderer.render(partial: pb_title, as: :object)
-        end
-      end
-
-      def unit
-        pb_unit = Playbook::PbBody::Body.new(classname: "unit", color: "light") do
-          formatted_value.last(2)
-        end
-        ApplicationController.renderer.render(partial: pb_unit, as: :object)
-      end
-
-      def currency_indicator
-        pb_currency_indicator = Playbook::PbBody::Body.new(classname: "currency_indicator", color: "light") do
-          formatted_value.first(1)
-        end
-        ApplicationController.renderer.render(partial: pb_currency_indicator, as: :object)
-      end
-
-      def kit_class
-        kit_options = [
-          "pb_currency_kit",
-          align,
-        ]
-        kit_options.join("_")
       end
 
       def to_partial_path
@@ -89,12 +53,23 @@ module Playbook
 
     private
 
-      DEFAULT = Object.new
-      private_constant :DEFAULT
-      def default_configuration
-        DEFAULT
+      def formatted_amount
+        number_to_currency(configured_amount)
       end
-      attr_accessor(*PROPS)
+
+      def unit
+        pb_unit = Playbook::PbBody::Body.new(classname: "unit", color: "light") do
+          formatted_amount.last(2)
+        end
+        ApplicationController.renderer.render(partial: pb_unit, as: :object)
+      end
+
+      def currency_indicator
+        pb_currency_indicator = Playbook::PbBody::Body.new(classname: "currency_indicator", color: "light") do
+          formatted_amount.first(1)
+        end
+        ApplicationController.renderer.render(partial: pb_currency_indicator, as: :object)
+      end
     end
   end
 end
