dd514295c07bd5295c1c3cc6587b5e7ec7c551fd
Stephen Marshall <stephen.marshall@powerhrg.com>
2019-10-10 16:03:56 -0500

    Swap sass-loader for fast-sass-loader to de-dupe CSS declarations

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,10 @@
 const { environment } = require('@rails/webpacker')
 const erb = require('./loaders/erb')
 
+// Swap sass-loader for fast-sass-loader
+require('./loaders/fast-sass-loader').use(environment)
+
+// Add .erb loader
 environment.loaders.prepend('erb', erb)
-  
+
 module.exports = environment
---
+++ config/webpack/loaders/fast-sass-loader.js
@@ -0,0 +1,7 @@
+module.exports = {
+    use: ({ loaders }) => {
+        loaders.get('sass').use.forEach(u => { 
+            if(u.loader === 'sass-loader') u.loader = 'fast-sass-loader'
+        })
+    }
+}
