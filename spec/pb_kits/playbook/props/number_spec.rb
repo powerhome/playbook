d17bd4ba0c410ef91c113216b1d980917eb20ffd
Kevin Skilton <kevin.skilton@powerhrg.com>
2019-10-10 09:46:37 -0400

    Add true to spec for nil

--- spec/pb_kits/playbook/props/number_spec.rb
+++ spec/pb_kits/playbook/props/number_spec.rb
@@ -2,8 +2,9 @@
 
 RSpec.describe Playbook::Props::Number do
   describe "#validate" do
-    it "returns true given a valid input" do
+    it "returns true given a valid input", :aggregate_failures do
       expect(Playbook::Props::Number.new.validate(45)).to eq true
+      expect(Playbook::Props::Number.new.validate(nil)).to eq true
     end
 
     it "returns false given anything something besides a number", :aggregate_failures do
@@ -11,7 +12,6 @@ RSpec.describe Playbook::Props::Number do
       expect(Playbook::Props::Number.new.validate(:false)).to eq false
       expect(Playbook::Props::Number.new.validate("a")).to eq false
       expect(Playbook::Props::Number.new.validate({})).to eq false
-      expect(Playbook::Props::Number.new.validate(nil)).to eq false
     end
   end
 end
