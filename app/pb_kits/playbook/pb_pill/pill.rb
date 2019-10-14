3d568a62912586b836f54934f6e7fe0a37ac9e8b
Ezz Abousaif <46111475+eabousaif@users.noreply.github.com>
2019-10-14 12:13:14 -0400

    Modernize Pill Kit (#358)
    * Modernize pill kit and add specs
    
    * remove h4 tag from title

--- app/pb_kits/playbook/pb_pill/pill.rb
+++ app/pb_kits/playbook/pb_pill/pill.rb
@@ -2,59 +2,23 @@
 
 module Playbook
   module PbPill
-    class Pill < Playbook::PbKit::Base
-      PROPS = %i[configured_classname
-                 configured_data
-                 configured_id
-                 configured_text
-                 configured_variant].freeze
+    class Pill
+      include Playbook::Props
 
-      def initialize(classname: default_configuration,
-                     data: default_configuration,
-                     id: default_configuration,
-                     text: default_configuration,
-                     variant: default_configuration)
-        self.configured_classname = classname
-        self.configured_data = data
-        self.configured_id = id
-        self.configured_text = text
-        self.configured_variant = variant
+      partial "pb_pill/pill"
+
+      prop :text
+      prop :variant, type: Playbook::Props::Enum,
+                     values: %w[success warning error info neutral],
+                     default: "neutral"
+
+      def classname
+        generate_classname("pb_pill_kit", variant)
       end
 
       def display_text
-        pb_text = Playbook::PbTitle::Title.new(size: 4, text: text, classname: "pb_pill_text")
-        ApplicationController.renderer.render(partial: pb_text, as: :object)
+        text.downcase
       end
-
-      def kit_class
-        kit_options = [
-          "pb_pill_kit",
-          variant,
-        ]
-        kit_options.join("_")
-      end
-
-      def to_partial_path
-        "pb_pill/pill"
-      end
-
-    private
-
-      def variant
-        variant_options = %w[success warning error info neutral]
-        one_of_value(configured_variant, variant_options, "neutral")
-      end
-
-      def text
-        default_value(configured_text.downcase, "")
-      end
-
-      DEFAULT = Object.new
-      private_constant :DEFAULT
-      def default_configuration
-        DEFAULT
-      end
-      attr_accessor(*PROPS)
     end
   end
 end
