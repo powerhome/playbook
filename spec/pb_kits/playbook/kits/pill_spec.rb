2a5c34556e98fb25a49516e51ac5f3dac319acd9
Ezz Abousaif <ezzeldin.abousaif@powerhrg.com>
2019-10-14 11:25:08 -0400

    Modernize pill kit and add specs

---
+++ spec/pb_kits/playbook/kits/pill_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require_relative "../../../../app/pb_kits/playbook/pb_pill/pill"
+
+RSpec.describe Playbook::PbPill::Pill do
+  subject { Playbook::PbPill::Pill }
+
+  it { is_expected.to define_partial }
+  it { is_expected.to define_prop(:text) }
+  it { is_expected.to define_enum_prop(:variant)
+                      .with_default("neutral")
+                      .with_values("neutral", "success", "warning", "error", "info") }
+  describe "#classname" do
+    it "returns namespaced class name", :aggregate_failures do
+      expect(subject.new({}).classname).to eq "pb_pill_kit_neutral"
+      expect(subject.new(classname: "additional_class").classname).to eq "pb_pill_kit_neutral additional_class"
+    end
+  end
+end
+
