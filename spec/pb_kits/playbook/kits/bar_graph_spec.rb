46eaf3dfaea6eeac52fdb22b1611c595ba3d8c24
Leila Weiss <leila.weiss@powerhrg.com>
2019-10-15 07:20:33 -0400

    Add tests to spec file

--- spec/pb_kits/playbook/kits/bar_graph_spec.rb
+++ spec/pb_kits/playbook/kits/bar_graph_spec.rb
@@ -3,16 +3,17 @@
 require_relative "../../../../app/pb_kits/playbook/pb_loading_inline/loading_inline"
 
 RSpec.describe Playbook::PbLoadingInline::LoadingInline do
-  subject { Playbook::LoadingInline::LoadingInline }
+  subject { Playbook::PbLoadingInline::LoadingInline }
 
   it { is_expected.to define_partial }
-  it { is_expected.to define_prop(:align)
+  it { is_expected.to define_boolean_prop(:dark) }
+  it { is_expected.to define_enum_prop(:align)
                   .with_default("left")
                   .with_values("left", "center", "right") }
-  it { is_expected.to define_enum_prop(:dark) }
  describe "#classname" do
     it "returns namespaced class name", :aggregate_failures do
-      expect(subject.new({}).classname).to eq "pb_loading_inline_left"
+      expect(subject.new({}).classname).to eq "pb_loading_inline_kit_left"
+      expect(subject.new(classname: "additional_class").classname).to eq "pb_loading_inline_kit_left additional_class"
     end
   end
 end
