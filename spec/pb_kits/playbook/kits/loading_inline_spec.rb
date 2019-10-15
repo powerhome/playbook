9874fb986af332d64dd8f96c72033c16b60651da
leilaweiss <leila.weiss@powerhrg.com>
2019-10-15 08:22:50 -0400

    Bab/modernize loading inline kit (#361)
    * Add spec file
    
    * Add tests to spec file
    
    * Rename spec file
    
    * Add more test to spec files

---
+++ spec/pb_kits/playbook/kits/loading_inline_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require_relative "../../../../app/pb_kits/playbook/pb_loading_inline/loading_inline"
+
+RSpec.describe Playbook::PbLoadingInline::LoadingInline do
+  subject { Playbook::PbLoadingInline::LoadingInline }
+
+  it { is_expected.to define_partial }
+  it { is_expected.to define_boolean_prop(:dark) }
+  it { is_expected.to define_enum_prop(:align)
+                  .with_default("left")
+                  .with_values("left", "center", "right") }
+ describe "#classname" do
+    it "returns namespaced class name", :aggregate_failures do
+      expect(subject.new({}).classname).to eq "pb_loading_inline_kit_left"
+      expect(subject.new(classname: "additional_class").classname).to eq "pb_loading_inline_kit_left additional_class"
+      expect(subject.new(align: "center").classname).to eq "pb_loading_inline_kit_center"
+    end
+  end
+end
