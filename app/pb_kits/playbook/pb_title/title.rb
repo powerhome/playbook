3d0a264426be231cd686202d33dca4c81e259085
Darren D'Costa <ddcosta@gmail.com>
2019-10-15 13:09:07 -0400

    Update pbtitle rb and write specs

--- app/pb_kits/playbook/pb_title/title.rb
+++ app/pb_kits/playbook/pb_title/title.rb
@@ -2,77 +2,30 @@
 
 module Playbook
   module PbTitle
-    class Title < Playbook::PbKit::Base
-      PROPS = %i[configured_aria
-                 configured_classname
-                 configured_dark
-                 configured_data
-                 configured_id
-                 configured_size
-                 configured_tag
-                 configured_text].freeze
+    class Title
+      include Playbook::Props
 
-      def initialize(aria: default_configuration,
-                     classname: default_configuration,
-                     dark: default_configuration,
-                     data: default_configuration,
-                     id: default_configuration,
-                     size: default_configuration,
-                     tag: default_configuration,
-                     text: default_configuration)
-        self.configured_aria = aria
-        self.configured_classname = classname
-        self.configured_dark = dark
-        self.configured_data = data
-        self.configured_id = id
-        self.configured_size = size
-        self.configured_tag = tag
-        self.configured_text = text
-      end
+      partial "pb_title/title"
 
-      def tag
-        tag_options = %w[h1 h2 h3 h4 h5 h6 p div span]
-        one_of_value(configured_tag, tag_options, "h3")
-      end
+      prop :dark, type: Playbook::Props::Boolean,
+                  default: false
+      prop :size, type: Playbook::Props::Enum,
+                  values: %w[1 2 3 4],
+                  default: "3"
+      prop :tag, type: Playbook::Props::Enum,
+                 values: %w[h1 h2 h3 h4 h5 h6 p div span],
+                 default: "h3"
+      prop :text
 
-      def text
-        default_value(configured_text, "Title text")
-      end
-
-      def size
-        size_options = [1, 2, 3, 4]
-        one_of_value(configured_size, size_options, 3)
-      end
-
-      def size_class
-        adjusted_value(size, size, nil)
-      end
-
-      def dark_class
-        true_value(configured_dark, "dark", nil)
-      end
-
-      def kit_class
-        title_options = [
-          "pb_title_kit",
-          size_class,
-          dark_class,
-        ]
-        title_options.reject(&:nil?).join("_")
-      end
-
-      def to_partial_path
-        "pb_title/title"
+      def classname
+        generate_classname("pb_title_kit", size, dark_class)
       end
 
     private
 
-      DEFAULT = Object.new
-      private_constant :DEFAULT
-      def default_configuration
-        DEFAULT
+      def dark_class
+        dark ? "dark" : nil
       end
-      attr_accessor(*PROPS)
     end
   end
 end
