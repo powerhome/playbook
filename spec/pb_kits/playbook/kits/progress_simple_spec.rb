4a021e35999075d282964463d7046a5cbaf3a621
Darren D'Costa <ddcosta@gmail.com>
2019-10-16 11:03:28 -0400

    Add progress_simple specs

---
+++ spec/pb_kits/playbook/kits/progress_simple_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require_relative "../../../../app/pb_kits/playbook/pb_progress_simple/progress_simple"
+
+RSpec.describe Playbook::PbProgressSimple::ProgressSimple do
+  subject { Playbook::PbProgressSimple::ProgressSimple }
+
+  it { is_expected.to define_partial }
+  it { is_expected.to define_enum_prop(:align)
+    .with_values("left", "center", "right")
+    .with_default("left") }
+  it { is_expected.to define_prop(:value) }
+  it { is_expected.to define_prop(:max) }
+  it { is_expected.to define_boolean_prop(:muted)
+    .with_default(false) }
+  it { is_expected.to define_prop(:percent) }
+  it { is_expected.to define_prop(:width) }
+
+  describe "#classname" do
+    it "returns namespaced class name", :aggregate_failures do
+      expect(subject.new({}).classname).to eq "pb_progress_simple_kit_left"
+      expect(subject.new(align: "center").classname).to eq "pb_progress_simple_kit_center"
+      expect(subject.new(muted: true).classname).to eq "pb_progress_simple_kit_muted_left"
+      expect(subject.new(classname: "additional_class").classname).to eq "pb_progress_simple_kit_left additional_class"
+    end
+  end
+end
