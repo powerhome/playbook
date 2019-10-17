884b21453ec2e3d70a33b85b34e7a1673cb62baf
Kevin Skilton <kevin.skilton@powerhrg.com>
2019-10-17 12:58:39 -0400

    Modernized but still work in progress

--- app/pb_kits/playbook/pb_appointment_result/appointment_result.rb
+++ app/pb_kits/playbook/pb_appointment_result/appointment_result.rb
@@ -2,60 +2,29 @@
 
 module Playbook
   module PbAppointmentResult
-    class AppointmentResult < Playbook::PbKit::Base
-      PROPS = %i[configured_classname
-                 configured_dark
-                 configured_data
-                 configured_id
-                 configured_result
-                 configured_text
-                 configured_user].freeze
+    class AppointmentResult
+      include Playbook::Props
 
-      def initialize(classname: default_configuration,
-                     dark: default_configuration,
-                     data: default_configuration,
-                     id: default_configuration,
-                     result: default_configuration,
-                     text: default_configuration,
-                     user: default_configuration)
-        self.configured_classname = classname
-        self.configured_dark = dark
-        self.configured_data = data
-        self.configured_id = id
-        self.configured_result = result
-        self.configured_text = text
-        self.configured_user = user
+      partial "pb_appointment_result/appointment_result"
+
+      prop :dark, type: Playbook::Props::Boolean, default: false
+      prop :result, type: Playbook::Props::Enum,
+                    values: %w["No Pitch", "Pitch Miss", "Sold", "Unconfirmed", "Canceled", ""],
+                    default: ""
+      prop :text
+      prop :user
+
+      def classname
+        generate_classname( "pb_appointment_result", dark_class)
       end
 
-      def dark
-        true_value(configured_dark, "true value", "false value")
-      end
-
-      def result
-        result_options = ["No Pitch", "Pitch Miss", "Sold", "Unconfirmed", "Canceled", ""]
-        one_of_value(configured_result, result_options, "")
-      end
-
-      def text
-        default_value(configured_text, "")
-      end
-
-      def user
-        default_value(configured_user, "")
-      end
-
-      def to_partial_path
-        "pb_appointment_result/appointment_result"
-      end
 
     private
 
-      DEFAULT = Object.new
-      private_constant :DEFAULT
-      def default_configuration
-        DEFAULT
+      def dark_class
+        dark ? "dark" : nil
       end
-      attr_accessor(*PROPS)
+
     end
   end
 end
