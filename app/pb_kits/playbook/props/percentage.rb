20abd638441ec47ac43e5f34a550f3317bda4d3a
Darren D'Costa <ddcosta@gmail.com>
2019-10-17 08:04:17 -0400

    Refactor percentage prop

--- app/pb_kits/playbook/props/percentage.rb
+++ app/pb_kits/playbook/props/percentage.rb
@@ -4,8 +4,13 @@ module Playbook
   module Props
     class Percentage < Playbook::Props::Base
       def validate(value)
-        value.nil? ||
-          (value.is_a?(Numeric) &&
+        value.nil? || numeric_in_range?(value)
+      end
+
+    private
+
+      def numeric_in_range?(value)
+        (value.is_a?(Numeric) &&
           value <= 100 &&
           value >= 0)
       end
