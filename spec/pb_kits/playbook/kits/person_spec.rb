9e1c39a83626ff7fd4aa39da1e9a97162e3d5f8d
Darren D'Costa <ddcosta@gmail.com>
2019-10-21 10:32:27 -0400

    Add specs for props

--- spec/pb_kits/playbook/kits/person_spec.rb
+++ spec/pb_kits/playbook/kits/person_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe Playbook::PbPerson::Person do
   subject { Playbook::PbPerson::Person }
 
   it { is_expected.to define_partial }
+  it { is_expected.to define_prop(:first_name) }
+  it { is_expected.to define_prop(:last_name) }
 
   describe "#classname" do
     it "returns namespaced class name", :aggregate_failures do
