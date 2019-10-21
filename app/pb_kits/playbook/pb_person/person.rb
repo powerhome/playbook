a2fc29999923158f0403b882ff80064437b76b17
Darren D'Costa <ddcosta@gmail.com>
2019-10-21 10:26:16 -0400

    Configure erb partial

--- app/pb_kits/playbook/pb_person/person.rb
+++ app/pb_kits/playbook/pb_person/person.rb
@@ -10,20 +10,6 @@ module Playbook
       prop :first_name
       prop :last_name
 
-      def first_name
-        pb_first_name = Playbook::PbBody::Body.new(tag: "span", classname: "pb_person_first") do
-          default_value(configured_first_name, "")
-        end
-        ApplicationController.renderer.render(partial: pb_first_name, as: :object)
-      end
-
-      def last_name
-        if is_set? configured_last_name
-          pb_last_name = Playbook::PbTitle::Title.new(text: configured_last_name, size: 4)
-          ApplicationController.renderer.render(partial: pb_last_name, as: :object)
-        end
-      end
-
       def classname
         generate_classname("pb_person_kit", first_name, last_name)
       end
--- spec/pb_kits/playbook/kits/person_spec.rb
+++ spec/pb_kits/playbook/kits/person_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe Playbook::PbPerson::Person do
   subject { Playbook::PbPerson::Person }
 
   it { is_expected.to define_partial }
+
+  describe "#classname" do
+    it "returns namespaced class name", :aggregate_failures do
+      expect(subject.new({}).classname).to eq "pb_person_kit"
+      expect(subject.new(classname: "additional_class").classname).to eq "pb_person_kit additional_class"
+    end
+  end
 end
