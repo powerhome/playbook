572109e44bfbd745fc6eea566b7e26b0db6cfe2a
Ezz Abousaif <ezzeldin.abousaif@powerhrg.com>
2019-10-21 10:09:08 -0400

    Add more specs to table kit

--- spec/pb_kits/playbook/kits/table_spec.rb
+++ spec/pb_kits/playbook/kits/table_spec.rb
@@ -13,13 +13,25 @@ RSpec.describe Playbook::PbTable::Table do
   it { is_expected.to define_boolean_prop(:single_line).with_default(false) }
   it { is_expected.to define_boolean_prop(:dark).with_default(false) }
   it { is_expected.to define_boolean_prop(:disable_hover).with_default(false) }
-  it { is_expected.to define_boolean_prop(:container).with_default(false) }
+  it { is_expected.to define_boolean_prop(:container).with_default(true) }
   it { is_expected.to define_prop(:text) }
+
+  describe "#container" do
+    it "returns false when it is specified" do
+      expect(subject.new(container: false).container).to eq false
+    end
+  end
 
   describe "#classname" do
     it "returns namespaced class name", :aggregate_failures do
-      expect(subject.new({}).classname).to eq "pb_table table-md"
-      expect(subject.new(classname: "additional_class").classname).to eq "pb_table table-md additional_class"
+      expect(subject.new({}).classname).to eq "pb_table table-md table-card"
+      expect(subject.new(classname: "additional_class").classname).to eq "pb_table table-md table-card additional_class"
+      expect(subject.new(dark: true).classname).to eq "pb_table table-md dark table-card"
+      expect(subject.new(size: "sm").classname).to eq "pb_table table-sm table-card"
+      expect(subject.new(single_line: true).classname).to eq "pb_table table-md single-line table-card"
+      expect(subject.new(container: false).classname).to eq "pb_table table-md"
+      expect(subject.new(disable_hover: true).classname).to eq "pb_table table-md no-hover table-card"
+      expect(subject.new(disable_hover: true, dark: true, size: "lg", single_line: true).classname).to eq "pb_table table-lg single-line dark no-hover table-card"
     end
   end
 end
