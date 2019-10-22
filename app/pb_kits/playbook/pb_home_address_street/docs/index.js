9842b200b99234f04b0e8bdb70f70dbc9032bb9c
Kevin Skilton <kevin.skilton@powerhrg.com>
2019-10-22 09:31:06 -0400

    Add 3rd example with different options

--- app/pb_kits/playbook/pb_home_address_street/docs/index.js
+++ app/pb_kits/playbook/pb_home_address_street/docs/index.js
@@ -1,2 +1,3 @@
 export {default as HomeAddressStreetDefault} from './_home_address_street_default.jsx';
+export {default as HomeAddressStreetModified} from './_home_address_street_modified.jsx';
 export {default as HomeAddressStreetDark} from './_home_address_street_dark.jsx';
