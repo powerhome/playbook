aebf354396b0b2bbed6916a7f4c521bb14696232
Gillian Wenhold <gwenhold@mac.com>
2019-10-23 11:40:23 -0400

    Modernized kit, added specs

---
+++ spec/pb_kits/playbook/kits/icon_circle_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require_relative "../../../../app/pb_kits/playbook/pb_icon_circle/icon_circle"
+
+RSpec.describe Playbook::PbIconCircle::IconCircle do
+  subject { Playbook::PbIconCircle::IconCircle }
+
+  it { is_expected.to define_partial }
+
+  it { is_expected.to define_prop(:icon) }
+  it { is_expected.to define_enum_prop(:size)
+                      .with_default("md")
+                      .with_values("xs", "sm", "md", "base", "lg", "xl") }
+  it { is_expected.to define_enum_prop(:variant)
+                      .with_default("default")
+                      .with_values("default", "royal", "blue", "purple", "teal", "red", "yellow", "green") }
+
+  describe "#classname" do
+    it "returns namespaced class name", :aggregate_failures do
+      expect(subject.new({}).classname).to eq "pb_icon_circle_kit_md_default"
+      expect(subject.new(variant: "royal").classname).to eq "pb_icon_circle_kit_md_royal"
+      expect(subject.new(size: "sm").classname).to eq "pb_icon_circle_kit_sm_default"
+      expect(subject.new(variant: "purple", size: "lg").classname).to eq "pb_icon_circle_kit_lg_purple"
+      expect(subject.new(classname: "additional_class").classname).to eq "pb_icon_circle_kit_md_default additional_class"
+    end
+  end
+end
