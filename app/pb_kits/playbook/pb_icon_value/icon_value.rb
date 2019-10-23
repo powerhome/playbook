c4463918b108f9f01af51175725db410d47665c5
Kyle Fadigan <kfadigan@yahoo.com>
2019-10-23 15:14:10 -0400

    Require text for the kit & write spec

--- app/pb_kits/playbook/pb_icon_value/icon_value.rb
+++ app/pb_kits/playbook/pb_icon_value/icon_value.rb
@@ -7,21 +7,11 @@ module Playbook
 
       partial "pb_icon_value/icon_value"
 
-      prop :value, type: Playbook::Props::
-      prop :text
+      prop :text, required: true
       prop :icon, required: true
       prop :align, type: Playbook::Props::Enum,
-                  values: %w[left center right],
-                  default: "left"
-
-      def value
-        if is_set? configured_text
-          pb_icon_value = Playbook::PbBody::Body.new(color: "light") do
-            icon + text
-          end
-          ApplicationController.renderer.render(partial: pb_icon_value, as: :object)
-        end
-      end
+                   values: %w[left center right],
+                   default: "left"
 
       def classname
         generate_classname("pb_icon_value_kit", align)
