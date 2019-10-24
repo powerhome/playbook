244f896849fe9c633632fd583b8b0cc36da18322
Megan Trimble <mtrimble6@me.com>
2019-10-24 12:29:22 -0400

    Circle button progress

--- app/pb_kits/playbook/pb_circle_icon_button/circle_icon_button.rb
+++ app/pb_kits/playbook/pb_circle_icon_button/circle_icon_button.rb
@@ -6,6 +6,45 @@ module Playbook
       include Playbook::Props
 
       partial "pb_circle_icon_button/circle_icon_button"
+
+      prop :dark, type: Playbook::Props::Boolean,
+                  default: false
+      prop :icon
+      prop :link, default: "https://www.google.com"
+      prop :variant, type: Playbook::Props::Enum,
+                     values: %w[primary secondary link],
+                     default: "primary"
+      
+      def classname
+        generate_classname("pb_circle_icon_button_kit", variant, dark_class)
+      end
+
+      def dark_class
+        dark ? "dark" : nil
+      end
+      
+      def icon
+        if is_set? configured_icon
+          pb_icon = Playbook::PbIcon::Icon.new(icon: configured_icon, fixed_width: true, size: "md")
+          ApplicationController.renderer.render(partial: pb_icon, as: :object)
+        end
+      end
+
+      def kit_class
+        kit_options = [
+          "pb_circle_icon_button_kit",
+          variant,
+        ]
+        kit_options.join("_")
+      end
+
+      def to_partial_path
+        "pb_circle_icon_button/circle_icon_button"
+      end
+
+    private
+
     end
+
   end
 end
