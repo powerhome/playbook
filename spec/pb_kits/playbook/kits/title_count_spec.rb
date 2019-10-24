e47d20b53ddbe7cfda118833be08e3cb198576e9
Darren D'Costa <ddcosta@gmail.com>
2019-10-24 14:34:04 -0400

    Configure rb, partial and create spec

---
+++ spec/pb_kits/playbook/kits/title_count_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require_relative "../../../../app/pb_kits/playbook/pb_title_count/title_count"
+
+RSpec.describe Playbook::PbTitleCount::TitleCount do
+  subject { Playbook::PbTitleCount::TitleCount }
+
+  it { is_expected.to define_partial }
+
+  describe "#classname" do
+    it "returns namespaced class name", :aggregate_failures do
+      expect(subject.new({}).classname).to eq "pb_title_kit_count"
+    end
+  end
+end
