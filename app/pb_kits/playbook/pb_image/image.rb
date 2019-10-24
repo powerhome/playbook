f6eccf21566738779268d2c7a20a3537df1d4d72
Kyle Fadigan <kfadigan@yahoo.com>
2019-10-24 10:32:40 -0400

    Modernize props and classname

--- app/pb_kits/playbook/pb_image/image.rb
+++ app/pb_kits/playbook/pb_image/image.rb
@@ -2,58 +2,18 @@
 
 module Playbook
   module PbImage
-    class Image < Playbook::PbKit::Base
-      PROPS = %i[configured_alt
-                 configured_aria
-                 configured_classname
-                 configured_data
-                 configured_id
-                 configured_url].freeze
+    class Image
+      include Playbook::Props
 
-      def initialize(alt: default_configuration,
-                     aria: default_configuration,
-                     classname: default_configuration,
-                     data: default_configuration,
-                     id: default_configuration,
-                     url: default_configuration)
-        self.configured_alt = alt
-        self.configured_aria = aria
-        self.configured_classname = classname
-        self.configured_data = data
-        self.configured_id = id
-        self.configured_url = url
+      partial "pb_image/image"
+
+      prop :alt
+      prop :url
+
+      def classname
+        generate_classname(
+          "pb_image_kit", "lazyload", "blur_up", separator: " ")
       end
-
-      def alt
-        default_value(configured_alt, "")
-      end
-
-      def url
-        default_value(configured_url, "")
-      end
-
-      def kit_class
-        image_options = %w[
-          pb_image_kit
-          lazyload
-          blur_up
-        ]
-        image_options.reject(&:nil?).join(" ")
-      end
-
-      def to_partial_path
-        "pb_image/image"
-      end
-
-    private
-
-      DEFAULT = Object.new
-      private_constant :DEFAULT
-      def default_configuration
-        DEFAULT
-      end
-      attr_reader(*PROPS)
-      attr_accessor(*PROPS)
     end
   end
 end
