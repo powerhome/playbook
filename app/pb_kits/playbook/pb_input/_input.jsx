1fa79b11f92201f39549c3055b756745d33636e6
Jasper Furniss <jasper.furniss@powerhrg.com>
2019-10-24 17:19:44 -0400

    Added OnChange prop for Input

--- app/pb_kits/playbook/pb_input/_input.jsx
+++ app/pb_kits/playbook/pb_input/_input.jsx
@@ -1,57 +1,55 @@
-import React from 'react'
-import PropTypes from "prop-types"
-import classnames from 'classnames'
-import {Caption} from "../"
+import React from "react";
+import PropTypes from "prop-types";
+import classnames from "classnames";
+import { Caption } from "../";
 
 const propTypes = {
   className: PropTypes.string,
+  onChange: PropTypes.func,
   name: PropTypes.string,
   label: PropTypes.string,
   placeholder: PropTypes.string,
   type: PropTypes.string,
-  value: PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.number
-  ])
-}
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+};
 
 const defaultProps = {
   type: "text"
-}
+};
 
 class Input extends React.Component {
   render() {
     const {
       className,
-      name,
       label,
+      name,
+      onChange,
       placeholder,
       type,
       value
-    } = this.props
+    } = this.props;
 
-    const css = classnames([
-      `pb_input_kit`,
-      className,
-    ])
+    const css = classnames([`pb_input_kit`, className]);
 
     return (
       <div className="pb_input_kit">
         <Caption text={label} />
         <div className="input_wrapper">
-          <input className={css}
+          <input
+              className={css}
               name={name}
+              onChange={onChange}
               placeholder={placeholder}
               type={type}
               value={value}
           />
         </div>
       </div>
-    )
+    );
   }
 }
 
-Input.propTypes = propTypes
-Input.defaultProps = defaultProps
+Input.propTypes = propTypes;
+Input.defaultProps = defaultProps;
 
-export default Input
+export default Input;
