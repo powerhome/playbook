cbf2523923974fea121e5ea4a7397f7a1c2c8090
Megan Trimble <mtrimble6@me.com>
2019-10-24 12:29:22 -0400

    Circle button progress

--- app/pb_kits/playbook/pb_circle_icon_button/_circle_icon_button.jsx
+++ app/pb_kits/playbook/pb_circle_icon_button/_circle_icon_button.jsx
@@ -2,23 +2,27 @@
 /*eslint-disable react/no-multi-comp, flowtype/space-before-type-colon */
 
 import React from 'react'
-
-/*
-// Un-comment to import kits here
-import { Body } from '../'
-*/
+import classnames from "classnames"
+import Icon from '../pb_icon/_icon.jsx'
 
 type CircleIconButtonProps = {
   className?: String,
   data?: String,
   id?: String,
-  
+  icon: String,
+  variant?: 'primary' | 'secondary',
+  link: String,
+
 }
 
-const CircleIconButton = ({ className, data, id }: CircleIconButtonProps) => (
-  <div>
-    {`kit content`}
-  </div>
-)
+const CircleIconButton = ({ className, data, id, icon, variant='primary', link}: CircleIconButtonProps) => {
+  return (
+    <div className={`pb_circle_icon_button_kit_${variant} ${className}`}>
+      <a href={link}>
+        <Icon icon={icon} fixed_width={true} />
+      </a>
+    </div>
+  )
+}
 
 export default CircleIconButton
