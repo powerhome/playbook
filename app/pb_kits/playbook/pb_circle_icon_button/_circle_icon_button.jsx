faf7452bd1cd735afb5c723e7b525a94a4e3b274
Jasper Furniss <jasper.furniss@powerhrg.com>
2019-10-24 13:07:29 -0400

    Reformatted and Bug Fix

--- app/pb_kits/playbook/pb_circle_icon_button/_circle_icon_button.jsx
+++ app/pb_kits/playbook/pb_circle_icon_button/_circle_icon_button.jsx
@@ -1,28 +1,36 @@
 /* @flow */
 /*eslint-disable react/no-multi-comp, flowtype/space-before-type-colon */
 
-import React from 'react'
-import classnames from "classnames"
-import Icon from '../pb_icon/_icon.jsx'
+import React from "react";
+import classnames from "classnames";
+import Icon from "../pb_icon/_icon.jsx";
 
 type CircleIconButtonProps = {
   className?: String,
   data?: String,
-  id?: String,
   icon: String,
-  variant?: 'primary' | 'secondary',
+  id?: String,
   link: String,
+  variant?: "primary" | "secondary"
+};
 
-}
-
-const CircleIconButton = ({ className, data, id, icon, variant='primary', link}: CircleIconButtonProps) => {
+const CircleIconButton = ({
+  className,
+  data,
+  icon,
+  id,
+  link,
+  variant = "primary"
+}: CircleIconButtonProps) => {
   return (
     <div className={`pb_circle_icon_button_kit_${variant} ${className}`}>
       <a href={link}>
-        <Icon icon={icon} fixed_width={true} />
+        <Icon fixedWidth
+            icon={icon}
+        />
       </a>
     </div>
-  )
-}
+  );
+};
 
-export default CircleIconButton
+export default CircleIconButton;
--- app/pb_kits/playbook/pb_icon/_icon.jsx
+++ app/pb_kits/playbook/pb_icon/_icon.jsx
@@ -1,73 +1,86 @@
 /* @flow */
 
-import React from 'react'
-import classnames from 'classnames'
+import React from "react";
+import classnames from "classnames";
 
 type IconProps = {
   aria?: Object,
   border?: Boolean,
   className?: String,
   fixedWidth?: Boolean,
-  flip?: 'horizontal' | 'vertical' | 'both' | 'none',
+  flip?: "horizontal" | "vertical" | "both" | "none",
   icon: String,
   id?: String,
   inverse?: Boolean,
   listItem?: Boolean,
-  pull?: 'left' | 'right' | 'none',
+  pull?: "left" | "right" | "none",
   pulse?: Boolean,
   rotation?: 90 | 180 | 270,
-  size?: 'lg' | 'xs' | 'sm' | '1x' | '2x' | '3x' | '4x' | '5x' | '6x' | '7x' | '8x' | '9x' | '10x',
+  size?: | "lg"
+    | "xs"
+    | "sm"
+    | "1x"
+    | "2x"
+    | "3x"
+    | "4x"
+    | "5x"
+    | "6x"
+    | "7x"
+    | "8x"
+    | "9x"
+    | "10x",
   spin?: Boolean
-}
+};
 
 const Icon = (props: IconProps) => {
   const {
     aria,
-    border=false,
+    border = false,
     className,
-    fixedWidth=true,
-    flip=false,
+    fixedWidth = true,
+    flip = false,
     icon,
     id,
-    inverse=false,
-    listItem=false,
+    inverse = false,
+    listItem = false,
     pull,
-    pulse=false,
+    pulse = false,
     rotation,
     size,
-    spin=false
-  } = props
+    spin = false
+  } = props;
 
-  const hClass = "fa-flip-horizontal"
-  const vClass = "fa-flip-vertical"
+  const hClass = "fa-flip-horizontal";
+  const vClass = "fa-flip-vertical";
 
   const flipClass = (function(flip) {
-    switch(flip) {
-      case 'horizontal':
+    switch (flip) {
+      case "horizontal":
         return hClass;
-      case 'vertical':
+      case "vertical":
         return vClass;
-      case 'both':
+      case "both":
         return `${vClass} ${hClass}`;
       default:
-        return '';
+        return "";
     }
-  })(flip)
+  })(flip);
 
-  const iconClass = icon !== '' && icon !== undefined ? `fa-${icon}` : ''
-  const borderClass = border ? 'fa-border' : ''
-  const fixedWidthClass = fixedWidth ? 'fa-fw' : ''
-  const inverseClass = inverse ? 'fa-inverse' : ''
-  const listItemClass = listItem ? 'fa-li' : ''
-  const pullClass = pull !== '' && pull !== undefined ? `fa-pull-${pull}` : ''
-  const pulseClass = pulse ? 'fa-pulse' : ''
-  const rotationClass = rotation !== '' && rotation !== undefined ? `fa-rotate-${rotation}` : ''
-  const sizeClass = size !== '' && size !== undefined ? `fa-${size}` : ''
-  const spinClass = spin ? 'fa-spin' : ''
+  const iconClass = icon !== "" && icon !== undefined ? `fa-${icon}` : "";
+  const borderClass = border ? "fa-border" : "";
+  const fixedWidthClass = fixedWidth ? "fa-fw" : "";
+  const inverseClass = inverse ? "fa-inverse" : "";
+  const listItemClass = listItem ? "fa-li" : "";
+  const pullClass = pull !== "" && pull !== undefined ? `fa-pull-${pull}` : "";
+  const pulseClass = pulse ? "fa-pulse" : "";
+  const rotationClass =
+    rotation !== "" && rotation !== undefined ? `fa-rotate-${rotation}` : "";
+  const sizeClass = size !== "" && size !== undefined ? `fa-${size}` : "";
+  const spinClass = spin ? "fa-spin" : "";
 
   const iconCss = classnames(
-    'pb_icon_kit',
-    'far',
+    "pb_icon_kit",
+    "far",
     iconClass,
     borderClass,
     fixedWidthClass,
@@ -80,11 +93,11 @@ const Icon = (props: IconProps) => {
     sizeClass,
     spinClass,
     className
-  )
+  );
 
-  return (
-    <i id={id} className={iconCss}></i>
-  )
-}
+  return (<i className={iconCss}
+      id={id}
+          />);
+};
 
-export default Icon
+export default Icon;
