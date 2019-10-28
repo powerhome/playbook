aea1beb315aacf7f6360d8cd17dff59f6aeb58b1
Gillian Wenhold <gwenhold@mac.com>
2019-10-28 16:34:01 -0400

    Modernized pb kit for html

--- app/pb_kits/playbook/pb_line_graph/line_graph.rb
+++ app/pb_kits/playbook/pb_line_graph/line_graph.rb
@@ -2,84 +2,31 @@
 
 module Playbook
   module PbLineGraph
-    class LineGraph < Playbook::PbKit::Base
-      PROPS = %i[
-        configured_aria
-        configured_axis_title
-        configured_chart_data
-        configured_classname
-        configured_data
-        configured_gradient
-        configured_id
-        configured_point_start
-        configured_subtitle
-        configured_title
-      ].freeze
+    class LineGraph
+      include Playbook::Props
 
-      def initialize(
-        aria: default_configuration,
-        axis_title: default_configuration,
-        chart_data: default_configuration,
-        classname: default_configuration,
-        data: default_configuration,
-        gradient: default_configuration,
-        id: default_configuration,
-        point_start: default_configuration,
-        subtitle: default_configuration,
-        title: default_configuration
-      )
-        self.configured_aria = aria
-        self.configured_axis_title = axis_title
-        self.configured_chart_data = chart_data
-        self.configured_classname = classname
-        self.configured_data = data
-        self.configured_gradient = gradient
-        self.configured_id = id
-        self.configured_point_start = point_start
-        self.configured_subtitle = subtitle
-        self.configured_title = title
-      end
+      partial "pb_line_graph/line_graph"
 
-      def gradient
-        is_true? configured_gradient
-      end
+      prop :axis_title, type: Playbook::Props::String,
+                        default: ""
+      prop :chart_data, type: Playbook::Props::Array,
+                        default: []
+      prop :gradient, type: Playbook::Props::Boolean,
+                      default: false
+      prop :point_start, type: Playbook::Props::String,
+                         default: ""
+      prop :subtitle, type: Playbook::Props::String,
+                      default: ""
+      prop :title, type: Playbook::Props::String,
+                   default: ""
 
       def chart_type
-        gradient == true ? "area" : "line"
+        gradient ? "area" : "line"
       end
 
-      def title
-        default_value(configured_title, "")
+      def classname
+        generate_classname("pb_line_graph")
       end
-
-      def subtitle
-        default_value(configured_subtitle, "")
-      end
-
-      def axis_title
-        default_value(configured_axis_title, "")
-      end
-
-      def to_partial_path
-        "pb_line_graph/line_graph"
-      end
-
-      def point_start
-        default_value(configured_point_start, "")
-      end
-
-      def chart_data
-        adjusted_value(configured_chart_data, configured_chart_data.to_json.html_safe, {})
-      end
-
-    private
-
-      DEFAULT = Object.new
-      private_constant :DEFAULT
-      def default_configuration
-        DEFAULT
-      end
-      attr_accessor(*PROPS)
     end
   end
 end
