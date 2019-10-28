54cea555d6a7c353c8fd06c821c26d87e0c30229
Kyle Fadigan <kfadigan@yahoo.com>
2019-10-28 13:00:43 -0400

    Get kit to render

--- app/pb_kits/playbook/pb_label_pill/label_pill.rb
+++ app/pb_kits/playbook/pb_label_pill/label_pill.rb
@@ -14,19 +14,6 @@ module Playbook
       def classname
         generate_classname("pb_label_pill_label")
       end
-      # def label
-      #   if is_set? configured_label
-      #     pb_caption = Playbook::PbCaption::Caption.new(text: configured_label, classname: "pb_label_pill_label")
-      #     ApplicationController.renderer.render(partial: pb_caption, as: :object)
-      #   end
-      # end
-
-      # def pill
-      #   if is_set? configured_pill_value
-      #     pb_pill = Playbook::PbPill::Pill.new(text: configured_pill_value, variant: variant, classname: "pb_label_pill_pill")
-      #     ApplicationController.renderer.render(partial: pb_pill, as: :object)
-      #   end
-      # end
     end
   end
 end
