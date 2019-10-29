579820e9456f0c801c14e0c54b3db39cf489ebc6
Kyle Fadigan <kfadigan@yahoo.com>
2019-10-29 07:09:34 -0400

    Modernize Label Pill kit (#388)
    * Convert props to new format, start work on erb
    
    * Get kit to render
    
    * Generate classname correctly
    
    * Give variant proper prop type
    
    * Correct misspelling of variant
    
    * Add spec file
    
    * Omit classname prop from the label & use different variant for spec example
    
    * Pass the proper classnames to the kits

---
+++ spec/pb_kits/playbook/kits/label_pill_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require_relative "../../../../app/pb_kits/playbook/pb_label_pill/label_pill"
+
+RSpec.describe Playbook::PbLabelPill::LabelPill do
+  subject { Playbook::PbLabelPill::LabelPill }
+
+  it { is_expected.to define_partial }
+  it { is_expected.to define_prop(:label) }
+  it { is_expected.to define_prop(:pill_value) }
+  it { is_expected.to define_enum_prop(:variant)
+                      .with_default("neutral")
+                      .with_values("neutral", "success", "warning", "error", "info") }
+  describe "#classname" do
+    it "returns namespaced class name", :aggregate_failures do
+      expect(subject.new({}).classname).to eq "pb_label_pill_kit_neutral"
+      expect(subject.new(variant: "success").classname).to eq "pb_label_pill_kit_success"
+      expect(subject.new(classname: "additional_class").classname).to eq "pb_label_pill_kit_neutral additional_class"
+    end
+  end
+end
