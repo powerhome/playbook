dcf70d8adf42791b2feac470a93b18d2ea42663a
Megan Trimble <mtrimble6@me.com>
2019-10-31 15:25:19 -0400

    Selectable Card React Update WIP

--- app/pb_kits/playbook/pb_selectable_card/_selectable_card.jsx
+++ app/pb_kits/playbook/pb_selectable_card/_selectable_card.jsx
@@ -2,49 +2,37 @@
 /*eslint-disable react/no-multi-comp, flowtype/space-before-type-colon */
 
 import React from 'react'
-import classnames from 'classnames'
-import { Card } from "../"
 
 type SelectableCardProps = {
   checked?: Boolean,
   className?: String,
-  dark?: Boolean,
+  data?: String,
   disabled?: Boolean,
+  id?: String,
   name?: String,
   showSelected?: Boolean,
   showUnselected?: Boolean,
   text?: String,
   value?: String,
-}
 
-const cardCSS = ({ selected = false }: CardPropTypes) => {
-  let css = "";
-  css += selected ? "_selected" : "_deselected";
-  return css;
-};
+}
 
 const SelectableCard = ({ 
-  checked, 
-  className, 
-  dark,
-  disabled, 
-  name, 
-  showSelected, 
-  showUnselected, 
-  text, 
-  value 
-}: SelectableCardProps) => {
-  const css = classnames ([
-    'pb_selectable_card_kit',
-    className,
-  ])
-
-  return (
-    <div className={`pb_selectable_card_kit ${className} ${showSelected} ${showUnselected} ${checked}`}>
-     <SelectableCard
-     />
-    </div>
-  )
-}
+  checked,
+  className,
+  data,
+  disabled,
+  id,
+  name,
+  showSelected,
+  showUnselected,
+  text,
+  value
+}: SelectableCardProps) => (
+  <span className={className}>
+    <input type="checkbox" name={name} id={id} value={value} checked={checked}/>
+    <label for={name}>{text}</label>
+  </span>
+)
 
 export default SelectableCard
