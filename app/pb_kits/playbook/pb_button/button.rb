df74a42fe2943a6df6d0ed510d48813d29617a45
Ezz Abousaif <46111475+eabousaif@users.noreply.github.com>
2019-11-01 09:58:30 -0400

    Modernize button kit (#410)
    * remove configured classes and add props
    
    * Refactor code and add spec file

--- app/pb_kits/playbook/pb_button/button.rb
+++ app/pb_kits/playbook/pb_button/button.rb
@@ -2,144 +2,67 @@
 
 module Playbook
   module PbButton
-    class Button < Playbook::PbKit::Base
-      PROPS = %i[configured_aria
-                 configured_classname
-                 configured_data
-                 configured_disabled
-                 configured_full_width
-                 configured_id
-                 configured_link
-                 configured_loading
-                 configured_new_window
-                 configured_variant
-                 configured_tag
-                 configured_text
-                 configured_type
-                 configured_value
-                 block].freeze
+    class Button
+      include Playbook::Props
 
-      def initialize(aria: default_configuration,
-                     classname: default_configuration,
-                     data: default_configuration,
-                     disabled: default_configuration,
-                     full_width: default_configuration,
-                     id: default_configuration,
-                     link: default_configuration,
-                     loading: default_configuration,
-                     new_window: default_configuration,
-                     variant: default_configuration,
-                     tag: default_configuration,
-                     text: default_configuration,
-                     type: default_configuration,
-                     value: default_configuration,
-                     &block)
-        self.configured_aria = aria
-        self.configured_classname = classname
-        self.configured_data = data
-        self.configured_disabled = disabled
-        self.configured_full_width = full_width
-        self.configured_id = id
-        self.configured_link = link
-        self.configured_loading = loading
-        self.configured_new_window = new_window
-        self.configured_variant = variant
-        self.configured_tag = tag
-        self.configured_text = text
-        self.configured_type = type
-        self.configured_value = value
-        self.block = block_given? ? block : nil
+      partial "pb_button/button"
+
+      prop :disabled, type: Playbook::Props::Boolean,
+                      default: false
+      prop :full_width, type: Playbook::Props::Boolean,
+                        default: false
+      prop :link
+      prop :loading, type: Playbook::Props::Boolean,
+                     default: false
+      prop :new_window, type: Playbook::Props::Boolean,
+                        default: false
+      prop :variant, type: Playbook::Props::Enum,
+                     values: %w[primary secondary link],
+                     default: "primary"
+      prop :text
+      prop :type
+      prop :value
+
+      def options
+        {
+          id: id,
+          data: data,
+          class: classname,
+          disabled: disabled,
+          aria: aria,
+          type: type,
+          value: value,
+        }.compact
       end
 
-      def disabled
-        is_true? configured_disabled
+      def link_options
+        options.merge(
+          href: link,
+          target: new_window ? "_blank" : "_self"
+        )
       end
 
-      def loading
-        is_true? configured_loading
+      def tag
+        link ? "a" : "button"
+      end
+
+    private
+
+      def classname
+        generate_classname("pb_button_kit", variant, full_width_class, disabled_class, loading_class)
+      end
+
+      def disabled_class
+        disabled ? "disabled" : "enabled"
+      end
+
+      def full_width_class
+        full_width ? "block" : "inline"
       end
 
       def loading_class
         loading ? "loading" : nil
       end
-
-      def loading_icon
-        pb_icon = Playbook::PbIcon::Icon.new(icon: "spinner",
-                                             pulse: true,
-                                             fixed_width: true,
-                                             classname: "loading-icon")
-        ApplicationController.renderer.render(partial: pb_icon, as: :object)
-      end
-
-      def disabled_class
-        true_value(disabled, "disabled", "enabled")
-      end
-
-      def full_width_class
-        true_value(configured_full_width, "block", "inline")
-      end
-
-      def link
-        default_value(configured_link, "")
-      end
-
-      def variant
-        variant_options = %w[primary secondary link]
-        one_of_value(configured_variant, variant_options, "primary")
-      end
-
-      def tag
-        tag_options = %w[button a]
-        if link.empty?
-          one_of_value(configured_tag, tag_options, "button")
-        else
-          "a"
-        end
-      end
-
-      def new_window
-        true_value(configured_new_window, "_blank", "_self")
-      end
-
-      def text
-        default_value(configured_text, "")
-      end
-
-      def type
-        default_value(configured_type, "button")
-      end
-
-      def value
-        configured_value unless configured_value == DEFAULT
-      end
-
-      def yield(context:)
-        !block.nil? ? context.capture(&block) : text
-      end
-
-      def kit_class
-        kit_options = [
-          "pb_button_kit",
-          variant,
-          full_width_class,
-          disabled_class,
-          loading_class,
-        ]
-        kit_options.compact.join("_")
-      end
-
-      def to_partial_path
-        "pb_button/button"
-      end
-
-    private
-
-      DEFAULT = Object.new
-      private_constant :DEFAULT
-      def default_configuration
-        DEFAULT
-      end
-      attr_accessor(*PROPS)
     end
   end
 end
