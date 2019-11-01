23fd30da4f56a9d7782c77817db6b560de7bebaa
Kevin Skilton <kevin.skilton@powerhrg.com>
2019-11-01 10:48:00 -0400

    Remove prop url

--- app/pb_kits/playbook/pb_home_address_street/home_address_street.rb
+++ app/pb_kits/playbook/pb_home_address_street/home_address_street.rb
@@ -15,7 +15,6 @@ module Playbook
       prop :state
       prop :zipcode
       prop :territory
-      prop :url
       prop :dark, type: Playbook::Props::Boolean, default: false
 
       def classname
