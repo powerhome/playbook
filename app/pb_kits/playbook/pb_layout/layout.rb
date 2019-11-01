482e3df1710432a2229a19c94354bfc3fa6834ae
Gillian Wenhold <gwenhold@mac.com>
2019-11-01 12:17:34 -0400

    Updated layout

--- app/pb_kits/playbook/pb_layout/layout.rb
+++ app/pb_kits/playbook/pb_layout/layout.rb
@@ -2,92 +2,42 @@
 
 module Playbook
   module PbLayout
-    class Layout < Playbook::PbKit::Base
-      PROPS = %i[configured_classname
-                 configured_collapse
-                 configured_dark
-                 configured_data
-                 configured_full
-                 configured_id
-                 configured_position
-                 configured_transparent
-                 configured_size
-                 block].freeze
+    class Layout
+      include Playbook::Props
 
-      def initialize(classname: default_configuration,
-                     collapse: default_configuration,
-                     dark: default_configuration,
-                     data: default_configuration,
-                     full: default_configuration,
-                     id: default_configuration,
-                     position: default_configuration,
-                     transparent: default_configuration,
-                     size: default_configuration,
-                     &block)
-        self.configured_classname = classname
-        self.configured_collapse = collapse
-        self.configured_dark = dark
-        self.configured_data = data
-        self.configured_full = full
-        self.configured_id = id
-        self.configured_position = position
-        self.configured_transparent = transparent
-        self.configured_size = size
-        self.block = block_given? ? block : nil
+      partial "pb_layout/layout"
+
+      prop :collapse, default: "xs"
+      prop :dark, type: Playbook::Props::Boolean, default: false
+      prop :full, type: Playbook::Props::Boolean, default: false
+      prop :position, default: "left"
+      prop :transparent, type: Playbook::Props::Boolean, default: false
+      prop :size, default: "md"
+
+      def dark_class
+        dark ? "dark" : nil
       end
 
-      def position
-        if configured_position == default_configuration
-          "_left"
-        else
-          "_#{configured_position}"
-        end
+      def full_class
+        full ? " full" : ""
       end
 
-      def transparent
-        true_value(configured_transparent, "_transparent", "")
+      def transparent_class
+        transparent ? "transparent" : nil
       end
 
-      def size
-        if configured_size == default_configuration
-          "md"
-        else
-          configured_size
-        end
+      def collapse_class
+        collapse == "xs" ? " layout_#{position}_collapse_xs" : " layout_#{position}_collapse_#{collapse}"
       end
 
-      def collapse
-        if configured_collapse == default_configuration
-          " layout#{position}_collapse_xs"
-        else
-          " layout#{position}_collapse_#{configured_collapse}"
-        end
-      end
-
-      def full
-        true_value(configured_full, " full", "")
-      end
-
-      def dark
-        true_value(configured_dark, "_dark", "")
+      def classname
+        first_class = generate_classname("pb_layout", size, position, dark_class, transparent_class)
+        classname = first_class + full_class + collapse_class
       end
 
       def yield(context:)
-        context.capture(&block)
+        context.capture(&children)
       end
-
-      def to_partial_path
-        "pb_layout/layout"
-      end
-
-    private
-
-      DEFAULT = Object.new
-      private_constant :DEFAULT
-      def default_configuration
-        DEFAULT
-      end
-      attr_accessor(*PROPS)
     end
   end
 end
