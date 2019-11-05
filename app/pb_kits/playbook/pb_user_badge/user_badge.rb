a5bb5512773a47e7a485ef6faa79478283f2e8e0
Ezz Abousaif <46111475+eabousaif@users.noreply.github.com>
2019-11-05 09:59:07 -0500

    Modernize User Badge Kit (#420)

--- app/pb_kits/playbook/pb_user_badge/user_badge.rb
+++ app/pb_kits/playbook/pb_user_badge/user_badge.rb
@@ -2,61 +2,25 @@
 
 module Playbook
   module PbUserBadge
-    class UserBadge < Playbook::PbKit::Base
-      include ActionView::Helpers::AssetTagHelper
+    class UserBadge
+      include Playbook::Props
 
-      PROPS = %i[configured_badge
-                 configured_classname
-                 configured_data
-                 configured_id
-                 configured_size].freeze
+      partial "pb_user_badge/user_badge"
 
-      def initialize(badge: default_configuration,
-                     classname: default_configuration,
-                     data: default_configuration,
-                     id: default_configuration,
-                     size: default_configuration)
-        self.configured_badge = badge
-        self.configured_classname = classname
-        self.configured_data = data
-        self.configured_id = id
-        self.configured_size = size
+      prop :badge, type: Playbook::Props::Enum,
+                   values: %w[million-dollar veteran],
+                   default: "million-dollar"
+      prop :size, type: Playbook::Props::Enum,
+                  values: %w[sm md lg],
+                  default: "md"
+
+      def classname
+        generate_classname("pb_user_badge_kit", size)
       end
 
       def display_badge
-        "pb_user_badge/badges/#{badge}" if is_set? configured_badge
+        "pb_user_badge/badges/#{badge}"
       end
-
-      def kit_class
-        kit_options = [
-          "pb_user_badge_kit",
-          size,
-        ]
-        kit_options.join("_")
-      end
-
-      def to_partial_path
-        "pb_user_badge/user_badge"
-      end
-
-    private
-
-      def badge
-        badge_options = %w[million-dollar veteran]
-        one_of_value(configured_badge, badge_options, "million-dollar")
-      end
-
-      def size
-        size_options = %w[sm md lg]
-        one_of_value(configured_size, size_options, "md")
-      end
-
-      DEFAULT = Object.new
-      private_constant :DEFAULT
-      def default_configuration
-        DEFAULT
-      end
-      attr_accessor(*PROPS)
     end
   end
 end
