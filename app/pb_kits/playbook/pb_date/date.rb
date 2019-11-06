d912c676717ff911c1e5d1198e73981bf70cb5dd
Leila Weiss <leila.weiss@powerhrg.com>
2019-11-06 09:26:59 -0500

    Render calender icon correctly

--- app/pb_kits/playbook/pb_date/date.rb
+++ app/pb_kits/playbook/pb_date/date.rb
@@ -13,64 +13,27 @@ module Playbook
                   values: %w[lg sm xs],
                   default: "sm"
 
-      def display_size
-        case size
-        when "lg"
-          lg_date_string
-        when "sm"
-          sm_date_string
-        else
-          xs_date_string
-        end
-      end
-
       def classname
         generate_classname("pb_date_kit")
       end
 
-      def xs_date_string
+      def xs_date
         "#{pb_date_time.to_day_of_week.upcase} &middot; #{pb_date_time.to_month.upcase} #{pb_date_time.to_day}".html_safe
       end
 
-      def lg_date_string
+      def lg_date
         "#{pb_date_time.to_month.upcase} #{pb_date_time.to_day}"
       end
 
-      def sm_date_string
+      def sm_date
         "#{pb_date_time.to_day_of_week.upcase} &middot; #{pb_date_time.to_month.upcase} #{pb_date_time.to_day}".html_safe
       end
 
     private
 
       def pb_date_time
-        @pb_date_time ||= Playbook::PbKit::PbDateTime.new(date)
+        pb_date_time ||= Playbook::PbKit::PbDateTime.new(date)
       end
-
-      # def icon
-      #   pb_icon = Playbook::PbIcon::Icon.new(icon: "calendar", fixed_width: true)
-      #   ApplicationController.renderer.render(partial: pb_icon, as: :object)
-      # end
-
-      # def text
-      #   content_tag(:span) do
-      #     "#{pb_date_time.to_day_of_week.upcase} &middot; #{pb_date_time.to_month.upcase} #{pb_date_time.to_day}".html_safe
-      #   end
-      # end
-
-      # def display_size_xs
-      #   pb_value = Playbook::PbTitle::Title.new(size: 4, text: text)
-      #   ApplicationController.renderer.render(partial: pb_value, as: :object)
-      # end
-
-      # def display_size_sm
-      #   pb_value = Playbook::PbTitle::Title.new(size: 4, text: icon + text)
-      #   ApplicationController.renderer.render(partial: pb_value, as: :object)
-      # end
-
-      # def display_size_lg
-      #   pb_value_lg = Playbook::PbTitle::Title.new(size: 3, text: "#{timestamp.to_month.upcase} #{timestamp.to_day}")
-      #   ApplicationController.renderer.render(partial: pb_value_lg, as: :object)
-      # end
     end
   end
 end
