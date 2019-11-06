c800ed780a06e45f15f1e81e8d9c9685ded7c7dc
Leila Weiss <leila.weiss@powerhrg.com>
2019-11-06 07:34:04 -0500

    Create methods for sizes

--- app/pb_kits/playbook/pb_date/date.rb
+++ app/pb_kits/playbook/pb_date/date.rb
@@ -8,24 +8,19 @@ module Playbook
       include Playbook::Props
       partial "pb_date/date"
 
-      prop :date
+      prop :date, required: true
       prop :size, type: Playbook::Props::Enum,
                   values: %w[lg sm xs],
                   default: "sm"
-      prop :timestamp
-      prop :timezone, type: Playbook::Props::Enum,
-                      default: "America/New_York"
 
-
-
-      def display_value
+      def display_size
         case size
         when "lg"
-          display_value_lg
+          lg_date_string
         when "sm"
-          display_value_sm
+          sm_date_string
         else
-          display_value_xs
+          xs_date_string
         end
       end
 
@@ -33,53 +28,49 @@ module Playbook
         generate_classname("pb_date_kit")
       end
 
+      def xs_date_string
+        "#{pb_date_time.to_day_of_week.upcase} &middot; #{pb_date_time.to_month.upcase} #{pb_date_time.to_day}".html_safe
+      end
+
+      def lg_date_string
+        "#{pb_date_time.to_month.upcase} #{pb_date_time.to_day}"
+      end
+
+      def sm_date_string
+        "#{pb_date_time.to_day_of_week.upcase} &middot; #{pb_date_time.to_month.upcase} #{pb_date_time.to_day}".html_safe
+      end
 
     private
 
-      def timestamp
-        if is_set? date
-          date = date
-        else
-          date = timestamp
-        end
-        Playbook::PbKit::PbDateTime.new(date, timezone_value)
+      def pb_date_time
+        @pb_date_time ||= Playbook::PbKit::PbDateTime.new(date)
       end
-
-      # def timezone_value
-      #   default_value(configured_timezone, "America/New_York")
-      # end
 
       # def icon
       #   pb_icon = Playbook::PbIcon::Icon.new(icon: "calendar", fixed_width: true)
       #   ApplicationController.renderer.render(partial: pb_icon, as: :object)
       # end
 
-      def text
-        content_tag(:span) do
-          "#{timestamp.to_day_of_week.upcase} &middot; #{timestamp.to_month.upcase} #{timestamp.to_day}".html_safe
-        end
-      end
+      # def text
+      #   content_tag(:span) do
+      #     "#{pb_date_time.to_day_of_week.upcase} &middot; #{pb_date_time.to_month.upcase} #{pb_date_time.to_day}".html_safe
+      #   end
+      # end
 
-      def display_value_xs
-        if is_set?(timestamp) || is_set?(date)
-          pb_value = Playbook::PbTitle::Title.new(size: 4, text: text)
-          ApplicationController.renderer.render(partial: pb_value, as: :object)
-        end
-      end
+      # def display_size_xs
+      #   pb_value = Playbook::PbTitle::Title.new(size: 4, text: text)
+      #   ApplicationController.renderer.render(partial: pb_value, as: :object)
+      # end
 
-      def display_value_sm
-        if is_set?(timestamp) || is_set?(date)
-          pb_value = Playbook::PbTitle::Title.new(size: 4, text: icon + text)
-          ApplicationController.renderer.render(partial: pb_value, as: :object)
-        end
-      end
+      # def display_size_sm
+      #   pb_value = Playbook::PbTitle::Title.new(size: 4, text: icon + text)
+      #   ApplicationController.renderer.render(partial: pb_value, as: :object)
+      # end
 
-      def display_value_lg
-        if is_set?(timestamp) || is_set?(date)
-          pb_value_lg = Playbook::PbTitle::Title.new(size: 3, text: "#{timestamp.to_month.upcase} #{timestamp.to_day}")
-          ApplicationController.renderer.render(partial: pb_value_lg, as: :object)
-        end
-      end
+      # def display_size_lg
+      #   pb_value_lg = Playbook::PbTitle::Title.new(size: 3, text: "#{timestamp.to_month.upcase} #{timestamp.to_day}")
+      #   ApplicationController.renderer.render(partial: pb_value_lg, as: :object)
+      # end
     end
   end
 end
