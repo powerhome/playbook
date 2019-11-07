4258445feb1c9b551e53959f1ef64363f4662d6a
Darren D'Costa <darren.dcosta@powerhrg.com>
2019-11-07 13:57:18 -0500

    Modernize Time Playbook kit (#428)
    * Format time kit and add spec
    
    * Configure time kit
    
    Work on timezone
    
    * Fix timestamp
    
    * Create format timezone string
    
    * Add specs
    
    * Add timezone prop spec

---
+++ spec/pb_kits/playbook/kits/time_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require_relative "../../../../app/pb_kits/playbook/pb_time/time"
+
+RSpec.describe Playbook::PbTime::Time do
+  subject { Playbook::PbTime::Time }
+
+  it { is_expected.to define_partial }
+  it { is_expected.to define_prop(:time) }
+  it { is_expected.to define_prop(:timezone).with_default("America/New_York") }
+  it do
+    is_expected.to define_enum_prop(:size)
+      .with_default("sm")
+      .with_values("lg", "sm", "xs")
+  end
+
+  describe "#classname" do
+    it "returns namespaced class name", :aggregate_failures do
+      required_props = { time: Time.now }
+
+      expect(subject.new(required_props).classname).to eq "pb_time_kit_sm"
+      expect(subject.new(required_props.merge(size: "lg")).classname).to eq "pb_time_kit_lg"
+    end
+  end
+end
