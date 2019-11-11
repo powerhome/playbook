e2f1f71de7d8aaad20b53c0119725d482df3afcf
Jonathan Zazula <djonyriz@gmail.com>
2019-11-11 09:28:45 -0500

    Updating Tests

--- spec/pb_kits/playbook/kits/nav_spec.rb
+++ spec/pb_kits/playbook/kits/nav_spec.rb
@@ -10,11 +10,16 @@ RSpec.describe Playbook::PbNav::Nav do
   it { is_expected.to define_prop(:link)
                   .with_default("#") }
   it { is_expected.to define_prop(:title) }
+  it { is_expected.to define_enum_prop(:orientation)
+                      .with_values("horizontal", "vertical")
+                      .with_default("vertical")}
 
   describe "#classname" do
     it "returns namespaced class name", :aggregate_failures do
-      expect(subject.new({}).classname).to eq "pb_nav_list"
-      expect(subject.new(classname: "additional_class").classname).to eq "pb_nav_list additional_class"
+      expect(subject.new({}).classname).to start_with "pb_nav_list_vertical"
+      expect(subject.new(orientation: "horizontal").classname).to eq "pb_nav_list_horizontal"
+      expect(subject.new(orientation: "vertical").classname).to eq "pb_nav_list_vertical"
     end
   end
+
 end
