72ee4f02c90ba2da798d8af16e7742c083c95277
Megan Trimble <mtrimble6@me.com>
2019-11-11 16:35:09 -0500

    Selectable Card React WIP

--- app/pb_kits/playbook/pb_selectable_card/docs/_selectable_card_default.jsx
+++ app/pb_kits/playbook/pb_selectable_card/docs/_selectable_card_default.jsx
@@ -9,12 +9,18 @@ function SelectableCardDefault() {
         id={"selectable_card"}
         name={"selectable_card"}
         text={"Selectable Card"}
+        value={"1"}
+        checked={true}
       />
+
+      <br></br>
+
       <SelectableCard 
         className={"pb_selectable_card_kit"}
-        id={"selectable_card"}
-        name={"selectable_card"}
+        id={"selectable_card_2"}
+        name={"selectable_card_2"}
         text={"Selectable Card"}
+        value={"2"}
       />
     </div>
   )
