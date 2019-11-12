48ccdef05ea748db09ccb77994d8e02d1654f42b
Eva Louderback <evalouderback@gmail.com>
2019-11-11 19:05:04 -0500

    added users

--- app/pb_kits/playbook/pb_multiple_users/_multiple_users.jsx
+++ app/pb_kits/playbook/pb_multiple_users/_multiple_users.jsx
@@ -3,60 +3,22 @@
 
 import React from 'react'
 
-import classnames from 'classnames'
-import { map } from 'lodash'
-import { Image } from "../"
+/*
+// Un-comment to import kits here
+import { Body } from '../'
+*/
 
 type MultipleUsersProps = {
   className?: String,
-  name: String,
-  number?: '1' | '2' | '3' | '4',
-  url: String,
+  data?: String,
+  id?: String,
+
 }
 
-const initials = function(name) {
-  if (name) {
-    return map(name.split(/\s/), name => name[0]).join('').substring(0,2)
-  }
-}
-
-const image = function(url, name) {
-  if (url) {
-    return (
-      <Image alt={name}
-          url={url}
-      />
-    )
-  }
-}
-
-const PbStatus = ({ size, status } : { size: String, status: String }) => (
-  <div className={`pb_online_status_kit_${status} size_${size}`}/>
+const MultipleUsers = ({ className, data, id }: MultipleUsersProps) => (
+  <div>
+    {`kit content`}
+  </div>
 )
-
-const MultipleUsers = ({
-  className,
-  name=null,
-  number='1',
-  url
-}: MultipleUsersProps) => {
-  const css = classnames([
-    `pb_multiple_users_kit`,
-    `multiple_users_${number}`,
-    className,
-  ])
-
-  return (
-    <div className={css}
-        data-initials={initials(name)}
-    >
-      <div
-          className="multiple_users_wrapper"
-          data-initials={initials(name)}
-      >
-        {image(url, name)}
-      </div>
-  )
-}
 
 export default MultipleUsers
--- app/pb_kits/playbook/pb_multiple_users/docs/_multiple_users_default.jsx
+++ app/pb_kits/playbook/pb_multiple_users/docs/_multiple_users_default.jsx
@@ -1,35 +1,12 @@
 import React from "react"
-import { MultipleUsers } from "../../"
+import {MultipleUsers} from "../../"
 
 function MultipleUsersDefault() {
   return (
     <div>
-      <Avatar
-          name="Terry Johnson"
-          number="1"
-          url="https://randomuser.me/api/portraits/men/44.jpg"
-      />
-      <br/>
-      <Avatar
-          name="Terry Johnson"
-          number="2"
-          url="https://randomuser.me/api/portraits/men/44.jpg"
-      />
-      <br/>
-      <Avatar
-          name="Terry Johnson"
-          number="3"
-          url="https://randomuser.me/api/portraits/men/44.jpg"
-      />
-      <br/>
-      <Avatar
-          name="Terry Johnson"
-          number="4"
-          url="https://randomuser.me/api/portraits/men/44.jpg"
-      />
-      <br/>
+      <MultipleUsers />
     </div>
   )
 }
 
-export default MultipleUsersDefault;
+export default MultipleUsersDefault
