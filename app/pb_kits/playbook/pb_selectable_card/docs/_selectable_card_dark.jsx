41f004bfc1ab838ff01e8cb19e93380e418147d2
Megan Trimble <mtrimble6@me.com>
2019-11-12 13:08:46 -0500

    Selectable Card Block Content Added WIP

--- app/pb_kits/playbook/pb_selectable_card/docs/_selectable_card_dark.jsx
+++ app/pb_kits/playbook/pb_selectable_card/docs/_selectable_card_dark.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import SelectableCard from "../_selectable_card.jsx"
+import Icon from "../../pb_icon/_icon.jsx"
 
 function SelectableCardDark() {
   return (
@@ -23,6 +24,35 @@ function SelectableCardDark() {
         text={"Selectable Card"}
         value={"2"}
         dark={true}
+      />
+
+      <br></br>
+
+      <SelectableCard 
+        className={"pb_selectable_card_kit_dark"}
+        id={"selectable_card_dark_3"}
+        name={"selectable_card_dark_3"}
+        value={"3"}
+        dark={true}
+        >
+        <div>
+        <Icon icon={"user"}></Icon>
+        </div>
+        <div>
+        The Content for Selectable Card goes here
+        </div>
+      </SelectableCard>
+
+     <br></br>
+
+      <SelectableCard 
+        className={"pb_selectable_card_kit_dark"}
+        id={"selectable_card_dark_3"}
+        name={"selectable_card_dark_4"}
+        text={"Selectable Card Disabled"}
+        value={"4"}
+        dark={true}
+        disabled={true}
       />
     </div>
   )
