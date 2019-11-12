c21e88226dccc607d369abf35f24b984dc152e27
christina tai <christina.tai@powerhrg.com>
2019-11-12 12:51:22 -0500

    Fixed testing failures

--- spec/pb_kits/playbook/kits/date_stacked_spec.rb
+++ spec/pb_kits/playbook/kits/date_stacked_spec.rb
@@ -2,11 +2,46 @@
 
 require_relative "../../../../app/pb_kits/playbook/pb_date_stacked/date_stacked"
 
-RSpec.describe Playbook::PbDate stacked::Date stacked do
-  subject { Playbook::PbDate stacked::Date stacked }
+RSpec.describe Playbook::PbDateStacked::DateStacked do
+  subject { Playbook::PbDateStacked::DateStacked }
 
   it { is_expected.to define_partial }
 
-  # Do not leave this file blank. Use other spec files for example tests.
+  it { is_expected.to define_enum_prop(:align)
+                      .with_values("left", "center", "right")
+                      .with_default "left" }
+  it { is_expected.to define_enum_prop(:size)
+                      .with_values("sm", "md")
+                      .with_default "sm" }
+  it { is_expected.to define_boolean_prop(:dark)
+                      .with_default false }
+  it { is_expected.to define_boolean_prop(:reverse)
+                      .with_default false }
+  it { is_expected.to define_string_prop(:date) }
 
+  describe "#month" do
+    it "returns the date prop's month and month as a string" do
+      expect(subject.new(date: Date.today).month).to eq Date.today.month.to_s
+    end
+  end
+
+  describe "#day" do
+    it "returns the date prop's day and month as a string" do
+      expect(subject.new(date: Date.today).day).to eq Date.today.day.to_s
+    end
+  end
+
+  describe "#year" do
+    it "returns the date prop's year as a string if date prop's year is not current year" do
+      expect(subject.new(date: Date.new(2018, 03, 20)).year).to include "2018"
+    end
+  end
+
+  describe "#classname" do
+    it "returns namespaced class name", :aggregate_failures do
+      expect(subject.new({}).classname).to eq "pb_date_year_stacked_left"
+      expect(subject.new(align: "right").classname).to eq "pb_date_year_stacked_right"
+      expect(subject.new(classname: "additional_class").classname).to eq "pb_date_year_stacked_left additional_class"
+    end
+  end
 end
