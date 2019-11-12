19d30e48b9343daa02b93995d0f5da95b532c89b
Jasper Furniss <jasper.furniss@powerhrg.com>
2019-11-12 18:00:56 -0500

    Added OnSelect Handler for SelectableCard

--- app/pb_kits/playbook/pb_selectable_card/_selectable_card.jsx
+++ app/pb_kits/playbook/pb_selectable_card/_selectable_card.jsx
@@ -1,9 +1,10 @@
 /* @flow */
 /*eslint-disable react/no-multi-comp, flowtype/space-before-type-colon */
 
-import React from 'react'
-import { Icon } from '../'
+import React from "react";
+import { Icon } from "../";
 
+type EventHandler = (SyntheticInputEvent<HTMLInputElement>) => void;
 type SelectableCardProps = {
   checked?: Boolean,
   children?: Array<React.ReactChild>,
@@ -14,37 +15,40 @@ type SelectableCardProps = {
   icon?: Boolean,
   id?: String,
   name?: String,
-  showSelected?: Boolean,
-  showUnselected?: Boolean,
+  onSelect?: EventHandler,
   text?: String,
-  value?: String,
+  value?: String
+};
 
-}
-
-const SelectableCard = ({ 
+const SelectableCard = ({
   checked,
   children,
   className,
-  dark,
   data,
   disabled,
   icon,
   id,
   name,
-  showSelected,
-  showUnselected,
+  onSelect,
   text,
   value
 }: SelectableCardProps) => (
-  <span className={className} >
-    <input type="checkbox" name={name} id={id} value={value} defaultChecked={checked} showSelected />
+  <span className={className}>
+    <input
+      type="checkbox"
+      name={name}
+      id={id}
+      value={value}
+      defaultChecked={checked}
+      onChange={onSelect}
+    />
     <label htmlFor={name} dark>
-      { text || children }
-      <div className={"pb_selectable_card_circle"}> 
-        <Icon icon="check" fixedWidth/>
+      {children}
+      <div className={"pb_selectable_card_circle"}>
+        <Icon icon="check" fixedWidth />
       </div>
     </label>
   </span>
-)
+);
 
-export default SelectableCard
+export default SelectableCard;
