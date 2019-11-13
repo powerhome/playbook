2da839bcac995495a194eced9369a4870704f052
Eva Louderback <evalouderback@gmail.com>
2019-11-12 19:12:13 -0500

    progress

--- app/pb_kits/playbook/pb_multiple_users/multiple_users.rb
+++ app/pb_kits/playbook/pb_multiple_users/multiple_users.rb
@@ -7,24 +7,14 @@ module Playbook
 
       partial "pb_multiple_users/multiple_users"
 
-      prop :user, type: Playbook::Props::HashArray, default: []
-      prop :user_one
-      prop :user_two
-      prop :user_three
-      prop :user_four
-
-
+      prop :users, type: Playbook::Props::HashArray, default: []
 
       def more_than_four
-        user.select {|user| user[:user_type] == "wrong number" }
-      end
-
-      def valid_contacts
-        user.select {|user| user[:user_type] != "wrong number" }
+        users.count > 4
       end
 
       def classname
-        generate_classname("pb_person_contact_kit")
+        generate_classname("pb_multiple_users_kit")
       end
     end
   end
