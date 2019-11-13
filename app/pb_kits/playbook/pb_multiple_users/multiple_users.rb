cf963ee9d40f3db228bf3ec88ec78a9100e8fea2
Eva Louderback <evalouderback@gmail.com>
2019-11-13 16:13:20 -0500

    multiple_users_wip

--- app/pb_kits/playbook/pb_multiple_users/multiple_users.rb
+++ app/pb_kits/playbook/pb_multiple_users/multiple_users.rb
@@ -8,13 +8,18 @@ module Playbook
       partial "pb_multiple_users/multiple_users"
 
       prop :users, type: Playbook::Props::HashArray, default: []
+      prop :reverse, type: Playbook::Props::Boolean, default: false
 
       def more_than_four
         users.count > 4
       end
 
       def classname
-        generate_classname("pb_multiple_users_kit")
+        generate_classname("pb_multiple_users_kit", reverse_class)
+      end
+
+      def reverse_class
+        reverse ? "reverse" : nil
       end
     end
   end
