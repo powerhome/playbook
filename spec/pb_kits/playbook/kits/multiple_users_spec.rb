e5726466fe669455d8a1e152fb28d6a0e6ee1e19
Markie Arnold <markiearnold@gmail.com>
2019-11-13 20:57:12 -0600

    Multiple Users Support Branch

--- spec/pb_kits/playbook/kits/multiple_users_spec.rb
+++ spec/pb_kits/playbook/kits/multiple_users_spec.rb
@@ -2,11 +2,25 @@
 
 require_relative "../../../../app/pb_kits/playbook/pb_multiple_users/multiple_users"
 
-RSpec.describe Playbook::PbMultiple users::Multiple users do
-  subject { Playbook::PbMultiple users::Multiple users }
+RSpec.describe Playbook::PbMultipleUsers::MultipleUsers do
+  subject { Playbook::PbMultipleUsers::MultipleUsers }
 
   it { is_expected.to define_partial }
 
-  # Do not leave this file blank. Use other spec files for example tests.
+  it { is_expected.to define_boolean_prop(:reverse)
+                      .with_default(false) }
+  it { is_expected.to define_prop(:users)
+                      .of_type(Playbook::Props::HashArray) }
 
+  describe "#classname" do
+    it "returns namespaced class name", :aggregate_failures do
+      expect(subject.new(users: []).classname).to eq "pb_multiple_users_kit"
+      expect(subject.new(users: [], classname: "additional_class").classname).to eq "pb_multiple_users_kit additional_class"
+      expect(subject.new(users: [], reverse: true).classname).to eq "pb_multiple_users_kit_reverse"
+    end
+  end
+
+  it "raises an error when not given users" do
+    expect { subject.new({}) }.to raise_error(Playbook::Props::Error)
+  end
 end
