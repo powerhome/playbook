eae3674f8312e2ec795d8abd0c57a01c2ef96441
Markie Arnold <markiearnold@gmail.com>
2019-11-13 22:51:59 -0600

    Circle Icon Button

--- app/pb_kits/playbook/pb_circle_icon_button/docs/_circle_icon_button_dark.jsx
+++ app/pb_kits/playbook/pb_circle_icon_button/docs/_circle_icon_button_dark.jsx
@@ -3,20 +3,30 @@ import { CircleIconButton } from "../../";
 
 const CircleIconButtonDark = () => (
   <div>
-    <CircleIconButton icon="plus"
-      button={{dark: true}}   
+    <CircleIconButton
+      button={{dark: true}}
+      icon="plus"
     />
-  <br/>
-    <CircleIconButton icon="pen"
+
+    <br/>
+
+    <CircleIconButton
       button={{variant: "secondary", dark: true}}
+      icon="pen"
     />
-  <br/>
-    <CircleIconButton icon="pen"
+
+    <br/>
+
+    <CircleIconButton
       button={{disabled: true, dark: true}}
+      icon="times"
     />
-  <br/>
-    <CircleIconButton icon="pen"
+
+    <br/>
+
+    <CircleIconButton
       button={{variant: "link", dark: true}}
+      icon="user"
     />
   </div>
 );
