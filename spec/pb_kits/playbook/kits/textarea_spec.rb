446e2ebd97ddf91af16dfa58ea33171a04246902
christina tai <christina.tai@powerhrg.com>
2019-11-29 14:46:18 -0500

    Update textarea_spec.rb

--- spec/pb_kits/playbook/kits/textarea_spec.rb
+++ spec/pb_kits/playbook/kits/textarea_spec.rb
@@ -7,6 +7,19 @@ RSpec.describe Playbook::PbTextarea::Textarea do
 
   it { is_expected.to define_partial }
 
-  # Do not leave this file blank. Use other spec files for example tests.
+  it { is_expected.to define_string_prop(:label)
+  it { is_expected.to define_string_prop(:placeholder)
+  it { is_expected.to define_string_prop(:value)
+  it { is_expected.to define_boolean_prop(:dark)
+                      .with_default(false) }
+  it { is_expected.to define_prop(:rows)
+                      .of_type(Playbook::Props::Number)
+                      .with_default(4) }
 
+  describe "#classname" do
+    it "returns namespaced class name", :aggregate_failures do
+      expect(subject.new({}).classname).to eq "pb_textarea_kit"
+      expect(subject.new({dark:true}).classname).to eq "pb_textarea_kit_dark"
+    end
+  end
 end
