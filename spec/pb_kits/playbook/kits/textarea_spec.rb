7b204ddbab5cee9c4e84633f770dac8e5b7eea29
christina tai <christina.tai@powerhrg.com>
2019-11-29 15:12:17 -0500

    Fixed syntax error on textarea_spec.rb

--- spec/pb_kits/playbook/kits/textarea_spec.rb
+++ spec/pb_kits/playbook/kits/textarea_spec.rb
@@ -7,9 +7,9 @@ RSpec.describe Playbook::PbTextarea::Textarea do
 
   it { is_expected.to define_partial }
 
-  it { is_expected.to define_string_prop(:label)
-  it { is_expected.to define_string_prop(:placeholder)
-  it { is_expected.to define_string_prop(:value)
+  it { is_expected.to define_string_prop(:label) }
+  it { is_expected.to define_string_prop(:placeholder) }
+  it { is_expected.to define_string_prop(:value) }
   it { is_expected.to define_boolean_prop(:dark)
                       .with_default(false) }
   it { is_expected.to define_prop(:rows)
@@ -19,7 +19,6 @@ RSpec.describe Playbook::PbTextarea::Textarea do
   describe "#classname" do
     it "returns namespaced class name", :aggregate_failures do
       expect(subject.new({}).classname).to eq "pb_textarea_kit"
-      expect(subject.new({dark:true}).classname).to eq "pb_textarea_kit_dark"
     end
   end
 end
