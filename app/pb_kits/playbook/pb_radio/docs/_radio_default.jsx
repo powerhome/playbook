5eb3e7b5838acafe6781bf44910b3876d8145ffc
Eva Louderback <evalouderback@gmail.com>
2019-11-29 15:33:24 -0500

    created radio button with rails, react, and dark mode

---
+++ app/pb_kits/playbook/pb_radio/docs/_radio_default.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+import {Radio} from "../_radio.jsx"
+
+function RadioDefault() {
+  return (
+    <>
+      <Radio label="Power"
+          name="Group1"
+          value="Power"
+      />
+      <br/>
+      <Radio label="Nitro"
+          name="Group1"
+          value="Nitro"
+      />
+      <br/>
+      <Radio label="Google"
+          name="Group1"
+          value="Google"
+      />
+    </>
+  )
+}
+export default RadioDefault
---
+++ app/pb_kits/playbook/pb_radio/docs/index.js
@@ -0,0 +1,2 @@
+export {default as RadioDefault} from './_radio_default.jsx';
+export {default as RadioDark} from './_radio_dark.jsx';
