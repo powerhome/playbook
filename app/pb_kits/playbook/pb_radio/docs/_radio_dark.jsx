7e50d57dd4dd5897ae1b9ef803c4b334b2bc900e
Eva Louderback <50629488+evalouderback@users.noreply.github.com>
2019-12-04 14:30:11 -0500

    Update app/pb_kits/playbook/pb_radio/docs/_radio_dark.jsx
    Co-Authored-By: Stephen Marshall <smarshall1980@gmail.com>

--- app/pb_kits/playbook/pb_radio/docs/_radio_dark.jsx
+++ app/pb_kits/playbook/pb_radio/docs/_radio_dark.jsx
@@ -6,41 +6,16 @@ function RadioDark() {
     <>
       <Radio
           dark
-<<<<<<< HEAD
           label='Power'
           name='Group1'
           value='Power'
-=======
-          label="Power"
-          name="Group1"
-          value="Power"
->>>>>>> updated radio button
       />
     <br/>
       <Radio
           dark
-<<<<<<< HEAD
           label='Nitro'
           name='Group1'
           value='Nitro'
-=======
-          label="Nitro"
-          name="Group1"
-          value="Nitro"
->>>>>>> updated radio button
-      />
-    <br/>
-      <Radio
-          dark
-<<<<<<< HEAD
-          label='Google'
-          name='Group1'
-          value='Google'
-=======
-          label="Google"
-          name="Group1"
-          value="Google"
->>>>>>> updated radio button
       />
     </>
   )
