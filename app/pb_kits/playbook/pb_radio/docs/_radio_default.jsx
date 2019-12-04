f5796dfdadcb7fc0ad67479e6f7c85c8213ab989
Eva Louderback <evalouderback@gmail.com>
2019-12-04 17:02:44 -0500

    got rid of double quotes and used body kit

--- app/pb_kits/playbook/pb_radio/docs/_radio_default.jsx
+++ app/pb_kits/playbook/pb_radio/docs/_radio_default.jsx
@@ -1,22 +1,22 @@
-import React from "react"
-import {Radio} from "../_radio.jsx"
+import React from 'react'
+import {Radio} from '../_radio.jsx'
 
 function RadioDefault() {
   return (
     <>
-      <Radio label="Power"
-          name="Group1"
-          value="Power"
+      <Radio label='Power'
+          name='Group1'
+          value='Power'
       />
       <br/>
-      <Radio label="Nitro"
-          name="Group1"
-          value="Nitro"
+      <Radio label='Nitro'
+          name='Group1'
+          value='Nitro'
       />
       <br/>
-      <Radio label="Google"
-          name="Group1"
-          value="Google"
+      <Radio label='Google'
+          name='Group1'
+          value='Google'
       />
     </>
   )
