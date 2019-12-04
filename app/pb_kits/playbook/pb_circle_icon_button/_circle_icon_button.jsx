ffe9aac2208d25094b274978ff78d668e683dce1
christina tai <christina.tai@powerhrg.com>
2019-12-04 09:03:02 -0500

    Fixed spec.rb

--- app/pb_kits/playbook/pb_circle_icon_button/_circle_icon_button.jsx
+++ app/pb_kits/playbook/pb_circle_icon_button/_circle_icon_button.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import classnames from "classnames"
 import {Button, Icon} from "../"
 
 type CircleIconButtonProps = {
@@ -10,17 +9,6 @@ type CircleIconButtonProps = {
   icon: String,
   id?: String,
 };
-
-const circleIconButtonCSS = ({
-  variant='primary',
-  disabled=false,
-  dark=false,
-}: circleIconButtonProps) => {
-  const variantStyle = variant !== null ? `_${variant}` : ''
-  const disabledStyle = disabled === true ? '_disabled' : ''
-  const darkStyle = dark === true ? '_dark' : ''
-  return 'pb_circle_icon_button_kit' + variantStyle + disabledStyle + darkStyle
-}
 
 const CircleIconButton = (props: CircleIconButtonProps) => {
   const {
--- spec/pb_kits/playbook/kits/circle_icon_button_spec.rb
+++ spec/pb_kits/playbook/kits/circle_icon_button_spec.rb
@@ -12,10 +12,16 @@ RSpec.describe Playbook::PbCircleIconButton::CircleIconButton do
                       .with_values("primary", "secondary", "link") }
   it { is_expected.to define_boolean_prop(:dark).with_default(false) }
   it { is_expected.to define_boolean_prop(:disabled).with_default(false) }
+  it { is_expected.to define_prop(:icon).that_is_required }
 
   describe "#classname" do
     it "returns namespaced class name", :aggregate_failures do
-      expect(subject.new({}).classname).to eq "pb_circle_icon_button_kit"
+      icon = "user"
+      expect(subject.new(icon: icon).classname).to eq "pb_circle_icon_button_kit"
+    end
+
+    it "raises an error when not given an icon" do
+      expect { subject.new{} }.to raise_error(Playbook::Props::Error)
     end
   end
 end
