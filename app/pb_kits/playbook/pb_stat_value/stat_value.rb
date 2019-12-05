e853647b792c69c03bee6ea2e6c9ffee646eec01
Stephen Marshall <stephen.marshall@powerhrg.com>
2019-12-05 13:52:39 -0600

    Use number_with_delimiter for stat_value.value

--- app/pb_kits/playbook/pb_stat_value/stat_value.rb
+++ app/pb_kits/playbook/pb_stat_value/stat_value.rb
@@ -4,11 +4,16 @@ module Playbook
   module PbStatValue
     class StatValue
       include Playbook::Props
+      include ActionView::Helpers::NumberHelper
 
       partial "pb_stat_value/stat_value"
 
       prop :unit
       prop :value, type: Playbook::Props::Number
+
+      def formatted_value
+        number_with_delimiter(value, delimiter: ",")
+      end
 
       def classname
         generate_classname("pb_stat_value_kit")
