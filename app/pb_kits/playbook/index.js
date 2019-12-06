6810ab0690b4c0c07be6dbc614dd71a83828830d
Rafael Garcia <rafbgarcia@gmail.com>
2019-12-06 10:43:40 -0300

    Export components directly

--- app/pb_kits/playbook/index.js
+++ app/pb_kits/playbook/index.js
@@ -1,130 +1,65 @@
 // React Component JSX Imports from the React Kits
-import Avatar from './pb_avatar/_avatar.jsx'
-import Badge from './pb_badge/_badge.jsx'
-import BarGraph from './pb_bar_graph/_bar_graph.jsx'
-import Body from './pb_body/_body.jsx'
-import Button from './pb_button/_button.jsx'
-import Caption from './pb_caption/_caption.jsx'
-import Card from './pb_card/_card.jsx'
-import Checkbox from './pb_checkbox/_checkbox.jsx'
-import CircleIconButton from './pb_circle_icon_button/_circle_icon_button.jsx'
-import Contact from './pb_contact/_contact.jsx'
-import Currency from './pb_currency/_currency.jsx'
-import DashboardValue from './pb_dashboard_value/_dashboard_value.jsx'
-import Date from './pb_date/_date.jsx'
-import DateRangeInline from './pb_date_range_inline/_date_range_inline.jsx'
-import DateYearStacked from './pb_date_year_stacked/_date_year_stacked.jsx'
-import DistributionBar from './pb_distribution_bar/_distribution_bar.jsx'
-import FixedConfirmationToast from './pb_fixed_confirmation_toast/_fixed_confirmation_toast.jsx'
-import Hashtag from './pb_hashtag/_hashtag.jsx'
-import HomeAddressStreet from './pb_home_address_street/_home_address_street.jsx'
-import Icon from './pb_icon/_icon.jsx'
-import IconCircle from './pb_icon_circle/_icon_circle.jsx'
-import IconValue from './pb_icon_value/_icon_value.jsx'
-import Image from './pb_image/_image.jsx'
-import LabelPill from './pb_label_pill/_label_pill.jsx'
-import LabelValue from './pb_label_value/_label_value.jsx'
-import Layout from './pb_layout/_layout.jsx'
-import LineGraph from './pb_line_graph/_line_graph.jsx'
-import List from './pb_list/_list.jsx'
-import ListItem from './pb_list/_list_item.jsx'
-import LoadingInline from './pb_loading_inline/_loading_inline.jsx'
-import Message from './pb_message/_message.jsx'
-import MultipleUsers from './pb_multiple_users/_multiple_users.jsx'
-import Nav from './pb_nav/_nav.jsx'
-import OnlineStatus from './pb_online_status/_online_status.jsx'
-import Person from './pb_person/_person.jsx'
-import PersonContact from './pb_person_contact/_person_contact.jsx'
-import Pill from './pb_pill/_pill.jsx'
-import ProgressPills from './pb_progress_pills/_progress_pills.jsx'
-import ProgressSimple from './pb_progress_simple/_progress_simple.jsx'
-import SectionSeparator from './pb_section_separator/_section_separator.jsx'
-import Select from './pb_select/_select.jsx'
-import SelectableCard from './pb_selectable_card/_selectable_card.jsx'
-import Source from './pb_source/_source.jsx'
-import StarRating from './pb_star_rating/_star_rating.jsx'
-import StatChange from './pb_stat_change/_stat_change.jsx'
-import StatValue from './pb_stat_value/_stat_value.jsx'
-import Table from './pb_table/_table.jsx'
-import Textarea from './pb_textarea/_textarea.jsx'
-import TextInput from './pb_text_input/_text_input.jsx'
-import Time from './pb_time/_time.jsx'
-import TimeStamp from './pb_timestamp/_timestamp.jsx'
-import Title from './pb_title/_title.jsx'
-import Toggle from './pb_toggle/_toggle.jsx'
-import User from './pb_user/_user.jsx'
-import UserBadge from './pb_user_badge/_user_badge.jsx'
+export Avatar from './pb_avatar/_avatar.jsx'
+export Badge from './pb_badge/_badge.jsx'
+export BarGraph from './pb_bar_graph/_bar_graph.jsx'
+export Body from './pb_body/_body.jsx'
+export Button from './pb_button/_button.jsx'
+export Caption from './pb_caption/_caption.jsx'
+export Card from './pb_card/_card.jsx'
+export Checkbox from './pb_checkbox/_checkbox.jsx'
+export CircleIconButton from './pb_circle_icon_button/_circle_icon_button.jsx'
+export Contact from './pb_contact/_contact.jsx'
+export Currency from './pb_currency/_currency.jsx'
+export DashboardValue from './pb_dashboard_value/_dashboard_value.jsx'
+export Date from './pb_date/_date.jsx'
+export DateRangeInline from './pb_date_range_inline/_date_range_inline.jsx'
+export DateYearStacked from './pb_date_year_stacked/_date_year_stacked.jsx'
+export DistributionBar from './pb_distribution_bar/_distribution_bar.jsx'
+export FixedConfirmationToast from './pb_fixed_confirmation_toast/_fixed_confirmation_toast.jsx'
+export Hashtag from './pb_hashtag/_hashtag.jsx'
+export HomeAddressStreet from './pb_home_address_street/_home_address_street.jsx'
+export Icon from './pb_icon/_icon.jsx'
+export IconCircle from './pb_icon_circle/_icon_circle.jsx'
+export IconValue from './pb_icon_value/_icon_value.jsx'
+export Image from './pb_image/_image.jsx'
+export LabelPill from './pb_label_pill/_label_pill.jsx'
+export LabelValue from './pb_label_value/_label_value.jsx'
+export Layout from './pb_layout/_layout.jsx'
+export LineGraph from './pb_line_graph/_line_graph.jsx'
+export List from './pb_list/_list.jsx'
+export ListItem from './pb_list/_list_item.jsx'
+export LoadingInline from './pb_loading_inline/_loading_inline.jsx'
+export Message from './pb_message/_message.jsx'
+export MultipleUsers from './pb_multiple_users/_multiple_users.jsx'
+export Nav from './pb_nav/_nav.jsx'
+export OnlineStatus from './pb_online_status/_online_status.jsx'
+export Person from './pb_person/_person.jsx'
+export PersonContact from './pb_person_contact/_person_contact.jsx'
+export Pill from './pb_pill/_pill.jsx'
+export ProgressPills from './pb_progress_pills/_progress_pills.jsx'
+export ProgressSimple from './pb_progress_simple/_progress_simple.jsx'
+export SectionSeparator from './pb_section_separator/_section_separator.jsx'
+export Select from './pb_select/_select.jsx'
+export SelectableCard from './pb_selectable_card/_selectable_card.jsx'
+export Source from './pb_source/_source.jsx'
+export StarRating from './pb_star_rating/_star_rating.jsx'
+export StatChange from './pb_stat_change/_stat_change.jsx'
+export StatValue from './pb_stat_value/_stat_value.jsx'
+export Table from './pb_table/_table.jsx'
+export Textarea from './pb_textarea/_textarea.jsx'
+export TextInput from './pb_text_input/_text_input.jsx'
+export Time from './pb_time/_time.jsx'
+export TimeStamp from './pb_timestamp/_timestamp.jsx'
+export Title from './pb_title/_title.jsx'
+export Toggle from './pb_toggle/_toggle.jsx'
+export User from './pb_user/_user.jsx'
+export UserBadge from './pb_user_badge/_user_badge.jsx'
 
 // Dashboard Settings
-import { commonSettings, dataColors } from './pb_dashboard/commonSettings'
-import lineGraphSettings from './pb_line_graph/lineGraphSettings'
-import barGraphSettings from './pb_bar_graph/barGraphSettings'
-import dashboardValueSettings from './pb_dashboard_value/dashboardValueSettings'
+export { commonSettings, dataColors } from './pb_dashboard/commonSettings'
+export lineGraphSettings from './pb_line_graph/lineGraphSettings'
+export barGraphSettings from './pb_bar_graph/barGraphSettings'
+export dashboardValueSettings from './pb_dashboard_value/dashboardValueSettings'
 
 // Other JS/Plugins
-import pbChart from './plugins/pb_chart_plugin.js'
-
-// All Exports for Consumption in Nitro
-export {
-  Avatar,
-  Badge,
-  BarGraph,
-  barGraphSettings,
-  Body,
-  Button,
-  Caption,
-  Card,
-  Checkbox,
-  CircleIconButton,
-  commonSettings,
-  Contact,
-  Currency,
-  DashboardValue,
-  dashboardValueSettings,
-  dataColors,
-  Date,
-  DateRangeInline,
-  DateYearStacked,
-  DistributionBar,
-  FixedConfirmationToast,
-  Hashtag,
-  HomeAddressStreet,
-  Icon,
-  IconCircle,
-  IconValue,
-  Image,
-  LabelPill,
-  LabelValue,
-  Layout,
-  LineGraph,
-  lineGraphSettings,
-  List,
-  ListItem,
-  LoadingInline,
-  Message,
-  MultipleUsers,
-  Nav,
-  OnlineStatus,
-  pbChart,
-  Person,
-  PersonContact,
-  Pill,
-  ProgressPills,
-  ProgressSimple,
-  SectionSeparator,
-  Select,
-  SelectableCard,
-  Source,
-  StarRating,
-  StatChange,
-  StatValue,
-  Table,
-  Textarea,
-  TextInput,
-  Time,
-  TimeStamp,
-  Title,
-  Toggle,
-  User,
-  UserBadge,
-}
+export pbChart from './plugins/pb_chart_plugin.js'
