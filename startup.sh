30a450c6286253a2459cf7aaa6d913088f97f214
Rafael Garcia <rafbgarcia@gmail.com>
2019-12-07 16:56:48 -0300

    Verbose assets:precompile

--- startup.sh
+++ startup.sh
@@ -1,11 +1,11 @@
 if [ $# -eq 0 ] ; then
   echo '=========== Precompiling'
-  cd spec/dummy; RAILS_ENV=production SECRET_KEY_BASE=does_not_matter_here bin/rails assets:precompile
+  cd spec/dummy; RAILS_ENV=production SECRET_KEY_BASE=does_not_matter_here bin/rails assets:precompile --trace
 else
   if [ $1 = "disable" ] ; then
     echo "=========== Precompile disabled"
   else
     echo 'argument supplied but different from not, precompiling'
-    cd spec/dummy; RAILS_ENV=production SECRET_KEY_BASE=does_not_matter_here bin/rails assets:precompile
+    cd spec/dummy; RAILS_ENV=production SECRET_KEY_BASE=does_not_matter_here bin/rails assets:precompile --trace
   fi
 fi
