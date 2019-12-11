d1a28542411f1daa6733ee7d36b7ec0467fc12bc
christina tai <christina.tai@powerhrg.com>
2019-12-11 14:02:29 -0500

    Changed single quotes to double quotes
    Also changed prop-types to flow types

--- app/pb_kits/playbook/pb_nav/docs/_block_no_title_nav.jsx
+++ app/pb_kits/playbook/pb_nav/docs/_block_no_title_nav.jsx
@@ -1,13 +1,13 @@
-import React from "react"
-import {Nav} from "../../"
-import NavItem from "../_item.jsx"
+import React from 'react'
+import {Nav} from '../../'
+import NavItem from '../_item.jsx'
 
 function BlockNoTitleNav() {
   return (
-    <Nav link="#" orientation="vertical">
-        <NavItem link="#" active={true}>{`Active Nav Item using text prop`}</NavItem>
-        <NavItem link="#">{`Nav Item using text prop`}</NavItem>
-        <NavItem link="#">{`Nav Item using text prop`}</NavItem>
+    <Nav link='#' orientation='vertical'>
+        <NavItem link='#' active={true}>{`Active Nav Item using text prop`}</NavItem>
+        <NavItem link='#'>{`Nav Item using text prop`}</NavItem>
+        <NavItem link='#'>{`Nav Item using text prop`}</NavItem>
     </Nav>
   )
 }
