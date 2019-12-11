07a915f8a51423c55c14836183cc5bce464af967
christina tai <christina.tai@powerhrg.com>
2019-12-11 12:10:35 -0500

    Nav (React) Kit Created
    Includes:
    - vertical
    - horizontal
    - block
    - example without title

--- app/pb_kits/playbook/pb_nav/_item.jsx
+++ app/pb_kits/playbook/pb_nav/_item.jsx
@@ -4,24 +4,34 @@ import PropTypes from "prop-types";
 const propTypes = {
   text: PropTypes.string.isRequired,
   link: PropTypes.string,
-  active: PropTypes.bool
+  active: PropTypes.bool,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ])
 };
 
 const defaultProps = {
-  text: "Hello Item",
   link: "#",
-  active: false
+  active: false,
+  text: ''
 };
 
 class NavItem extends Component {
   render() {
-    const { text, link, active } = this.props;
+    const { 
+      text, 
+      link, 
+      active, 
+      children
+    } = this.props;
+    const active_class = active === true ? "_active" : ""
     return (
-      <li className={"pb_nav_list_border_item" + active}>
-        <a className="pb_nav_list_item_link"
-            href={link}
-        >
-          <span className="pb_nav_list_item_text">{text}</span>
+      <li className={`pb_nav_list_border_item${active_class}`}>
+        <a className="pb_nav_list_item_link" href={link}>
+          <span className="pb_nav_list_item_text">
+            { text || children }
+          </span>
         </a>
       </li>
     );
