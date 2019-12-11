60bf1b0a533ed24d5cd8550bd04b837fc07993a9
Rafael Garcia <rafbgarcia@gmail.com>
2019-12-11 00:04:20 -0300

    Attempt to get correct branch name :)

--- spec/changelog_spec.rb
+++ spec/changelog_spec.rb
@@ -2,8 +2,10 @@
 
 RSpec.describe "CHANGELOG.md" do
   it "fails if CHANGELOG.md is not update in the branch" do
-    current_branch = `git rev-parse --abbrev-ref HEAD`.strip
+    current_branch = ENV["BRANCH_NAME"] || `git rev-parse --abbrev-ref HEAD`.strip
     changed_files = `git diff --name-only master #{current_branch}`.split
+    puts ">>> #{current_branch}"
+    puts ">>> #{changed_files}"
 
     expect(changed_files).to include "CHANGELOG.md"
   end
