c5282480fca25e46a9ce8513be443d7673d4766b
christina tai <christina.tai@powerhrg.com>
2019-12-11 12:10:35 -0500

    Nav (React) Kit Created
    Includes:
    - vertical
    - horizontal
    - block
    - example without title

--- app/pb_kits/playbook/pb_nav/docs/index.js
+++ app/pb_kits/playbook/pb_nav/docs/index.js
@@ -1,1 +1,4 @@
 export {default as DefaultNav} from './_default_nav.jsx';
+export {default as HorizontalNav} from './_horizontal_nav.jsx';
+export {default as BlockNav} from './_block_nav.jsx';
+export {default as BlockNoTitleNav} from './_block_no_title_nav.jsx';
