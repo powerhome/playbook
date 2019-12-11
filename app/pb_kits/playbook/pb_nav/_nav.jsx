db85fb1525e6c885ca942141df4b519081b7c20d
christina tai <christina.tai@powerhrg.com>
2019-12-11 14:02:29 -0500

    Changed single quotes to double quotes
    Also changed prop-types to flow types

--- app/pb_kits/playbook/pb_nav/_nav.jsx
+++ app/pb_kits/playbook/pb_nav/_nav.jsx
@@ -1,38 +1,30 @@
-import React, { Component } from "react"
-import PropTypes from "prop-types"
-import NavItem from "./_item"
-import {Caption} from "../"
+import React, { Component } from 'react'
+import NavItem from './_item'
+import {Caption} from '../'
 
-const propTypes = {
-  link: PropTypes.string,
-  title: PropTypes.string,
-  orientation: PropTypes.oneOf(["vertical", "horizontal"]),
-  children: PropTypes.oneOfType([
-        PropTypes.arrayOf(PropTypes.node),
-        PropTypes.node
-    ])
+type NavProps = {
+  link: String,
+  title: String,
+  orientation?: 'vertical' | 'horizontal',
+  children?: Array<React.ReactNode> | React.ReactNode,
 }
 
-class Nav extends Component {
-  static NavItem = NavItem
-  render() {
-    const {
-      title='',
-      orientation='vertical',
-    } = this.props;
-    return (
-      <div className={`pb_nav_list_${orientation}`}>
-        <div className={`pb_nav_list_title`}>
-          <a className="pb_nav_list_item_link_text">
-            <Caption size="md" text={`${title}`} />
-          </a>
-        </div>
-        <ul>{this.props.children}</ul>
+const Nav = (props: Nav) => {
+  const {
+    title = '',
+    orientation = 'vertical',
+  } = this.props;
+
+  return (
+    <div className={`pb_nav_list_${orientation}`}>
+      <div className={`pb_nav_list_title`}>
+        <a className='pb_nav_list_item_link_text'>
+          <Caption size='md' text={`${title}`} />
+        </a>
       </div>
-    )
-  }
+      <ul>{this.props.children}</ul>
+    </div>
+  )
 }
-
-Nav.propTypes = propTypes
 
 export default Nav
--- app/pb_kits/playbook/pb_nav/docs/_block_nav.jsx
+++ app/pb_kits/playbook/pb_nav/docs/_block_nav.jsx
@@ -1,13 +1,13 @@
-import React from "react"
-import {Nav} from "../../"
-import NavItem from "../_item.jsx"
+import React from 'react'
+import {Nav} from '../../'
+import NavItem from '../_item.jsx'
 
 function BlockNav() {
   return (
-    <Nav title="Title example" link="#" orientation="vertical">
-        <NavItem link="#" active={true}>{`Active Nav Item using text prop`}</NavItem>
-        <NavItem link="#">{`Nav Item using text prop`}</NavItem>
-        <NavItem link="#">{`Nav Item using text prop`}</NavItem>
+    <Nav title='Title example' link='#' orientation='vertical'>
+        <NavItem link='#' active={true}>{`Active Nav Item using text prop`}</NavItem>
+        <NavItem link='#'>{`Nav Item using text prop`}</NavItem>
+        <NavItem link='#'>{`Nav Item using text prop`}</NavItem>
     </Nav>
   )
 }
