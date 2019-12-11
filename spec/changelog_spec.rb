2931b78a5214643a68619bbd95ca3c445dbb10e5
Rafael Garcia <rafbgarcia@gmail.com>
2019-12-11 01:21:26 -0300

    I think this works

--- spec/changelog_spec.rb
+++ spec/changelog_spec.rb
@@ -2,11 +2,7 @@
 
 RSpec.describe "CHANGELOG.md" do
   it "fails if CHANGELOG.md is not update in the branch" do
-    current_branch = ENV["BRANCH_NAME"] || `git rev-parse --abbrev-ref HEAD`.strip
-    changed_files = `git diff --name-only origin/master #{current_branch}`.split
-    puts ">>> #{current_branch}"
-    puts ">>> #{changed_files}"
-
+    changed_files = `git diff --name-only origin/master HEAD`.split
     expect(changed_files).to include "CHANGELOG.md"
   end
 end
