aeaa0e7db76de1020b250f9b7ec1806deb74a9bb
christina tai <christina.tai@powerhrg.com>
2019-12-11 12:10:35 -0500

    Nav (React) Kit Created
    Includes:
    - vertical
    - horizontal
    - block
    - example without title

---
+++ app/pb_kits/playbook/pb_nav/docs/_block_no_title_nav.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import {Nav} from "../../"
+import NavItem from "../_item.jsx"
+
+function BlockNoTitleNav() {
+  return (
+    <Nav link="#" orientation="vertical">
+        <NavItem link="#" active={true}>{`Active Nav Item using text prop`}</NavItem>
+        <NavItem link="#">{`Nav Item using text prop`}</NavItem>
+        <NavItem link="#">{`Nav Item using text prop`}</NavItem>
+    </Nav>
+  )
+}
+
+export default BlockNoTitleNav;
