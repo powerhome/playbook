8336f007de59e0e22da51c78628dbb985e4f03e0
Megan Trimble <mtrimble6@me.com>
2019-12-19 15:22:43 -0500

    Double quotes changed to single quotes

--- app/pb_kits/playbook/pb_fixed_confirmation_toast/docs/_fixed_confirmation_toast_multi_line.jsx
+++ app/pb_kits/playbook/pb_fixed_confirmation_toast/docs/_fixed_confirmation_toast_multi_line.jsx
@@ -1,10 +1,10 @@
-import React from "react"
-import {FixedConfirmationToast} from "../../"
+import React from 'react'
+import {FixedConfirmationToast} from '../../'
 
 function FixedConfirmationToastMultiLine() {
   return (
     <div>
-        <FixedConfirmationToast text={`Scan to Assign Selected Items.\n Click X to close at any time`} status="tip" />
+        <FixedConfirmationToast text={`Scan to Assign Selected Items.\n Click X to close at any time`} status='tip' />
     </div>
   )
 }
