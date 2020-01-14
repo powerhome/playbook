bd09f422d77b3372ca5d09beb67176ba00268f28
Stephen Marshall <smarshall1980@gmail.com>
2020-01-14 15:19:11 -0600

    Add error state to Radio kit (#558)

---
+++ app/pb_kits/playbook/pb_radio/docs/_radio_dark_error.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Radio } from '../..'
+
+const RadioDarkError = () => {
+  return (
+    <div>
+      <Radio
+          dark
+          error
+          label="Power"
+          name="Group2"
+          value="Power"
+      />
+    </div>
+  )
+}
+export default RadioDarkError
