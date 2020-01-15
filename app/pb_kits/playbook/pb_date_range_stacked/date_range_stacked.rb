18df40dbcde5d98acd2ed51789dd9752610e330b
Anil Cherukuri <anil.cherukuri03@gmail.com>
2020-01-14 20:25:28 -0500

    Uses date year stacked kit

--- app/pb_kits/playbook/pb_date_range_stacked/date_range_stacked.rb
+++ app/pb_kits/playbook/pb_date_range_stacked/date_range_stacked.rb
@@ -11,22 +11,8 @@ module Playbook
       prop :end_date, type: Playbook::Props::Date, required: true
       prop :start_date, type: Playbook::Props::Date, required: true
 
-      def year(date)
-        as_date(date).to_year
-      end
-
-      def day_month(date)
-        "#{as_date(date).to_day.strip} #{as_date(date).to_month}"
-      end
-
       def classname
         generate_classname("pb_date_range_stacked")
-      end
-      
-    private
-
-      def as_date(date)
-        Playbook::PbKit::PbDateTime.new(date)
       end
     end
   end
