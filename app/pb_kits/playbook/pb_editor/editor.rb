9479de600d247f159be5551c79469deebb4e75ec
Jasper Furniss <jasper.furniss@powerhrg.com>
2020-01-23 09:00:41 -0500

    Latest for Full Page Stuff

---
+++ app/pb_kits/playbook/pb_editor/editor.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Playbook
+  module PbEditor
+    class Editor
+      include Playbook::Props
+
+      partial "pb_editor/editor"
+    end
+  end
+end
---
+++ spec/pb_kits/playbook/kits/editor_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative "../../../../app/pb_kits/playbook/pb_editor/editor"
+
+RSpec.describe Playbook::PbEditor::Editor do
+  subject { Playbook::PbEditor::Editor }
+
+  it { is_expected.to define_partial }
+
+  # Do not leave this file blank. Use other spec files for example tests.
+end
