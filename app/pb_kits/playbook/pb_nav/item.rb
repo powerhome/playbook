223c953a40bb56f181fd7c0f3cbcf911bffb8b55
Jason Cypret <jason@jasoncypret.com>
2020-01-31 14:15:50 -0500

    New navigation categories with tons of additions to the nav kit

--- app/pb_kits/playbook/pb_nav/item.rb
+++ app/pb_kits/playbook/pb_nav/item.rb
@@ -8,11 +8,29 @@ module Playbook
       partial "pb_nav/item"
 
       prop :active, type: Playbook::Props::Boolean, default: false
-      prop :link, default: "#"
+      prop :link
       prop :text
+      prop :icon_left
+      prop :icon_right
 
       def classname
-        generate_classname("pb_nav_list_border_item", active_class)
+        generate_classname("pb_nav_list_kit_item", active_class)
+      end
+
+      def tag
+        link ? "a" : "div"
+      end
+
+      def options
+        {
+          class: "pb_nav_list_item_link",
+        }.compact
+      end
+
+      def link_options
+        options.merge(
+          href: link
+        )
       end
 
     private
