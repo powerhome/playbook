6c4464829713720914ac28b69cb499031a8aa1bc
Jasper Furniss <jasper.furniss@powerhrg.com>
2020-02-04 16:58:27 -0500

    Renamed to Samples

---
+++ app/controllers/playbook/samples_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require_dependency "playbook/application_controller"
+require "yaml"
+
+module Playbook
+  class SamplesController < ApplicationController
+    layout "playbook/samples"
+    def dashboards
+      render template: "playbook/samples/dashboards/index"
+
+      
+    end
+  end
+end
