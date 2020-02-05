730b54e5185476edd06903024c0cc6a9d43a8f2e
Stephen Marshall <stephen.marshall@powerhrg.com>
2020-02-05 11:04:33 -0600

    Add exception logging to std_out

--- app/controllers/playbook/application_controller.rb
+++ app/controllers/playbook/application_controller.rb
@@ -2,11 +2,18 @@
 
 module Playbook
   class ApplicationController < ActionController::Base
+    rescue_from Exception, with: :log_exception
     protect_from_forgery with: :exception
 
     helper Webpacker::Helper
     helper Playbook::PbKitHelper
     helper Playbook::PbDocHelper
     append_view_path Playbook::Engine.root + "app/pb_kits"
+
+  private
+
+    def log_exception(exception)
+      logger.log(exception.stacktrace)
+    end
   end
 end
