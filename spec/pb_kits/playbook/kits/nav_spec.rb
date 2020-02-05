415934d5efa325a64009c171e0a2e9a7ad93a6b7
Jason Cypret <jason@jasoncypret.com>
2020-02-05 08:54:11 -0600

    Update nav_spec.rb

--- spec/pb_kits/playbook/kits/nav_spec.rb
+++ spec/pb_kits/playbook/kits/nav_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe Playbook::PbNav::Nav do
       expect(subject.new({}).classname).to start_with "pb_nav_list_normal_vertical_highlight"
       expect(subject.new(orientation: "horizontal").classname).to eq "pb_nav_list_normal_horizontal_highlight"
       expect(subject.new(orientation: "vertical").classname).to eq "pb_nav_list_normal_vertical_highlight"
-      expect(subject.new(variant: "subtle").classname).to eq "pb_nav_list_subtle_horizontal_highlight"
+      expect(subject.new(variant: "subtle").classname).to eq "pb_nav_list_subtle_vertical_highlight"
     end
   end
 
