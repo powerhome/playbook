a7d5192f78c593819c3ac963636b7bf76f0bf5d2
Jasper Furniss <jasper.furniss@powerhrg.com>
2020-02-07 11:48:47 -0500

    Latest for Full Page Samples

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,9 @@ Playbook::Engine.routes.draw do
 
 
   # Full Page Samples Get Generated Here
-  get 'dashboards', to: "samples#dashboards"
+  get 'samples',       to: "samples#samples"
+  get 'samples/:name', to: "samples#sample_show_rails", as: 'sample_show'
+  get 'samples/:name/rails', to: "samples#sample_show_rails", as: 'sample_show_rails'
+  get 'samples/:name/react', to: "samples#sample_show_react", as: 'sample_show_reacts'
 
 end
