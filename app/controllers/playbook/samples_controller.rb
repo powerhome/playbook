60b74b4dba705a1b4430c6636cd2a7abc051fa2e
Jasper Furniss <jasper.furniss@powerhrg.com>
2020-02-10 17:58:52 -0500

    Working State for Rails & React

--- app/controllers/playbook/samples_controller.rb
+++ app/controllers/playbook/samples_controller.rb
@@ -15,21 +15,25 @@ module Playbook
     end
 
     def sample_show_rails
+      params[:type] ||= "rails"
+      @type = params[:type]
       render template: "playbook/samples/sample_show"
     end
 
     def sample_show_react
+      params[:type] ||= "react"
+      @type = params[:type]
       render template: "playbook/samples/sample_show"
     end
 
   private
 
     def set_sample
-      menu = MENU["samples"]
-      if menu.include?(params[:name])
+      menu = MENU["samples"].map { |link| link.is_a?(Hash) ? link.first.last : link }
+      if menu.flatten.include?(params[:name])
         @sample = params[:name]
       else
-        redirect_to root_path
+        redirect_to root_path, flash: { error: "That kit does not exist" }
       end
     end
   end
