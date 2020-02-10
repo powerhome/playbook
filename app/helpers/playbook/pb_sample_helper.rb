8206057d2eadb63f792a5ebf8434aa00f5fad016
Jasper Furniss <jasper.furniss@powerhrg.com>
2020-02-10 17:58:52 -0500

    Working State for Rails & React

--- app/helpers/playbook/pb_sample_helper.rb
+++ app/helpers/playbook/pb_sample_helper.rb
@@ -11,20 +11,9 @@ module Playbook
       end
     end
 
-    def pb_sample(sample: "", type: "rails", show_code: true)
+    def pb_sample(sample: "", type: "rails")
       @type = type
-      # @sample_examples = get_sample_examples(sample, type)
-      # @show_code = show_code
-      # render partial: "config/sample_example"
-    end
-
-    def pb_samples(type: "rails")
-      display_samples = []
-      samples = MENU['samples']
-      samples.sort.each do |sample|
-        display_samples << render_pb_doc_sample(sample, type, false)
-      end
-      raw("<div class='pb--docItem'>" + display_samples.join("</div><div class='pb--docItem'>") + "</div>")
+      @sample = sample
     end
   end
 end
---
+++ app/pb_kits/playbook/packs/samples.js
@@ -0,0 +1,17 @@
+// ===========================================
+// Generated file from kit generator.
+//
+// KIT EXAMPLES
+// ===========================================
+
+import WebpackerReact from 'webpacker-react'
+
+//
+// ===========================================
+// ===========================================
+// Generated file from kit generator.
+// KIT EXAMPLES
+
+import Dashboards from '../../../views/playbook/samples/dashboards/dashboards.jsx'
+
+WebpackerReact.setup({ Dashboards })
