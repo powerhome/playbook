49d2245a443509c28d36f45629f8f3cebfa86e29
Jasper Furniss <jasper.furniss@powerhrg.com>
2020-02-13 12:32:03 -0500

    Ability to Go Fullscreen, Toggle, and Copy Code

--- app/pb_kits/playbook/packs/samples.js
+++ app/pb_kits/playbook/packs/samples.js
@@ -1,7 +1,6 @@
 // ===========================================
 // Generated file from kit generator.
 //
-// KIT EXAMPLES
 // ===========================================
 
 import WebpackerReact from 'webpacker-react'
@@ -10,23 +9,41 @@ import WebpackerReact from 'webpacker-react'
 // ===========================================
 // ===========================================
 // Generated file from kit generator.
-// KIT EXAMPLES
 
 import Dashboards from '../../../views/playbook/samples/dashboards/dashboards.jsx'
 
 WebpackerReact.setup({ Dashboards })
 
-// window.$(document).on('click', '[data-toggle]', function(e) {
-//   e.preventDefault()
-
-//   var kitContainer = window.$(this).closest('.pb--doc')
-//   var toggleTarget = window.$(this).data('toggle')
-
-//   window.$(kitContainer).find('[data-action="toggle"]').hide()
-//   window.$(kitContainer).find('[data-togglable="' + toggleTarget + '"]').show()
-// })
+$('.pb--codeCopy').show()
+$('.compress').hide()
 
 window.$(document).on('click', '.toggle-button', (e) => {
   e.preventDefault()
-  $('.pb--codeCopy').show()
+  $('.pb--codeCopy').toggleClass('close')
 })
+
+$('.expand').click(() => {
+  $('.sample-nav').hide()
+  $('.compress').show()
+})
+
+$('.compress').click(() => {
+  $('.sample-nav').show()
+  $('.compress').hide()
+})
+
+const setClipboard = (value) => {
+  var tempInput = document.createElement('textarea')
+  tempInput.style = 'position: absolute; left: -1000px; top: -1000px'
+  tempInput.value = value
+  document.body.appendChild(tempInput)
+  tempInput.select()
+  document.execCommand('copy')
+  document.body.removeChild(tempInput)
+}
+
+$('.copy-clipboard').click(() => {
+  var copyText = document.querySelector('.hiddenCodeforCopy').textContent
+  setClipboard(copyText)
+})
+
