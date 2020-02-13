2adaa4aad5519459a16f2764c4c9b17f31f38c6d
Jasper Furniss <jasper.furniss@powerhrg.com>
2020-02-13 08:39:27 -0500

    Full Page Nav Bar

--- app/pb_kits/playbook/packs/samples.js
+++ app/pb_kits/playbook/packs/samples.js
@@ -15,3 +15,18 @@ import WebpackerReact from 'webpacker-react'
 import Dashboards from '../../../views/playbook/samples/dashboards/dashboards.jsx'
 
 WebpackerReact.setup({ Dashboards })
+
+// window.$(document).on('click', '[data-toggle]', function(e) {
+//   e.preventDefault()
+
+//   var kitContainer = window.$(this).closest('.pb--doc')
+//   var toggleTarget = window.$(this).data('toggle')
+
+//   window.$(kitContainer).find('[data-action="toggle"]').hide()
+//   window.$(kitContainer).find('[data-togglable="' + toggleTarget + '"]').show()
+// })
+
+window.$(document).on('click', '.toggle-button', (e) => {
+  e.preventDefault()
+  $('.pb--codeCopy').show()
+})
