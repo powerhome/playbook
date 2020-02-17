3eaca437c2c78a725022db988f0ff462e755b4e7
Jonathan Zazula <djonyriz@gmail.com>
2020-02-17 17:01:42 -0500

    WIP

--- app/pb_kits/playbook/pb_filter/filter.rb
+++ app/pb_kits/playbook/pb_filter/filter.rb
@@ -6,6 +6,19 @@ module Playbook
       include Playbook::Props
 
       partial "pb_filter/filter"
+
+      def sort_list
+        sort_item = Playbook::PbList::Item.new( 
+          text: "it works"
+        )
+        sort_list = Playbook::PbList::List.new(
+          ) { sort_item}
+        ApplicationController.renderer.render(partial: sort_list, as: :object)
+      end
+
+      def classname
+        generate_classname("pb_filter_kit")
+      end
     end
   end
 end
