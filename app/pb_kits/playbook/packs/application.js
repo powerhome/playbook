ce3b4b01b6460a4d1214d5a15fd6f1de4e18c6a5
Jasper Furniss <jasper.furniss@powerhrg.com>
2020-02-20 16:51:11 -0500

    Clean Up, and Moving Files

--- app/pb_kits/playbook/packs/application.js
+++ app/pb_kits/playbook/packs/application.js
@@ -13,3 +13,38 @@ window.$(document).on('click', '[data-toggle]', function(e) {
   window.$(kitContainer).find('[data-action="toggle"]').hide()
   window.$(kitContainer).find('[data-togglable="' + toggleTarget + '"]').show()
 })
+
+$(document).ready(() => {
+  $('.pb--codeCopy').show()
+  $('.compress').hide()
+
+  window.$(document).on('click', '.toggle-button', (e) => {
+    e.preventDefault()
+    $('.pb--codeCopy').toggleClass('close')
+  })
+
+  $('.expand').click(() => {
+    $('.sample-nav').hide()
+    $('.compress').show()
+  })
+
+  $('.compress').click(() => {
+    $('.sample-nav').show()
+    $('.compress').hide()
+  })
+
+  const setClipboard = (value) => {
+    var tempInput = document.createElement('textarea')
+    tempInput.style = 'position: absolute; left: -1000px; top: -1000px'
+    tempInput.value = value
+    document.body.appendChild(tempInput)
+    tempInput.select()
+    document.execCommand('copy')
+    document.body.removeChild(tempInput)
+  }
+
+  $('.copy-clipboard').click(() => {
+    var copyText = document.querySelector('.hiddenCodeforCopy').textContent
+    setClipboard(copyText)
+  })
+})
