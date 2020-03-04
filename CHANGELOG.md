27fbd53fdcc3b535fe7aedee9471049c074f7571
Jasper Furniss <jasper.furniss@powerhrg.com>
2020-03-04 10:02:26 -0500

    Full Page Final Fixes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,8 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [4.5.2] 2020-3-03
+### Fixed 
+- Fix Full Page Samples code display([#650](https://github.com/powerhome/playbook/pull/650) @jasperfurniss)
 
 ### Changed
 - Fix Layout Kit SCSS ([#648](https://github.com/powerhome/playbook/pull/648) @thestephenmarshall)
--- app/pb_kits/playbook/packs/application.js
+++ app/pb_kits/playbook/packs/application.js
@@ -10,6 +10,47 @@ window.$(document).on('click', '[data-toggle]', function(e) {
   var kitContainer = window.$(this).closest('.pb--doc')
   var toggleTarget = window.$(this).data('toggle')
 
-  window.$(kitContainer).find('[data-action="toggle"]').hide()
-  window.$(kitContainer).find('[data-togglable="' + toggleTarget + '"]').show()
+  window
+    .$(kitContainer)
+    .find('[data-action="toggle"]')
+    .hide()
+  window
+    .$(kitContainer)
+    .find('[data-togglable="' + toggleTarget + '"]')
+    .show()
+})
+
+window.$(document).ready(() => {
+  $('.pb--codeCopy').show()
+  $('.compress').hide()
+
+  window.$(document).on('click', '.toggle-button', (e) => {
+    e.preventDefault()
+    $('.pb--codeCopy').toggleClass('close')
+  })
+
+  $('.expand').click(() => {
+    $('.sample-nav').hide()
+    $('.compress').show()
+  })
+
+  $('.compress').click(() => {
+    $('.sample-nav').show()
+    $('.compress').hide()
+  })
+
+  const setClipboard = (value) => {
+    var tempInput = document.createElement('textarea')
+    tempInput.style = 'position: absolute; left: -1000px; top: -1000px'
+    tempInput.value = value
+    document.body.appendChild(tempInput)
+    tempInput.select()
+    document.execCommand('copy')
+    document.body.removeChild(tempInput)
+  }
+
+  $('.copy-clipboard').click(() => {
+    var copyText = document.querySelector('.hiddenCodeforCopy').textContent
+    setClipboard(copyText)
+  })
 })
