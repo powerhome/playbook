ba039e1a51b5547f169b02b8fadb9544e357700e
Jasper Furniss <jasper.furniss@powerhrg.com>
2020-03-19 18:39:32 -0400

    Fixing Last Change

--- spec/pb_kits/playbook/kits/form_pill_spec.rb
+++ spec/pb_kits/playbook/kits/form_pill_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Playbook::PbFormPill::FormPill do
   it { is_expected.to define_prop(:text) }
   it { is_expected.to define_prop(:name) }
   it { is_expected.to define_boolean_prop(:avatar)	
-    .with_default(false) }
+                      .with_default(false) }
   it { is_expected.to define_prop(:avatar_url) }
 
 
