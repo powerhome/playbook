17f454a9b0729f2ca1454cfae9d1068060ec87c8
Jasper Furniss <jasper.furniss@powerhrg.com>
2020-03-20 17:54:49 -0400

    Fixed Lint Errors

--- app/pb_kits/playbook/pb_textarea/docs/_textarea_resize.jsx
+++ app/pb_kits/playbook/pb_textarea/docs/_textarea_resize.jsx
@@ -5,25 +5,25 @@ const TextareaResize = () => {
   return (
     <div>
       <Textarea
-        label="vertical"
-        placeholder="Resize Vertical"
-        resize="vertical"
+          label="vertical"
+          placeholder="Resize Vertical"
+          resize="vertical"
       />
 
       <br />
 
       <Textarea
-        label="both"
-        placeholder="Resize Both"
-        resize="both"
+          label="both"
+          placeholder="Resize Both"
+          resize="both"
       />
 
       <br />
 
       <Textarea
-        label="horizontal"
-        placeholder="Resize Horizontal"
-        resize="horizontal"
+          label="horizontal"
+          placeholder="Resize Horizontal"
+          resize="horizontal"
       />
 
     </div>
