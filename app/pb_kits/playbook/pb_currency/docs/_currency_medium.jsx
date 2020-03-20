2350ff437a8f7fbd3347b7710ef5d924ecec0530
Ramon Ruiz <30808035+rruiz85@users.noreply.github.com>
2020-03-20 09:25:54 -0400

    Currency updates (#673)
    * Update form_pill_spec.rb
    
    * fixed currency and added dark
    
    Co-authored-by: Jason Cypret <jason@jasoncypret.com>

--- app/pb_kits/playbook/pb_currency/docs/_currency_medium.jsx
+++ app/pb_kits/playbook/pb_currency/docs/_currency_medium.jsx
@@ -1,27 +1,30 @@
 import React from 'react'
 import { Currency } from '../../'
 
-const CurrencyDefault = () => {
+const CurrencyMedium = () => {
   return (
     <div>
       <Currency
           amount="2,000.50"
           label="Caption"
+          size="md"
       />
       <Currency
           align="center"
           amount="342"
           label="Caption"
+          size="md"
           symbol="€"
       />
       <Currency
           align="right"
           amount="45"
           label="Caption"
+          size="md"
           unit="/mo"
       />
     </div>
   )
 }
 
-export default CurrencyDefault
+export default CurrencyMedium
