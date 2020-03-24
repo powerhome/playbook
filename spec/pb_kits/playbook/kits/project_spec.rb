b45896f5cf8a2198a5f67d418e10b336c74cb8f9
Katherine Muedas <muedasnka@gmail.com>
2020-03-24 12:34:24 -0400

    adds specs

--- spec/pb_kits/playbook/kits/project_spec.rb
+++ spec/pb_kits/playbook/kits/project_spec.rb
@@ -6,6 +6,16 @@ RSpec.describe Playbook::PbProject::Project do
   subject { Playbook::PbProject::Project }
 
   it { is_expected.to define_partial }
+  it { is_expected.to define_boolean_prop(:dark).with_default(false) }
+  it { is_expected.to define_prop(:project_name) }
+  it { is_expected.to define_prop(:project_number) }
+  it { is_expected.to define_prop(:date) }
+  it { is_expected.to define_prop(:link) }
 
-  # Do not leave this file blank. Use other spec files for example tests.
+  describe "#classname" do
+    it "returns namespaced class name", :aggregate_failures do
+      expect(subject.new({}).classname).to eq "pb_project_kit"
+      expect(subject.new(classname: "additional_class").classname).to eq "pb_project_kit additional_class"
+    end
+  end
 end
