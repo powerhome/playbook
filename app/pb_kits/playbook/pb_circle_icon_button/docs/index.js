507984ce06dc590c088e336452db7594c3852b31
Stephen Marshall <smarshall1980@gmail.com>
2020-04-07 11:43:10 -0500

    Add onClick prop to kits in need (#694)

--- app/pb_kits/playbook/pb_circle_icon_button/docs/index.js
+++ app/pb_kits/playbook/pb_circle_icon_button/docs/index.js
@@ -1,2 +1,3 @@
 export { default as CircleIconButtonDefault } from './_circle_icon_button_default.jsx'
+export { default as CircleIconButtonClick } from './_circle_icon_button_click.jsx'
 export { default as CircleIconButtonDark } from './_circle_icon_button_dark.jsx'
