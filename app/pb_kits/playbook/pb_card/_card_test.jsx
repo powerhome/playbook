3b3080640f8166d7bf629631db94ad73629c377b
christina tai <christina.tai@powerhrg.com>
2020-04-15 10:10:59 -0400

    WIP - General functionality of card header

--- app/pb_kits/playbook/pb_card/_card_test.jsx
+++ app/pb_kits/playbook/pb_card/_card_test.jsx
@@ -1,105 +1,73 @@
 /* @flow */
 
 import React from 'react'
+import { Card } from '../'
 import classnames from 'classnames'
 import { buildCss } from '../utilities/props'
 
-//  Card props
-/*
-type CardPropTypes = {
+type CardProps = {
   children: Array<React.ReactNode> | React.ReactNode,
   className?: String,
-  header?: {
-    padding?: 'none' | 'xs' | 'sm' | 'md' | 'lg' | 'xl',
-    color?: String
-  }
+  headerPadding?: 'none' | 'xs' | 'sm' | 'md' | 'lg' | 'xl',
   bodyPadding?: 'none' | 'xs' | 'sm' | 'md' | 'lg' | 'xl',
 }
 
-// Establishing css classes
-const CardTest = ({
-  children,
-  className,
-  header = {},
-  bodyPadding = 'md',
-}: CardPropTypes) => {
-	const mainCSS = buildCss('pb_card_kit', className)
-  const headerCSS = buildCss('pb_card_header_kit', {
-		[`header_${header.padding}`]: header.padding,
-		[`header_${header.color}`]: header.color,
-	})
-  const bodyCSS = buildCss('pb_card_body_kit', bodyPadding)
-}
-*/
-
-// FindByType util
-const findByType = (children, component) => {
-const result = [];
-/* This is the array of result since Article can have multiple times the same sub-component */
-const type = [component.displayName] || [component.name];
-/* We can store the actual name of the component through the displayName or name property of our sub-component */
-React.Children.forEach(children, child => {
-    const childType =
-    child && child.type && (child.type.displayName || child.type.name);
-    if (type.includes(childType)) {
-    result.push(child);
-    }
-});
-
-/* Then we go through each React children, if one of matches the name of the sub-component we’re looking for we put it in the result array */
-return result[0];
-};
-
-
-// We instantiate the sub-components
-const Header = () => null;
-const Body = () => null;
-
-
-//  Creating a class called CardTest
-class CardTest extends React.Component {
-
-	// This is the function that will take care of rendering our Header sub-component
-	renderHeader () {
-		const { children } = this.props;
-		// First we try to find the Header sub-component among the children of Article
-		const header = findByType(children, Header);
-		// If there is a header, then return Header sub-component
-		if (header) {
-			return (
-				<div className={'pb_card_header_kit_md_royal'}>
-					{children}
-				</div>
-			)
-		}
-		// Else we return null
-		return null;
-	}
-		
-	renderBody () {
-		const { children } = this.props;
-		const body = findByType(children, Body);
-		if (body) {
-			return (
-				<div className={'pb_card_body_kit_md'}>
-					{children}
-				</div>
-			)
-		}
-		return null;
-	}
-
-	render () {
-		const { children, className } = this.props;
-		return (
-			<CardTest>
-			</CardTest>
-		);
-	}
+const Header = () => null
+const Body = () => null
+class CardTest extends React.Component<Props> {
+  static defaultProps = {
+    children: null,
+    className: null,
+    headerPadding: 'sm',
+    headerColor: 'royal',
+    bodyPadding: 'sm',
+  }
+  props: CardProps
+  renderHeader() {
+    const { children } = this.props
+    const header = React.Children.map(children, child => {
+      if (child.type.displayName === 'Header' || child.type.name === 'Header') {
+        return React.cloneElement(child)
+      }
+    })
+    return (
+      header.length > 0 ? header[0].props.children : ''
+    )
+  }
+  renderBody() {
+    const { children } = this.props;
+    const body = React.Children.map(children, child => {
+      if (child.type.displayName === 'Body' || child.type.name === 'Body') {
+        return React.cloneElement(child)
+      }
+    })
+    return (
+      <div className="pb_card_body_kit">{body.length > 0 ? body[0].props.children : '' }</div>
+    )
+  }
+  render() {
+    const {
+      className,
+      headerPadding,
+      headerColor,
+      bodyPadding,
+    } = this.props
+    const headerCSS = buildCss('pb_card_header_kit', headerPadding, headerColor)
+    const bodyCSS = buildCss('pb_card_body_kit', bodyPadding)
+    return (
+      <Card padding="none">
+        <div className={classnames(headerCSS, className)}>
+          {this.renderHeader()}
+        </div>
+        <div className={classnames(bodyCSS, className)}>
+          {this.renderBody()}
+        </div>
+      </Card>
+    )
+  }
 }
 
-// Lastly we expose the Title sub-component through Article
-CardTest.Header = Header;
-CardTest.Body = Body;
+CardTest.Header = Header
+CardTest.Body = Body
 
 export default CardTest
--- app/pb_kits/playbook/pb_card/docs/_card_header.jsx
+++ app/pb_kits/playbook/pb_card/docs/_card_header.jsx
@@ -1,13 +1,47 @@
 import React from 'react'
 import CardTest from '../_card_test.jsx'
-
+import { Body } from '../../'
 
 const CardHeader = () => {
   return (
+    <div>
+      Default
       <CardTest>
-          <CardTest.Header>Header</CardTest.Header>
-          <CardTest.Body>Body</CardTest.Body>
+        <CardTest.Header>
+          <Body dark 
+                text="Header"  />
+        </CardTest.Header>
+        <CardTest.Body>
+          <Body text="Body" />
+        </CardTest.Body>
       </CardTest>
+      <br />
+      Header and body padding large
+      <CardTest bodyPadding="lg" 
+                headerPadding="lg">
+        <CardTest.Header>
+          <Body dark 
+                text="Header"  />
+        </CardTest.Header>
+        <CardTest.Body>
+          <Body text="Body" />
+        </CardTest.Body>
+      </CardTest>
+      <br />
+      Only body
+      <CardTest>
+        <CardTest.Body>
+          Body
+        </CardTest.Body>
+      </CardTest>
+      <br />
+      Only header
+      <CardTest>
+        <CardTest.Header>
+          Header
+        </CardTest.Header>
+      </CardTest>
+    </div>
   )
 }
 
