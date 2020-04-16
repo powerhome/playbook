357a73ed0fefa3dc535d8c891a83286b99af4e2a
Christina Tai <42459486+christinaatai@users.noreply.github.com>
2020-04-16 15:28:10 -0400

    Vertical section separator variant & dark version (#729)
    * React WIP
    
    * Added react and dark versions
    
    * Updated spec and changelog

--- spec/pb_kits/playbook/kits/section_seperator_spec.rb
+++ spec/pb_kits/playbook/kits/section_seperator_spec.rb
@@ -11,10 +11,20 @@ RSpec.describe Playbook::PbSectionSeparator::SectionSeparator do
   it { is_expected.to define_enum_prop(:variant)
                       .with_default("card")
                       .with_values("card", "background") }
+  it { is_expected.to define_enum_prop(:orientation)
+                      .with_default("horizontal")
+                      .with_values("horizontal", "vertical") }
+  it { is_expected.to define_boolean_prop(:dark)
+                      .with_default(false) }
+
    describe "#classname" do
     it "returns namespaced class name", :aggregate_failures do
-      expect(subject.new({}).classname).to eq "pb_section_separator_kit_card"
-      expect(subject.new(classname: "additional_class").classname).to eq "pb_section_separator_kit_card additional_class"
+      expect(subject.new({}).classname).to eq "pb_section_separator_kit_card_horizontal"
+      expect(subject.new(classname: "additional_class").classname).to eq "pb_section_separator_kit_card_horizontal additional_class"
+      expect(subject.new(variant: "background", classname: "additional_class", dark: true).classname).to eq "pb_section_separator_kit_background_horizontal_dark additional_class"
+      expect(subject.new(orientation: "vertical", classname: "additional_class", dark: true).classname).to eq "pb_section_separator_kit_card_vertical_dark additional_class"
+      expect(subject.new(classname: "additional_class", dark: true).classname).to eq "pb_section_separator_kit_card_horizontal_dark additional_class"
+
     end
   end
 end
