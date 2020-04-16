cf70582d626501f57d4d04e46909ac353052db34
christina tai <christina.tai@powerhrg.com>
2020-04-16 13:17:33 -0400

    Added react and dark versions

--- app/pb_kits/playbook/pb_section_separator/docs/_section_separator_vertical.jsx
+++ app/pb_kits/playbook/pb_section_separator/docs/_section_separator_vertical.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
-import { SectionSeparator } from '../../'
+import { Flex, FlexItem, SectionSeparator } from '../../'
 
 const SectionSeparatorVertical = () => {
   return (
-    <div>
-        <SectionSeparator />
-    </div>
+    <Flex
+        inline="flex-container"
+        vertical="stretch"
+    >
+      <FlexItem>
+        {'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'}
+      </FlexItem>
+      <SectionSeparator orientation="vertical" />
+      <FlexItem>
+        {'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'}
+      </FlexItem>
+      <SectionSeparator orientation="vertical" />
+      <FlexItem>
+        {'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'}
+      </FlexItem>
+    </Flex>
   )
 }
 
--- app/pb_kits/playbook/pb_section_separator/section_separator.rb
+++ app/pb_kits/playbook/pb_section_separator/section_separator.rb
@@ -14,9 +14,17 @@ module Playbook
       prop :orientation, type: Playbook::Props::Enum,
                          values: %w[horizontal vertical],
                          default: "horizontal"
+      prop :dark, type: Playbook::Props::Boolean,
+                  default: false
 
       def classname
-        generate_classname("pb_section_separator_kit", variant, orientation)
+        generate_classname("pb_section_separator_kit", variant, orientation, dark_class)
+      end
+
+    private
+
+      def dark_class
+        dark ? "dark" : nil
       end
     end
   end
