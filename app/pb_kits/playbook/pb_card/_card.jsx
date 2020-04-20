13587c61fdad3e4946785f6109267ecf46fc1bd5
Stephen Marshall <stephen.marshall@powerhrg.com>
2020-04-20 16:29:40 -0500

    Update card react kit to use sub-components

--- app/pb_kits/playbook/pb_card/_card.jsx
+++ app/pb_kits/playbook/pb_card/_card.jsx
@@ -18,6 +18,50 @@ type CardPropTypes = {
   dark?: Boolean,
 }
 
+type CardHeaderProps = {
+  children: Array<React.ReactNode> | React.ReactNode,
+  className?: String,
+  padding?: 'none' | 'xs' | 'sm' | 'md' | 'lg' | 'xl',
+  color?: 'category_1' | 'category_2' | 'category_3' | 'category_4' | 'category_5' | 'category_6' | 'category_7' | 'category_8' |
+          'category_9' | 'category_10' | 'category_11' | 'category_12' | 'category_13' | 'category_14' | 'category_15' | 'category_16' |
+          'category_17' | 'category_18' | 'category_19' | 'category_20' | 'category_21',
+}
+
+type CardBodyProps = {
+  children: Array<React.ReactNode> | React.ReactNode | String,
+  className?: String,
+  padding?: 'none' | 'xs' | 'sm' | 'md' | 'lg' | 'xl',
+}
+
+// Header component
+const Header = ({
+  children,
+  className,
+  padding = 'sm',
+  color = 'category_14',
+}: CardHeaderProps) => {
+  const headerCSS = buildCss('pb_card_header_kit', padding, color)
+  return (
+    <div className={classnames(headerCSS, className)}>
+      {children}
+    </div>
+  )
+}
+
+// Body component
+const Body = ({
+  children,
+  className,
+  padding = 'sm',
+}: CardBodyProps) => {
+  const bodyCSS = buildCss('pb_card_body_kit', padding)
+  return (
+    <div className={classnames(bodyCSS, className)}>
+      {children}
+    </div>
+  )
+}
+
 const Card = ({
   children,
   className,
@@ -36,13 +80,30 @@ const Card = ({
     [`highlight_${highlight.color}`]: highlight.color,
   })
 
+  // coerce to array
+  const leChildren = typeof(children) === 'object' && children.length ? children : [children]
+
+  const subComponentTags = (tagName) => {
+    return leChildren.filter((c) => {
+      return c.type && c.type.displayName === tagName
+    }).map((child, i) => {
+      return React.cloneElement(child, { key: `${tagName.toLowerCase()}-${i}` })
+    })
+  }
+
+  const nonHeaderChildren = leChildren.filter((child) => !child.type || child.type.displayName !== 'Header')
+
   return (
     <div className={classnames(cardCss, className)}>
+      {subComponentTags('Header')}
       <div className={bodyCSS}>
-        {children}
+        {nonHeaderChildren}
       </div>
     </div>
   )
 }
+
+Card.Header = Header
+Card.Body = Body
 
 export default Card
--- app/pb_kits/playbook/pb_card/docs/_card_header.jsx
+++ app/pb_kits/playbook/pb_card/docs/_card_header.jsx
@@ -1,40 +1,40 @@
 import React from 'react'
-import CardTest from '../_card_test.jsx'
-import { Body } from '../../'
+import { Body, Card } from '../../'
 
 const CardHeader = () => {
   return (
     <div>
-      <CardTest padding="none">
-        {/* <CardTest.Header>
-          <Body dark 
-                text="Header"  />
-        </CardTest.Header> */}
-        <CardTest.Body>
+      <Card padding="none">
+        <Card.Body>
           <Body text="Body" />
-        </CardTest.Body>
-      </CardTest>
+        </Card.Body>
+      </Card>
 
       <br />
 
-      <CardTest padding="none">
-        <CardTest.Header 
-          padding="md"
-          color="category_11">
-          <Body dark 
-                text="Header" />
-        </CardTest.Header>
-        <CardTest.Body
-          padding="lg">
+      <Card padding="none">
+        <Card.Header
+            color="category_11"
+            padding="md"
+        >
+          <Body
+              dark
+              text="Header"
+          />
+        </Card.Header>
+        <Card.Body
+            padding="lg"
+        >
           <Body text="Body" />
-        </CardTest.Body>
-      </CardTest>
-      
+        </Card.Body>
+      </Card>
       <br />
 
-      <CardTest highlight={{ position: 'side', color: 'windows' }}>
-        Text
-      </CardTest>
+      <Card
+          highlight={{ position: 'side', color: 'windows' }}
+      >
+        {'Text'}
+      </Card>
 
     </div>
   )
