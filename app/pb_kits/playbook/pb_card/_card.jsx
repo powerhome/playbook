03fd6076665ca7957a180936e61d3e7b995597dd
Christina Tai <42459486+christinaatai@users.noreply.github.com>
2020-04-24 08:22:07 -0400

    Adding card header prop to card kit & category colors (#746)
    * Update CHANGELOG.md
    
    * WIP - Adding card header in react via sub-component
    
    * WIP - General functionality of card header
    
    * Add category colors to utility and to card headers
    
    * Update card react kit to use sub-components
    
    * Cleaned card kit scss files
    
    * Reordered category colors
    
    * Update default category colors and in example doc
    
    * Removed old shadows from card doc examples and changed card header radius
    
    * Allow highlight to pass category colors and made category color prop pass in a number
    
    Co-authored-by: Jason Cypret <jason@jasoncypret.com>
    Co-authored-by: Stephen Marshall <stephen.marshall@powerhrg.com>
    Co-authored-by: Jon Zazula <djonyriz@gmail.com>

--- app/pb_kits/playbook/pb_card/_card.jsx
+++ app/pb_kits/playbook/pb_card/_card.jsx
@@ -2,7 +2,6 @@
 
 import React from 'react'
 import classnames from 'classnames'
-
 import { buildCss } from '../utilities/props'
 
 type CardPropTypes = {
@@ -14,8 +13,50 @@ type CardPropTypes = {
   },
   padding?: 'none' | 'xs' | 'sm' | 'md' | 'lg' | 'xl',
   selected?: Boolean,
-  shadow?: 'none' | 'shallow' | 'default' | 'deep' | 'deeper' | 'deepest',
+  shadow?: 'none' | 'deep' | 'deeper' | 'deepest',
   dark?: Boolean,
+}
+
+type CardHeaderProps = {
+  children: Array<React.ReactNode> | React.ReactNode,
+  className?: String,
+  padding?: 'none' | 'xs' | 'sm' | 'md' | 'lg' | 'xl',
+  categoryColor?: Number,
+}
+
+type CardBodyProps = {
+  children: Array<React.ReactNode> | React.ReactNode | String,
+  className?: String,
+  padding?: 'none' | 'xs' | 'sm' | 'md' | 'lg' | 'xl',
+}
+
+// Header component
+const Header = ({
+  children,
+  className,
+  padding = 'sm',
+  categoryColor = 1,
+}: CardHeaderProps) => {
+  const headerCSS = buildCss('pb_card_header_kit', padding, `category_${categoryColor}`)
+  return (
+    <div className={classnames(headerCSS, className)}>
+      {children}
+    </div>
+  )
+}
+
+// Body component
+const Body = ({
+  children,
+  className,
+  padding = 'sm',
+}: CardBodyProps) => {
+  const bodyCSS = buildCss('pb_card_body_kit', padding)
+  return (
+    <div className={classnames(bodyCSS, className)}>
+      {children}
+    </div>
+  )
 }
 
 const Card = ({
@@ -36,13 +77,30 @@ const Card = ({
     [`highlight_${highlight.color}`]: highlight.color,
   })
 
+  // coerce to array
+  const cardChildren = typeof(children) === 'object' && children.length ? children : [children]
+
+  const subComponentTags = (tagName) => {
+    return cardChildren.filter((c) => {
+      return c.type && c.type.displayName === tagName
+    }).map((child, i) => {
+      return React.cloneElement(child, { key: `${tagName.toLowerCase()}-${i}` })
+    })
+  }
+
+  const nonHeaderChildren = cardChildren.filter((child) => !child.type || child.type.displayName !== 'Header')
+
   return (
     <div className={classnames(cardCss, className)}>
+      {subComponentTags('Header')}
       <div className={bodyCSS}>
-        {children}
+        {nonHeaderChildren}
       </div>
     </div>
   )
 }
+
+Card.Header = Header
+Card.Body = Body
 
 export default Card
