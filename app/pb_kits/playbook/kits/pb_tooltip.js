c4800557be4e69190509a4868525f7b6384374d3
Jason Cypret <jason@jasoncypret.com>
2020-05-12 17:04:23 -0500

    fixed open div and added fade in animation

--- app/pb_kits/playbook/kits/pb_tooltip.js
+++
@@ -1,4 +0,0 @@
-import Tooltip from '../pb_tooltip/_tooltip.jsx'
-
-import WebpackerReact from 'webpacker-react'
-WebpackerReact.setup({ Tooltip })
--- spec/pb_kits/playbook/kits/tooltip_spec.rb
+++ spec/pb_kits/playbook/kits/tooltip_spec.rb
@@ -7,5 +7,18 @@ RSpec.describe Playbook::PbTooltip::Tooltip do
 
   it { is_expected.to define_partial }
 
-  # Do not leave this file blank. Use other spec files for example tests.
+  it { is_expected.to define_prop(:position) }
+  it { is_expected.to define_prop(:trigger_element_id) }
+  it { is_expected.to define_prop(:tooltip_id) }
+  it { is_expected.to define_boolean_prop(:dark).with_default(false) }
+
+
+
+  describe "#classname" do
+    it "returns namespaced class name", :aggregate_failures do
+      expect(subject.new(dark: true).classname).to eq "pb_tooltip_kit_dark"
+      expect(subject.new(classname: "additional_class").classname).to eq "pb_tooltip_kit additional_class"
+  end
+  end
+
 end
