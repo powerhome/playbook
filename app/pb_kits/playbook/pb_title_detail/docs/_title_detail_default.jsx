d8a80446007f15dfb0f7c0cb6a5100ee9f07dd2d
kellyeryan <kellyeryan@gmail.com>
2020-05-15 16:42:06 -0400

    Create TitleDetail React component

---
+++ app/pb_kits/playbook/pb_title_detail/docs/_title_detail_default.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { TitleDetail } from '../../'
+
+const TitleDetailDefault = () => (
+  <div>
+    <TitleDetail
+        detail="Commits data and history"
+        title="Email Notifications"
+    />
+
+    <TitleDetail
+        align="center"
+        detail="Commits data and history"
+        title="Email Notifications"
+    />
+
+    <TitleDetail
+        align="right"
+        detail="Commits data and history"
+        title="Email Notifications"
+    />
+
+  </div>
+)
+
+export default TitleDetailDefault
