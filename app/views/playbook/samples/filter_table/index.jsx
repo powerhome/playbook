8842c7f9d18981cbaae90c8e8251cbe42646eb80
Eva Louderback <evalouderback@gmail.com>
2020-05-18 17:02:20 -0400

    wip reference error

---
+++ app/views/playbook/samples/filter_table/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { BarGraph, LineGraph, Nav, User } from '../../../../pb_kits/playbook'
+import NavItem from '../../../../pb_kits/playbook/pb_nav/_item.jsx'
+
+const FilterTable = () => (
+  <div>
+    hello world
+  </div>
+)
+
+export default FilterTable
