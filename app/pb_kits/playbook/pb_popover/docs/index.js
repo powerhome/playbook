157ae4bf808415451fde6db6cbb02eeec8c18a3b
Stephen Marshall <smarshall1980@gmail.com>
2020-05-22 11:21:44 -0500

    Tooltip V2 (#750)

--- app/pb_kits/playbook/pb_popover/docs/index.js
+++ app/pb_kits/playbook/pb_popover/docs/index.js
@@ -1,5 +1,4 @@
 export { default as PopoverDefault } from './_popover_default.jsx'
-export { default as PopoverWithButton } from './_popover_with_button.jsx'
 export { default as PopoverList } from './_popover_list.jsx'
 export { default as PopoverPortal } from './_popover_portal.jsx'
 export { default as PopoverClickOutside } from './_popover_click_outside.jsx'
---
+++ app/pb_kits/playbook/pb_tooltip/tooltip.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module Playbook
+  module PbTooltip
+    class Tooltip
+      include Playbook::Props
+      partial "pb_tooltip/tooltip"
+
+      prop :position
+      prop :trigger_element_id
+      prop :tooltip_id
+      prop :dark, type: Playbook::Props::Boolean,
+                          default: false
+
+      def classname
+        generate_classname("pb_tooltip_kit", dark_class)
+      end
+
+      def data
+        Hash(values[:data]).merge(
+          pb_tooltip_kit: true,
+          pb_tooltip_position: position,
+          pb_tooltip_trigger_element_id: trigger_element_id,
+          pb_tooltip_tooltip_id: tooltip_id,
+        )
+      end
+
+      private
+      def dark_class
+        dark ? "dark" : nil
+      end
+    end
+  end
+end
