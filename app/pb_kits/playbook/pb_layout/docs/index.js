66dd3321d9dec872878708e85cc34e442d9aa304
Christina Tai <42459486+christinaatai@users.noreply.github.com>
2020-05-22 10:47:44 -0400

    Updated react version of layout kit to use sub-components (#801)

--- app/pb_kits/playbook/pb_layout/docs/index.js
+++ app/pb_kits/playbook/pb_layout/docs/index.js
@@ -1,1 +1,4 @@
-export { default as LayoutDefault } from './_layout_default.jsx'
+export { default as LayoutColors } from './_layout_colors.jsx'
+export { default as LayoutTransparent } from './_layout_transparent.jsx'
+export { default as LayoutSizes } from './_layout_sizes.jsx'
+export { default as LayoutSizesDark } from './_layout_sizes_dark.jsx'
