39e9ffb264bf41248f04b4f5e695b538845d4f86
kellyeryan <kellyeryan@gmail.com>
2020-05-22 13:33:35 -0400

    Fix minor bugs and add documentations for sourceNoicon and sourceTypes

---
+++ app/pb_kits/playbook/pb_source/docs/_source_noicon.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Source } from '../../'
+
+const SourceNoicon = () => {
+  <>
+    <Source
+        source="BJ's Johnston-208"
+        type="retail"
+        hideIcon
+    />
+
+    <br />
+    <br />
+
+    <Source
+        source="Referral-phrg"
+        type="inbound"
+        hideIcon
+    />
+
+    <br />
+    <br />
+
+    <Source
+        source="BJ's Future CB"
+        type="user"
+        hideIcon
+        user={
+              { name: 'Anna Black',
+                image: {
+                  url: 'https://randomuser.me/api/portraits/women/44.jpg',
+                  },
+                userId: '48582',
+                }
+              }
+    />
+  </>
+}
+
+export default SourceNoicon
