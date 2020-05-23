c1584b4c572acb1e3040f931be4e907a1ca736dd
kellyeryan <kellyeryan@gmail.com>
2020-05-23 10:10:55 -0400

    Fix minor bugs and add documentations for sourceNoicon and sourceTypes

--- app/pb_kits/playbook/pb_source/_source.jsx
+++ app/pb_kits/playbook/pb_source/_source.jsx
@@ -1,21 +1,128 @@
+/* @flow */
+
 import React from 'react'
-import PropTypes from 'prop-types'
+import classnames from 'classnames'
 
-const propTypes = {
-  className: PropTypes.string,
-  id: PropTypes.string,
+import {
+  buildDataProps,
+} from '../utilities/props'
+import {
+  titleize
+} from '../utilities/text'
+import {
+  Avatar,
+  Body,
+  Caption,
+  IconCircle,
+  Title,
+} from '../'
+
+type SourceProps = {
+  className?: String,
+  data?: object,
+  hideIcon: Boolean,
+  id?: String,
+  source?: String,
+  type: "events" | "inbound" | "outbound" | "prospecting" | "referral" | "retail" | "user",
+  user: object,
 }
 
-class Source extends React.Component {
-  render() {
-    return (
-      <div className="pb_source">
-        <span>{'SOURCE CONTENT'}</span>
-      </div>
-    )
+const Source = ({
+  className,
+  data = {},
+  hideIcon = false,
+  id,
+  source,
+  type = 'inbound',
+  user = {},
+}: SourceProps) => {
+  const dataProps = buildDataProps(data)
+
+  const css = classnames([
+    'pb_source_kit',
+    className,
+  ])
+
+  const avatar = () => {
+    if ((type === 'user' || type === 'referral') && user.name !== undefined) {
+      const avatarProps = Object.assign({}, user)
+      avatarProps['size'] = 'sm'
+      delete avatarProps.user_id
+      return avatarProps
+    }
   }
-}
 
-Source.propTypes = propTypes
+  const typeText = () => {
+    if (type === 'user' || (type === 'referral' && user.name !== undefined)) {
+      return user.name
+    } else {
+      return titleize(type)
+    }
+  }
+
+  const typeIconName = () => {
+    const option = type
+    switch (option) {
+    case 'events':
+      return 'calendar-alt'
+      break
+      // case "inbound"
+      //   return "sign-in"
+      //   break;
+    case 'outbound':
+      return 'sign-out'
+      break
+    case 'prospecting':
+      return 'binoculars'
+      break
+    case 'referral':
+      return 'handshake'
+      break
+    case 'retail':
+      return 'shopping-bag'
+      break
+    default:
+      return 'sign-in'
+    }
+  }
+
+  const showIcon = () => {
+    (typeIconName !== undefined && avatar === undefined) ? true : false
+  }
+
+  return (
+    <div
+        {...dataProps}
+        className={css}
+        id={id}
+    >
+
+      <div className="pb__source_layout">
+
+        <div className="pb__source_content">
+          <Title
+              size={4}
+              tag="h4"
+              text={source}
+          />
+
+          <div className="pb__source_value">
+            <Body
+                color="light"
+                text={typeText()}
+            />
+
+            <If condition={user.userId}>
+              <Caption
+                  text={user.userId}
+              />
+            </If>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  )
+}
 
 export default Source
