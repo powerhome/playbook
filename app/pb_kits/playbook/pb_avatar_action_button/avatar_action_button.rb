03f25544c6271a89f582e0d6004b5edbc500c899
Brendan Huffman <bh247484@gmail.com>
2020-05-26 17:12:00 -0400

    created rails kit foundation, started styling

--- app/pb_kits/playbook/pb_avatar_action_button/avatar_action_button.rb
+++ app/pb_kits/playbook/pb_avatar_action_button/avatar_action_button.rb
@@ -5,7 +5,20 @@ module Playbook
     class AvatarActionButton
       include Playbook::Props
 
+      prop :image_url, type: Playbook::Props::String
+      prop :name, type: Playbook::Props::String,
+                  default: ""
+      prop :size, type: Playbook::Props::Enum,
+                  values: %w[xs sm md base lg xl],
+                  default: "md"
+      prop :icon, type: Playbook::Props::String,
+                  default: "user"
+
       partial "pb_avatar_action_button/avatar_action_button"
+
+      def classname
+        generate_classname("pb_avatar_action_button_kit")
+      end
     end
   end
 end
