f027b07b020f3d902445bed41aa976489df6e311
Brendan Huffman <bh247484@gmail.com>
2020-05-27 15:13:07 -0400

    refined positioning, added placement, tooltip, onclick, and link props

--- app/pb_kits/playbook/pb_avatar_action_button/avatar_action_button.rb
+++ app/pb_kits/playbook/pb_avatar_action_button/avatar_action_button.rb
@@ -6,6 +6,10 @@ module Playbook
       include Playbook::Props
 
       prop :image_url, type: Playbook::Props::String
+      prop :link_url, type: Playbook::Props::String
+      prop :tooltip_text, type: Playbook::Props::String
+      prop :tooltip_id, type: Playbook::Props::String
+      prop :onclick, type: Playbook::Props::String
       prop :name, type: Playbook::Props::String,
                   default: ""
       prop :size, type: Playbook::Props::Enum,
@@ -13,6 +17,8 @@ module Playbook
                   default: "md"
       prop :icon, type: Playbook::Props::String,
                   default: "user"
+      prop :placement, type: Playbook::Props::String,
+                       default: "bottom-left"
 
       partial "pb_avatar_action_button/avatar_action_button"
 
