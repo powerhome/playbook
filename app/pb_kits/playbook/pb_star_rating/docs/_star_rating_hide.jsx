59329863a69fae099c6096d2c6922767ec767fba
Kelly Ryan <51907753+kellyeryan@users.noreply.github.com>
2020-05-28 08:49:17 -0400

    Star rating react and fix hide_rating feature (#822)
    * StarRating in React - WIP
    
    * Create StarRating React kit
    
    * Add React star rating kit and fix hide_rating feature
    
    * Write CHANGELOG
    
    Co-authored-by: Jason Cypret <jason@jasoncypret.com>

---
+++ app/pb_kits/playbook/pb_star_rating/docs/_star_rating_hide.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { StarRating } from '../../'
+
+const StarRatingHide = () => (
+
+  <StarRating
+      hideRating
+      rating={3.5}
+  />
+
+)
+
+export default StarRatingHide
---
+++ app/pb_kits/playbook/pb_star_rating/docs/index.js
@@ -0,0 +1,3 @@
+export { default as StarRatingDefault } from './_star_rating_default.jsx'
+
+export { default as StarRatingHide } from './_star_rating_hide.jsx'
