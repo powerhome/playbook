b9a5b90d4129e090b581601723a07d3ade2be580
kellyeryan <kellyeryan@gmail.com>
2020-06-01 12:06:24 -0400

    Complete TitleCount React kit

--- app/pb_kits/playbook/pb_title_count/_title_count.jsx
+++ app/pb_kits/playbook/pb_title_count/_title_count.jsx
@@ -1,39 +1,61 @@
 /* @flow */
+
 import React from 'react'
+import classnames from 'classnames'
+
+import { buildAriaProps, buildCss, buildDataProps } from '../utilities/props'
+
 import { Body, Title } from '../'
 
 type TitleCountProps = {
+  align: "center" | "left" | "right",
+  aria?: object,
   className?: String,
-  data?: String,
+  count?: Numeric,
+  data?: object,
   id?: String,
   title?: String,
-  count?: Number,
-  size?: String
-}
+  size?: "lg" | "sm",
+};
 
 const TitleCount = ({
-  className = 'pb_title_count_kit',
-  data,
+  align = 'left',
+  aria = {},
+  className,
+  data = {},
   count,
   id,
   title,
-  size,
-}: TitleCountProps) => (
-  <div
-      className={className}
-      data={data}
-      id={id}
-  >
-    <Title
-        className="pb_title_count_text"
-        size={size == 'lg' ? 3 : 4}
-        text={title}
-    />
-    <Body
-        color="light"
-        text={`${count}`}
-    />
-  </div>
-)
+  size = 'sm',
+}: TitleCountProps) => {
+  const ariaProps = buildAriaProps(aria)
+  const dataProps = buildDataProps(data)
+
+  const css = classnames(
+    className,
+    buildCss('pb_title_count_kit', align, size)
+  )
+
+  const formatCount = count.toLocaleString()
+
+  return (
+    <div
+        {...ariaProps}
+        {...dataProps}
+        className={css}
+        id={id}
+    >
+      <Title
+          className={title !== undefined ? 'pb_title_count_text' : ''}
+          size={size === 'lg' ? 3 : 4}
+          text={title}
+      />
+
+      <Body color="light">
+        <If condition={count}>{formatCount}</If>
+      </Body>
+    </div>
+  )
+}
 
 export default TitleCount
