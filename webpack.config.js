d3d069c17fbfaa8647a1de7ef11e780eb2d87c47
Rafael Garcia <rafbgarcia@gmail.com>
2020-06-03 00:37:24 -0400

    Release task

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const path = require('path')
+
+module.exports = (env) => {
+  return {
+    mode: env.development ? 'development' : 'production',
+    entry: ['./app/pb_kits/playbook/index.js', './app/pb_kits/playbook/_playbook.scss'],
+    output: {
+      filename: env.development ? 'playbook.js' : 'playbook.min.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          styles: {
+            name: 'styles',
+            test: /\.css$/,
+            chunks: 'all',
+            enforce: true,
+          },
+        },
+      },
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: env.development ? 'playbook.css' : 'playbook.min.css',
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.scss$/i,
+          use: [
+            MiniCssExtractPlugin.loader,
+            { loader: 'css-loader' },
+            { loader: 'sass-loader' },
+          ],
+        },
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.(js|jsx|mjs)$/,
+          use: 'babel-loader',
+          exclude: /node_modules(?!\/playbook-ui)/,
+        },
+        {
+          test: /\.svg$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'svg-url-loader',
+              options: {
+                limit: 10000,
+              },
+            },
+          ],
+        },
+      ],
+    },
+  }
+}
