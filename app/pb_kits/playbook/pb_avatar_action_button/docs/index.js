16acf9ff8c53bc0fc76da4846b9ccb5a8c1466fe
Brendan Huffman <bh247484@gmail.com>
2020-06-03 17:17:06 -0400

    moved js onclick from index.js to onclick erb

--- app/pb_kits/playbook/pb_avatar_action_button/docs/index.js
+++ app/pb_kits/playbook/pb_avatar_action_button/docs/index.js
@@ -2,7 +2,3 @@ export { default as AvatarActionButtonDefault } from './_avatar_action_button_de
 export { default as AvatarActionButtonPlacement } from './_avatar_action_button_placement.jsx'
 export { default as AvatarActionButtonActions } from './_avatar_action_button_actions.jsx'
 export { default as AvatarActionButtonOnClick } from './_avatar_action_button_on_click.jsx'
-
-if (document.querySelector('#clickable')) {
-  document.querySelector('#clickable').addEventListener('click', () => alert('clickable clicked!'))
-}
