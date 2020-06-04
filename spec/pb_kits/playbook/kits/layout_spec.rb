14ca3ee47697371a5aa4ad8839fabf54caa5baf7
Jonathan Zazula <djonyriz@gmail.com>
2020-06-04 19:41:22 -0400

    added spec and updated changelog

--- spec/pb_kits/playbook/kits/layout_spec.rb
+++ spec/pb_kits/playbook/kits/layout_spec.rb
@@ -21,14 +21,18 @@ RSpec.describe Playbook::PbLayout::Layout do
   it { is_expected.to define_enum_prop(:variant)
                       .with_values("light", "dark", "gradient")
                       .with_default("light") }
+  it { is_expected.to define_enum_prop(:layout)
+                      .with_values("sidebar", "collection")
+                      .with_default("sidebar") }
 
   describe "#classname" do
     it "returns namespaced class name", :aggregate_failures do
       text = "Test text"
-      expect(subject.new({}).classname).to eq "pb_layout_kit_size_md_left_light layout_left_collapse_xs"
-      expect(subject.new(full: true).classname).to eq "pb_layout_kit_size_md_left_light full layout_left_collapse_xs"
-      expect(subject.new(variant: "dark", transparent: true, full: true).classname).to eq "pb_layout_kit_size_md_left_dark_transparent full layout_left_collapse_xs"
-      expect(subject.new(variant: "dark", position: "right", size: "sm").classname).to eq "pb_layout_kit_size_sm_right_dark layout_right_collapse_xs"
+      expect(subject.new({}).classname).to eq "pb_layout_kit_sidebar_size_md_left_light layout_left_collapse_xs"
+      expect(subject.new(full: true).classname).to eq "pb_layout_kit_sidebar_size_md_left_light full layout_left_collapse_xs"
+      expect(subject.new(layout: "collection").classname).to eq "pb_layout_kit_collection"
+      expect(subject.new(variant: "dark", transparent: true, full: true).classname).to eq "pb_layout_kit_sidebar_size_md_left_dark_transparent full layout_left_collapse_xs"
+      expect(subject.new(variant: "dark", position: "right", size: "sm").classname).to eq "pb_layout_kit_sidebar_size_sm_right_dark layout_right_collapse_xs"
     end
   end
 end
