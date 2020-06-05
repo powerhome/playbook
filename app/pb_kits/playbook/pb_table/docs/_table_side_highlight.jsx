75db24a754fb9db791ea714c87215241cf85117b
Christina Tai <42459486+christinaatai@users.noreply.github.com>
2020-06-05 08:40:33 -0400

    Added category colors to table side highlight kit (#829)
    * Added category colors to table side highlight kit
    
    * Updated changelog
    
    * Fixed typos
    
    Co-authored-by: Jon Zazula <djonyriz@gmail.com>

--- app/pb_kits/playbook/pb_table/docs/_table_side_highlight.jsx
+++ app/pb_kits/playbook/pb_table/docs/_table_side_highlight.jsx
@@ -4,7 +4,7 @@ import { Table, TableRow } from '../../'
 const TableSideHighlight = () => {
   return (
     <div>
-      <Table size="md">
+      <Table size="sm">
         <thead>
           <tr>
             <th>{'Product colors'}</th>
@@ -15,24 +15,22 @@ const TableSideHighlight = () => {
           </tr>
         </thead>
         <tbody>
-          <TableRow
-              sideHighlightColor="windows"
-          >
-            <td>{'Windows'}</td>
+          <TableRow sideHighlightColor="solar">
+            <td>{'Solar'}</td>
             <td>{'Value 2'}</td>
             <td>{'Value 3'}</td>
             <td>{'Value 4'}</td>
             <td>{'Value 5'}</td>
           </TableRow>
-          <TableRow sideHighlightColor="siding">
-            <td>{'Siding'}</td>
+          <TableRow sideHighlightColor="roofing">
+            <td>{'Roofing'}</td>
             <td>{'Value 2'}</td>
             <td>{'Value 3'}</td>
             <td>{'Value 4'}</td>
             <td>{'Value 5'}</td>
           </TableRow>
-          <TableRow sideHighlightColor="doors">
-            <td>{'Doors'}</td>
+          <TableRow sideHighlightColor="gutters">
+            <td>{'Gutters'}</td>
             <td>{'Value 2'}</td>
             <td>{'Value 3'}</td>
             <td>{'Value 4'}</td>
@@ -50,7 +48,7 @@ const TableSideHighlight = () => {
 
       <br />
 
-      <Table size="md">
+      <Table size="sm">
         <thead>
           <tr>
             <th>{'Status colors'}</th>
@@ -77,6 +75,50 @@ const TableSideHighlight = () => {
           </TableRow>
           <TableRow sideHighlightColor="error">
             <td>{'Error'}</td>
+            <td>{'Value 2'}</td>
+            <td>{'Value 3'}</td>
+            <td>{'Value 4'}</td>
+            <td>{'Value 5'}</td>
+          </TableRow>
+          <TableRow sideHighlightColor="none">
+            <td>{'None'}</td>
+            <td>{'Value 2'}</td>
+            <td>{'Value 3'}</td>
+            <td>{'Value 4'}</td>
+            <td>{'Value 5'}</td>
+          </TableRow>
+        </tbody>
+      </Table>
+
+      <br />
+
+      <Table size="sm">
+        <thead>
+          <tr>
+            <th>{'Category Colors'}</th>
+            <th>{'Column 2'}</th>
+            <th>{'Column 3'}</th>
+            <th>{'Column 4'}</th>
+            <th>{'Column 5'}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <TableRow sideHighlightColor="category_1">
+            <td>{'Category Color 1'}</td>
+            <td>{'Value 2'}</td>
+            <td>{'Value 3'}</td>
+            <td>{'Value 4'}</td>
+            <td>{'Value 5'}</td>
+          </TableRow>
+          <TableRow sideHighlightColor="category_2">
+            <td>{'Category Color 2'}</td>
+            <td>{'Value 2'}</td>
+            <td>{'Value 3'}</td>
+            <td>{'Value 4'}</td>
+            <td>{'Value 5'}</td>
+          </TableRow>
+          <TableRow sideHighlightColor="category_3">
+            <td>{'Category Color 3'}</td>
             <td>{'Value 2'}</td>
             <td>{'Value 3'}</td>
             <td>{'Value 4'}</td>
