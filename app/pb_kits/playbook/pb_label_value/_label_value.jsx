bfdb9572bbfa295d70663f7006aeff19800b8b16
kellyeryan <kellyeryan@gmail.com>
2020-06-05 11:19:33 -0400

    Add aria, data, id props to React LabelValue, aria to Rails

--- app/pb_kits/playbook/pb_label_value/_label_value.jsx
+++ app/pb_kits/playbook/pb_label_value/_label_value.jsx
@@ -4,27 +4,42 @@ import React from 'react'
 import classnames from 'classnames'
 import { Body, Caption } from '../'
 
+import {
+  buildAriaProps,
+  buildCss,
+  buildDataProps,
+} from '../utilities/props'
+
 type LabelValueProps = {
+  aria?: object,
   className?: String,
+  dark?: Boolean,
+  data?: object,
+  id?: String,
   label: String,
   value: String,
-  dark?: Boolean
 }
 
 const LabelValue = ({
+  aria = {},
   className,
+  dark = false,
+  data = {},
+  id,
   label,
   value,
-  dark = false,
 }: LabelValueProps) => {
-  const themeStyle = dark === true ? '_dark' : ''
-  const css = classnames([
-    'pb_label_value_kit' + themeStyle,
-    className,
-  ])
+  const ariaProps = buildAriaProps(aria)
+  const dataProps = buildDataProps(data)
+  const classes = classnames(className, buildCss('pb_label_value_kit', { 'dark': dark }))
 
   return (
-    <div className={css}>
+    <div
+        {...ariaProps}
+        {...dataProps}
+        className={classes}
+        id={id}
+    >
       <Caption
           text={label}
       />
