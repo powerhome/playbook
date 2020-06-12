01f7596e496bd49be6b6ad0211ec333414799aa6
Stephen Marshall <smarshall1980@gmail.com>
2020-06-12 11:51:56 -0500

    Revert "Filter and table page example (#789)"
    This reverts commit ac5fa9a6be6ad9a65d926b4c84447b6248c348f8.

--- app/pb_kits/playbook/packs/samples.js
+++ app/pb_kits/playbook/packs/samples.js
@@ -11,7 +11,5 @@ import WebpackerReact from 'webpacker-react'
 // Generated file from kit generator.
 
 import Dashboards from '../../../views/playbook/samples/dashboards/index.jsx'
-import FilterTable from '../../../views/playbook/samples/filter_table/index.jsx'
 
 WebpackerReact.setup({ Dashboards })
-WebpackerReact.setup({ FilterTable })
