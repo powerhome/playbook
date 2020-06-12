ac5fa9a6be6ad9a65d926b4c84447b6248c348f8
Eva Louderback <50629488+evalouderback@users.noreply.github.com>
2020-06-12 12:50:37 -0400

    Filter and table page example (#789)
    Co-authored-by: Jason Cypret <jason@jasoncypret.com>

--- app/pb_kits/playbook/packs/samples.js
+++ app/pb_kits/playbook/packs/samples.js
@@ -11,5 +11,7 @@ import WebpackerReact from 'webpacker-react'
 // Generated file from kit generator.
 
 import Dashboards from '../../../views/playbook/samples/dashboards/index.jsx'
+import FilterTable from '../../../views/playbook/samples/filter_table/index.jsx'
 
 WebpackerReact.setup({ Dashboards })
+WebpackerReact.setup({ FilterTable })
