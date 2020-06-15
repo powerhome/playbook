862af72878378160ac1d91b83ed64decaaf0b6ea
Stephen Marshall <smarshall1980@gmail.com>
2020-06-15 11:50:34 -0500

    Add Terser plugin for prod uglification

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,11 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
+const TerserPlugin = require('terser-webpack-plugin')
+
+environment.optimization = {
+  minimize: true,
+  minimizer: [new TerserPlugin({cache: true})],
+}
 
 module.exports = environment.toWebpackConfig()
