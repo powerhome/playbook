9e6da9046f00a7acbdbf433d9f659dc9d13cd83d
Christina Tai <42459486+christinaatai@users.noreply.github.com>
2020-07-10 12:21:01 -0400

    Added prop to remove card border (#900)
    * Added prop to remove card border
    
    * Updated changelog and spec doc
    
    * Removed trailing white space
    
    * Fixed react prop name to be camelcased
    
    * Renamed rails border prop
    
    * Updated spec doc

---
+++ app/pb_kits/playbook/pb_card/docs/_card_border_none.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Card from '../_card.jsx'
+
+const CardBorderNone = () => {
+  return (
+    <div className="bg-light-doc-example">
+
+      <Card borderNone>{'Card content'}</Card>
+
+    </div>
+  )
+}
+
+export default CardBorderNone
