339b6296e52bc27419ef3508ab56e8d9ce4f65a7
Brendan Huffman <bh247484@gmail.com>
2020-07-13 15:29:50 -0400

    rescoped styles, cleaned live data doc example

--- app/pb_kits/playbook/pb_gauge/docs/_gauge_live_data.jsx
+++ app/pb_kits/playbook/pb_gauge/docs/_gauge_live_data.jsx
@@ -4,30 +4,6 @@ import { Button, Gauge } from '../../'
 const GaugeLiveData = () => {
 
   const [value, setValue] = useState(50)
-  // let yourChart
-
-  // useEffect(() => {
-
-  //   // Store array of pages' Highchart objects in variable 'charts'
-  //   // Must import `Highcharts` for this method call
-  //   const charts = Highcharts.charts
-
-  //   // Loop through array to find the chart with the id you passed as prop below
-  //   charts.forEach((chart) => {
-  //     if (chart.renderTo.id === 'gauge-live-data') {
-  //       yourChart = chart
-  //     }
-  //   })
-
-  //   // window['gauge-live-data'].series[0].setData([value])
-
-
-  //   // Set new data value for your chart sourced by React component state
-  //   // Uses Highcharts api method `.setData`
-  //   // React state value must be passed in as an Array, i.e. `[value]`
-  //   yourChart.series[0].setData([value])
-  //   console.log(Highcharts.charts)
-  // })
 
   const updateState = () => {
     // set value state to random number between 1-100
