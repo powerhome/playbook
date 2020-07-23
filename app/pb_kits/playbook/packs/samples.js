b2f7301ab4104b6554e770aa7f63a54645ed8ce4
Eva Louderback <50629488+evalouderback@users.noreply.github.com>
2020-07-23 14:13:05 -0400

    Collection Detail Full Page Example (#902)
    * Added unreleased section for 5.3.0
    
    * rails done, except for nav issue, react started
    
    * Update dependency npm to v6.14.6 [SECURITY] (#896)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * made improvements to nav to allow nesting
    
    * Add aria, data props to React Badge kit (#901)
    
    * Add aria, data props to React Badge kit
    
    * Add to CHANGELOG
    
    * Added prop to remove card border (#900)
    
    * Added prop to remove card border
    
    * Updated changelog and spec doc
    
    * Removed trailing white space
    
    * Fixed react prop name to be camelcased
    
    * Renamed rails border prop
    
    * Updated spec doc
    
    * v5.3.0
    
    * rails and react complete
    
    * updated collection detail kit, full page example ready to go
    
    * Add props to Circle Icon Button React/Rails kits (#907)
    
    * Add props to Circle Icon Button React/Rails kits
    
    * Add to CHANGELOG
    
    Co-authored-by: Jasper Furniss <jasperfurniss@gmail.com>
    
    * Add props to Body React kit (#904)
    
    * Add props to Body React kit
    
    * Add to CHANGELOG
    
    * Fix trailing spaces
    
    Co-authored-by: Jasper Furniss <jasperfurniss@gmail.com>
    
    * Add to kit generator (#916)
    
    * Test 1, 2, 3 of kit generator -- add aria props, add object as option
    
    * Finish additions and modifications to kit generator
    
    * Fix line spacing and missing curly braces, fix case statement
    
    * Add missing underscore and line break
    
    * Linting
    
    * Add to CHANGELOG
    
    * Fix Nav Item Icon to handle conditions for icon (#913)
    
    * Adds a div with key to wrap icon
    
    * Updates logchange
    
    * Make key unique from right to left
    
    * Undo key change
    
    * Add props to checkbox (#905)
    
    * Add props to React Checkbox kit and add checked example
    
    * Add aria prop to Rails Checkbox kit
    
    * Add to CHANGELOG
    
    Co-authored-by: Jasper Furniss <jasperfurniss@gmail.com>
    
    * Fixed Up Changelog
    
    * v5.4.0
    
    * rails done, except for nav issue, react started
    
    * made improvements to nav to allow nesting
    
    * rails and react complete
    
    * updated collection detail kit, full page example ready to go
    
    * attempt to fix failing spec
    
    Co-authored-by: Jonathan Zazula <djonyriz@gmail.com>
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Kelly Ryan <51907753+kellyeryan@users.noreply.github.com>
    Co-authored-by: Christina Tai <42459486+christinaatai@users.noreply.github.com>
    Co-authored-by: Stephen Marshall <smarshall1980@gmail.com>
    Co-authored-by: Jasper Furniss <jasperfurniss@gmail.com>
    Co-authored-by: Katherine Muedas <muedasnka@gmail.com>
    Co-authored-by: Jasper Furniss <jasper.furniss@powerhrg.com>

--- app/pb_kits/playbook/packs/samples.js
+++ app/pb_kits/playbook/packs/samples.js
@@ -13,7 +13,9 @@ import WebpackerReact from 'webpacker-react'
 import Dashboards from '../../../views/playbook/samples/dashboards/index.jsx'
 import FilterTable from '../../../views/playbook/samples/filter_table/index.jsx'
 import Registration from '../../../views/playbook/samples/registration/index.jsx'
+import CollectionDetail from '../../../views/playbook/samples/collection_detail/index.jsx'
 
 WebpackerReact.setup({ Dashboards })
 WebpackerReact.setup({ FilterTable })
 WebpackerReact.setup({ Registration })
+WebpackerReact.setup({ CollectionDetail })
