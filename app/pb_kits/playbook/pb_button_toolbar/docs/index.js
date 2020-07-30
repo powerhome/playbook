96eeba15945d80cecdcf53ce56a5867920f9a23f
kellyeryan <kellyeryan@gmail.com>
2020-07-30 14:51:15 -0400

    Add secondary buttons

--- app/pb_kits/playbook/pb_button_toolbar/docs/index.js
+++ app/pb_kits/playbook/pb_button_toolbar/docs/index.js
@@ -1,2 +1,4 @@
 export { default as ButtonToolbarDefault } from './_button_toolbar_default.jsx'
 export { default as ButtonToolbarVertical } from './_button_toolbar_vertical.jsx'
+export { default as ButtonToolbarSecondary } from './_button_toolbar_secondary.jsx'
+export { default as ButtonToolbarVerticalSecondary } from './_button_toolbar_vertical_secondary.jsx'
