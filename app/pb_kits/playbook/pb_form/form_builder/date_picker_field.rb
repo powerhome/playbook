91713a6953fb49f4191c688204c1cc3d1b9b8df8
Brendan Huffman <bh247484@gmail.com>
2020-08-03 10:59:36 -0400

    experimented with date-picker form helper, upversioned alpha

--- app/pb_kits/playbook/pb_form/form_builder/date_picker_field.rb
+++ app/pb_kits/playbook/pb_form/form_builder/date_picker_field.rb
@@ -4,6 +4,15 @@ module Playbook
   module PbForm
     module FormBuilder
       module DatePickerField
+        # def date_field(name, props: {})
+        #   # props[:name] = name
+
+        #   input = super(name)
+
+        #   @template.pb_rails("date_picker", props: props) do
+        #     input
+        #   end
+        # end
         def date_picker(name, props: {})
           props[:name] = name
 
