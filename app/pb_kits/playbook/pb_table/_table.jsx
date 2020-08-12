f30f79b900ce9539828edc8ffb9e29ceecafe944
kellyeryan <kellyeryan@gmail.com>
2020-08-12 13:52:06 -0400

    Update table dark mode

--- app/pb_kits/playbook/pb_table/_table.jsx
+++ app/pb_kits/playbook/pb_table/_table.jsx
@@ -2,15 +2,18 @@
 
 import React, { type Node } from 'react'
 import classnames from 'classnames'
+import { buildAriaProps, buildCss, buildDataProps } from '../utilities/props'
 import { globalProps } from '../utilities/globalProps.js'
 
 type TableProps = {
+  aria?: object,
   children: array<Node> | Node,
   className: string,
   container: boolean,
-  dark: boolean,
+  data?: object,
   dataTable: boolean,
   disableHover: boolean,
+  id?: string,
   responsive: "collapse" | "scroll" | "none",
   singleLine: boolean,
   size: "sm" | "md" | "lg",
@@ -18,19 +21,27 @@ type TableProps = {
 
 const Table = (props: TableProps) => {
   const {
+    aria = {},
     children,
     className,
     container = true,
-    dark = false,
+    data = {},
     dataTable = false,
     disableHover = false,
+    id,
     responsive = 'collapse',
     singleLine = false,
     size = 'sm',
   } = props
 
+  const ariaProps = buildAriaProps(aria)
+  const dataProps = buildDataProps(data)
+
   return (
     <table
+        {...ariaProps}
+        {...dataProps}
+        id={id}
         className={classnames(
         className,
         'pb_table',
@@ -38,7 +49,6 @@ const Table = (props: TableProps) => {
         `table-responsive-${responsive}`,
         {
           'table-card': container,
-          'table-dark': dark,
           'data_table': dataTable,
           'single-line': singleLine,
           'no-hover': disableHover,
