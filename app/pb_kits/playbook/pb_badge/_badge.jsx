243228cf7f899e2a6ab99913137993c3196434d2
kellyeryan <kellyeryan@gmail.com>
2020-08-13 10:24:09 -0400

    Linting

--- app/pb_kits/playbook/pb_badge/_badge.jsx
+++ app/pb_kits/playbook/pb_badge/_badge.jsx
@@ -32,7 +32,7 @@ const Badge = (props: BadgeProps) => {
   const ariaProps = buildAriaProps(aria)
   const dataProps = buildDataProps(data)
   const css = classnames(
-    buildCss('pb_badge_kit', variant, {rounded: rounded}), className,
+    buildCss('pb_badge_kit', variant, { rounded: rounded }), className,
     globalProps(props))
 
   return (
