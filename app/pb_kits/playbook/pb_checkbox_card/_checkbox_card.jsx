1f3eebe7551b1b6060befb346719ca45463632bb
Pat McClernan <patmccler@gmail.com>
2020-08-18 14:27:25 -0400

    use useState to keep track of selected status

--- app/pb_kits/playbook/pb_checkbox_card/_checkbox_card.jsx
+++ app/pb_kits/playbook/pb_checkbox_card/_checkbox_card.jsx
@@ -2,7 +2,7 @@
 
 /* @flow */
 
-import React from 'react'
+import React, { useState } from 'react'
 import classnames from 'classnames'
 import { buildAriaProps, buildCss, buildDataProps } from '../utilities/props'
 import { globalProps } from '../utilities/globalProps.js'
@@ -10,19 +10,30 @@ import { Card, Checkbox } from '../'
 
 type CheckboxCardProps = {
   aria?: object,
+  children?: array<React.ReactNode> | React.ReactNode,
   className?: string,
   data?: object,
   id?: string,
+  text?: string,
 }
 
 const CheckboxCard = (props: CheckboxCardProps) => {
   const {
-  aria = {},
-  children,
-  className,
-  data = {},
-  id,
+    aria = {},
+    children,
+    className,
+    data = {},
+    dark = false,
+    id,
+    checked = false,
+    text = '',
   } = props
+
+  const [isChecked, setIsChecked] = useState(checked)
+  const checkboxChanged = e => {
+    e.persist()
+    setIsChecked(e.target.checked)
+  }
 
   const ariaProps = buildAriaProps(aria)
   const dataProps = buildDataProps(data)
@@ -35,10 +46,9 @@ const CheckboxCard = (props: CheckboxCardProps) => {
         className={classes}
         id={id}
     >
-      <Card>
-        <Checkbox>
-          {children}
-        </Checkbox>
+      <Card dark selected={isChecked}>
+        <Checkbox dark checked={isChecked} onChange={checkboxChanged}/>
+        {children || text}
       </Card>
     </div>
   )
--- app/pb_kits/playbook/pb_checkbox_card/docs/_checkbox_card_default.jsx
+++ app/pb_kits/playbook/pb_checkbox_card/docs/_checkbox_card_default.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import { CheckboxCard } from '../../'
 
+
 const CheckboxCardDefault = () => (
   <div>
-    <CheckboxCard />
+    <CheckboxCard checked />
+    <CheckboxCard checked text="Text Prop"/>
+    <CheckboxCard>
+      Child Text
+    </CheckboxCard>
+    <CheckboxCard>
+      <span>Hello in a span child</span>
+    </CheckboxCard>
   </div>
+
 )
 
 export default CheckboxCardDefault
