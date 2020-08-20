4e728fddb534d3f93ad4ac3cbaee2128df4c0f59
Jasper Furniss <jasperfurniss@gmail.com>
2020-08-20 14:28:43 -0400

    Revert "Create online status dark mode (#991)" (#1009)
    This reverts commit f2263e1203d43d5de4ca7f269ed64453a846e1fd.

--- spec/pb_kits/playbook/kits/online_status_spec.rb
+++ spec/pb_kits/playbook/kits/online_status_spec.rb
@@ -14,7 +14,6 @@ RSpec.describe Playbook::PbOnlineStatus::OnlineStatus do
   describe "#classname" do
     it "returns namespaced class name", :aggregate_failures do
       expect(subject.new({}).classname).to eq "pb_online_status_kit_offline"
-      expect(subject.new(dark: true).classname).to eq "pb_online_status_kit_offline dark"
       expect(subject.new(classname: "additional_class").classname).to eq "pb_online_status_kit_offline additional_class"
     end
   end
