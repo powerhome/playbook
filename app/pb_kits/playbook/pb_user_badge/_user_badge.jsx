a244c39a4761cc33e03b86fec815ac77ff928cf3
Kelly Ryan <51907753+kellyeryan@users.noreply.github.com>
2020-08-20 15:20:40 -0400

    Add User Badge dark mode (#984)
    * Add User Badge dark mode
    
    * Add rspec test

--- app/pb_kits/playbook/pb_user_badge/_user_badge.jsx
+++ app/pb_kits/playbook/pb_user_badge/_user_badge.jsx
@@ -1,21 +1,41 @@
 /* @flow */
 import React from 'react'
 import classnames from 'classnames'
+import { buildAriaProps, buildCss, buildDataProps } from '../utilities/props'
 import { globalProps } from '../utilities/globalProps.js'
 
 type UserBadgeProps = {
-  className?: string,
-  id?: string,
+  aria?: object,
   badge?: "million-dollar" | "veteran",
+  className?: string,
+  data?: object,
+  id?: string,
   size?: "sm" | "md" | "lg",
 }
 
 const UserBadge = (props: UserBadgeProps) => {
-  const { badge = 'million-dollar', size = 'md' } = props
+  const {
+    aria = {},
+    badge = 'million-dollar',
+    className,
+    data = {},
+    id,
+    size = 'md',
+  } = props
+
   const image = require(`./badges/_${badge}.svg`)
+  const ariaProps = buildAriaProps(aria)
+  const dataProps = buildDataProps(data)
+  const classes = classnames(buildCss('pb_user_badge_kit', size), globalProps(props),
+    className)
 
   return (
-    <div className={classnames(`pb_user_badge_kit_${size}`, globalProps(props))}>
+    <div
+        {...ariaProps}
+        {...dataProps}
+        className={classes}
+        id={id}
+    >
       <div className="pb_user_badge_wrapper">
         <img src={image} />
       </div>
