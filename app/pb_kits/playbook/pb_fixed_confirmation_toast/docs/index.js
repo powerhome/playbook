0a54a1700b227bdcbb335a0797bc07c511ca1ca2
Kelly Ryan <51907753+kellyeryan@users.noreply.github.com>
2020-08-20 13:44:10 -0400

    Add Fixed Confirmation Toast dark mode (#981)

--- app/pb_kits/playbook/pb_fixed_confirmation_toast/docs/index.js
+++ app/pb_kits/playbook/pb_fixed_confirmation_toast/docs/index.js
@@ -1,2 +1,4 @@
 export { default as FixedConfirmationToastDefault } from './_fixed_confirmation_toast_default.jsx'
 export { default as FixedConfirmationToastMultiLine } from './_fixed_confirmation_toast_multi_line.jsx'
+export { default as FixedConfirmationToastDark } from './_fixed_confirmation_toast_dark.jsx'
+export { default as FixedConfirmationToastMultiLineDark } from './_fixed_confirmation_toast_multi_line_dark.jsx'
