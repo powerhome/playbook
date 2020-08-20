1b5c936b38c37d33c1090f12620a4378c6cfbc7e
Kelly Ryan <51907753+kellyeryan@users.noreply.github.com>
2020-08-20 18:02:59 -0400

    Table dark mode (#986)
    * Update table dark mode
    
    * Fix rspec
    
    * Removed unneeded import and sort props alphabetically
    
    * Update app/pb_kits/playbook/pb_table/_table_row.jsx
    
    Co-authored-by: Jasper Furniss <jasperfurniss@gmail.com>

--- spec/pb_kits/playbook/kits/table_spec.rb
+++ spec/pb_kits/playbook/kits/table_spec.rb
@@ -27,13 +27,13 @@ RSpec.describe Playbook::PbTable::Table do
     it "returns namespaced class name", :aggregate_failures do
       expect(subject.new({}).classname).to eq "pb_table table-md table-card table-responsive-collapse"
       expect(subject.new(classname: "additional_class").classname).to eq "pb_table table-md table-card table-responsive-collapse additional_class"
-      expect(subject.new(dark: true).classname).to eq "pb_table table-md dark table-card table-responsive-collapse dark"
+      expect(subject.new(dark: true).classname).to eq "pb_table table-md table-card table-responsive-collapse dark"
       expect(subject.new(data_table: true).classname).to eq "pb_table table-md table-card data_table table-responsive-collapse"
       expect(subject.new(size: "sm").classname).to eq "pb_table table-sm table-card table-responsive-collapse"
       expect(subject.new(single_line: true).classname).to eq "pb_table table-md single-line table-card table-responsive-collapse"
       expect(subject.new(container: false).classname).to eq "pb_table table-md table-responsive-collapse"
       expect(subject.new(disable_hover: true).classname).to eq "pb_table table-md no-hover table-card table-responsive-collapse"
-      expect(subject.new(disable_hover: true, dark: true, size: "lg", single_line: true).classname).to eq "pb_table table-lg single-line dark no-hover table-card table-responsive-collapse dark"
+      expect(subject.new(disable_hover: true, dark: true, size: "lg", single_line: true).classname).to eq "pb_table table-lg single-line no-hover table-card table-responsive-collapse dark"
     end
   end
 end
