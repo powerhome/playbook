6fda4ede0cb85e31a101a343f206de53587642da
Kelly Ryan <51907753+kellyeryan@users.noreply.github.com>
2020-08-21 10:42:07 -0400

    Update label value dark mode (#968)
    * Update label value dark mode
    
    * Add CHANGELOG
    
    * Add CHANGELOG
    
    * Fix label value to include dark mode prop on child kits
    
    * Delete CHANGELOG entry
    
    * Delete CHANGELOG entry
    
    * Once more unto the CHANGELOG
    
    * Removed dark proptype and prop
    
    * Update app/pb_kits/playbook/pb_label_value/_label_value.jsx
    
    Co-authored-by: Jonathan Zazula <djonyriz@gmail.com>
    Co-authored-by: Jasper Furniss <jasperfurniss@gmail.com>

--- spec/pb_kits/playbook/kits/label_value_spec.rb
+++ spec/pb_kits/playbook/kits/label_value_spec.rb
@@ -11,7 +11,6 @@ RSpec.describe Playbook::PbLabelValue::LabelValue do
   it { is_expected.to define_enum_prop(:variant)
                       .with_values("default", "details")
                       .with_default("default") }
-  it { is_expected.to define_boolean_prop(:dark).with_default(false) }
 
   describe "#classname" do
     it "returns namespaced class name", :aggregate_failures do
@@ -20,10 +19,10 @@ RSpec.describe Playbook::PbLabelValue::LabelValue do
       expect(subject.new(label: label, value: value).classname).to eq "pb_label_value_kit"
       expect(subject.new(label: label, variant: "default").classname).to eq "pb_label_value_kit"
       expect(subject.new(label: label, variant: "details").classname).to eq "pb_label_value_kit_details"
-      expect(subject.new(label: label, variant: "details", dark: true).classname).to eq "pb_label_value_kit_details_dark dark"
-      expect(subject.new(label: label, value: value, dark: true).classname).to eq "pb_label_value_kit_dark dark"
+      expect(subject.new(label: label, variant: "details", dark: true).classname).to eq "pb_label_value_kit_details dark"
+      expect(subject.new(label: label, value: value, dark: true).classname).to eq "pb_label_value_kit dark"
       expect(subject.new(label: label, value: value, classname: "additional_class").classname).to eq "pb_label_value_kit additional_class"
-      expect(subject.new(label: label, value: value, classname: "additional_class", dark: true).classname).to eq "pb_label_value_kit_dark additional_class dark"
+      expect(subject.new(label: label, value: value, classname: "additional_class", dark: true).classname).to eq "pb_label_value_kit additional_class dark"
     end
   end
 end
