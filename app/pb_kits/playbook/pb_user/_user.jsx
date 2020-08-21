886709e914f9950a91290b3d33fab8145f485d76
Jonathan Zazula <djonyriz@gmail.com>
2020-08-21 10:36:14 -0400

    Fix failing tests

--- app/pb_kits/playbook/pb_user/_user.jsx
+++ app/pb_kits/playbook/pb_user/_user.jsx
@@ -75,7 +75,10 @@ const User = (props: UserProps) => {
             size={size == 'lg' ? 3 : 4}
             text={name}
         />
-        <Body color="light" dark={dark}>
+        <Body
+            color="light"
+            dark={dark}
+        >
           {territory === '' ? title : `${territory} • ${title}`}
         </Body>
       </div>
