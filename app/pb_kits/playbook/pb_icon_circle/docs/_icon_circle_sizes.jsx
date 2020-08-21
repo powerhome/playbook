758abdf57b0c93bda6287b0dfd75e114e8cec307
Kelly Ryan <51907753+kellyeryan@users.noreply.github.com>
2020-08-21 11:41:28 -0400

    Add Dark Mode to the Icon Circle (#973)
    * Add Dark Mode to the Iron Circle
    
    * Add CHANGELOG
    
    * Fix missing s
    
    * Add dark prop to child kits and add missing aria, data props
    
    * Delete CHANGELOG
    
    * Delete CHANGELOG
    
    * Once more unto the CHANGELOG
    
    * Update app/pb_kits/playbook/pb_icon_circle/_icon_circle.jsx
    
    Co-authored-by: Jasper Furniss <jasperfurniss@gmail.com>

---
+++ app/pb_kits/playbook/pb_icon_circle/docs/_icon_circle_sizes.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { IconCircle } from '../..'
+
+const IconCircleSizes = () => {
+  return (
+    <div>
+      <IconCircle
+          icon="rocket"
+          size="sm"
+      />
+      <br />
+      <IconCircle
+          icon="rocket"
+          size="md"
+      />
+      <br />
+      <IconCircle
+          icon="rocket"
+          size="lg"
+      />
+      <br />
+      <IconCircle
+          icon="rocket"
+          size="xl"
+      />
+    </div>
+  )
+}
+
+export default IconCircleSizes
