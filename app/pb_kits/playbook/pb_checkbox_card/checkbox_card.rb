201666d759875cf020e5de5e427551f2c56a1af8
Pat McClernan <patmccler@gmail.com>
2020-08-24 17:12:00 -0400

    Change styles and layout to get card to look better
    Checkbox wasnt staying square, now it is. The body kit needs to be inline somehow to get it to show on same line as checkbox. That's WIP

--- app/pb_kits/playbook/pb_checkbox_card/checkbox_card.rb
+++ app/pb_kits/playbook/pb_checkbox_card/checkbox_card.rb
@@ -6,6 +6,26 @@ module Playbook
       include Playbook::Props
 
       partial "pb_checkbox_card/checkbox_card"
+
+      prop :checked, type: Playbook::Props::Boolean,
+                  default: false
+      prop :disabled, type: Playbook::Props::Boolean,
+                  default: false
+      prop :icon, type: Playbook::Props::Boolean,
+                  default: false
+
+      prop :input_id, type: Playbook::Props::String
+      prop :name
+      prop :text
+      prop :value
+
+      def classname
+        generate_classname("pb_checkbox_card_kit", checked_class)
+      end
+
+      def checked_class
+        checked ? "checked" : nil
+      end
     end
   end
 end
