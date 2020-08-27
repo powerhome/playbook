951b99cba27870253ba7b977a72d57aed432dffe
Megan Trimble <mtrimble6@me.com>
2020-08-27 11:49:33 -0400

    Button & Circle Icon Button Cleanup

--- app/pb_kits/playbook/pb_button/docs/_button_default.jsx
+++ app/pb_kits/playbook/pb_button/docs/_button_default.jsx
@@ -4,26 +4,22 @@ import { Button } from '../../'
 const ButtonDefault = () => (
   <div>
     <Button
-        dark
-        marginRight="xl"
         onClick={() => alert('button clicked!')}
         text="Button Primary"
     />
+    {' '}
     <Button
         onClick={() => alert('button clicked!')}
         text="Button Secondary"
         variant="secondary"
     />
+    {' '}
     <Button
         onClick={() => alert('button clicked!')}
         text="Button Link"
         variant="link"
     />
-    <Button
-        disabled
-        onClick={() => alert('button clicked!')}
-        text="Button Disabled"
-    />
+    {' '}
     <Button
         disabled
         onClick={() => alert('button clicked!')}
