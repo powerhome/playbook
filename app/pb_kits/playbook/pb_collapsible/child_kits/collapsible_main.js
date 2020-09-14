5b2ba1e7613c9338b1f39b2c95e0e7c51c967740
Aaron Wilson <adub65@gmail.com>
2020-09-14 11:41:17 -0400

    Add event listener to Main section

---
+++ app/pb_kits/playbook/pb_collapsible/child_kits/collapsible_main.js
@@ -0,0 +1,29 @@
+// window.addEventListener.onload = () => {
+//   document.getElementById("pb_collapsible_main_kit sm p_sm").style.display === 'none'
+// }
+
+// const mainSection = document.querySelector(".pb_collapsible_main_kit")
+// console.log(5)
+
+// mainSection.addEventListener("click", (event) => {
+//   console.log(event)
+//   event.preventDefault()
+//   toggleContent()
+// })
+
+// function toggleContent() {
+//   const contentSection = document.querySelector('.pb_collapsible_content_kit')
+//   if (contentSection.style.display === "none") {
+//     contentSection.style.display = "block";
+//   } else {
+//     contentSection.style.display = "none";
+//   }
+// }
+
+//  if chevron is up
+//    return height === 0
+//  if chevron is down
+//    return height === auto
+// const renderChevron(height) => {}
+//  const direction = height === 0 ? 'down' : 'up'
+// <i className={`far fa-2x fa-chevron-${direction} fa-fw`} />
