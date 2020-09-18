4d3827ae59fa6c074bb6178de22264f9e4260977
Madison McCardle <madison.mccardle@powerhrg.com>
2020-09-18 15:30:46 -0400

    Add image background
    Need to fix text

--- app/pb_kits/playbook/pb_background/_background.jsx
+++ app/pb_kits/playbook/pb_background/_background.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 import classnames from 'classnames'
 import { buildAriaProps, buildCss, buildDataProps } from '../utilities/props'
 import { globalProps } from '../utilities/globalProps.js'
-import { Image, Title } from '../'
+import { Image } from '../'
 
 type BackgroundProps = {
   aria?: object,
@@ -15,7 +15,7 @@ type BackgroundProps = {
   id?: string,
   tag?: string,
   imageUrl?: string,
-  padding?: string,
+  padding?: "none" | "xs" | "sm" | "md" | "lg" | "xl",
   children?: array<React.ReactNode> | React.ReactNode,
 }
 
@@ -31,13 +31,13 @@ const Background = (props: BackgroundProps) => {
     id,
     imageUrl = '',
     tag = "div",
-    padding = 'md',
+    padding = "md",
   } = props
 
   const borderCSS = borderNone == true ? 'border_none' : ''
   const ariaProps = buildAriaProps(aria)
   const dataProps = buildDataProps(data)
-  const classes = classnames(buildCss('pb_background_kit'), `pb--color_bg_${backgroundColor}`, className, borderCSS, globalProps(props, { padding }))
+  const classes = classnames(buildCss('pb_background_kit'), className, borderCSS, globalProps(props, { padding }))
   const Tag = `${tag}`
 
   return (
@@ -45,13 +45,21 @@ const Background = (props: BackgroundProps) => {
     <div
         {...ariaProps}
         {...dataProps}
-        alt={alt}
-        className={classes}
-        data-src={imageUrl}
         id={id}
     >
-      {children}
-      {/* <Image url={imageUrl}></Image>` */}
+      <If condition={imageUrl}>
+        <Image
+            alt={name}
+            url={imageUrl}
+            className={classes}
+        >
+          {children}
+        </Image>
+        <Else/>
+          <div className={classes + `pb--color_bg_${backgroundColor}`}>
+            {children}
+          </div>
+      </If>
     </div>
 
   )
--- app/pb_kits/playbook/pb_background/docs/_background_default.jsx
+++ app/pb_kits/playbook/pb_background/docs/_background_default.jsx
@@ -1,16 +1,30 @@
 import React from 'react'
 import { Background } from '../../'
-import { Image } from '../../'
+import { Body, Title, Flex, FlexItem } from '../../'
 
 const BackgroundDefault = () => (
   <div>
-    <Background backgroundColor="gradient"> Background Kit </Background>
-    <Background backgroundColor="dark"> Background Kit </Background>
+    <Background backgroundColor="gradient" padding="xl">
+      <Flex
+            horizontal="center"
+            orientation="row"
+        >
+          <FlexItem>
+            <Title dark size={1} text="Background Kit"></Title>
+          </FlexItem>
+      </Flex>
+    </Background>
+
+    <Background backgroundColor="dark" padding= "xl">
+      <Title dark size={4} text="hello"></Title>
+    </Background>
+
     <Background backgroundColor="light"> Background Kit </Background>
-    <Background backgroundColor=""> Background Kit </Background>
-    {/* <Background image_url='https://www.proflowers.com/blog/wp-content/uploads/2016/04/hero-blue-flowers.jpg'>
-      background image
-    </Background> */}
+
+    <Background backgroundColor="" padding="lg"> Background Kit </Background>
+    <Background imageUrl='https://www.proflowers.com/blog/wp-content/uploads/2016/04/hero-blue-flowers.jpg'>
+      <Body text= "Background Kit"></Body>
+    </Background>
   </div>
 )
 
