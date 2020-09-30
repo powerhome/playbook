5b08831c8177ae7e06f5912e0b0cf817f09bbe93
Maricris Nonato <maricris.nonato@powerhrg.com>
2020-09-30 23:06:45 +0800

    WIP: Timestamp kit update Issue #618

--- app/pb_kits/playbook/pb_timestamp/timestamp.rb
+++ app/pb_kits/playbook/pb_timestamp/timestamp.rb
@@ -7,10 +7,44 @@ module Playbook
 
       partial "pb_timestamp/timestamp"
 
-      prop :text
+      prop :timestamp, required: true
+      prop :show_date, type: Playbook::Props::Boolean, default: true
+
+      prop :dark, type: Playbook::Props::Boolean,
+                  default: false
+
+      prop :align,  type: Playbook::Props::Enum,
+                    values: %w[left center right],
+                    default: "left"
 
       def classname
-        generate_classname("pb_timestamp_kit")
+        generate_classname("pb_timestamp_kit", align, dark_class)
+      end
+
+      def format_year_string
+        pb_date_time.to_year != DateTime.now.year.to_s ? ", #{pb_date_time.to_year}" : ""
+      end
+
+      def format_time_string
+        "#{pb_date_time.to_hour}:#{pb_date_time.to_minutes}#{pb_date_time.to_meridian}"
+      end
+
+      def format_date_string
+        "#{pb_date_time.to_month_downcase} #{pb_date_time.to_unpadded_day}#{format_year_string}"
+      end
+
+      def format_datetime_string
+        "#{format_date_string} &middot; #{format_time_string}".html_safe
+      end
+
+    private
+
+      def pb_date_time
+        Playbook::PbKit::PbDateTime.new(timestamp)
+      end
+
+      def dark_class
+        dark ? "dark" : nil
       end
     end
   end
