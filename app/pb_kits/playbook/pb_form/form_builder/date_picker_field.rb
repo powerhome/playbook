ae13f0b8898f326a0ca07a4d322ab519f2a48856
Brendan Huffman <bh247484@gmail.com>
2020-10-01 15:01:37 -0400

    rails form label fix

--- app/pb_kits/playbook/pb_form/form_builder/date_picker_field.rb
+++ app/pb_kits/playbook/pb_form/form_builder/date_picker_field.rb
@@ -8,11 +8,10 @@ module Playbook
           prefix = @object_name
           html_attribute_name = "#{prefix}[#{name}]"
           html_id = "#{prefix}_#{name}"
+          props[:label] = @template.label(@object_name, name) if props[:label] && !props[:label].is_a?(String)
 
           props[:name] = html_attribute_name
           props[:picker_id] = html_id
-
-          # props[:allow_input] = true if props[:required]
 
           @template.pb_rails("date_picker", props: props)
         end
