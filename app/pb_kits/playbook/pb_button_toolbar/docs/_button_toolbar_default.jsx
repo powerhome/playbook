a5efb32fc4c3ed9a0555be07044cb0b7627f89c7
christina tai <christina.tai@powerhrg.com>
2020-10-01 15:40:23 -0400

    Fixed dark bug for react
    Fixed dark bug for react

--- app/pb_kits/playbook/pb_button_toolbar/docs/_button_toolbar_default.jsx
+++ app/pb_kits/playbook/pb_button_toolbar/docs/_button_toolbar_default.jsx
@@ -1,46 +1,58 @@
 import React from 'react'
 import { Button, ButtonToolbar } from '../..'
 
-const ButtonToolbarDefault = () => (
+const ButtonToolbarDefault = (props) => (
   <div className="pb--doc-demo-row">
     <ButtonToolbar
+        {...props}
         connected
         orientation="vertical"
     >
       <Button
+          {...props}
           text="Field"
       />
       <Button
+          {...props}
           text="Retail"
       />
       <Button
+          {...props}
           text="Event"
       />
       <Button
+          {...props}
           text="Training"
       />
       <Button
+          {...props}
           text="Not Working"
       />
     </ButtonToolbar>
 
     <ButtonToolbar
+        {...props}
         connected
         orientation="horizontal"
     >
       <Button
+          {...props}
           text="Field"
       />
       <Button
+          {...props}
           text="Retail"
       />
       <Button
+          {...props}
           text="Event"
       />
       <Button
+          {...props}
           text="Training"
       />
       <Button
+          {...props}
           text="Not Working"
       />
     </ButtonToolbar>
--- app/pb_kits/playbook/pb_button_toolbar/docs/_button_toolbar_secondary.jsx
+++ app/pb_kits/playbook/pb_button_toolbar/docs/_button_toolbar_secondary.jsx
@@ -1,57 +1,69 @@
 import React from 'react'
 import { Button, ButtonToolbar } from '../..'
 
-const ButtonToolbarSecondary = () => (
+const ButtonToolbarSecondary = (props) => (
   <div className="pb--doc-demo-row">
     <ButtonToolbar
+        {...props}
         connected
         orientation="vertical"
         variant="secondary"
     >
       <Button
+          {...props}
           text="Field"
           variant="secondary"
       />
       <Button
+          {...props}
           text="Retail"
           variant="secondary"
       />
       <Button
+          {...props}
           text="Event"
           variant="secondary"
       />
       <Button
+          {...props}
           text="Training"
           variant="secondary"
       />
       <Button
+          {...props}
           text="Not Working"
           variant="secondary"
       />
     </ButtonToolbar>
 
     <ButtonToolbar
+        {...props}
         connected
         orientation="horizontal"
         variant="secondary"
     >
       <Button
+          {...props}
           text="Field"
           variant="secondary"
       />
       <Button
+          {...props}
           text="Retail"
           variant="secondary"
       />
       <Button
+          {...props}
           text="Event"
           variant="secondary"
       />
       <Button
+          {...props}
           text="Training"
           variant="secondary"
       />
       <Button
+          {...props}
           text="Not Working"
           variant="secondary"
       />
