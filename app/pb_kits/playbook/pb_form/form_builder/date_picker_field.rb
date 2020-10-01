acafccce3ff93e94a50f490d8648cb8939d34e4b
Brendan Huffman <bh247484@gmail.com>
2020-10-01 15:24:27 -0400

    text_field init

--- app/pb_kits/playbook/pb_form/form_builder/date_picker_field.rb
+++ app/pb_kits/playbook/pb_form/form_builder/date_picker_field.rb
@@ -12,9 +12,23 @@ module Playbook
           props[:name] = html_attribute_name
           props[:picker_id] = html_id
 
-          @template.pb_rails("date_picker", props: props)
+          # puts text_field_tag("test-name", value = nil, options = { id: html_id })
+          # puts text_field_tag(:name)
+          puts text_field(name)
+
+          @template.pb_rails("date_picker", props: props) do
+          end
         end
       end
     end
   end
 end
+
+# Create text input with rails form helper
+# pass options to text input
+
+# add hidden, implicit prop identifying it as form helper
+# inside dp erb, create conditional if check to leave out text_input kit
+
+# pass that input as a block to @template.pb_rails("date_picker")
+# let rails do the magic
