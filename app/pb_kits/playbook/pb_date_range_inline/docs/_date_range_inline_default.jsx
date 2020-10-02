dec1e20a8447a1370c46cfca4882ddbead6a53b2
Matthew Dodds <matthew.dodds@powerhrg.com>
2020-10-02 10:31:58 -0400

    Pass the props so darkmode works

--- app/pb_kits/playbook/pb_date_range_inline/docs/_date_range_inline_default.jsx
+++ app/pb_kits/playbook/pb_date_range_inline/docs/_date_range_inline_default.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
 import DateRangeInline from '../_date_range_inline.jsx'
 
-const DateRangeInlineDefault = () => {
+const DateRangeInlineDefault = (props) => {
   return (
     <div>
       <DateRangeInline
+          {...props}
           endDate={new Date('20 Mar 2015')}
           size="xs"
           startDate={new Date('18 Jun 2013')}
       />
       <DateRangeInline
+          {...props}
           endDate={new Date('20 Mar 2015')}
           marginTop="xs"
           size="sm"
           startDate={new Date('18 Jun 2013')}
       />
       <DateRangeInline
+          {...props}
           endDate={new Date(`15 Aug ${new Date().getFullYear()}`)}
           icon
           marginTop="md"
@@ -23,6 +26,7 @@ const DateRangeInlineDefault = () => {
           startDate={new Date(`15 Jan ${new Date().getFullYear()}`)}
       />
       <DateRangeInline
+          {...props}
           endDate={new Date(`15 Aug ${new Date().getFullYear()}`)}
           icon
           marginTop="xs"
