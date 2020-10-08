d8e9647742b5fc674b61901167dee8524038ed29
Stephen Marshall <smarshall1980@gmail.com>
2020-10-08 16:04:18 -0500

    Typeahead with Pills (Rails) (#1104)
    * Update PULL_REQUEST_TEMPLATE.md
    
    * WIP: Typeahead w/ Pills (React)
    
    * WIP
    
    * Basic pub-sub working for add, remove and clear
    
    * Revert changes to index.js
    
    * Async options
    
    * Spec coverage
    
    * Add code example docs
    
    Co-authored-by: Jason Cypret <jason@jasoncypret.com>

--- app/pb_kits/playbook/pb_typeahead/components/Option.jsx
+++ app/pb_kits/playbook/pb_typeahead/components/Option.jsx
@@ -12,21 +12,29 @@ const Option = (props: any) => {
     imageUrl,
     label,
   } = props.data
+
+  const handleOptionClicked = () => {
+    const resultSelectedEvent = new CustomEvent('pb-typeahead-kit-result-option-select', { detail: props.data })
+    document.dispatchEvent(resultSelectedEvent)
+  }
+
   return (
     <components.Option {...props}>
-      <Choose>
-        <When condition={imageUrl}>
-          <User
-              align="left"
-              avatarUrl={imageUrl}
-              name={label}
-              orientation="horizontal"
-          />
-        </When>
-        <When condition={!imageUrl}>
-          {label}
-        </When>
-      </Choose>
+      <div onClick={handleOptionClicked}>
+        <Choose>
+          <When condition={imageUrl}>
+            <User
+                align="left"
+                avatarUrl={imageUrl}
+                name={label}
+                orientation="horizontal"
+            />
+          </When>
+          <When condition={!imageUrl}>
+            {label}
+          </When>
+        </Choose>
+      </div>
     </components.Option>
   )
 }
