6961a279c73ed6725304ca733207d3bc7f4c6e9b
Maricris Nonato <maricris.nonato@powerhrg.com>
2020-10-09 17:18:28 +0800

    Adding more specs to timestamp kit

--- spec/pb_kits/playbook/kits/timestamp_spec.rb
+++ spec/pb_kits/playbook/kits/timestamp_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "../../../../app/pb_kits/playbook/pb_timestamp/timestamp"
+include ActionView::Helpers::DateHelper
 
 RSpec.describe Playbook::PbTimestamp::Timestamp do
   subject { Playbook::PbTimestamp::Timestamp }
@@ -44,18 +45,103 @@ RSpec.describe Playbook::PbTimestamp::Timestamp do
     it "returns nothing if timestamp is in year" do
       expect(subject.new(timestamp: DateTime.current).format_year_string).to eq("")
     end
-
   end
 
   describe "#format_time_string" do
+    it "returns HH:MM with meridian", :focus do
+      timestamp = DateTime.new(2020, 10, 10, 20, 30, 00).in_time_zone("America/New_York").freeze
+      expect(subject.new(timestamp: timestamp).format_time_string).to eq(" 4:30p")
+    end
   end
 
-  xdescribe "#format_date_string" do
+  describe "#format_date_string" do
+    it "returns date in specific format without year" do
+      expect(subject.new(timestamp: DateTime.current).format_date_string).to eq("#{DateTime.current.strftime('%b %-d')}")
+    end
+
+    it "returns date in specific format with year" do
+      expect(subject.new(timestamp: DateTime.current + 4.years).format_date_string).to eq("#{(DateTime.current + 4.years).strftime('%b %-d, %Y')}")
+    end
   end
 
   xdescribe "#format_datetime_string" do
+    it "returns date with time separated by middot" do
+      expect(subject.new(timestamp: DateTime.current).format_datetime_string).to eq("#{DateTime.current.strftime('%b %-d')} " + '&middot;' + " #{DateTime.current.strftime('%l:%m%P')}")
+    end
+
+    it "returns full date and time separated by middot" do
+      expect(subject.new(timestamp: DateTime.current + 4.years).format_datetime_string).to eq("#{(DateTime.current + 4.years).strftime('%b %-d, %Y')} " + '&middot;' + " #{DateTime.current.strftime('%l:%m%P')}")
+    end
   end
 
-  xdescribe "#format_updated_string" do
+  describe "#format_updated_string" do
+    let(:name) { "Maricris Nonato" }
+
+    context "for variant updated" do
+      let(:variant) { "updated" }
+
+      context "if show_user is true" do
+        let(:show_user) { true }
+        it "returns last updated string including user's name" do
+          timestamp = DateTime.new(2020, 10, 10, 20, 30, 00).in_time_zone("America/New_York").freeze
+          date = "Oct 10"
+          time = " 4:30p"
+
+          expect(subject.new(timestamp: timestamp, variant: variant, show_user: show_user, name: name).format_updated_string).to eq("Last updated by #{name} on #{date} at #{time}")
+        end
+
+        it "returns last updated with year including user's name" do
+          timestamp = DateTime.new(2024, 10, 10, 20, 30, 00).in_time_zone("America/New_York").freeze
+          date = "Oct 10, 2024"
+          time = " 4:30p"
+
+          expect(subject.new(timestamp: timestamp, variant: variant, show_user: show_user, name: name).format_updated_string).to eq("Last updated by #{name} on #{date} at #{time}")
+        end
+      end
+
+      context "if show_user is false" do
+        let(:show_user) { false }
+        it "returns last updated without user's name" do
+          timestamp = DateTime.new(2020, 10, 10, 20, 30, 00).in_time_zone("America/New_York").freeze
+          date = "Oct 10"
+          time = " 4:30p"
+
+          expect(subject.new(timestamp: timestamp, variant: variant, show_user: show_user).format_updated_string).to eq("Last updated on #{date} at #{time}")
+        end
+
+        it "returns last updated with year without user's name" do
+          timestamp = DateTime.new(2024, 10, 10, 20, 30, 00).in_time_zone("America/New_York").freeze
+          date = "Oct 10, 2024"
+          time = " 4:30p"
+
+          expect(subject.new(timestamp: timestamp, variant: variant, show_user: show_user).format_updated_string).to eq("Last updated on #{date} at #{time}")
+        end
+      end
+    end
+
+    context "for variant elapsed" do
+      let(:variant) { "elapsed" }
+      context "if show_user is true" do
+        let(:show_user) { true }
+        it "returns time elaspsed string including user's name" do
+          timestamp = DateTime.new(2020, 10, 10, 20, 30, 00).in_time_zone("America/New_York").freeze
+          date = "Oct 10"
+          time = " 4:30p"
+
+          expect(subject.new(timestamp: timestamp, variant: variant, show_user: show_user, name: name).format_updated_string).to eq("Last updated by #{name} #{time_ago_in_words(timestamp)} ago")
+        end
+      end
+
+      context "if show_user is false" do
+        let(:show_user) { false }
+        it "returns time elaspsed string sans user's name" do
+          timestamp = DateTime.new(2020, 10, 10, 20, 30, 00).in_time_zone("America/New_York").freeze
+          date = "Oct 10"
+          time = " 4:30p"
+
+          expect(subject.new(timestamp: timestamp, variant: variant, show_user: show_user).format_updated_string).to eq("Last updated #{time_ago_in_words(timestamp)} ago")
+        end
+      end
+    end
   end
 end
