f6cc5d76fc6a58cd41b24832a155e37ce40aa6d5
derrick lannaman <derrick.lannaman@powerhrg.com>
2020-10-13 08:29:32 -0500

    Add pagination behaviors

--- app/pb_kits/playbook/pb_pagination/pagination.rb
+++ app/pb_kits/playbook/pb_pagination/pagination.rb
@@ -7,14 +7,60 @@ module Playbook
 
       partial "pb_pagination/pagination"
 
-      prop :page_number, type: Playbook::Props::Array, default: []
       prop :variant, type: Playbook::Props::Enum,
                      values: %w[basic collapsed expanded scroll],
                      default: "basic"
-      prop :active, type: Playbook::Props::Boolean, default: false
+      prop :pages, type: Playbook::Props::Array, default: []
+      prop :target_page, type: Playbook::Props::Number, default: 1
+
+      prop :page_range, type: Playbook::Props::Array, default: []
+      prop :active_page, type: Playbook::Props::Boolean, default: false
+
+      PAGINATE_RANGE = 2
 
       def classname
         generate_classname("pb_pagination_kit", variant)
+      end
+
+      def total_page_count per_page, total_items
+        total_items / per_page
+      end
+
+      def number_of_pages target_page, per_pages, total_items
+        return if (target_page > total_page_count(per_pages, total_items))
+        "#{target_page} of #{ total_page_count(per_pages, total_items) }"
+      end
+
+      def calc_next_pages page, next_nums
+        (page..page + next_nums).to_a
+      end
+
+      def calc_previous_pages page, prev_nums
+        (page - prev_nums..page).to_a
+      end
+
+      def calculate_page_range
+        page = values[:target_page]
+        max_page = total_page_count values[:per_page], values[:total_items]
+
+        if page <= PAGINATE_RANGE
+          prev_pages = (1..page).to_a
+          next_pages = calc_next_pages(page, PAGINATE_RANGE)
+
+        elsif page < (max_page - PAGINATE_RANGE)
+          prev_pages = calc_previous_pages(page, PAGINATE_RANGE)
+          next_pages = calc_next_pages(page, PAGINATE_RANGE)
+
+        elsif (max_page - PAGINATE_RANGE..max_page).to_a.include?(page)
+          prev_pages = calc_previous_pages(page, PAGINATE_RANGE)
+          next_pages = (page.upto(max_page)).to_a
+
+        elsif page >= max_page
+          prev_pages = calc_previous_pages(max_page, PAGINATE_RANGE)
+          next_pages = calc_next_pages(max_page, 0)
+        end
+
+        range = prev_pages | next_pages
       end
     end
   end
