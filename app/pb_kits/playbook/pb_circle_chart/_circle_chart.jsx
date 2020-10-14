027331e9cf7daa7e8d3e1b83d5f7b960904468f3
Brendan Huffman <bh247484@gmail.com>
2020-10-14 15:08:52 -0400

    optimized conditional

--- app/pb_kits/playbook/pb_circle_chart/_circle_chart.jsx
+++ app/pb_kits/playbook/pb_circle_chart/_circle_chart.jsx
@@ -101,9 +101,7 @@ const CircleChart = (props: CircleChartProps) => {
   useEffect(() => {
     if (componentDidMount.current) {
       Highcharts.charts.forEach((chart) => {
-        if (chart === undefined){
-          return
-        } else if (chart.renderTo.id === id) {
+        if (chart && chart.renderTo.id === id) {
           const updatedValueArray = chartData.map((obj) => {
             return obj.value
           })
