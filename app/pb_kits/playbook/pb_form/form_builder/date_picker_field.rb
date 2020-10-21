92c61078ad9a7dc7f3c57012f8d2c7713c5ee101
Brendan Huffman <bh247484@gmail.com>
2020-10-21 14:01:34 -0400

    fixed label conditionals

--- app/pb_kits/playbook/pb_form/form_builder/date_picker_field.rb
+++ app/pb_kits/playbook/pb_form/form_builder/date_picker_field.rb
@@ -8,7 +8,9 @@ module Playbook
           prefix = @object_name
           html_attribute_name = "#{prefix}[#{name}]"
           html_id = "#{prefix}_#{name}"
+
           props[:label] = @template.label(@object_name, name) if props[:label] == true
+          props[:label] = "Date Picker" if props[:label].nil?
 
           props[:name] = html_attribute_name
           props[:picker_id] = html_id
@@ -21,7 +23,7 @@ module Playbook
             aria: props[:input_aria],
             props: {
               error: props[:error],
-              label: props[:label],
+              label: props[:hide_label] ? nil : props[:label],
               placeholder: props[:placeholder],
               required: props[:required],
             }
--- lib/playbook/version.rb
+++ lib/playbook/version.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 module Playbook
-  VERSION = "7.2.0"
+  VERSION = "7.3.0.pre.alpha6"
 end
