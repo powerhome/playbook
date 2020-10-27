35aa18b4e77ae7fbde5d7f851e8783a2cb6c56fe
Brendan Huffman <bh247484@gmail.com>
2020-10-27 17:08:39 -0400

    design mark ii

--- app/pb_kits/playbook/packs/docs_components/SnippetToggle.jsx
+++ app/pb_kits/playbook/packs/docs_components/SnippetToggle.jsx
@@ -18,31 +18,22 @@ const SnippetToggle = (props) => {
   }
 
   return (
-    <>
+    <div style={{cursor: "pointer"}}>
       <Nav
-        link="#"
         orientation="horizontal"
       >
         <NavItem
             active={codeSnippet === 'rails'}
-            link="#"
             text="Rails"
             onClick={() => toggleHook('rails')}
         />
         <NavItem
             active={codeSnippet === 'react'}
-            link="#"
             text="React"
             onClick={() => toggleHook('react')}
         />
       </Nav>
-      {/* <If condition={codeSnippet === 'rails'} >
-        <h1>Rails!!</h1>
-      </If>
-      <If condition={codeSnippet === 'react'} >
-        <h1>React!!</h1>
-      </If> */}
-    </>
+    </div>
   )
 }
 
