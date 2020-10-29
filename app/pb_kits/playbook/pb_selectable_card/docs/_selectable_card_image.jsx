732a4a64834a3b4537ba36168180f736b6f35949
Idan Zonshein <idan.zonshein@powerhrg.com>
2020-10-29 15:50:18 -0400

    Finish react side to selectable image

---
+++ app/pb_kits/playbook/pb_selectable_card/docs/_selectable_card_image.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import {
+  Body,
+  SelectableCard,
+  Image,
+} from '../../'
+
+class SelectableCardImage extends React.Component {
+  state = {
+    selectableImage: true,
+    unselectedImage: false,
+  }
+
+  handleSelect = (event) => {
+    this.setState({
+      [event.target.id]: event.target.checked,
+    })
+  }
+
+  render() {
+    return (
+      <div className="pb--doc-demo-row">
+
+        <SelectableCard
+            checked={this.state.selectableImage}
+            icon
+            inputId="selectableImage"
+            name="selectableImage"
+            onChange={this.handleSelect}
+            value="selectableImage"
+        >
+          <Image
+            rounded
+            url="https://unsplash.it/500/400/?image=634"
+          />
+
+          <Body>{'Add text here'}</Body>
+        </SelectableCard>
+
+        <SelectableCard
+            checked={this.state.unselectedImage}
+            icon
+            inputId="unselectedImage"
+            name="unselectedImage"
+            onChange={this.handleSelect}
+            value="unselectedImage"
+        >
+          <Image
+            rounded
+            size="xl"
+            url="https://unsplash.it/500/400/?image=634"
+          />
+        </SelectableCard>
+
+      </div>
+    )
+  }
+}
+
+export default SelectableCardImage
---
+++ app/pb_kits/playbook/pb_selectable_card/docs/_selectable_card_image.md
@@ -0,0 +1,1 @@
+Selectable Cards can pass images with optional text.
