5bbde0467803aab23d92c24a4101054e68ff2144
Madison McCardle <madison.mccardle@powerhrg.com>
2020-11-04 15:34:21 -0500

    Adds react side/ examples to doc

--- app/pb_kits/playbook/pb_form_group/docs/index.js
+++ app/pb_kits/playbook/pb_form_group/docs/index.js
@@ -1,1 +1,3 @@
 export { default as FormGroupDefault } from './_form_group_default.jsx'
+export { default as FormGroupButton } from './_form_group_button.jsx'
+export { default as FormGroupDatePicker } from './_form_group_date_picker.jsx'
