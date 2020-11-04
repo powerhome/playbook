822f560b5244621ed48509020e046581af898449
christina tai <christina.tai@powerhrg.com>
2020-11-04 10:31:24 -0500

    Removed description & ids in react doc examples

--- app/pb_kits/playbook/pb_selectable_list/docs/_selectable_list_checkbox.jsx
+++ app/pb_kits/playbook/pb_selectable_list/docs/_selectable_list_checkbox.jsx
@@ -6,20 +6,17 @@ const SelectableListDefault = () => {
     <div>
       <SelectableList variant="checkbox">
         <SelectableList.Item
-            id="checkbox-id-1"
             label="Mild"
             name="checkbox-name-1"
             value="1"
         />
         <SelectableList.Item
             checked
-            id="checkbox-id-2"
             label="Medium"
             name="checkbox-name-2"
             value="2"
         />
         <SelectableList.Item
-            id="checkbox-id-3"
             label="Hot"
             name="checkbox-name-3"
             value="3"
--- app/pb_kits/playbook/pb_selectable_list/docs/_selectable_list_radio.jsx
+++ app/pb_kits/playbook/pb_selectable_list/docs/_selectable_list_radio.jsx
@@ -6,20 +6,17 @@ const SelectableListDefault = () => {
     <div>
       <SelectableList variant="radio">
         <SelectableList.Item
-            id="radio-id-1"
             label="Small"
             name="radio"
             value="1"
         />
         <SelectableList.Item
             defaultChecked
-            id="radio-id-2"
             label="Medium"
             name="radio"
             value="2"
         />
         <SelectableList.Item
-            id="radio-id-3"
             label="Large"
             name="radio"
             value="3"
