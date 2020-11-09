2aa67d5e5c7afbd4d9a7fa48f6ac32ef04914387
Madison McCardle <madison.mccardle@powerhrg.com>
2020-11-09 11:36:06 -0500

    Adds spec

--- spec/pb_kits/playbook/kits/form_group_spec.rb
+++ spec/pb_kits/playbook/kits/form_group_spec.rb
@@ -2,10 +2,15 @@
 
 require_relative "../../../../app/pb_kits/playbook/pb_form_group/form_group"
 
-RSpec.describe Playbook::PbForm group::Form group do
-  subject { Playbook::PbForm group::Form group }
+RSpec.describe Playbook::PbFormGroup::FormGroup do
+  subject { Playbook::PbFormGroup::FormGroup }
 
   it { is_expected.to define_partial }
 
-  # Do not leave this file blank. Use other spec files for example tests.
+  describe "#classname" do
+    it "returns namespaced class name" do
+      expect(subject.new({}).classname).to eq "pb_form_group_kit"
+    end
+  end
+
 end
