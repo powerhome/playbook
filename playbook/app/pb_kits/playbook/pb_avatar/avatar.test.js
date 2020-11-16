d09e57a10a226a17aa3bddc2e92c57fc6bc85af3
Stephen Marshall <smarshall1980@gmail.com>
2020-11-16 14:19:13 -0600

    Check for initials on kit wrapper

--- playbook/app/pb_kits/playbook/pb_avatar/avatar.test.js
+++ playbook/app/pb_kits/playbook/pb_avatar/avatar.test.js
@@ -1,14 +1,16 @@
+// TODO: abstract away to wrapper
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
+// TODO: abstract away to wrapper
 
 import { Avatar } from '../'
 
 const imageUrl = 'https://randomuser.me/api/portraits/men/44.jpg',
-testId = 'tjohnson',
-name = 'Terry Johnson'
+      testId = 'tjohnson',
+      name = 'Terry Johnson'
 
-test('test avatar', () => {
+test('loads the given image url and name', () => {
   render(
     <Avatar
         data={{testid: testId}}
@@ -17,8 +19,11 @@ test('test avatar', () => {
     />
   )
 
-  const element = screen.getByAltText(name)
+  const kit      = screen.getByTestId(testId)
+  const image    = screen.getByAltText(name)
+  const initials = name.split(/\s/)[0].substr(0,1) + name.split(/\s/)[1].substr(0,1)
 
-  expect(element).toHaveAttribute('data-src', imageUrl)
-  expect(element).toHaveAttribute('src', imageUrl)
+  expect(kit).toHaveAttribute('data-initials', initials)
+  expect(image).toHaveAttribute('data-src', imageUrl)
+  expect(image).toHaveAttribute('src', imageUrl)
 })
