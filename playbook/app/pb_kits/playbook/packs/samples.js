bacff6167dce28b3f89a3437d9a3fe540fa0a65d
Brendan Huffman <57693937+bh247484@users.noreply.github.com>
2020-11-19 16:13:42 -0500

    [NUXE-47] Create samples categories (#1214)
    * added new samples category, updated generator
    
    * made sample categories iterable, fixed sass as needed
    
    * fixed new line linting errors
    
    * generator fix
    
    * test commit
    
    * sass and generator fixes
    
    * removed test examples, cleaned up code
    
    * fixed react comp naming convention
    
    * js import samples layout fix

--- playbook/app/pb_kits/playbook/packs/samples.js
+++ playbook/app/pb_kits/playbook/packs/samples.js
@@ -11,13 +11,13 @@ import WebpackerReact from 'webpacker-react'
 // Generated file from kit generator.
 
 import AnalyticsDashboard from '../../../views/playbook/samples/analytics_dashboard/index.jsx'
-import CRMClientList from '../../../views/playbook/samples/crm_client_list/index.jsx'
+import CrmClientList from '../../../views/playbook/samples/crm_client_list/index.jsx'
 import ConferenceStatistics from '../../../views/playbook/samples/conference_statistics/index.jsx'
 import NewsMagazine from '../../../views/playbook/samples/news_magazine/index.jsx'
 import TrendingRepositories from '../../../views/playbook/samples/trending_repositories/index.jsx'
 
 WebpackerReact.setup({ AnalyticsDashboard })
-WebpackerReact.setup({ CRMClientList })
+WebpackerReact.setup({ CrmClientList })
 WebpackerReact.setup({ ConferenceStatistics })
 WebpackerReact.setup({ NewsMagazine })
 WebpackerReact.setup({ TrendingRepositories })
--- playbook/lib/generators/sample/sample_generator.rb
+++ playbook/lib/generators/sample/sample_generator.rb
@@ -4,17 +4,36 @@ require "yaml"
 
 class SampleGenerator < Rails::Generators::NamedBase
   desc "This generator creates a new Full Page Sample"
+  source_root File.expand_path("templates", __dir__)
+  class_option :category, type: :string, default: "full_page_samples"
 
   def create_indexes
     sample_name = name.strip.downcase
     @sample_name_underscore = sample_name.parameterize.underscore
-    create_file "app/views/playbook/samples/#{@sample_name_underscore}/index.html.erb"
-    create_file "app/views/playbook/samples/#{@sample_name_underscore}/index.jsx"
+    @react_comp = @sample_name_underscore.camelize
+    template "sample_rails_index.erb", "app/views/playbook/samples/#{@sample_name_underscore}/index.html.erb"
+    template "sample_react_index.erb", "app/views/playbook/samples/#{@sample_name_underscore}/index.jsx"
   end
 
   def update_yaml
+    category = options[:category].strip.downcase.underscore
     samples_yaml = YAML.load_file("app/pb_kits/playbook/data/samples.yml")
-    samples_yaml["samples"].push(@sample_name_underscore)
+
+    if samples_yaml[category]
+      samples_yaml[category].push(@sample_name_underscore)
+    else
+      samples_yaml[category] = [@sample_name_underscore]
+    end
+
     File.open("app/pb_kits/playbook/data/samples.yml", "w") { |out| YAML.dump(samples_yaml, out) }
+  end
+
+  def js_imports
+    append_to_file("app/pb_kits/playbook/packs/samples.js") do
+      "\nimport #{@react_comp} from '../../../views/playbook/samples/#{@sample_name_underscore}/index.jsx'\n"
+    end
+    append_to_file("app/pb_kits/playbook/packs/samples.js") do
+      "WebpackerReact.setup({ #{@react_comp} })\n"
+    end
   end
 end
