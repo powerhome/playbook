d6fa988d227b967150768b43f5b7bbd59b0bb9b6
Carlos Palhares <chjunior@gmail.com>
2020-11-24 18:37:13 -0300

    Remove unused helper methods

--- playbook/app/helpers/playbook/pb_sample_helper.rb
+++ playbook/app/helpers/playbook/pb_sample_helper.rb
@@ -2,20 +2,6 @@
 
 module Playbook
   module PbSampleHelper
-    def has_sample_type?(sample, type)
-      type ||= "rails"
-      if type == "rails"
-        Dir["../../views/playbook/samples/#{sample}/*.html.erb"].empty?
-      elsif type == "react"
-        Dir["../../views/playbook/samples/#{sample}/*.jsx"].empty?
-      end
-    end
-
-    def pb_sample(sample: "", type: "rails")
-      @type = type
-      @sample = sample
-    end
-
     def read_file(filename)
       if File.file?(filename)
         File.read(filename)
