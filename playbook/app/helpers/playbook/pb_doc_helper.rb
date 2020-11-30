f40c2a822343f6c54a04d425c6e46be8d818eace
Carlos Palhares <chjunior@gmail.com>
2020-11-30 17:14:28 -0300

    Use preloaded MENU on pb doc helper

--- playbook/app/helpers/playbook/pb_doc_helper.rb
+++ playbook/app/helpers/playbook/pb_doc_helper.rb
@@ -69,8 +69,7 @@ module Playbook
     end
 
     def get_kits
-      menu = YAML.load_file("#{Playbook::Engine.root}/app/pb_kits/playbook/data/menu.yml")
-      menu["kits"]
+      MENU["kits"]
     end
 
     def pb_category_kits(category_kits: [], type: "rails")
