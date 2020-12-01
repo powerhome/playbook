0691e0f5b9533930351590ebb5cd04b6a672088a
Carlos Palhares <chjunior@gmail.com>
2020-12-01 19:55:18 -0300

    Move webpacker's webpack config to playbook-doc

--- playbook-docs/config/webpack/development.js
+++ playbook-docs/config/webpack/development.js
@@ -1,5 +1,17 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const path = require('path')
 const environment = require('./environment')
+
+environment.plugins.append(
+  'CleanWebpackPlugin',
+  new CleanWebpackPlugin({
+    root: path.resolve(__dirname, '../../public'),
+    verbose: true,
+  })
+)
+
+environment.splitChunks()
 
 module.exports = environment.toWebpackConfig()
--- playbook-docs/config/webpack/environment.js
+++ playbook-docs/config/webpack/environment.js
@@ -1,3 +1,19 @@
 const { environment } = require('@rails/webpacker')
+const svg = require('./loaders/svg')
+
+environment.loaders.insert('react-svg', svg)
+environment.loaders.insert('javascript', {
+  test: /\.(js|jsx|mjs)$/,
+  use: {
+    loader: 'babel-loader',
+    options: {
+      cacheDirectory: true,
+    },
+  },
+  exclude: /(node_modules)/,
+})
+
+const fileLoader = environment.loaders.get('file')
+fileLoader.exclude = svg.test
 
 module.exports = environment
