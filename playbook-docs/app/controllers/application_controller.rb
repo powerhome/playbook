dce7bbe938bd496ae2c65a9ebe984497987f7259
Carlos Palhares <chjunior@gmail.com>
2020-12-01 18:37:12 -0300

    Brink markdown helper back to render kits descriptions

--- playbook-docs/app/controllers/application_controller.rb
+++ playbook-docs/app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  helper ApplicationHelper
+  helper MarkdownHelper
+
   append_view_path Playbook::Engine.root + "app/pb_kits"
 end
