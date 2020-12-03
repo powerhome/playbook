a48bccc011a187a6eed6148898dd8f892fd90996
Carlos Palhares <chjunior@gmail.com>
2020-12-03 15:22:24 -0300

    Move react examples pack to playbook-docs

---
+++ playbook-docs/app/javascript/packs/react-examples.js
@@ -0,0 +1,179 @@
+import WebpackerReact from 'webpacker-react'
+
+import * as Avatar from 'playbook-ui/app/pb_kits/playbook/pb_avatar/docs'
+import * as AvatarActionButton from 'playbook-ui/app/pb_kits/playbook/pb_avatar_action_button/docs'
+import * as Background from 'playbook-ui/app/pb_kits/playbook/pb_background/docs'
+import * as Badge from 'playbook-ui/app/pb_kits/playbook/pb_badge/docs'
+import * as BarGraphDocs from 'playbook-ui/app/pb_kits/playbook/pb_bar_graph/docs'
+import * as Body from 'playbook-ui/app/pb_kits/playbook/pb_body/docs'
+import * as Button from 'playbook-ui/app/pb_kits/playbook/pb_button/docs'
+import * as ButtonToolbar from 'playbook-ui/app/pb_kits/playbook/pb_button_toolbar/docs'
+import * as Caption from 'playbook-ui/app/pb_kits/playbook/pb_caption/docs'
+import * as Card from 'playbook-ui/app/pb_kits/playbook/pb_card/docs'
+import * as Checkbox from 'playbook-ui/app/pb_kits/playbook/pb_checkbox/docs'
+import * as CircleChart from 'playbook-ui/app/pb_kits/playbook/pb_circle_chart/docs'
+import * as CircleIconButton from 'playbook-ui/app/pb_kits/playbook/pb_circle_icon_button/docs'
+import * as Contact from 'playbook-ui/app/pb_kits/playbook/pb_contact/docs'
+import * as Currency from 'playbook-ui/app/pb_kits/playbook/pb_currency/docs'
+import * as DashboardValue from 'playbook-ui/app/pb_kits/playbook/pb_dashboard_value/docs'
+import * as Date from 'playbook-ui/app/pb_kits/playbook/pb_date/docs'
+import * as DatePicker from 'playbook-ui/app/pb_kits/playbook/pb_date_picker/docs'
+import * as DateRangeInline from 'playbook-ui/app/pb_kits/playbook/pb_date_range_inline/docs'
+import * as DateRangeStacked from 'playbook-ui/app/pb_kits/playbook/pb_date_range_stacked/docs'
+import * as DateStacked from 'playbook-ui/app/pb_kits/playbook/pb_date_stacked/docs'
+import * as DateYearStacked from 'playbook-ui/app/pb_kits/playbook/pb_date_year_stacked/docs'
+import * as DistributionBarDocs from 'playbook-ui/app/pb_kits/playbook/pb_distribution_bar/docs'
+import * as DateTime from 'playbook-ui/app/pb_kits/playbook/pb_date_time/docs'
+import * as DateTimeStacked from 'playbook-ui/app/pb_kits/playbook/pb_date_time_stacked/docs'
+import * as FileUpload from 'playbook-ui/app/pb_kits/playbook/pb_file_upload/docs'
+import * as Filter from 'playbook-ui/app/pb_kits/playbook/pb_filter/docs'
+import * as FixedConfirmationToast from 'playbook-ui/app/pb_kits/playbook/pb_fixed_confirmation_toast/docs'
+import * as Flex from 'playbook-ui/app/pb_kits/playbook/pb_flex/docs'
+import * as FormGroup from 'playbook-ui/app/pb_kits/playbook/pb_form_group/docs'
+import * as FormPill from 'playbook-ui/app/pb_kits/playbook/pb_form_pill/docs'
+import * as Gauge from 'playbook-ui/app/pb_kits/playbook/pb_gauge/docs'
+import * as Hashtag from 'playbook-ui/app/pb_kits/playbook/pb_hashtag/docs'
+import * as Highlight from 'playbook-ui/app/pb_kits/playbook/pb_highlight/docs'
+import * as HomeAddressStreet from 'playbook-ui/app/pb_kits/playbook/pb_home_address_street/docs'
+import * as Icon from 'playbook-ui/app/pb_kits/playbook/pb_icon/docs'
+import * as IconCircle from 'playbook-ui/app/pb_kits/playbook/pb_icon_circle/docs'
+import * as IconStatValue from 'playbook-ui/app/pb_kits/playbook/pb_icon_stat_value/docs'
+import * as IconValue from 'playbook-ui/app/pb_kits/playbook/pb_icon_value/docs'
+import * as Image from 'playbook-ui/app/pb_kits/playbook/pb_image/docs'
+import * as LabelPill from 'playbook-ui/app/pb_kits/playbook/pb_label_pill/docs'
+import * as LabelValue from 'playbook-ui/app/pb_kits/playbook/pb_label_value/docs'
+import * as Layout from 'playbook-ui/app/pb_kits/playbook/pb_layout/docs'
+import * as LegendDocs from 'playbook-ui/app/pb_kits/playbook/pb_legend/docs'
+import * as LineGraphDocs from 'playbook-ui/app/pb_kits/playbook/pb_line_graph/docs'
+import * as List from 'playbook-ui/app/pb_kits/playbook/pb_list/docs'
+import * as LoadingInline from 'playbook-ui/app/pb_kits/playbook/pb_loading_inline/docs'
+import * as Message from 'playbook-ui/app/pb_kits/playbook/pb_message/docs'
+import * as MultipleUsers from 'playbook-ui/app/pb_kits/playbook/pb_multiple_users/docs'
+import * as MultipleUsersStacked from 'playbook-ui/app/pb_kits/playbook/pb_multiple_users_stacked/docs'
+import * as Nav from 'playbook-ui/app/pb_kits/playbook/pb_nav/docs'
+import * as OnlineStatus from 'playbook-ui/app/pb_kits/playbook/pb_online_status/docs'
+import * as PbReactPopover from 'playbook-ui/app/pb_kits/playbook/pb_popover/docs'
+import * as Person from 'playbook-ui/app/pb_kits/playbook/pb_person/docs'
+import * as PersonContact from 'playbook-ui/app/pb_kits/playbook/pb_person_contact/docs'
+import * as Pill from 'playbook-ui/app/pb_kits/playbook/pb_pill/docs'
+import * as ProgressPills from 'playbook-ui/app/pb_kits/playbook/pb_progress_pills/docs'
+import * as ProgressSimple from 'playbook-ui/app/pb_kits/playbook/pb_progress_simple/docs'
+import * as ProgressStep from 'playbook-ui/app/pb_kits/playbook/pb_progress_step/docs'
+import * as Radio from 'playbook-ui/app/pb_kits/playbook/pb_radio/docs'
+import * as RichTextEditor from 'playbook-ui/app/pb_kits/playbook/pb_rich_text_editor/docs'
+import * as SectionSeparator from 'playbook-ui/app/pb_kits/playbook/pb_section_separator/docs'
+import * as Select from 'playbook-ui/app/pb_kits/playbook/pb_select/docs'
+import * as SelectableCard from 'playbook-ui/app/pb_kits/playbook/pb_selectable_card/docs'
+import * as SelectableCardIcon from 'playbook-ui/app/pb_kits/playbook/pb_selectable_card_icon/docs'
+import * as SelectableIcon from 'playbook-ui/app/pb_kits/playbook/pb_selectable_icon/docs'
+import * as SelectableList from 'playbook-ui/app/pb_kits/playbook/pb_selectable_list/docs'
+import * as Source from 'playbook-ui/app/pb_kits/playbook/pb_source/docs'
+import * as StarRating from 'playbook-ui/app/pb_kits/playbook/pb_star_rating/docs'
+import * as StatChange from 'playbook-ui/app/pb_kits/playbook/pb_stat_change/docs'
+import * as StatValue from 'playbook-ui/app/pb_kits/playbook/pb_stat_value/docs'
+import * as Table from 'playbook-ui/app/pb_kits/playbook/pb_table/docs'
+import * as Textarea from 'playbook-ui/app/pb_kits/playbook/pb_textarea/docs'
+import * as TextInput from 'playbook-ui/app/pb_kits/playbook/pb_text_input/docs'
+import * as Time from 'playbook-ui/app/pb_kits/playbook/pb_time/docs'
+import * as Timeline from 'playbook-ui/app/pb_kits/playbook/pb_timeline/docs'
+import * as TimeStacked from 'playbook-ui/app/pb_kits/playbook/pb_time_stacked/docs'
+import * as Timestamp from 'playbook-ui/app/pb_kits/playbook/pb_timestamp/docs'
+import * as TimeRangeInline from 'playbook-ui/app/pb_kits/playbook/pb_time_range_inline/docs'
+import * as Title from 'playbook-ui/app/pb_kits/playbook/pb_title/docs'
+import * as TitleCount from 'playbook-ui/app/pb_kits/playbook/pb_title_count/docs'
+import * as TitleDetail from 'playbook-ui/app/pb_kits/playbook/pb_title_detail/docs'
+import * as Typeahead from 'playbook-ui/app/pb_kits/playbook/pb_typeahead/docs'
+import * as Toggle from 'playbook-ui/app/pb_kits/playbook/pb_toggle/docs'
+import * as User from 'playbook-ui/app/pb_kits/playbook/pb_user/docs'
+import * as UserBadge from 'playbook-ui/app/pb_kits/playbook/pb_user_badge/docs'
+import * as WeekdayStacked from 'playbook-ui/app/pb_kits/playbook/pb_weekday_stacked/docs'
+
+WebpackerReact.setup({
+  ...Avatar,
+  ...AvatarActionButton,
+  ...Background,
+  ...Badge,
+  ...BarGraphDocs,
+  ...Body,
+  ...Button,
+  ...ButtonToolbar,
+  ...Caption,
+  ...Card,
+  ...Checkbox,
+  ...CircleChart,
+  ...CircleIconButton,
+  ...Contact,
+  ...Currency,
+  ...DashboardValue,
+  ...Date,
+  ...DatePicker,
+  ...DateRangeInline,
+  ...DateRangeStacked,
+  ...DateStacked,
+  ...DateYearStacked,
+  ...DistributionBarDocs,
+  ...DateTime,
+  ...DateTimeStacked,
+  ...FileUpload,
+  ...Filter,
+  ...FixedConfirmationToast,
+  ...Flex,
+  ...FormGroup,
+  ...FormPill,
+  ...Gauge,
+  ...Hashtag,
+  ...Highlight,
+  ...HomeAddressStreet,
+  ...Icon,
+  ...IconCircle,
+  ...IconStatValue,
+  ...IconValue,
+  ...Image,
+  ...LabelPill,
+  ...LabelValue,
+  ...Layout,
+  ...LegendDocs,
+  ...LineGraphDocs,
+  ...List,
+  ...LoadingInline,
+  ...Message,
+  ...MultipleUsers,
+  ...MultipleUsersStacked,
+  ...Nav,
+  ...OnlineStatus,
+  ...PbReactPopover,
+  ...Person,
+  ...PersonContact,
+  ...Pill,
+  ...ProgressPills,
+  ...ProgressSimple,
+  ...ProgressStep,
+  ...Radio,
+  ...RichTextEditor,
+  ...SectionSeparator,
+  ...Select,
+  ...SelectableCard,
+  ...SelectableCardIcon,
+  ...SelectableIcon,
+  ...SelectableList,
+  ...Source,
+  ...StarRating,
+  ...StatChange,
+  ...StatValue,
+  ...Table,
+  ...Textarea,
+  ...TextInput,
+  ...Time,
+  ...Timeline,
+  ...TimeStacked,
+  ...Timestamp,
+  ...TimeRangeInline,
+  ...Title,
+  ...TitleCount,
+  ...TitleDetail,
+  ...Typeahead,
+  ...Toggle,
+  ...User,
+  ...UserBadge,
+  ...WeekdayStacked,
+})
