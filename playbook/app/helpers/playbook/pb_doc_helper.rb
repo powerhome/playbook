e3e1aba72e2e03256b7d67c8454815e6cee5a525
Carlos Palhares <chjunior@gmail.com>
2020-12-07 16:40:35 -0300

    Remove unused Playbook::PbKit::Base

--- playbook/app/helpers/playbook/pb_doc_helper.rb
+++ playbook/app/helpers/playbook/pb_doc_helper.rb
@@ -99,11 +99,7 @@ module Playbook
 
     def pb_kit_api(kit)
       kit_class_obj = get_class_name(kit.to_s)
-      @kit_api = if kit_class_obj < Playbook::PbKit::Base
-                   kit_class_obj.instance_method(:initialize).parameters.map(&:last)
-                 else
-                   kit_class_obj.props.keys
-                 end
+      @kit_api = kit_class_obj.props.keys
       render partial: "playbook/config/pb_kit_api"
     end
 
