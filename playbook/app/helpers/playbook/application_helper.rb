eaeb857fe2040d00339f8c3935aefc93e1624f60
Carlos Palhares <chjunior@gmail.com>
2020-12-08 18:31:33 -0300

    Remove remaining webpacker instance reference

--- playbook/app/helpers/playbook/application_helper.rb
+++ playbook/app/helpers/playbook/application_helper.rb
@@ -10,10 +10,6 @@ module Playbook
     include ::Webpacker::Helper
     include ::Webpacker::React::Helpers
 
-    def current_webpacker_instance
-      Playbook.webpacker
-    end
-
     def dark_mode
       if cookies[:dark_mode] == "true"
         true
