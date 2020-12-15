b67c677ee90603367ea659c6d2d5fd560bbcca5c
Liam Tinney <ljtinney@gmail.com>
2020-12-15 13:37:45 -0500

    Add Props to Background Kits

--- playbook/app/pb_kits/playbook/pb_background/docs/_background_light.jsx
+++ playbook/app/pb_kits/playbook/pb_background/docs/_background_light.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Background } from '../..'
 
-const BackgroundLight = () => (
+const BackgroundLight = (props) => (
   <Background
       backgroundColor="light"
       padding="xl"
+      {...props}
   />
 )
 
