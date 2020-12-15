5f8f1df9dac18af8f34220022fb4cf43cdbbf285
Liam Tinney <ljtinney@gmail.com>
2020-12-15 14:27:29 -0500

    Move Props to Top of Date React Kits

--- playbook/app/pb_kits/playbook/pb_date/docs/_date_alignment.jsx
+++ playbook/app/pb_kits/playbook/pb_date/docs/_date_alignment.jsx
@@ -5,28 +5,28 @@ const DateAlignment = (props) => {
   return (
     <div>
       <FormattedDate
+          {...props}
           dayOfWeek
           icon
           value="1995-12-25"
-          {...props}
       />
 
       <br />
 
       <FormattedDate
+          {...props}
           alignment="center"
           dayOfWeek
           icon
           value="2020-12-25"
-          {...props}
       />
 
       <br />
 
       <FormattedDate
+          {...props}
           alignment="right"
           value={new Date()}
-          {...props}
       />
     </div>
   )
