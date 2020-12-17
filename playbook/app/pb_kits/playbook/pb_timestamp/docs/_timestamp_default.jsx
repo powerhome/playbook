eff42ac090ddacbfbecd2cf7d83d2f6478c30b84
Stephen McKeon <stephen.mckeon@powerhrg.com>
2020-12-17 16:54:31 -0500

    Revert change

--- playbook/app/pb_kits/playbook/pb_timestamp/docs/_timestamp_default.jsx
+++ playbook/app/pb_kits/playbook/pb_timestamp/docs/_timestamp_default.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
-import { Timestamp } from '../../'
+import Timestamp from '../_timestamp.jsx'
 
-const TimestampDefault = () => (
-  <Timestamp text="20 seconds ago" />
-)
+const TimestampDefault = (props) => {
+  return (
+    <div>
+      <Timestamp
+          align="left"
+          showDate={false}
+          timestamp={new Date().getTime()}
+          {...props}
+      />
+
+      <br />
+
+      <Timestamp
+          align="left"
+          showDate
+          timestamp={new Date().getTime()}
+          {...props}
+      />
+
+      <br />
+
+      <Timestamp
+          align="left"
+          showDate
+          timestamp={new Date((new Date()).getFullYear() + 4, (new Date().getMonth()), (new Date().getDate()), (new Date().getHours()), (new Date().getMinutes()))}
+          {...props}
+      />
+    </div>
+  )
+}
 
 export default TimestampDefault
