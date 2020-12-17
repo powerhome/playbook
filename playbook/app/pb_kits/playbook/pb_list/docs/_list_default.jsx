80538f1a7a8ade59110834affd9026daf8233b1b
Liam Tinney <ljtinney@gmail.com>
2020-12-17 14:46:29 -0500

    Add Props to PB List Kit React Examples

--- playbook/app/pb_kits/playbook/pb_list/docs/_list_default.jsx
+++ playbook/app/pb_kits/playbook/pb_list/docs/_list_default.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 
 import { List, ListItem } from '../../'
 
-const ListDefault = () => {
+const ListDefault = (props) => {
   return (
     <>
       <List>
         <ListItem>{'Item 1'}</ListItem>
         <ListItem>{'Item 2'}</ListItem>
         <ListItem>{'Item 3'}</ListItem>
+        {...props}
       </List>
     </>
   )
