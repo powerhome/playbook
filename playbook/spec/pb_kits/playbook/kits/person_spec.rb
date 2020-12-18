111deef6640350c61e25cdd988c68232f341a4de
Carlos Palhares <chjunior@gmail.com>
2020-12-18 00:48:27 -0300

    Convert Person kit to view_component

--- playbook/spec/pb_kits/playbook/kits/person_spec.rb
+++ playbook/spec/pb_kits/playbook/kits/person_spec.rb
@@ -5,7 +5,6 @@ require_relative "../../../../app/pb_kits/playbook/pb_person/person"
 RSpec.describe Playbook::PbPerson::Person do
   subject { Playbook::PbPerson::Person }
 
-  it { is_expected.to define_partial }
   it { is_expected.to define_prop(:first_name) }
   it { is_expected.to define_prop(:last_name) }
 
