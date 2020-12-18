e7be5a0e1faf64ea0c0b3676e7cb52c52673ebdb
Carlos Palhares <chjunior@gmail.com>
2020-12-18 14:55:53 -0300

    Convert StatChange kit to view_component

--- playbook/spec/pb_kits/playbook/kits/stat_change_spec.rb
+++ playbook/spec/pb_kits/playbook/kits/stat_change_spec.rb
@@ -5,7 +5,6 @@ require_relative "../../../../app/pb_kits/playbook/pb_stat_change/stat_change"
 RSpec.describe Playbook::PbStatChange::StatChange do
   subject { Playbook::PbStatChange::StatChange }
 
-  it { is_expected.to define_partial }
   it { is_expected.to define_prop(:value).of_type(Playbook::Props::Numeric) }
   it { is_expected.to define_enum_prop(:change)
                    .with_default("neutral")
