95f81540338c7fe236dbc4621839aea10e9d95c8
Liam Tinney <ljtinney@gmail.com>
2020-12-18 12:56:44 -0500

    Add Props to PB Nav Kit React Examples

--- playbook/app/pb_kits/playbook/pb_nav/docs/_new_tab.jsx
+++ playbook/app/pb_kits/playbook/pb_nav/docs/_new_tab.jsx
@@ -2,31 +2,36 @@ import React from 'react'
 import { Nav } from '../../'
 import NavItem from '../_item.jsx'
 
-const NewTab = () => (
+const NewTab = (props) => (
   <Nav
       link="#"
       orientation="vertical"
       title="Popular Websites"
+      {...props}
   >
     <NavItem
         link="https://www.google.com/"
         target="_blank"
         text="Google"
+        {...props}
     />
     <NavItem
         link="https://www.youtube.com/"
         target="_blank"
         text="YouTube"
+        {...props}
     />
     <NavItem
         link="https://www.facebook.com/"
         target="_blank"
         text="Facebook"
+        {...props}
     />
     <NavItem
         link="https://www.amazon.com/"
         target="_blank"
         text="Amazon"
+        {...props}
     />
   </Nav>
 )
