10e8cdcf788699ac33b8763a284995b9ddd6d3a7
Andrew Kloecker <lt.kvothe@gmail.com>
2020-12-21 09:33:40 -0500

    linter fixes

--- playbook/app/pb_kits/playbook/pb_rich_text_editor/docs/_rich_text_editor_preview.jsx
+++ playbook/app/pb_kits/playbook/pb_rich_text_editor/docs/_rich_text_editor_preview.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
-import { Title } from '../../'
+import React, { useState } from 'react'
+import {
+  Button,
+  Card,
+  RichTextEditor,
+} from '../../'
 
-const RichTextEditorPreview = () => (
-  <Title text="test" />
-)
+const RichTextEditorPreview = () => {
+  const [showPreview, setShowPreview] = useState(false)
+  const [previewText, setPreviewText] = useState(<div />)
+
+  const handleChange = (event) => setPreviewText(event)
+  const handleClick = () => {
+    setShowPreview(true)
+  }
+
+  return (
+    <div>
+      <RichTextEditor
+          id="content-preview-editor"
+          onChange={handleChange}
+      />
+      <If condition={showPreview}>
+        <Card marginY="md">
+          <div
+              // eslint-disable-next-line react/no-danger
+              dangerouslySetInnerHTML={{ __html: previewText }}
+              id="preview-content"
+          />
+        </Card>
+        <Else />
+        <div />
+      </If>
+      <Button
+          id="preview-button"
+          onClick={handleClick}
+          text="Preview Output"
+          variant="secondary"
+      />
+    </div>
+  )
+}
 export default RichTextEditorPreview
