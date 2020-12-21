4756669ef1c3f1ad7e2d290dd4b4a1e91617fd18
Carlos Palhares <chjunior@gmail.com>
2020-12-21 18:35:06 -0300

    Introduce form/form_with and form/simple_form view_component kits

---
+++ playbook/app/pb_kits/playbook/pb_form/form_with.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+# require_dependency "app/pb_kits/playbook/pb_form/form_builder"
+
+module Playbook
+  module PbForm
+    class FormWith < Playbook::KitBase
+      class FormBuilder < ::ActionView::Helpers::FormBuilder
+        include ::Playbook::PbForm::FormBuilder
+      end
+
+      prop :options, type: Playbook::Props::Hash, default: {}
+      prop :validate, type: Playbook::Props::Boolean, default: false
+
+      def data
+        prop(:data).merge("pb-form-validation" => validate)
+      end
+
+      def options
+        Hash(prop(:options))
+      end
+
+      def classname
+        [generate_classname("pb-form"), options[:class]].join(" ")
+      end
+
+      def form_builder
+        ::Playbook::PbForm::FormWith::FormBuilder
+      end
+
+      def render_in(view_context, &_block)
+        super(view_context, &nil)
+      end
+    end
+  end
+end
