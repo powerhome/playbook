3135e6fc05f8a99e0266594321b70d2783ed430c
Jasper Furniss <jasper.furniss@powerhrg.com>
2020-12-22 12:40:38 -0500

    Starting RB

--- playbook/app/pb_kits/playbook/pb_dialog/dialog.rb
+++ playbook/app/pb_kits/playbook/pb_dialog/dialog.rb
@@ -6,6 +6,36 @@ module Playbook
       include Playbook::Props
 
       partial "pb_dialog/dialog"
+
+      prop :aria?: object
+      prop :cancelButton?: string
+      prop :children: array<React.ReactNode> | React.ReactNode | string
+      prop :className?: string
+      prop :closeable: boolean
+      prop :confirmButton?: string
+      prop :data?: object
+      prop :id?: string
+      prop :onCancel?: () => void
+      prop :onChange?: () => void
+      prop :onClose?: () => void
+      prop :onConfirm?: () => void
+      prop :opened: boolean
+      prop :size?: "sm" | "md" | "lg" | "content"
+      prop :text?: string
+      prop :title?: string
+
+      def dialog_options
+        {
+          id: id,
+          className: classname,
+          focus: focus,
+          simple: simple,
+          sticky: sticky,
+          value: value,
+          template: template,
+          placeholder: placeholder
+        }
+      end
     end
   end
 end
