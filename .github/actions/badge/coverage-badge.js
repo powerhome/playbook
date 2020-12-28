eaeaa73d38bfdc18939778e2d722c7fd8cd10eea
Stephen Marshall <smarshall1980@gmail.com>
2020-12-28 10:09:12 -0600

    Updates

--- .github/actions/badge/coverage-badge.js
+++ .github/actions/badge/coverage-badge.js
@@ -1,20 +1,43 @@
 
-const core = require("@actions/core");
-// const { execSync } = require("child_process");
-const { GitHub, context } = require("@actions/github");
+const { readFileSync } = require('fs'),
+  { join } = require('path');
+
+const core = require("@actions/core"),
+  github = require("@actions/github"),
+  context = github.context;
 
 const main = async () => {
-  const repoName = context.repo.repo,
-    repoOwner = context.repo.owner,
-    githubToken = core.getInput("github-token"),
-    coverageJSON = core.getInput("coverage-json");
+  const githubToken = core.getInput('github-token'),
+    githubRepo = core.getInput('github-repo').split('/'),
+    minCovPct = core.getInput('minimum-coverage-pct'),
+    octokit = github.getOctokit(githubToken),
+    pullNumber = core.getInput('pull-number'),
+    repoName = context.repo.repo,
+    repoOwner = context.repo.owner;
 
-  // const githubClient = new GitHub(githubToken);
+  const coverageInput = readFileSync(join(__dirname, core.getInput('coverage-summary-file'))),
+    coverageJSON = JSON.parse(coverageInput),
+    { lines, statements, functions } = coverageJSON.total,
+    totalAveCovPct = parseFloat((lines.pct + statements.pct + functions.pct) / 3).toFixed(2),
+    badgeColor = parseInt(minCovPct) > totalAveCovPct ? 'red' : 'brightgreen';
 
-  // coveragePercentage = parseFloat(coveragePercentage).toFixed(2);
-  console.log(coverageJSON)
+  const CREDS = {
+    owner: githubRepo[0],
+    repo: githubRepo[1],
+    pull_number: pullNumber,
+  }
 
-  // https://img.shields.io/badge/React%20Kit%20Coverage-90%25-brightgreen
+  core.info('Hello!');
+
+  const pr = await octokit.pulls.get(CREDS);
+
+  const { body } = pr.data,
+    badgeTitle = core.getInput('badge-title'),
+    badge = `![${badgeTitle}](https://img.shields.io/badge/${encodeURI(badgeTitle)}-${totalAveCovPct}%25-${badgeColor})`,
+    badgeSubst = new RegExp('(\\!\\[' + badgeTitle + '\\]\\().+\\)/g'),
+    updatedBody = body.indexOf(badgeTitle) > -1 ? body.replace(badgeSubst, `${badge}`) : `${badge}\r${body}`;
+
+  await octokit.pulls.update({...CREDS, body: updatedBody});
 };
 
 main().catch(err => core.setFailed(err.message));
