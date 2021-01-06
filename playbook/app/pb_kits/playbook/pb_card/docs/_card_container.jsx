6bf597ec9dd576dc77f0dce7267388073f177da2
Jo Fischer <josephinefischer9@gmail.com>
2021-01-06 16:12:11 -0500

    Fix lint errors

--- playbook/app/pb_kits/playbook/pb_card/docs/_card_container.jsx
+++ playbook/app/pb_kits/playbook/pb_card/docs/_card_container.jsx
@@ -4,10 +4,30 @@ import Card from '../_card.jsx'
 const CardLight = (props) => {
   return (
     <div>
-      <Card {...props} container="sm">{'Container Small'}</Card>
-      <Card {...props} container="md">{'Container Medium'}</Card>
-      <Card {...props} container="lg">{'Container Large'}</Card>
-      <Card {...props} container="xl">{'Container Extra Large'}</Card>
+      <Card
+          {...props}
+          container="sm"
+      >
+        {'Container Small'}
+      </Card>
+      <Card
+          {...props}
+          container="md"
+      >
+        {'Container Medium'}
+      </Card>
+      <Card
+          {...props}
+          container="lg"
+      >
+        {'Container Large'}
+      </Card>
+      <Card
+          {...props}
+          container="xl"
+      >
+        {'Container Extra Large'}
+      </Card>
     </div>
   )
 }
