dbe2fa3524185c3c07cc90ef2db0ad4633d55e37
Jon Zazula <djonyriz@gmail.com>
2021-01-07 17:20:44 -0500

    Dashboard cards (#1296)
    * fixing dashboard cards
    
    * updates
    
    * Fixes genrator error (#1278)
    
    * Revert "Fixes genrator error (#1278)" (#1285)
    
    This reverts commit d7ae6982d79dca683da1ac9bdd7a72e2c2319002.
    
    * Added fixes and style tags
    
    * Fixed React Dashobaord Cards
    
    * Fix linter errors
    
    * yarn lint fixes
    
    Co-authored-by: Andrew Kloecker <lt.kvothe@gmail.com>
    Co-authored-by: Jason Cypret <jason@jasoncypret.com>
    Co-authored-by: Stephen Marshall <smarshall1980@gmail.com>

--- playbook/app/pb_kits/playbook/packs/samples.js
+++ playbook/app/pb_kits/playbook/packs/samples.js
@@ -6,7 +6,7 @@ import ConferenceStatistics from '../../../views/playbook/samples/conference_sta
 import NewsMagazine from '../../../views/playbook/samples/news_magazine/index.jsx'
 import TrendingRepositories from '../../../views/playbook/samples/trending_repositories/index.jsx'
 import CrowdsourcedPosts from '../../../views/playbook/samples/crowdsourced_posts/index.jsx'
-import CardDashboard from '../../../views/playbook/samples/card_dashboards/index.jsx'
+import CardDashboard from '../../../views/playbook/samples/card_dashboard/index.jsx'
 
 WebpackerReact.setup({
   AnalyticsDashboard,
