c6006ff7d2a1c86ad7678d6b01c21e3b676b25bf
Stephen McKeon <stephen.mckeon@powerhrg.com>
2021-01-07 17:32:08 -0500

    [NUXE-363] UX - KIT DEV - Add character counter prop to Textarea Kit (#1277)
    * Implement character counter for react
    
    * Updated char counter error to be on same line as counter
    
    * Implement basic character count for rails
    
    * Implement rails docs examples

--- playbook/app/pb_kits/playbook/pb_textarea/textarea.rb
+++ playbook/app/pb_kits/playbook/pb_textarea/textarea.rb
@@ -19,9 +19,16 @@ module Playbook
       prop :rows, type: Playbook::Props::Number,
             default: 4
       prop :value
+      prop :character_count
+      prop :onkeyup
+      prop :max_characters
 
       def classname
         generate_classname("pb_textarea_kit") + error_class + resize_class
+      end
+
+      def character_counter
+        max_characters && character_count ? "#{character_count} / #{max_characters}" : character_count
       end
 
     private
