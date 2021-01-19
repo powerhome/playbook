c656ae07ef7a2f132cef72a50353b8e361183800
Jo Fischer <josephinefischer9@gmail.com>
2021-01-19 15:57:34 -0500

    Fix lint errors

--- playbook/app/pb_kits/playbook/pb_textarea/docs/_textarea_auto_resize.jsx
+++ playbook/app/pb_kits/playbook/pb_textarea/docs/_textarea_auto_resize.jsx
@@ -2,10 +2,9 @@ import React from 'react'
 import { Textarea } from '../../'
 
 const TextareaAutoResize = (props) => {
-
   const handleKeyDown = (e) => {
-    e.target.style.height = 'inherit';
-    e.target.style.height = `${e.target.scrollHeight}px`;
+    e.target.style.height = 'inherit'
+    e.target.style.height = `${e.target.scrollHeight}px`
   }
 
   return (
