d404bc30d9837d61a9d134c51826041c6a624361
Jo Fischer <josephinefischer9@gmail.com>
2021-01-21 11:32:29 -0500

    More WIP

--- playbook/app/pb_kits/playbook/pb_textarea/index.js
+++ playbook/app/pb_kits/playbook/pb_textarea/index.js
@@ -6,14 +6,26 @@ export default class PbTextarea extends PbEnhancedElement {
     return '.resize_auto textarea'
   }
 
-  onInput() {
-    this.style.height = 'auto';
-    this.style.height = (this.scrollHeight) + 'px';
-  }
+  // onInput() {
+  //   this.style.height = 'auto';
+  //   this.style.height = (this.scrollHeight) + 'px';
+  // }
 
   connect() {
-    console.log(this.element)
-    this.element.setAttribute('style', 'height:' + (this.element.scrollHeight) + 'px;overflow-y:hidden;');
-    this.element.addEventListener("input", this.onInput, false);
+console.log(this.element)
+    const textareas = document.querySelectorAll('.resize_auto textarea');
+
+    function OnInput() {
+      this.style.height = 'auto';
+      this.style.height = (this.scrollHeight) + 'px';
+    }
+
+    for (let i = 0; i < textareas.length; i++) {
+      textareas[i].setAttribute('style', 'height:' + (textareas[i].scrollHeight) + 'px;overflow-y:hidden;');
+      textareas[i].addEventListener("input", OnInput, false);
+    }
+    // console.log(this.element)
+    // this.element.setAttribute('style', 'height:' + (this.element.scrollHeight) + 'px;overflow-y:hidden;');
+    // this.element.addEventListener("input", this.onInput, false);
   }
 }
