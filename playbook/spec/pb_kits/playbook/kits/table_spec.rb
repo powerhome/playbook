631fb9df6685195125b9fe165ebfc0b2af06f27a
Josephine Fischer <64423298+josephinef9@users.noreply.github.com>
2021-01-21 12:36:26 -0500

    Add Table Collapse prop for specific breakpoints (#1329)
    * WIP responsive props
    
    * Make wording consistent
    
    * Rename prop and get table headers on collapse for react
    
    * Fix lint errors
    
    * Fix for spec
    
    * Fix spec again and different wording
    
    * Change wording again
    
    * Fix lint errors

--- playbook/spec/pb_kits/playbook/kits/table_spec.rb
+++ playbook/spec/pb_kits/playbook/kits/table_spec.rb
@@ -25,15 +25,15 @@ RSpec.describe Playbook::PbTable::Table do
 
   describe "#classname" do
     it "returns namespaced class name", :aggregate_failures do
-      expect(subject.new({}).classname).to eq "pb_table table-md table-card table-responsive-collapse"
-      expect(subject.new(classname: "additional_class").classname).to eq "pb_table table-md table-card table-responsive-collapse additional_class"
-      expect(subject.new(dark: true).classname).to eq "pb_table table-md table-dark table-card table-responsive-collapse dark"
-      expect(subject.new(data_table: true).classname).to eq "pb_table table-md table-card data_table table-responsive-collapse"
-      expect(subject.new(size: "sm").classname).to eq "pb_table table-sm table-card table-responsive-collapse"
-      expect(subject.new(single_line: true).classname).to eq "pb_table table-md single-line table-card table-responsive-collapse"
-      expect(subject.new(container: false).classname).to eq "pb_table table-md table-responsive-collapse"
-      expect(subject.new(disable_hover: true).classname).to eq "pb_table table-md no-hover table-card table-responsive-collapse"
-      expect(subject.new(disable_hover: true, dark: true, size: "lg", single_line: true).classname).to eq "pb_table table-lg single-line table-dark no-hover table-card table-responsive-collapse dark"
+      expect(subject.new({}).classname).to eq "pb_table table-md table-card table-collapse-sm table-responsive-collapse"
+      expect(subject.new(classname: "additional_class").classname).to eq "pb_table table-md table-card table-collapse-sm table-responsive-collapse additional_class"
+      expect(subject.new(dark: true).classname).to eq "pb_table table-md table-dark table-card table-collapse-sm table-responsive-collapse dark"
+      expect(subject.new(data_table: true).classname).to eq "pb_table table-md table-card data_table table-collapse-sm table-responsive-collapse"
+      expect(subject.new(size: "sm").classname).to eq "pb_table table-sm table-card table-collapse-sm table-responsive-collapse"
+      expect(subject.new(single_line: true).classname).to eq "pb_table table-md single-line table-card table-collapse-sm table-responsive-collapse"
+      expect(subject.new(container: false).classname).to eq "pb_table table-md table-collapse-sm table-responsive-collapse"
+      expect(subject.new(disable_hover: true).classname).to eq "pb_table table-md no-hover table-card table-collapse-sm table-responsive-collapse"
+      expect(subject.new(disable_hover: true, dark: true, size: "lg", single_line: true).classname).to eq "pb_table table-lg single-line table-dark no-hover table-card table-collapse-sm table-responsive-collapse dark"
     end
   end
 end
