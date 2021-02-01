e901e08bbb940c82b3b0120ced8cd89f43bb9484
Brendan Huffman <bh247484@gmail.com>
2021-02-01 13:22:51 -0500

    Remove jquery js include tags, add vanilla js toggle solution

--- playbook/app/pb_kits/playbook/packs/site.js
+++ playbook/app/pb_kits/playbook/packs/site.js
@@ -1,20 +1,41 @@
 // Move to separate file
-window.addEventListener('DOMContentLoaded', function() {
-  document.querySelectorAll('[data-toggle]').forEach(function(toggle) {
-    toggle.addEventListener('click', function(e) {
-      e.preventDefault()
+// window.addEventListener('DOMContentLoaded', function() {
+//   document.querySelectorAll('[data-toggle]').forEach(function(toggle) {
+//     toggle.addEventListener('click', function(e) {
+//       e.preventDefault()
 
-      var kitContainer = window.$(this).closest('.pb--doc')
-      var toggleTarget = window.$(this).data('toggle')
+//       var kitContainer = window.$(this).closest('.pb--doc')
+//       var toggleTarget = window.$(this).data('toggle')
 
-      window
-        .$(kitContainer)
-        .find('[data-action="toggle"]')
-        .hide()
-      window
-        .$(kitContainer)
-        .find('[data-togglable="' + toggleTarget + '"]')
-        .show()
-    })
+//       window
+//         .$(kitContainer)
+//         .find('[data-action="toggle"]')
+//         .hide()
+//       window
+//         .$(kitContainer)
+//         .find('[data-togglable="' + toggleTarget + '"]')
+//         .show()
+//     })
+//   })
+// })
+
+window.addEventListener('DOMContentLoaded', ()=> {
+  document.querySelectorAll('#code-wrapper').forEach((wrapper)=> {
+    const openToggle = wrapper.querySelector('#toggle-open')
+    const closeToggle = wrapper.querySelector('#toggle-close')
+    const codeDrawer = wrapper.querySelector('.pb--codeCopy')
+    
+    const toggleLambda = (element) => {
+      if(element.style.display === "none") {
+        element.style.display = "block"
+      } else { 
+        element.style.display = "none"
+      }
+    }
+
+    // openToggle.addEventListener('click', (e)=> toggleLambda(wrapper.querySelector('.pb--codeCopy')))
+    // closeToggle.addEventListener('click', (e)=> toggleLambda(wrapper.querySelector('.pb--codeCopy')))
+    openToggle.addEventListener('click', ()=> toggleLambda(codeDrawer))
+    closeToggle.addEventListener('click', ()=> toggleLambda(codeDrawer))
   })
 })
