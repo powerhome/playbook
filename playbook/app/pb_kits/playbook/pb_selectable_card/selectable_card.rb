27227f6d1508bd5d6263691998ab005236fa54bf
Aaron Wilson <adub65@gmail.com>
2021-02-02 11:22:36 -0500

    Add error to selectable_card.rb

--- playbook/app/pb_kits/playbook/pb_selectable_card/selectable_card.rb
+++ playbook/app/pb_kits/playbook/pb_selectable_card/selectable_card.rb
@@ -11,6 +11,8 @@ module Playbook
                      default: false
       prop :disabled, type: Playbook::Props::Boolean,
                       default: false
+      prop :error, type: Playbook::Props::Boolean,
+                   default: false
       prop :icon, type: Playbook::Props::Boolean,
                   default: false
       prop :multi, type: Playbook::Props::Boolean,
@@ -27,7 +29,7 @@ module Playbook
 
       def classname
         [
-          generate_classname_without_spacing("pb_selectable_card_kit", checked_class, enable_disabled_class),
+          generate_classname_without_spacing("pb_selectable_card_kit", checked_class, enable_disabled_class) + error_class,
           dark_props,
         ].compact.join(" ")
       end
@@ -63,6 +65,10 @@ module Playbook
         disabled ? "disabled" : ""
       end
 
+      def status
+        error ? "negative" : nil
+      end
+
     private
 
       def checked_class
@@ -71,6 +77,10 @@ module Playbook
 
       def enable_disabled_class
         disabled ? "disabled" : "enabled"
+      end
+
+      def error_class
+        error ? " error" : ""
       end
     end
   end
