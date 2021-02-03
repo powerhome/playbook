e72c272f92f5714e2c527e8e63544b53375ad7f2
Pat McClernan <patmccler@gmail.com>
2021-02-03 15:44:50 -0500

    Add some prelim examples

--- playbook/app/pb_kits/playbook/pb_text_input/docs/_text_input_password_strength.jsx
+++ playbook/app/pb_kits/playbook/pb_text_input/docs/_text_input_password_strength.jsx
@@ -13,6 +13,30 @@ const TextInputPasswordStrength = (props) => {
           variant="passwordStrength"
           {...props}
       />
+      <TextInput
+          label="Password"
+          onChange={(e) => setValue(e.target.value)}
+          placeholder="Enter a password"
+          value={value}
+          variant="passwordStrength1"
+          {...props}
+      />
+      <TextInput
+          label="Password"
+          onChange={(e) => setValue(e.target.value)}
+          placeholder="Enter a password"
+          value={value}
+          variant="passwordStrength2"
+          {...props}
+      />
+      <TextInput
+          label="Password"
+          onChange={(e) => setValue(e.target.value)}
+          placeholder="Enter a password"
+          value={value}
+          variant="passwordStrength3"
+          {...props}
+      />
     </div>
   )
 }
