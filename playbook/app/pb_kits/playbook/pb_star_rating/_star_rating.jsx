85bf581dd659e20bbb39b18ff259d2796d8f5143
Brendan Huffman <bh247484@gmail.com>
2021-02-10 14:52:51 -0500

    Remove trailing space

--- playbook/app/pb_kits/playbook/pb_star_rating/_star_rating.jsx
+++ playbook/app/pb_kits/playbook/pb_star_rating/_star_rating.jsx
@@ -92,7 +92,7 @@ const StarRating = ({
           <Icon
               fixedWidth={false}
               icon="star"
-          /> 
+          />
           <Icon
               fixedWidth={false}
               icon="star"
