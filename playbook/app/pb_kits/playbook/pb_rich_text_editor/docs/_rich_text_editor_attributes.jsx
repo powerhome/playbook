fe8900c7c998ee8bf955f29a16e7caf5944e14a8
Cole Erikson <coleerikson@users.noreply.github.com>
2021-02-11 20:20:25 -0300

    Add data, aria, & name props to RichTextEditor (#1352)
    * Add data, aria, name props to richtext
    
    * Add Jest test to richtext

---
+++ playbook/app/pb_kits/playbook/pb_rich_text_editor/docs/_rich_text_editor_attributes.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { RichTextEditor } from '../../'
+
+const RichTextEditorAttributes = (props) => (
+  <div>
+    <RichTextEditor
+        aria={{ label: 'rich textarea' }}
+        data={{ key: 'value', key2: 'value2' }}
+        name="name-attribute"
+        {...props}
+    />
+  </div>
+)
+
+export default RichTextEditorAttributes
