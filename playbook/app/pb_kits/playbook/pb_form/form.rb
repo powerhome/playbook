bc21689e47a3f2afe13eea6e213ebacd19e77850
Carlos Palhares <carlos.palhares@powerhrg.com>
2021-02-17 19:06:23 -0300

    Introduce form view component (#1313)
    * Introduce form/form_with and form/simple_form view_component kits
    
    * Convert Form kit to view_component
    
    * Remove pb form partial path spec that doesn't make sense anymore
    
    * Remove invalid form specs
    
    * Refactor ActionArea into a kit
    
    * Fix form_with implementation
    
    * Remove unnecessary capture block
    
    * Add validation data attribute to simple form

--- playbook/app/pb_kits/playbook/pb_form/form.rb
+++ playbook/app/pb_kits/playbook/pb_form/form.rb
@@ -1,28 +1,26 @@
 # frozen_string_literal: true
 
-# require_dependency "app/pb_kits/playbook/pb_form/form_builder"
-
 module Playbook
   module PbForm
-    class Form
-      include Playbook::Props
-
-      clear_props
+    class Form < ::Playbook::KitBase
       prop :form_system, type: Playbook::Props::Enum,
                          values: %w[form_with simple_form],
                          default: "form_with"
       prop :form_system_options, type: Playbook::Props::Base
-      prop :children, type: Playbook::Props::Proc
       prop :validate, type: Playbook::Props::Boolean, default: false
 
-      delegate :to_partial_path, to: :specific_form
-      delegate :merged_form_system_options, to: :specific_form
-      delegate :form_builder, to: :specific_form
+      def form_system_options
+        form_options = prop(:form_system_options)
+        if form_options.is_a?(::Array)
+          model, options = *form_options
+          (options || {}).merge(model: model)
+        else
+          form_options
+        end
+      end
 
-    private
-
-      def specific_form
-        @specific_form ||= "#{self.class}::#{form_system.classify}Form".constantize.new(self)
+      def render_in(view_context, &_block)
+        super(view_context, &nil)
       end
     end
   end
---
+++ playbook/app/pb_kits/playbook/pb_form/form_with.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module Playbook
+  module PbForm
+    class FormWith < Playbook::KitBase
+      class FormBuilder < ::ActionView::Helpers::FormBuilder
+        include ::Playbook::PbForm::FormBuilder
+      end
+
+      prop :options, type: Playbook::Props::Hash, default: {}
+      prop :validate, type: Playbook::Props::Boolean, default: false
+
+      def data
+        prop(:data).merge("pb-form-validation" => validate)
+      end
+
+      def options
+        { url: "" }.merge(Hash(prop(:options)))
+      end
+
+      def classname
+        [generate_classname("pb-form"), options[:class]].join(" ")
+      end
+
+      def form_builder
+        ::Playbook::PbForm::FormWith::FormBuilder
+      end
+
+      def render_in(view_context, &_block)
+        super(view_context, &nil)
+      end
+    end
+  end
+end
