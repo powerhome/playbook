9eae55ec0645621b72236e27f079df058a0b304a
Jason Cypret <jason@jasoncypret.com>
2021-02-23 14:45:57 -0600

    Update CHANGELOG.md

--- playbook/app/pb_kits/playbook/pb_form/form_builder/action_area.rb
+++ playbook/app/pb_kits/playbook/pb_form/form_builder/action_area.rb
@@ -3,21 +3,28 @@
 module Playbook
   module PbForm
     module FormBuilder
-      class ActionArea < Playbook::KitBase
-        prop :submit_default_value, type: Playbook::Props::String
+      class ActionArea
+        def initialize(view_context, submit_default_value)
+          self.view_context = view_context
+          self.submit_default_value = submit_default_value
+        end
 
         def submit(value = nil, props: {})
           props[:type] ||= "submit"
+          props[:text] ||= value || submit_default_value
+
           button(value, props: props)
         end
 
         def button(value = nil, props:)
-          props[:text] ||= value || submit_default_value
-
-          content_tag(:li) do
-            pb_rails("button", props: props)
+          view_context.content_tag(:li) do
+            view_context.pb_rails("button", props: props)
           end
         end
+
+      private
+
+        attr_accessor :view_context, :submit_default_value
       end
     end
   end
