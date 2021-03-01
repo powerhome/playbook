0461d69e0eddc9c532864b84ccbb0059c90b54c1
Cole Erikson <cole.erikson@powerhrg.com>
2021-03-01 14:43:56 -0300

    Add indeterminate checkbox option to kits

--- playbook/app/pb_kits/playbook/pb_checkbox/_checkbox.jsx
+++ playbook/app/pb_kits/playbook/pb_checkbox/_checkbox.jsx
@@ -1,6 +1,6 @@
 /* @flow */
 
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Body from '../pb_body/_body.jsx'
 import Icon from '../pb_icon/_icon.jsx'
 import { buildAriaProps, buildCss, buildDataProps } from '../utilities/props'
@@ -16,6 +16,7 @@ type CheckboxProps = {
   data?: object,
   error?: boolean,
   id?: string,
+  indeterminate?: boolean,
   name: string,
   onChange: (boolean) => void,
   tabIndex: number,
@@ -33,6 +34,7 @@ const Checkbox = (props: CheckboxProps) => {
     data = {},
     error = false,
     id,
+    indeterminate = false,
     name = '',
     onChange = () => {},
     tabIndex,
@@ -40,13 +42,20 @@ const Checkbox = (props: CheckboxProps) => {
     value = '',
   } = props
 
+  const checkRef = useRef()
   const dataProps = buildDataProps(data)
   const ariaProps = buildAriaProps(aria)
   const classes = classnames(
-    buildCss('pb_checkbox_kit', { checked, error }),
+    buildCss('pb_checkbox_kit', { checked, error, indeterminate }),
     globalProps(props),
     className
   )
+
+  useEffect(() => {
+    if (checkRef.current) {
+      checkRef.current.indeterminate = indeterminate
+    }
+  }, [indeterminate])
 
   return (
     <label
@@ -62,19 +71,32 @@ const Checkbox = (props: CheckboxProps) => {
             defaultChecked={checked}
             name={name}
             onChange={onChange}
+            ref={checkRef}
             tabIndex={tabIndex}
             type="checkbox"
             value={value}
         />
       </If>
+      <If condition={!indeterminate}>
+        <span className="pb_checkbox_checkmark">
+          <Icon
+              className="check_icon"
+              fixedWidth
+              icon="check"
+          />
+        </span>
+      </If>
 
-      <span className="pb_checkbox_checkmark">
-        <Icon
-            className="check_icon"
-            fixedWidth
-            icon="check"
-        />
-      </span>
+      <If condition={indeterminate}>
+        <span className="pb_checkbox_indeterminate">
+          <Icon
+              className="indeterminate_icon"
+              fixedWidth
+              icon="minus"
+          />
+        </span>
+      </If>
+
       <Body
           className="pb_checkbox_label"
           dark={dark}
