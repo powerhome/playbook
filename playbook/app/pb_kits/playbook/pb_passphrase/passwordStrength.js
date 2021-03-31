0c4d0ed968f7dc601c7c2b433310596eec0b6ac5
Pat McClernan <patmccler@gmail.com>
2021-03-31 14:04:55 -0400

    Add min length support

--- playbook/app/pb_kits/playbook/pb_passphrase/passwordStrength.js
+++ playbook/app/pb_kits/playbook/pb_passphrase/passwordStrength.js
@@ -1,34 +1,41 @@
 import zxcvbn from 'zxcvbn'
 
 export const zxcvbnPasswordScore = (options) => {
-  const { calculate = zxcvbn } = options
+  const {
+    calculate = zxcvbn,
+    averageThreshold = 2,
+    strongThreshold = 3,
+    minLength = 12,
+  } = options
 
   return {
-    averageThreshold: options.averageThreshold || 2,
-    strongThreshold: options.strongThreshold || 3,
+    minLength,
+    averageThreshold,
+    strongThreshold,
     test: function (password = '') {
       const feedbackValues = (str) => {
-        let percent = 0
-        let variant = 'default'
+        let percent = '0'
+        let variant = 'negative'
         let text = ''
 
-        if (str > 0 && str < this.averageThreshold) {
-          percent = 25
-          variant = 'negative'
-          text = 'Weak Passphrase'
-        } else if (str >= this.averageThreshold && str < this.strongThreshold){
-          percent = 50
-          variant = 'warning'
-          text = 'Average Passphrase'
-        } else if (str >= this.strongThreshold) {
-          percent = 100
-          variant = 'positive'
-          text = 'Strong Passphrase'
+        if (password.length > 0) {
+          if (password.length < this.minLength || str > 0 && str < this.averageThreshold) {
+            percent = '25'
+            variant = 'negative'
+            text = 'Weak Passphrase'
+          } else if (str >= this.averageThreshold && str < this.strongThreshold){
+            percent = '50'
+            variant = 'warning'
+            text = 'Average Passphrase'
+          } else if (str >= this.strongThreshold) {
+            percent = '100'
+            variant = 'positive'
+            text = 'Strong Passphrase'
+          }
         }
-        return { percent: percent.toString(), variant, text }
+        return { percent, variant, text }
       }
 
-      if (password.length === 0) return {}
       const result = calculate(password)
 
       return (
