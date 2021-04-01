cbd1089151a180e6c417a08f1a1cec7146b4d497
Pat McClernan <patmccler@gmail.com>
2021-04-01 14:21:54 -0400

    Add callback for strength changing

--- playbook/app/pb_kits/playbook/pb_passphrase/docs/_passphrase_default.jsx
+++ playbook/app/pb_kits/playbook/pb_passphrase/docs/_passphrase_default.jsx
@@ -5,6 +5,9 @@ const PassphraseDefault = (props) => {
   const [input, setInput] = useState('')
 
   const handleChange = (e) => setInput(e.target.value)
+
+  const [strength, setStrength] = useState(0)
+  const handleStrengthChange = (str) => setStrength(str)
   return (
     <>
       <div>
@@ -16,6 +19,7 @@ const PassphraseDefault = (props) => {
       </div>
       <div>
         <Passphrase
+            marginBottom="xs"
             onChange={handleChange}
             tips={['Use a bunch of letters', 'Make it less bad', 'Try typing in a bunch of letters to make it really long, kind of like how long this tip is']}
             value={input}
@@ -25,11 +29,23 @@ const PassphraseDefault = (props) => {
       <div>
         <Passphrase
             confirmation
+            inputProps={{ name: 'TEST' }}
             onChange={handleChange}
             tips={['Use a bunch of letters', 'Make it less bad', 'Try typing in a bunch of letters to make it really long, kind of like how long this tip is']}
             value={input}
             {...props}
         />
+      </div>
+      <div>
+        <Passphrase
+            inputProps={{ name: 'TEST' }}
+            onChange={handleChange}
+            strengthChanged={handleStrengthChange}
+            tips={['Use a bunch of letters', 'Make it less bad', 'Try typing in a bunch of letters to make it really long, kind of like how long this tip is']}
+            value={input}
+            {...props}
+        />
+        <span>{`Current strength is ${strength}`}</span>
       </div>
     </>
   )
