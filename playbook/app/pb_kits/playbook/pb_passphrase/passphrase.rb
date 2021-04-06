b820df0d594ade029b5fb68a0126c0a45bcfd6b0
Pat McClernan <patmccler@gmail.com>
2021-04-06 07:53:24 -0400

    Add basic props to rails kit

--- playbook/app/pb_kits/playbook/pb_passphrase/passphrase.rb
+++ playbook/app/pb_kits/playbook/pb_passphrase/passphrase.rb
@@ -2,7 +2,35 @@
 
 module Playbook
   module PbPassphrase
-    class Passphrase < ::Playbook::KitBase
+    class Passphrase < Playbook::KitBase
+      prop :average_threshold
+      prop :confirmation, type: Playbook::Props::Boolean, default: false
+      prop :input_props, type: Playbook::Props::HashArray, default: []
+      prop :label
+      prop :min_length
+      prop :show_tips_below
+      prop :strong_threshold
+      prop :tips, type: Playbook::Props::Array, default: []
+
+      # might not need these?
+      # prop :common
+      # prop :on_strength_change
+      # prop :on_change
+
+      def passphrase_options
+        {
+          dark: dark,
+          id: id,
+          averageThreshold: average_threshold,
+          confirmation: confirmation,
+          inputProps: input_props,
+          label: label,
+          minLength: min_length,
+          showTipsBelow: show_tips_below,
+          strongThreshold: strong_threshold,
+          tips: tips,
+        }.compact
+      end
     end
   end
 end
