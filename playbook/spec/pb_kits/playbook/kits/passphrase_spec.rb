7379f5c2f9a187896743f8074d80e042b62dadc2
Pat McClernan <patmccler@gmail.com>
2021-04-06 14:05:20 -0400

    Update passphrase spec

--- playbook/spec/pb_kits/playbook/kits/passphrase_spec.rb
+++ playbook/spec/pb_kits/playbook/kits/passphrase_spec.rb
@@ -5,7 +5,45 @@ require_relative "../../../../app/pb_kits/playbook/pb_passphrase/passphrase"
 RSpec.describe Playbook::PbPassphrase::Passphrase do
   subject { Playbook::PbPassphrase::Passphrase }
 
-  # it { is_expected.to define_partial }
+  it { is_expected.to define_prop(:average_threshold) }
+  it { is_expected.to define_prop(:confirmation).with_default(false) }
+  it { is_expected.to define_prop(:input_props).with_default({}) }
+  it { is_expected.to define_prop(:label) }
+  it { is_expected.to define_prop(:min_length) }
+  it { is_expected.to define_prop(:show_tips_below) }
+  it { is_expected.to define_prop(:strong_threshold) }
+  it { is_expected.to define_prop(:tips).with_default([]) }
 
-  # Do not leave this file blank. Use other spec files for example tests.
+  describe "passphrase_options" do
+    it "returns base props", :aggregate_failures do
+      base_example = subject.new
+      expect(base_example.passphrase_options[:uncontrolled]).to be true
+      expect(base_example.passphrase_options[:confirmation]).to be false
+    end
+
+    it "passes the props given along" do
+      custom_example = subject.new(
+        average_threshold: 1,
+        dark: true,
+        id: "some-id",
+        label: "A Label",
+        min_length: 8,
+        show_tips_below: "lg",
+        strong_threshold: 4,
+        tips: ["some tips to show"]
+      )
+
+      expect(custom_example.passphrase_options[:averageThreshold]).to be 1
+      expect(custom_example.passphrase_options[:confirmation]).to be false
+      expect(custom_example.passphrase_options[:dark]).to be true
+      expect(custom_example.passphrase_options[:id]).to be "some-id"
+      expect(custom_example.passphrase_options[:label]).to be "A Label"
+      expect(custom_example.passphrase_options[:minLength]).to be 8
+      expect(custom_example.passphrase_options[:showTipsBelow]).to be "lg"
+      expect(custom_example.passphrase_options[:strongThreshold]).to be 4
+      expect(custom_example.passphrase_options[:tips]).to eq ["some tips to show"]
+
+      expect(custom_example.passphrase_options[:input_props]).to be nil
+    end
+  end
 end
