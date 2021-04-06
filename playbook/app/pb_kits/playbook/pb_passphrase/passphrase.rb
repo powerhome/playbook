49f536565990917da804257ed3b3e56ae04c0150
Pat McClernan <patmccler@gmail.com>
2021-04-06 11:59:52 -0400

    Add other rails docs

--- playbook/app/pb_kits/playbook/pb_passphrase/passphrase.rb
+++ playbook/app/pb_kits/playbook/pb_passphrase/passphrase.rb
@@ -5,16 +5,16 @@ module Playbook
     class Passphrase < Playbook::KitBase
       prop :average_threshold
       prop :confirmation, type: Playbook::Props::Boolean, default: false
-      prop :input_props, type: Playbook::Props::HashArray, default: []
+      prop :input_props, type: Playbook::Props::Hash, default: {}
       prop :label
       prop :min_length
+      prop :on_strength_change
       prop :show_tips_below
       prop :strong_threshold
       prop :tips, type: Playbook::Props::Array, default: []
 
       # might not need these?
       # prop :common
-      # prop :on_strength_change
       # prop :on_change
 
       def passphrase_options
@@ -26,6 +26,7 @@ module Playbook
           inputProps: input_props,
           label: label,
           minLength: min_length,
+          onStrengthChange: on_strength_change,
           showTipsBelow: show_tips_below,
           strongThreshold: strong_threshold,
           tips: tips,
