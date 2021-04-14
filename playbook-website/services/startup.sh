a579e46c28e17cb88415528a38d1fbcb0d7d9425
Carlos Palhares <chjunior@gmail.com>
2021-04-14 13:43:16 -0300

    Fix startup/precompile script

--- playbook-website/services/startup.sh
+++ playbook-website/services/startup.sh
@@ -1,6 +1,8 @@
-if [ $1 = "disable" ] ; then
+#!/usr/bin/env bash
+
+if [[ $1 == "disable" ]] ; then
   echo "=========== Precompile disabled"
 else
-  echo 'argument supplied but different from not, precompiling'
-  cd RAILS_ENV=production SECRET_KEY_BASE=does_not_matter_here bin/rails assets:precompile
+  echo 'argument supplied but different from "disable", precompiling'
+  RAILS_ENV=production SECRET_KEY_BASE=does_not_matter_here bin/rails assets:precompile
 fi
