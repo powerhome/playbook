894411b94354934400b5a20432fb207ab966d952
Kelly Ryan <kellyeryan@gmail.com>
2021-04-16 10:26:35 -0400

    Linting

--- playbook/app/pb_kits/playbook/pb_fixed_confirmation_toast/_fixed_confirmation_toast.jsx
+++ playbook/app/pb_kits/playbook/pb_fixed_confirmation_toast/_fixed_confirmation_toast.jsx
@@ -27,7 +27,7 @@ const FixedConfirmationToast = (props: FixedConfirmationToastProps) => {
   const { className, closeable = false, multiLine = false, status = 'neutral', text } = props
   const css = classnames(
     `pb_fixed_confirmation_toast_kit_${status}`,
-    {'_multi_line' : multiLine},
+    { '_multi_line': multiLine },
     globalProps(props),
     className
   )
