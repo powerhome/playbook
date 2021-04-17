444645226d3ea1ab57a37f66ccf5bb0586f9b911
Carlos Palhares <chjunior@gmail.com>
2021-04-17 02:55:34 -0300

    Remove Terser plugin from webpack
    Filesizes don't differ from each other, and compilation was faster

--- playbook-website/config/webpack/production.js
+++ playbook-website/config/webpack/production.js
@@ -1,15 +1,7 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
-const TerserPlugin = require('terser-webpack-plugin')
 
 environment.config.merge({ devtool: 'none' })
-environment.optimization = {
-  minimize: true,
-  minimizer: [new TerserPlugin({
-    cache: true,
-    exclude: /(node_modules|playbook-ui)/
-  })],
-}
 
 module.exports = environment.toWebpackConfig()
