879a5e064c64fb37defcd832a5527879ba732a51
Carlos Palhares <chjunior@gmail.com>
2021-04-19 15:54:19 -0300

    Move samples to playbook-website

--- playbook-website/config/initializers/global_variables.rb
+++ playbook-website/config/initializers/global_variables.rb
@@ -1,2 +1,2 @@
 MENU = YAML.load_file(Playbook::Engine.root.join("app/pb_kits/playbook/data/menu.yml"))
-SAMPLES = YAML.load_file(Playbook::Engine.root.join("app/pb_kits/playbook/data/samples.yml"))
+SAMPLES = YAML.load_file(Rails.root.join("config/samples.yml"))
