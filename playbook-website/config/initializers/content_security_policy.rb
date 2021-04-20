9d2a8b20f43c6fbec121cf4d9284e976f1b088ff
Carlos Palhares <chjunior@gmail.com>
2021-04-20 17:01:05 -0300

    Enable HMR for development

--- playbook-website/config/initializers/content_security_policy.rb
+++ playbook-website/config/initializers/content_security_policy.rb
@@ -3,13 +3,15 @@ Rails.application.config.content_security_policy do |p|
   p.font_src    :self, :https, :data, :unsafe_inline
   p.img_src     :self, :https, :data, :unsafe_inline
   p.object_src  :none
-  p.script_src  :self, :https, :unsafe_eval
   p.style_src   :self, :https, :unsafe_inline
 
   # To allow connections to the webpack-dev-server running in
   # a separate docker container
   if Rails.env.development?
+    p.script_src  :self, :https, :unsafe_eval
     p.connect_src :self, :https, 'http://localhost:3035', 'ws://localhost:3035', 'http://0.0.0.0:3035', 'ws://0.0.0.0:3035'
+  else
+    p.script_src  :self, :https, :unsafe_inline
   end
 
   # Specify URI for violation reports
