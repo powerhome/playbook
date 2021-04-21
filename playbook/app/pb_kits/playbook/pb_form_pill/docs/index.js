1d23cdc22f45242cb3303fac097ae790dc99b7be
Brendan Huffman <bh247484@gmail.com>
2021-04-21 11:51:49 -0400

    Create Form Pill size prop

--- playbook/app/pb_kits/playbook/pb_form_pill/docs/index.js
+++ playbook/app/pb_kits/playbook/pb_form_pill/docs/index.js
@@ -1,2 +1,3 @@
 export { default as FormPillUser } from './_form_pill_user.jsx'
+export { default as FormPillSize } from './_form_pill_size.jsx'
 export { default as FormPillTag } from './_form_pill_tag.jsx'
