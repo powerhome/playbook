9f54900d50fea1bc8850211b273b4f4d8afdb52e
Brendan Huffman <bh247484@gmail.com>
2021-05-14 11:33:21 -0400

    Add alignment prop to radio kit and styles to match

--- playbook/app/pb_kits/playbook/pb_radio/docs/_radio_alignment.jsx
+++ playbook/app/pb_kits/playbook/pb_radio/docs/_radio_alignment.jsx
@@ -5,20 +5,25 @@ const RadioAlignment = () => {
   return (
     <Flex>
       <Radio
+          alignment="vertical"
           label="Power"
+          marginRight="sm"
           name="Group2"
           tabIndex={0}
           value="Power"
       />
       <br />
       <Radio
+          alignment="vertical"
           defaultChecked={false}
           label="Nitro"
+          marginRight="sm"
           name="Group2"
           value="Nitro"
       />
       <br />
       <Radio
+          alignment="vertical"
           defaultChecked={false}
           label="Google"
           name="Group2"
