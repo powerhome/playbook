1d53bede2106a8e270d703b30fc66832da13be9c
Carlos Palhares <chjunior@gmail.com>
2021-05-18 19:02:01 -0300

    Avoid error when importing Trix

--- playbook/app/pb_kits/playbook/pb_rich_text_editor/_rich_text_editor.jsx
+++ playbook/app/pb_kits/playbook/pb_rich_text_editor/_rich_text_editor.jsx
@@ -3,9 +3,16 @@
 import React, { useEffect, useRef } from 'react'
 import classnames from 'classnames'
 import useFocus from './useFocus'
-import Trix from 'trix'
 import { globalProps } from '../utilities/globalProps'
 import { buildAriaProps, buildDataProps } from '../utilities/props'
+
+try {
+  const Trix = require('trix')
+  Trix.config.textAttributes.inlineCode = {
+    tagName: 'code',
+    inheritable: true,
+  }
+} catch (_e) { /* do nothing */ }
 
 type RichTextEditorProps = {
   aria?: object,
@@ -46,11 +53,6 @@ const RichTextEditor = (props: RichTextEditorProps) => {
   const dataProps = buildDataProps(data)
 
   useEffect(() => {
-    Trix.config.textAttributes.inlineCode = {
-      tagName: 'code',
-      inheritable: true,
-    }
-
     trixRef.current.addEventListener('trix-initialize', (event) => {
       const element = event.target
 
