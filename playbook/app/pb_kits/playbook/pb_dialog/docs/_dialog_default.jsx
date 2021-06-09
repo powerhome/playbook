76f99494c996a71acb5d072e49c662a6f82a7b11
Steve Landwehr <steve.landwehr@powerhrg.com>
2021-06-09 04:59:33 -0700

    Add the value of a Dialog's portalClassName prop to the React Modal portal DIV class attribute (#1507)

--- playbook/app/pb_kits/playbook/pb_dialog/docs/_dialog_default.jsx
+++ playbook/app/pb_kits/playbook/pb_dialog/docs/_dialog_default.jsx
@@ -11,11 +11,13 @@ const DialogDefault = () => {
       <Button onClick={open}>{'Open Dialog'}</Button>
       <Dialog
           cancelButton="Cancel"
+          className="wrapper"
           confirmButton="Okay"
           onCancel={close}
           onClose={close}
           onConfirm={close}
           opened={isOpen}
+          portalClassName="portal"
           size="sm"
           text="Hello Body Text, Nice to meet ya."
           title="Header Title is the Title Prop"
