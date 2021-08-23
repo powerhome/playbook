e7611d8c1f4f09586124e240d9fc9cd96e9c67df
derrick lannaman <derrick.lannaman@powerhrg.com>
2021-08-23 16:40:12 -0500

    [BOP-1024] Implement Inline Date Picker (React)

---
+++ playbook/app/pb_kits/playbook/pb_date_picker/docs/_date_picker_inline.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import DatePicker from '../_date_picker'
+
+const DatePickerInline = (props) => {
+  const selectDateHandler = (dateSelected) => {
+    if (dateSelected) {
+      document.querySelector('.inline-date-picker').classList.add('show-angle-down-icon')
+    }
+  }
+
+  return (
+    <div>
+      <DatePicker
+          className='inline-date-picker'
+          hideIcon
+          inLine
+          onChange={selectDateHandler}
+          pickerId='date-picker-inline'
+          {...props}
+      />
+    </div>
+  )
+}
+
+export default DatePickerInline
