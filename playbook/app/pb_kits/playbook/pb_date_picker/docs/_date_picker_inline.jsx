8a4085bbe46b7af2f7f91d29c52c52403846d7b9
derrick lannaman <derrick.lannaman@powerhrg.com>
2021-08-24 16:10:47 -0500

    [BOP-1024] Prefer double quotes

--- playbook/app/pb_kits/playbook/pb_date_picker/docs/_date_picker_inline.jsx
+++ playbook/app/pb_kits/playbook/pb_date_picker/docs/_date_picker_inline.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
+import React from "react"
 
-import DatePicker from '../_date_picker'
+import DatePicker from "../_date_picker"
 
 const DatePickerInline = (props) => {
   const selectDateHandler = (dateSelected) => {
     if (dateSelected) {
-      document.querySelector('.inline-date-picker').classList.add('show-angle-down-icon')
+      document.querySelector(".inline-date-picker").classList.add("show-angle-down-icon")
     }
   }
 
   return (
     <div>
       <DatePicker
-          className='inline-date-picker'
+          className="inline-date-picker"
           hideIcon
           inLine
           onChange={selectDateHandler}
-          pickerId='date-picker-inline'
+          pickerId="date-picker-inline"
           {...props}
       />
     </div>
