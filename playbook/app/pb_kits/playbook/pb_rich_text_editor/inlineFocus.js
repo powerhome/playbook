7acfb922995543b42a15aaaa111181c863b9bf21
Steve Landwehr <steve.landwehr@powerhrg.com>
2021-08-26 09:18:44 -0700

    Fix React Rich Text Editor to better support inline version (#1591)
    * changes for inline React Rich Text Editor kit
    
    * change placeholder color

---
+++ playbook/app/pb_kits/playbook/pb_rich_text_editor/inlineFocus.js
@@ -0,0 +1,10 @@
+const inlineFocus = () => {
+  const trixEditorElement = event.target
+  const trixEditorContainer = trixEditorElement.closest('.pb_rich_text_editor_kit')
+
+  if (!trixEditorContainer.classList.contains('inline')) return
+
+  trixEditorContainer.classList.toggle('focused')
+}
+
+export default inlineFocus
