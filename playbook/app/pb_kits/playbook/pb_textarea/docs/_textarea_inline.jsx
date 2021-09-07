fa6ae8f9db6a63ce790aa8a595619cf6ea7eedfa
Idan Zonshein <idan.zonshein@powerhrg.com>
2021-09-07 13:34:29 -0400

    Finish up visual and css updates for inline prop

--- playbook/app/pb_kits/playbook/pb_textarea/docs/_textarea_inline.jsx
+++ playbook/app/pb_kits/playbook/pb_textarea/docs/_textarea_inline.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Textarea from '../_textarea'
 
 const TextareaInline = (props) => {
+  const [value, setValue] = useState('Try clicking into this text.')
+  const handleChange = (event) => {
+    setValue(event.target.value)
+  }
   return (
     <div>
       <Textarea
           inline
+          onChange={(e) => handleChange(e)}
+          resize="auto"
           rows={1}
-          value="Try clicking into this text."
+          value={value}
           {...props}
       />
     </div>
