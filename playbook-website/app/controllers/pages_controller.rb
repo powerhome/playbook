c346c170e56c861a179c64c3c962dc56957bc85e
Jason Cypret <jason@jasoncypret.com>
2021-11-22 15:54:42 -0600

    Fixed spacing and layout for Number Spacing

--- playbook-website/app/controllers/pages_controller.rb
+++ playbook-website/app/controllers/pages_controller.rb
@@ -76,6 +76,7 @@ class PagesController < ApplicationController
 
   def visual_guidelines
     @max_width_code_example = Rails.root.join("app/views/pages/code_snippets/width_jsx.txt").read
+    @number_spacing_code_example = Rails.root.join("app/views/pages/code_snippets/number_spacing_jsx.txt").read
   end
 
 private
