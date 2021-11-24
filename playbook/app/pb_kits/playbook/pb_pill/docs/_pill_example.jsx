9c311527291c126dec09102da4a461b71bea3f94
batistaDev1113 <yuniorbatista1113@gmail.com>
2021-11-24 17:49:59 -0500

    pill examples have been added

---
+++ playbook/app/pb_kits/playbook/pb_pill/docs/_pill_example.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Pill from '../_pill'
+
+const PillExample = () => {
+  return (
+    <div>
+      <Pill
+          text="Success"
+          textTransform="none"
+          variant="success"
+      />
+    </div>
+  )
+}
+
+export default PillExample
