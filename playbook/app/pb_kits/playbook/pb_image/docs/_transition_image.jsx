f8dcc7551073d629dcddea7f4467bb8081ded2e0
Paul Park <paulpark@pauls-mbp.lan>
2021-12-15 12:10:17 -0500

    added image transition fix - rails & react

--- playbook/app/pb_kits/playbook/pb_image/docs/_transition_image.jsx
+++ playbook/app/pb_kits/playbook/pb_image/docs/_transition_image.jsx
@@ -4,23 +4,35 @@ import Image from '../_image'
 import Select from '../../pb_select/_select'
 import FlexItem from '../../pb_flex/_flex_item'
 import Flex from '../../pb_flex/_flex'
+import Button from '../../pb_button/_button'
 
 const TransitionImage = (props) => {
   const [transition, setTransition] = useState('')
-  const handleTransition = ({ target }) => setTransition(target.value)
+  const [apply, setApply] = useState({
+    url: '',
+    transition: '',
+  })
+
+  const loadImage = () => {
+    setApply({
+      url: 'https://unsplash.it/500/400/?image=634',
+      transition: transition,
+    })
+  }
+
+  const handleTransition = ({ target }) => {
+    setTransition(target.value)
+  }
 
   const options = [
     {
       value: 'fade',
-      text: 'fade',
     },
     {
       value: 'blur',
-      text: 'blur',
     },
     {
       value: 'scale',
-      text: 'scale',
     },
   ]
 
@@ -36,17 +48,26 @@ const TransitionImage = (props) => {
               {...props}
           />
         </FlexItem>
+        <FlexItem>
+          <Button
+              disabled={transition === '' ? true : false}
+              marginLeft="sm"
+              onClick={loadImage}
+              text="Load Image"
+              {...props}
+          />
+        </FlexItem>
       </Flex>
-      <Image
-          alt="picture of a misty forest"
-          transition={transition}
-          url="https://unsplash.it/500/400/?image=634"
-          {...props}
-      />
-      <br />
+      <div style={{ display: apply.url === '' ? 'none' : 'block' }}>
+        <Image
+            alt="picture of a misty forest"
+            transition={apply.transition}
+            url={apply.url}
+            {...props}
+        />
+      </div>
     </>
   )
 }
 
 export default TransitionImage
-
