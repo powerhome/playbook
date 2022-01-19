7aed016e7377a9fe73be4e069668dc6581cd5edf
Rob <robert.gentile@powerhrg.com>
2022-01-19 09:21:16 -0500

    Set default value in title kit

--- playbook/app/pb_kits/playbook/pb_title/title.rb
+++ playbook/app/pb_kits/playbook/pb_title/title.rb
@@ -4,8 +4,8 @@ module Playbook
   module PbTitle
     class Title < Playbook::KitBase
       prop :color, type: Playbook::Props::Enum,
-                   values: %w[default light lighter success error link],
-                   default: "default"
+                   values: [nil, "default", "light", "lighter", "success", "error", "link"],
+                   default: nil
       prop :size, type: Playbook::Props::Enum,
                   values: [1, 2, 3, 4],
                   default: 3
