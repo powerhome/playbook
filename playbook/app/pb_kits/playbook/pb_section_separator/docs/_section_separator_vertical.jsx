2be25271ef9ad0bd6e21ce2f34082a4e0636ca3b
Caitlin Vargas <caitlin.vargas@powerhrg.com>
2022-01-20 15:49:05 -0500

    doc done

--- playbook/app/pb_kits/playbook/pb_section_separator/docs/_section_separator_vertical.jsx
+++ playbook/app/pb_kits/playbook/pb_section_separator/docs/_section_separator_vertical.jsx
@@ -7,28 +7,72 @@ import FlexItem from '../../pb_flex/_flex_item'
 
 const SectionSeparatorVertical = (props) => {
   return (
-    <Flex
-        inline="flex-container"
-        vertical="stretch"
-    >
-      <FlexItem>
+    <div>
+      <Flex
+          inline="flex-container"
+          marginBottom="lg"
+          marginLeft="sm"
+          vertical="stretch"
+      >
+        <FlexItem>
+          {'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'}
+        </FlexItem>
+        <SectionSeparator
+            {...props}
+            orientation="vertical"
+        />
+        <FlexItem>
+          {'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'}
+        </FlexItem>
+        <SectionSeparator
+            {...props}
+            orientation="vertical"
+        />
+        <FlexItem>
+          {'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'}
+        </FlexItem>
+      </Flex>
+      <br />
+
+      <Flex
+          inline="flex-container"
+          marginBottom="lg"
+          vertical="stretch"
+      >
+        <SectionSeparator
+            {...props}
+            orientation="vertical"
+        />
+        <div style={{ height: '50px' }}>
+          {'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'}
+          {'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'}
+          {'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'}
+          {'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'}
+          {'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'}
+        </div>
+      </Flex>
+      <br />
+
+      <Flex
+          inline="flex-container"
+          marginTop="lg"
+          vertical="stretch"
+      >
+        <SectionSeparator
+            {...props}
+            orientation="vertical"
+        />
         {'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'}
-      </FlexItem>
-      <SectionSeparator
-          {...props}
-          orientation="vertical"
-      />
-      <FlexItem>
         {'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'}
-      </FlexItem>
-      <SectionSeparator
-          {...props}
-          orientation="vertical"
-      />
-      <FlexItem>
         {'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'}
-      </FlexItem>
-    </Flex>
+        {'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'}
+        {'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'}
+        {'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'}
+        {'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'}
+        {'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'}
+      </Flex>
+    </div>
+
   )
 }
 
---
+++ playbook/app/pb_kits/playbook/pb_section_separator/docs/_section_separator_vertical.md
@@ -0,0 +1,6 @@
+The first example shows Section Seperator inside the `flex kit`, and between `flex_item` kits.
+
+Second example shows Section Separator in the container with ` fixed height`.
+
+Third example shows Section Separator in the container `without fixed height`.
+  
