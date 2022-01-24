9c61fb2708268298fdcaa78b0634b22e2fc09126
Kyle Schechter <kyle.schechter@powerhrg.com>
2022-01-24 15:02:50 -0500

    Starting over

--- playbook/app/pb_kits/playbook/pb_text_input/_text_input.jsx
+++ playbook/app/pb_kits/playbook/pb_text_input/_text_input.jsx
@@ -75,9 +75,6 @@ const TextInput = (props: TextInputProps, ref: React.ElementRef<"input">) => {
     globalProps(props),
     className,
   ])
-  const generateUniqueKey = (id) => {
-    return `text_input_${id}_${ new Date().getTime() }`
-  }
   const addOnIcon = (
     <Icon
         className="add-on-icon"
@@ -107,7 +104,6 @@ const TextInput = (props: TextInputProps, ref: React.ElementRef<"input">) => {
       <Flex
           className={`add-on-${addOnAlignment} ${borderCss}`}
           inline="flex-container"
-          key={generateUniqueKey(id)}
           vertical="center"
       >
         <If condition={addOnAlignment == 'left'}>
