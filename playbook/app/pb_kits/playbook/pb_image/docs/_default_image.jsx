a03c8db2265ffcf7223292d662b754231f800eae
Kyle Schechter <kyle.schechter@powerhrg.com>
2022-01-24 10:35:35 -0500

    Removed div and adjusted React docs

--- playbook/app/pb_kits/playbook/pb_image/docs/_default_image.jsx
+++ playbook/app/pb_kits/playbook/pb_image/docs/_default_image.jsx
@@ -5,41 +5,54 @@ import Image from '../_image'
 const DefaultImage = (props) => {
   return (
     <>
-      <Image
-          alt="picture of a misty forest"
-          size="xs"
-          url="https://unsplash.it/500/400/?image=634"
-          {...props}
-      />
-      <Image
-          alt="picture of a misty forest"
-          size="sm"
-          url="https://unsplash.it/500/400/?image=634"
-          {...props}
-      />
-      <Image
-          alt="picture of a misty forest"
-          size="md"
-          url="https://unsplash.it/500/400/?image=634"
-          {...props}
-      />
-      <Image
-          alt="picture of a misty forest"
-          size="lg"
-          url="https://unsplash.it/500/400/?image=634"
-          {...props}
-      />
-      <Image
-          alt="picture of a misty forest"
-          size="xl"
-          url="https://unsplash.it/500/400/?image=634"
-          {...props}
-      />
-      <Image
-          alt="picture of a misty forest"
-          url="https://unsplash.it/500/400/?image=634"
-          {...props}
-      />
+      <br />
+      <div>
+        <Image
+            alt="picture of a misty forest"
+            size="xs"
+            url="https://unsplash.it/500/400/?image=634"
+            {...props}
+        />
+      </div>
+      <div>
+        <Image
+            alt="picture of a misty forest"
+            size="sm"
+            url="https://unsplash.it/500/400/?image=634"
+            {...props}
+        />
+      </div>
+      <div>
+        <Image
+            alt="picture of a misty forest"
+            size="md"
+            url="https://unsplash.it/500/400/?image=634"
+            {...props}
+        />
+      </div>
+      <div>
+        <Image
+            alt="picture of a misty forest"
+            size="lg"
+            url="https://unsplash.it/500/400/?image=634"
+            {...props}
+        />
+      </div>
+      <div>
+        <Image
+            alt="picture of a misty forest"
+            size="xl"
+            url="https://unsplash.it/500/400/?image=634"
+            {...props}
+        />
+      </div>
+      <div>
+        <Image
+            alt="picture of a misty forest"
+            url="https://unsplash.it/500/400/?image=634"
+            {...props}
+        />
+      </div>
     </>
   )
 }
