70b50d6dcb51280d75a58c8eb46a6012c8487543
jon1735 <jonathan.gonzalez@powerhrg.com>
2022-01-31 11:07:54 -0500

    Fixing transition spec

--- playbook/spec/pb_kits/playbook/kits/background_spec.rb
+++ playbook/spec/pb_kits/playbook/kits/background_spec.rb
@@ -28,7 +28,7 @@ RSpec.describe Playbook::PbBackground::Background do
       expect(subject.new(padding: "xl").classname).to eq "pb_background_kit lazyload pb_background_color_light p_xl"
       expect(subject.new(background_color: "success").classname).to eq "pb_background_kit lazyload pb_background_color_success"
       expect(subject.new(background_color: "category_1").classname).to eq "pb_background_kit lazyload pb_background_color_category_1"
-      expect(subject.new(transition: nil).classname).to eq "pb_background_kit lazyload pb_background_color_light"
+      expect(subject.new(transition: "fade").classname).to eq "pb_background_kit lazyload pb_background_color_light"
     end
   end
 end
