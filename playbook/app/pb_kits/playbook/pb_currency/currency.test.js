6633dde652e86279fdbbbfb6887611dc5eccc20e
Kyle Schechter <84349455+kyleschechter@users.noreply.github.com>
2022-02-15 10:40:16 -0500

    [PLAY-88] Currency kit new abbreviate prop (#1763)
    * Abbreviate prop added to rails and react w/ doc examples
    
    * Refactored logic for both versions and added specs for react
    
    * Fixed specs for thousands value
    
    * Remove whitespace from abbreviated currency in rails plus spec tweaks
    
    * Refactored Rails logic in Currency kit to apply unit styling to abbreviation
    
    * Refactored react logic for currency kit abbreviation styling
    
    * Refactored react to leave abbreviation logic out of template
    
    * Added rspec testing for use of abbreviate prop in Currency kit
    
    * Rewrote spec tests for Currency kit abbreviate prop to make them pass

---
+++ playbook/app/pb_kits/playbook/pb_currency/currency.test.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { render, screen } from '../utilities/test-utils'
+import Currency from './_currency'
+
+test('abbreviate prop returns proper abbreviated amount', () => {
+  render(
+    <>
+      <Currency
+          abbreviate
+          amount="3200"
+          data={{ testid: 'test-thousands' }}
+      />
+      <Currency
+          abbreviate
+          amount="3,200,000"
+          data={{ testid: 'test-millions' }}
+      />
+      <Currency
+          abbreviate
+          amount="3200000000"
+          data={{ testid: 'test-billions' }}
+      />
+      <Currency
+          abbreviate
+          amount="3,200,000,000,000"
+          data={{ testid: 'test-trillions' }}
+      />
+    </>
+  )
+
+  expect(screen.getByTestId('test-thousands')).toHaveTextContent('$3.2K')
+  expect(screen.getByTestId('test-millions')).toHaveTextContent('$3.2M')
+  expect(screen.getByTestId('test-billions')).toHaveTextContent('$3.2B')
+  expect(screen.getByTestId('test-trillions')).toHaveTextContent('$3.2T')
+})
