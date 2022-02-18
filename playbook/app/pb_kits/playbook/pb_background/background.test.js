67464755123a075f85cae631d0135ddd5bc77e26
Stephen Cassidy <stephen.cassidy@powerhrg.com>
2022-02-18 12:40:05 -0500

    [YNGJ-574] Background Kit - Set Background Size
    * add unit test for background size - check css style for background size style

--- playbook/app/pb_kits/playbook/pb_background/background.test.js
+++ playbook/app/pb_kits/playbook/pb_background/background.test.js
@@ -23,5 +23,5 @@ test('backgroundColor = category_1', () => {
 
 test('backgroundSize = auto', () => {
   const kit = renderKit(Background, props, { backgroundSize: 'auto' })
-  expect(kit).toHaveStyle('pb_background_kit pb_background_size_auto')
+  expect(kit).toHaveStyle('background-size: auto')
 })
