a6dad03d2ca8e2ee4d95a2f47e194856f63f16f0
jon1735 <jonathan.gonzalez@powerhrg.com>
2022-02-18 11:47:32 -0500

    Partially working JS table spec

---
+++ playbook/app/pb_kits/playbook/pb_table/table.test.js
@@ -0,0 +1,17 @@
+import { ensureAccessible, renderKit } from '../utilities/test-utils'
+
+import Table from './_table'
+
+const props = {
+  data: { testid: 'table' },
+  sticky: false,
+}
+
+it('Should be accessible', async () => {
+  ensureAccessible(Table, props)
+})
+
+test('sticky = true', () => {
+  const kit = renderKit(Table, props, { sticky: 'true' })
+  expect(kit).toHaveClass('pb-table table-sm table-responsive-collapse table-card sticky-header table-collapse-sm')
+})
