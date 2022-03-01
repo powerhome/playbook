3c19839965664b3f10aa686aaeadf28fb322abf9
Stephen Marshall <smarshall1980@gmail.com>
2022-03-01 14:51:58 -0600

    Add each example

--- playbook/app/pb_kits/playbook/pb_background/docs/_background_size.jsx
+++ playbook/app/pb_kits/playbook/pb_background/docs/_background_size.jsx
@@ -1,15 +1,35 @@
-import React from 'react'
+import React, { Fragment as F } from 'react'
 import { Background } from '../..'
 
 const BackgroundSize = (props) => (
-  <Background
-      alt="colorful background"
-      backgroundSize="cover"
-      className="background lazyload"
-      imageUrl="https://images.unsplash.com/photo-1528459801416-a9e53bbf4e17?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80"
-      padding="xl"
-      {...props}
-  />
+  <F>
+    <Background
+        alt="colorful background"
+        backgroundSize="auto"
+        className="background lazyload"
+        imageUrl="https://images.unsplash.com/photo-1528459801416-a9e53bbf4e17?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80"
+        padding="xl"
+        {...props}
+    />
+    <br/>
+    <Background
+        alt="colorful background"
+        backgroundSize="contain"
+        className="background lazyload"
+        imageUrl="https://images.unsplash.com/photo-1528459801416-a9e53bbf4e17?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80"
+        padding="xl"
+        {...props}
+    />
+    <br/>
+    <Background
+        alt="colorful background"
+        backgroundSize="cover"
+        className="background lazyload"
+        imageUrl="https://images.unsplash.com/photo-1528459801416-a9e53bbf4e17?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80"
+        padding="xl"
+        {...props}
+    />
+  </F>
 )
 
 export default BackgroundSize
