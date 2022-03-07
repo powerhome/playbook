3791a8c4e96293c2e537c1ed8bae64965bcea23d
Terry Finn <tfinn@noman.local>
2022-03-07 11:25:57 -0500

    Support disabled aria-disabled for buttons
    HTML buttons have a disabled attribute, and this prevents the button
    form being clicked and focused. Additionally, buttons can have a
    aria-disabled attrubte to indicate to a user the button is disabled. The
    presence of either of these should render the same, but button is not
    intended to be clickable.
    
    For backwards compatibility, we are allowing the explicit presence of
    the disabled classes to do the same thing, and adding the disabled or
    aria-disabled attribute, but it would be better to encourage the use of
    the disabled attributes to do this.

--- playbook/spec/pb_kits/playbook/kits/button_spec.rb
+++ playbook/spec/pb_kits/playbook/kits/button_spec.rb
@@ -68,12 +68,12 @@ RSpec.describe Playbook::PbButton::Button do
 
   describe "#classname" do
     it "returns namespaced class name", :aggregate_failures do
-      expect(subject.new({}).classname).to eq "pb_button_kit_primary_inline_enabled"
-      expect(subject.new(variant: "secondary").classname).to eq "pb_button_kit_secondary_inline_enabled"
-      expect(subject.new(disabled: true).classname).to eq "pb_button_kit_primary_inline_disabled"
-      expect(subject.new(full_width: true).classname).to eq "pb_button_kit_primary_block_enabled"
-      expect(subject.new(size: "sm").classname).to eq "pb_button_kit_primary_inline_enabled size_sm"
-      expect(subject.new(size: "lg").classname).to eq "pb_button_kit_primary_inline_enabled size_lg"
+      expect(subject.new({}).classname).to eq "pb_button_kit_primary_inline"
+      expect(subject.new(variant: "secondary").classname).to eq "pb_button_kit_secondary_inline"
+      expect(subject.new(disabled: true).classname).to eq "pb_button_kit_primary_inline"
+      expect(subject.new(full_width: true).classname).to eq "pb_button_kit_primary_block"
+      expect(subject.new(size: "sm").classname).to eq "pb_button_kit_primary_inline size_sm"
+      expect(subject.new(size: "lg").classname).to eq "pb_button_kit_primary_inline size_lg"
     end
   end
 end
