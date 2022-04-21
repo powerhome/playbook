600f860b0f6a09b795dc388eec9715f0791ce400
Erica Winne <erica.winne@powerhrg.com>
2022-04-21 15:08:41 -0400

    Merge Master

--- playbook/app/pb_kits/playbook/pb_body/_body.tsx
+++ playbook/app/pb_kits/playbook/pb_body/_body.tsx
@@ -9,11 +9,11 @@ import Highlight from '../pb_highlight/_highlight'
 type BodyProps = {
   aria?: {[key: string]: string},
   className?: string,
-  children?: array<React.ReactChild>,
+  children?: React.ReactChild[],
   color?: 'default' | 'light' | 'lighter' | 'link',
   dark?: boolean,
   data?: {[key: string]: string},
-  highlightedText?: array<string>,
+  highlightedText?: string[],
   highlighting?: boolean,
   id?: string,
   status?: 'negative' | 'neutral' | 'positive',
@@ -22,30 +22,31 @@ type BodyProps = {
   variant: null | 'link',
 }
 
-const Body = (props: BodyProps) => {
+const Body = (props: BodyProps): React.ReactElement => {
   const {
     aria = {},
-    className,
     children,
+    className,
     color = '',
     data = {},
     highlightedText = [],
     highlighting = false,
-    id,
-    status,
+    id = '',
+    status = null,
     tag = 'div',
-    text,
+    text = '',
     variant = null,
   } = props
 
-  const ariaProps = buildAriaProps(aria)
-  const dataProps = buildDataProps(data)
+  const ariaProps: {[key: string]: any} = buildAriaProps(aria)
+  const dataProps: {[key: string]: any} = buildDataProps(data)
   const classes = classnames(
     buildCss('pb_body_kit', color, variant, status),
     globalProps(props),
     className
   )
-  const Tag = `${tag}`
+  const Tag: React.ReactElement | any = `${tag}`
+
 
   return (
     <Tag
@@ -54,11 +55,17 @@ const Body = (props: BodyProps) => {
         className={classes}
         id={id}
     >
-      <If condition={highlighting}>
-        <Highlight highlightedText={highlightedText}>{text || children}</Highlight>
-        <Else />
-        { text || children }
-      </If>
+      { highlighting && (
+         <Highlight
+             highlightedText={highlightedText}
+             text={text}
+         >
+            {children}
+         </Highlight>
+      ) }
+      { !highlighting && (
+        text || children
+      ) }
     </Tag>
   )
 }
