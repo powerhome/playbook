045d37706f843a46e3abe1ac72ebcbdd39cc153c
Stephen Marshall <smarshall1980@gmail.com>
2022-04-28 10:26:44 -0500

    Separate base types

--- playbook/app/pb_kits/playbook/types/base.ts
+++ playbook/app/pb_kits/playbook/types/base.ts
@@ -1,4 +1,8 @@
+// Simple base types
 import { SyntheticEvent } from "react"
 
+export type Binary = 0 | 1
 export type Callback<T, K> = (arg: T) => K
+export type EmptyObject = Record<string, unknown>
 export type InputCallback<T> = Callback<SyntheticEvent<T>, void>
+export type Noop = () => void
---
+++ playbook/app/pb_kits/playbook/types/display.ts
@@ -0,0 +1,13 @@
+import { Sizes } from './sizes'
+
+export type Display = {
+  display?: "block" | "flex" | "hidden" | "inline_block" | "inline" | "inline_flex",
+} | DisplaySizes
+
+export type DisplayType = "hidden" | "flex" | "inline" | "inline_block" | "block"
+
+export type DisplaySizes = {
+  [key in Sizes]: DisplayType
+}
+
+export type None = "none"
