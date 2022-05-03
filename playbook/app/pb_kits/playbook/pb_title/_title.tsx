836e7b711647181ca561a64004ec47fd4a6fcc6a
Andrew McIndoe <amcindoe@powerhrg.com>
2022-05-03 14:29:19 -0400

    Converted Title to TSX file

---
+++ playbook/app/pb_kits/playbook/pb_title/_title.tsx
@@ -0,0 +1,57 @@
+/* @flow */
+
+import React from 'react'
+import classnames from 'classnames'
+import { buildAriaProps, buildCss, buildDataProps } from '../utilities/props'
+import { deprecatedProps, globalProps } from '../utilities/globalProps'
+
+type TitleProps = {
+  aria?: object,
+  children?: array<React.ReactNode> | React.ReactNode,
+  className?: string,
+  color?: "default" | "light" | "lighter" | "success" | "error" | "link",
+  data?: object,
+  id?: string,
+  size?: 1 | 2 | 3 | 4,
+  tag?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "div" | "span",
+  text?: string,
+  variant?: null | "link",
+}
+
+const Title = (props: TitleProps) => {
+  if (props.variant) deprecatedProps('Title', ['variant']) //variant prop is deprecated, use color instead
+  const {
+    aria = {},
+    children,
+    className,
+    color,
+    data = {},
+    id,
+    size = 3,
+    tag = 'h3',
+    text,
+    variant = null,
+  } = props
+
+  const ariaProps = buildAriaProps(aria)
+  const dataProps = buildDataProps(data)
+  const classes = classnames(
+    buildCss('pb_title_kit', size, variant, color),
+    globalProps(props),
+    className,
+  )
+  const Tag = `${tag}`
+
+  return (
+    <Tag
+        {...ariaProps}
+        {...dataProps}
+        className={classes}
+        id={id}
+    >
+      {text || children}
+    </Tag>
+  )
+}
+
+export default Title
