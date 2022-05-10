ca4d626f4fdd4b54034b3d6eda716cee7e8c3ecc
Ivan Candido <ivan.candido@powerhrg.com>
2022-05-10 16:58:02 -0400

    Removed framer-motion from slides

--- playbook/app/pb_kits/playbook/pb_lightbox/Carousel/Slides.jsx
+++ playbook/app/pb_kits/playbook/pb_lightbox/Carousel/Slides.jsx
@@ -1,11 +1,9 @@
 /* @flow */
 
 import { noop } from 'lodash'
-import { motion } from 'framer-motion'
 import React, { useState } from 'react'
 
 import Slide from './Slide'
-import useSlides from './useSlides'
 
 type SlidesType = {
   urls: Array<string>,
@@ -17,37 +15,22 @@ type SlidesType = {
 export default function Slides({
   urls = [],
   current = 0,
-  onClick = noop,
+  // onClick = noop,
   onChange = noop,
 }: SlidesType) {
   const [zooming, setZooming] = useState(false)
-  const { controls, dragConstraints, handleDragEnd } = useSlides({
-    current,
-    pagesCount: urls.length,
-    onChange,
-  })
 
   const handleZoom = (isZooming) => setZooming(isZooming)
-
   return (
-    <motion.div
-        animate={controls}
+    <div
         className="Slides"
-        drag={!zooming && 'x'}
-        dragConstraints={dragConstraints}
-        dragElastic={0.05}
-        onDragEnd={handleDragEnd}
-        transition={{ type: 'spring', bounce: 0 }}
     >
-      {urls.map((url, i) => (
         <Slide
-            key={i}
-            onClick={() => onClick(i)}
+            onClick={() => onChange(current)}
             onZoom={handleZoom}
-            url={url}
+            url={urls[current]}
             zooming={zooming}
         />
-      ))}
-    </motion.div>
+    </div>
   )
 }
