99d2792add0dce9da74cc9b7d001a5c0ee5ab59a
Stephen Marshall <smarshall1980@gmail.com>
2022-05-11 12:33:06 -0500

    Update flexbox guideline

--- playbook/app/pb_kits/playbook/pb_card/docs/_card_light.jsx
+++ playbook/app/pb_kits/playbook/pb_card/docs/_card_light.jsx
@@ -4,22 +4,7 @@ import Card from '../_card.jsx'
 const CardLight = (props) => {
   return (
     <div>
-      <Card
-          flexDirection={{xs: "row", sm: "column", md: "rowReverse", lg: "columnReverse"}}
-          {...props}
-      >{'Card content'}</Card>
-      <Card
-          flexDirection="column"
-          {...props}
-      >{'Card content'}</Card>
-      <Card
-          flexDirection="rowReverse"
-          {...props}
-      >{'Card content'}</Card>
-      <Card
-          flexDirection="columnReverse"
-          {...props}
-      >{'Card content'}</Card>
+      <Card {...props}>{'Card content'}</Card>
     </div>
   )
 }
