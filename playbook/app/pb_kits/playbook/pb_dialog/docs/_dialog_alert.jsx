6137d50ea220d6e1ebe0788662f2e97fe093f38d
Cooper Sawyer <cooper.sawyer@powerhrg.com>
2022-05-11 11:08:52 -0400

    Added alert box and button on playbook page

---
+++ playbook/app/pb_kits/playbook/pb_dialog/docs/_dialog_alert.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import {
+  Body,
+  Button,
+  Caption,
+  Dialog,
+  RichTextEditor,
+  Typeahead,
+} from '../../'
+
+const DialogAlert = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const close = () => setIsOpen(false)
+  const open = () => setIsOpen(true)
+  // const [isLoading, setIsLoading] = useState(false)
+
+  return (
+    <>
+      <Button onClick={open}>{'Open a Complex Dialog'}</Button>
+      <Dialog
+          onClose={close}
+          opened={isOpen}
+          size="lg"
+      >
+        <Dialog.Header>
+          <Body>{'What do you need us to take care of?'}</Body>
+        </Dialog.Header>
+        <Dialog.Body>
+          <Caption marginBottom="xs">{'Description'}</Caption>
+          <RichTextEditor />
+          <br />
+          <Caption>
+            {
+              'Type in a word or term too help find tickets later. ex. training,'
+            }
+            {'phone setup, hr'}
+          </Caption>
+          <Typeahead placeholder="Tags.." />
+        </Dialog.Body>
+        <Dialog.Footer>
+          <Button onClick={close}>{'Send My Issue'}</Button>
+          <Button
+              onClick={close}
+              variant="link"
+          >
+            {'Back'}
+          </Button>
+        </Dialog.Footer>
+      </Dialog>
+    </>
+  )
+}
+
+export default DialogAlert
---
+++ playbook/app/pb_kits/playbook/pb_dialog/docs/_dialog_alert.md
@@ -0,0 +1,1 @@
+The dialog kit is utilized for alerts for when you need these mama jamas
