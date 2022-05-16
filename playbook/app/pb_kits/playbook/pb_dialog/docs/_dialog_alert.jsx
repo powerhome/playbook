b7ca9d2bed43302f6be0b7139ffc5a2e5e8b782b
Jasper Furniss <jasper.furniss@powerhrg.com>
2022-05-16 09:20:06 -0400

    Latest

--- playbook/app/pb_kits/playbook/pb_dialog/docs/_dialog_alert.jsx
+++ playbook/app/pb_kits/playbook/pb_dialog/docs/_dialog_alert.jsx
@@ -1,65 +1,105 @@
-import React, { useState } from "react"
+/* @flow */
 
-import {
-  Body,
-  Button,
-  Dialog,
-  Flex,
-  IconCircle,
-  Title
-} from "../../"
+import React, { useState } from "react"
+import { Button, Dialog, Flex } from "../../"
+
+const useDialog = (visible = false) => {
+  const [opened, setOpened] = useState(visible)
+  const toggle = () => setOpened(!opened)
+
+  return [opened, toggle]
+}
 
 const DialogAlert = () => {
-  const [isOpen, setIsOpen] = useState(false)
-  const close = () => setIsOpen(false)
-  const open = () => setIsOpen(true)
+  const [infoAlertOpened, toggleInfoAlert] = useDialog()
+  const [cautionAlertOpened, toggleCautionAlert] = useDialog()
+  const [successAlertOpened, toggleSuccessAlert] = useDialog()
+  const [errorAlertOpened, toggleErrorAlert] = useDialog()
+  const [deleteAlertOpened, toggleDeleteAlert] = useDialog()
+
+  const dialogs = [
+    {
+      sweetAlert: "default",
+      status: "info",
+      text: "Body on small dialog",
+      title: "Header on small dialog",
+      toggle: toggleInfoAlert,
+      visible: infoAlertOpened,
+    },
+    {
+      sweetAlert: "default",
+      status: "caution",
+      text: "Body on medium dialog",
+      title: "Header on medium dialog",
+      toggle: toggleCautionAlert,
+      visible: cautionAlertOpened,
+    },
+    {
+      sweetAlert: "default",
+      status: "delete",
+      text: "Body on large dialog",
+      title: "Header on large dialog",
+      toggle: toggleDeleteAlert,
+      visible: deleteAlertOpened,
+    },
+    {
+      sweetAlert: "default",
+      status: "error",
+      text: "Body on large dialog",
+      title: "Header on large dialog",
+      toggle: toggleErrorAlert,
+      visible: errorAlertOpened,
+    },
+    {
+      sweetAlert: "default",
+      status: "success",
+      text: "Body on large dialog",
+      title: "Header on large dialog",
+      toggle: toggleSuccessAlert,
+      visible: successAlertOpened,
+    },
+  ]
 
   return (
-    <>
-      <Button onClick={open}>{"Open a Default Alert"}</Button>
-      <Dialog
-          borderRadius="xl"
-          onClose={close}
-          opened={isOpen}
-          size="md"
-      >
-        <Dialog.Body>
-          <Flex
-              align="center"
-              orientation="column"
-          >
-            <IconCircle
-                icon="exclamation-circle"
-                marginY="xs"
-                size="lg"
-            />
-            <Title
-                marginY="sm"
-                size={3}
-            >
-                  {"Are you sure?"}
-            </Title>
-            <Body
-                marginY="xs"
-                text="Text explaining why this alert happened"
-            />
-          </Flex>
-        </Dialog.Body>
-        <Dialog.Footer>
-          <Button
-              onClick={close}
-              variant="secondary"
-          >
-            {"No, Cancel"}
-          </Button>
-          <Button
-              onClick={close}
-          >
-            {"Yes, Do things"}
-          </Button>
-        </Dialog.Footer>
-      </Dialog>
-    </>
+    <div>
+      <Flex>
+        <Button id='sm'
+            marginRight='xl'
+            onClick={toggleCautionAlert}>
+          {"Info Status"}
+        </Button>
+        <Button marginRight='xl'
+            onClick={toggleCautionAlert}>
+          {"Caution Status"}
+        </Button>
+        <Button marginRight='xl'
+            onClick={toggleSuccessAlert}>
+          {"Success Status"}
+        </Button>
+        <Button marginRight='xl'
+            onClick={toggleErrorAlert}>
+          {"Error Status"}
+        </Button>
+        <Button marginRight='xl'
+            onClick={toggleDeleteAlert}>
+          {"Delete Status"}
+        </Button>
+      </Flex>
+      <Flex>
+        {dialogs.map((dialog) => (
+          <Dialog
+              key={dialog.status}
+              // eslint-disable-next-line react/jsx-handler-names
+              onClose={dialog.toggle}
+              opened={dialog.visible}
+              status={dialog.status}
+              sweetAlert={dialog.sweetAlert}
+              text='Hello'
+              title='Testing Title'
+          />
+        ))}
+      </Flex>
+    </div>
   )
 }
 
