9b6170b1970033c13e1c621213aae68ce07f8ef0
Cooper Sawyer <cooper.sawyer@powerhrg.com>
2022-05-17 15:13:23 -0400

    Revert "Latest"
    This reverts commit b7ca9d2bed43302f6be0b7139ffc5a2e5e8b782b.

--- playbook/app/pb_kits/playbook/pb_dialog/_dialog.jsx
+++ playbook/app/pb_kits/playbook/pb_dialog/_dialog.jsx
@@ -1,21 +1,17 @@
 /* @flow */
 
-import React, { useState } from "react"
-import classnames from "classnames"
-import Modal from "react-modal"
+import React, { useState } from 'react'
+import classnames from 'classnames'
+import Modal from 'react-modal'
 
-import { buildAriaProps, buildCss, buildDataProps } from "../utilities/props"
-import { globalProps } from "../utilities/globalProps"
+import { buildAriaProps, buildCss, buildDataProps } from '../utilities/props'
+import { globalProps } from '../utilities/globalProps'
 
-import Button from "../pb_button/_button"
-import Flex from "../pb_flex/_flex"
-import IconCircle from "../pb_icon_circle/_icon_circle"
-import Title from "../pb_title/_title"
-import Body from "../pb_body/_body"
-import DialogHeader from "./child_kits/_dialog_header"
-import DialogFooter from "./child_kits/_dialog_footer"
-import DialogBody from "./child_kits/_dialog_body"
-import { DialogContext } from "./_dialog_context"
+import Button from '../pb_button/_button'
+import DialogHeader  from './child_kits/_dialog_header'
+import DialogFooter from './child_kits/_dialog_footer'
+import DialogBody from './child_kits/_dialog_body'
+import { DialogContext } from './_dialog_context'
 
 type DialogProps = {
   aria?: object,
@@ -35,11 +31,9 @@ type DialogProps = {
   portalClassName?: string,
   shouldCloseOnOverlayClick: boolean,
   size?: "sm" | "md" | "lg" | "content",
-  status?: "info" | "caution" | "delete" | "error" | "success",
-  sweetAlert?: "default" | "dismiss" | "compact" | "stacked",
   text?: string,
   title?: string,
-  trigger?: string,
+  trigger?: string
 }
 
 const Dialog = (props: DialogProps) => {
@@ -50,7 +44,7 @@ const Dialog = (props: DialogProps) => {
     className,
     data = {},
     id,
-    size = "md",
+    size = 'md',
     children,
     loading = false,
     opened,
@@ -59,8 +53,6 @@ const Dialog = (props: DialogProps) => {
     onClose = () => {},
     portalClassName,
     shouldCloseOnOverlayClick = true,
-    sweetAlert = null,
-    status,
     text,
     title,
     trigger,
@@ -69,29 +61,27 @@ const Dialog = (props: DialogProps) => {
   const dataProps = buildDataProps(data)
 
   const dialogClassNames = {
-    base: classnames("pb_dialog", buildCss("pb_dialog", size)),
-    afterOpen: "pb_dialog_after_open",
-    beforeClose: "pb_dialog_before_close",
+    base: classnames('pb_dialog', buildCss('pb_dialog', size)),
+    afterOpen: 'pb_dialog_after_open',
+    beforeClose: 'pb_dialog_before_close',
   }
 
   const overlayClassNames = {
-    base: "pb_dialog_overlay",
-    afterOpen: "pb_dialog_overlay_after_open",
-    beforeClose: "pb_dialog_overlay_before_close",
+    base: 'pb_dialog_overlay',
+    afterOpen: 'pb_dialog_overlay_after_open',
+    beforeClose: 'pb_dialog_overlay_before_close',
   }
 
   const classes = classnames(
-    buildCss("pb_dialog_wrapper"),
+    buildCss('pb_dialog_wrapper'),
     globalProps(props),
     className
   )
 
   const api = {
-    onClose: trigger
-      ? function () {
-          setTriggerOpened(false)
-        }
-      : onClose,
+    onClose: trigger ? function(){
+      setTriggerOpened(false)
+    } : onClose,
   }
 
   const [triggerOpened, setTriggerOpened] = useState(false),
@@ -99,152 +89,60 @@ const Dialog = (props: DialogProps) => {
 
   if (trigger) {
     const modalTrigger = document.querySelector(trigger)
-    modalTrigger.addEventListener(
-      "click",
-      () => {
-        setTriggerOpened(true)
-        document
-          .querySelector("#cancel-button")
-          .addEventListener("click", () => {
-            setTriggerOpened(false)
-          })
-      },
-      { once: true }
-    )
-  }
-
-  const sweetAlertStyle = {
-    default: {
-      size: "md",
-    },
-    dismiss: {
-      size: "sm",
-    },
-    compact: {
-      size: "sm",
-    },
-    stacked: {
-      size: "sm",
-    },
-  }
-
-  const sweetAlertStatus = {
-    default: {
-      icon: "exclamation-circle",
-      variant: "default",
-    },
-    caution: {
-      icon: "triangle-warning",
-      variant: "yellow",
-    },
-    delete: {
-      icon: "trash",
-      variant: "red",
-    },
-    error: {
-      icon: "times-circle",
-      variant: "red",
-    },
-    success: {
-      icon: "check-circle",
-      variant: "green",
-    },
-  }
-
-  const DefaultModal = () => {
-    return (
-      <Modal
-          ariaHideApp={false}
-          className={dialogClassNames}
-          closeTimeoutMS={200}
-          contentLabel='Minimal Modal Example'
-          id={id}
-          isOpen={modalIsOpened}
-          onRequestClose={onClose}
-          overlayClassName={overlayClassNames}
-          portalClassName={portalClassName}
-          shouldCloseOnOverlayClick={shouldCloseOnOverlayClick}
-      >
-        <If condition={title}>
-          <Dialog.Header>{title}</Dialog.Header>
-        </If>
-        <If condition={text}>
-          <Dialog.Body>{text}</Dialog.Body>
-        </If>
-        <If condition={cancelButton && confirmButton}>
-          <Dialog.Footer>
-            <Button loading={loading}
-                onClick={onConfirm}>
-              {confirmButton}
-            </Button>
-            <Button id='cancel-button'
-                onClick={onCancel}
-                variant='link'>
-              {cancelButton}
-            </Button>
-          </Dialog.Footer>
-        </If>
-        {children}
-      </Modal>
-    )
+    modalTrigger.addEventListener('click', () => {
+      setTriggerOpened(true)
+      document.querySelector('#cancel-button').addEventListener('click', () => {
+        setTriggerOpened(false)
+      })
+    }, { once: true })
   }
 
   return (
     <DialogContext.Provider value={api}>
-      <div {...ariaProps}
+      <div
+          {...ariaProps}
           {...dataProps}
-          className={classes}>
-        {sweetAlert ? (
-          <Modal
-              ariaHideApp={false}
-              className={classnames(
-              "pb_dialog",
-              buildCss("pb_dialog", sweetAlertStyle[sweetAlert].size)
-            )}
-              closeTimeoutMS={200}
-              contentLabel='Minimal Modal Example'
-              id={id}
-              isOpen={modalIsOpened}
-              onRequestClose={onClose}
-              overlayClassName={overlayClassNames}
-              portalClassName={portalClassName}
-              shouldCloseOnOverlayClick={shouldCloseOnOverlayClick}
-          >
-            <Dialog.Body>
-              <Flex align='center'
-                  orientation='column'>
-                <IconCircle
-                    icon={sweetAlertStatus[status].icon}
-                    marginY='xs'
-                    size='lg'
-                    variant={sweetAlertStatus[status].variant}
-                />
-                <Title marginY='sm'
-                    size={3}>
-                  {title}
-                </Title>
-                <Body marginY='xs'
-                    text={text} />
-              </Flex>
-            </Dialog.Body>
-            <If condition={cancelButton && confirmButton}>
-              <Dialog.Footer>
-                <Button loading={loading}
-                    onClick={onConfirm}>
-                  {confirmButton}
-                </Button>
-                <Button id='cancel-button'
-                    onClick={onCancel}
-                    variant='link'>
-                  {cancelButton}
-                </Button>
-              </Dialog.Footer>
-            </If>
-            {children}
-          </Modal>
-        ) : (
-          <DefaultModal />
-        )}
+          className={classes}
+      >
+        <Modal
+            ariaHideApp={false}
+            className={dialogClassNames}
+            closeTimeoutMS={200}
+            contentLabel="Minimal Modal Example"
+            id={id}
+            isOpen={modalIsOpened}
+            onRequestClose={onClose}
+            overlayClassName={overlayClassNames}
+            portalClassName={portalClassName}
+            shouldCloseOnOverlayClick={shouldCloseOnOverlayClick}
+        >
+          <If condition={title}>
+            <Dialog.Header>{title}</Dialog.Header>
+          </If>
+          <If condition={text}>
+            <Dialog.Body>{text}</Dialog.Body>
+          </If>
+
+          <If condition={cancelButton && confirmButton}>
+            <Dialog.Footer>
+              <Button
+                  loading={loading}
+                  onClick={onConfirm}
+              >
+                {confirmButton}
+              </Button>
+              <Button
+                  id="cancel-button"
+                  onClick={onCancel}
+                  variant="link"
+              >
+                {cancelButton}
+              </Button>
+            </Dialog.Footer>
+          </If>
+
+          {children}
+        </Modal>
       </div>
     </DialogContext.Provider>
   )
