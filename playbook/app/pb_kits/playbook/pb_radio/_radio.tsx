32701013ecfc79bdb1ce258e2acfed9a4ebd1484
Andrew McIndoe <amcindoe@powerhrg.com>
2022-05-23 13:50:56 -0400

    Refactored ifelse

--- playbook/app/pb_kits/playbook/pb_radio/_radio.tsx
+++ playbook/app/pb_kits/playbook/pb_radio/_radio.tsx
@@ -4,24 +4,24 @@ import React, { forwardRef } from 'react'
 import Body from '../pb_body/_body'
 import classnames from 'classnames'
 import { buildAriaProps, buildCss, buildDataProps } from '../utilities/props'
-import { globalProps } from '../utilities/globalProps'
+import { globalProps, GlobalProps } from '../utilities/globalProps'
 
 type RadioProps = {
-  aria?: object,
+  aria?: {[key: string]: string},
   alignment?: String,
   checked?: Boolean,
   children?: Node,
   className?: String,
   dark?: boolean,
-  data?: object,
+  data?: {[key: string]: string},
   error?: Boolean,
   id?: String,
   label: String,
   name: String,
   value: String,
   text: String,
-  onChange: (Boolean)=>void,
-}
+  onChange: (event: React.FormEvent<HTMLInputElement>)=>void,
+} & GlobalProps
 
 const Radio = ({
   aria = {},
@@ -38,10 +38,27 @@ const Radio = ({
   value = 'radio_text',
   onChange = () => {},
   ...props
-}: RadioProps, ref) => {
+}: RadioProps, ref): React.ReactElement => {
   const ariaProps = buildAriaProps(aria)
   const dataProps = buildDataProps(data)
   const classes = classnames(buildCss('pb_radio_kit'), { error }, { dark }, globalProps(props), alignment, className)
+
+  const displayRadio = () => {
+    if (children)
+      return (children)
+    else 
+    return (
+    <input
+        {...props}
+        id={id}
+        name={name}
+        onChange={onChange}
+        ref={ref}
+        text={text}
+        type="radio"
+        value={value}
+    />
+  )}
 
   return (
     <label
@@ -50,20 +67,7 @@ const Radio = ({
         className={classes}
         htmlFor={id}
     >
-      <If condition={children}>
-        {children}
-        <Else />
-        <input
-            {...props}
-            id={id}
-            name={name}
-            onChange={onChange}
-            ref={ref}
-            text={text}
-            type="radio"
-            value={value}
-        />
-      </If>
+    <>{displayRadio()}</>
       <span className="pb_radio_button" />
       <Body
           dark={dark}
