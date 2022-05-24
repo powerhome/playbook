197673aa1885e82e644442905b3871fe8ea3011c
Andrew McIndoe <amcindoe@powerhrg.com>
2022-05-24 16:08:39 -0400

    Adjusted syntax for Build CSS due to test error

--- playbook/app/pb_kits/playbook/pb_radio/_radio.tsx
+++ playbook/app/pb_kits/playbook/pb_radio/_radio.tsx
@@ -42,7 +42,7 @@ const Radio = ({
   const ariaProps = buildAriaProps(aria)
   const dataProps = buildDataProps(data)
   const classes = classnames(
-    buildCss('pb_radio_kit', alignment, error ? 'error': null , dark ? 'dark': null),
+    buildCss('pb_radio_kit', alignment, dark ? 'dark': null, error ? 'error': null ),
     globalProps(props),
     className)
 
