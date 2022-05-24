db21ff631aa5c8d1e24dca580a783c78913c5978
Gordon Hodanich <ghodanich@gmail.com>
2022-05-24 13:00:47 -0400

    Code clean-up

--- playbook-website/app/javascript/components/VisualGuidelines/Examples/Display.tsx
+++ playbook-website/app/javascript/components/VisualGuidelines/Examples/Display.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {
-  Caption,
-  Title,
   Body,
-  Table
-
+  Caption,
+  Table,
+  Title,
 } from 'playbook-ui'
+
+import Example from '../Templates/Example'
 
 const screenSizeProps = {
   display: ['xs', 'sm', 'md', 'lg', 'xl']
@@ -17,74 +18,70 @@ const UTILITY_CLASSES = [
   {size: 'md', media: '@media screen and (max-width: 768px)', class: '.display_md_inline_block', properties: 'display: inline-block !important'},
   {size: 'lg', media: '@media screen and (max-width: 992px)', class: '.display_lg_inline', properties: 'display: inline !important'},
   {size: 'xl', media: '@media screen and (max-width: 1200px)', class: '.display_xl_flex', properties: 'display: flex !important'},
-
 ]
 
 const DISPLAY_VALUES = ['inline', 'flex', 'inline_flex', 'inline_block', 'block', 'none']
 
-import Example from '../Templates/Example'
-
 const Display = ({example}: {example: string}) => (
-    <React.Fragment>
-      <Example
-          example={example}
-          globalProps={{
-            display: DISPLAY_VALUES
-          }}
-          screenSizes={screenSizeProps}
-          title='Display'
-      />
-      <Title
-          text='Utility Classes'
-          paddingTop='sm'
-          marginBottom='xs'
-          size={4}
-      />
-      <Body
-          text='Just want the raw classes? We got you. All of our global props are simple CSS utilities available through classes.'
-          marginBottom='sm'
-      />
-      <Caption
-          text='Visual Guide'
-          marginBottom='sm'
-      />
-      <Table>
-        <thead>
-          <tr>
-            <th>{'Screen Size'}</th>
-            <th>{'@Media Screen'}</th>
-            <th>{'Class'}</th>
-            <th>{'Properties'}</th>
+  <React.Fragment>
+    <Example
+        example={example}
+        globalProps={{
+          display: DISPLAY_VALUES
+        }}
+        screenSizes={screenSizeProps}
+        title='Display'
+    />
+    <Title
+        marginBottom='xs'
+        paddingTop='sm'
+        size={4}
+        text='Utility Classes'
+    />
+    <Body
+        text='Just want the raw classes? We got you. All of our global props are simple CSS utilities available through classes.'
+        marginBottom='sm'
+    />
+    <Caption
+        text='Visual Guide'
+        marginBottom='sm'
+    />
+    <Table>
+      <thead>
+        <tr>
+          <th>{'Screen Size'}</th>
+          <th>{'@Media Screen'}</th>
+          <th>{'Class'}</th>
+          <th>{'Properties'}</th>
+        </tr>
+      </thead>
+      <tbody>
+        {UTILITY_CLASSES.map((utilityClass: any) => (
+          <tr key={utilityClass} >
+            <td>
+              <Body
+                text={utilityClass.size}
+              />
+            </td>
+            <td>
+              <Body
+                text={utilityClass.media}
+              />
+            </td>
+            <td>
+              <Body
+                text={utilityClass.class}
+              />
+            </td>
+            <td>
+              <Body
+                text={utilityClass.properties}
+              />
+            </td>
           </tr>
-        </thead>
-        <tbody>
-          {UTILITY_CLASSES.map((utilityClass: any) => (
-            <tr key={utilityClass} >
-              <td>
-                <Body
-                  text={utilityClass.size}
-                />
-              </td>
-              <td>
-                <Body
-                  text={utilityClass.media}
-                />
-              </td>
-              <td>
-                <Body
-                  text={utilityClass.class}
-                />
-              </td>
-              <td>
-                <Body
-                  text={utilityClass.properties}
-                />
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
-    </React.Fragment>
+        ))}
+      </tbody>
+    </Table>
+  </React.Fragment>
 )
-
 export default Display
