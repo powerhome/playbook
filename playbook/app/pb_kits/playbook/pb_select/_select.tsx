cbfb743f8a6fbe2cafb495a28b6d70c3e384f2fb
Matheus Abegg <1006630+mpabegg@users.noreply.github.com>
2022-05-25 12:43:13 -0300

    Enhance SelectProps type with GlobalProps

--- playbook/app/pb_kits/playbook/pb_select/_select.tsx
+++ playbook/app/pb_kits/playbook/pb_select/_select.tsx
@@ -2,7 +2,7 @@ import React, { forwardRef } from 'react'
 import classnames from 'classnames'
 
 import { buildAriaProps, buildCss, buildDataProps } from '../utilities/props'
-import { globalProps, domSafeProps } from '../utilities/globalProps'
+import { globalProps, domSafeProps, GlobalProps } from '../utilities/globalProps'
 import type { InputCallback } from '../types'
 
 import Body from '../pb_body/_body'
@@ -36,7 +36,7 @@ type SelectProps = {
   name?: string,
   required?: boolean,
   value?: string,
-}
+} & GlobalProps
 
 const createOptions = (options: SelectOption[]) => options.map((option, index) => (
   <option
