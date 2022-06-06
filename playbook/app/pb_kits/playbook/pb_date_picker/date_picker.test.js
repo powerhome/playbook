098318e26db7620c1daa3196542bab5941a14546
Stephen Marshall <smarshall1980@gmail.com>
2022-06-06 15:21:01 -0500

    Datepicker Allow Time Selection (#1906)
    * Staged Changes
    
    * Converting title kit to ts (#1872)
    
    * Removed flow and Renamed file
    
    * Updated Types
    
    * Removed JSX file extentions from imports
    
    * Fix syntax error
    
    * Fix tests
    
    Co-authored-by: Erica Winne <erica.winne@powerhrg.com>
    
    * Converting caption to tsx (#1874)
    
    * Started Converting
    
    * Rename file to TSX and Updated types
    
    * Added/Joined & GlobalProps
    
    * Merge Master
    
    * Changed back default blank string
    
    Co-authored-by: Erica Winne <erica.winne@powerhrg.com>
    
    * [PLAY-142] Visual Guidelines - Switch to React - Examples Template (#1849)
    
    * Rendered rough example in browser
    
    * Section separator between props and values
    
    * Added pills and continued to change styling
    
    * Fixed width of cards
    
    * Fixed flexitem alignment
    
    * Added title and body kits
    
    * Fixed more styling
    
    * Changed out background kit to fix border radius
    
    * Fixed border radius
    
    * Fixed border radius on other templates
    
    * Updated caption card
    
    * Fixed title margins
    
    * Corrected camelcase in pills
    
    * Three templates for visual guidelines
    
    * Fixed syntax error
    
    * Fixed syntax again
    
    * Testing client-side code example colorization
    
    * Use Rouge to format code examples
    
    * Template Complete
    
    Co-authored-by: Stephen Marshall <smarshall1980@gmail.com>
    
    * Set up Doc files
    
    * Added example code
    
    * Updated Plugin Method
    
    * adjusting helper
    
    * Adjusting Helper
    
    * changed id and removed other code
    
    * removed commented out and unneeded code
    
    * Revert "removed commented out and unneeded code"
    
    This reverts commit ac61b1b52f537940b837478c85844f817a23bb38.
    
    * Updated rspec
    
    * removed unneeded partial for styling
    
    * updated rails example doc
    
    * updated spec
    
    * Updated Rspec
    
    * updated default
    
    * Updated Rails doc
    
    * Revert "updated spec"
    
    This reverts commit 5d824ef6539d17a25671dea3296f23d6ae8e78ba.
    
    * adjusted alignment
    
    * added prop
    
    * refactored to adding new prop instead of replacing
    
    * WIP: Allow Time to Display
    
    * start set up of rails prop
    
    * adjusted rails prop
    
    * updated instructions
    
    * corrected instructions
    
    * updated rspec file
    
    * Corrected test
    
    * DatePicker timeSelection plugin
    
    * Custom AM PM Form Group Selectable Cards
    
    * Flatpickr library attempt to customize AM/PM
    
    * Add test coverage for new feature
    
    * Remove selection_type default in Rails kit
    
    * Re-enable other examples
    
    * Convert plugin to js
    
    * [PLAY-198] Fix Title Bug (#1925)
    
    * Fixed bug
    
    * Went with top
    
    * Update view_component requirement from = 2.48.0 to = 2.55.0 in /playbook (#1927)
    
    * Update view_component requirement from = 2.48.0 to = 2.55.0 in /playbook
    
    Updates the requirements on [view_component](https://github.com/github/view_component) to permit the latest version.
    - [Release notes](https://github.com/github/view_component/releases)
    - [Changelog](https://github.com/github/view_component/blob/main/docs/CHANGELOG.md)
    - [Commits](https://github.com/github/view_component/compare/v2.48.0...v2.55.0)
    
    ---
    updated-dependencies:
    - dependency-name: view_component
      dependency-type: direct:production
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Update website Gemfile.lock
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Stephen Marshall <smarshall1980@gmail.com>
    
    * Remove comments
    
    * Remove Flatpickr minute increment default
    
    Co-authored-by: Andrew McIndoe <amcindoe@powerhrg.com>
    Co-authored-by: Erica Winne <erica.winne@powerhrg.com>
    Co-authored-by: Kurstyn Storms <84343098+KurstynStorms@users.noreply.github.com>
    Co-authored-by: Ivan Candido <84343331+Ivancandido11@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

---
+++ playbook/app/pb_kits/playbook/pb_date_picker/date_picker.test.js
@@ -0,0 +1,160 @@
+/* eslint-disable no-console */
+import React from 'react'
+import { fireEvent, render, screen, waitFor, within } from '../utilities/test-utils'
+
+import DatePicker from './_date_picker'
+import { getTimezoneText } from './plugins/timeSelect'
+
+const DEFAULT_DATE = new Date()
+DEFAULT_DATE.setFullYear(2022)
+DEFAULT_DATE.setMonth(1)
+DEFAULT_DATE.setDate(1)
+DEFAULT_DATE.setHours(12)
+DEFAULT_DATE.setMinutes(0)
+
+describe('DatePicker Kit', () => {
+  beforeEach(() => {
+    jest.spyOn(console, 'error').mockImplementation(() => {});
+  });
+
+  test('renders DatePicker input field', () => {
+    const testId = 'datepicker-kit'
+    render(
+      <DatePicker
+          data={{ testid: testId }}
+          defaultDate={DEFAULT_DATE}
+          pickerId="date-picker"
+      />
+    )
+
+    const kit = screen.getByTestId(testId)
+    expect(kit).toHaveClass('pb_date_picker_kit')
+  })
+
+  test('shows DatePicker date format m/d/Y', async () => {
+    const testId = 'datepicker-date'
+    render(
+      <DatePicker
+          data={{ testid: testId }}
+          defaultDate={DEFAULT_DATE}
+          format="m/d/Y"
+          pickerId="date-picker-date"
+      />
+    )
+
+    const kit = screen.getByTestId(testId)
+
+    const input = within(kit).getByPlaceholderText('Select Date')
+    expect(input).toBeInTheDocument()
+    await waitFor(() => {
+      expect(input).toHaveValue('02/01/2022')
+    })
+  })
+
+  test('shows DatePicker time caption text', async () => {
+    const testId = 'datepicker-time-caption'
+    render(
+      <DatePicker
+          data={{ testid: testId }}
+          defaultDate={DEFAULT_DATE}
+          enableTime
+          pickerId="date-picker-time-caption"
+      />
+    )
+
+    const kit = screen.getByTestId(testId)
+    const input = within(kit).getByPlaceholderText('Select Date')
+
+    fireEvent(
+      input,
+      new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true,
+      }),
+    )
+    await waitFor(() => {
+      const caption = within(kit).getByText('Select Time')
+      expect(caption).toBeInTheDocument()
+    })
+  })
+
+  test('shows DatePicker timezone text', async () => {
+    const testId = 'datepicker-timezone'
+    render(
+      <DatePicker
+          data={{ testid: testId }}
+          defaultDate={DEFAULT_DATE}
+          enableTime
+          pickerId="date-picker-timezone"
+          showTimezone
+      />
+    )
+
+    const kit = screen.getByTestId(testId)
+    const input = within(kit).getByPlaceholderText('Select Date')
+    const defaultTimezoneText = getTimezoneText(DEFAULT_DATE)
+
+    fireEvent(
+      input,
+      new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true,
+      }),
+    )
+    await waitFor(() => {
+      const timezoneText = within(kit).getByText(defaultTimezoneText)
+      expect(timezoneText).toBeInTheDocument()
+    })
+  })
+
+  test('shows DatePicker meridiem toggle', async () => {
+    const testId = 'datepicker-meridiem'
+    render(
+      <DatePicker
+          data={{ testid: testId }}
+          defaultDate={DEFAULT_DATE}
+          enableTime
+          pickerId="date-picker-meridiem"
+      />
+    )
+
+    const kit = screen.getByTestId(testId)
+    const input = within(kit).getByPlaceholderText('Select Date')
+
+    fireEvent(
+      input,
+      new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true,
+      }),
+    )
+    const meridiemToggleAM = within(kit).getByLabelText('AM')
+    const meridiemTogglePM = within(kit).getByLabelText('PM')
+    await waitFor(() => {
+      expect(meridiemToggleAM).toBeInTheDocument()
+      expect(meridiemTogglePM).toBeInTheDocument()
+    })
+
+    fireEvent(
+      meridiemToggleAM,
+      new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true,
+      }),
+    )
+    await waitFor(() => {
+      expect(input).toHaveValue('02/01/2022 at 12:00 AM')
+    })
+
+    fireEvent(
+      meridiemTogglePM,
+      new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true,
+      }),
+    )
+    await waitFor(() => {
+      expect(input).toHaveValue('02/01/2022 at 12:00 PM')
+    })
+  })
+})
