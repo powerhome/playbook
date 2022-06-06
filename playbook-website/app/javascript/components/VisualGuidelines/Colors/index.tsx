bedb9fb0cf702fcb30225c129baa22a49a630c17
Gordon Hodanich <ghodanich@gmail.com>
2022-06-06 16:22:11 -0400

    Updates Visual Guidelines Color Partial to TSX (#1899)
    * Converting title kit to ts (#1872)
    
    * Removed flow and Renamed file
    
    * Updated Types
    
    * Removed JSX file extentions from imports
    
    * Fix syntax error
    
    * Fix tests
    
    Co-authored-by: Erica Winne <erica.winne@powerhrg.com>
    
    * Converting caption to tsx (#1874)
    
    * Started Converting
    
    * Rename file to TSX and Updated types
    
    * Added/Joined & GlobalProps
    
    * Merge Master
    
    * Changed back default blank string
    
    Co-authored-by: Erica Winne <erica.winne@powerhrg.com>
    
    * Converted Colors partial to TSX
    
    * Replaced removed .slim file
    
    * Added missing color
    
    Co-authored-by: Stephen Marshall <smarshall1980@gmail.com>
    Co-authored-by: Erica Winne <erica.winne@powerhrg.com>

--- playbook-website/app/javascript/components/VisualGuidelines/Colors/index.tsx
+++ playbook-website/app/javascript/components/VisualGuidelines/Colors/index.tsx
@@ -7,7 +7,17 @@ import { Title } from 'playbook-ui'
 import Example from './Example'
 
 import {
-  TEXT_COLORS,
+  ACTIONS,
+  ACTIVE,
+  BACKGROUND,
+  BORDER,
+  CARDS,
+  CATEGORY,
+  DATA,
+  PRODUCTS,
+  SHADOW,
+  STATUS,
+  TEXT_COLORS
 } from './variables'
 
 const Colors = (): React.ReactElement => (
@@ -22,6 +32,46 @@ const Colors = (): React.ReactElement => (
     <Example
         colors={TEXT_COLORS}
         title="Text Colors"
+    />
+    <Example
+        colors={BACKGROUND}
+        title="Backgrounds"
+    />
+    <Example
+        colors={CARDS}
+        title="Cards"
+    />
+    <Example
+        colors={STATUS}
+        title="Status"
+    />
+    <Example
+        colors={DATA}
+        title="Data"
+    />
+    <Example
+        colors={ACTIONS}
+        title="Actions"
+    />
+    <Example
+        colors={ACTIVE}
+        title="Active"
+    />
+    <Example
+        colors={BORDER}
+        title="Border"
+    />
+    <Example
+        colors={SHADOW}
+        title="Shadow"
+    />
+    <Example
+        colors={PRODUCTS}
+        title="Product Colors"
+    />
+    <Example
+        colors={CATEGORY}
+        title="Category Colors"
     />
   </React.Fragment>
 )
