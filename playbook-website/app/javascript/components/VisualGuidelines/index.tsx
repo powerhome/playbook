5e5b1c256a4c58e43a773fbd76757233f05f17de
Ivan Candido <84343331+Ivancandido11@users.noreply.github.com>
2022-06-07 12:28:46 -0400

    Create React Spacing Partial (#1916)
    * Updated example for multi props
    
    * Added spacing partial
    
    * Added style changes
    
    * [PLAY-198] Fix Title Bug (#1925)
    
    * Fixed bug
    
    * Went with top
    
    * Update view_component requirement from = 2.48.0 to = 2.55.0 in /playbook (#1927)
    
    * Update view_component requirement from = 2.48.0 to = 2.55.0 in /playbook
    
    Updates the requirements on [view_component](https://github.com/github/view_component) to permit the latest version.
    - [Release notes](https://github.com/github/view_component/releases)
    - [Changelog](https://github.com/github/view_component/blob/main/docs/CHANGELOG.md)
    - [Commits](https://github.com/github/view_component/compare/v2.48.0...v2.55.0)
    
    ---
    updated-dependencies:
    - dependency-name: view_component
      dependency-type: direct:production
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Update website Gemfile.lock
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Stephen Marshall <smarshall1980@gmail.com>
    
    * No explicit imagePullSecrets (#1922)
    
    Co-authored-by: Stephen Marshall <smarshall1980@gmail.com>
    
    * Convert Visual Guidelines Cursor Partial to React (#1915)
    
    * Converting title kit to ts (#1872)
    
    * Removed flow and Renamed file
    
    * Updated Types
    
    * Removed JSX file extentions from imports
    
    * Fix syntax error
    
    * Fix tests
    
    Co-authored-by: Erica Winne <erica.winne@powerhrg.com>
    
    * Converting caption to tsx (#1874)
    
    * Started Converting
    
    * Rename file to TSX and Updated types
    
    * Added/Joined & GlobalProps
    
    * Merge Master
    
    * Changed back default blank string
    
    Co-authored-by: Erica Winne <erica.winne@powerhrg.com>
    
    * Create cursor partial in tsx
    
    Co-authored-by: Stephen Marshall <smarshall1980@gmail.com>
    Co-authored-by: Erica Winne <erica.winne@powerhrg.com>
    
    * Conditionally use render_component method (#1926)
    
    Co-authored-by: Stephen Marshall <smarshall1980@gmail.com>
    
    * bundled
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Stephen Marshall <smarshall1980@gmail.com>
    Co-authored-by: Ryan Rodriguez <62253265+ryanmrodriguez@users.noreply.github.com>
    Co-authored-by: Gordon Hodanich <ghodanich@gmail.com>
    Co-authored-by: Erica Winne <erica.winne@powerhrg.com>
    Co-authored-by: Stephen Greer <stephen.greer@powerhrg.com>

--- playbook-website/app/javascript/components/VisualGuidelines/index.tsx
+++ playbook-website/app/javascript/components/VisualGuidelines/index.tsx
@@ -6,6 +6,7 @@ import React from 'react'
 import Colors from './Colors'
 import MaxWidth from './Examples/MaxWidth'
 import Positioning from './Examples/Positioning'
+import Spacing from './Examples/Spacing'
 import BorderRadius from './Examples/BorderRadius'
 import Display from './Examples/Display'
 import Cursor from './Examples/Cursor'
@@ -20,7 +21,11 @@ const VisualGuidelines = ({ examples }: {examples: {[key: string]: string}}): Re
           example={examples.positioning_jsx}
           tokensExample={examples.position_token}
       />
-      <BorderRadius tokensExample={examples.border_radius_tokens}/>
+      <Spacing
+          example={examples.spacing_global_props_jsx}
+          tokensExample={examples.spacing_tokens_jsx}
+      />
+      <BorderRadius tokensExample={examples.border_radius_tokens} />
       <Display example={examples.display_in_use_jsx} />
       <Cursor example={examples.cursor_jsx} />
       <FlexBox example={examples.justify_self_jsx} />
