ecff6dc1662deb9ee994da66d2e659624bb1c104
Carlos Palhares <chjunior@gmail.com>
2022-06-08 18:33:11 -0300

    Fix rendering of emphasis subkits

--- playbook/app/pb_kits/playbook/pb_home_address_street/city_emphasis.rb
+++ playbook/app/pb_kits/playbook/pb_home_address_street/city_emphasis.rb
@@ -2,15 +2,15 @@
 
 module Playbook
   module PbHomeAddressStreet
-    class HomeAddressCityEmphasis < Playbook::KitBase
-      # prop :address_house_style
-      # prop :address_house_style2
-      # prop :city_state
-      # prop :home_id, type: Playbook::Props::Number
-      # prop :home_url
-      # prop :territory
-      # prop :zip
-      # prop :dark, type: Playbook::Props::Boolean, default: false
+    class CityEmphasis < Playbook::KitBase
+      prop :address_house_style
+      prop :address_house_style2
+      prop :city_state
+      prop :home_id, type: Playbook::Props::Number
+      prop :home_url
+      prop :territory
+      prop :zip
+      prop :dark, type: Playbook::Props::Boolean, default: false
     end
   end
 end
--- playbook/app/pb_kits/playbook/pb_home_address_street/street_emphasis.rb
+++ playbook/app/pb_kits/playbook/pb_home_address_street/street_emphasis.rb
@@ -2,14 +2,14 @@
 
 module Playbook
   module PbHomeAddressStreet
-    class HomeAddressStreetEmphasis < Playbook::KitBase
-      # prop :address_house_style
-      # prop :address_house_style2
-      # prop :city_state_zip
-      # prop :home_id, type: Playbook::Props::Number
-      # prop :home_url
-      # prop :territory
-      # prop :dark, type: Playbook::Props::Boolean, default: false
+    class StreetEmphasis < Playbook::KitBase
+      prop :address_house_style
+      prop :address_house_style2
+      prop :city_state_zip
+      prop :home_id, type: Playbook::Props::Number
+      prop :home_url
+      prop :territory
+      prop :dark, type: Playbook::Props::Boolean, default: false
     end
   end
 end
