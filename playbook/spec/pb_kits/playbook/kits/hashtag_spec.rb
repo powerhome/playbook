0dc219b967762d4b1e6dde749efad66a23692007
Israel Molestina <molestinaisrael@gmail.com>
2022-08-22 15:44:39 -0400

    Passed in the correct methdo in the hashtag.spec.rb file on lines 31 and 32. link_option was the correct method to test for

--- playbook/spec/pb_kits/playbook/kits/hashtag_spec.rb
+++ playbook/spec/pb_kits/playbook/kits/hashtag_spec.rb
@@ -28,8 +28,8 @@ RSpec.describe Playbook::PbHashtag::Hashtag do
 
   describe "#window_target" do
     it "should allow links to open in a new window", :aggregate_failures do
-      expect(subject.new({}).target).to eq "_self"
-      expect(subject.new({ new_window: true }).target).to eq "_blank"
+      expect(subject.new({}).link_option).to eq "_self"
+      expect(subject.new({ new_window: true }).link_option).to eq "_blank"
     end
   end
 end
