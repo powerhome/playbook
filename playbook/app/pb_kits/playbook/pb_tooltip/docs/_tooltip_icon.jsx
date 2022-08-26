cf4523d533e2e0ec8384cb5dcd9d3e98a39a7aaf
Augusto Mallmann <augustocmallmann@gmail.com>
2022-08-26 12:36:54 -0400

    [PLAY-184] Tooltip - React Kit (#1994)
    * React Tooltip Kit
    
    * Tooltip Styles, Refinements and Examples
    
    * Add dark mode styles and props
    
    * Generate data and aria pros
    
    * Tooltip Jest Test
    
    * Add component interaction tests
    
    * Format document and remove unused function
    
    * Add Icon and Interaction Props
    
    * Update tooltip react documentation
    
    * dark theme as default
    
    * Remove light prop, make tooltip dark via css by default and fix scroll when interactive
    
    * change let to const
    
    * Remove white prop and fix CSS
    
    * Restored Rails Circle Example
    
    Co-authored-by: Jasper Furniss <jasper.furniss@powerhrg.com>

---
+++ playbook/app/pb_kits/playbook/pb_tooltip/docs/_tooltip_icon.jsx
@@ -0,0 +1,52 @@
+// @flow
+
+import React from 'react'
+import { Tooltip, Button, Flex } from '../..'
+
+const TooltipIcon = (props) => {
+  return (
+    <Flex flexDirection='row'
+        gap='md'
+        wrap
+    >
+      <Tooltip
+          icon='paper-plane'
+          placement='top'
+          text='Send Email'
+          zIndex={10}
+          {...props}
+      >
+        <Button text='Tooltip With Icon' />
+      </Tooltip>
+      <Tooltip
+          icon='paper-plane'
+          placement='bottom'
+          text='Send Email'
+          zIndex={10}
+          {...props}
+      >
+        <Button text='Tooltip With Icon' />
+      </Tooltip>
+      <Tooltip
+          icon='paper-plane'
+          placement='right'
+          text='Send Email'
+          zIndex={10}
+          {...props}
+      >
+        <Button text='Tooltip With Icon' />
+      </Tooltip>
+      <Tooltip
+          icon='paper-plane'
+          placement='left'
+          text='Send Email'
+          zIndex={10}
+          {...props}
+      >
+        <Button text='Tooltip With Icon' />
+      </Tooltip>
+    </Flex>
+  )
+}
+
+export default TooltipIcon
---
+++ playbook/app/pb_kits/playbook/pb_tooltip/tooltip.test.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { cleanup, render, screen, fireEvent } from "../utilities/test-utils";
+import { Button, Tooltip } from "..";
+
+function TooltipTest() {
+  const text = "this is a text",
+    placement = "top",
+    triggerText = "hover me",
+    zIndex = "10";
+
+  return (
+    <Tooltip
+        data={{ testid: "primary-test" }}
+        placement={placement}
+        text={text}
+        zIndex={zIndex}
+    >
+      <Button text={triggerText} />
+    </Tooltip>
+  );
+}
+
+test("renders the component", () => {
+  render(<TooltipTest />);
+
+  const kit = screen.getByTestId("primary-test");
+  expect(kit).toBeInTheDocument();
+  expect(kit).toHaveClass("pb_tooltip_kit");
+
+  cleanup();
+});
+
+test("opens on mouseenter", () => {
+  render(<TooltipTest />);
+
+  fireEvent.mouseEnter(screen.getByRole("tooltip_trigger"));
+  expect(screen.queryByRole("tooltip")).toBeInTheDocument();
+
+  cleanup();
+});
+
+test("closes on mouseleave", () => {
+  render(<TooltipTest />);
+
+  fireEvent.mouseEnter(screen.getByRole("tooltip_trigger"));
+  fireEvent.mouseLeave(screen.getByRole("tooltip_trigger"));
+  expect(screen.queryByRole("tooltip")).not.toBeInTheDocument();
+
+  cleanup();
+});
