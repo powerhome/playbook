288e338eef079e2564a07d1d1597952e2a85d61a
Stephen Marshall <smarshall1980@gmail.com>
2022-08-30 15:14:22 -0400

    Add jest test to date time kit

---
+++ playbook/app/pb_kits/playbook/pb_date_time/dateTime.test.js
@@ -0,0 +1,100 @@
+import React from 'react'
+import { render, screen } from '../utilities/test-utils'
+
+import DateTime from './_date_time'
+
+jest.setSystemTime(new Date('01/01/2020'));
+const DEFAULT_DATE = new Date()
+const testId = 'datetime-kit'
+
+describe('DateTime Kit', () => {
+  test('renders DatePicker className', () => {
+    render(
+      <DateTime
+          data={{ testid: testId }}
+          datetime={DEFAULT_DATE}
+          showDayOfWeek
+          showIcon
+      />
+    )
+
+    const kit = screen.getByTestId(testId)
+    expect(kit).toHaveClass('pb_date_time_md')
+  })
+  test('renders DatePicker text left', () => {
+    render(
+      <DateTime
+          data={{ testid: testId }}
+          datetime={DEFAULT_DATE}
+          showDayOfWeek
+          showIcon
+      />
+    )
+
+    const kit = screen.getByTestId(testId)
+    const text = kit.querySelector('.pb_date_kit_left')
+    expect(text.textContent).toEqual('Wed • Jan 1')
+  })
+  test('renders DatePicker text right', () => {
+    render(
+      <DateTime
+          data={{ testid: testId }}
+          datetime={DEFAULT_DATE}
+          showDayOfWeek
+          showIcon
+      />
+    )
+
+    const kit = screen.getByTestId(testId)
+    const text = kit.querySelector('.pb_time_kit_md.ml_sm')
+    const clock = kit.querySelector('.pb_icon_kit.far.fa-fw.fa-clock')
+
+    expect(clock).toBeInTheDocument()
+    expect(text.textContent).toEqual(' 12:00a EST')
+  })
+  test('renders DatePicker text right no icon', () => {
+    render(
+      <DateTime
+          data={{ testid: testId }}
+          datetime={DEFAULT_DATE}
+          showDayOfWeek
+      />
+    )
+
+    const kit = screen.getByTestId(testId)
+    const clock = kit.querySelector('.pb_icon_kit.far.fa-fw.fa-clock')
+
+    expect(clock).toBeNull()
+  })
+  test('renders DatePicker text right timezone', () => {
+    render(
+      <DateTime
+          data={{ testid: testId }}
+          datetime={DEFAULT_DATE}
+          showDayOfWeek
+          timeZone="Asia/Tokyo"
+      />
+    )
+
+    const kit = screen.getByTestId(testId)
+    const text = kit.querySelector('.pb_time_kit_md.ml_sm')
+
+    expect(text.textContent).toEqual('2:00p JST')
+  })
+  test('renders DatePicker size', () => {
+    render(
+      <DateTime
+          data={{ testid: testId }}
+          datetime={DEFAULT_DATE}
+          showDayOfWeek
+          size="sm"
+      />
+    )
+
+    const kit = screen.getByTestId(testId)
+    const rightSide = kit.querySelector('.pb_time_kit_sm.ml_sm')
+
+    expect(kit).toHaveClass('pb_date_time_sm')
+    expect(rightSide).toBeInTheDocument()
+  })
+})
