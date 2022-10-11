3d6d021490bc0c67887c4ac3b3871cc2ca3cb21e
nida.ghuman <nidaqg@gmail.com>
2022-10-11 10:27:12 -0400

    PLAY-399-Textarea Doc Bug Fix

--- playbook/app/pb_kits/playbook/pb_textarea/docs/_textarea_error.jsx
+++ playbook/app/pb_kits/playbook/pb_textarea/docs/_textarea_error.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Textarea } from '../..'
 
 const TextareaError = (props) => {
+  const [value, setValue] = useState('default value text')
+  const handleChange = (event) => {
+    setValue(event.target.value)
+  }
   return (
     <div>
       <Textarea
           error="This field has an error!"
           label="Label"
           name="comment"
+          onChange={(e)=> handleChange(e)}
           placeholder="Placeholder text"
-          value="Default value text"
+          value={value}
           {...props}
       />
 
