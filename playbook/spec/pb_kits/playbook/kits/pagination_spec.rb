f73dab0d43a4b1e6d9dab00885b2150d3d6189af
nida.ghuman <nidaqg@gmail.com>
2022-10-25 14:08:05 -0400

    Fixed spec test

--- playbook/spec/pb_kits/playbook/kits/pagination_spec.rb
+++ playbook/spec/pb_kits/playbook/kits/pagination_spec.rb
@@ -5,7 +5,9 @@ require_relative "../../../../app/pb_kits/playbook/pb_pagination/pagination"
 RSpec.describe Playbook::PbPagination::Pagination do
   subject { Playbook::PbPagination::Pagination }
 
-  it { is_expected.to define_partial }
-
-  # Do not leave this file blank. Use other spec files for example tests.
+  describe "#classname" do
+    it "returns namespaced class name", :aggregate_failures do
+      expect(subject.new({}).classname).to eq "pb_pagination"
+    end
+  end
 end
