0f6ebf510b99c3b0ba54a9d8449b1f0262296378
nida.ghuman <nidaqg@gmail.com>
2022-11-01 09:05:18 -0400

    PLAY-446-Set up rails version of Dialog kit, docs not done

--- playbook/app/pb_kits/playbook/pb_dialog/dialog.rb
+++ playbook/app/pb_kits/playbook/pb_dialog/dialog.rb
@@ -2,12 +2,7 @@
 
 module Playbook
   module PbDialog
-    class Dialog
-      include Playbook::Props
-
-      partial "pb_dialog/dialog"
-
-      prop :ref
+    class Dialog < Playbook::KitBase
       prop :cancel_button
       prop :closeable, type: Playbook::Props::Boolean, default: true
       prop :confirm_button
@@ -15,6 +10,16 @@ module Playbook
       prop :onchange
       prop :onclose
       prop :onconfirm
+      prop :id
+      prop :full_height, type: Playbook::Props::Boolean, default: false
+      prop :loading, type: Playbook::Props::Boolean, default: false
+      prop :portal_classname
+      prop :placement, type: Playbook::Props::Enum,
+                       values: %w[left center right],
+                       default: "center"
+      prop :should_close_on_overlay_click, type: Playbook::Props::Boolean, default: true
+      prop :status, type: Playbook::Props::String,
+                    default: ""
       prop :opened, type: Playbook::Props::Boolean, default: false
       prop :size, type: Playbook::Props::Enum,
                   values: %w[sm md lg content],
@@ -23,24 +28,33 @@ module Playbook
       prop :title
       prop :trigger
 
+      def classname
+        generate_classname("pb_dialog")
+      end
+
       def dialog_options
         {
           id: id,
-          ref: ref,
+          onCancel: oncancel,
+          onChange: onchange,
+          onClose: onclose,
+          onConfirm: onconfirm,
           trigger: trigger,
           className: classname,
           cancelButton: cancel_button,
           closeable: closeable,
           confirmButton: confirm_button,
-          onCancel: oncancel,
-          onChange: onchange,
-          onClose: onclose,
-          onConfirm: onconfirm,
           opened: opened,
           size: size,
           text: text,
           title: title,
-        }
+          fullHeight: full_height,
+          loading: loading,
+          portalClassName: portal_classname,
+          placement: placement,
+          shouldCloseOnOverlayClick: should_close_on_overlay_click,
+          status: status,
+        }.compact
       end
     end
   end
