e79447481a1d8b0e494cf71369810817e6328b4b
nida.ghuman <nidaqg@gmail.com>
2022-11-02 11:38:10 -0400

    Fixed rspec test

--- playbook/spec/pb_kits/playbook/kits/dialog_spec.rb
+++ playbook/spec/pb_kits/playbook/kits/dialog_spec.rb
@@ -5,7 +5,13 @@ require_relative "../../../../app/pb_kits/playbook/pb_dialog/dialog"
 RSpec.describe Playbook::PbDialog::Dialog do
   subject { Playbook::PbDialog::Dialog }
 
-  it { is_expected.to define_partial }
-
-  # Do not leave this file blank. Use other spec files for example tests.
+  it { is_expected.to define_prop(:title) }
+  it { is_expected.to define_prop(:text) }
+  it { is_expected.to define_prop(:confirm_button) }
+  it { is_expected.to define_prop(:cancel_button) }
+  it {
+    is_expected.to define_enum_prop(:size)
+      .with_values("sm", "md", "lg", "xl", "status_size", "content")
+      .with_default("md")
+  }
 end
