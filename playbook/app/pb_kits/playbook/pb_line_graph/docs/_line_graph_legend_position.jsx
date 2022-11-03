51326bb7af8861f4363865eebffd143386f109a8
Israel.Molestina <molestinaisrael@gmail.com>
2022-11-03 14:57:33 -0400

    created new doc for line_graph to showcase 5 new legend position props

---
+++ playbook/app/pb_kits/playbook/pb_line_graph/docs/_line_graph_legend_position.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { LineGraph } from '../..'
+
+const data = [{
+  name: 'Installation',
+  data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175],
+}, {
+  name: 'Manufacturing',
+  data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434],
+}, {
+  name: 'Sales & Distribution',
+  data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387],
+}, {
+  name: 'Project Development',
+  data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227],
+}, {
+  name: 'Other',
+  data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111],
+}]
+
+const LineGraphLegendPosition = (props) => (
+  <div>
+    <LineGraph
+        align='left'
+        axisTitle="Number of Employees"
+        chartData={data}
+        id="legend-position"
+        layout="vertical"
+        legend
+        title="Line Graph with Legend"
+        verticalAlign="top"
+        x={10}
+        xAxisCategories={['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug']}
+        y={100}
+        yAxisMin={0}
+        {...props}
+    />
+  </div>
+)
+
+export default LineGraphLegendPosition
