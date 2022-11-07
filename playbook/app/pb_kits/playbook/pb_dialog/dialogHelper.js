464043b149596d68bdded9707678668329b1203d
nida.ghuman <nidaqg@gmail.com>
2022-11-07 13:00:04 -0500

    Fixed error with button clicks only registering with edge clicks

--- playbook/app/pb_kits/playbook/pb_dialog/dialogHelper.js
+++ playbook/app/pb_kits/playbook/pb_dialog/dialogHelper.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     const openTrigger = document.querySelectorAll('[data-open-dialog]');
     const closeTrigger = document.querySelectorAll('[data-close-dialog]');
-    openTrigger.forEach((open) => {open.addEventListener("click", (event) => {
-      if (!event?.target?.dataset?.openDialog) return;
-        document.getElementById(event.target.dataset.openDialog).showModal()
+    openTrigger.forEach((open) => {open.addEventListener("click", () => {
+        var openTriggerData = open.dataset.openDialog
+        document.getElementById(openTriggerData).showModal()
       });
     })
-    closeTrigger.forEach((close) => {close.addEventListener("click", (event) => {
-      if (!event?.target?.dataset?.closeDialog) return;
-        document.getElementById(event.target.dataset.closeDialog).close()
+    closeTrigger.forEach((close) => {close.addEventListener("click", () => {
+        var closeTriggerData = close.dataset.closeDialog
+        document.getElementById(closeTriggerData).close()
     })
   })
   });
