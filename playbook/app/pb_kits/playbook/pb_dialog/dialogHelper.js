e910485f247be2820e073b2e6bd100ec0d817421
Jasper Furniss <jasper.furniss@powerhrg.com>
2022-11-08 12:32:31 -0500

    Cleanup

--- playbook/app/pb_kits/playbook/pb_dialog/dialogHelper.js
+++ playbook/app/pb_kits/playbook/pb_dialog/dialogHelper.js
@@ -1,15 +1,16 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const openTrigger = document.querySelectorAll('[data-open-dialog]');
-    const closeTrigger = document.querySelectorAll('[data-close-dialog]');
-    openTrigger.forEach((open) => {open.addEventListener("click", () => {
-        var openTriggerData = open.dataset.openDialog
-        document.getElementById(openTriggerData).showModal()
-      });
-    })
-    closeTrigger.forEach((close) => {close.addEventListener("click", () => {
-        var closeTriggerData = close.dataset.closeDialog
-        document.getElementById(closeTriggerData).close()
+document.addEventListener("DOMContentLoaded", function () {
+  const openTrigger = document.querySelectorAll('[data-open-dialog]');
+  const closeTrigger = document.querySelectorAll('[data-close-dialog]');
+  openTrigger.forEach((open) => {
+    open.addEventListener("click", () => {
+      var openTriggerData = open.dataset.openDialog
+      document.getElementById(openTriggerData).showModal()
+    });
+  })
+  closeTrigger.forEach((close) => {
+    close.addEventListener("click", () => {
+      var closeTriggerData = close.dataset.closeDialog
+      document.getElementById(closeTriggerData).close()
     })
   })
-  });
-  
+});
