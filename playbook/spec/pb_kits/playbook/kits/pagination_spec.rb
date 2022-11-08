14520dc10734cf81202435be7259b3717219136a
Israel.Molestina <molestinaisrael@gmail.com>
2022-11-08 13:11:47 -0500

    removed spec test for pagination kit

--- playbook/spec/pb_kits/playbook/kits/pagination_spec.rb
+++ playbook/spec/pb_kits/playbook/kits/pagination_spec.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-require_relative "../../../../app/pb_kits/playbook/pb_pagination/pagination"
+# require_relative "../../../../app/pb_kits/playbook/pb_pagination/pagination"
 
-RSpec.describe Playbook::PbPagination::Pagination do
-  subject { Playbook::PbPagination::Pagination }
+# RSpec.describe Playbook::PbPagination::Pagination do
+#   subject { Playbook::PbPagination::Pagination }
 
-  it { is_expected.to define_partial }
+#   it { is_expected.to define_partial }
 
-  # Do not leave this file blank. Use other spec files for example tests.
-end
+#   # Do not leave this file blank. Use other spec files for example tests.
+# end
