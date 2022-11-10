2a7b50fcd27d311b8b68558e466ec8f2b8e3fb6a
nida.ghuman <nidaqg@gmail.com>
2022-11-10 11:21:36 -0500

    Fixed error where openmodal was getting called multiple times

--- playbook/app/pb_kits/playbook/pb_dialog/dialogHelper.js
+++ playbook/app/pb_kits/playbook/pb_dialog/dialogHelper.js
@@ -1,16 +1,21 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const openTrigger = document.querySelectorAll('[data-open-dialog]');
-  const closeTrigger = document.querySelectorAll('[data-close-dialog]');
+const dialogHelper = () => {
+  const openTrigger = document.querySelectorAll("[data-open-dialog]");
+  const closeTrigger = document.querySelectorAll("[data-close-dialog]");
+
   openTrigger.forEach((open) => {
     open.addEventListener("click", () => {
-      var openTriggerData = open.dataset.openDialog
-      document.getElementById(openTriggerData).showModal()
+      var openTriggerData = open.dataset.openDialog;
+      if (document.getElementById(openTriggerData).open) return;
+      document.getElementById(openTriggerData).showModal();
     });
-  })
+  });
+
   closeTrigger.forEach((close) => {
     close.addEventListener("click", () => {
-      var closeTriggerData = close.dataset.closeDialog
-      document.getElementById(closeTriggerData).close()
-    })
-  })
-});
+      var closeTriggerData = close.dataset.closeDialog;
+      document.getElementById(closeTriggerData).close();
+    });
+  });
+};
+
+export default dialogHelper;
