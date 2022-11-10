2ee6bc640cd6ff4dc83030b279725df2ff3a35dc
Carlos Lima <carloslimasd@gmail.com>
2022-11-10 12:51:34 -0300

    Changing bg color for the selected item

--- playbook/app/pb_kits/playbook/pb_selectable_list/_selectable_list.jsx
+++ playbook/app/pb_kits/playbook/pb_selectable_list/_selectable_list.jsx
@@ -1,5 +1,5 @@
 /* @flow */
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import classnames from 'classnames'
 
 import { buildAriaProps, buildCss, buildDataProps } from '../utilities/props'
@@ -29,7 +29,30 @@ const SelectableList = (props: SelectableListProps) => {
   const ariaProps = buildAriaProps(aria)
   const classes = classnames(buildCss('pb_selectable_list_kit'), globalProps(props), className)
   const dataProps = buildDataProps(data)
+  const isRadio = props.variant === "radio"
+  const [selectedRadioValue, setSelectedRadioValue] = useState()
+  
+  const onChangeRadioValue = (event) => {
+    setSelectedRadioValue(event.target.value)
+  }
 
+  let newChildren = children
+
+  useEffect(() => {
+    if (isRadio) {
+      newChildren = children.map(({props}) => {
+        return (
+          <SelectableListItem
+              {...props}
+              className={classnames(selectedRadioValue === props.value ? "checked_item" : "", props.className)}
+              key={props.value}
+              onChange={(evt) => { onChangeRadioValue(evt); props?.onChange?.(evt); }}
+          />
+        )
+      })
+    }
+  }, []);
+  
   return (
     <div
         {...ariaProps}
@@ -38,7 +61,7 @@ const SelectableList = (props: SelectableListProps) => {
         id={id}
     >
       <List {...props}>
-        {children}
+        {newChildren}
       </List>
     </div>
   )
--- playbook/app/pb_kits/playbook/pb_selectable_list/docs/_selectable_list_radio.jsx
+++ playbook/app/pb_kits/playbook/pb_selectable_list/docs/_selectable_list_radio.jsx
@@ -2,26 +2,37 @@ import React from 'react'
 import { SelectableList } from '../..'
 
 const SelectableListDefault = (props) => {
+  // const [value, setValue] = useState()
+  
+  // const onChange = ({ target }) => {
+  //   setValue(target.value)
+  // }
   return (
     <div>
       <SelectableList variant="radio">
         <SelectableList.Item
+            //className={value === "1" ? "checked_item" : ""}
             label="Small"
             name="radio"
             value="1"
+            //onChange={onChange}
             {...props}
         />
         <SelectableList.Item
+            //className={value === "2" ? "checked_item" : ""}
             defaultChecked
             label="Medium"
             name="radio"
             value="2"
+            //onChange={onChange}
             {...props}
         />
         <SelectableList.Item
+            //className={value === "3" ? "checked_item" : ""}
             label="Large"
             name="radio"
             value="3"
+            //onChange={onChange}
             {...props}
         />
       </SelectableList>
