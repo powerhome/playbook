6ad48836c09417edc1f216a53cea2253cc94d629
Jasper Furniss <jasper.furniss@powerhrg.com>
2022-11-14 14:15:08 -0500

    Added Hover State to Buttons with Link Variant (#2169)

--- playbook/app/pb_kits/playbook/pb_button/docs/_button_default.jsx
+++ playbook/app/pb_kits/playbook/pb_button/docs/_button_default.jsx
@@ -1,32 +1,33 @@
-import React from 'react'
-import { Button } from '../../'
+import React from "react"
+import { Button } from "../../"
 
 const ButtonDefault = (props) => (
   <div>
     <Button
-        marginRight="xl"
-        onClick={() => alert('button clicked!')}
-        text="Button Primary"
+        marginRight='lg'
+        onClick={() => alert("button clicked!")}
+        text='Button Primary'
         {...props}
-    />
-    {' '}
+    />{" "}
     <Button
-        onClick={() => alert('button clicked!')}
-        text="Button Secondary"
-        variant="secondary"
+        marginRight='lg'
+        onClick={() => alert("button clicked!")}
+        text='Button Secondary'
+        variant='secondary'
         {...props}
-    />
-    {' '}
+    />{" "}
     <Button
-        onClick={() => alert('button clicked!')}
-        text="Button Link"
-        variant="link"
+        marginRight='lg'
+        onClick={() => alert("button clicked!")}
+        text='Button Link'
+        variant='link'
         {...props}
     />
     <Button
         disabled
-        onClick={() => alert('button clicked!')}
-        text="Button Disabled"
+        marginRight='lg'
+        onClick={() => alert("button clicked!")}
+        text='Button Disabled'
         {...props}
     />
   </div>
