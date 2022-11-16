e641037d90509d88c88b91fb7a7e3bf716d4d19f
Carlos Lima <carloslimasd@gmail.com>
2022-11-16 16:50:04 -0300

    Changing checkbox bg color for Rails

--- playbook/app/pb_kits/playbook/pb_selectable_list/selectable_list.rb
+++ playbook/app/pb_kits/playbook/pb_selectable_list/selectable_list.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "securerandom"
+
 module Playbook
   module PbSelectableList
     class SelectableList < Playbook::KitBase
@@ -14,6 +16,10 @@ module Playbook
 
       def classname
         generate_classname("pb_selectable_list_kit")
+      end
+
+      def get_id(item)
+        item[:id] || ("a".."z").to_a.sample(12).join
       end
     end
   end
