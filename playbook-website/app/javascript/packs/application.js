41212976e1a76ab07fd0203d078ff5ed5cf7cb50
nida.ghuman <nidaqg@gmail.com>
2022-11-16 13:01:28 -0500

    PLAY-490- removed FA from dist, imported into playbook via yarn

--- playbook-website/app/javascript/packs/application.js
+++ playbook-website/app/javascript/packs/application.js
@@ -4,8 +4,8 @@ import WebpackerReact from 'webpacker-react'
 
 import 'playbook-ui/dist/playbook-rails'
 import 'playbook-ui/dist/playbook-doc' // playbook-react doc components
-import 'playbook-ui/dist/fonts/fontawesome-min'
-import 'playbook-ui/dist/fonts/regular-min'
+import '@fortawesome/fontawesome-pro/css/all'
+import '@fortawesome/fontawesome-pro/js/all'
 
 import '../site_styles/main.scss'
 
