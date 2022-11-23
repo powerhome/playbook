66bdab86a307026ab0c5cd67eb5dff6737894d36
Carlos Lima <carloslimasd@gmail.com>
2022-11-23 11:44:27 -0300

    Converting Enhanced Element to Typescript

--- playbook/app/pb_kits/playbook/pb_enhanced_element/element_observer.ts
+++ playbook/app/pb_kits/playbook/pb_enhanced_element/element_observer.ts
@@ -1,34 +1,45 @@
+import PbEnhancedElement from "./index"
+
 export default class ElementObserver {
-  constructor(matchDelegate, target = document) {
+  matchDelegate: PbEnhancedElement
+  target: Document
+  _mutationObserver: MutationObserver
+
+  constructor(matchDelegate: PbEnhancedElement, target = document) {
     this.matchDelegate = matchDelegate
     this.target = target
   }
 
-  start() {
+  get mutationObserver(): MutationObserver {
+    return this._mutationObserver =
+      this._mutationObserver || new MutationObserver((mutationList) => this.processMutationList(mutationList))
+  }
+
+  start(): void {
     this.mutationObserver.observe(this.target, { attributes: true, childList: true, subtree: true })
     this.catchup()
   }
 
-  stop() {
+  stop(): void {
     this.mutationObserverdisconnect()
   }
 
-  catchup() {
-    this.handleAdditions(this.matchDelegate.matches(this.target))
+  catchup(): void {
+    this.handleAdditions(this.matchDelegate.matches(this.target as unknown as Element))
   }
 
-  processMutationList(mutationList) {
+  processMutationList(mutationList: Array<MutationRecord>): void {
     for (const mutation of mutationList) {
       if (mutation.type == 'attributes') {
-        this.processAttributeChange(mutation.target)
+        this.processAttributeChange(mutation.target as Element)
       } else if (mutation.type == 'childList') {
-        this.processRemovedNodes(Array.from(mutation.removedNodes))
-        this.processAddedNodes(Array.from(mutation.addedNodes))
+        this.processRemovedNodes(Array.from(mutation.removedNodes) as Array<Element>)
+        this.processAddedNodes(Array.from(mutation.addedNodes) as Array<Element>)
       }
     }
   }
 
-  processAttributeChange(node) {
+  processAttributeChange(node: Element): void | Array<Element> {
     if (node.nodeType !== Node.ELEMENT_NODE) return
 
     const matches = this.matchDelegate.matches(node)
@@ -38,30 +49,25 @@ export default class ElementObserver {
     this.handleAdditions(matches)
   }
 
-  processRemovedNodes(nodes) {
+  processRemovedNodes(nodes: Array<Element>): void {
     for (const node of nodes) {
       if (node.nodeType !== Node.ELEMENT_NODE) continue
       this.handleRemovals(this.matchDelegate.matches(node))
     }
   }
 
-  processAddedNodes(nodes) {
+  processAddedNodes(nodes: Array<Element>): void {
     for (const node of nodes) {
       if (node.nodeType !== Node.ELEMENT_NODE) continue
       this.handleAdditions(this.matchDelegate.matches(node))
     }
   }
 
-  handleRemovals(elements) {
+  handleRemovals(elements: Array<Element>): void {
     for (const element of elements) this.matchDelegate.removeMatch(element)
   }
 
-  handleAdditions(elements) {
+  handleAdditions(elements: Array<Element>): void {
     for (const element of elements) this.matchDelegate.addMatch(element)
-  }
-
-  get mutationObserver() {
-    return this._mutationObserver =
-      this._mutationObserver || new MutationObserver((mutationList) => this.processMutationList(mutationList))
   }
 }
