e2217f6a97e223a7ebd6949de1189535d83ddb1e
Stephen Marshall <smarshall1980@gmail.com>
2022-12-05 15:54:33 -0500

    Datepicker closeOnSelect (#2187)

--- playbook/app/pb_kits/playbook/pb_date_picker/docs/_date_picker_time.jsx
+++ playbook/app/pb_kits/playbook/pb_date_picker/docs/_date_picker_time.jsx
@@ -1,22 +1,38 @@
-import React from 'react'
+/* eslint-disable react/no-multi-comp */
+import React, { useMemo, useState } from 'react'
 
 import DatePicker from '../_date_picker'
+import Body from '../../pb_body/_body'
 
 const DEFAULT_DATE = new Date()
 DEFAULT_DATE.setHours(12)
 DEFAULT_DATE.setMinutes(0)
 
-const DatePickerTime = (props) => (
-  <div>
-    <DatePicker
-        defaultDate={DEFAULT_DATE}
-        enableTime
-        pickerId="date-picker-time"
-        showTimezone
-        {...props}
-    />
+const DatePickerTime = (props) => {
+  const [selectedDateTime, setSelectedDateTime] = useState(DEFAULT_DATE)
 
-  </div>
-)
+  const refExample = React.createRef()
+
+  const handleOnInputChanged = (dateTime) => {
+    setSelectedDateTime(dateTime)
+  }
+
+  return (
+    <div ref={refExample}>
+      <Body marginBottom="md">{selectedDateTime.toString()}</Body>
+      {useMemo(() => (
+        <DatePicker
+            closeOnSelect={false}
+            defaultDate={DEFAULT_DATE}
+            enableTime
+            onChange={handleOnInputChanged}
+            pickerId="date-picker-time"
+            showTimezone
+            {...props}
+        />
+      ), [props])}
+    </div>
+  )
+}
 
 export default DatePickerTime
