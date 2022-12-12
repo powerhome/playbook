1c99ab50f62c482411b174d900e0e8548f0b2b08
Israel.Molestina <molestinaisrael@gmail.com>
2022-12-12 14:19:34 -0500

    added an array of objects to pass into the pagination kit

--- playbook/app/pb_kits/playbook/pb_pagination/pagination.rb
+++ playbook/app/pb_kits/playbook/pb_pagination/pagination.rb
@@ -3,11 +3,11 @@
 module Playbook
   module PbPagination
     class Pagination < ::Playbook::KitBase
-      # require "json"
-      # file = File.read("/.user.json")
-      # data = JSON.parse file
-
-      # @user_data = data.paginate(page: params[:page], per_page: 20)
+      prop :data, type: Playbook::Props::Array,
+                  default: []
+      def page_data
+        data.paginate(page: params[:page], per_page: 1)
+      end
     end
   end
 end
