dd3dd78e18a024f9f89cc6580354cab2dfa55c7d
Carlos Lima <carloslimasd@gmail.com>
2022-12-16 14:40:24 -0300

    Implementing a Sandpack Example

--- playbook-website/app/javascript/components/PbKitReact/index.tsx
+++ playbook-website/app/javascript/components/PbKitReact/index.tsx
@@ -1,14 +1,41 @@
 import React from 'react'
-
 import { Title } from 'playbook-ui'
+import { Sandpack } from '@codesandbox/sandpack-react'
+import TableSmCode from 'playbook-ui/app/pb_kits/playbook/pb_table/docs/_table_sm'
 
 const PbKitReact = ({ kit }) => {
+
   return (
     <>
       <Title
           size={1}
           tag="h1"
           text={kit}
+      />
+
+      <Sandpack
+        theme="dark" //https://sandpack.codesandbox.io/docs/getting-started/themes
+        template="react" //https://sandpack.codesandbox.io/docs/getting-started/custom-content#templates
+        files={{
+          "/App.js": TableSmCode, //https://sandpack.codesandbox.io/docs/getting-started/custom-content#custom-file-contents
+        }}
+        customSetup={{
+          dependencies: { //https://sandpack.codesandbox.io/docs/getting-started/custom-content#npm-dependencies
+            'playbook-ui': 'latest',
+          },
+        }}
+        options={{ //https://sandpack.codesandbox.io/docs/getting-started/custom-ui#editor-settings
+          showLineNumbers: true,
+          externalResources: [ 
+            /* https://sandpack.codesandbox.io/docs/getting-started/custom-content#static-external-resources
+              We can use this config to get all the CSS we would need from Playbook
+              'playbook-ui/dist/reset.css',
+              'playbook-ui/dist/playbook.css',
+              'playbook-ui/dist/fonts/fontawesome-min',
+              'playbook-ui/dist/fonts/regular-min',
+            */
+          ],
+        }}
       />
     </>
   )
--- playbook/app/pb_kits/playbook/pb_table/docs/_table_sm.jsx
+++ playbook/app/pb_kits/playbook/pb_table/docs/_table_sm.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+/*
+I tried 4 options to pass this file as a string.
+- We could use "source" from Rails as Jasper did here: https://github.com/powerhome/playbook/commit/5f01b2fcaff67c5b2944a799908681a1e1db6bc5#diff-9933dfd1f79c7c87374bd19e7575b231da08f834b3360d7c02e6dc1d0fd5d100
+- We could use MDX lib to serialize the code: https://github.com/hashicorp/next-mdx-remote
+- We could use raw-loader from Asset Modules: https://webpack.js.org/guides/asset-modules/
+- Or we could just export the string in the file as I did below.
 
-import Table from '../_table'
+But even so, in the end, we would have to change the document a little bit to turn it into "real code" without errors.
 
-const TableSm = (props) => {
+For example: for this one, I had to remove the parameter "props" (because we were not using it) and change the way that we import the component Table from Playbook.
+*/
+const TableSmCode = `import React from 'react'
+
+import { Table } from 'playbook-ui'
+
+const TableSm = () => {
   return (
     <Table
         size="sm"
-        {...props}
     >
       <thead>
         <tr>
@@ -44,4 +54,6 @@ const TableSm = (props) => {
   )
 }
 
-export default TableSm
+export default TableSm`
+
+export default TableSmCode
