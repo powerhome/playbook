ab326781a083233395a96c2765231c28277f8f56
Israel.Molestina <molestinaisrael@gmail.com>
2023-01-04 11:40:26 -0500

    turning hash into array before passing into pagination kit

--- playbook/app/pb_kits/playbook/pb_pagination/pagination.rb
+++ playbook/app/pb_kits/playbook/pb_pagination/pagination.rb
@@ -7,7 +7,11 @@ module Playbook
       prop :data, type: Playbook::Props::Array,
                   default: []
       def page_data
-        data.paginate(page: params[:page], per_page: 1)
+        new_user_array = []
+        data.each do |user|
+          new_user_array.push(user)
+        end
+        new_user_array.paginate(page: params[:page], per_page: 1)
       end
     end
   end
