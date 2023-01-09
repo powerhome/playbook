9336c96851b7e2f2a2b6be7cfffc9f6ce138cfea
nida.ghuman <nidaqg@gmail.com>
2023-01-09 14:09:52 -0500

    Additional jest tests for Popover

--- playbook/app/pb_kits/playbook/pb_popover/popover.test.js
+++ playbook/app/pb_kits/playbook/pb_popover/popover.test.js
@@ -33,4 +33,59 @@ describe("Popover Kit", () => {
     expect(kit).toHaveClass("pb_popover_kit");
   });
 
+  test("renders Popover with z index", async () => {
+    const setStateMock = jest.fn();
+    const useStateMock = (useState) => [useState, setStateMock];
+    jest.spyOn(React, "useState").mockImplementationOnce(useStateMock);
+    const popoverReference = (
+      <Button onClick={setStateMock(!useStateMock)} 
+          text="Click Me"
+       />
+    );
+
+  render(
+      <PbReactPopover
+          data={{ testid: testId }}
+          offset
+          placement="top"
+          reference={popoverReference}
+          show={useStateMock}
+          zIndex={3}
+      >
+        {"A Popover"}
+      </PbReactPopover>
+    );
+
+    await fireEvent.click(screen.getByText(/click me/i));
+    expect(screen.getByText("A Popover")).toHaveClass("pb_popover_body z_index_3");
+  });
+
+  test("renders Popover with max height and max width", async () => {
+    const setStateMock = jest.fn();
+    const useStateMock = (useState) => [useState, setStateMock];
+    jest.spyOn(React, "useState").mockImplementationOnce(useStateMock);
+    const popoverReference = (
+      <Button onClick={setStateMock(!useStateMock)} 
+          text="Click Me"
+       />
+    );
+
+  render(
+      <PbReactPopover
+          data={{ testid: testId }}
+          maxHeight="150px"
+          maxWidth="240px"
+          offset
+          placement="top"
+          reference={popoverReference}
+          show={useStateMock}
+      >
+        {"A Popover"}
+      </PbReactPopover>
+    );
+
+    await fireEvent.click(screen.getByText(/click me/i));
+    expect(screen.getByText("A Popover")).toHaveClass("pb_popover_body max_width_240px overflow_handling");
+  });
+
 });
