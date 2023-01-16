a5f0ef4ae7cd931b7f63179045e88d58a9b24962
Jason Cypret <jason@jasoncypret.com>
2023-01-16 12:30:07 -0600

    fixed broken spec

--- playbook/spec/pb_kits/playbook/kits/paginate_spec.rb
+++ playbook/spec/pb_kits/playbook/kits/paginate_spec.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
+require "will_paginate"
+require "playbook/pagination_renderer"
 require_relative "../../../../app/pb_kits/playbook/pb_paginate/paginate"
 
 RSpec.describe Playbook::PbPaginate::Paginate do
   subject { Playbook::PbPaginate::Paginate }
 
-  it { is_expected.to define_partial }
-
-  # Do not leave this file blank. Use other spec files for example tests.
+  it { is_expected.to define_prop(:model) }
 end
