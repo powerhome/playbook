b8c1a6263207da0ececc2b0d402dbd81cdfb070b
nida.ghuman <nidaqg@gmail.com>
2023-01-20 08:40:52 -0500

    Added HQ address + styles

--- playbook/app/pb_kits/playbook/pb_map/docs/_map_default.jsx
+++ playbook/app/pb_kits/playbook/pb_map/docs/_map_default.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import Map from '../_map'
-import { Map as PigeonMap, Marker } from "pigeon-maps"
+import { Map as PigeonMap, Marker, ZoomControl } from "pigeon-maps"
 
 const MapDefault = () => (
   <div>
     <Map>
-    <PigeonMap defaultCenter={[50.879, 4.6997]} 
-        defaultZoom={11}
-        height={300} 
-    >
-      <Marker anchor={[50.879, 4.6997]}
-          width={50}  
-      />
-    </PigeonMap>
-      </Map>
+      <PigeonMap defaultCenter={[39.831690, -75.383050]} 
+          defaultZoom={11}
+          height={600} 
+      >
+        <Marker anchor={[39.831690, -75.383050]}
+            width={50}  
+        />
+        <ZoomControl />
+      </PigeonMap>
+    </Map>
   </div>
 )
 
