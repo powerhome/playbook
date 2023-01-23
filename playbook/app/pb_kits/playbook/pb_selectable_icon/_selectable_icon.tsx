8d015c7f24d08004e731fb769266036673c308a9
Carlos Lima <carloslimasd@gmail.com>
2023-01-23 15:42:09 -0300

    [PLAY-527] Converting Selectable Icon kit to Typescript (#2259)
    * Converting Selectable Icon kit to Typescript
    
    * Removing unnecessary props
    
    * Fixing some warnings and removing unnecessary props
    
    * Adding tests
    
    Co-authored-by: Jasper Furniss <jasper.furniss@powerhrg.com>

--- playbook/app/pb_kits/playbook/pb_selectable_icon/_selectable_icon.tsx
+++ playbook/app/pb_kits/playbook/pb_selectable_icon/_selectable_icon.tsx
@@ -1,23 +1,20 @@
 /* @flow */
-
 import React from 'react'
 import classnames from 'classnames'
-
 import { globalProps } from '../utilities/globalProps'
 import {
   buildAriaProps,
   buildCss,
   buildDataProps,
 } from '../utilities/props'
-
 import Icon from '../pb_icon/_icon'
 import Title from '../pb_title/_title'
 
 type SelectableIconProps = {
-  aria?: Object,
+  aria?: {[key: string]: string},
   checked?: boolean,
   className?: string,
-  customIcon?: SVGElement,
+  customIcon?: {[key: string] :SVGElement},
   disabled?: boolean,
   data?: Object,
   icon: string,
@@ -59,54 +56,54 @@ const SelectableIcon = ({
   )
 
   const inputType = multi === false ? 'radio' : 'checkbox'
-
   const inputIdPresent = inputId !== null ? inputId : name
 
   return (
     <div
-        {...ariaProps}
-        {...dataProps}
-        className={classes}
+      {...ariaProps}
+      {...dataProps}
+      className={classes}
     >
-      <If condition={inputs === 'disabled'}>
+      {inputs === 'disabled' && (
         <>
           <Icon
-              customIcon={customIcon}
-              icon={icon}
-              size="2x"
+            customIcon={customIcon}
+            icon={icon}
+            size="2x"
           />
           <Title
-              size={4}
-              tag="h4"
-              text={text}
+            size={4}
+            tag="h4"
+            text={text}
           />
         </>
-      </If>
-      <If condition={inputs === 'enabled'}>
+      )}
+          
+      {inputs === 'enabled' && (
         <>
           <input
-              {...props}
-              checked={checked}
-              disabled={disabled}
-              id={inputIdPresent}
-              name={name}
-              type={inputType}
-              value={value}
+            {...props}
+            checked={checked}
+            disabled={disabled}
+            id={inputIdPresent}
+            name={name}
+            type={inputType}
+            value={value}
           />
           <label htmlFor={inputIdPresent}>
             <Icon
-                customIcon={customIcon}
-                icon={icon}
-                size="2x"
+              customIcon={customIcon}
+              icon={icon}
+              size="2x"
             />
             <Title
-                size={4}
-                tag="h4"
-                text={text}
+              size={4}
+              tag="h4"
+              text={text}
             />
           </label>
         </>
-      </If>
+      )}
     </div>
   )
 }
