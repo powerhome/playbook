7974d070a74234012b21dca004d63466fb9ccac9
Israel.Molestina <molestinaisrael@gmail.com>
2023-01-27 09:56:30 -0500

    added view prop

--- playbook/app/pb_kits/playbook/pb_paginate/paginate.rb
+++ playbook/app/pb_kits/playbook/pb_paginate/paginate.rb
@@ -5,9 +5,14 @@ module Playbook
     class Paginate < ::Playbook::KitBase
       prop :strategy # Need to add protection
       prop :model # Need to add protection
+      prop :view # Need to add protection
 
       def classname
         generate_classname("pb_paginate")
+      end
+
+      def url_for(options = nil)
+        view.url_for(options)
       end
     end
   end
