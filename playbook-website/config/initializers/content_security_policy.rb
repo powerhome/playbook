0e353d38e1f70e104ad00f011f7ab09bfe7b9764
nida.ghuman <nidaqg@gmail.com>
2023-01-30 09:39:46 -0500

    testing blob in src wroker security setting

--- playbook-website/config/initializers/content_security_policy.rb
+++ playbook-website/config/initializers/content_security_policy.rb
@@ -6,14 +6,15 @@ Rails.application.config.content_security_policy do |p|
   p.img_src     :self, :https, :data, :unsafe_inline
   p.object_src  :none
   p.style_src   :self, :https, :unsafe_inline
+  p.worker_src :self, "blob:"
 
   # To allow connections to the webpack-dev-server running in
   # a separate docker container
   if Rails.env.development?
-    p.script_src  :self, :https, :unsafe_inline, :unsafe_eval, "blob:"
+    p.script_src  :self, :https, :unsafe_inline, :unsafe_eval
     p.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035", "http://0.0.0.0:3035", "ws://0.0.0.0:3035"
   else
-    p.script_src  :self, :https, :unsafe_inline, "blob:"
+    p.script_src  :self, :https, :unsafe_inline
   end
 
   # Specify URI for violation reports
