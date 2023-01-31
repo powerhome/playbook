8d7ef67adefc6f81fcbd9ab57ce433681a69eff8
Carlos Lima <carloslimasd@gmail.com>
2023-01-31 13:55:40 -0300

    [PLAY-481] Implementing a Sandpack Example (#2224)
    * Implementing a Sandpack Example
    
    * Adding yarn lock changes
    
    * Changing Sandpack layout
    
    * Added Sandpack for React Examples
    
    * WIP Fixing some bugs
    
    * Reseting TableSm JSX file to master code
    
    * Removing JS in string to test Milano security error
    
    * Testing CSP Rails config
    
    * Using new route to render Sandpack
    
    * Cleaning comments on the CSP file
    
    * Adding header and footer descriptions
    
    * Removing unnecessary code
    
    * Adding new route demo to show different examples
    
    ---------
    
    Co-authored-by: Jasper Furniss <jasper.furniss@powerhrg.com>

--- playbook-website/config/initializers/content_security_policy.rb
+++ playbook-website/config/initializers/content_security_policy.rb
@@ -6,15 +6,10 @@ Rails.application.config.content_security_policy do |p|
   p.img_src     :self, :https, :data, :unsafe_inline
   p.object_src  :none
   p.style_src   :self, :https, :unsafe_inline
+  p.script_src  :self, :https, :unsafe_inline, :unsafe_eval
 
-  # To allow connections to the webpack-dev-server running in
-  # a separate docker container
-  if Rails.env.development?
-    p.script_src  :self, :https, :unsafe_inline, :unsafe_eval
-    p.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035", "http://0.0.0.0:3035", "ws://0.0.0.0:3035"
-  else
-    p.script_src  :self, :https, :unsafe_inline
-  end
+  # To allow connections to the webpack-dev-server running in a separate docker container
+  p.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035", "http://0.0.0.0:3035", "ws://0.0.0.0:3035" if Rails.env.development?
 
   # Specify URI for violation reports
   # p.report_uri "/csp-violation-report-endpoint"
