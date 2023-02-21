88fd849aea19918ba7577e45d52c8138034f0466
nida.ghuman <nidaqg@gmail.com>
2023-02-21 11:54:01 -0500

    Custom buttons style + function, styles for popup

--- playbook/app/pb_kits/playbook/pb_map/_map.tsx
+++ playbook/app/pb_kits/playbook/pb_map/_map.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import classnames from 'classnames'
 import { buildAriaProps, buildCss, buildDataProps } from '../utilities/props'
 import { globalProps } from '../utilities/globalProps'
-
+import Flex  from "../pb_flex/_flex"
+import SectionSeparator from '../pb_section_separator/_section_separator'
+import Icon from '../pb_icon/_icon'
 
 type MapProps = {
   aria?: { [key: string]: string },
@@ -10,6 +12,8 @@ type MapProps = {
   className?: string,
   data?: { [key: string]: string },
   id?: string,
+  zoomBtns?: boolean
+  flyTo?: boolean
 }
 
 const Map = (props: MapProps) => {
@@ -19,6 +23,8 @@ const Map = (props: MapProps) => {
   className,
   data = {},
   id,
+  zoomBtns = false,
+  flyTo = false
   } = props
 
   const ariaProps = buildAriaProps(aria)
@@ -32,6 +38,32 @@ const Map = (props: MapProps) => {
         className={classes}
         id={id}
     >
+      {
+        zoomBtns ? (
+          <Flex className="custom-nav-control" orientation='column'>
+          <div className="custom-nav-control-zoom">
+            <div className='map-zoom-in-button'
+                id="zoom-in-button">
+                  <Icon icon="plus"/>
+            </div>
+            <SectionSeparator />
+            <div className='map-zoom-out-button'
+                id="zoom-out-button">
+                  <Icon icon="minus"/>
+            </div>
+          </div>
+          {
+            flyTo ? (
+          <div className='map-flyto-button'
+                  id="flyto-button">
+                    <Icon icon="eye"/>
+              </div>
+            ) : null
+          }
+         
+      </Flex>
+         ) : null
+        }
       {children}
     </div>
   )
--- playbook/app/pb_kits/playbook/pb_map/docs/_map_default.jsx
+++ playbook/app/pb_kits/playbook/pb_map/docs/_map_default.jsx
@@ -7,34 +7,66 @@ const MapDefault = () => {
 
   const mapContainerRef = useRef(null)
 
-    useEffect(() => {
-      if (!maplibregl.supported()) {
-        alert('Your browser does not support MapLibre GL');
-        } else {
-         const map = new maplibregl.Map({
-            container: mapContainerRef.current,
-            style: 'https://basemaps.cartocdn.com/gl/positron-gl-style/style.json',
-            center: [-75.379143, 39.831200],
-            zoom: 13,
-        })
+  //This function should contain all maplibre related code
+  const loadMap = ( { target: map }) => {
+
+    const defaultPosition = [-75.379143, 39.831200]
         //set marker/pin
         /* eslint-disable-next-line */
         const marker = new maplibregl.Marker({
           color: "#0056CF",
-        }).setLngLat([-75.379143, 39.831200])
+        }).setLngLat(defaultPosition)
         .setPopup(new maplibregl.Popup({className: 'map_popup', closeButton: false}).setHTML(`<h4 class="pb_title_kit_size_4">Hello World!</h4>`)) // add popup
         .addTo(map);
-
-        //add zoom controls
-        map.addControl(new maplibregl.NavigationControl({showCompass: false}))
 
         // disable map zoom when using scroll
         map.scrollZoom.disable();
 
-      }
+        //add custom buttons for zoom in and out control
+          const zoomInBtn = document.querySelector("#zoom-in-button")
+          zoomInBtn.addEventListener('click', function () {
+            map.zoomIn({duration: 1000})
+          })
+          const zoomOutBtn = document.querySelector("#zoom-out-button")
+          zoomOutBtn.addEventListener('click', function () {
+            map.zoomOut({duration: 1000})
+          })
+
+        //Add flyTo button
+          const button = document.querySelector("#flyto-button")
+          button.addEventListener('click', function () {
+             map.flyTo({
+                center: [-75.379143, 39.831200],
+                zoom: 13,
+                bearing: 0,
+                curve: 1, // change the speed at which it zooms out
+                easing: function (t) {
+                return t;
+                },
+                essential: true
+                });
+          })  
+        
+        //add attributioncontrols
+        map.addControl(new maplibregl.AttributionControl({
+          compact: true
+          }));
+  }
+
+    useEffect(() => {
+         new maplibregl.Map({
+            attributionControl: false,
+            container: mapContainerRef.current,
+            style: 'https://basemaps.cartocdn.com/gl/positron-gl-style/style.json',
+            center: [-75.379143, 39.831200],
+            zoom: 13,
+        }).on('load', loadMap)
     }, [])
+
 return ( 
-  <Map>
+  <Map flyTo = "true"
+      zoomBtns="true"
+  >
        <div
            ref={mapContainerRef}
            style={{
