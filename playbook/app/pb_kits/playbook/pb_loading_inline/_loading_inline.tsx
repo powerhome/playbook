268a90479a0fbc7f3009f2367b02900ff33d631a
Carlos Lima <carloslimasd@gmail.com>
2023-02-22 18:55:14 -0300

    Converting Loading Inline kit to Typescrit

---
+++ playbook/app/pb_kits/playbook/pb_loading_inline/_loading_inline.tsx
@@ -0,0 +1,57 @@
+/* @flow */
+
+import React from 'react'
+import classnames from 'classnames'
+
+import { buildAriaProps, buildCss, buildDataProps } from '../utilities/props'
+import { globalProps } from '../utilities/globalProps'
+
+import Body from '../pb_body/_body'
+import Icon from '../pb_icon/_icon'
+
+type LoadingInlineProps = {
+  align?: "left" | "center" | "right",
+  aria?: { [key: string]: string },
+  className?: string,
+  data?: { [key: string]: string },
+  id?: string,
+}
+
+const LoadingInline = (props: LoadingInlineProps) => {
+  const {
+    align = 'left',
+    aria = {},
+    className,
+    data = {},
+    id,
+  } = props
+
+  const ariaProps = buildAriaProps(aria)
+  const dataProps = buildDataProps(data)
+  const classes = classnames(
+    buildCss(`pb_loading_inline_kit_${align}`),
+    globalProps(props),
+    className
+  )
+
+  return (
+    <div
+      {...ariaProps}
+      {...dataProps}
+      className={classes}
+      id={id}
+    >
+      <Body color="light">
+        <Icon
+          aria={{ label: 'loading icon' }}
+          fixedWidth
+          icon="spinner"
+          pulse
+        />
+        {' Loading'}
+      </Body>
+    </div>
+  )
+}
+
+export default LoadingInline
---
+++ playbook/app/pb_kits/playbook/pb_loading_inline/loading_inline.test.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { render, screen } from '../utilities/test-utils'
+import LoadingInline from './_loading_inline'
+
+const testId = "loadingInline"
+
+test('should render custom class and data', () => {
+    render(
+        <LoadingInline
+            className='custom-class'
+            data={{ testid: testId }}
+        />
+    )
+
+    const kit = screen.getByTestId(testId)
+    expect(kit).toHaveClass('custom-class')
+})
+
+test('should render id', () => {
+    render(
+        <LoadingInline
+            data={{ testid: testId }}
+            id={testId}
+        />
+    )
+
+    const kit = screen.getByTestId(testId)
+    expect(kit).toHaveProperty('id', testId)
+})
+
+test('should render aria-label', () => {
+    render(
+        <LoadingInline
+            aria={{ label: testId }}
+            data={{ testid: testId }}
+        />
+    )
+
+    const kit = screen.getByTestId(testId)
+    expect(kit).toHaveAttribute('aria-label', testId)
+})
