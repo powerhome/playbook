f80494d1fd33012febd168c6955fb5018c4131e6
Jasper Furniss <jasper.furniss@powerhrg.com>
2023-03-01 17:39:37 -0500

    Select All Working with Heirarchy Mode, When Expanded

--- playbook/app/pb_kits/playbook/pb_multi_level_select/_multi_level_select.tsx
+++ playbook/app/pb_kits/playbook/pb_multi_level_select/_multi_level_select.tsx
@@ -1,60 +1,62 @@
-import React, { ReactNode } from 'react'
-import classnames from 'classnames'
-import { buildAriaProps, buildCss, buildDataProps } from '../utilities/props'
-import { globalProps } from '../utilities/globalProps'
-import DropdownTreeSelect from 'react-dropdown-tree-select'
-import 'react-dropdown-tree-select/dist/styles.css'
+import React, { ReactNode } from "react"
+import classnames from "classnames"
+import { buildAriaProps, buildCss, buildDataProps } from "../utilities/props"
+import { globalProps } from "../utilities/globalProps"
+import DropdownTreeSelect from "react-dropdown-tree-select"
+import "react-dropdown-tree-select/dist/styles.css"
 
 type MultiLevelSelectProps = {
-  aria?: { [key: string]: string },
-  className?: string,
-  data?: { [key: string]: string },
-  id?: string,
-  treeData?: { [key: string]: string }[],
+  aria?: { [key: string]: string }
+  className?: string
+  data?: { [key: string]: string }
+  id?: string
+  treeData?: { [key: string]: string }[]
 }
 
 const MultiLevelSelect = (props: MultiLevelSelectProps) => {
-  const {
-  aria = {},
-  className,
-  data = {},
-  id,
-  treeData,
-  } = props
+  
+
+  const { aria = {}, className, data = {}, id, treeData } = props
 
   const ariaProps = buildAriaProps(aria)
   const dataProps = buildDataProps(data)
-  const classes = classnames(buildCss('pb_multi_level_select'), globalProps(props), className)
+  const classes = classnames(
+    buildCss("pb_multi_level_select"),
+    globalProps(props),
+    className
+  )
 
-  const onChange = (currentNode:ReactNode, selectedNodes:ReactNode) => {
-    console.log('Current node:',currentNode, 'Selected nodes:',selectedNodes)
+  const onChange = (currentNode: any, selectedNodes: ReactNode) => {
+    if (currentNode._children && currentNode.checked) {
+      currentNode._children.forEach(function (child: any) {
+        document.getElementById(child).checked = true
+        onChange(child, selectedNodes)
+      })
+    }
+    console.log(selectedNodes)
+  }
 
+  const onAction = (node: ReactNode, action: any) => {
+    console.log("onAction:", action, node)
   }
-  const onAction = (node:ReactNode, action:any) => {
-    console.log('onAction:', action, node)
-  }
-  const onNodeToggle = (currentNode:ReactNode) => {
-    console.log('ToggledNode:', currentNode)
+  const onNodeToggle = (currentNode: ReactNode) => {
+    currentNode
   }
 
   return (
-    <div
-        {...ariaProps}
-        {...dataProps}
-        className={classes}
+    <div {...ariaProps} {...dataProps} className={classes} id={id}>
+      <DropdownTreeSelect
+        data={treeData}
         id={id}
-    >
-    <DropdownTreeSelect 
-      data={treeData} 
-      id={id}
-      keepTreeOnSearch
-      keepChildrenOnSearch
-      onChange={onChange} 
-      onAction={onAction} 
-      onNodeToggle={onNodeToggle} 
-      texts={{ placeholder: 'Select...' }}
-      mode="hierarchical"
-    />
+        keepOpenOnSelect
+        keepTreeOnSearch
+        keepChildrenOnSearch
+        onChange={onChange}
+        onAction={onAction}
+        onNodeToggle={onNodeToggle}
+        texts={{ placeholder: "Select..." }}
+        mode='hierarchical'
+      />
     </div>
   )
 }
