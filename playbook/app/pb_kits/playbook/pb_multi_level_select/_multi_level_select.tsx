0258012f35c0097532b1f52780317951dd53fd12
nida.ghuman <nidaqg@gmail.com>
2023-02-28 21:50:15 -0500

    removed unused code

--- playbook/app/pb_kits/playbook/pb_multi_level_select/_multi_level_select.tsx
+++ playbook/app/pb_kits/playbook/pb_multi_level_select/_multi_level_select.tsx
@@ -26,45 +26,15 @@ const MultiLevelSelect = (props: MultiLevelSelectProps) => {
   const dataProps = buildDataProps(data)
   const classes = classnames(buildCss('pb_multi_level_select'), globalProps(props), className)
 
-// console.log(treeData)
-  // const prepareData = (treeData:any) => {
+  const onChange = (currentNode:ReactNode, selectedNodes:ReactNode) => {
+    console.log('Current node:',currentNode, 'Selected nodes:',selectedNodes)
 
-  //   // insert special select all node
-  //   treeData.splice(0, 0, {
-  //     label: "Select All",
-  //     value: "selectAll",
-  //     className: "select-all"
-  //   });
-
-  //   return treeData;
-  // };
-
-  // const [customData, setCustomData]= useState(treeData)
-
-  // const toggleAll = (checked:any) => {
-  //   for (var i = 1; i < customData.length; i++) {
-  //     customData[i].checked = checked;
-  //   }
-  //   setCustomData(customData);
-  // };
-
-  // const handleChange = ({ value, checked }:any) => {
-  //   if (value === "selectAll") toggleAll(checked);
-  //   console.log(customData)
-  // };
-  const onChange = (currentNode:any, selectedNodes:ReactNode) => {
-    console.log('onChange:', currentNode, selectedNodes)
-
-    // currentNode.checked && currentNode._children && (
-    //   console.log(currentNode._children)
-      
-    // )
   }
   const onAction = (node:ReactNode, action:any) => {
     console.log('onAction:', action, node)
   }
   const onNodeToggle = (currentNode:ReactNode) => {
-    console.log('onNodeToggle:', currentNode)
+    console.log('ToggledNode:', currentNode)
   }
 
   return (
