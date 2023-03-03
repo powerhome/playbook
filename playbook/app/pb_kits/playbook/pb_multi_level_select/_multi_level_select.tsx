998937d2450719f2968adcef9b01b3a267280988
nida.ghuman <nidaqg@gmail.com>
2023-03-03 12:06:17 -0500

    Fixed bug where duplicate objects were getting passed to selected array

--- playbook/app/pb_kits/playbook/pb_multi_level_select/_multi_level_select.tsx
+++ playbook/app/pb_kits/playbook/pb_multi_level_select/_multi_level_select.tsx
@@ -1,33 +1,33 @@
-import React, {useState, useEffect} from "react"
-import classnames from "classnames"
-import { buildAriaProps, buildCss, buildDataProps } from "../utilities/props"
-import { globalProps } from "../utilities/globalProps"
-import { findItemById, checkIt, unCheckIt } from "./helper_functions"
-import MultiSelectHelper from "./_multi_select_helper"
+import React, { useState, useEffect } from "react";
+import classnames from "classnames";
+import { buildAriaProps, buildCss, buildDataProps } from "../utilities/props";
+import { globalProps } from "../utilities/globalProps";
+import { findItemById, checkIt, unCheckIt } from "./helper_functions";
+import MultiSelectHelper from "./_multi_select_helper";
 
 type MultiLevelSelectProps = {
-  aria?: { [key: string]: string }
-  className?: string
-  data?: { [key: string]: string }
-  id?: string
-  treeData?: { [key: string]: string }[]
-  onChange?: any
-  onSelect?: (SelectedNodes:{[key: string]: any;})=>void
-}
+  aria?: { [key: string]: string };
+  className?: string;
+  data?: { [key: string]: string };
+  id?: string;
+  treeData?: { [key: string]: string }[];
+  onChange?: any;
+  onSelect?: (SelectedNodes: { [key: string]: any }) => void;
+};
 
 const MultiLevelSelect = (props: MultiLevelSelectProps) => {
-  const { aria = {}, className, data = {}, id, treeData, onSelect } = props
+  const { aria = {}, className, data = {}, id, treeData, onSelect } = props;
 
-  const ariaProps = buildAriaProps(aria)
-  const dataProps = buildDataProps(data)
+  const ariaProps = buildAriaProps(aria);
+  const dataProps = buildDataProps(data);
   const classes = classnames(
     buildCss("pb_multi_level_select"),
     globalProps(props),
     className
-  )
+  );
 
-  const [formattedData, setFormattedData] = useState(treeData)
-  const [selectedItems, setSelectedItems] =useState([])
+  const [formattedData, setFormattedData] = useState(treeData);
+  const [selectedItems, setSelectedItems] = useState([]);
 
   const onChange = (currentNode: { [key: string]: any }) => {
     const updatedData = formattedData.map((item: any) => {
@@ -47,17 +47,23 @@ const MultiLevelSelect = (props: MultiLevelSelectProps) => {
           }
         }
       }
-  
+
       return item;
     });
-    
-    setFormattedData(updatedData)
-  }
-  
+
+    setFormattedData(updatedData);
+  };
+
   useEffect(() => {
-    const selected = selectedItems.filter((item: { [key: string]: any }) => item.checked)
-    onSelect(selected)
-  }, [selectedItems])
+    const selected = selectedItems.filter(
+      (item: { [key: string]: any }) => item.checked
+    );
+    //filter to remove duplicate items
+    const uniqueSelected = selected.filter(
+      (obj, index, self) => index === self.findIndex((t) => t.id === obj.id)
+    );
+    onSelect(uniqueSelected);
+  }, [selectedItems]);
 
   return (
     <div {...ariaProps} {...dataProps} className={classes} id={id}>
@@ -69,7 +75,7 @@ const MultiLevelSelect = (props: MultiLevelSelectProps) => {
         {...props}
       />
     </div>
-  )
-}
+  );
+};
 
-export default MultiLevelSelect
+export default MultiLevelSelect;
--- playbook/app/pb_kits/playbook/pb_multi_level_select/helper_functions.ts
+++ playbook/app/pb_kits/playbook/pb_multi_level_select/helper_functions.ts
@@ -1,28 +1,34 @@
-export const findItemById = (items: { [key: string]: any }[], id: string) :any => {
-    for (const item of items) {
-      if (item.id === id) {
-        return item
-      }
-      if (item.children) {
-        const found = findItemById(item.children, id)
-        if (found) {
-          return found
-        }
+export const findItemById = (
+  items: { [key: string]: any }[],
+  id: string
+): any => {
+  for (const item of items) {
+    if (item.id === id) {
+      return item;
+    }
+    if (item.children) {
+      const found = findItemById(item.children, id);
+      if (found) {
+        return found;
       }
     }
-    return null
-}
-  
+  }
+  return null;
+};
 
-export const checkIt = (foundItem: { [key: string]: any}, selectedItems: any[], setSelectedItems: Function) => {
+export const checkIt = (
+  foundItem: { [key: string]: any },
+  selectedItems: any[],
+  setSelectedItems: Function
+) => {
   if (!foundItem) {
     return;
   }
-  
+
   foundItem.checked = true;
   foundItem.expanded = true;
   selectedItems.push(foundItem);
-  
+
   if (foundItem.children) {
     foundItem.children.map((x: any) => {
       checkIt(x, selectedItems, setSelectedItems);
@@ -30,19 +36,25 @@ export const checkIt = (foundItem: { [key: string]: any}, selectedItems: any[], 
   }
 
   setSelectedItems([...selectedItems]);
-}
+};
 
-export const unCheckIt = (foundItem: { [key: string]: any }, selectedItems: any, setSelectedItems: any) => {
+export const unCheckIt = (
+  foundItem: { [key: string]: any },
+  selectedItems: any,
+  setSelectedItems: any
+) => {
   if (!foundItem) {
     return;
   }
 
   foundItem.checked = false;
-  const newSelectedItems = selectedItems.filter((item: any) => item.id !== foundItem.id);
+  const newSelectedItems = selectedItems.filter(
+    (item: any) => item.id !== foundItem.id
+  );
   if (foundItem.children) {
     foundItem.children.map((x: any) => {
       unCheckIt(x, selectedItems, setSelectedItems);
     });
   }
   setSelectedItems([...newSelectedItems]);
-}
+};
