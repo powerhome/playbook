34f1808973cb27c47873312be56146278dcc0387
Carlos Lima <carloslimasd@gmail.com>
2023-03-07 12:50:09 -0300

    Adding allow input prop to docs

---
+++ playbook/app/pb_kits/playbook/pb_date_picker/docs/_date_picker_allow_input.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import DatePicker from '../_date_picker'
+
+const DatePickerAllowInput = (props) => (
+    <>
+        <DatePicker
+            allowInput
+            pickerId="date-picker-allow-input"
+            {...props}
+        />
+    </>
+)
+
+export default DatePickerAllowInput
