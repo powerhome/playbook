b55019429d1cd33a5b76d9fa56608f3546bcaf8f
Augusto Mallmann <augustocmallmann@gmail.com>
2023-03-08 11:30:45 -0300

    [NUXE-1380] Update the look of the editor: add tooltip delay (#2347)
    #### Screens
    
    <img width="1407" alt="Screenshot 2023-03-06 at 10 58 37 AM"
    src="https://user-images.githubusercontent.com/8194056/223130736-f9e390a0-5a05-4a90-86ef-dc2fae5cdaed.png">
    
    #### Breaking Changes
    
    No, this feature adds the `delay` prop which can be used to delay the
    `open` and/or `close` event.
    
    This feature is related to the NUXE-1380 story. As a Huddle user, I want
    the tooltip do be delayed on mouse hover. More details on the
    [handoff](https://www.figma.com/proto/iUQ0Jrj1Xa78qvyLZgKphb/3.0-(Huddle)?page-id=1616%3A42621&node-id=1963%3A54663&viewport=212%2C-4987%2C0.13&scaling=min-zoom&starting-point-node-id=1963%3A54663&show-proto-sidebar=1).
    
    #### Runway Ticket URL
    
    [[NUXE-1380]
    ](https://nitro.powerhrg.com/runway/backlog_items/NUXE-1380)
    
    #### How to test this
    
    [INSERT TESTING DETAILS]
    
    #### Checklist:
    
    - [ ] **LABELS** Add a label: `enhancement`, `bug`, `improvement`, `new
    kit`, `deprecated`, or `breaking`. See [Changelog &
    Labels](https://github.com/powerhome/playbook/wiki/Changelog-&-Labels)
    for details.
    - [ ] **DEPLOY** Please add the `Milano` label when you are ready for a
    review.
    - [ ] **SCREENSHOT** Please add a screen shot or two.
    - [ ] **SPECS** Please cover your changes with specs.
    - [ ] **READ DOCS** Please make sure you have read and understand the
    [Playbook Release
    Process](https://github.com/powerhome/playbook/wiki/Playbook-Releases)

---
+++ playbook/app/pb_kits/playbook/pb_tooltip/docs/_tooltip_delay.jsx
@@ -0,0 +1,56 @@
+// @flow
+
+import React from 'react'
+import { Button, Tooltip, Flex, FlexItem } from '../..';
+
+const TooltipDelay = (props) => {
+
+  return (
+   <Flex 
+       flexDirection='row'
+       gap='md'
+       justifyContent='center'
+       wrap
+   >
+    <FlexItem>
+      <Tooltip 
+          delay={1000}
+          placement='top' 
+          text="1s open/close delay" 
+          zIndex={10}
+          {...props}
+      >
+        <Button text="1s delay"/>
+      </Tooltip>
+    </FlexItem>
+    <FlexItem>
+      <Tooltip 
+          delay={{
+            open: 1000
+          }}
+          placement='top' 
+          text="1s open delay" 
+          zIndex={10}
+          {...props}
+      >
+        <Button text="Open only"/>
+      </Tooltip>
+    </FlexItem>
+    <FlexItem>
+      <Tooltip 
+          delay={{
+            close: 1000
+          }}
+          placement='top' 
+          text="1s close delay" 
+          zIndex={10}
+          {...props}
+      >
+        <Button text="Close only"/>
+      </Tooltip>
+    </FlexItem>
+   </Flex>
+  )
+}
+
+export default TooltipDelay
