99a2c732207e68eb6782507e96efc5b88373ee1a
Carlos Lima <carloslimasd@gmail.com>
2023-03-21 15:31:10 -0300

    Add Articles doc to Body kit

--- playbook-website/app/javascript/components/VisualGuidelines/Examples/MaxWidth.tsx
+++ playbook-website/app/javascript/components/VisualGuidelines/Examples/MaxWidth.tsx
@@ -13,7 +13,7 @@ const SIZES = ['xs', 'sm', 'md', 'lg', 'xl'] //TODO: investigate using types
 
 const MaxWidth = ({ example }: {example: string}) => (
   <Example
-      description="When building your interface it is common to add max width to specific kits so the interface looks good at all sizes. We've made it easy to add max with ANY kit through our global props. See below:"
+      description={`When building your interface it is common to add max width to specific kits so the interface looks good at all sizes. For articles we recommend paring a "medium" width with "loose" line height. See our <a href="https://www.w3schools.com/">Body Kit</a> for an example. We've made it easy to add max with ANY kit through our global props. See below:`}
       example={example}
       globalProps={{
         maxWidth: SIZES,
---
+++ playbook/app/pb_kits/playbook/pb_body/docs/_body_articles.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Body } from '../..'
+
+const BodyBlock = (props) => {
+    return (
+        <>
+            <Body
+                lineHeight='loose'
+                maxWidth='md'
+                {...props}
+            >
+                <p>{`Infuse your life with action. Don't wait for it to happen. Make it happen. Make your own future. Make your own hope. Make your own love. And whatever your beliefs, honor your creator, not by passively waiting for grace to come down from upon high, but by doing what you can to make grace happen... yourself, right now, right down here on Earth.`}</p>
+                <p>{`- Bradley Whitford`}</p>
+            </Body>
+        </>
+    )
+}
+
+export default BodyBlock
