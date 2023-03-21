d8b59c73a6d46f4612c2d25478abaf5377f0f903
Carlos Lima <carloslimasd@gmail.com>
2023-03-21 16:37:37 -0300

    Splitting nav tests and adding more tests

--- playbook/app/pb_kits/playbook/pb_nav/_nav.test.js
+++ playbook/app/pb_kits/playbook/pb_nav/_nav.test.js
@@ -12,13 +12,14 @@ const navTitle = 'Menu'
 const itemTitle = 'Photos'
 const itemImageUrl = 'https://upload.wikimedia.org/wikipedia/commons/0/00/Apple_News_icon_%28macOS%29.png'
 
-const NavDefault = () => {
+const NavDefault = (props = []) => {
     return (
         <Nav
             aria={{ label: navTestId }}
             className={navClassName}
             data={{ testid: navTestId }}
             title={navTitle}
+            {...props}
         >
             <NavItem
                 aria={{ label: itemTestId }}
@@ -45,40 +46,22 @@ const NavDefault = () => {
     )
 }
 
-test('should pass nav data prop', () => {
+test('should pass data prop', () => {
     render(<NavDefault />)
     const kit = screen.getByTestId(navTestId)
     expect(kit).toBeInTheDocument()
 })
 
-test('should pass nav className prop', () => {
+test('should pass className prop', () => {
     render(<NavDefault />)
     const kit = screen.getByTestId(navTestId)
     expect(kit).toHaveClass(navClassName)
 })
 
-test('should pass nav aria prop', () => {
+test('should pass aria prop', () => {
     render(<NavDefault />)
     const kit = screen.getByTestId(navTestId)
     expect(kit).toHaveAttribute('aria-label', navTestId)
-})
-
-test('should pass nav item data prop', () => {
-    render(<NavDefault />)
-    const kit = screen.getByTestId(itemTestId)
-    expect(kit).toBeInTheDocument()
-})
-
-test('should pass nav item className prop', () => {
-    render(<NavDefault />)
-    const kit = screen.getByTestId(itemTestId)
-    expect(kit).toHaveClass(itemClassName)
-})
-
-test('should pass nav item aria prop', () => {
-    render(<NavDefault />)
-    const kit = screen.getByTestId(itemTestId)
-    expect(kit).toHaveAttribute('aria-label', itemTestId)
 })
 
 test('should render nav title', () => {
@@ -87,8 +70,50 @@ test('should render nav title', () => {
     expect(kit).toBeInTheDocument()
 })
 
-test('should render nav item title', () => {
+test('should not be borderless by default', () => {
     render(<NavDefault />)
-    const kit = screen.getByText(itemTitle)
-    expect(kit).toBeInTheDocument()
+    const kit = screen.getByTestId(navTestId)
+    expect(kit).toHaveClass('pb_nav_list_normal_vertical_highlight')
+})
+
+test('should be borderless', () => {
+    render(<NavDefault borderless />)
+    const kit = screen.getByTestId(navTestId)
+    expect(kit).toHaveClass('pb_nav_list_normal_vertical_highlight_borderless')
+})
+
+test('should highlight by default', () => {
+    render(<NavDefault />)
+    const kit = screen.getByTestId(navTestId)
+    expect(kit).toHaveClass('pb_nav_list_normal_vertical_highlight')
+})
+
+test('should not highlight', () => {
+    render(<NavDefault highlight={false} />)
+    const kit = screen.getByTestId(navTestId)
+    expect(kit).toHaveClass('pb_nav_list_normal_vertical')
+})
+
+test('should have vertical orientation by default', () => {
+    render(<NavDefault />)
+    const kit = screen.getByTestId(navTestId)
+    expect(kit).toHaveClass('pb_nav_list_normal_vertical_highlight')
+})
+
+test('should change orientation', () => {
+    render(<NavDefault orientation='horizontal' />)
+    const kit = screen.getByTestId(navTestId)
+    expect(kit).toHaveClass('pb_nav_list_normal_horizontal_highlight')
+})
+
+test('should have normal variant by default', () => {
+    render(<NavDefault />)
+    const kit = screen.getByTestId(navTestId)
+    expect(kit).toHaveClass('pb_nav_list_normal_vertical_highlight')
+})
+
+test('should change variant', () => {
+    render(<NavDefault variant='subtle' />)
+    const kit = screen.getByTestId(navTestId)
+    expect(kit).toHaveClass('pb_nav_list_subtle_vertical_highlight')
 })
