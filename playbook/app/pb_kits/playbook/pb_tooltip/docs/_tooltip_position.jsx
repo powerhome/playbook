5ef556969f493371e73d544136fd84535dc2b27a
Augusto Mallmann <augustocmallmann@gmail.com>
2023-03-30 16:18:21 -0300

    refactor: doc

--- playbook/app/pb_kits/playbook/pb_tooltip/docs/_tooltip_position.jsx
+++ playbook/app/pb_kits/playbook/pb_tooltip/docs/_tooltip_position.jsx
@@ -1,44 +1,61 @@
 // @flow
 
-import React from 'react'
+import React from "react";
 
-import { Body, Button, Flex, Tooltip } from '../..';
+import { Body, Button, Flex, Tooltip } from "../..";
 
 const TooltipPosition = (props) => {
   return (
-    <Flex 
-        gap='md'
-        justify='center'
-    >
-      <div style={{
-        overflow: 'scroll',
-        height: '85px',
-        width: '85px'
-      }}>
+    <Flex align="center">
+      <Flex 
+          flex="1"
+          flexDirection="column"
+          gap="md"
+      >
         <Body 
-            text='Scroll down this div and hover the button' 
+            text="When adding a Tooltip inside of a relative positioned div with an overflow style applied to it, the Tooltip content can break or not respect its original placement. To fix that you can change the default position to fixed" 
             {...props}
         />
+        <Body 
+            text="Both tooltips are set to be placed on the bottom of the button." 
+            {...props}
+        />
+        <Body 
+            text="The position prop is optional and defaults to absolute. The Tooltip accepts the position values of fixed and absolute." 
+            {...props}
+        />
+      </Flex>
+      <div
+          style={{
+            position: "relative",
+            display: "flex",
+            flex: "1",
+            alignItems: "center",
+            justifyContent: "space-around",
+            overflowY: "scroll",
+            height: "100px",
+          }}
+      >
         <Tooltip
-            placement='bottom'
-            position='fixed'
-            text='Fixed Tooltip'
+            placement="bottom"
+            position="fixed"
+            text="Fixed Tooltip"
             zIndex={10}
             {...props}
         >
-          <Button text='Fixed' />
+          <Button text="Fixed" />
+        </Tooltip>
+        <Tooltip
+            placement="bottom"
+            text="Broken tooltip content"
+            zIndex={10}
+            {...props}
+        >
+          <Button text="Absolute" />
         </Tooltip>
       </div>
-      <Tooltip
-          placement='bottom'
-          text='Absolute Tooltip'
-          zIndex={10}
-          {...props}
-      >
-        <Button text='Absolute' />
-      </Tooltip>
     </Flex>
   );
-}
+};
 
-export default TooltipPosition
+export default TooltipPosition;
--- playbook/app/pb_kits/playbook/pb_tooltip/docs/_tooltip_position.md
+++
@@ -1,3 +0,0 @@
-When adding Tooltip within overflow divs you might see it hidden behind the content or not respecting its placement. To fix that you can change the default position to `fixed`.
-
-`position` prop is optional, it accepts the values `fixed` and `absolute` which is the default. 
