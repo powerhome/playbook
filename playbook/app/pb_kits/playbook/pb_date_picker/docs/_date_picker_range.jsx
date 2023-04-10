3594d2d747183ac40d187852f96c83213b5b3495
Israel.Molestina <molestinaisrael@gmail.com>
2023-04-10 15:03:23 -0400

    remvoed allowInput prop from range doc example

--- playbook/app/pb_kits/playbook/pb_date_picker/docs/_date_picker_range.jsx
+++ playbook/app/pb_kits/playbook/pb_date_picker/docs/_date_picker_range.jsx
@@ -5,7 +5,6 @@ import DatePicker from '../_date_picker'
 const DatePickerRange = (props) => (
   <div>
     <DatePicker
-        allowInput
         defaultDate={[new Date(), new Date().fp_incr(7)]}
         mode="range"
         pickerId="date-picker-range"
