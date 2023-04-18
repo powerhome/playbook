fff86dbe44207a91682225d7a526517acd5ba5dc
Israel.Molestina <molestinaisrael@gmail.com>
2023-04-18 12:12:43 -0400

    seperarted out values from the props

--- playbook/app/pb_kits/playbook/pb_docs/kit_api.rb
+++ playbook/app/pb_kits/playbook/pb_docs/kit_api.rb
@@ -6,14 +6,44 @@ module Playbook
       prop :kit, type: Playbook::Props::String, required: true
 
       def kit_local_props
-        # global = []
-
         local = []
-        # puts kit_props.class
         kit_props.each do |key, value|
-          value.kit == Playbook::PbButton::Button && local.push(key)
+          # puts key
+          # puts value
+          value.kit != Playbook::KitBase && local.push({ key: key, value: value })
         end
         local
+      end
+
+      def local_prop_data
+        local_props = []
+
+        kit_local_props.each do |key, _value|
+          # Itterate over values
+          # grab the: Name, Type, Default, Values
+          # puts key
+          # puts "THIS IS THE NAME: ========== #{key[:value].instance_variable_get(:@name)}"
+          name = key[:value].instance_variable_get(:@name)
+          # puts "THIS IS THE TYPE: ========== #{key[:value]}"
+          type = key[:value].class.to_s.split("::").last
+          # puts "THIS IS THE DEFAULT: ========== #{key[:value].instance_variable_get(:@default)}"
+          default = key[:value].instance_variable_get(:@default)
+          # puts "THIS IS THE DEFAULT: ========== #{key[:value].instance_variable_get(:@values)}"
+          values = key[:value].instance_variable_get(:@values)
+
+          local_props.push(name)
+          local_props.push(type)
+          local_props.push(default)
+          local_props.push(values)
+
+          # puts Time.current
+          # key.each do |prop|
+          #   puts prop[:values]
+          # end
+          # puts key
+          # puts key[:value].instance_variable_get(:@values)
+        end
+        local_props
       end
 
       def kit_props
