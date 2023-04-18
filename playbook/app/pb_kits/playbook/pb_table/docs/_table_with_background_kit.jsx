2533eb87071f5dc7a7a2d5d050f1a3c390b8bc04
Israel.Molestina <molestinaisrael@gmail.com>
2023-04-18 16:11:00 -0400

    made changes to the doc example

--- playbook/app/pb_kits/playbook/pb_table/docs/_table_with_background_kit.jsx
+++ playbook/app/pb_kits/playbook/pb_table/docs/_table_with_background_kit.jsx
@@ -35,34 +35,19 @@ const TableWithBackgroundKit = (props) => {
             <td>{'Value 2'}</td>
             <td>{'Value 3'}</td>
           </tr>
-        </tbody>
-      </Table>
-      <Table
-          {...props}
-      >
-        <thead>
-        <tr>
-          <th>{'Column 1'}</th>
-          <th>{'Column 2'}</th>
-          <th>{'Column 3'}</th>
-        </tr>
-        </thead>
-        <tbody>
-          <tr>
-          <Background
-              backgroundColor="warning_subtle"
-              tag='td'
-          >
-              {'Value 1'}
-            </Background>
-            <td>{'Value 2'}</td>
-            <td>{'Value 3'}</td>
-          </tr>
           <tr>
             <td>{'Value 1'}</td>
             <td>{'Value 2'}</td>
             <td>{'Value 3'}</td>
           </tr>
+          <Background
+              backgroundColor="warning_subtle"
+              tag='tr'
+          >
+            <td>{'Value 1'}</td>
+            <td>{'Value 2'}</td>
+            <td>{'Value 3'}</td>
+          </Background>
           <tr>
             <td>{'Value 1'}</td>
             <td>{'Value 2'}</td>
