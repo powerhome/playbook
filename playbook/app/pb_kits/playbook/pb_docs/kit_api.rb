0af24bce470e8e52e58caa5c1e070f6a99a2bc50
Israel.Molestina <molestinaisrael@gmail.com>
2023-04-20 10:30:25 -0400

    finsished styling table and added conditional to check if there are local props

--- playbook/app/pb_kits/playbook/pb_docs/kit_api.rb
+++ playbook/app/pb_kits/playbook/pb_docs/kit_api.rb
@@ -8,8 +8,6 @@ module Playbook
       def kit_local_props
         local = []
         kit_props.each do |key, value|
-          # puts key
-          # puts value
           value.kit != Playbook::KitBase && local.push({ key: key, value: value })
         end
         local
@@ -19,30 +17,11 @@ module Playbook
         local_props = {}
 
         kit_local_props.each do |key, _value|
-          # Itterate over values
-          # grab the: Name, Type, Default, Values
-          # puts key
-          # puts "THIS IS THE NAME: ========== #{key[:value].instance_variable_get(:@name)}"
           name = key[:value].instance_variable_get(:@name)
-          # puts "THIS IS THE TYPE: ========== #{key[:value]}"
           type = key[:value].class.to_s.split("::").last
-          # puts "THIS IS THE DEFAULT: ========== #{key[:value].instance_variable_get(:@default)}"
           default = key[:value].instance_variable_get(:@default)
-          # puts "THIS IS THE DEFAULT: ========== #{key[:value].instance_variable_get(:@values)}"
           values = key[:value].instance_variable_get(:@values)
-
-          # local_props.push([{ key: "name", value: name }, { key: "type", value: type }, { key: "default", value: default }, { key: "values", value: values }])
           local_props[name.to_sym] = { "type": type, "default": default, "values": values }
-          # local_props.push({ key: "type", value: type })
-          # local_props.push({ key: "default", value: default })
-          # local_props.push({ key: "values", value: values })
-
-          # puts Time.current
-          # key.each do |prop|
-          #   puts prop[:values]
-          # end
-          # puts key
-          # puts key[:value].instance_variable_get(:@values)
         end
         local_props
       end
