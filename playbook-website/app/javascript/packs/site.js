7bef3dce03a825171ccef034b96091c29af73211
Augusto Mallmann <augustocmallmann@gmail.com>
2023-04-24 16:13:07 -0300

    Revert "fix: close/show icon toggle"
    This reverts commit 8cd2e57d324ae571b4deecc3e2c26d17731deef2.

--- playbook-website/app/javascript/packs/site.js
+++ playbook-website/app/javascript/packs/site.js
@@ -1,24 +1,22 @@
 window.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('#code-wrapper').forEach((wrapper) => {
-    const showCodeButton = wrapper.querySelector('#code-example-button')
+    const openToggle = wrapper.querySelector('#toggle-open')
     const codeDrawer = wrapper.querySelector('.pb--codeCopy')
-    const showCodeButtonContent = showCodeButton.querySelector('#code-example-button-content')
-    const openExampleIcon = showCodeButton.querySelector('#code-example-open-icon')
-    const closeExampleIcon = showCodeButton.querySelector('#code-example-close-icon')
 
-    const toggleLambda = (elements) => {
-      elements.forEach((element) =>
-        element.style.display === 'none'
-          ? (element.style.display = 'block')
-          : (element.style.display = 'none')
-      )
+    const toggleLambda = (element) => {
+      if (element.style.display === 'none') {
+        element.style.display = 'block'
+      } else {
+        element.style.display = 'none'
+      }
     }
 
-    showCodeButton.addEventListener('click', () => {
-      showCodeButtonContent.innerHTML === 'Show code'
-        ? showCodeButtonContent.innerHTML = 'Close code'
-        : showCodeButtonContent.innerHTML = 'Show code'
-      toggleLambda([closeExampleIcon, openExampleIcon, codeDrawer])
+    openToggle.addEventListener('click', () => {
+      const buttonContent = openToggle.getElementsByClassName('pb_button_content')[0]
+      buttonContent.innerHTML === 'Show code'
+        ? (buttonContent.innerHTML = 'Close code')
+        : (buttonContent.innerHTML = 'Show code')
+      toggleLambda(codeDrawer)
     })
   })
 
