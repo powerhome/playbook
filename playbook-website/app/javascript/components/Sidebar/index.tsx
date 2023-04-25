4ae1d788bd7197bce195ee8a4c9741173a0a27b8
Nida Ghuman <nidaqg@gmail.com>
2023-04-25 08:43:24 -0400

    [PLAY-748] Restructure Visual Guidelines Page (#2476)
    **What does this PR do?** A clear and concise description with your
    runway ticket url.
    Instead of hash links to different sections of the same page, the visual
    guidelines sidebar will now link to different pages for each item on the
    visual guidelines page.
    
    **Screenshots:** Screenshots to visualize your addition/change
    
    ![Playbook-Design-System
    (1)](https://user-images.githubusercontent.com/73710701/233168478-4dc319e0-4470-40eb-9fb8-c6299a108c54.png)
    
    **How to test?** Steps to confirm the desired behavior:
    1. Go to '...'
    2. Click on '....'
    3. Scroll down to '....'
    4. See addition/change
    
    
    #### Checklist:
    - [ ] **LABELS** Add a label: `enhancement`, `bug`, `improvement`, `new
    kit`, `deprecated`, or `breaking`. See [Changelog &
    Labels](https://github.com/powerhome/playbook/wiki/Changelog-&-Labels)
    for details.
    - [ ] **DEPLOY** I have added the `milano` label to show I'm ready for a
    review.
    - [ ] **TESTS** I have added test coverage to my code.

--- playbook-website/app/javascript/components/Sidebar/index.tsx
+++ playbook-website/app/javascript/components/Sidebar/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Nav, NavItem, Caption} from 'playbook-ui'
+import { Nav, NavItem } from 'playbook-ui'
 
 export const MENU_ITEMS = [
   "Colors",
@@ -18,15 +18,15 @@ export const MENU_ITEMS = [
 ];
 
 const Sidebar = () => {
-
+  const regex = /(?:(?:[^/]*\/){2})(.*)/;
   return (
     <>
-    <Caption marginY="md" marginLeft="md" text="Visual Guidelines"/>
     <Nav variant="subtle">
         {
             MENU_ITEMS.map(item => (
                 <NavItem
-                link={`#${item.replace(/\s+/g, '')}`}
+                active={item.toLowerCase().replace(/[\sA-Z-]+/g, (match) => " ".replace(/[\s-]/g, '_')) == window.location.pathname.match(regex)[1]}
+                link={`/visual_guidelines/${item.toLowerCase().replace(/[\sA-Z-]+/g, (match) => " ".replace(/[\s-]/g, '_'))}`}
                 text={item}
                 />
             ))
