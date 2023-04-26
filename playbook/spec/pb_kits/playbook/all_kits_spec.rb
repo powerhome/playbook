1219f400c41a2d17d03097b69641c8369060151b
Mark <mark.rosenberg@powerhrg.com>
2023-04-26 10:55:17 -0400

    Loop through all kits without YAML

--- playbook/spec/pb_kits/playbook/all_kits_spec.rb
+++ playbook/spec/pb_kits/playbook/all_kits_spec.rb
@@ -5,25 +5,19 @@ require "yaml"
 
 RSpec.describe "All our Kits that are React Components" do
   it "should have a const classes that ends with className" do
-    menu = YAML.load_file(Pathname.new("../../playbook-website/config/menu.yml"))
     all_kits = []
-    menu["kits"].each do |kit|
-      if kit.is_a? Hash
-        kit.values[0].each do |sub_kit|
-          all_kits.push(sub_kit)
-        end
-      else
-        all_kits.push(kit)
-      end
+    Pathname.new("../app/pb_kits/playbook").children.select(&:directory?).each do |kit|
+      all_kits.push(kit.to_s.split("../app/pb_kits/playbook/pb_"))
     end
 
     all_kits.each do |kit|
       path = Pathname.new("../app/pb_kits/playbook/pb_#{kit}/_#{kit}.tsx")
       path = Pathname.new("../app/pb_kits/playbook/pb_#{kit}/_#{kit}.jsx") unless File.exist?(path)
-      if File.exist?(path)
-        file = File.open(path, "r:UTF-8", &:read)
-        expect(file.include?("className)")).to be_truthy if file.include?("constclasses=classnames(")
-      end
+      next unless File.exist?(path)
+
+      file = File.open(path, "r:UTF-8", &:read)
+
+      expect(file.include?("className)")).to be_truthy if file.include?("constclasses=classnames(")
     end
   end
 end
