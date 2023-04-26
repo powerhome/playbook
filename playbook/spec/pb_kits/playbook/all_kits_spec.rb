c86a080d62c3275749414aca644e63ae32489d40
Mark <mark.rosenberg@powerhrg.com>
2023-04-26 09:27:42 -0400

    Include JSX files

--- playbook/spec/pb_kits/playbook/all_kits_spec.rb
+++ playbook/spec/pb_kits/playbook/all_kits_spec.rb
@@ -3,7 +3,7 @@
 require "pathname"
 require "yaml"
 
-describe "All our Kits that are React Components" do
+RSpec.describe "All our Kits that are React Components" do
   it "should have a const classes that ends with className" do
     menu = YAML.load_file(Pathname.new("../../playbook-website/config/menu.yml"))
     all_kits = []
@@ -19,8 +19,9 @@ describe "All our Kits that are React Components" do
 
     all_kits.each do |kit|
       path = Pathname.new("../app/pb_kits/playbook/pb_#{kit}/_#{kit}.tsx")
+      path = Pathname.new("../app/pb_kits/playbook/pb_#{kit}/_#{kit}.jsx") unless File.exist?(path)
       if File.exist?(path)
-        file = File.open(Pathname.new("../app/pb_kits/playbook/pb_#{kit}/_#{kit}.tsx"), "r:UTF-8", &:read)
+        file = File.open(path, "r:UTF-8", &:read)
         expect(file.include?("className)")).to be_truthy if file.include?("constclasses=classnames(")
       end
     end
