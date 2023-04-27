1c29305bca982045cdc90785a2e7c08ab6eda181
Carlos Lima <carloslimasd@gmail.com>
2023-04-27 15:59:02 -0300

    Converting Title Detail kit to TS

--- playbook/app/pb_kits/playbook/pb_title_detail/_title_detail.tsx
+++ playbook/app/pb_kits/playbook/pb_title_detail/_title_detail.tsx
@@ -1,41 +1,42 @@
-/* @flow */
-
 import React from 'react'
 import classnames from 'classnames'
 
 import { globalProps } from '../utilities/globalProps'
-import { buildCss, buildDataProps } from '../utilities/props'
+import { buildAriaProps, buildCss, buildDataProps } from '../utilities/props'
 
 import Body from '../pb_body/_body'
 import Title from '../pb_title/_title'
 
 type TitleDetailProps = {
   align?: "left" | "center" | "right",
+  aria?: { [key: string]: string },
   className?: string,
-  data?: object,
+  data?: { [key: string]: string },
   detail: string,
   id?: string,
   title: string,
 }
 
 const TitleDetail = (props: TitleDetailProps) => {
-  const { align = 'left', className, data = {}, detail, id, title } = props
+  const { align = 'left', aria = {}, className, data = {}, detail, id, title } = props
+  const ariaProps = buildAriaProps(aria)
   const dataProps = buildDataProps(data)
   const pbCss = buildCss('pb_title_detail_kit', align)
 
   return (
     <div
-        {...dataProps}
-        className={classnames(pbCss, globalProps(props), className)}
-        id={id}
+      {...ariaProps}
+      {...dataProps}
+      className={classnames(pbCss, globalProps(props), className)}
+      id={id}
     >
       <Title
-          size={4}
-          text={title}
+        size={4}
+        text={title}
       />
       <Body
-          color="light"
-          text={detail}
+        color="light"
+        text={detail}
       />
     </div>
   )
