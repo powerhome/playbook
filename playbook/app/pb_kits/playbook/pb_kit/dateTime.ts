7297144c460a4feeeddadc6994a85012f6a072a7
Jasper <jasperfurniss@gmail.com>
2023-05-10 23:38:47 -0400

    Removing Moment

--- playbook/app/pb_kits/playbook/pb_kit/dateTime.ts
+++ playbook/app/pb_kits/playbook/pb_kit/dateTime.ts
@@ -1,90 +1,90 @@
-
-import moment, { Moment } from 'moment'
-import 'moment-strftime'
-import 'moment-timezone'
-
 type DateTimeType = {
   value: string | Date,
   zone?: string,
 }
 
-const ABBR_DAYS = ['SU', 'M', 'T', 'W', 'TH', 'F', 'S']
+const ABBR_DAYS = ['SU', 'M', 'T', 'W', 'TH', 'F', 'S'];
 
 export default class DateTime {
-  value: Moment & any
+  value: any
   constructor({ value, zone = 'America/New_York' }: DateTimeType) {
     this.value = this.convertToTimestampZone(value, zone)
   }
 
   convertToTimestampZone(value: string | Date, zone: string) {
-    return moment(value).tz(zone)
+    const date = new Date(value);
+    return new Date(date.toLocaleString('en-US', { timeZone: zone }));
   }
 
   convertToTimezone() {
-    return this.value.strftime('%Z')
+    return Intl.DateTimeFormat().resolvedOptions().timeZone;
   }
 
   toCustomFormat(format = '%-m/%-d') {
-    return this.value.strftime(format)
+    return this.value.toLocaleDateString('en-US', { timeZone: this.convertToTimezone(), formatMatcher: 'basic', month: 'numeric', day: 'numeric' }).replace(/(\d+)\/(\d+)/, format);
   }
 
   toYear() {
-    return this.value.strftime('%Y')
+    return this.value.getFullYear().toString();
   }
 
   toMonth() {
-    return this.value.strftime('%b')
+    return this.value.toLocaleString('en-US', { timeZone: this.convertToTimezone(), formatMatcher: 'basic', month: 'short' });
   }
 
   toMonthNum() {
-    return this.value.strftime('%-m')
+    return (this.value.getMonth() + 1).toString();
   }
 
   toMonthFull() {
-    return this.value.strftime('%B')
+    return this.value.toLocaleString('en-US', { timeZone: this.convertToTimezone(), formatMatcher: 'basic', month: 'long' });
   }
 
   toDay() {
-    return this.value.strftime('%e')
+    return this.value.getDate().toString();
   }
 
   toDayAbbr() {
-    return ABBR_DAYS[this.value.day()]
+    return ABBR_DAYS[this.value.getDay()];
   }
 
   toWeekday() {
-    return this.value.strftime('%a')
+    return this.value.toLocaleString('en-US', { timeZone: this.convertToTimezone(), formatMatcher: 'basic', weekday: 'short' });
   }
 
   toHour() {
-    return this.value.strftime('%l')
+    return this.value.toLocaleString('en-US', { timeZone: this.convertToTimezone(), formatMatcher: 'basic', hour: 'numeric', hour12: true });
   }
 
   toMinute() {
-    return this.value.strftime('%M')
+    return this.value.toLocaleString('en-US', {
+      timeZone: this.convertToTimezone(),
+
+      formatMatcher: 'basic', minute: '2-digit'
+    });
   }
 
   toMeridian() {
-    return this.value.strftime('%P')[0]
+    return this.value.toLocaleString('en-US', { timeZone: this.convertToTimezone(), formatMatcher: 'basic', hour: 'numeric', hour12: true }).slice(-2).toUpperCase();
   }
 
   toIso() {
-    return this.value.toISOString()
+    return this.value.toISOString();
   }
 
   toTime() {
-    const time = this.value.strftime('%I:%M')
+    const time = this.toHour() + ':' + this.toMinute();
 
-    // strftime adds a leading 0 on single hour times. ie 08:31.
-    // this removes that 0 to match the rails kit.
-    return time.charAt() === '0' ? time.slice(1) : time
+    // Intl.DateTimeFormat adds a leading 0 on single hour times. ie 08:31.
+    // this removes that 0 to match the moment.js implementation.
+    return time.charAt() === '0' ? time.slice(1) : time;
   }
 
   toTimezone() {
-    return this.value.strftime('%Z')
+    return this.convertToTimezone();
   }
 
   toTimeWithMeridian() {
-    return this.toTime() + this.toMeridian()
+    return this.toTime() + this.toMeridian();
   }
 }
