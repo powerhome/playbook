06feb662aad80b6e9c19e61fdef4f7176a4e9f75
nida.ghuman <nidaqg@gmail.com>
2023-05-17 17:39:56 -0400

    Added second extension to example

--- playbook/app/pb_kits/playbook/pb_rich_text_editor/docs/_rich_text_editor_more_extensions.jsx
+++ playbook/app/pb_kits/playbook/pb_rich_text_editor/docs/_rich_text_editor_more_extensions.jsx
@@ -5,6 +5,7 @@ import StarterKit from "@tiptap/starter-kit"
 import Link from '@tiptap/extension-link'
 
 import HorizontalRule from "@tiptap/extension-horizontal-rule"
+import Highlight from '@tiptap/extension-highlight'
 
 
 const RichTextEditorMoreExtensions = (props) => {
@@ -13,7 +14,8 @@ const RichTextEditorMoreExtensions = (props) => {
         extensions: [
             StarterKit,
             Link,
-            HorizontalRule
+            HorizontalRule,
+            Highlight.configure({ multicolor: true })
         ],
         content:"Add your text here. You can format your text, add links, quotes, and bullets."
     })
@@ -27,6 +29,12 @@ const RichTextEditorMoreExtensions = (props) => {
         isActive: editor.isActive("horizontalRule"),
         text: "Horizontal Rule",
         onclick: () => editor.chain().focus().setHorizontalRule().run(),
+    },
+    {
+      icon: "highlighter",
+      isActive: editor.isActive("highlight"),
+      text: "Highlighter",
+      onclick: () => editor.chain().focus().toggleHighlight().run(),
     }
   ]
 
--- playbook/app/pb_kits/playbook/pb_rich_text_editor/docs/_rich_text_editor_more_extensions.md
+++ playbook/app/pb_kits/playbook/pb_rich_text_editor/docs/_rich_text_editor_more_extensions.md
@@ -1,6 +1,6 @@
 This variant allows you to optionally include any of [Tiptap’s 53 extensions](https://tiptap.dev/extensions) within any advanced editor by using the `extensions` prop.
 
-__NOTE__: In order to leverage this prop, you __must__ install the extension you need in your project, import it and pass it to the extensions array as shown in this example with the HorizontalRule extension. 
+__NOTE__: In order to leverage this prop, you __must__ install the extension you need in your project, import it and pass it to the extensions array as shown in this example with the HorizontalRule and the Highlight extensions. 
 
 In order to add the extension to the editor toolbar, create an array of objects (as shown in the ExtensionsList array in the example below). Each object in this array should include:
 
