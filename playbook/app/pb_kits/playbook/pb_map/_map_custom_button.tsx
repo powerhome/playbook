e276a065ec447eb7482c43c0ee8e010efd783080
Nida Ghuman <nidaqg@gmail.com>
2023-07-06 13:17:55 -0400

    [PLAY-837] Map Custom Button (#2626)
    **What does this PR do?**
    
    1) Adds a custom button component for Map kit
    2) Refactors code to extract controls into Map.Controls component that
    will allow Map kit to be used as compound kit if needed
    3) Doc example for new method
    4) No breaking change for existing Map kit implementations
    
    [Runway Story](https://nitro.powerhrg.com/runway/backlog_items/PLAY-837)
    
    **Screenshots:** 
    
    <img width="1196" alt="Screenshot 2023-07-01 at 12 54 29 AM"
    src="https://github.com/powerhome/playbook/assets/73710701/fd887cfe-2579-41a8-89a8-988da3f59ee5">

---
+++ playbook/app/pb_kits/playbook/pb_map/_map_custom_button.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Button from "../pb_button/_button";
+import Icon from "../pb_icon/_icon";
+
+type MapCustomButtonTypes = {
+  onClick?: () => {};
+  icon?: string;
+};
+
+const MapCustomButton = ({ onClick, icon }: MapCustomButtonTypes) => {
+  return (
+    <Button className="pb_map-custom-button" onClick={onClick}>
+      <Icon icon={icon} />
+    </Button>
+  );
+};
+
+export default MapCustomButton;
