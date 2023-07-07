1e3c788c7e7c5ccaaafde6add6ae1f30373ac9c4
Carlos Lima <carloslimasd@gmail.com>
2023-07-06 22:42:54 -0300

    Implementing Responsive Title

---
+++ playbook/app/pb_kits/playbook/pb_title/docs/_title_responsive.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Title from '../_title'
+
+const TitleResponsive = (props) => {
+  return (
+    <>
+      <Title
+          size={{xs: "3", sm: "2", md: "1"}}
+          text="Responsive Title"
+          {...props}
+      />
+    </>
+  )
+}
+
+export default TitleResponsive
--- playbook/app/pb_kits/playbook/pb_title/title.rb
+++ playbook/app/pb_kits/playbook/pb_title/title.rb
@@ -6,9 +6,7 @@ module Playbook
       prop :color, type: Playbook::Props::Enum,
                    values: [nil, "default", "light", "lighter", "success", "error", "link"],
                    default: nil
-      prop :size, type: Playbook::Props::Enum,
-                  values: [1, 2, 3, 4],
-                  default: 3
+      prop :size, default: 3
       prop :tag, type: Playbook::Props::Enum,
                  values: %w[h1 h2 h3 h4 h5 h6 p div span],
                  default: "h3"
@@ -20,11 +18,26 @@ module Playbook
       prop :bold, type: Playbook::Props::Boolean, default: true
 
       def classname
-        generate_classname("pb_title_kit", size, variant, color, is_bold)
+        title_class = generate_classname("pb_title_kit", variant, color, is_bold)
+        title_class + generate_size_classname
       end
 
       def is_bold
         bold ? nil : "thin"
+      end
+
+      def generate_size_classname
+        responsive = try(:size).is_a?(::Hash)
+        css = ""
+        if responsive
+          size.each do |key, value|
+            css += " pb_title_kit_#{key}_#{value}"
+          end
+        else
+          css += " pb_title_kit_size_#{size}"
+        end
+
+        css unless css.blank?
       end
     end
   end
