a4fe68dc42d0a5739db839511a7b6babb148a6d0
nida.ghuman <nidaqg@gmail.com>
2023-07-10 09:24:27 -0400

    Updated doc examnples, fixed class name in rails

--- playbook/app/pb_kits/playbook/pb_icon_circle/docs/_icon_circle_emoji.jsx
+++ playbook/app/pb_kits/playbook/pb_icon_circle/docs/_icon_circle_emoji.jsx
@@ -7,16 +7,21 @@ const IconCircleEmoji = (props) => {
     <div>
       <IconCircle
           emoji="😁"
+          size="sm"
           {...props}
       />
       <br/>
       <IconCircle
           emoji="&#128525;"
+          size="md"
+          variant="red"
           {...props}
       />
       <br/>
       <IconCircle
           emoji="&#x1F389;"
+          size="lg"
+          variant="teal"
           {...props}
       />
     </div>
