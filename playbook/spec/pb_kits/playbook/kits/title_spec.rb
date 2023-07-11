3b733003c63475dc22151aafc7fcec11e9fccddf
Carlos Lima <carloslimasd@gmail.com>
2023-07-11 14:20:53 -0300

    [PLAY-802] Implementing Responsive Title (#2631)
    **What does this PR do?**
    It implements responsive Title sizes so that we can dynamically change
    Title sizes based on screen size.
    
    **Screenshots:** Screenshots to visualize your addition/change
    <img width="859" alt="image"
    src="https://github.com/powerhome/playbook/assets/2573205/56494ffd-86a2-4a89-99ca-7b86c803e436">
    
    **How to test?** Steps to confirm the desired behavior:
    1. Go to Title Kit on Playbook Website
    2. Scroll down to **Responsive** example
    
    
    #### Checklist:
    - [x] **LABELS** Add a label: `enhancement`, `bug`, `improvement`, `new
    kit`, `deprecated`, or `breaking`. See [Changelog &
    Labels](https://github.com/powerhome/playbook/wiki/Changelog-&-Labels)
    for details.
    - [x] **DEPLOY** I have added the `milano` label to show I'm ready for a
    review.
    - [ ] **TESTS** I have added test coverage to my code.
    
    ---------
    
    Co-authored-by: Nida Ghuman <nidaqg@gmail.com>

--- playbook/spec/pb_kits/playbook/kits/title_spec.rb
+++ playbook/spec/pb_kits/playbook/kits/title_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe Playbook::PbTitle::Title do
     is_expected.to define_boolean_prop(:dark)
       .with_default(false)
   }
-  it { is_expected.to define_prop(:size).of_type(Playbook::Props::Enum).with_default(3) }
+  it { is_expected.to define_prop(:size).with_default(3) }
   it {
     is_expected.to define_enum_prop(:tag)
       .with_values("h1", "h2", "h3", "h4", "h5", "h6", "p", "div", "span")
@@ -29,15 +29,15 @@ RSpec.describe Playbook::PbTitle::Title do
 
   describe "#classname" do
     it "returns namespaced class name", :aggregate_failures do
-      expect(subject.new({}).classname).to eq "pb_title_kit_3"
+      expect(subject.new({}).classname).to eq "pb_title_kit pb_title_kit_size_3"
 
-      expect(subject.new(classname: "additional_class").classname).to eq "pb_title_kit_3 additional_class"
-      expect(subject.new(dark: true).classname).to eq "pb_title_kit_3 dark"
-      expect(subject.new(size: nil).classname).to eq "pb_title_kit_3"
-      expect(subject.new(size: 4).classname).to eq "pb_title_kit_4"
-      expect(subject.new(tag: "h3").classname).to eq "pb_title_kit_3"
-      expect(subject.new(size: 4, color: "link").classname).to eq "pb_title_kit_4_link"
-      expect(subject.new(bold: false).classname).to eq "pb_title_kit_3_thin"
+      expect(subject.new(classname: "additional_class").classname).to eq "pb_title_kit additional_class pb_title_kit_size_3"
+      expect(subject.new(dark: true).classname).to eq "pb_title_kit dark pb_title_kit_size_3"
+      expect(subject.new(size: nil).classname).to eq "pb_title_kit pb_title_kit_size_3"
+      expect(subject.new(size: 4).classname).to eq "pb_title_kit pb_title_kit_size_4"
+      expect(subject.new(tag: "h3").classname).to eq "pb_title_kit pb_title_kit_size_3"
+      expect(subject.new(size: 4, color: "link").classname).to eq "pb_title_kit_link pb_title_kit_size_4"
+      expect(subject.new(bold: false).classname).to eq "pb_title_kit_thin pb_title_kit_size_3"
     end
   end
 
