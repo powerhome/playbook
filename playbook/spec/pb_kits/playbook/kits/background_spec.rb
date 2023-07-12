db008c46fd19be965a94716246e1467582ddedcf
Israel Molestina <molestinaisrael@gmail.com>
2023-07-12 14:24:01 -0400

    updated tests for new props and values in playbook kits

--- playbook/spec/pb_kits/playbook/kits/background_spec.rb
+++ playbook/spec/pb_kits/playbook/kits/background_spec.rb
@@ -46,11 +46,11 @@ RSpec.describe Playbook::PbBackground::Background do
     end
   end
 
-  describe "#custom_background_color" do
+  describe "#custom_background_color_and_position" do
     it "returns a hash with a hex value assigned to the background-color" do
-      kit = subject.new(custom_color: "#1d99a8")
+      kit = subject.new(custom_color: "#1d99a8", background_position: "center right")
 
-      expect(kit.custom_background_color).to eq "background-color: #1d99a8;"
+      expect(kit.custom_background_color).to eq "background-color: #1d99a8;\n        background-position: center right"
     end
   end
 end
