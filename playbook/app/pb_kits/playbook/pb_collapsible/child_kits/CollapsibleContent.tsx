f11ab2b8a1adf6af7b560659a1e7aed31cb91411
nida.ghuman <nidaqg@gmail.com>
2023-07-27 16:52:21 -0400

    fixing react side animation

--- playbook/app/pb_kits/playbook/pb_collapsible/child_kits/CollapsibleContent.tsx
+++ playbook/app/pb_kits/playbook/pb_collapsible/child_kits/CollapsibleContent.tsx
@@ -1,7 +1,8 @@
 import classnames from 'classnames'
-import React, { useContext } from 'react'
+import React, { useContext, useRef, useEffect } from 'react'
 import { buildCss } from '../../utilities/props'
 import { globalProps } from '../../utilities/globalProps'
+import { hideElement, showElement } from '../_helper_functions'
 
 import CollapsibleContext from '../context'
 
@@ -20,9 +21,23 @@ const CollapsibleContent = ({
   const context: {[key: string]: boolean | string} = useContext(CollapsibleContext)
   const contentCSS = buildCss('pb_collapsible_content_kit')
   const contentSpacing = globalProps(props, { padding })
+  const contentRef = useRef(null);
+
+  useEffect(() => {
+    // Use the showElement and hideElement functions based on the context value
+    if (contentRef.current) {
+      if (context.collapsed) {
+        hideElement(contentRef.current);
+      } else {
+        showElement(contentRef.current);
+      }
+    }
+  }, [context.collapsed]);
 
   return (
-    <div data-collapsible-content="true" className={classnames(contentCSS, contentSpacing, "toggle-content", context.collapsed ? "" : "is-visible", className)}>
+    <div ref={contentRef}
+      data-collapsible-content="true" 
+      className={classnames(contentCSS, contentSpacing, "toggle-content", className)}>
       {children}
     </div>
   )
