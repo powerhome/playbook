6679a985648c5dc477259095921715222d1114e5
Nida Ghuman <nidaqg@gmail.com>
2023-07-28 09:35:41 -0400

    [PLAY-905] Reaction Button Variant for Button Kit (#2658)
    **What does this PR do?** 
    
    Adds the new `reaction` variant to the button kit
    
    **Screenshots:** 
    ![Screenshot 2023-07-25 at 3 35 16
    PM](https://github.com/powerhome/playbook/assets/73710701/8152d3ec-4a49-4a14-9c05-8ddb5b452630)
    
    **How to test?** Steps to confirm the desired behavior:
    1. Go to '...'
    2. Click on '....'
    3. Scroll down to '....'
    4. See addition/change
    
    ---------
    
    Co-authored-by: Jasper <jasperfurniss@gmail.com>

---
+++ playbook/app/pb_kits/playbook/pb_button/docs/_button_reaction.jsx
@@ -0,0 +1,47 @@
+import React, {useState} from "react"
+import { Button } from "../../"
+
+const ButtonReaction = (props) => {
+
+const [highlightActive, setHighlightActive] =useState(false)
+const reactionCount = 153
+
+return (
+  <div>
+    <Button
+        count={highlightActive ? (reactionCount + 1) : reactionCount}
+        highlight = {highlightActive}
+        icon="&#127881;"
+        onClick={()=> setHighlightActive(!highlightActive)}
+        tabIndex={0}
+        variant="reaction"
+        {...props}
+    />
+    <Button
+        count={5}
+        icon="😍"
+        marginLeft='lg'
+        tabIndex={0}
+        variant="reaction"
+        {...props}
+    />
+    <Button
+        marginLeft='lg'
+        tabIndex={0}
+        variant="reaction"
+        {...props}
+    />
+    <Button
+        icon="user"
+        marginLeft='lg'
+        tabIndex={0}
+        variant="reaction"
+        {...props}
+    />
+
+
+    </div>
+)
+}
+
+export default ButtonReaction
