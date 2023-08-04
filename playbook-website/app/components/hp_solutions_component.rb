8c434d524a4ff030c1e7349c09f338d8de56dc34
Israel Molestina <molestinaisrael@gmail.com>
2023-08-04 11:34:47 -0400

    creating slider

--- playbook-website/app/components/hp_solutions_component.rb
+++ playbook-website/app/components/hp_solutions_component.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class HpSolutionsComponent < Playbook::KitBase
+  def render_code(text, language)
+    formatter = Rouge::Formatters::HTML.new(scope: ".highlight")
+    lexer = Rouge::Lexer.find(language)
+    formatter.format(lexer.lex(text))
+  end
 end
