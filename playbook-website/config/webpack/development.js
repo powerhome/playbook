8254b7a5d3be6415caedd8160010eed621f90b82
Stephen Marshall <smarshall1980@gmail.com>
2023-08-10 12:20:42 -0400

    Add webpack bundle analyzer

--- playbook-website/config/webpack/development.js
+++ playbook-website/config/webpack/development.js
@@ -1,5 +1,15 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
 const environment = require('./environment')
+
+environment.config.merge({
+  plugins: [
+    new BundleAnalyzerPlugin({
+      generateStatsFile: true,
+    }),
+  ],
+})
 
 module.exports = environment.toWebpackConfig()
