cebd9e5c19bc34d86dfeb768ef02924a92026be6
Jasper <jasperfurniss@gmail.com>
2023-08-14 20:05:21 -0400

    Latest For File Upload

--- playbook/app/pb_kits/playbook/pb_file_upload/file_upload.rb
+++ playbook/app/pb_kits/playbook/pb_file_upload/file_upload.rb
@@ -5,8 +5,15 @@ module Playbook
     class FileUpload < Playbook::KitBase
       prop :accept, type: Playbook::Props::String,
                     default: ""
+
       prop :files, type: Playbook::Props::Array,
                    default: []
+
+      prop :label, type: Playbook::Props::String,
+                   default: "Upload File"
+
+      prop :placeholder, type: Playbook::Props::String,
+                         default: "No file chosen"
 
       def classname
         generate_classname("pb_file_upload_kit")
