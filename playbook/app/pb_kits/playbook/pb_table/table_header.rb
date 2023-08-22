35a647c6cd2689875d49cf67dbf6508b94cb4661
Mark <mark.rosenberg@powerhrg.com>
2023-08-22 13:31:04 -0400

    Add dropdown function and example

--- playbook/app/pb_kits/playbook/pb_table/table_header.rb
+++ playbook/app/pb_kits/playbook/pb_table/table_header.rb
@@ -3,10 +3,14 @@
 module Playbook
   module PbTable
     class TableHeader < Playbook::KitBase
-      prop :colspan, type: Playbook::Props::Number,
-                     default: 1
       prop :align, type: Playbook::Props::String,
                    default: "start"
+      prop :colspan, type: Playbook::Props::Number,
+                     default: 1
+      prop :placement, type: Playbook::Props::Enum,
+                       values: %w[top bottom left right top-start top-end bottom-start bottom-end right-start right-end left-start left-end],
+                       default: "bottom-start"
+      prop :sort_menu, type: Playbook::Props::HashArray, default: [{}]
 
       def classname
         generate_classname("pb_table_header_kit", align_class)
@@ -14,6 +18,17 @@ module Playbook
 
       def align_class
         align.present? ? "align_#{align}" : nil
+      end
+
+      def sort_icon(direction)
+        case direction
+        when "asc"
+          "sort-amount-up"
+        when "desc"
+          "sort-amount-down"
+        else
+          ""
+        end
       end
     end
   end
