3b5889a285ffbdb2e59331c2cc5893f96724ef2a
nickamantia <nick.amantia@powerhrg.com>
2023-08-23 10:06:01 -0400

    milano test

--- playbook-website/app/helpers/changelog_helper.rb
+++ playbook-website/app/helpers/changelog_helper.rb
@@ -6,24 +6,22 @@ module ChangelogHelper
     document = Nokogiri::HTML5(md_html)
     posts = []
 
-    document.search("h1").each_with_index do |title, i|
+    document.css("h1").each_with_index do |title, i|
       index_element = i + 1
-      start_element = "//h1[#{index_element}]"
-      end_element = "//h1[#{index_element + 1}]"
-      set_a = "#{start_element}/following-sibling::*"
-      set_b = "#{end_element}/preceding-sibling::*"
-      my_content = document.xpath("#{set_a}[ count(.|#{set_b}) = count(#{set_b}) ]
-        | #{start_element} | #{end_element}")
-      image = my_content.search("img").map { |img| img["src"] }&.first
+      start_element = "h1:nth-of-type(#{index_element})"
+      end_element = "h1:nth-of-type(#{index_element + 1})"
+      set_a = "#{start_element} ~ *:not(#{end_element})"
+      my_content = document.css(set_a)
+      image = my_content.css("img").map { |img| img["src"] }
       next if image.nil?
 
       posts << {
         title: title.text,
-        description: my_content.css("p ~ p").map(&:text)&.first,
-        date: my_content.search("h5").map(&:text)&.first,
+        description: my_content.css("p ~ p").map(&:text).first,
+        date: my_content.css("h5").map(&:text).first,
         image: image,
-        link: title.xpath("a[1]")[0]["href"]&.gsub!("#", ""),
-        content: my_content.search("p").to_s,
+        link: title.css("a:first-child").attr("href")&.value&.gsub("#", ""),
+        content: my_content.css("p").to_s,
       }
     end
 
