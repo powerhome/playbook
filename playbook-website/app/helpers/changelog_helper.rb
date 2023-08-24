48597e6e919417e1b7be2a4803eb4995addb9e09
nickamantia <nick.amantia@powerhrg.com>
2023-08-24 14:12:39 -0400

    reverting back to prev code

--- playbook-website/app/helpers/changelog_helper.rb
+++ playbook-website/app/helpers/changelog_helper.rb
@@ -2,10 +2,10 @@
 
 module ChangelogHelper
   def changelog_to_hash(changelog)
+    posts = []
+
     markdown_html = render_markdown(changelog)
     document = parse_html(markdown_html)
-
-    posts = []
 
     document.css("h1").each_with_index do |title_element, index|
       post = extract_post_data(document, title_element, index)
@@ -18,11 +18,15 @@ module ChangelogHelper
 private
 
   def parse_html(html)
-    Nokogiri::HTML5.parse(html)  # Use Nokogiri's parse method directly
+    Nokogiri::HTML5(html)
   end
 
   def extract_post_data(document, title_element, index)
-    content = extract_content(document, index)
+    index_element = index + 1
+    start_element = "h1:nth-of-type(#{index_element})"
+    end_element = "h1:nth-of-type(#{index_element + 1})"
+    set_a = "#{start_element} ~ *:not(#{end_element})"
+    content = document.css(set_a)
 
     image_urls = extract_images(content)
     return {} if image_urls.empty?
@@ -37,21 +41,13 @@ private
     }
   end
 
-  def extract_content(document, index)
-    index_element = index + 1
-    start_element = "h1:nth-of-type(#{index_element})"
-    end_element = "h1:nth-of-type(#{index_element + 1})"
-    set_a = "#{start_element} ~ *:not(#{end_element})"
-    document.css(set_a)
-  end
-
   def extract_description(content)
-    first_paragraph = content.at_css("p ~ p")
+    first_paragraph = content.css("p ~ p").first
     first_paragraph&.text
   end
 
   def extract_date(content)
-    first_h5 = content.at_css("h5")
+    first_h5 = content.css("h5").first
     first_h5&.text
   end
 
@@ -60,8 +56,7 @@ private
   end
 
   def extract_link(title_element)
-    link_element = title_element.at_css("a:first-child")
-    link = link_element&.attr("href")
+    link = title_element.css("a:first-child").attr("href")&.value
     link&.gsub("#", "")
   end
 end
