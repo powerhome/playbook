8fc5c5496932e0bcf346df776f0ae4ea78e81b80
Mark <mark.rosenberg@powerhrg.com>
2023-08-25 12:05:06 -0400

    Fix popover issue

--- playbook/app/pb_kits/playbook/pb_table/table_header.rb
+++ playbook/app/pb_kits/playbook/pb_table/table_header.rb
@@ -5,17 +5,18 @@ module Playbook
     class TableHeader < Playbook::KitBase
       prop :align, type: Playbook::Props::String,
                    default: "start"
+      prop :align_content, type: Playbook::Props::Enum,
+                           values: %w[start center end stretch baseline none],
+                           default: "center"
       prop :colspan, type: Playbook::Props::Number,
                      default: 1
       prop :placement, type: Playbook::Props::Enum,
                        values: %w[top bottom left right top-start top-end bottom-start bottom-end right-start right-end left-start left-end],
                        default: "bottom-start"
-      prop :sort_menu, type: Playbook::Props::HashArray, default: [{}]
+      prop :sort_menu, type: Playbook::Props::HashArray,
+                       default: [{}]
       prop :text, type: Playbook::Props::String,
                   default: ""
-      prop :align_content, type: Playbook::Props::Enum,
-                           values: %w[start center end stretch baseline none],
-                           default: "center"
 
       def classname
         generate_classname("pb_table_header_kit", align_class)
