a226818ed01f4d586c99fcf6cde07efbfecd3225
nida.ghuman <nidaqg@gmail.com>
2023-08-25 14:13:59 -0400

    removed unneeded code

--- playbook/app/pb_kits/playbook/pb_nav/nav.rb
+++ playbook/app/pb_kits/playbook/pb_nav/nav.rb
@@ -13,7 +13,6 @@ module Playbook
                      default: "normal"
       prop :highlight, type: Playbook::Props::Boolean, default: true
       prop :borderless, type: Playbook::Props::Boolean, default: false
-      prop :item_padding, type: Playbook::Props::Hash, default: {}
 
       def classname
         generate_classname("pb_nav_list", variant, orientation, highlight_class, borderless_class)
@@ -25,18 +24,6 @@ module Playbook
 
       def borderless_class
         borderless ? "borderless" : nil
-      end
-
-      def modified_content
-        parsed_content = Nokogiri::HTML.fragment(content.presence)
-
-        parsed_content.css("li").each do |element|
-          element["data-orientation"] = orientation
-          element["data-variant"] = variant
-          element["data-spacing"] = item_padding
-        end
-
-        parsed_content.to_html
       end
     end
   end
