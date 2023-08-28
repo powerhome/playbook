6bb242f6970ef50740aa787eab8faf5bec05152b
nickamantia <nick.amantia@powerhrg.com>
2023-08-28 12:04:46 -0400

    this might work????

--- playbook-website/app/helpers/changelog_helper.rb
+++ playbook-website/app/helpers/changelog_helper.rb
@@ -8,8 +8,12 @@ module ChangelogHelper
     document = parse_html(markdown_html)
 
     document.css("h1").lazy.take(5).each_with_index do |title_element, index|
-      post = extract_post_data(document, title_element, index)
-      posts << post if post[:image].present?
+      title_text = title_element.text
+      puts title_text
+      if title_text.match?(/[a-zA-Z]/)
+        post = extract_post_data(document, title_element, index)
+        posts << post
+      end
     end
 
     posts
@@ -28,14 +32,22 @@ private
     set_a = "#{start_element} ~ *:not(#{end_element})"
     content = document.css(set_a)
 
-    image_urls = extract_images(content)
-    return {} if image_urls.empty?
+    # Initialize variables
+    # image = nil
+    # image_index = 0
+    title_text = title_element.text
+
+    if title_text.match?(/[a-zA-Z]/)
+      image = extract_image(content)
+      # image_index += 1
+    end
 
     {
-      title: title_element.text,
+      title: title_text,
       description: extract_description(content),
       date: extract_date(content),
-      image: image_urls,
+      # image: image,
+      image: image[0],
       link: extract_link(title_element),
       content: content.css("p").to_s,
     }
@@ -51,8 +63,9 @@ private
     first_h5&.text
   end
 
-  def extract_images(content)
-    content.css("p a img").map { |img| img["src"] }
+  def extract_image(content)
+    # content.css("img")[0].attr("src");
+    content.css("img").map { |img| img["src"] }
   end
 
   def extract_link(title_element)
