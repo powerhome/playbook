55498a97ea6277f6950b9ab0e3f014fa7600deff
nida.ghuman <nidaqg@gmail.com>
2023-09-06 22:38:45 -0400

    custom toggling + keep clicked item toggled open

--- playbook-website/app/javascript/components/MainSidebar/index.tsx
+++ playbook-website/app/javascript/components/MainSidebar/index.tsx
@@ -1,8 +1,33 @@
-import React from "react";
-import { Nav, NavItem } from "playbook-ui";
+import React, { useEffect } from "react";
+import { Nav, NavItem, useCollapsible } from "playbook-ui";
 import { linkFormat } from "../../utilities/website_sidebar_helper";
 
 const MainSidebar = ({ dark, type, category, kit, kits }) => {
+  const collapsibles = kits.map(() => useCollapsible(true));
+
+  //using localstorage to store index of clicked item in between page reloads
+  useEffect(() => {
+    const clicked = localStorage.getItem("clickedNavItem");
+    setTimeout(() => {
+      collapsibles.forEach(([, , setCollapsed], idx) => {
+        if (idx.toString() === clicked) {
+          setCollapsed(false);
+        }
+      });
+    }, 100);
+  }, []);
+
+  //set up custom toggling
+  const handleMainClick = (index) => {
+    collapsibles.forEach(([, , setCollapsed], idx) => {
+      if (idx === index) {
+        setCollapsed(false);
+        localStorage.setItem("clickedNavItem", idx);
+      } else {
+        setCollapsed(true);
+      }
+    });
+  };
 
   return (
     <>
@@ -26,35 +51,38 @@ const MainSidebar = ({ dark, type, category, kit, kits }) => {
           text="Components"
           paddingY="xxs"
         >
-          {kits.map((link, i) =>
-            typeof link === "object" ? (
-                <NavItem
-                  active={category === Object.keys(link)[0]}
-                  collapsible
-                  collapsibleTrail
-                  dark={dark}
-                  fontSize="small"
-                  iconRight={["plus", "minus"]}
-                  key={`${Object.keys(link)[0]}-${i}`}
-                  link={`/kit_category/${Object.keys(link)}?type=${type}`}
-                  marginBottom="none"
-                  marginTop="xxs"        
-                  text={linkFormat(Object.keys(link))}
-                  paddingY="xxs"
-                >
-                  {link[Object.keys(link)[0]].map((sublink, i) => (
-                    <NavItem
-                      active={kit === sublink}
-                      dark={dark}
-                      fontSize="small"
-                      key={`${sublink}-${i}`}
-                      link={`/kits/${sublink}/${type}`}
-                      marginY="none"
-                      paddingY="xxs"
-                      text={linkFormat(sublink)}
-                    />
-                  ))}
-                </NavItem>
+          {kits.map((link, i) => {
+            const [collapsed] = collapsibles[i];
+            return typeof link === "object" ? (
+              <NavItem
+                active={category === Object.keys(link)[0]}
+                collapsed={collapsed}
+                collapsible
+                collapsibleTrail
+                dark={dark}
+                fontSize="small"
+                iconRight={["plus", "minus"]}
+                key={`${Object.keys(link)[0]}-${i}`}
+                link={`/kit_category/${Object.keys(link)}?type=${type}`}
+                marginBottom="none"
+                marginTop="xxs"
+                onClick={() => handleMainClick(i)}
+                paddingY="xxs"
+                text={linkFormat(Object.keys(link))}
+              >
+                {link[Object.keys(link)[0]].map((sublink, i) => (
+                  <NavItem
+                    active={kit === sublink}
+                    dark={dark}
+                    fontSize="small"
+                    key={`${sublink}-${i}`}
+                    link={`/kits/${sublink}/${type}`}
+                    marginY="none"
+                    paddingY="xxs"
+                    text={linkFormat(sublink)}
+                  />
+                ))}
+              </NavItem>
             ) : (
               <NavItem
                 active={kit === link}
@@ -63,12 +91,12 @@ const MainSidebar = ({ dark, type, category, kit, kits }) => {
                 key={`${link}-${i}`}
                 link={`/kits/${link}?type=${type}`}
                 marginBottom="none"
-                marginTop="xxs"      
+                marginTop="xxs"
                 text={linkFormat(link)}
                 paddingY="xxs"
               />
-            )
-          )}
+            );
+          })}
         </NavItem>
       </Nav>
     </>
