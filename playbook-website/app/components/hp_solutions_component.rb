80f39c5a4ed8dcafa7db0fe16e94146adfeb932f
gavhuang <47684670+gavhuang@users.noreply.github.com>
2023-09-07 17:02:41 -0400

    Include render_code helper, refactor CSS, add background to What's New

--- playbook-website/app/components/hp_solutions_component.rb
+++ playbook-website/app/components/hp_solutions_component.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
 class HpSolutionsComponent < Playbook::KitBase
-  def render_code(text, language)
-    formatter = Rouge::Formatters::HTML.new(scope: ".highlight")
-    lexer = Rouge::Lexer.find(language)
-    formatter.format(lexer.lex(text))
-  end
+  include PlaybookWebsite::Markdown::Helper
 end
