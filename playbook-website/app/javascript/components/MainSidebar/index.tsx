6cd00b5273602fff8f931960f822742ae81254be
nida.ghuman <nidaqg@gmail.com>
2023-09-06 20:42:37 -0400

    collapsible nav into sidebar

--- playbook-website/app/javascript/components/MainSidebar/index.tsx
+++ playbook-website/app/javascript/components/MainSidebar/index.tsx
@@ -3,50 +3,77 @@ import { Nav, NavItem } from "playbook-ui";
 import { linkFormat } from "../../utilities/website_sidebar_helper";
 
 const MainSidebar = ({ dark, type, category, kit, kits }) => {
+  const handleClick = (link, type) => {
+    setTimeout(() => {
+      window.location.href = `/kit_category/${Object.keys(link)}?type=${type}`;
+    }, 1200);
+  };
+
   return (
     <>
       <Nav
         dark={dark}
         link={`/kits${type ? `?type=${type}` : ""}`}
-        paddingY="sm"
-        title="Kits"
-        variant="subtle"
+        variant="bold"
       >
-        {kits.map((link, i) =>
-          typeof link === "object" ? (
-            <div key={`${Object.keys(link)[0]}-${i}`} className="category_section">
-              <NavItem
-                active={category === Object.keys(link)[0]}
-                className="category"
-                dark={dark}
-                iconRight="angle-down"
-                key={`${Object.keys(link)[0]}-${i}`}
-                link={`/kit_category/${Object.keys(link)}?type=${type}`}
-                text={linkFormat(Object.keys(link))}
-              />
-
-              {link[Object.keys(link)[0]].map((sublink, i) => (
+        <NavItem
+          collapsed={false}
+          collapsible
+          collapsibleTrail
+          dark={dark}
+          fontSize="small"
+          fontWeight="bolder"
+          iconRight={["plus", "minus"]}
+          key="top-nav-item"
+          link={`/kits`}
+          marginY="xxs"
+          text="Components"
+          paddingY="xxs"
+        >
+          {kits.map((link, i) =>
+            typeof link === "object" ? (
                 <NavItem
-                  active={kit === sublink}
+                  // onClick={() => handleClick(link, type)}
+                  // onIconRightClick={}
+                  active={category === Object.keys(link)[0]}
+                  collapsible
+                  collapsibleTrail
                   dark={dark}
-                  key={`${sublink}-${i}`}
-                  marginLeft="xl"
-                  link={`/kits/${sublink}/${type}`}
-                  text={linkFormat(sublink)}
-                />
-              ))}
-            </div>
-          ) : (
-            <NavItem
-              active={kit === link}
-              className="category"
-              dark={dark}
-              key={`${link}-${i}`}
-              text={linkFormat(link)}
-              link={`/kits/${link}?type=${type}`}
-            />
-          )
-        )}
+                  fontSize="small"
+                  iconRight={["plus", "minus"]}
+                  key={`${Object.keys(link)[0]}-${i}`}
+                  link={`/kit_category/${Object.keys(link)}?type=${type}`}
+                  marginY="xxs"
+                  text={linkFormat(Object.keys(link))}
+                  paddingY="xxs"
+                >
+                  {link[Object.keys(link)[0]].map((sublink, i) => (
+                    <NavItem
+                      active={kit === sublink}
+                      dark={dark}
+                      fontSize="small"
+                      key={`${sublink}-${i}`}
+                      link={`/kits/${sublink}/${type}`}
+                      marginY="none"
+                      paddingY="xxs"
+                      text={linkFormat(sublink)}
+                    />
+                  ))}
+                </NavItem>
+            ) : (
+              <NavItem
+                active={kit === link}
+                dark={dark}
+                fontSize="small"
+                key={`${link}-${i}`}
+                link={`/kits/${link}?type=${type}`}
+                marginY="xxs"
+                text={linkFormat(link)}
+                paddingY="xxs"
+              />
+            )
+          )}
+        </NavItem>
       </Nav>
     </>
   );
