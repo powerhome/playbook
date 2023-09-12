16c9ee3088b5d269fbbc44f325c01f1c37ea6b5c
gavhuang <47684670+gavhuang@users.noreply.github.com>
2023-09-12 14:52:21 -0400

    Remove transaction traces (see Huddle PR#422)

--- playbook-website/config/initializers/sentry.rb
+++ playbook-website/config/initializers/sentry.rb
@@ -3,11 +3,4 @@
 Sentry.init do |config|
   config.dsn = ENV["SENTRY_DSN"]
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
-
-  # Set traces_sample_rate to 1.0 to capture 100%
-  # of transactions for performance monitoring.
-  # We recommend adjusting this value in production.
-  config.traces_sample_rate = 0.5
-  # or
-  config.traces_sampler = ->(_) { true }
 end
