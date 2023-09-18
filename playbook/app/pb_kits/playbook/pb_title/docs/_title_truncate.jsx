978eedc071c57dadcf66cc764c5e06f60aeb1d4c
gavhuang <47684670+gavhuang@users.noreply.github.com>
2023-09-18 14:45:29 -0400

    Title - React

--- playbook/app/pb_kits/playbook/pb_title/docs/_title_truncate.jsx
+++ playbook/app/pb_kits/playbook/pb_title/docs/_title_truncate.jsx
@@ -1,36 +1,62 @@
-import React from "react"
+import React from 'react';
 
-import Title from "../_title"
+import Title from '../_title';
+import Caption from '../../pb_caption/_caption'
 
 const TitleTruncate = (props) => {
+  const lorem =
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat est quae at commodi natus eos, doloribus ducimus consectetur expedita nisi nihil itaque dolorum fugit ratione earum distinctio reprehenderit! Veniam minus repellat laudantium provident tempora exercitationem unde dignissimos voluptas illo aperiam ut pariatur voluptatibus fugiat, incidunt rem, temporibus porro. Earum optio sint laboriosam eius ad obcaecati, accusamus ab inventore distinctio doloribus architecto sit! Eveniet perspiciatis dolore corporis ratione dicta! Quia, quibusdam."
+
   return (
-    <div>
+    <>
+      <Caption text="After first row" />
       <Title
-          text='Title 1'
+          size={4}
+          text={lorem}
           truncate={1}
           {...props}
       />
+
+      <br />
+
+      <Caption text="After second row" />
       <Title
-          text='Title 1'
+          size={4}
+          text={lorem}
           truncate={2}
           {...props}
       />
+
+      <br />
+
+      <Caption text="After third row" />
       <Title
-          text='Title 1'
+          size={4}
+          text={lorem}
           truncate={3}
           {...props}
       />
+
+      <br />
+
+      <Caption text="After fourth row" />
       <Title
-          text='Title 1'
+          size={4}
+          text={lorem}
           truncate={4}
           {...props}
       />
+
+      <br />
+
+      <Caption text="After fifth row" />
       <Title
-          text='Title 1'
+          size={4}
+          text={lorem}
           truncate={5}
           {...props}
       />
-    </div>
+    </>
   )
 }
 
