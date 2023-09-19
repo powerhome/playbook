76c6cd975ac4515b9117a5344f918aa5021a9687
nida.ghuman <nidaqg@gmail.com>
2023-09-18 22:19:19 -0400

    active state for top level nav item

--- playbook-website/app/javascript/components/MainSidebar/index.tsx
+++ playbook-website/app/javascript/components/MainSidebar/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Nav, NavItem, useCollapsible } from "playbook-ui";
 import { renderNavItem } from "./NestedNavItems";
 
@@ -13,15 +13,26 @@ const MainSidebar = ({ dark, type, category, kit, kits }) => {
   const [isTopLevelCollapsed, setIsTopLevelCollapsed] = useState(false);
 
   const currentURL = window.location.pathname + window.location.search;
-  const componentsLink = currentURL === `/kits${type ? `?type=${type}` : ""}`
-  ? ""
-  : `/kits${type ? `?type=${type}` : ""}`
+  const componentsLink =
+    currentURL === `/kits${type ? `?type=${type}` : ""}`
+      ? ""
+      : `/kits${type ? `?type=${type}` : ""}`;
 
   //set up toggling for top level item
-  const handleComponentsClick = () => {
+  const handleComponentsClick = (item) => {
+    setIsActive(() => {
+      const newIsActive = {};
+      newIsActive[item] = true;
+      return newIsActive;
+    });
+
     //return true at end to disable default collapsible behavior
     return true;
   };
+
+  useEffect(() => {
+    console.log(isActive);
+  }, [isActive]);
 
   //right icon click for top level item
   const handleComponentsIconClick = () => {
@@ -30,11 +41,18 @@ const MainSidebar = ({ dark, type, category, kit, kits }) => {
       : setIsTopLevelCollapsed(true);
   };
 
+  const activeTopLevel = () => {
+    return isActive["top-nav-item"]
+      ? true
+      : Object.keys(isActive).length === 0
+      ? currentURL === `/kits${type ? `?type=${type}` : ""}`
+      : false;
+  };
 
   return (
     <Nav dark={dark} variant="bold" paddingTop="xxs">
       <NavItem
-        // active={currentURL === `/kits${type ? `?type=${type}` : ""}`}
+        active={activeTopLevel()}
         collapsed={isTopLevelCollapsed}
         collapsible
         collapsibleTrail
@@ -46,12 +64,24 @@ const MainSidebar = ({ dark, type, category, kit, kits }) => {
         key="top-nav-item"
         link={componentsLink}
         marginY="none"
-        onClick={handleComponentsClick}
+        onClick={() => handleComponentsClick("top-nav-item")}
         onIconRightClick={handleComponentsIconClick}
         paddingY="xxs"
         text="Components"
       >
-        {kits.map((link, i) => renderNavItem(link, i, collapsibles, category, type, dark, kit, isActive, setIsActive))}
+        {kits.map((link, i) =>
+          renderNavItem(
+            link,
+            i,
+            collapsibles,
+            category,
+            type,
+            dark,
+            kit,
+            isActive,
+            setIsActive
+          )
+        )}
       </NavItem>
     </Nav>
   );
