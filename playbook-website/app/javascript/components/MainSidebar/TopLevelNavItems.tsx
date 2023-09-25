4ac0d1f015dd99e7a300de88973c8e09b1499385
nida.ghuman <nidaqg@gmail.com>
2023-09-25 14:38:09 -0400

    fix for right icon bug for top level nav items

--- playbook-website/app/javascript/components/MainSidebar/TopLevelNavItems.tsx
+++ playbook-website/app/javascript/components/MainSidebar/TopLevelNavItems.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { NavItem, useCollapsible } from "playbook-ui";
 import { renderNavItem, kitsType } from "./KitsNavItems";
 import { SideBarNavItems } from "./MenuData/SidebarNavItems";
@@ -15,7 +15,7 @@ export const renderTopLevelNavItem = (
   kit,
   category,
   collapsibles,
-  samples, 
+  samples
 ) => {
   //hook into collapsible logic for top level item
   const topLevelCollapsibles = SideBarNavItems.map(() => useCollapsible());
@@ -49,15 +49,6 @@ export const renderTopLevelNavItem = (
     return true;
   };
 
-  //right icon click for top level item
-  const handleComponentsIconClick = (i) => {
-    topLevelCollapsibles.forEach(([, toggle], idx) => {
-      if (idx === i) {
-        toggle();
-      }
-    });
-  };
-
   //NOTE: All toggle and active state logic should be replaced with state once website moves to react router
   const activeTopLevel = (key, link) => {
     const kitsLink =
@@ -71,73 +62,93 @@ export const renderTopLevelNavItem = (
       : false;
   };
 
-  //collapsed logic for toplevel nav items
-  const toggleTopLevelNavItem = (key, link, collapsed) => {
-    // if url starts with /visual_guidelines, then relevant collapsible nav to be toggled open on first render
-    const currentPage = currentURL.match(/^(\/[^/]+)\/[^/]+/);
-    // if url starts with /kit, then relevant collapsible nav to be toggled open on first render
-    const kitCategoryPage = currentURL.match(/^\/([^/]{3})/);
-    // if url matches /guides, than relevant collapsible nav to ne toggled open on first render
-    const guidesPage = currentURL.split('/').slice(0, 3).join('/');
-    //NOTE: All toggle and active state logic should be replaced with state once website moves to react router
-    const match =
+  // if url starts with /visual_guidelines, then relevant collapsible nav to be toggled open on first render
+  const currentPage = currentURL.match(/^(\/[^/]+)\/[^/]+/);
+  // if url starts with /kit, then relevant collapsible nav to be toggled open on first render
+  const kitCategoryPage = currentURL.match(/^\/([^/]{3})/);
+  // if url matches /guides, than relevant collapsible nav to be toggled open on first render
+  const guidesPage = currentURL.split("/").slice(0, 3).join("/");
+
+  //extract render logic out of return for better performance
+  const renderTopItems = (name, key, children, leftIcon, link, i) => {
+    const [collapsed] = topLevelCollapsibles[i];
+    //use state to hanlde toggle logic to make sure both main click and right icon click works as expected
+    const [toggleWithIcon, setToggleWithIcon] = useState(false);
+
+    const categoryMatch =
       currentPage &&
       (currentPage[1] === link ||
         (kitCategoryPage &&
           `/${kitCategoryPage[1]}` === link.substring(0, link.length - 1)));
-    return activeTopLevel(key, link) || match || (guidesPage === link) ? false : collapsed;
+
+    const guidesMatch = guidesPage === link;
+
+    useEffect(() => {
+      setToggleWithIcon(categoryMatch || guidesMatch ? false : collapsed);
+    }, [collapsed]);
+
+    //right icon click for top level item
+    const handleComponentsIconClick = (i) => {
+      topLevelCollapsibles.forEach(([, toggle], idx) => {
+        if (idx === i) {
+          toggleWithIcon === true
+            ? setToggleWithIcon(false)
+            : setToggleWithIcon(true);
+        }
+      });
+    };
+
+    return (
+      <NavItem
+        active={activeTopLevel(key, link)}
+        collapsed={children && toggleWithIcon}
+        collapsible={children}
+        collapsibleTrail={children}
+        cursor="pointer"
+        dark={dark}
+        fontSize="small"
+        fontWeight="bolder"
+        iconRight={children && ["plus", "minus"]}
+        key={key}
+        iconLeft={leftIcon}
+        link={TopLevelLink(link)}
+        marginY="none"
+        onClick={() => handleComponentsClick(key, i)}
+        onIconRightClick={children && (() => handleComponentsIconClick(i))}
+        paddingY="xxs"
+        text={name}
+      >
+        {children && (
+          <>
+            {name === "Components" ? (
+              <>
+                {kits.map((link, i) =>
+                  renderNavItem(
+                    link,
+                    i,
+                    collapsibles,
+                    category,
+                    type,
+                    dark,
+                    kit,
+                    isActive,
+                    setIsActive
+                  )
+                )}
+              </>
+            ) : (
+              renderOtherNavItems(name, currentURL, dark, samples)
+            )}
+          </>
+        )}
+      </NavItem>
+    );
   };
 
   return (
     <>
       {SideBarNavItems.map(({ name, key, children, leftIcon, link }, i) => {
-        const [collapsed] = topLevelCollapsibles[i];
-
-        return (
-          <NavItem
-            active={activeTopLevel(key, link)}
-            collapsed={children && toggleTopLevelNavItem(key, link, collapsed)}
-            collapsible={children}
-            collapsibleTrail={children}
-            cursor="pointer"
-            dark={dark}
-            fontSize="small"
-            fontWeight="bolder"
-            iconRight={children && ["plus", "minus"]}
-            key={key}
-            iconLeft={leftIcon}
-            link={TopLevelLink(link)}
-            marginY="none"
-            onClick={() => handleComponentsClick(key, i)}
-            onIconRightClick={children && (() => handleComponentsIconClick(i))}
-            paddingY="xxs"
-            text={name}
-          >
-            {children && (
-              <>
-                {name === "Components" ? (
-                  <>
-                    {kits.map((link, i) =>
-                      renderNavItem(
-                        link,
-                        i,
-                        collapsibles,
-                        category,
-                        type,
-                        dark,
-                        kit,
-                        isActive,
-                        setIsActive
-                      )
-                    )}
-                  </>
-                ) : (
-                  renderOtherNavItems(name, currentURL, dark, samples)
-                )}
-              </>
-            )}
-          </NavItem>
-        );
+        return renderTopItems(name, key, children, leftIcon, link, i);
       })}
     </>
   );
