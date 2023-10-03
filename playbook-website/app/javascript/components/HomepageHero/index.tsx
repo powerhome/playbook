c405143262ad5e4d8763bd047810f36cd6289bbe
nida.ghuman <nidaqg@gmail.com>
2023-10-03 19:44:58 -0400

    scaffold react component for homepage hero

---
+++ playbook-website/app/javascript/components/HomepageHero/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {Title, Caption, Background} from "playbook-ui";
+
+
+const HomepageHero = () => {
+    return (
+        <>
+        <Caption text="Welcome to Playbook" color="link" />
+            <Title text="The Design System to help you ship faster."/>
+        </>
+    )
+}
+
+export default HomepageHero
