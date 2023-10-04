383edd73e6509b5c6d158deb2969baf187f61205
nida.ghuman <nidaqg@gmail.com>
2023-10-03 22:26:09 -0400

    testing components on page

--- playbook-website/app/javascript/components/HomepageHero/index.tsx
+++ playbook-website/app/javascript/components/HomepageHero/index.tsx
@@ -1,9 +1,16 @@
 import React from "react";
-import {
-  Background,
-} from "playbook-ui";
+import { Background } from "playbook-ui";
 import WelcomeComponent from "./HeroComponents/Welcome";
-
+import ConnectedDevicesCard from "./HeroComponents/ConnectedDevices";
+import HoverCard from "./HeroComponents/Hover";
+import LettuceCheckboxCard from "./HeroComponents/LettuceCheckbox";
+import NewsNavCard from "./HeroComponents/NewsNav";
+import NotificationsLargeCard from "./HeroComponents/NotificationsLarge";
+import NotificationsSmallCard from "./HeroComponents/NotificationsSmall";
+import OrderDetailsCard from "./HeroComponents/OrderDetals";
+import SelectPlanCard from "./HeroComponents/SelectPlan";
+import SubscribeToggleCard from "./HeroComponents/SubscribeToggle";
+import TicketsChartCard from "./HeroComponents/TicketsChart";
 
 const HomepageHero = () => {
   return (
@@ -14,10 +21,18 @@ const HomepageHero = () => {
         paddingTop="xl"
         backgroundColor="white"
       >
-        <WelcomeComponent/>
-        <Background maxWidth="xl">
-          
-
+        <WelcomeComponent />
+        <Background backgroundColor="white" maxWidth="xl">
+          <ConnectedDevicesCard />
+          <HoverCard />
+          <LettuceCheckboxCard />
+          <NewsNavCard />
+          <NotificationsLargeCard />
+          <NotificationsSmallCard />
+          <OrderDetailsCard />
+          <SelectPlanCard />
+          <SubscribeToggleCard />
+          <TicketsChartCard />
         </Background>
       </Background>
     </>
