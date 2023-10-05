dc1201ddfc0fc4e3d83513aef32daff52edcc49e
nida.ghuman <nidaqg@gmail.com>
2023-10-05 09:43:00 -0400

    Added underline to ship faster

--- playbook-website/app/javascript/components/HomepageHero/HeroComponents/Welcome.tsx
+++ playbook-website/app/javascript/components/HomepageHero/HeroComponents/Welcome.tsx
@@ -1,13 +1,7 @@
 import React from "react";
-import {
-  Title,
-  Body,
-  Flex,
-  FlexItem,
-  Image,
-  Button,
-} from "playbook-ui";
+import { Title, Body, Flex, FlexItem, Image, Button } from "playbook-ui";
 import { Logos } from "../ComponentData/LogosData";
+import Underline from "../../../images/PurpleUnderline.svg";
 
 const WelcomeComponent = () => {
   return (
@@ -15,11 +9,18 @@ const WelcomeComponent = () => {
       <Flex className="welcome_component">
         <FlexItem fixedSize="546px">
           <Title text="WELCOME TO PLAYBOOK" size={4} color="link" />
-          <Title
-            size={1}
-            text="The Design System to help you ship faster."
-            paddingBottom="sm"
-          />
+          <Title size={1} paddingBottom="sm" position="relative">
+            {" "}
+            The Design System to help you{" "}
+            <span>
+              ship faster.
+              <Image
+                position="absolute"
+                className="welcome_component_underline"
+                url={Underline}
+              />
+            </span>
+          </Title>
           <Body
             color="light"
             text="Playbook makes it easy to support bleeding edge, or legacy systems. Use Playbook’s 200+ components and end-to-end design language to create simple, intuitive and beautiful experiences with ease."
