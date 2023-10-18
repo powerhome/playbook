205532078cb366a4932150b7452c07710fe4736b
Jasper Furniss <jasperfurniss@gmail.com>
2023-10-18 10:50:20 -0400

    Revert "[PLAY-946] React Scaffold (#2775)"
    This reverts commit cd14f9563d5f43ff9a324e8de0fd575de786a3ed.

--- playbook-website/app/controllers/pages_controller.rb
+++ playbook-website/app/controllers/pages_controller.rb
@@ -12,16 +12,6 @@ class PagesController < ApplicationController
   before_action :delete_dark_mode_cookie, only: %i[home getting_started visual_guidelines]
   before_action :set_show_sidebar, only: %i[kits kit_category_show_rails kit_category_show_react kit_show_react kit_show_rails rails_in_react kit_show_demo kit_show_new visual_guidelines kit_show_swift home]
 
-  def react_app
-    @kit = params[:name]
-    @kits = MENU["kits"]
-    @dark = cookies[:dark_mode] == "true"
-    respond_to do |format|
-      format.html { render template: "react_app", layout: false }
-      format.json { render json: { kits: @kits, kit: @kit, dark: @dark } }
-    end
-  end
-
   def disable_dark_mode
     cookies[:dark_mode] = {
       value: "false",
