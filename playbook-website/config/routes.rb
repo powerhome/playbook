4d487e73a319c337c0221c833153577090692549
Jasper <jasperfurniss@gmail.com>
2023-10-18 12:47:45 -0400

    Fixed Bug that Needed Reverting

--- playbook-website/config/routes.rb
+++ playbook-website/config/routes.rb
@@ -10,11 +10,11 @@ Rails.application.routes.draw do
   # Kits
   get "beta/kits", to: "pages#react_app"
   get "kits", to: "pages#kits"
-  get "kits/:name",                 to: "pages#kit_show_rails", as: "kit_show"
+  get "kits/:name",                 to: "pages#kit_show_rails",           as: "kit_show"
   get "kits/:name/rails",           to: "pages#kit_show_rails",           as: "kit_show_rails"
   get "kits/:name/react",           to: "pages#kit_show_react",           as: "kit_show_reacts"
   get "kits/:name/swift",           to: "pages#kit_show_swift",           as: "kit_show_swift"
-  get "kit_category/:name",         to: "pages#react_app"
+  get "kit_category/:name",         to: "pages#kit_category_show_rails",  as: "kit_category_show"
   get "kit_category/:name/rails",   to: "pages#kit_category_show_rails",  as: "kit_category_show_rails"
   get "kit_category/:name/react",   to: "pages#kit_category_show_react",  as: "kit_category_show_reacts"
   get "kit_collection/*names/:name/rails",    to: "pages#kit_collection_show_rails",                        as: "kit_collection_show_rails"
