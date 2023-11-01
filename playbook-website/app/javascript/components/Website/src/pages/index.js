4501b7df57b218e7dceeb04830cdc1c2c4747840
Jasper <jasperfurniss@gmail.com>
2023-11-01 19:47:25 -0400

    Fixed Bad Imports

--- playbook-website/app/javascript/components/Website/src/pages/index.js
+++ playbook-website/app/javascript/components/Website/src/pages/index.js
@@ -1,2 +1,3 @@
 export { default as ComponentsList } from './ComponentsList'
 export { default as Component } from './Component'
+export { default as Scaffold } from './Scaffold'
--- playbook-website/app/javascript/packs/app.js
+++ playbook-website/app/javascript/packs/app.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
-import App from '../components/Website/App'
+import App from '../components/Website'
 import '@fortawesome/fontawesome-pro/js/fontawesome.min.js'
 import '@fortawesome/fontawesome-pro/js/regular.min.js'
 import '../site_styles/main.scss'
@@ -14,9 +14,8 @@ import {
   Route,
   RouterProvider,
 } from 'react-router-dom'
-import Scaffold from '../components/Scaffold'
 import RedirectToType from '../components/RedirectToType'
-import { Component, ComponentsList } from 'components/Website/src/pages'
+import { Component, ComponentsList, Scaffold } from '../components/Website/src/pages'
 
 const rootElement = document.getElementById('react-root')
 
