4280c6213a7df8494e43fed97ab6458eda47a4eb
Mark <mark.rosenberg@powerhrg.com>
2023-11-13 11:25:35 -0500

    Remove Glob code

--- playbook/lib/playbook/pb_doc_helper.rb
+++ playbook/lib/playbook/pb_doc_helper.rb
@@ -33,9 +33,9 @@ module Playbook
       kits.each do |kit|
         if kit.is_a?(Hash)
           nav_hash_array(kit).each do |sub_kit|
-            display_kits << render_pb_doc_kit(sub_kit, type, limit_examples, false, dark_mode) if pb_doc_has_kit_type?(sub_kit, type)
+            display_kits << render_pb_doc_kit(sub_kit, type, limit_examples, false, dark_mode)
           end
-        elsif pb_doc_has_kit_type?(kit, type)
+        else
           display_kits << render_pb_doc_kit(kit, type, limit_examples, false, dark_mode)
         end
       end
@@ -90,25 +90,6 @@ module Playbook
       else
         []
       end
-    end
-
-    def pb_doc_has_kit_type?(kit, type = "rails")
-      case type
-      when "rails"
-        extension = "erb"
-        query_string = extension
-      when "react"
-        extension = "jsx"
-        query_string = extension
-      when "swift"
-        extension = "md"
-        query_string = "_swift"
-      end
-
-      pb_doc_kit_path(kit, "docs")
-        .glob("**/*.#{extension}")
-        .any? { |path| path.basename.to_s.include?(query_string) }
-        .present?
     end
 
     def pb_doc_render_clickable_title(kit, type)
