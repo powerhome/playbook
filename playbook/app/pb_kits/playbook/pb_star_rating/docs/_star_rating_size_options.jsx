bc72003548bbe2ea922930bb12d6e0d9dd5377c4
Mark <mark.rosenberg@powerhrg.com>
2023-11-16 15:58:47 -0500

    Fix sizing

---
+++ playbook/app/pb_kits/playbook/pb_star_rating/docs/_star_rating_size_options.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+
+import StarRating from '../_star_rating'
+
+const StarRatingSizeOptions = (props) => (
+  <>
+
+    <StarRating
+        rating={3}
+        size="xs"
+        {...props}
+    />
+
+    <br />
+
+    <StarRating
+        rating={3}
+        size="sm"
+        {...props}
+    />
+
+    <br />
+
+    <StarRating
+        rating={3}
+        size="md"
+        {...props}
+    />
+
+    <br />
+
+    <StarRating
+        rating={3}
+        size="lg"
+        {...props}
+    />
+
+    <br />
+
+    <StarRating
+        layoutOption="number"
+        rating={3}
+        size="lg"
+        {...props}
+    />
+
+    <br />
+
+    <StarRating
+        layoutOption="onestar"
+        rating={3}
+        size="lg"
+        {...props}
+    />
+
+  </>
+)
+
+export default StarRatingSizeOptions
--- playbook/app/pb_kits/playbook/pb_star_rating/docs/index.js
+++ playbook/app/pb_kits/playbook/pb_star_rating/docs/index.js
@@ -5,3 +5,5 @@ export { default as StarRatingColorOptions } from './_star_rating_color_options.
 export { default as StarRatingHide } from './_star_rating_hide.jsx'
 
 export { default as StarRatingNumberConfig } from './_star_rating_number_config.jsx'
+
+export { default as StarRatingSizeOptions } from './_star_rating_size_options.jsx'
