d8f3dee1f7edac68ee8db0a5e172550e5f9a4dd5
Augusto Mallmann <augustocmallmann@gmail.com>
2023-12-07 14:29:09 -0300

    feat: sort data

--- playbook-website/app/javascript/components/Website/src/hooks/loaders.ts
+++ playbook-website/app/javascript/components/Website/src/hooks/loaders.ts
@@ -1,12 +1,44 @@
-export const ComponentsLoader = async () => {
-  const response = await fetch('/beta/kits.json')
-  const data = await response.json()
-  return data
+import { LoaderFunctionArgs } from "react-router-dom";
+
+interface ComponentTypes {
+  name: string;
+  platforms: string[];
+  description: string;
 }
 
-export const CategoryLoader = async ({params}) => {
-  const response = await fetch('/beta/kits.json')
-  const { kits } = await response.json()
-
-  return kits.filter(kit => kit.name === params.name)[0] 
+interface CategpryTypes {
+  name: string;
+  description: string;
+  components: ComponentTypes[];
 }
+
+const sortByName = (a: ComponentTypes, b: ComponentTypes) =>
+  a.name.localeCompare(b.name);
+
+export const ComponentsLoader: () => Promise<CategpryTypes[]> = async () => {
+  const response = await fetch("/beta/kits.json");
+  const data = await response.json();
+
+  const sortComponentsByName = (kitCategory: CategpryTypes) => {
+    kitCategory.components.sort(sortByName);
+  };
+
+  data.kits.sort(sortByName).forEach(sortComponentsByName);
+
+  return data;
+};
+
+export const CategoryLoader: (
+  props: LoaderFunctionArgs
+) => Promise<ComponentTypes> = async ({ params }) => {
+  const response = await fetch("/beta/kits.json");
+  const { kits } = await response.json();
+
+  const filteredData = kits.filter(
+    (kit: ComponentTypes) => kit.name === params.name
+  )[0];
+
+  filteredData.components.sort(sortByName);
+
+  return filteredData;
+};
