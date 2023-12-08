544cd6966e2cf137378306cda66159d7403db423
Augusto Mallmann <augustocmallmann@gmail.com>
2023-12-08 14:02:40 -0300

    refactor: category title

--- playbook-website/app/javascript/components/Website/src/pages/ComponentList.tsx
+++ playbook-website/app/javascript/components/Website/src/pages/ComponentList.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { NavLink, Outlet, useLoaderData, useOutlet } from "react-router-dom";
-import { Title } from "playbook-ui";
 
-import { linkFormat } from "../../../../utilities/website_sidebar_helper";
 import { KitCard } from "../components/KitCard";
 import { Hero } from "../components/Hero";
 import { KitGrid } from "../components/KitGrid";
 import { PageContainer } from "../components/PageContainer";
+import { CategoryTitle } from "../components/CategoryTitle";
 
 const description =
   "Components are the reusable building blocks of our design system. Each component meets a specific interaction or UI need, and has been specifically created to work together to create patterns and intuitive user experiences.";
@@ -28,12 +27,7 @@ export default function ComponentList() {
                 id={name}
               >
                 <NavLink to={`/beta/kit_category/${name}`}>
-                  <Title
-                    marginBottom="md"
-                    size={2}
-                    tag="h1"
-                    text={linkFormat(name)}
-                  />
+                  <CategoryTitle name={name} />
                 </NavLink>
                 <KitGrid>
                   {components.map(({ name, description }, index) => (
