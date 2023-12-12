ed9e12e2b3d3d3049fa62d64ce3df1410ae5d4e2
Augusto Mallmann <augustocmallmann@gmail.com>
2023-12-12 16:40:02 -0300

    wip

--- playbook/app/pb_kits/playbook/pb_dialog/child_kits/_dialog_footer.tsx
+++ playbook/app/pb_kits/playbook/pb_dialog/child_kits/_dialog_footer.tsx
@@ -38,7 +38,7 @@ const DialogFooter = (props: DialogFooterProps): React.ReactElement => {
 
   return (
     <div 
-      {...htmlProps}
+        {...htmlProps}
     >
       {separator &&
         <SectionSeparator />
