0aa2417ad640ba2af6e17930570eb9fb5bd85fcd
Mark <mark.rosenberg@powerhrg.com>
2024-01-10 10:07:34 -0500

    Fix React

--- playbook/app/pb_kits/playbook/pb_icon/docs/_icon_default.jsx
+++ playbook/app/pb_kits/playbook/pb_icon/docs/_icon_default.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
 import BetaIcon from '../../pb_beta_icon/_beta_icon'
+import Icon from '../_icon'
+
 
 const IconDefault = (props) => {
   return (
     <div>
+      <Icon
+          fixedWidth
+          icon="user"
+          {...props}
+      />
       <BetaIcon
+          fixedWidth
+          icon="pen"
           {...props}
       />
     </div>
