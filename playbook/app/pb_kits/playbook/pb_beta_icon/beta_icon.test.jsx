3f14e90af8ec830f3ae99094597fe75ad19b5bd9
Mark <mark.rosenberg@powerhrg.com>
2024-01-10 15:43:02 -0500

    Add tests

--- playbook/app/pb_kits/playbook/pb_beta_icon/beta_icon.test.jsx
+++ playbook/app/pb_kits/playbook/pb_beta_icon/beta_icon.test.jsx
@@ -1,17 +1,155 @@
-import { renderKit } from '../utilities/test-utils'
+import React from 'react'
+import { render, screen, cleanup } from '../utilities/test-utils'
 
-import { BetaIcon } from '../'
+import BetaIcon from './_beta_icon'
 
-/* See these resources for more testing info:
-  - https://github.com/testing-library/jest-dom#usage for useage and examples
-  - https://jestjs.io/docs/en/using-matchers
-*/
+const testId = "icon-kit"
 
-test('generated scaffold test - update me', () => {
-  const props = {
-    data: { testid: 'default' }
-  }
+describe("Icon Kit", () => {
+    test("renders Icon classname", () => {
+        render(
+            <BetaIcon
+                data={{ testid: testId }}
+                fixedWidth
+                icon="user"
+      />
+        )
 
-  const kit = renderKit(BetaIcon , props)
-  expect(kit).toBeInTheDocument()
+        const kit = screen.getByTestId(testId)
+        expect(kit).toHaveClass("fa-user pb_icon_kit fa-fw far")
+    })
+
+    test("renders rotate prop", () => {[
+        "90", "180", "270"].forEach((rotateProp)=> {
+        render(
+            <BetaIcon
+                data={{ testid: testId }}
+                fixedWidth
+                icon="user"
+                rotation={rotateProp}
+      />
+        )
+
+        const kit = screen.getByTestId(testId)
+        expect(kit).toHaveClass(`fa-user pb_icon_kit fa-fw fa-rotate-${rotateProp} far`)
+
+        cleanup()
+    })
+})
+
+    test("renders flip prop", () => {
+        render(
+            <BetaIcon
+                data={{ testid: testId }}
+                fixedWidth
+                flip="horizontal"
+                icon="user"
+      />
+        )
+
+        const kit = screen.getByTestId(testId)
+        expect(kit).toHaveClass("fa-user pb_icon_kit fa-fw fa-flip-horizontal far")
+    })
+
+
+    test("renders spinning icon", () => {
+        render(
+            <BetaIcon
+                data={{ testid: testId }}
+                fixedWidth
+                icon="spinner"
+                spin
+      />
+        )
+
+        const kit = screen.getByTestId(testId)
+        expect(kit).toHaveClass("fa-spinner pb_icon_kit fa-fw fa-spin far")
+    })
+
+    test("renders pull icon", () => {
+        render(
+            <BetaIcon
+                data={{ testid: testId }}
+                fixedWidth
+                icon="arrow-left"
+                pull="left"
+      />
+        )
+
+        const kit = screen.getByTestId(testId)
+        expect(kit).toHaveClass("fa-arrow-left pb_icon_kit fa-fw fa-pull-left far")
+    })
+
+    test("renders pull icon", () => {
+        render(
+            <BetaIcon
+                data={{ testid: testId }}
+                fixedWidth
+                icon="arrow-left"
+                pull="left"
+      />
+        )
+
+        const kit = screen.getByTestId(testId)
+        expect(kit).toHaveClass("fa-arrow-left pb_icon_kit fa-fw fa-pull-left far")
+    })
+
+    test("renders border around icon", () => {
+        render(
+            <BetaIcon
+                border
+                data={{ testid: testId }}
+                fixedWidth
+                icon="user"
+      />
+        )
+
+        const kit = screen.getByTestId(testId)
+        expect(kit).toHaveClass("fa-user pb_icon_kit fa-border fa-fw far")
+    })
+
+    test("renders size prop", () => {
+        ["lg",
+        "sm",
+        "xs",
+        "1x",
+        "2x",
+        "3x",
+        "4x",
+        "5x",
+        "6x",
+        "7x",
+        "8x",
+        "9x",
+        "10x"].forEach(
+            (sizeProp) => {
+            render(
+                <BetaIcon
+                    data={{ testid: testId }}
+                    icon="user"
+                    size={sizeProp}
+          />
+            )
+
+            const kit = screen.getByTestId(testId)
+            expect(kit).toHaveClass(`pb_icon_kit fa-user fa-fw fa-${sizeProp} far`)
+
+            cleanup()
+        })
+    })
+
+    test("renders fontStyle prop", () => {
+        render(
+            <BetaIcon
+                data={{ testid: testId }}
+                fixedWidth
+                fontStyle="fas"
+                icon="user"
+      />
+        )
+
+        const kit = screen.getByTestId(testId)
+        expect(kit).toHaveClass("fa-user pb_icon_kit fa-fw fas")
+    })
+
 })
--- playbook/spec/pb_kits/playbook/kits/beta_icon_spec.rb
+++ playbook/spec/pb_kits/playbook/kits/beta_icon_spec.rb
@@ -2,10 +2,75 @@
 
 require_relative "../../../../app/pb_kits/playbook/pb_beta_icon/beta_icon"
 
-RSpec.describe Playbook::PbBeta_icon::Beta_icon do
-  subject { Playbook::PbBeta_icon::Beta_icon }
+RSpec.describe Playbook::PbBetaIcon::BetaIcon do
+  subject { Playbook::PbBetaIcon::BetaIcon }
 
-  it { is_expected.to define_partial }
+  it {
+    is_expected.to define_prop(:border)
+      .of_type(Playbook::Props::Boolean)
+  }
+  it {
+    is_expected.to define_prop(:fixed_width)
+      .of_type(Playbook::Props::Boolean)
+  }
+  it {
+    is_expected.to define_enum_prop(:flip)
+      .with_values("horizontal", "vertical", "both", nil)
+  }
+  it { is_expected.to define_prop(:icon) }
+  it {
+    is_expected.to define_prop(:custom_icon)
+      .of_type(Playbook::Props::String)
+      .with_default(nil)
+  }
+  it {
+    is_expected.to define_prop(:inverse)
+      .of_type(Playbook::Props::Boolean)
+  }
+  it {
+    is_expected.to define_prop(:list_item)
+      .of_type(Playbook::Props::Boolean)
+  }
+  it {
+    is_expected.to define_enum_prop(:pull)
+      .with_values("left", "right", nil)
+  }
+  it {
+    is_expected.to define_prop(:pulse)
+      .of_type(Playbook::Props::Boolean)
+  }
+  it {
+    is_expected.to define_enum_prop(:rotation)
+      .with_values(90, 180, 270, nil)
+  }
+  it {
+    is_expected.to define_enum_prop(:size)
+      .with_values("lg", "xs", "sm", "1x", "2x", "3x", "4x", "5x", "6x", "7x", "8x", "9x", "10x", nil)
+  }
+  it {
+    is_expected.to define_prop(:spin)
+      .of_type(Playbook::Props::Boolean)
+  }
 
-  # Do not leave this file blank. Use other spec files for example tests.
+  describe "#classname" do
+    it "returns namespaced class name", :aggregate_failures do
+      icon = "user"
+      pull = "right"
+      rotation = 90
+      size = "sm"
+
+      expect(subject.new(icon: icon, border: true).classname).to eq "pb_icon_kit far fa-user fa-border"
+      expect(subject.new(icon: icon, fixed_width: true).classname).to eq "pb_icon_kit far fa-user fa-fw"
+      expect(subject.new(icon: icon, flip: "horizontal").classname).to eq "pb_icon_kit far fa-user fa-flip-horizontal"
+      expect(subject.new(icon: icon).classname).to eq "pb_icon_kit far fa-user"
+      expect(subject.new(icon: icon, inverse: true).classname).to eq "pb_icon_kit far fa-user fa-inverse"
+      expect(subject.new(icon: icon, list_item: true).classname).to eq "pb_icon_kit far fa-user fa-li"
+      expect(subject.new(icon: icon, pull: pull).classname).to eq "pb_icon_kit far fa-user fa-pull-#{pull}"
+      expect(subject.new(icon: icon, pulse: true).classname).to eq "pb_icon_kit far fa-user fa-pulse"
+      expect(subject.new(icon: icon, rotation: rotation).classname).to eq "pb_icon_kit far fa-user fa-rotate-#{rotation}"
+      expect(subject.new(icon: icon, size: size).classname).to eq "pb_icon_kit far fa-user fa-#{size}"
+      expect(subject.new(icon: icon, spin: true).classname).to eq "pb_icon_kit far fa-user fa-spin"
+      expect(subject.new(icon: icon, classname: "additional_class").classname).to eq "pb_icon_kit far fa-user additional_class"
+    end
+  end
 end
