855ad945bd14b388dbf14ed448113461e4441f92
nida.ghuman <nidaqg@gmail.com>
2024-01-31 15:33:43 -0500

    Docs for collapsible trail

--- playbook/app/pb_kits/playbook/pb_advanced_table/docs/index.js
+++ playbook/app/pb_kits/playbook/pb_advanced_table/docs/index.js
@@ -1,5 +1,6 @@
 export { default as AdvancedTableDefault } from './_advanced_table_default.jsx'
 export { default as AdvancedTableSort } from './_advanced_table_sort.jsx'
 export { default as AdvancedTableLoading } from './_advanced_table_loading.jsx'
+export { default as AdvancedTableCollapsibleTrail } from './_advanced_table_collapsible_trail.jsx'
 export { default as AdvancedTableTableOptions } from './_advanced_table_table_options.jsx'
 export { default as AdvancedTableTableProps } from './_advanced_table_table_props.jsx'
