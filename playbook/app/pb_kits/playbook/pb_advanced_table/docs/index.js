837a2e255c3b4fc697faab7364a19c972e8164ac
nida.ghuman <nidaqg@gmail.com>
2024-01-31 13:15:02 -0500

    Doc example for tableProps

--- playbook/app/pb_kits/playbook/pb_advanced_table/docs/index.js
+++ playbook/app/pb_kits/playbook/pb_advanced_table/docs/index.js
@@ -1,3 +1,4 @@
 export { default as AdvancedTableDefault } from './_advanced_table_default.jsx'
 export { default as AdvancedTableSort } from './_advanced_table_sort.jsx'
 export { default as AdvancedTableLoading } from './_advanced_table_loading.jsx'
+export { default as AdvancedTableTableProps } from './_advanced_table_table_props.jsx'
