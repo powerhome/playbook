c57a6df06e716afabcc5af42ac6432513ffd4435
nida.ghuman <nidaqg@gmail.com>
2024-01-31 13:45:09 -0500

    Doc example for tableOptions

---
+++ playbook/app/pb_kits/playbook/pb_advanced_table/docs/_advanced_table_table_options.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { AdvancedTable } from "../../";
+import { MOCK_DATA } from "./_mock_data";
+
+const AdvancedTableTableOptions = (props) => {
+  const columnDefinitions = [
+    {
+      accessor: "year",
+      label: "Year",
+      cellAccessors: ["quarter", "month", "day"],
+    },
+    {
+      accessor: "newEnrollments",
+      label: "New Enrollments",
+    },
+    {
+      accessor: "scheduledMeetings",
+      label: "Scheduled Meetings",
+    },
+    {
+      accessor: "attendanceRate",
+      label: "Attendance Rate",
+    },
+    {
+      accessor: "completedClasses",
+      label: "Completed Classes",
+    },
+    {
+      accessor: "classCompletionRate",
+      label: "Class Completion Rate",
+    },
+    {
+      accessor: "graduatedStudents",
+      label: "Graduated Students",
+    },
+  ];
+
+  const tableOptions = {
+    initialState: {
+        sorting: [
+          {
+            id: "year",
+            desc: true,
+          },
+        ],
+      },
+  }
+
+  return (
+    <div>
+      <AdvancedTable
+          columnDefinitions={columnDefinitions}
+          tableData={MOCK_DATA}
+          tableOptions={tableOptions}
+          {...props}
+      />
+    </div>
+  );
+};
+
+export default AdvancedTableTableOptions;
--- playbook/app/pb_kits/playbook/pb_advanced_table/docs/index.js
+++ playbook/app/pb_kits/playbook/pb_advanced_table/docs/index.js
@@ -1,4 +1,5 @@
 export { default as AdvancedTableDefault } from './_advanced_table_default.jsx'
 export { default as AdvancedTableSort } from './_advanced_table_sort.jsx'
 export { default as AdvancedTableLoading } from './_advanced_table_loading.jsx'
+export { default as AdvancedTableTableOptions } from './_advanced_table_table_options.jsx'
 export { default as AdvancedTableTableProps } from './_advanced_table_table_props.jsx'
