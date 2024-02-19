0589d87c9edf2b52cb5eabbb3d42f22f2e2d0ab0
nida.ghuman <nidaqg@gmail.com>
2024-02-19 15:05:06 -0500

    Jest test for inlineRowLoading prop

--- playbook/app/pb_kits/playbook/pb_advanced_table/advanced_table.test.jsx
+++ playbook/app/pb_kits/playbook/pb_advanced_table/advanced_table.test.jsx
@@ -42,6 +42,36 @@ const MOCK_DATA = [
   },
 ];
 
+const MOCK_DATA_LOADING = [
+  {
+    year: "2021",
+    quarter: null,
+    month: null,
+    day: null,
+    newEnrollments: "20",
+    scheduledMeetings: "10",
+    children: [],
+  },
+  {
+    year: "2022",
+    quarter: null,
+    month: null,
+    day: null,
+    newEnrollments: "20",
+    scheduledMeetings: "10",
+    children: [
+      {
+        year: "2022",
+        quarter: "Q1",
+        month: null,
+        day: null,
+        newEnrollments: "2",
+        scheduledMeetings: "35",
+      },
+    ],
+  },
+];
+
 const columnDefinitions = [
   {
     accessor: "year",
@@ -378,4 +408,22 @@ test("Generates Table.Body default + custom classname", () => {
   const kit = screen.getByTestId(testId);
   const tableHeader = kit.querySelector('tbody')
   expect(tableHeader).toHaveClass('pb_advanced_table_body custom-body-classname')
+});
+
+test("inlineRowLoading prop renders inline loading if true", () => {
+  render(
+    <AdvancedTable
+        columnDefinitions={columnDefinitions}
+        data={{ testid: testId }}
+        inlineRowLoading
+        tableData={MOCK_DATA_LOADING}
+    />
+  );
+
+  const kit = screen.getByTestId(testId);
+  const rowButton = kit.querySelector(".gray-icon.expand-toggle-icon")
+  expect(rowButton).toBeInTheDocument()
+  rowButton.click()
+  const inlineLoading = kit.querySelector(".fa-spinner")
+  expect(inlineLoading).toBeInTheDocument()
 });
