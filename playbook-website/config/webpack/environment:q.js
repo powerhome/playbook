fe8e76c284d037d56c55b74c9589c02212e27ddd
Mark <mark@does.work>
2024-02-27 18:30:17 -0500

    what is my diff

---
+++ playbook-website/config/webpack/environment:q.js
@@ -0,0 +1,38 @@
+const path = require('path')
+
+const { environment } = require('@rails/webpacker')
+
+environment.loaders.insert('javascript', {
+  test: /\.(js|jsx)$/,
+  use: {
+    loader: 'babel-loader',
+    options: {
+      cacheDirectory: true,
+    },
+  },
+  include: path.resolve(__dirname, '../../app'),
+})
+
+// Allow ESM modules
+environment.config.merge({
+  module: {
+    rules: [
+      {
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: 'javascript/auto',
+      },
+    ],
+  },
+})
+
+environment.config.merge({
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: { chunks: 'all' },
+    removeAvailableModules: false,
+    removeEmptyChunks: true,
+  },
+})
+
+module.exports = environment
