7c10ab3a97160cb8a2f8fc186b896d3763931763
Carlos Lima <carloslimasd@gmail.com>
2024-02-28 07:49:03 -0300

    Adding div support for Table

---
+++ playbook/app/pb_kits/playbook/pb_table/docs/_table_div.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import Table from '../_table'
+
+const TableDiv = (props) => {
+  return (
+    <Table
+        size="sm"
+        striped
+        {...props}
+    >
+      <div className="thead">
+        <div className="tr">
+          <div className="th">{'Column 1'}</div>
+          <div className="th">{'Column 2'}</div>
+          <div className="th">{'Column 3'}</div>
+          <div className="th">{'Column 4'}</div>
+          <div className="th">{'Column 5'}</div>
+        </div>
+      </div>
+      <div className="tbody">
+        <div className="tr">
+          <div className="td">{'Value 1'}</div>
+          <div className="td">{'Value 2'}</div>
+          <div className="td">{'Value 3'}</div>
+          <div className="td">{'Value 4'}</div>
+          <div className="td">{'Value 5'}</div>
+        </div>
+        <div className="tr">
+          <div className="td">{'Value 1'}</div>
+          <div className="td">{'Value 2'}</div>
+          <div className="td">{'Value 3'}</div>
+          <div className="td">{'Value 4'}</div>
+          <div className="td">{'Value 5'}</div>
+        </div>
+        <div className="tr">
+          <div className="td">{'Value 1'}</div>
+          <div className="td">{'Value 2'}</div>
+          <div className="td">{'Value 3'}</div>
+          <div className="td">{'Value 4'}</div>
+          <div className="td">{'Value 5'}</div>
+        </div>
+      </div>
+    </Table>
+  )
+}
+
+export default TableDiv
