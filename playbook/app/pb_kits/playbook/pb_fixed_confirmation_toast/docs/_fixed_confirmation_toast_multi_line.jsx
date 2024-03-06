f4d629b94cc5725c3c9702a507c877ae9e051516
Mark <mark@does.work>
2024-03-06 12:10:50 -0500

    Add rails example

--- playbook/app/pb_kits/playbook/pb_fixed_confirmation_toast/docs/_fixed_confirmation_toast_multi_line.jsx
+++ playbook/app/pb_kits/playbook/pb_fixed_confirmation_toast/docs/_fixed_confirmation_toast_multi_line.jsx
@@ -1,18 +1,52 @@
-import React from 'react'
 
+import React, { useState } from 'react'
+
+import Button from '../../pb_button/_button'
 import FixedConfirmationToast from '../_fixed_confirmation_toast'
 
-const FixedConfirmationToastMultiLine = (props) => {
+const FixedConfirmationToastPositions = (props) => {
+  const [state, setState] = useState({
+    open: false,
+    vertical: 'top',
+    horizontal: 'center',
+  })
+
+  const { vertical, horizontal, open } = state
+
+  const handleClick = (newState) => () => {
+    setState({ open: true, ...newState })
+  }
+
+  const handleClose = () => {
+    setState({ ...state, open: false })
+  }
+
   return (
     <div>
+      <Button
+          onClick={handleClick({
+            horizontal: 'center',
+            open: true,
+            vertical: 'top',
+          })}
+          text="Multiline Example"
+          variant="primary"
+          {...props}
+      />
+      {' '}
       <FixedConfirmationToast
+          closeable
+          horizontal={horizontal}
           multiLine
+          onClose={handleClose}
+          open={open}
           status="tip"
-          text="Scan to Assign Selected Items. Click here to generate report"
+          text={`Multi-line is used when the given text will not fit on one line. Using Multi Line allows the height of the confirmation toast to grow. Simply resize the screen to see the fixed confirmation toast wrap the text.`}
+          vertical={vertical}
           {...props}
       />
     </div>
   )
 }
 
-export default FixedConfirmationToastMultiLine
+export default FixedConfirmationToastPositions
