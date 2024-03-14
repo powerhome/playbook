40e6e55e11c38ab9df65b2ba56b569e5da07edbd
nida.ghuman <nidaqg@gmail.com>
2024-03-14 15:44:18 -0400

    basic table with table_data and column_definitions

--- playbook/app/pb_kits/playbook/pb_advanced_table/advanced_table.rb
+++ playbook/app/pb_kits/playbook/pb_advanced_table/advanced_table.rb
@@ -3,6 +3,11 @@
 module Playbook
   module PbAdvancedTable
     class AdvancedTable < Playbook::KitBase
+      prop :table_data, type: Playbook::Props::Array,
+                        default: []
+      prop :column_definitions, type: Playbook::Props::Array,
+                                default: []
+
       def classname
         generate_classname("pb_advanced_table")
       end
