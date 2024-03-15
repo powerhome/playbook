096d5c29f22f3264091aa2a9db83abd1b26e4629
Nida Ghuman <nidaqg@gmail.com>
2024-03-15 10:54:08 -0400

    [PBNTR-211] Table Kit Subcomponents React (#3204)
    NOTE: Cherry pick [this
    commit](https://github.com/powerhome/nitro-web/pull/38387/commits/35570ae69b5eb283c54ca9d5d0450175a59a0de8)
    on the release that this work ships on
    
    **What does this PR do?**
    [RUNWAY
    STORY](https://nitro.powerhrg.com/runway/backlog_items/PBNTR-211)
    
    Creates the following subcomponents for the react side of the Table kit:
    
    Table.Body (Maps to tbody element)
    Table.Head (Maps to thead element)
    Table.Cell (Maps to td element)
    Table.Header (Maps to th element)
    
    Updates the following existing subcomponents to work with new structure:
    
    Table.Row (Maps to tr element)
    
    Creates the tag prop on all subcomponents to allow for use as table
    element or div with classname.
    
    #### Additional Changes:
    - Adds compound component Table.Row in addition to the existing exported
    TableRow subcomponent
    - This means Table.Row can be used as part of a compound component
    without having to individually import it, while at the same time not
    removing the TableRow export. There are 13 places in Nitro currently
    using TableRow, will test/make changes to these on the alpha and provide
    commit to cherry pick for release.
    - Change default for sideHighlightColor to “none” to match rails side +
    make the Table.Row usable (otherwise would have to explicitly set
    sideHighlightColor to none everytime a dev wanted to use the subkit
    structure)
    - Same as Table.Row above, will test/make changes in Nitro as needed on
    the alpha to set sideHighlightColor to “windows” if relying on default.
    Will provide commit to cherry pick for release
    
    
    
    **Screenshots:** 
    
    <img width="1205" alt="Screenshot 2024-03-06 at 9 18 02 PM"
    src="https://github.com/powerhome/playbook/assets/73710701/b489e80e-7368-41f3-b388-1fc634fe4724">
    
    <img width="566" alt="Screenshot 2024-03-06 at 9 18 12 PM"
    src="https://github.com/powerhome/playbook/assets/73710701/fddefe8e-d606-4010-949e-8ac38b6da90d">
    
    ---------
    
    Co-authored-by: Carlos Lima <carloslimasd@gmail.com>

---
+++ playbook/app/pb_kits/playbook/pb_table/subcomponents/_table_cell.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import classnames from "classnames";
+import {
+  buildAriaProps,
+  buildDataProps,
+  buildHtmlProps,
+} from "../../utilities/props";
+import { globalProps } from "../../utilities/globalProps";
+
+type TableCellPropTypes = {
+  aria?: { [key: string]: string };
+  children: React.ReactNode[] | React.ReactNode;
+  className: string;
+  data?: { [key: string]: string };
+  htmlOptions?: { [key: string]: string | number | boolean | (() => void) };
+  id?: string;
+  tag?: "table" | "div";
+  text?: string
+};
+
+const TableCell = (props: TableCellPropTypes) => {
+  const {
+    aria = {},
+    children,
+    className,
+    data = {},
+    htmlOptions = {},
+    id,
+    tag = "table",
+    text,
+  } = props;
+
+  const ariaProps = buildAriaProps(aria);
+  const dataProps = buildDataProps(data);
+  const htmlProps = buildHtmlProps(htmlOptions);
+  const classes = classnames("pb_table_td", globalProps(props), className);
+  const isTableTag = tag === "table";
+
+  return (
+    <>
+      {isTableTag ? (
+        <td
+            {...ariaProps}
+            {...dataProps}
+            {...htmlProps}
+            className={classes}
+            id={id}
+        >
+          {text || children}
+        </td>
+      ) : (
+        <div
+            {...ariaProps}
+            {...dataProps}
+            {...htmlProps}
+            className={classes}
+            id={id}
+        >
+          {text || children}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default TableCell;
