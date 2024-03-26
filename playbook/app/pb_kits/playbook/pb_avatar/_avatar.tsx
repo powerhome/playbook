cbb9201f02369d513a225371e66ecee0168b5f64
nida.ghuman <nidaqg@gmail.com>
2024-03-25 22:11:17 -0400

    testing prop

--- playbook/app/pb_kits/playbook/pb_avatar/_avatar.tsx
+++ playbook/app/pb_kits/playbook/pb_avatar/_avatar.tsx
@@ -4,8 +4,12 @@ import classnames from 'classnames'
 import { buildAriaProps, buildCss, buildDataProps, buildHtmlProps } from '../utilities/props'
 import { globalProps, GlobalProps } from '../utilities/globalProps'
 
-import Image from '../pb_image/_image'
-import OnlineStatus from '../pb_online_status/_online_status'
+import Image from "../pb_image/_image";
+import OnlineStatus from "../pb_online_status/_online_status";
+import Flex from "../pb_flex/_flex";
+import Badge from "../pb_badge/_badge";
+import IconCircle from "../pb_icon_circle/_icon_circle";
+import Card from "../pb_card/_card";
 
 export type AvatarProps = {
   aria?: {[key: string]: string},
@@ -33,6 +37,7 @@ const Avatar = (props: AvatarProps): React.ReactElement => {
     data = {},
     htmlOptions = {},
     name = undefined,
+    overlay,
     id = '',
     imageAlt = '',
     imageUrl,
@@ -57,6 +62,18 @@ const Avatar = (props: AvatarProps): React.ReactElement => {
 
   const canShowImage = imageUrl && !error
 
+  //example of placement configurations, will need to account for different Avatar sizes + will be different for badge vs IconCircle
+  const getPlacementProps = (placement: string) => {
+  const placementMapping: any = {
+    'top-right': { top: '0', right: '0' },
+    'bottom-left': { bottom: '0', left: '0' },
+    'top-left': { top: '0', left: '0' },
+    'bottom-right': { bottom: '0', right: '0' },
+  }
+
+  return placementMapping[placement] || {};
+  }
+
   return (
     <div
         {...ariaProps}
@@ -65,24 +82,68 @@ const Avatar = (props: AvatarProps): React.ReactElement => {
         className={classes}
         id={id}
     >
-      <div
-          className="avatar_wrapper"
-          data-initials={initials}
-      >
-        { canShowImage && (
-          <Image
-              alt={imageAlt ? imageAlt : name}
-              onError={handleError}
-              url={imageUrl}
-          />
-        )}
-      </div>
-      {status && (
-        <OnlineStatus
-            className={`size_${size}`}
-            dark={dark}
-            status={status}
-        />
+      {overlay ? (
+        <Flex display="display_inline_block" 
+            position="relative"
+        >
+          <div className="avatar_wrapper" 
+              data-initials={initials}
+          >
+            {canShowImage && (
+              <Image
+                  alt={imageAlt ? imageAlt : name}
+                  onError={handleError}
+                  url={imageUrl}
+              />
+            )}
+          </div>
+
+          {overlay.component === "badge" && (
+            <Badge
+                position="absolute"
+                rounded
+                text={overlay.text}
+                variant={overlay.variant}
+                {...getPlacementProps(overlay.placement)}
+            />
+          )}
+          {overlay.component === "icon" && (
+            <Card
+                borderNone
+                borderRadius="rounded"
+                padding="xxs"
+                position="absolute"
+                {...getPlacementProps(overlay.placement)}
+            >
+              <IconCircle
+                  icon={overlay.icon}
+                  size="xs"
+                  variant={overlay.variant}
+              />
+            </Card>
+          )}
+        </Flex>
+      ) : (
+        <>
+          <div className="avatar_wrapper" 
+              data-initials={initials}
+          >
+            {canShowImage && (
+              <Image
+                  alt={imageAlt ? imageAlt : name}
+                  onError={handleError}
+                  url={imageUrl}
+              />
+            )}
+          </div>
+          {status && (
+            <OnlineStatus
+                className={`size_${size}`}
+                dark={dark}
+                status={status}
+            />
+          )}
+        </>
       )}
     </div>
   )
