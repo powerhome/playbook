716932cfc4cff83c6ac05503d2c36013440f5be9
Carlos Lima <carloslimasd@gmail.com>
2024-04-05 08:52:25 -0300

    Testing CI

--- playbook/app/pb_kits/playbook/pb_advanced_table/docs/advanced_table_example_mock_data.rb
+++ playbook/app/pb_kits/playbook/pb_advanced_table/docs/advanced_table_example_mock_data.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 
-advanced_table_mock_data = File.read("../playbook/app/pb_kits/playbook/pb_advanced_table/docs/advanced_table_mock_data.json")
-TABLE_DATA = JSON.parse(advanced_table_mock_data, object_class: OpenStruct)
+# advanced_table_mock_data = File.read("../playbook/app/pb_kits/playbook/pb_advanced_table/docs/advanced_table_mock_data.json")
+# TABLE_DATA = JSON.parse(advanced_table_mock_data, object_class: OpenStruct)
