d06134be861bf0c8de99c652782e854bbac0ddf3
Elisa Shapiro <elisashapiro@gmail.com>
2024-04-25 14:20:46 -0400

    Added disabled doc examples for checkbox and radio for both rails and react

---
+++ playbook/app/pb_kits/playbook/pb_checkbox/docs/_checkbox_disabled.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import Checkbox from '../_checkbox'
+
+const CheckboxDisabled = (props) => {
+  return (
+    <div>
+      <Checkbox
+          disabled
+          name="default name"
+          tabIndex={0}
+          text="Disabled unchecked"
+          value="default value"
+          {...props}
+      />
+      <br />
+      <br />
+      <Checkbox
+          checked
+          disabled
+          name="checkbox-name"
+          text="Disabled checked"
+          value="check-box value"
+          {...props}
+      />
+    </div>
+  )
+}
+
+export default CheckboxDisabled
---
+++ playbook/app/pb_kits/playbook/pb_radio/docs/_radio_disabled.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import Radio from '../_radio'
+
+const RadioDisabled = () => {
+  const ref = React.createRef()
+
+  return (
+    <div>
+      <Radio
+          disabled
+          label="Disabled unselected no checked info"
+          name="DisabledGroup"
+          ref={ref}
+          tabIndex={0}
+          value="Disabled unselected no checked info"
+      />
+      <br />
+      <Radio
+          defaultChecked={false}
+          disabled
+          label="Disabled unselected with default checked false"
+          name="DisabledGroup"
+          value="Disabled unselected with default checked false"
+      />
+      <br />
+      <Radio
+          defaultChecked={choice === 'TBDisabled selected with default checked true'}
+          disabled
+          label="TBDisabled selected with default checked true"
+          name="DisabledGroup"
+          value="TBDisabled selected with default checked true"
+      />
+    </div>
+  )
+}
+export default RadioDisabled
