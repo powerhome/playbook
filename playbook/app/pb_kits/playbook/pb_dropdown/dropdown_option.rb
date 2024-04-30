a1d3dbc4b20ccd4c306aa7e6f215d8320cafdf5e
nida.ghuman <nidaqg@gmail.com>
2024-04-30 12:32:13 -0400

    options prop + default example

--- playbook/app/pb_kits/playbook/pb_dropdown/dropdown_option.rb
+++ playbook/app/pb_kits/playbook/pb_dropdown/dropdown_option.rb
@@ -3,8 +3,10 @@
 module Playbook
   module PbDropdown
     class DropdownOption < Playbook::KitBase
+      prop :option, type: Playbook::Props::String
+
       def classname
-        generate_classname("pb_dropdown_option")
+        generate_classname("pb_dropdown_option", "list")
       end
     end
   end
