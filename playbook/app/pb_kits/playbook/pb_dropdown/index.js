12590bf41c5057f6ad3595357ed5650a29549768
nida.ghuman <nidaqg@gmail.com>
2024-05-02 11:13:37 -0400

    Separate accessibility logic to separate file

--- playbook/app/pb_kits/playbook/pb_dropdown/index.js
+++ playbook/app/pb_kits/playbook/pb_dropdown/index.js
@@ -1,4 +1,5 @@
 import PbEnhancedElement from '../pb_enhanced_element'
+import { PbDropdownKeyboard } from './keyboard_accessibility'
 
 const DROPDOWN_SELECTOR = '[data-pb-dropdown]'
 const TRIGGER_SELECTOR = '[data-dropdown-trigger]'
@@ -7,74 +8,6 @@ const DOWN_ARROW_SELECTOR = '#dropdown_open_icon'
 const UP_ARROW_SELECTOR = '#dropdown_close_icon'
 const OPTION_SELECTOR = '[data-dropdown-option-label]'
 const CUSTOM_DISPLAY_SELECTOR = '[data-dropdown-custom-trigger]'
-
-
-// Keyboard accessibility
-class PbDropdownKeyboard {
-  constructor(dropdown) {
-      this.dropdown = dropdown;
-      this.dropdownElement = dropdown.element;
-      this.options = Array.from(this.dropdownElement.querySelectorAll(OPTION_SELECTOR));
-      this.focusedOptionIndex = -1;
-      this.init();
-  }
-
-  init() {
-      this.dropdownElement.addEventListener('keydown', this.handleKeyDown.bind(this));
-  }
-
-  handleKeyDown(event) {
-      switch (event.key) {
-          case 'ArrowDown':
-              event.preventDefault();
-              if (!this.dropdown.target.classList.contains('open')) {
-                this.dropdown.showElement(this.dropdown.target);
-                this.dropdown.displayUpArrow();
-            }
-              this.moveFocus(1);
-              break;
-          case 'ArrowUp':
-              event.preventDefault();
-              this.moveFocus(-1);
-              break;
-          case 'Enter':
-            event.preventDefault();
-            if (this.focusedOptionIndex !== -1) {
-                this.selectOption();
-            } else {
-                if (!this.dropdown.target.classList.contains('open')) {
-                    this.dropdown.showElement(this.dropdown.target);
-                    this.dropdown.displayUpArrow();
-                }
-            }
-            break;
-          case 'Escape':
-              this.dropdown.hideElement(this.dropdown.target);
-              break;
-              case 'Tab':
-                this.dropdown.hideElement(this.dropdown.target);
-                this.dropdown.displayDownArrow();
-                this.resetFocus();
-                break;
-          default:
-              break;
-      }
-  }
-
-  moveFocus(direction) {
-      if (this.focusedOptionIndex !== -1) {
-          this.options[this.focusedOptionIndex].classList.remove('pb_dropdown_option_focused');
-      }
-      this.focusedOptionIndex = (this.focusedOptionIndex + direction + this.options.length) % this.options.length;
-      this.options[this.focusedOptionIndex].classList.add('pb_dropdown_option_focused');
-  }
-
-  selectOption() {
-      const option = this.options[this.focusedOptionIndex];
-      this.dropdown.onOptionSelected(option.dataset.dropdownOptionLabel, option);
-      this.dropdown.hideElement(this.dropdown.target);
-  }
-}
 
 export default class PbDropdown extends PbEnhancedElement {
   static get selector() {
