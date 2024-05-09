53bd7adbdad601741d181a699d6335a99f7ff127
nickamantia <nick.amantia@powerhrg.com>
2024-05-09 16:44:56 -0400

    upadted doc

--- playbook/app/pb_kits/playbook/pb_avatar/docs/_avatar_badge_component_overlay.jsx
+++ playbook/app/pb_kits/playbook/pb_avatar/docs/_avatar_badge_component_overlay.jsx
@@ -7,30 +7,6 @@ const AvatarBadgeComponentOverlay = () => {
             <Avatar
                 componentOverlay={{
                     component: "badge",
-                    placement: "top-left",
-                    text: "12"
-                }}
-                imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-                marginBottom="sm"
-                size="xxs"
-            />
-
-            {/* Existing code */}
-            <Avatar
-                componentOverlay={{
-                    component: "badge",
-                    placement: "top-right",
-                    text: "12"
-                }}
-                imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-                marginBottom="sm"
-                size="xs"
-            />
-
-            {/* Existing code */}
-            <Avatar
-                componentOverlay={{
-                    component: "badge",
                     placement: "bottom-right",
                     text: "12"
                 }}
@@ -39,31 +15,6 @@ const AvatarBadgeComponentOverlay = () => {
                 size="sm"
             />
 
-            {/* Existing code */}
-            <Avatar
-                componentOverlay={{
-                    component: "badge",
-                    placement: "bottom-center",
-                    text: "12"
-                }}
-                imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-                marginBottom="sm"
-                size="md"
-            />
-
-            {/* Existing code */}
-            <Avatar
-                componentOverlay={{
-                    component: "badge",
-                    placement: "bottom-left",
-                    text: "12"
-                }}
-                imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-                marginBottom="sm"
-                size="lg"
-            />
-
-            {/* Existing code */}
             <Avatar
                 componentOverlay={{
                     component: "badge",
@@ -72,287 +23,32 @@ const AvatarBadgeComponentOverlay = () => {
                 }}
                 imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
                 marginBottom="sm"
+                size="md"
+            />
+
+            <Avatar
+                componentOverlay={{
+                    component: "badge",
+                    placement: "top-center",
+                    text: "On Roadtip",
+                    variant: "info"
+                }}
+                imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
+                marginBottom="sm"
+                size="lg"
+            />
+
+            <Avatar
+                componentOverlay={{
+                    component: "badge",
+                    placement: "bottom-center",
+                    text: "Out of Office",
+                    variant: "error"
+                }}
+                imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
+                marginBottom="sm"
                 size="xl"
             /> 
-
-
-            <Avatar
-                componentOverlay={{
-                    component: "badge",
-                    placement: "top-center",
-                    text: "On Roadtrip"
-                }}
-                imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-                marginBottom="sm"
-                size="xxs"
-            />
-
-            <Avatar
-                componentOverlay={{
-                    component: "badge",
-                    placement: "bottom-center",
-                    text: "On Roadtrip"
-                }}
-                imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-                marginBottom="sm"
-                size="xxs"
-            />
-
-
-            <Avatar
-                componentOverlay={{
-                    component: "badge",
-                    placement: "left-center",
-                    text: "12"
-                }}
-                imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-                marginBottom="sm"
-                size="xxs"
-            />
-
-
-            <Avatar
-                componentOverlay={{
-                    component: "badge",
-                    placement: "right-center",
-                    text: "12"
-                }}
-                imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-                marginBottom="sm"
-                size="xxs"
-            />
-
-
-            <Avatar
-                componentOverlay={{
-                    component: "badge",
-                    placement: "top-center",
-                    text: "On Roadtrip"
-                }}
-                imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-                marginBottom="sm"
-                size="xs"
-            />
-
-            <Avatar
-                componentOverlay={{
-                    component: "badge",
-                    placement: "bottom-center",
-                    text: "On Roadtrip"
-                }}
-                imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-                marginBottom="sm"
-                size="xs"
-            />
-
-
-            <Avatar
-                componentOverlay={{
-                    component: "badge",
-                    placement: "left-center",
-                    text: "12"
-                }}
-                imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-                marginBottom="sm"
-                size="xs"
-            />
-
-
-            <Avatar
-                componentOverlay={{
-                    component: "badge",
-                    placement: "right-center",
-                    text: "12"
-                }}
-                imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-                marginBottom="sm"
-                size="xs"
-            />
-
-
-            <Avatar
-                componentOverlay={{
-                    component: "badge",
-                    placement: "top-center",
-                    text: "On Roadtrip"
-                }}
-                imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-                marginBottom="sm"
-                size="sm"
-            />
-
-            <Avatar
-                componentOverlay={{
-                    component: "badge",
-                    placement: "bottom-center",
-                    text: "On Roadtrip"
-                }}
-                imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-                marginBottom="sm"
-                size="sm"
-            />
-
-
-            <Avatar
-                componentOverlay={{
-                    component: "badge",
-                    placement: "left-center",
-                    text: "12"
-                }}
-                imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-                marginBottom="sm"
-                size="sm"
-            />
-
-
-            <Avatar
-                componentOverlay={{
-                    component: "badge",
-                    placement: "right-center",
-                    text: "12"
-                }}
-                imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-                marginBottom="sm"
-                size="sm"
-            />
-
-            <Avatar
-                componentOverlay={{
-                    component: "badge",
-                    placement: "top-center",
-                    text: "On Roadtrip"
-                }}
-                imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-                marginBottom="sm"
-                size="md"
-            />
-
-        <Avatar
-            componentOverlay={{
-                    component: "badge",
-                    placement: "bottom-center",
-                    text: "On Roadtrip"
-                }}
-            imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-            marginBottom="sm"
-            size="md"
-        />
-
-
-            <Avatar
-                componentOverlay={{
-                    component: "badge",
-                    placement: "left-center",
-                    text: "12"
-                }}
-                imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-                marginBottom="sm"
-                size="md"
-            />
-
- 
-            <Avatar
-                componentOverlay={{
-                    component: "badge",
-                    placement: "right-center",
-                    text: "12"
-                }}
-                imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-                marginBottom="sm"
-                size="md"
-            />
-
-            {/* New code */}
-            <Avatar
-                componentOverlay={{
-                    component: "badge",
-                    placement: "top-center",
-                    text: "On Roadtrip"
-                }}
-                imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-                marginBottom="sm"
-                size="lg"
-            />
-
-
-            <Avatar
-                componentOverlay={{
-                    component: "badge",
-                    placement: "bottom-center",
-                    text: "On Roadtrip"
-                }}
-                imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-                marginBottom="sm"
-                size="lg"
-            />
-
-            <Avatar
-                componentOverlay={{
-                    component: "badge",
-                    placement: "left-center",
-                    text: "12"
-                }}
-                imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-                marginBottom="sm"
-                size="lg"
-            />
-
-            <Avatar
-                componentOverlay={{
-                    component: "badge",
-                    placement: "right-center",
-                    text: "12"
-                }}
-                imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-                marginBottom="sm"
-                size="lg"
-            />
-
-            <Avatar
-                componentOverlay={{
-                    component: "badge",
-                    placement: "top-center",
-                    text: "On Roadtrip"
-                }}
-                imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-                marginBottom="sm"
-                size="xl"
-            />
-
-        <Avatar
-            componentOverlay={{
-                    component: "badge",
-                    placement: "bottom-center",
-                    text: "On Roadtrip"
-                }}
-            imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-            marginBottom="sm"
-            size="xl"
-        />
-
-
-            <Avatar
-                componentOverlay={{
-                    component: "badge",
-                    placement: "left-center",
-                    text: "12"
-                }}
-                imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-                marginBottom="sm"
-                size="xl"
-            />
-
-
-            <Avatar
-                componentOverlay={{
-                    component: "badge",
-                    placement: "right-center",
-                    text: "12"
-                }}
-                imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-                marginBottom="sm"
-                size="xl"
-            />
         </div>
     )
 }
--- playbook/app/pb_kits/playbook/pb_avatar/docs/_avatar_circle_icon_component_overlay.jsx
+++ playbook/app/pb_kits/playbook/pb_avatar/docs/_avatar_circle_icon_component_overlay.jsx
@@ -7,338 +7,50 @@ const AvatarCircleIconComponentOverlay = () => {
         <Avatar
             componentOverlay={{
                 component: "iconCircle",
-                placement: "top-left",
-                icon: "plus"
-            }}
-            imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-            marginBottom="sm"
-            size="xxs"
-        />
-
-        {/* Existing code */}
-        <Avatar
-            componentOverlay={{
-                component: "iconCircle",
-                placement: "top-right",
-                icon: "plus"
-            }}
-            imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-            marginBottom="sm"
-            size="xs"
-        />
-
-        {/* Existing code */}
-        <Avatar
-            componentOverlay={{
-                component: "iconCircle",
                 placement: "bottom-right",
-                icon: "plus"
+                icon: "shield",
+                variant: "royal"
             }}
             imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
             marginBottom="sm"
             size="sm"
         />
 
-        {/* Existing code */}
         <Avatar
             componentOverlay={{
                 component: "iconCircle",
-                placement: "bottom-center",
-                icon: "plus"
+                placement: "bottom-right",
+                icon: "check",
+                variant: "green"
             }}
             imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
             marginBottom="sm"
             size="md"
         />
 
-        {/* Existing code */}
         <Avatar
             componentOverlay={{
                 component: "iconCircle",
-                placement: "bottom-left",
-                icon: "plus"
+                placement: "top-left",
+                icon: "lock",
+                variant: "red"
             }}
             imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
             marginBottom="sm"
             size="lg"
         />
 
-        {/* Existing code */}
         <Avatar
             componentOverlay={{
                 component: "iconCircle",
-                placement: "top-left",
-                icon: "plus"
+                placement: "left-center",
+                icon: "star",
+                variant: "yellow"
             }}
             imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
             marginBottom="sm"
             size="xl"
         /> 
-
-        <Avatar
-            componentOverlay={{
-                component: "iconCircle",
-                placement: "top-center",
-                icon: "plus"
-            }}
-            imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-            marginBottom="sm"
-            size="xxs"
-        />
-
-        <Avatar
-            componentOverlay={{
-                component: "iconCircle",
-                placement: "bottom-center",
-                icon: "plus"
-            }}
-            imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-            marginBottom="sm"
-            size="xxs"
-        />
-
-        <Avatar
-            componentOverlay={{
-                component: "iconCircle",
-                placement: "left-center",
-                icon: "plus"
-            }}
-            imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-            marginBottom="sm"
-            size="xxs"
-        />
-
-        <Avatar
-            componentOverlay={{
-                component: "iconCircle",
-                placement: "right-center",
-                icon: "plus"
-            }}
-            imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-            marginBottom="sm"
-            size="xxs"
-        />
-
-        <Avatar
-            componentOverlay={{
-                component: "iconCircle",
-                placement: "top-center",
-                icon: "plus"
-            }}
-            imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-            marginBottom="sm"
-            size="xs"
-        />
-
-        <Avatar
-            componentOverlay={{
-                component: "iconCircle",
-                placement: "bottom-center",
-                icon: "plus"
-            }}
-            imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-            marginBottom="sm"
-            size="xs"
-        />
-
-        <Avatar
-            componentOverlay={{
-                component: "iconCircle",
-                placement: "left-center",
-                icon: "plus"
-            }}
-            imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-            marginBottom="sm"
-            size="xs"
-        />
-
-        <Avatar
-            componentOverlay={{
-                component: "iconCircle",
-                placement: "right-center",
-                icon: "plus"
-            }}
-            imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-            marginBottom="sm"
-            size="xs"
-        />
-
-        <Avatar
-            componentOverlay={{
-                component: "iconCircle",
-                placement: "top-center",
-                icon: "plus"
-            }}
-            imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-            marginBottom="sm"
-            size="sm"
-        />
-
-        <Avatar
-            componentOverlay={{
-                component: "iconCircle",
-                placement: "bottom-center",
-                icon: "plus"
-            }}
-            imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-            marginBottom="sm"
-            size="sm"
-        />
-
-        <Avatar
-            componentOverlay={{
-                component: "iconCircle",
-                placement: "left-center",
-                icon: "plus"
-            }}
-            imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-            marginBottom="sm"
-            size="sm"
-        />
-
-        <Avatar
-            componentOverlay={{
-                component: "iconCircle",
-                placement: "right-center",
-                icon: "plus"
-            }}
-            imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-            marginBottom="sm"
-            size="sm"
-        />
-
-        <Avatar
-            componentOverlay={{
-                component: "iconCircle",
-                placement: "top-center",
-                icon: "plus"
-            }}
-            imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-            marginBottom="sm"
-            size="md"
-        />
-
-    <Avatar
-        componentOverlay={{
-                component: "iconCircle",
-                placement: "bottom-center",
-                icon: "plus"
-            }}
-        imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-        marginBottom="sm"
-        size="md"
-    />
-
-        <Avatar
-            componentOverlay={{
-                component: "iconCircle",
-                placement: "left-center",
-                icon: "plus"
-            }}
-            imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-            marginBottom="sm"
-            size="md"
-        />
-
-        <Avatar
-            componentOverlay={{
-                component: "iconCircle",
-                placement: "right-center",
-                icon: "plus"
-            }}
-            imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-            marginBottom="sm"
-            size="md"
-        />
-
-        <Avatar
-            componentOverlay={{
-                component: "iconCircle",
-                placement: "top-center",
-                icon: "plus"
-            }}
-            imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-            marginBottom="sm"
-            size="lg"
-        />
-
-
-        <Avatar
-            componentOverlay={{
-                component: "iconCircle",
-                placement: "bottom-center",
-                icon: "plus"
-            }}
-            imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-            marginBottom="sm"
-            size="lg"
-        />
-
-        <Avatar
-            componentOverlay={{
-                component: "iconCircle",
-                placement: "left-center",
-                icon: "plus"
-            }}
-            imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-            marginBottom="sm"
-            size="lg"
-        />
-
-        <Avatar
-            componentOverlay={{
-                component: "iconCircle",
-                placement: "right-center",
-                icon: "plus"
-            }}
-            imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-            marginBottom="sm"
-            size="lg"
-        />
-
-        <Avatar
-            componentOverlay={{
-                component: "iconCircle",
-                placement: "top-center",
-                icon: "plus"
-            }}
-            imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-            marginBottom="sm"
-            size="xl"
-        />
-
-    <Avatar
-        componentOverlay={{
-                component: "iconCircle",
-                placement: "bottom-center",
-                icon: "plus"
-            }}
-        imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-        marginBottom="sm"
-        size="xl"
-    />
-
-        <Avatar
-            componentOverlay={{
-                component: "iconCircle",
-                placement: "left-center",
-                icon: "plus"
-            }}
-            imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-            marginBottom="sm"
-            size="xl"
-        />
-
-        <Avatar
-            componentOverlay={{
-                component: "iconCircle",
-                placement: "right-center",
-                icon: "plus"
-            }}
-            imageUrl="https://randomuser.me/api/portraits/men/44.jpg"
-            marginBottom="sm"
-            size="xl"
-        />
     </div>
     )
 }
