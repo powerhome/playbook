f0cf4de8138980c609a4b89c14af2a4491b0668c
nida.ghuman <nidaqg@gmail.com>
2024-05-22 13:02:09 -0400

    label and margin

--- playbook/lib/playbook/forms/builder/dropdown_field.rb
+++ playbook/lib/playbook/forms/builder/dropdown_field.rb
@@ -5,6 +5,8 @@ module Playbook
     class Builder
       def dropdown_field(name, props: {})
         props[:name] = name
+        props[:margin_bottom] = "sm"
+        props[:label] = @template.label(@object_name, name) if props[:label] == true
         @template.pb_rails("dropdown", props: props)
       end
     end
