eb8f65f4ec3ae29127c049bca733d1eddbc9dba3
Mark <mark@does.work>
2024-05-23 13:50:19 -0400

    Use snapshot instead of string and replace

--- playbook/app/pb_kits/playbook/pb_collapsible/collapsible.test.js
+++ playbook/app/pb_kits/playbook/pb_collapsible/collapsible.test.js
@@ -50,7 +50,7 @@ test('returns namespaced additional_class class name', () => {
 })
 
 test('html structure is correct', () => {
-  render(
+  const { container } = render(
     <Collapsible
         className="additional_class"
         data={{ testid: testId }}
@@ -70,31 +70,5 @@ test('html structure is correct', () => {
     </Collapsible>
   )
 
-  const kit = screen.getByTestId(testId)
-  const normalizeHTML = (html) => html.replace(/\s+/g, '').trim();
-  const expectedHTML = `
-    <div data-testid="collapsible1" class="pb_collapsible_kit additional_class">
-      <div class="pb_collapsible_main_kit cursor_pointer">
-        <div>
-          <div class="pb_flex_kit_orientation_row_justify_content_left_align_items_center_spacing_between">
-            <div class="pb_flex_item_kit">
-              <div>Main Section</div>
-            </div>
-            <div class="pb_flex_item_kit">
-              <div class="icon_wrapper" style="vertical-align: middle; color: rgb(193, 205, 214);">
-                <i class="pb_icon_kit far fa-fw fa-lg fa-chevron-down"></i>
-                <span aria-label="chevron-down icon" hidden=""></span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="pb_collapsible_content_kit toggle-content" data-collapsible-content="true" style="height: 0px;">
-        <div>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vel erat sed purus hendrerit viverra. Duis et vestibulum metus. Sed consequat ut ante non vehicula. Etiam nunc massa, pharetra vel quam id, posuere rhoncus quam. Quisque imperdiet arcu enim, nec aliquet justo auctor eget. Curabitur in metus nec nunc rhoncus faucibus vitae ac elit. Nulla facilisi. Vestibulum quis pretium nulla. Nulla ut accumsan velit. Duis varius urna sed sem tempor, sit amet fermentum nibh auctor. Praesent lorem arcu, egestas non ante quis, placerat pellentesque lectus. Vestibulum lacinia ipsum quis venenatis tristique. Vivamus suscipit, libero eu fringilla egestas, orci urna commodo arcu, vel gravida turpis ipsum molestie nibh. Donec cursus eu ante sagittis ultrices. Phasellus id sagittis risus. Mauris dapibus neque faucibus, tempor ligula vel, cursus ante. Donec faucibus gravida porta. Nullam egestas est quis aliquam feugiat. Sed eget metus diam. Cras eget placerat libero.
-        </div>
-      </div>
-    </div>
-  `;
-  expect(normalizeHTML(kit.outerHTML)).toEqual(normalizeHTML(expectedHTML));
+  expect(container).toMatchSnapshot()
 })
