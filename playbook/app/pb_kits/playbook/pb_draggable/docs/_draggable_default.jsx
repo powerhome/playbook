ebe63ef56531a19f33356c8b98ece3816e815ad0
nida.ghuman <nidaqg@gmail.com>
2024-05-28 13:46:50 -0400

    Remove unneccessary props

--- playbook/app/pb_kits/playbook/pb_draggable/docs/_draggable_default.jsx
+++ playbook/app/pb_kits/playbook/pb_draggable/docs/_draggable_default.jsx
@@ -1,30 +1,22 @@
 import React, { useState } from "react";
-import {
- SelectableList,
-  Draggable,
-  DraggableProvider,
-} from "../../";
+import { SelectableList, Draggable, DraggableProvider } from "../../";
 
 // Initial items to be dragged
 const data = [
   {
     id: "1",
-    container: "List",
     text: "Task 1",
   },
   {
     id: "2",
-    container: "List",
     text: "Task 2",
   },
   {
     id: "3",
-    container: "List",
     text: "Task 3",
   },
   {
     id: "4",
-    container: "List",
     text: "Task 4",
   },
 ];
@@ -32,40 +24,27 @@ const data = [
 const DraggableDefault = (props) => {
   const [initialItems, setInitialItems] = useState(data);
 
-
   return (
     <DraggableProvider>
-      <Draggable draggableItems={data} 
-          onDragChange={(items)=> 
-          setInitialItems(items)} 
+      <Draggable
+          draggableItems={data}
+          onDragChange={(items) => setInitialItems(items)}
           {...props}
       >
-            <Draggable.Container 
-                container="List"
-            >
-                    <SelectableList variant="checkbox">
-                  {initialItems
-                    .map(
-                      ({
-                        id,
-                        text,
-                      }) => (
-                        <Draggable.Item 
-                            container="List"
-                            id={id} 
-                            key={id}
-                        >
-                        <SelectableList.Item
-                            label={text}
-                            name={id}
-                            value={id}
-                        />
-                          
-                        </Draggable.Item>
-                      )
-                    )}
-                    </SelectableList>
-            </Draggable.Container>
+        <Draggable.Container>
+          <SelectableList variant="checkbox">
+            {initialItems.map(({ id, text }) => (
+              <Draggable.Item id={id} 
+                  key={id}
+              >
+                <SelectableList.Item label={text} 
+                    name={id} 
+                    value={id} 
+                />
+              </Draggable.Item>
+            ))}
+          </SelectableList>
+        </Draggable.Container>
       </Draggable>
     </DraggableProvider>
   );
