9d66bb58eae9ed4c2bd67673ef0cedfa2429ad7f
Carlos Lima <carloslimasd@gmail.com>
2024-05-28 22:58:08 -0300

    Fixing identation

--- playbook/app/pb_kits/playbook/pb_gradient_overlay/gradient_overlay.test.jsx
+++ playbook/app/pb_kits/playbook/pb_gradient_overlay/gradient_overlay.test.jsx
@@ -7,60 +7,60 @@ const testId = "gradient-overlay"
 const children = "This is the Gradient Overlay children"
 
 test('should pass data prop', () => {
-  const props = {
-    children,
-    data: { testid: testId }
-  }
+    const props = {
+        children,
+        data: { testid: testId }
+    }
 
-  render(<GradientOverlay {...props} />)
-  const kit = screen.getByTestId(testId)
-  expect(kit).toBeInTheDocument()
+    render(<GradientOverlay {...props} />)
+    const kit = screen.getByTestId(testId)
+    expect(kit).toBeInTheDocument()
 })
 
 test("should pass className prop", () => {
-  const className = "custom-class-name";
-  const props = {
-      className,
-      children,
-      data: { testid: testId },
-  }
+    const className = "custom-class-name";
+    const props = {
+        className,
+        children,
+        data: { testid: testId },
+    }
 
-  render(<GradientOverlay {...props} />)
-  const kit = screen.getByTestId(testId)
-  expect(kit).toHaveClass(className)
+    render(<GradientOverlay {...props} />)
+    const kit = screen.getByTestId(testId)
+    expect(kit).toHaveClass(className)
 })
 
 test('should pass aria prop', () => {
-  const props = {
-    aria: { label: testId },
-    children,
-    data: { testid: testId },
-  }
+    const props = {
+        aria: { label: testId },
+        children,
+        data: { testid: testId },
+    }
 
-  render(<GradientOverlay {...props} />)
-  const kit = screen.getByTestId(testId)
-  expect(kit).toHaveAttribute('aria-label', testId)
+    render(<GradientOverlay {...props} />)
+    const kit = screen.getByTestId(testId)
+    expect(kit).toHaveAttribute('aria-label', testId)
 })
 
 test('should pass id prop', () => {
-  const props = {
-    children,
-    data: { testid: testId },
-    id: testId
-  }
+    const props = {
+        children,
+        data: { testid: testId },
+        id: testId
+    }
 
-  render(<GradientOverlay {...props} />)
-  const kit = screen.getByTestId(testId)
-  expect(kit).toHaveAttribute('id', testId)
+    render(<GradientOverlay {...props} />)
+    const kit = screen.getByTestId(testId)
+    expect(kit).toHaveAttribute('id', testId)
 })
 
 test('should render children', () => {
-  const props = {
-    children,
-    data: { testid: testId }
-  }
+    const props = {
+        children,
+        data: { testid: testId }
+    }
 
-  render(<GradientOverlay {...props} />)
-  const kit = screen.getByTestId(testId)
-  expect(kit).toHaveTextContent(props.children)
+    render(<GradientOverlay {...props} />)
+    const kit = screen.getByTestId(testId)
+    expect(kit).toHaveTextContent(props.children)
 })
