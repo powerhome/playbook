c2bb8820d568a16ed7558cdf9e6e48a7c4755814
nickamantia <nick.amantia@powerhrg.com>
2024-05-30 14:58:38 -0400

    added doc

--- playbook/app/pb_kits/playbook/pb_bar_graph/docs/_bar_graph_custom.jsx
+++ playbook/app/pb_kits/playbook/pb_bar_graph/docs/_bar_graph_custom.jsx
@@ -2,58 +2,55 @@ import React from 'react'
 
 import BarGraph from '../_bar_graph'
 
-const chartData = [{
-  name: 'Installation',
-  data: [1475, 200, 3000, 654, 656],
-}, {
-  name: 'Manufacturing',
-  data: [4434, 524, 2320, 440, 500],
-}, {
-  name: 'Sales & Distribution',
-  data: [3387, 743, 1344, 434, 440],
-}, {
-  name: 'Project Development',
-  data: [3227, 878, 999, 780, 1000],
-}, {
-  name: 'Other',
-  data: [1111, 677, 3245, 500, 200],
-}]
 
 const barGraphOptions = {
+  chart: {
+    type: 'column'
+  },
   subtitle: {
-    text: "Overwritten subtitle",
-    style: {
-      color: "red"
-    }
+    text: "Overridden Subtitle",
   },
   xAxis: {
-      labels: {
-          useHTML: true,
-          formatter: function() {
-              switch (this.value) {
-                  case 'Jan':
-                      return `<i class="far fa-apple-whole"></i> ${this.value}`
-                  case 'Feb':
-                      return `<i class="far fa-strawberry"></i> ${this.value}`
-                  case 'Mar':
-                      return `<i class="far fa-lemon"></i> ${this.value}`
-                  case 'Apr':
-                      return `<i class="far fa-pear"></i> ${this.value}`
-                  case 'May':
-                      return `<i class="far fa-peach"></i> ${this.value}`
-                  default:
-                      return ''
+      type: 'category',
+      categories: ['USA', 'Canada', 'Brazil']
+  },
+
+  plotOptions: {
+      series: {
+          cursor: 'pointer',
+          point: {
+              events: {
+                  click: function () {
+                      location.href = 'https://en.wikipedia.org/wiki/' +
+                          this.options.key;
+                  }
               }
           }
       }
-  }
+  },
+
+  series: [{
+      data: [{
+          y: 29.9,
+          name: 'USA',
+          key: 'United_States'
+      }, {
+          y: 71.5,
+          name: 'Canada',
+          key: 'Canada'
+      }, {
+          y: 106.4,
+          name: 'Brazil',
+          key: 'Brazil'
+      }]
+  }]
 }
 
 const BarGraphCustom = (props) => (
   <div>
     <BarGraph
         axisTitle="Number of Employees"
-        chartData={chartData}
+        // chartData={chartData}
         customOptions={barGraphOptions}
         id="bar-custom"
         subTitle="Subtitle to replace"
