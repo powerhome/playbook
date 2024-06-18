be72c205edcfda314a5ae85a2479c91466e14e82
nickamantia <nick.amantia@powerhrg.com>
2024-06-18 11:48:21 -0400

    percentage works

--- playbook/app/pb_kits/playbook/pb_overlay/docs/_overlay_default.jsx
+++ playbook/app/pb_kits/playbook/pb_overlay/docs/_overlay_default.jsx
@@ -31,7 +31,7 @@ const TableExample = () => {
 
 const OverlayDefault = () => (
     <>
-        <Overlay>
+        <Overlay layout={{"y":"100%"}}>
             <TableExample />
         </Overlay>
     </>
