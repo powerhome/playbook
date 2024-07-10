977a9efb0a93befb3e7e94e13387c9800cda4bbe
Elisa Shapiro <elisashapiro@gmail.com>
2024-07-10 10:34:36 -0400

    Update beta kits and kit_category pages to function with new category term, and filter out beta kits from beta kit_category page

--- playbook-website/app/javascript/components/Website/src/components/CategoryTitle/index.tsx
+++ playbook-website/app/javascript/components/Website/src/components/CategoryTitle/index.tsx
@@ -6,13 +6,14 @@ import { linkFormat } from "../../../../../utilities/website_sidebar_helper";
 import "./styles.scss";
 
 type CategoryTitleProps = {
-  name: string;
+  category: string;
 };
 
-export const CategoryTitle = ({ name }: CategoryTitleProps): React.ReactElement => {
+export const CategoryTitle = ({ category }: CategoryTitleProps): React.ReactElement => {
+  
   return (
     <Flex align="center" className="category-title" gap="xs" marginBottom="md">
-      <Title size={{ xs: 3, sm: 2, md: 2, lg: 2, xl: 2 }} tag="h1" text={linkFormat(name)} />
+      <Title size={{ xs: 3, sm: 2, md: 2, lg: 2, xl: 2 }} tag="h1" text={linkFormat(category)} />
       <Icon className="icon mobile" icon="circle-arrow-right" size="sm" />
       <Icon className="icon desktop" icon="circle-arrow-right" size="xl" />
     </Flex>
