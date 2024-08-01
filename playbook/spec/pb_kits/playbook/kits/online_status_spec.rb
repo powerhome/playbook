d6d652d83ca005bb269337a099139f7684fbb54b
Gary Kang <gary.kang@powerhrg.com>
2024-08-01 14:11:24 -0400

    Update spec tests for prop changes

--- playbook/spec/pb_kits/playbook/kits/online_status_spec.rb
+++ playbook/spec/pb_kits/playbook/kits/online_status_spec.rb
@@ -10,12 +10,22 @@ RSpec.describe Playbook::PbOnlineStatus::OnlineStatus do
       .with_default("offline")
       .with_values("online", "offline", "away", "error", "neutral", "success", "warning", "info", "primary")
   }
+  it {
+    is_expected.to define_enum_prop(:size)
+      .with_default("sm")
+      .with_values("sm", "md", "lg")
+  }
+  it { is_expected.to define_boolean_prop(:no_border).with_default(false) }
 
   describe "#classname" do
     it "returns namespaced class name", :aggregate_failures do
-      expect(subject.new({}).classname).to eq "pb_online_status_kit_offline"
-      expect(subject.new(dark: true).classname).to eq "pb_online_status_kit_offline dark"
-      expect(subject.new(classname: "additional_class").classname).to eq "pb_online_status_kit_offline additional_class"
+      expect(subject.new({}).classname).to eq "pb_online_status_kit_offline_sm"
+      expect(subject.new(dark: true).classname).to eq "pb_online_status_kit_offline_sm dark"
+      expect(subject.new(status: "online").classname).to eq "pb_online_status_kit_online_sm"
+      expect(subject.new(size: "lg").classname).to eq "pb_online_status_kit_offline_lg"
+      expect(subject.new(no_border: true).classname).to eq "pb_online_status_kit_offline_no_border_sm"
+      expect(subject.new(classname: "additional_class").classname).to eq "pb_online_status_kit_offline_sm additional_class"
+      expect(subject.new(status: "online", size: "lg", no_border: true, classname: "additional_class", dark: true).classname).to eq "pb_online_status_kit_online_no_border_lg additional_class dark"
     end
   end
 end
