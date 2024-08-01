0cbcaf0e9b63096c4b980423d490f305da9dbc39
Gary Kang <gary.kang@powerhrg.com>
2024-08-01 07:42:54 -0400

    Move 'How to Theme' to the end (after Figma)

--- playbook-website/config/initializers/global_variables.rb
+++ playbook-website/config/initializers/global_variables.rb
@@ -67,8 +67,8 @@ navigation[:whats_new] = {
 
 # Move HTML figma to the end
 
-move_pages = navigation[:getting_started][:pages].select { |page| ["HTML&_CSS", "Figma_setup", "figma_setup"].include?(page[:page_id]) }
-navigation[:getting_started][:pages].reject! { |page| ["HTML&_CSS", "Figma_setup", "figma_setup"].include?(page[:page_id]) }
+move_pages = navigation[:getting_started][:pages].select { |page| ["HTML&_CSS", "figma_setup", "how_to_theme"].include?(page[:page_id]) }
+navigation[:getting_started][:pages].reject! { |page| ["HTML&_CSS", "figma_setup", "how_to_theme"].include?(page[:page_id]) }
 navigation[:getting_started][:pages].concat(move_pages)
 
 DOCS = navigation
