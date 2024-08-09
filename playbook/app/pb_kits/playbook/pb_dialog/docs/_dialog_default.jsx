c304eb7a182d10976a80020f331cf1bf5a12b928
Gary Kang <gary.kang@powerhrg.com>
2024-08-09 13:38:25 -0400

    Remove "loading" state from Default example and extraneous attrs

--- playbook/app/pb_kits/playbook/pb_dialog/docs/_dialog_default.jsx
+++ playbook/app/pb_kits/playbook/pb_dialog/docs/_dialog_default.jsx
@@ -5,21 +5,17 @@ const DialogDefault = () => {
   const [isOpen, setIsOpen] = useState(false)
   const close = () => setIsOpen(false)
   const open = () => setIsOpen(true)
-  const [isLoading, setIsLoading] = useState(false)
 
   return (
     <>
       <Button onClick={open}>{'Open Dialog'}</Button>
       <Dialog
           cancelButton="Cancel Button"
-          className="wrapper"
           confirmButton="Okay"
-          loading={isLoading}
           onCancel={close}
           onClose={close}
-          onConfirm={() => setIsLoading(!isLoading)}
+          onConfirm={close}
           opened={isOpen}
-          portalClassName="portal"
           size="sm"
           text="Hello Body Text, Nice to meet ya."
           title="Header Title is the Title Prop"
