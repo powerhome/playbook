3f1c5dc2ec2a076bcae316f73af14bbb3891231f
Elisa Shapiro <elisashapiro@gmail.com>
2024-08-12 11:35:22 -0400

    Ensure only interactive stars are focusable

--- playbook/app/pb_kits/playbook/pb_star_rating/subcomponents/_star_rating_display.tsx
+++ playbook/app/pb_kits/playbook/pb_star_rating/subcomponents/_star_rating_display.tsx
@@ -37,7 +37,7 @@ const StarRatingDisplay = (props: StarRatingDisplayProps) => {
                         className={starIcon[colorOption].className}
                         customIcon={starIcon[colorOption].icon as unknown as { [key: string]: SVGElement }}
                         icon=""
-                        tabIndex={0}
+                        // tabIndex={0}
                     />
                 </React.Fragment>
             ))}
@@ -47,7 +47,7 @@ const StarRatingDisplay = (props: StarRatingDisplayProps) => {
                         className={starIcon[backgroundType].className}
                         customIcon={starIcon[backgroundType].icon  as unknown as { [key: string]: SVGElement }}
                         icon=""
-                        tabIndex={0}
+                        // tabIndex={0}
                     />
                 </React.Fragment>
             ))}
