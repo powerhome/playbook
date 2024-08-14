06b3425761167e04c54f5926fb1a34a74b515661
Mark <mark@does.work>
2024-08-14 14:56:27 -0400

    Make example with js

--- playbook/app/pb_kits/playbook/pb_dialog/dialog.rb
+++ playbook/app/pb_kits/playbook/pb_dialog/dialog.rb
@@ -15,6 +15,8 @@ module Playbook
       prop :text
       prop :confirm_button
       prop :cancel_button
+      prop :confirm_button_id
+      prop :cancel_button_id
       prop :status, type: Playbook::Props::Enum,
                     values: ["info", "caution", "delete", "error", "success", "default", ""],
                     default: ""
