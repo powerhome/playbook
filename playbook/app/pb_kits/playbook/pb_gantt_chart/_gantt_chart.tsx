f2e387ea3bc48d62e7a1c900d0e9c60aeaa94efb
nida.ghuman <nidaqg@gmail.com>
2024-08-20 17:31:22 -0400

    Use customOptions prop

--- playbook/app/pb_kits/playbook/pb_gantt_chart/_gantt_chart.tsx
+++ playbook/app/pb_kits/playbook/pb_gantt_chart/_gantt_chart.tsx
@@ -1,8 +1,7 @@
-
-import React, {useState, useEffect} from 'react'
-import classnames from 'classnames'
-import { buildAriaProps, buildCss, buildDataProps } from '../utilities/props'
-import { globalProps } from '../utilities/globalProps'
+import React, { useState, useEffect } from "react";
+import classnames from "classnames";
+import { buildAriaProps, buildCss, buildDataProps } from "../utilities/props";
+import { globalProps } from "../utilities/globalProps";
 import HighchartsReact from "highcharts-react-official";
 import Highcharts from "highcharts/highcharts-gantt";
 
@@ -10,68 +9,80 @@ import { highchartsTheme } from "../pb_dashboard/pbChartsLightTheme";
 import { highchartsDarkTheme } from "../pb_dashboard/pbChartsDarkTheme";
 
 type GanttChartProps = {
-  aria?: { [key: string]: string },
-  chartData: { name: string; data: number[], yAxis: number }[],
-  className?: string,
-  customOptions?: Partial<Highcharts.Options>,
-  dark?: boolean,
-  data?: { [key: string]: string },
-  id?: string,
-}
+  aria?: { [key: string]: string };
+  chartData: { name: string; data: number[]; yAxis: number }[];
+  className?: string;
+  customOptions?: Partial<Highcharts.Options>;
+  dark?: boolean;
+  data?: { [key: string]: string };
+  id?: string;
+};
 
 const GanttChart = (props: GanttChartProps) => {
   const {
-  aria = {},
-  chartData,
-  className,
-  customOptions = {},
-  dark = false,
-  data = {},
-  id,
-  } = props
+    aria = {},
+    chartData,
+    className,
+    customOptions = {},
+    dark = false,
+    data = {},
+    id,
+  } = props;
 
-  const ariaProps = buildAriaProps(aria)
-  const dataProps = buildDataProps(data)
-  const classes = classnames(buildCss('pb_gantt_chart'), globalProps(props), className)
+  const ariaProps = buildAriaProps(aria);
+  const dataProps = buildDataProps(data);
+  const classes = classnames(
+    buildCss("pb_gantt_chart"),
+    globalProps(props),
+    className
+  );
 
   const mockOptions = {
     title: {
-      text: 'Simple Gantt Chart'
+      text: "Simple Gantt Chart",
     },
-  
-    xAxis: [{
-      min: Date.UTC(2014, 10, 17),
-      max: Date.UTC(2014, 10, 30)
-    }],
-  
-    series: [{
-      name: 'Project 1',
-      data: [{
-        name: 'Start prototype',
-        start: Date.UTC(2014, 10, 18),
-        end: Date.UTC(2014, 10, 25)
-      }, {
-        name: 'Develop',
-        start: Date.UTC(2014, 10, 20),
-        end: Date.UTC(2014, 10, 25)
-      }, {
-        name: 'Run acceptance tests',
-        start: Date.UTC(2014, 10, 23),
-        end: Date.UTC(2014, 10, 26)
-      }, {
-        name: 'Test prototype',
-        start: Date.UTC(2014, 10, 27),
-        end: Date.UTC(2014, 10, 29)
-      }]
-    }]
-  }
 
-  const [options, setOptions] = useState(mockOptions);
+    xAxis: [
+      {
+        min: Date.UTC(2014, 10, 17),
+        max: Date.UTC(2014, 10, 30),
+      },
+    ],
 
+    series: [
+      {
+        name: "Project 1",
+        data: [
+          {
+            name: "Start prototype",
+            start: Date.UTC(2014, 10, 18),
+            end: Date.UTC(2014, 10, 25),
+          },
+          {
+            name: "Develop",
+            start: Date.UTC(2014, 10, 20),
+            end: Date.UTC(2014, 10, 25),
+          },
+          {
+            name: "Run acceptance tests",
+            start: Date.UTC(2014, 10, 23),
+            end: Date.UTC(2014, 10, 26),
+          },
+          {
+            name: "Test prototype",
+            start: Date.UTC(2014, 10, 27),
+            end: Date.UTC(2014, 10, 29),
+          },
+        ],
+      },
+    ],
+  };
 
-  // useEffect(() => {
-    // setOptions(customOptions);
-  // }, [customOptions]);
+  const [options, setOptions] = useState();
+
+  useEffect(() => {
+    setOptions(customOptions);
+  }, [customOptions]);
 
   const setupTheme = () => {
     dark
@@ -80,22 +91,21 @@ const GanttChart = (props: GanttChartProps) => {
   };
   setupTheme();
 
-
   return (
     <div>
-    <HighchartsReact
-        constructorType={"ganttChart"}
-        containerProps={{
-          className: classnames(globalProps(props), classes),
-          id: id,
-          ...ariaProps,
-          ...dataProps,
-        }}
-        highcharts={Highcharts}
-        options={options}
-    />
+      <HighchartsReact
+          constructorType={"ganttChart"}
+          containerProps={{
+            className: classnames(globalProps(props), classes),
+            id: id,
+            ...ariaProps,
+            ...dataProps,
+          }}
+          highcharts={Highcharts}
+          options={options}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default GanttChart
+export default GanttChart;
