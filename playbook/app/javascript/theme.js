c5eec356dcb14d869aa84f03f2029c8b3b53997a
nida.ghuman <nidaqg@gmail.com>
2024-08-20 17:39:39 -0400

    Test exporting theming files

--- playbook/app/javascript/theme.js
+++ playbook/app/javascript/theme.js
@@ -2,3 +2,5 @@
 export {default as mapTheme} from '../pb_kits/playbook/pb_map/pbMapTheme'
 export {default as useCollapsible} from '../pb_kits/playbook/pb_collapsible/useCollapsible'
 export {default as useDropdown } from '../pb_kits/playbook/pb_dropdown/hooks/useDropdown'
+export {default as highchartsTheme } from '../pb_kits/playbook/pb_dashboard/pbChartsLightTheme'
+export {default as highchartsDarkTheme } from '../pb_kits/playbook/pb_dashboard/pbChartsDarkTheme'
