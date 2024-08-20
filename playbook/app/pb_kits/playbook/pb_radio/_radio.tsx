6c0346846e60620a7eb5dda980e4b457eb8eee05
nickamantia <nick.amantia@powerhrg.com>
2024-08-20 14:32:23 -0400

    wip

--- playbook/app/pb_kits/playbook/pb_radio/_radio.tsx
+++ playbook/app/pb_kits/playbook/pb_radio/_radio.tsx
@@ -1,37 +1,36 @@
-/*eslint-disable react/no-multi-comp, flowtype/space-before-type-colon */
-
-import React, { forwardRef } from 'react'
+import React, { forwardRef, useRef } from 'react'
 import Body from '../pb_body/_body'
 import classnames from 'classnames'
 import { buildAriaProps, buildCss, buildDataProps, buildHtmlProps } from '../utilities/props'
 import { globalProps, GlobalProps } from '../utilities/globalProps'
 
 type RadioProps = {
-  aria?: {[key: string]: string},
+  aria?: { [key: string]: string },
   alignment?: string,
   checked?: boolean,
   children?: React.ReactChild[] | React.ReactChild,
+  childrenPosition?: string,
   className?: string,
   dark?: boolean,
-  data?: {[key: string]: string},
+  data?: { [key: string]: string },
   disabled?: boolean,
   error?: boolean,
-  htmlOptions?: {[key: string]: string | number | boolean | (() => void)},
+  htmlOptions?: { [key: string]: string | number | boolean | (() => void) },
   id?: string,
   label: string,
   name?: string,
   value?: string,
   text?: string,
-  onChange: (event: React.FormEvent<HTMLInputElement> | null)=>void,
+  onChange: (event: React.FormEvent<HTMLInputElement> | null) => void,
 } & GlobalProps
 
 const Radio = ({
   aria = {},
   alignment,
   children,
+  childrenPosition = "left",
   className,
   dark = false,
-  data = {},
   disabled = false,
   error = false,
   htmlOptions = {},
@@ -43,32 +42,39 @@ const Radio = ({
   onChange = () => { void 0 },
   ...props
 }: RadioProps, ref: any) => {
+  const internalRef = useRef<HTMLInputElement>(null);
+  const combinedRef = ref || internalRef;
+
   const ariaProps = buildAriaProps(aria)
-  const dataProps = buildDataProps(data)
+  const dataProps = buildDataProps({})
   const htmlProps = buildHtmlProps(htmlOptions)
   const classes = classnames(
-    buildCss('pb_radio_kit', alignment ),
-    dark ? 'dark': null, error ? 'error': null,
+    buildCss('pb_radio_kit', alignment),
+    dark ? 'dark' : null,
+    error ? 'error' : null,
     globalProps(props),
-    className)
+    className
+  )
 
-  const displayRadio = (props: RadioProps & any) => {
-    if (children)
-      return (children)
-    else
-    return (
+  const handleChildClick = () => {
+    if (combinedRef.current) {
+      combinedRef.current.click();
+    }
+  }
+
+  const renderInput = () => (
     <input
         disabled={disabled}
         id={id}
         name={name}
         onChange={onChange}
-        ref={ref}
+        ref={combinedRef}
         text={text}
         type="radio"
         value={value}
         {...props}
     />
-  )}
+  )
 
   return (
     <label
@@ -78,14 +84,31 @@ const Radio = ({
         className={classes}
         htmlFor={id}
     >
-    <>{displayRadio(props)}</>
-    <span className="pb_radio_button" />
-    <Body
-        dark={dark}
-        status={error ? 'negative' : null}
-        text={label}
-        variant={null}
-    />
+      {childrenPosition === "left" && (
+        <>
+          <div onClick={handleChildClick}>
+            {children}
+          </div>
+          {renderInput()}
+          <span className="pb_radio_button" />
+        </>
+      )}
+      <Body
+          dark={dark}
+          status={error ? 'negative' : null}
+          text={label}
+          variant={null}
+        />
+        {childrenPosition === "right" && (
+          <>
+            <span className="pb_radio_button" />
+            <div onClick={handleChildClick}>
+              {children}
+            </div>
+            {renderInput()}
+      
+          </>
+        )}
     </label>
   )
 }
--- playbook/app/pb_kits/playbook/pb_radio/docs/_radio_default.jsx
+++ playbook/app/pb_kits/playbook/pb_radio/docs/_radio_default.jsx
@@ -1,20 +1,33 @@
 import React from 'react'
 
 import Radio from '../_radio'
+import Typeahead from '../../pb_typeahead/_typeahead'
 
 const RadioDefault = (props) => {
   const ref = React.createRef()
 
+
+  const options = [
+    { label: 'Orange', value: '#FFA500' },
+    { label: 'Red', value: '#FF0000' },
+    { label: 'Green', value: '#00FF00' },
+    { label: 'Blue', value: '#0000FF' },
+  ]
+
   return (
     <div>
       <Radio
+          childrenPosition="right"
           label="Power"
           name="Group2"
           ref={ref}
           tabIndex={0}
           value="Power"
           {...props}
-      />
+      >
+        <Typeahead options={options}/>
+
+      </Radio>
       <br />
       <Radio
           defaultChecked={false}
@@ -22,7 +35,9 @@ const RadioDefault = (props) => {
           name="Group2"
           value="Nitro"
           {...props}
-      />
+      >
+        <Typeahead options={options}/>
+      </Radio>
       <br />
       <Radio
           defaultChecked={false}
