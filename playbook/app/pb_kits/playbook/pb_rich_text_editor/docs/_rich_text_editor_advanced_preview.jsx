ecc04188342c81922cef4c12035b9fe9106e3b0b
Elisa Shapiro <83474365+ElisaShapiro@users.noreply.github.com>
2024-08-21 13:57:43 -0400

    [PBNTR-286] RTE Advanced Previewer + output styles (React) (#3578)
    **What does this PR do?** A clear and concise description with your
    runway ticket url.
    [PBNTR-286](https://runway.powerhrg.com/backlog_items/PBNTR-286) creates
    a preview doc example for the Advanced editor in React (one already
    exists for the default editor). It uses a mixin file to incorporate the
    expected styles into the output on the preview card. The new default
    content text in the editor serves as instructions for how to use the
    previewer.
    
    **Screenshots:** Screenshots to visualize your addition/change
    Editor:
    <img width="772" alt="for PR example text 1"
    src="https://github.com/user-attachments/assets/c27176f4-b8f1-46fd-a9e6-41e641260431">
    Preview Card:
    <img width="754" alt="for PR example text 2"
    src="https://github.com/user-attachments/assets/8c40d4f9-545e-4667-bcd9-0026264663dc">
    
    
    
    **How to test?** Steps to confirm the desired behavior:
    1. Go to the new advanced preview doc example in the milano review
    environment
    [here](https://pr3578.playbook.beta.px.powerapp.cloud/kits/rich_text_editor/react#advanced-preview).
    2. Alter the text in the editor with styles in the editor toolbar, and
    hit preview output to see the result (aka, how the text output will look
    on the page once saved outside of the editor).
    3. Do this multiple times, see the results of several combinations.
    Editor options include font size (Paragraph, Heading 1, Heading 2,
    Heading 3), display type (bullet list, ordered list, block quote, code
    block), text appearance changes (bold, italicized, strike-through,
    link).
    
    
    #### Checklist:
    - [x] **LABELS** Add a label: `enhancement`, `bug`, `improvement`, `new
    kit`, `deprecated`, or `breaking`. See [Changelog &
    Labels](https://github.com/powerhome/playbook/wiki/Changelog-&-Labels)
    for details.
    - [x] **DEPLOY** I have added the `milano` label to show I'm ready for a
    review.
    ~~- [ ] **TESTS** I have added test coverage to my code.~~

---
+++ playbook/app/pb_kits/playbook/pb_rich_text_editor/docs/_rich_text_editor_advanced_preview.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react'
+import { Button, Card, RichTextEditor } from 'playbook-ui'
+import { useEditor, EditorContent } from "@tiptap/react"
+import StarterKit from "@tiptap/starter-kit"
+import Link from '@tiptap/extension-link'
+
+
+const RichTextEditorAdvancedPreview = (props) => {
+
+  const editor = useEditor({
+      extensions: [
+          StarterKit,
+          Link
+      ],
+      content: "Add text here, format it, and press \"Preview Output\" to see what your stylized output will look like on the page."
+  })
+
+  const [showPreview, setShowPreview] = useState(false)
+  const [previewText, setPreviewText] = useState(<div />)
+
+  const handleChange = () => {
+    if (editor) {
+      setPreviewText(editor.getHTML())
+    }
+  }
+
+  const handleClick = () => {
+    handleChange()
+    setShowPreview(true)
+  }
+  if (!editor) {
+    return null
+  } 
+      
+
+  return (
+    <div>
+      <RichTextEditor
+          advancedEditor={editor}
+          id="content-advanced-preview-editor"
+          onChange={handleChange}
+          {...props}
+      >
+        <EditorContent editor={editor}/>
+      </RichTextEditor>
+      {showPreview && (
+        <Card 
+            marginTop="md"
+            maxWidth="md"
+        >
+          <div
+              className="tiptap-content"
+              // eslint-disable-next-line react/no-danger
+              dangerouslySetInnerHTML={{ __html: previewText }}
+              id="advanced-preview-content"
+          />
+        </Card>
+      )}
+      {!showPreview && (
+        <div />
+      )}
+      <Button
+          id="preview-button"
+          marginTop="md"
+          onClick={handleClick}
+          text="Preview Output"
+          variant="secondary"
+      />
+    </div>
+  )
+}
+
+export default RichTextEditorAdvancedPreview
