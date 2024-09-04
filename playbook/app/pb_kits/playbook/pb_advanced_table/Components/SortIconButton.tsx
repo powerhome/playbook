5df93a972b3d71e5df1875a5154237a2eb874a53
Mark <mark@does.work>
2024-09-04 15:21:54 -0400

    Advanced table arrows

--- playbook/app/pb_kits/playbook/pb_advanced_table/Components/SortIconButton.tsx
+++ playbook/app/pb_kits/playbook/pb_advanced_table/Components/SortIconButton.tsx
@@ -4,6 +4,7 @@ import { Header } from "@tanstack/react-table"
 import { GenericObject } from "../../types"
 
 import Icon from "../../pb_icon/_icon"
+import { getAllIcons } from "../../utilities/icons/allicons"
 
 import { displayIcon } from "../Utilities/IconHelpers"
 
@@ -11,21 +12,33 @@ type SortIconButtonProps = {
   header: Header<GenericObject, unknown>
   sortIcon?: string | string[]
 }
+
 export const SortIconButton = ({ header, sortIcon }: SortIconButtonProps) => {
+
+const firstIcon = displayIcon(sortIcon)[0] 
+const secondIcon = displayIcon(sortIcon)[1] 
+const upIcon = getAllIcons()["arrowUpShortWide"].icon as unknown as { [key: string]: SVGElement }
+const downIcon = getAllIcons()["arrowDownShortWide"].icon as unknown as { [key: string]: SVGElement }
 
   return (
     <>
       {header.column.getIsSorted() === "desc" ? (
         <div className="sort-button-icon" 
-            key={displayIcon(sortIcon)[0]}
+            key={firstIcon}
         >
-          <Icon icon={displayIcon(sortIcon)[0]} />
+        { firstIcon === "arrow-up-short-wide" ? (
+          <Icon customIcon={upIcon} /> ) : (
+          <Icon icon={firstIcon} />
+        )}
         </div>
       ) : (
         <div className="sort-button-icon" 
-            key={displayIcon(sortIcon)[1]}
+            key={secondIcon}
         >
-          <Icon icon={displayIcon(sortIcon)[1]} />
+        { secondIcon === "arrow-down-short-wide" ? (
+          <Icon customIcon={downIcon} /> ) : (
+          <Icon icon={secondIcon} />
+        )}
         </div>
       )}
     </>
