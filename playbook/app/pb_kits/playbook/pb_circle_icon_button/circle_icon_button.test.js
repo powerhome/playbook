58352e498a9235d7feecb73bb92619c50865ef11
Gary Kang <42440452+kangaree@users.noreply.github.com>
2024-09-05 08:52:19 -0400

    [PLAY-1420] Add "loading" variant to Circle Icon Button (#3633)
    **What does this PR do?**
    
    - ✅ Expose loading prop in Circle Icon kit for underlying Button kit
    - ✅ Add examples and tests
    
    **Screenshots:**
    
    ![Screenshot 2024-08-26 at 9 48
    19 AM](https://github.com/user-attachments/assets/368dd489-927e-4d43-940e-f474d20446aa)
    
    **How to test?**
    1. Go to the Circle Icon Button kit page on Playbook
    2. Review the "loading" example
    3. There should be a pulse animation
    4. Review both Rails and React
    
    
    #### Checklist:
    - [x] **LABELS** Add a label: `enhancement`, `bug`, `improvement`, `new
    kit`, `deprecated`, or `breaking`. See [Changelog &
    Labels](https://github.com/powerhome/playbook/wiki/Changelog-&-Labels)
    for details.
    - [x] **DEPLOY** I have added the `milano` label to show I'm ready for a
    review.
    - [x] **TESTS** I have added test coverage to my code.

--- playbook/app/pb_kits/playbook/pb_circle_icon_button/circle_icon_button.test.js
+++ playbook/app/pb_kits/playbook/pb_circle_icon_button/circle_icon_button.test.js
@@ -15,3 +15,18 @@ test('default test', () => {
 
   expect(kit).toHaveClass('pb_circle_icon_button_kit')
 })
+
+test('passes loading prop to button', () => {
+  render(
+    <CircleIconButton
+        data={{ testid: 'loading-test' }}
+        icon="plus"
+        loading
+    />
+  )
+
+  const kit = screen.getByTestId('loading-test')
+  const button = kit.querySelector('.pb_button_kit_primary_inline_enabled_loading')
+  
+  expect(button).toBeInTheDocument()
+})
