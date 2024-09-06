18795bad169f339f6a15676e106245b852e18b9c
Gary Kang <42440452+kangaree@users.noreply.github.com>
2024-09-06 11:55:28 -0400

    [PLAY-1506] Fix `@import playbook.scss` Errors - Fix Theming / Overriding SCSS Variables (e.g., $z_10) (#3608)
    **What does this PR do?**
    
    - ✅ Fix `@import playbook.scss` errors. Allow theming (e.g., set $z_10).
    - ✅ Remove [PLAY-1414](https://github.com/powerhome/playbook/pull/3520)
    `calc()` functions that causes undefined variable errors.
    - ✅ Simplify by using standard SCSS calculations instead of calc()
    throughout playbook
    - ✅ Copy and use intl-tel-input node_modules CSS file so it works with
    SCSS.
    - ✅ Update "how to theme" doc
    - ✅ Prepare rails-react-example for PB 14.0.0 upgrade (add nullish
    coalescing operator to babel config)
    
    **How to test?** Steps to confirm the desired behavior:
    1. Go to /kits page
    2. Review and look for major style breaking changes
    
    #### Checklist:
    - [x] **LABELS** Add a label: `enhancement`, `bug`, `improvement`, `new
    kit`, `deprecated`, or `breaking`. See [Changelog &
    Labels](https://github.com/powerhome/playbook/wiki/Changelog-&-Labels)
    for details.
    - [x] **DEPLOY** I have added the `milano` label to show I'm ready for a
    review.
    
    ---------
    
    Co-authored-by: Carlos Lima <carloslimasd@gmail.com>

--- examples/rails-react-example/babel.config.js
+++ examples/rails-react-example/babel.config.js
@@ -46,6 +46,7 @@ module.exports = function(api) {
       ]
     ].filter(Boolean),
     plugins: [
+      '@babel/plugin-proposal-nullish-coalescing-operator',
       'babel-plugin-macros',
       '@babel/plugin-syntax-dynamic-import',
       isTestEnv && 'babel-plugin-dynamic-import-node',
