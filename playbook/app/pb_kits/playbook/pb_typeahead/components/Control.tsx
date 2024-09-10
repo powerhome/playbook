4efe4241929df4db59f6ae227e77b9087731e7d0
Elisa Shapiro <elisashapiro@gmail.com>
2024-09-10 16:29:39 -0400

    Minimize changes to Control subcomponent from current implementation

--- playbook/app/pb_kits/playbook/pb_typeahead/components/Control.tsx
+++ playbook/app/pb_kits/playbook/pb_typeahead/components/Control.tsx
@@ -4,32 +4,26 @@ import { components } from 'react-select'
 import Flex from '../../pb_flex/_flex'
 import TextInput from '../../pb_text_input/_text_input'
 
-
 type Props = {
   selectProps: any,
 }
 
-const TypeaheadControl = (props: Props) => {
-  const { selectProps } = props
-  const { dark, error, label } = selectProps
-
-  return (
-    <div className="pb_typeahead_wrapper">
-      <TextInput
-          dark={dark}
-          error={error}
-          label={label}
-          marginBottom="none"
-      >
-        <Flex>
-          <components.Control
-              className="text_input"
-              {...props}
-          />
-        </Flex>
-      </TextInput>
-    </div>
-  )
-}
+const TypeaheadControl = (props: Props) => (
+  <div className="pb_typeahead_wrapper">
+    <TextInput
+        dark={props.selectProps.dark}
+        error={props.selectProps.error}
+        label={props.selectProps.label}
+        marginBottom="none"
+    >
+      <Flex>
+        <components.Control
+            className="text_input"
+            {...props}
+        />
+      </Flex>
+    </TextInput>
+  </div>
+)
 
 export default TypeaheadControl
