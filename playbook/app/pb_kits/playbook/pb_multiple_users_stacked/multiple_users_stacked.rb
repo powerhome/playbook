c52fbe0a104e5090f676e463a2cfef99bbb7f35a
Mark <mark@does.work>
2024-09-24 10:17:57 -0400

    start working on css

--- playbook/app/pb_kits/playbook/pb_multiple_users_stacked/multiple_users_stacked.rb
+++ playbook/app/pb_kits/playbook/pb_multiple_users_stacked/multiple_users_stacked.rb
@@ -5,8 +5,8 @@ module Playbook
     class MultipleUsersStacked < Playbook::KitBase
       prop :users, type: Playbook::Props::HashArray, required: true
       prop :size, type: Playbook::Props::Enum,
-                  values: %w[sm md lg xl],
-                  default: "md"
+                  values: %w[xs sm md lg xl],
+                  default: "xs"
 
       prop :variant, type: Playbook::Props::Enum,
                      values: %w[default bubble],
@@ -34,6 +34,20 @@ module Playbook
 
       def quadruple_bubble
         bubble && users.count > 3
+      end
+
+      def other_users_size
+        bubble_size_map[size.to_sym]
+      end
+
+      def bubble_size_map
+        {
+          "xs": %w[xs xxs],
+          "sm": %w[xs xxs],
+          "md": %w[sm xs],
+          "lg": %w[md sm],
+          "xl": %w[lg md],
+        }
       end
 
       def classname
