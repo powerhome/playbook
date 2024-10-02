3d639edede562217f83a443ac42daaab8efdc9eb
Sam Duncan <samkduncan@gmail.com>
2024-10-02 08:46:32 -0500

    Updating js to also disable cancel button on submit

--- playbook/app/pb_kits/playbook/pb_form/formHelper.js
+++ playbook/app/pb_kits/playbook/pb_form/formHelper.js
@@ -5,17 +5,31 @@ const formHelper = () => {
             event.preventDefault();
 
             const submitButton = event['submitter'];
+            const cancelButton = event['target'].querySelector('button[type="reset"]');
 
             if (submitButton) {
                 let currentClass = submitButton.className;
                 let newClass = currentClass.replace("_enabled", "_disabled_loading");
 
+                let cancelClass = cancelButton.className;
+                let newCancelClass = cancelClass.replace("_enabled", "_disabled");
+
                 submitButton.disabled = true;
                 submitButton.className = newClass;
+
+                if (cancelButton) {
+                    cancelButton.disabled = true;
+                    cancelButton.className = newCancelClass;
+                }
 
                 setTimeout(function() {
                     submitButton.disabled = false;
                     submitButton.className = currentClass;
+
+                    if (cancelButton) {
+                        cancelButton.disabled = false;
+                        cancelButton.className = cancelClass;
+                    }
                 }, 5000);
             }
 
