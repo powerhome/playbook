cf7d657ed737300b9ed4ee266feccc9b1e3df26f
Carlos Lima <carloslimasd@gmail.com>
2024-10-08 14:46:18 -0400

    Getting kits raw text

--- playbook-website/app/javascript/components/AiAssistant/kitsCode.jsx
+++ playbook-website/app/javascript/components/AiAssistant/kitsCode.jsx
@@ -1,51 +1,44 @@
-import { Avatar, Badge, Card, Pill, Table, Title } from "playbook-ui";
-async function kitCode(url) {
-    try {
-        const response = await fetch(url);
-        if (response.ok) {
-            return await response.text();
-        }
-        throw new Error('Network response was not ok');
-    } catch (error) {
-        console.error('There was a problem with the fetch operation:', error);
-    }
+import avatarSource from '../../../../../playbook/app/pb_kits/playbook/pb_avatar/_avatar?raw';
+import badgeSource from '../../../../../playbook/app/pb_kits/playbook/pb_badge/_badge?raw';
+import barGraphSource from '../../../../../playbook/app/pb_kits/playbook/pb_bar_graph/_bar_graph?raw';
+import cardSource from '../../../../../playbook/app/pb_kits/playbook/pb_card/_card?raw';
+import pillSource from '../../../../../playbook/app/pb_kits/playbook/pb_pill/_pill?raw';
+import tableSource from '../../../../../playbook/app/pb_kits/playbook/pb_table/_table?raw';
+import titleSource from '../../../../../playbook/app/pb_kits/playbook/pb_title/_title?raw';
+import examplePageSource from '../../../views/samples/analytics_dashboard/index?raw';
+
+export function avatarCode() {
+    return avatarSource;
 }
 
-export async function avatarCode() {
-    return Avatar.toString();
+export function badgeCode() {
+    return badgeSource;
 }
 
-export async function badgeCode() {
-    return Badge.toString();
+export function cardCode() {
+    return cardSource;
 }
 
-export async function cardCode() {
-    return Card.toString();
+export function pillCode() {
+    return pillSource;
 }
 
-export async function pillCode() {
-    return Pill.toString();
+export function tableCode() {
+    return tableSource;
 }
 
-export async function tableCode() {
-    return Table.toString();
+export function barGraphCode() {
+    return barGraphSource;
 }
-
-export async function barGraphCode() {
-    const url = "https://raw.githubusercontent.com/powerhome/playbook/master/playbook/app/pb_kits/playbook/pb_bar_graph/_bar_graph.tsx";
-    return await kitCode(url);
-}
-
 
 export function tableExampleCode() {
     return 'import React from "react"; import { Table } from "playbook-ui"; const App = () => { return ( <Table><Table.Head><Table.Row><Table.Header>{"Column 1"}</Table.Header><Table.Header>{"Column 2"}</Table.Header><Table.Header>{"Column 3"}</Table.Header><Table.Header>{"Column 4"}</Table.Header><Table.Header>{"Column 5"}</Table.Header></Table.Row></Table.Head><Table.Body><Table.Row><Table.Cell>{"Value 1"}</Table.Cell><Table.Cell>{"Value 2"}</Table.Cell><Table.Cell>{"Value 3"}</Table.Cell><Table.Cell>{"Value 4"}</Table.Cell><Table.Cell>{"Value 5"}</Table.Cell></Table.Row><Table.Row><Table.Cell>{"Value 1"}</Table.Cell><Table.Cell>{"Value 2"}</Table.Cell><Table.Cell>{"Value 3"}</Table.Cell><Table.Cell>{"Value 4"}</Table.Cell><Table.Cell>{"Value 5"}</Table.Cell></Table.Row><Table.Row><Table.Cell>{"Value 1"}</Table.Cell><Table.Cell>{"Value 2"}</Table.Cell><Table.Cell>{"Value 3"}</Table.Cell><Table.Cell>{"Value 4"}</Table.Cell><Table.Cell>{"Value 5"}</Table.Cell></Table.Row></Table.Body></Table> ); }; export default App;'
 }
 
-export async function titleCode() {
-    return Title.toString();
+export function titleCode() {
+    return titleSource;
 }
 
 export async function examplePage() {
-    const url = "https://raw.githubusercontent.com/powerhome/playbook/master/playbook-website/app/views/samples/analytics_dashboard/index.jsx";
-    return await kitCode(url);
+    return examplePageSource;
 }
