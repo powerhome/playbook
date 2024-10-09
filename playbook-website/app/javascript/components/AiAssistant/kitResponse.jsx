99e65aadef55bfc59c1368824e06b206fa3fa389
nida.ghuman <nidaqg@gmail.com>
2024-10-09 13:59:02 -0400

    Fixes for component error

--- playbook-website/app/javascript/components/AiAssistant/kitResponse.jsx
+++ playbook-website/app/javascript/components/AiAssistant/kitResponse.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react"
 import { Dialog, FixedConfirmationToast, PbReactPopover, Tooltip, Drawer, Button, ButtonToolbar, CircleIconButton, Map, Table, AdvancedTable, List, Filter, DistributionBar, Legend, Gauge, BarGraph, CircleChart, GanttChart, LineGraph, TreemapChart, Date, DateRangeInline, DateRangeStacked, DateStacked, DateTime, DateTimeStacked, DateYearStacked, Time, TimeRangeInline, TimeStacked, Timestamp, WeekdayStacked, Contact, Currency, HomeAddressStreet, LabelPill, LabelValue, Person, PersonContact, Source, DashboardValue, StatChange, StatValue, TitleCount, TitleDetail, FileUpload, Toggle, FormPill, FormGroup, Passphrase, PhoneNumberInput, TextInput, RichTextEditor, Textarea, Typeahead, DatePicker, Dropdown, MultiLevelSelect, Select, SelectableCard, SelectableCardIcon, SelectableIcon, Radio, Checkbox, SelectableList, Icon, IconCircle, IconStatValue, IconValue, UserBadge, Image, Lightbox, StarRating, Flex, Layout, Card, SectionSeparator, Background, Collapsible, Overlay, Draggable, Highlight, Message, BreadCrumbs, Nav, Pagination, LoadingInline, ProgressPills, ProgressSimple, ProgressStep, Walkthrough, Timeline, Badge, OnlineStatus, Pill, Hashtag, Body, Caption, Detail, Title, Avatar, AvatarActionButton, MultipleUsers, MultipleUsersStacked, User } from 'playbook-ui';
-import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
+import { LiveProvider, LiveError, LivePreview } from 'react-live';
 
 const kitResponse = ({ response }) => {
   const [editorDisabled, setEditorDisabled] = useState(true);
@@ -15,18 +15,16 @@ const kitResponse = ({ response }) => {
     // Step 2: Remove export default at the bottom
     const codeWithoutExports = codeWithoutImports.replace(/export\s+default\s+\w+\s*;?/g, '');
   
-    // Step 3: Convert arrow function components to regular function components
-    const updatedComponent = codeWithoutExports.replace(/const\s+(\w+)\s*=\s*\(\)\s*=>\s*\{/g, 'function $1() {');
+    // Step 3: Convert arrow function components to a regular function called 'App'
+    const updatedComponent = codeWithoutExports.replace(/const\s+\w+\s*=\s*\(\)\s*=>\s*\{/g, 'function App() {');
   
-    // Step 4: Move any const declarations inside the function component
+    // Step 4: Move any const declarations inside the function component (generalized)
     const movedConstsInside = updatedComponent.replace(/(const\s+\w+\s*=\s*\[[\s\S]*?\];?)/g, (match, p1) => {
       return p1;
     });
   
-    // Step 5: Add a render statement at the bottom
-    const componentNameMatch = movedConstsInside.match(/function\s+(\w+)/);
-    const componentName = componentNameMatch ? componentNameMatch[1] : 'Component';
-    const renderStatement = `${movedConstsInside}\n\nrender(<${componentName} />);`;
+    // Step 5: Add the hardcoded render statement with App
+    const renderStatement = `${movedConstsInside}\n\nrender(<App />);`;
   
     return renderStatement.trim();
   };
