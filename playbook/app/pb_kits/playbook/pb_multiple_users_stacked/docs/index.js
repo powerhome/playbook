580161f45123097c98fb75af7a23bc57490fd65c
Mark Rosenberg <38965626+markdoeswork@users.noreply.github.com>
2024-10-17 11:18:28 -0400

    [PLAY-1566] Multiple User Bubble Size Prop (#3719)
    **What does this PR do?** A clear and concise description with your
    runway ticket url.
    Runway https://runway.powerhrg.com/backlog_items/PLAY-1566
    
    Added Size prop for multi user stacked kit's bubble variant
    
    All the deets are [in this
    figma](https://www.figma.com/design/Q1mz4nRApKN5rlzqCV4IC0/Multiple-User-Bubble?node-id=3701-1514&t=8SZl8NL1XQV1BelO-0)
    
    I had to manually map out a bunch of sizing and position css, I didn't
    use tokens because not all of the sizes matched our tokens.
    
    review env link
    https://pr3719.playbook.beta.hq.powerapp.cloud/kits/multiple_users_stacked/react#sizes
    
    
    ![screenshot-127_0_0_1_3000-2024_09_27-11_35_41](https://github.com/user-attachments/assets/ea4da377-4e83-41d7-9f9d-ba24c8d9a4e4)

--- playbook/app/pb_kits/playbook/pb_multiple_users_stacked/docs/index.js
+++ playbook/app/pb_kits/playbook/pb_multiple_users_stacked/docs/index.js
@@ -1,2 +1,3 @@
 export { default as MultipleUsersStackedDefault } from './_multiple_users_stacked_default.jsx'
 export { default as MultipleUsersStackedBubble } from './_multiple_users_stacked_bubble.jsx'
+export { default as MultipleUsersStackedSize } from './_multiple_users_stacked_size.jsx'
