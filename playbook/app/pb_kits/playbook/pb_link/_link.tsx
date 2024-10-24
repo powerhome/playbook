eb6cb90b510e18e4a4208122ae7b3ea63da65076
Gary Kang <gary.kang@powerhrg.com>
2024-10-23 20:53:12 -0400

    Remove extra body features/styles and combine
    Delete highlight and status
    Combine scss into one sheet
    Change variant from null to empty string

--- playbook/app/pb_kits/playbook/pb_link/_link.tsx
+++ playbook/app/pb_kits/playbook/pb_link/_link.tsx
@@ -2,9 +2,7 @@ import React from 'react'
 import classnames from 'classnames'
 
 import { buildAriaProps, buildCss, buildDataProps, buildHtmlProps } from '../utilities/props'
-import { deprecatedProps, globalProps, GlobalProps } from '../utilities/globalProps'
-
-import Highlight from '../pb_highlight/_highlight'
+import { globalProps, GlobalProps } from '../utilities/globalProps'
 
 type LinkProps = {
   aria?: {[key: string]: string},
@@ -13,43 +11,36 @@ type LinkProps = {
   color?: 'default' | 'body' | 'muted' | 'destructive',
   dark?: boolean,
   data?: {[key: string]: string},
-  highlightedText?: string[],
-  highlighting?: boolean,
   href?: string,
   htmlOptions?: {[key: string]: string | number | boolean | (() => void) | ((arg?: Event) => void)},
   id?: string,
-  status?: 'neutral' | 'negative' | 'positive',
   tag?: 'a' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'span' | 'div',
   text?: string,
   underlined?: boolean,
-  variant?: null | 'link',
+  variant?: '' | 'link',
 } & GlobalProps
 
 const Link = (props: LinkProps): React.ReactElement => {
-  if (props.variant) deprecatedProps() //status prop is deprecated, use color instead please
   const {
     aria = {},
     children,
     className,
     color = '',
     data = {},
-    highlightedText = [],
-    highlighting = false,
     href= '',
     htmlOptions = {},
     id = '',
-    status = null,
     tag = 'a',
     text = '',
     underlined = false,
-    variant = null,
+    variant = '',
   } = props
 
   const ariaProps: {[key: string]: string} = buildAriaProps(aria)
   const dataProps: {[key: string]: string} = buildDataProps(data)
   const htmlProps = buildHtmlProps(htmlOptions);
   const classes = classnames(
-    buildCss('pb_link_kit', color, variant, status, underlined ? 'underlined' : ''),
+    buildCss('pb_link_kit', color, variant, underlined ? 'underlined' : ''),
     globalProps(props),
     className
   )
@@ -64,17 +55,7 @@ const Link = (props: LinkProps): React.ReactElement => {
         href={href}
         id={id}
     >
-      { highlighting && (
-        <Highlight
-            highlightedText={highlightedText}
-            text={text}
-        >
-          {children}
-        </Highlight>
-      ) }
-      { !highlighting && (
-        text || children
-      ) }
+      { text || children }
     </Tag>
   )
 }
--- playbook/app/pb_kits/playbook/pb_link/link.rb
+++ playbook/app/pb_kits/playbook/pb_link/link.rb
@@ -7,44 +7,25 @@ module Playbook
                    values: %w[default body muted destructive],
                    default: "default"
       prop :href
-      prop :status, type: Playbook::Props::Enum,
-                    values: %w[neutral positive negative],
-                    default: "neutral",
-                    deprecated: true
       prop :tag, type: Playbook::Props::Enum,
                  values: %w[a h1 h2 h3 h4 h5 h6 p span div],
                  default: "a"
       prop :text
       prop :underlined, type: Playbook::Props::Boolean,
                         default: false
-      prop :highlighting, type: Playbook::Props::Boolean,
-                          default: false
-      prop :highlighted_text, type: Playbook::Props::Array,
-                              default: []
 
       def classname
-        generate_classname("pb_link_kit", color_class, status_class, underlined_class)
+        generate_classname("pb_link_kit", color_class, underlined_class)
       end
 
       def content
-        link_text = super.presence || text
-        highlighting ? apply_highlight(link_text) : link_text
+        text
       end
 
     private
 
-      def apply_highlight(text)
-        pb_highlight_output = pb_rails("highlight") { "|" }
-        highlight_tags = pb_highlight_output.split("|")
-        highlight(text, highlighted_text, highlighter: "#{highlight_tags.first.html_safe} \\1 #{highlight_tags.last.html_safe}")
-      end
-
       def color_class
         color == "default" ? nil : color
-      end
-
-      def status_class
-        status == "neutral" ? nil : status
       end
 
       def underlined_class
