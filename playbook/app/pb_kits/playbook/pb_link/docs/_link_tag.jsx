67f36d2039af6ce22d267564267eb808b59d252d
Gary Kang <gary.kang@powerhrg.com>
2024-10-25 08:49:45 -0400

    Add props to tag doc examples

--- playbook/app/pb_kits/playbook/pb_link/docs/_link_tag.jsx
+++ playbook/app/pb_kits/playbook/pb_link/docs/_link_tag.jsx
@@ -5,11 +5,20 @@ const LinkTag = (props) => (
   <div>
     <Link
         href="https://www.google.com/"
+        icon="arrow-up-right-from-square"
         tag="h1"
         text="h1 link example"
         {...props}
     />
     <Link
+        href="https://www.google.com/"
+        tag="h3"
+        text="h3 link example"
+        underline
+        {...props}
+    />
+    <Link
+        color="destructive"
         href="https://www.google.com/"
         tag="h6"
         text="h6 link example"
@@ -17,6 +26,7 @@ const LinkTag = (props) => (
     />
     <Link
         href="https://www.google.com/"
+        iconRight="chevron-right"
         tag="p"
         text="p link example"
         {...props}
