0185b4cbcd1ffb70d283c5a03eaf9c0db817975a
nida.ghuman <nidaqg@gmail.com>
2024-10-31 14:02:53 -0400

    Fixed issue with drag item becoming drag container

--- playbook/app/pb_kits/playbook/pb_draggable/index.js
+++ playbook/app/pb_kits/playbook/pb_draggable/index.js
@@ -1,55 +1,68 @@
 import PbEnhancedElement from "../pb_enhanced_element";
 
 const DRAGGABLE_SELECTOR = "[data-pb-draggable]";
+const DRAGGABLE_CONTAINER = ".pb_draggable_container";
 
-export default class PbDragabble extends PbEnhancedElement {
+export default class PbDraggable extends PbEnhancedElement {
   static get selector() {
     return DRAGGABLE_SELECTOR;
   }
 
   connect() {
-    this.bindEventListeners();
+    document.addEventListener("DOMContentLoaded", () => this.bindEventListeners());
   }
 
   bindEventListeners() {
-    // Event listeners for all draggable items and containers
     this.element.querySelectorAll(".pb_draggable_item").forEach(item => {
       item.addEventListener("dragstart", this.handleDragStart.bind(this));
       item.addEventListener("dragend", this.handleDragEnd.bind(this));
+      item.addEventListener("dragenter", this.handleDragEnter.bind(this));
     });
 
-    this.element.querySelectorAll(".pb_draggable_container").forEach(container => {
+    const container = this.element.querySelector(DRAGGABLE_CONTAINER);
+    if (container) {
       container.addEventListener("dragover", this.handleDragOver.bind(this));
       container.addEventListener("drop", this.handleDrop.bind(this));
-    });
+    }
   }
 
+  handleDragEnter(event) {
+    if (event.target.id === this.draggedItemId) {
+      event.target.classList.add("is_dragging");
+    }
+  }
   handleDragStart(event) {
     this.draggedItemId = event.target.id;
-    event.target.classList.add("is-dragging");
+    event.target.classList.add("is_dragging");
   }
 
   handleDragOver(event) {
     event.preventDefault(); 
-    event.target.classList.add("active-container");
+    event.target.classList.add("active_container");
   }
 
   handleDrop(event) {
     event.preventDefault();
     const target = event.target;
-    const draggedElement = document.getElementById(this.draggedItemId);
-
-    if (target && draggedElement && target !== draggedElement) {
-      target.appendChild(draggedElement);
-      draggedElement.classList.remove("is-dragging");
+  
+    if (!target.classList.contains("pb_draggable_container")) {
+      return;
     }
-
-    target.classList.remove("active-container");
-    this.draggedItemId = null; 
+  
+    const draggedElement = document.getElementById(this.draggedItemId);
+  
+    if (draggedElement) {
+      target.appendChild(draggedElement);
+      draggedElement.classList.remove("is_dragging");
+    }
+  
+    target.classList.remove("active_container");
+    this.draggedItemId = null;
   }
+  
 
   handleDragEnd(event) {
-    event.target.classList.remove("is-dragging");
+    event.target.classList.remove("is_dragging");
     this.draggedItemId = null; 
   }
 }
