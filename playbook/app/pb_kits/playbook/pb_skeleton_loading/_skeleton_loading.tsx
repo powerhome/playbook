a050617c603c873d16a0a15f54029ecbf3d4aa2c
Elisa Shapiro <elisashapiro@gmail.com>
2024-11-04 13:32:03 -0500

    initial prop setup

--- playbook/app/pb_kits/playbook/pb_skeleton_loading/_skeleton_loading.tsx
+++ playbook/app/pb_kits/playbook/pb_skeleton_loading/_skeleton_loading.tsx
@@ -2,8 +2,8 @@
 import React from 'react'
 import classnames from 'classnames'
 import { buildAriaProps, buildCss, buildDataProps } from '../utilities/props'
-import { globalProps } from '../utilities/globalProps'
-// import { Sizes } from '../types'
+import { globalProps, GlobalProps } from '../utilities/globalProps'
+import { Sizes } from '../types'
 
 
 type SkeletonLoadingProps = {
@@ -11,41 +11,82 @@ type SkeletonLoadingProps = {
   className?: string,
   data?: { [key: string]: string },
   id?: string,
-  // height?: string,
-  // width?: string,
-  // borderRadius: string,
-  // gap?: Sizes | "none",
-  // stack?: number,
-  // color?: "default" | "light",
-}
+  height?: string,
+  width?: string,
+  borderRadius?: string,
+  gap?: Sizes | "none",
+  stack?: number,
+  color?: "default" | "light",
+  dark?: boolean,
+} & GlobalProps
 
-const SkeletonLoading = (props: SkeletonLoadingProps) => {
+const SkeletonLoading = (props: SkeletonLoadingProps): React.ReactElement => {
   const {
     aria = {},
     className,
     data = {},
     id,
-    // height = "16px",
-    // width = "100%",
-    // borderRadius = "sm",
-    // gap = "xxs",
-    // stack = "1",
-    // color = "default",
+    height = "16px",
+    width = "100%",
+    borderRadius = "sm",
+    gap = "xxs",
+    stack = "1",
+    color = "default",
+    dark = false,
   } = props
 
   const ariaProps = buildAriaProps(aria)
   const dataProps = buildDataProps(data)
-  const classes = classnames(buildCss('pb_skeleton_loading'), globalProps(props), className)
+  // const classes = classnames(buildCss('pb_skeleton_loading'), globalProps(props), className)
+  // let css = classnames(globalProps(props, { borderRadius }), className)
+  // const skeletonContainerCss = classnames('pb_skeleton_loading', globalProps(props), className)
+  const skeletonContainerCss =classnames(buildCss('pb_skeleton_loading'), globalProps(props), className)
+  const gapClass = gap !== 'none' ? `gap_${gap}` : ''
+  const innerSkeletonCss = classnames(`border_radius_${borderRadius}`,`color_${color}`, dark && 'dark', )
+  const innerSizeStyle = { height, width }
 
   return (
     <div
         {...ariaProps}
         {...dataProps}
-        className={classes}
+        // className={classes}
+        // className={css}
+        className={skeletonContainerCss}
         id={id}
+        // style={sizeStyle}
+        style={{ display: "flex", flexDirection: "column" }}
     >
       {className}
-      {"hello skeleton_loading tsx file"}
+      {/* {"hello skeleton_loading tsx file"} */}
+      {/* <div style={{height: "16px", width: "100%", backgroundColor: "red"}}> </div> */}
+      {/* <div style={{height, width, }}> </div> */}
+      {Array.from({ length: Number(stack) }).map((_, index) => (
+        <div
+            // className={gap !== 'none' ? gapClass : ''}
+            // className={index > 0 && stack > 1 && gap !== 'none' ? `gap_${gap}` : ''}
+            // className={classnames(`color_${color}`, `border_radius_${borderRadius}`, dark && 'dark', index > 0 && gapClass)}
+            className={classnames(innerSkeletonCss, index > 0 && gapClass)}
+            // className={classnames((gapClass && index > 0), innerSkeletonCss)}
+            // className={classnames({ [gapClass]: index > 0, innerSkeletonCss })}
+            key={index}
+            // style={sizeStyle}
+            style={innerSizeStyle}
+        />
+      ))}
+      {/* {Array.from({ length: Number(stack) }).map((_, index) => {
+        const classNames = classnames(
+          innerSkeletonCss,
+          { [gapClass]: index > 0 }
+        )
+
+        return (
+          <div
+            key={index}
+            style={innerSizeStyle}
+            className={classNames}
+          />
+        )
+      })} */}
     </div>
   )
 }
--- playbook/app/pb_kits/playbook/pb_skeleton_loading/docs/_skeleton_loading_default.jsx
+++ playbook/app/pb_kits/playbook/pb_skeleton_loading/docs/_skeleton_loading_default.jsx
@@ -4,8 +4,34 @@ import { SkeletonLoading } from "playbook-ui"
 
 const SkeletonLoadingDefault = (props) => (
   <div>
-    {"default doc ex"}
+    {/* {"default doc ex"} */}
+    <SkeletonLoading {...props} />
+    <br />
     <SkeletonLoading
+        borderRadius="rounded"
+        gap="xl"
+        height="100px"
+        stack="2"
+        width="50%"
+        {...props}
+    />
+    <br />
+    <SkeletonLoading
+        borderRadius="none"
+        color="light"
+        gap="md"
+        height="10px"
+        stack="3"
+        width="20%"
+        {...props}
+    />
+    <br />
+    <SkeletonLoading
+        borderRadius="md"
+        color="light"
+        gap="xl"
+        height="50px"
+        width="30%"
         {...props}
     />
 
