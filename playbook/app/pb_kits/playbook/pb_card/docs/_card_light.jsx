7c32c55a73da1b0525cea51c41eafe545bbce577
Jasper <jasperfurniss@gmail.com>
2024-11-05 15:02:51 -0500

    Working Examples

--- playbook/app/pb_kits/playbook/pb_card/docs/_card_light.jsx
+++ playbook/app/pb_kits/playbook/pb_card/docs/_card_light.jsx
@@ -1,26 +1,12 @@
-import React from "react"
-import Card from "../_card"
+import React from 'react'
+import Card from '../_card'
 
 const CardLight = (props) => {
   return (
     <div>
-      <Card
-          height="100%"
-          htmlOptions={{
-            style: {
-              width: "100%",
-              backgroundColor: "blue",
-            },
-            tabIndex: 7
-          }}
-          minHeight="189px"
-          {...props}
-      >
-        {"HELLO WORLD!!!"}
-      </Card>
+      <Card {...props}>{'Card content'}</Card>
     </div>
   )
 }
 
 export default CardLight
-
