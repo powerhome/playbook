50fc9f6a058a5281c634b342d23e1122b7dac83d
Elisa Shapiro <83474365+ElisaShapiro@users.noreply.github.com>
2024-11-05 08:59:22 -0500

    [PBNTR-576] Tooltip for Truncated Form Pills (#3856)
    **What does this PR do?** A clear and concise description with your
    runway ticket url.
    [PBNTR-576](https://runway.powerhrg.com/backlog_items/PBNTR-576) follows
    up [PBNTR-550](https://runway.powerhrg.com/backlog_items/PBNTR-550) by
    adding a Tooltip containing text or tag content in full of a truncated
    Form Pill.
    
    This PR does the following:
    
    - Adds a Tooltip to the text, tag, or avatar name component of a
    truncated Form Pill. The Tooltip contains the entirety of the text
    content (does not include avatar image or icon)
    - Expands upon PBNTR-550 by adding the truncate prop to Rails Form Pills
    as well - all Rails Typeaheads With Pills are react-rendered-rails, so
    this expands the behavior so React/Rails are consistent. (**Note**:
    there are no solo rails form pills in use in
    [Nitro](https://github.com/search?q=repo%3Apowerhome%2Fnitro-web+pb_rails%28%22form_pill&type=code)).
    For Rails Tooltips, an id must be included; if not included, the Form
    Pill will be truncated but no Tooltip will appear (this is not necessary
    for React, nor for react-rendered-rails Typeahead).
    - Updates the Truncated Text Rails and React doc examples to include
    several form pill variations (an avatar, an icon+text tag, and a text
    tag only) within a Card of a small width to demonstrate the
    truncation/tooltip in a non-Typeahead environment. Locally I tested many
    variations (including truncation on a small Form Pill, or one with
    customized color).
    
    **Screenshots:** Screenshots to visualize your addition/change
    React Truncated Text doc example screenshots showing Tooltip
    <img width="1497" alt="react doc ex typeahead"
    src="https://github.com/user-attachments/assets/89aa1fc6-0ebd-43af-b108-9b85c77b11b5">
    <img width="1491" alt="react doc ex formpill"
    src="https://github.com/user-attachments/assets/920ec0c5-107d-447d-9604-76664a99d3eb">
    
    Rails Truncated Text doc example screenshots showing Tooltip
    <img width="1388" alt="for PR rails updated typeahead doc ex"
    src="https://github.com/user-attachments/assets/cc40eca6-068b-4023-8760-1e4c45f09b0d">
    <img width="1395" alt="for PR rails updated formpill doc ex"
    src="https://github.com/user-attachments/assets/2a9a3837-70e5-4f18-8f7a-a7dc5886f193">
    
    
    
    **How to test?** Steps to confirm the desired behavior:
    1. Go to the truncated text doc example
    ([rails](https://pr3856.playbook.beta.px.powerapp.cloud/kits/form_pill/rails#truncated-text)/[react](https://pr3856.playbook.beta.px.powerapp.cloud/kits/form_pill/react#truncated-text)).
    2. Go to the Typeahead in the "Truncation Within Typeahead" section and
    select a name. Hover over the Form Pill that appears with their
    truncated name and observe the Tooltip in action displaying their full
    name.
    3. Hover over one/all of the truncated Form Pills in the "Form Pill
    Truncation" section. Observe the Tooltip in action. For the last Form
    Pill in the Rails doc ex, note that no Tooltip will appear because it
    does not have an id.
    
    
    #### Checklist:
    - [x] **LABELS** Add a label: `enhancement`, `bug`, `improvement`, `new
    kit`, `deprecated`, or `breaking`. See [Changelog &
    Labels](https://github.com/powerhome/playbook/wiki/Changelog-&-Labels)
    for details.
    - [x] **DEPLOY** I have added the `milano` label to show I'm ready for a
    review.
    ~~- [ ] **TESTS** I have added test coverage to my code.~~

--- playbook/app/pb_kits/playbook/pb_form_pill/docs/_form_pill_truncated_text.jsx
+++ playbook/app/pb_kits/playbook/pb_form_pill/docs/_form_pill_truncated_text.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Typeahead from '../../pb_typeahead/_typeahead'
+import { Card, Caption, FormPill, Typeahead } from 'playbook-ui'
 
 const names = [
   { label: 'Alexander Nathaniel Montgomery', value: 'Alexander Nathaniel Montgomery' },
@@ -15,11 +15,34 @@ const FormPillTruncatedText = (props) => {
       <Typeahead
           htmlOptions={{ style: { maxWidth: "240px" }}}
           isMulti
-          label="Names"
+          label="Truncation Within Typeahead"
           options={names}
           truncate={1}
           {...props}
       />
+      <Caption text="Form Pill Truncation"/>
+      <Card maxWidth="xs">
+        <FormPill
+            avatarUrl="https://randomuser.me/api/portraits/women/44.jpg"
+            name="Princess Amelia Mignonette Grimaldi Thermopolis Renaldo"
+            onClick={() => alert('Click!')}
+            tabIndex={0}
+            truncate={1}
+        />
+        <FormPill
+            icon="badge-check"
+            onClick={() => {alert('Click!')}}
+            tabIndex={0}
+            text="icon and a very long tag to show truncation"
+            truncate={1}
+        />
+        <FormPill
+            onClick={() => {alert('Click!')}}
+            tabIndex={0}
+            text="form pill with a very long tag to show truncation"
+            truncate={1}
+        />
+      </Card>
     </>
   )
 }
