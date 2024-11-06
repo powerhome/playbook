e6f83d554608df05ead26e57e22b17d259793946
Elisa Shapiro <elisashapiro@gmail.com>
2024-11-06 11:24:43 -0500

    add htmlOptions, remove default className in div, remove comment

--- playbook/app/pb_kits/playbook/pb_skeleton_loading/_skeleton_loading.tsx
+++ playbook/app/pb_kits/playbook/pb_skeleton_loading/_skeleton_loading.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react'
 import classnames from 'classnames'
-import { buildAriaProps, buildCss, buildDataProps } from '../utilities/props'
+import { buildAriaProps, buildCss, buildDataProps, buildHtmlProps } from '../utilities/props'
 import { globalProps, GlobalProps } from '../utilities/globalProps'
 import { Sizes } from '../types'
 
@@ -11,6 +11,7 @@ type SkeletonLoadingProps = {
   className?: string,
   data?: { [key: string]: string },
   id?: string,
+  htmlOptions?: {[key: string]: string | number | boolean | (() => void)},
   height?: string,
   width?: string,
   borderRadius?: string,
@@ -26,6 +27,7 @@ const SkeletonLoading = (props: SkeletonLoadingProps): React.ReactElement => {
     className,
     data = {},
     id,
+    htmlOptions = {},
     height = "16px",
     width = "100%",
     borderRadius = "sm",
@@ -37,6 +39,7 @@ const SkeletonLoading = (props: SkeletonLoadingProps): React.ReactElement => {
 
   const ariaProps = buildAriaProps(aria)
   const dataProps = buildDataProps(data)
+  const htmlProps = buildHtmlProps(htmlOptions)
   const skeletonContainerCss =classnames(buildCss('pb_skeleton_loading'), globalProps(props), className)
   const gapClass = gap !== 'none' ? `gap_${gap}` : ''
   const innerSkeletonCss = classnames(`border_radius_${borderRadius}`,`color_${color}`, dark && 'dark', )
@@ -46,10 +49,10 @@ const SkeletonLoading = (props: SkeletonLoadingProps): React.ReactElement => {
     <div
         {...ariaProps}
         {...dataProps}
+        {...htmlProps}
         className={skeletonContainerCss}
         id={id}
     >
-      {className}
       {Array.from({ length: Number(stack) }).map((_, index) => (
         <div
             className={classnames(buildCss('pb_skeleton_loading'), innerSkeletonCss, index > 0 && gapClass)}
