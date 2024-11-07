244697cbb9c2de700a3fc7422723415cec66f73c
Elisa Shapiro <elisashapiro@gmail.com>
2024-11-07 13:43:39 -0500

    accessible and correctly named typeahead label

--- playbook/lib/playbook/forms/builder/typeahead_field.rb
+++ playbook/lib/playbook/forms/builder/typeahead_field.rb
@@ -5,6 +5,7 @@ module Playbook
     class Builder
       def typeahead(name, _options = {}, _html_options = {}, props: {})
         props[:name] = name
+        props[:label] = props[:label] == true ? name.to_s.underscore.humanize : props[:label]
         @template.pb_rails("typeahead", props: props)
       end
     end
