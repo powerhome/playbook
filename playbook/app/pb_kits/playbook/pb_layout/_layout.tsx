f7c7c0524c010346c4a7d245b13213b6b32de508
Gary Kang <gary.kang@powerhrg.com>
2024-11-22 08:54:20 -0500

    Add global height props to Layout kit

--- playbook/app/pb_kits/playbook/pb_layout/_layout.tsx
+++ playbook/app/pb_kits/playbook/pb_layout/_layout.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import classnames from 'classnames'
 import { buildAriaProps, buildCss, buildDataProps, buildHtmlProps } from '../utilities/props'
 
-import { globalProps } from '../utilities/globalProps'
+import { globalProps, globalInlineProps } from '../utilities/globalProps'
 
 type LayoutPropTypes = {
   aria?: {[key: string]: string},
@@ -159,6 +159,8 @@ const Layout = (props: LayoutPropTypes) => {
   const filteredProps = {...props}
   delete filteredProps?.position
 
+  const dynamicInlineProps = globalInlineProps(props)
+
   return (
     <div
         {...ariaProps}
@@ -171,7 +173,8 @@ const Layout = (props: LayoutPropTypes) => {
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         //@ts-ignore
         globalProps(filteredProps)
-      )}
+        )}
+        style={dynamicInlineProps}
     >
       {subComponentTags('Side')}
       {nonSideChildren}
