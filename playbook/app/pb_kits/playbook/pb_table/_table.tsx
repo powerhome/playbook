3655c1ce532fbc3289bf05ddafbba1db22844186
Nick Amantia <92755007+nickamantia@users.noreply.github.com>
2024-11-26 11:46:08 -0500

    [PBNTR-661] Create a "stickyLeftcolumn" prop for the Table kit (#3913)
    [PBNTR-661](https://runway.powerhrg.com/backlog_items/PBNTR-661)
    
    Adds a `stickyLeftcolumn` prop. You need to pass in an array with
    strings of the columns you want sticky. Also you need to give ids to the
    columns you want sticky.
    
    <img width="814" alt="Screenshot 2024-11-18 at 11 46 23 AM"
    src="https://github.com/user-attachments/assets/5bcb8057-2762-4aff-883f-cc3f4238d99b">
    
    
    
    
    **How to test?** Steps to confirm the desired behavior:
    1. Go to the Table kit and look at the new doc examples
    
    
    #### Checklist:
    - [ ] **LABELS** Add a label: `enhancement`, `bug`, `improvement`, `new
    kit`, `deprecated`, or `breaking`. See [Changelog &
    Labels](https://github.com/powerhome/playbook/wiki/Changelog-&-Labels)
    for details.
    - [ ] **DEPLOY** I have added the `milano` label to show I'm ready for a
    review.
    - [ ] **TESTS** I have added test coverage to my code.

--- playbook/app/pb_kits/playbook/pb_table/_table.tsx
+++ playbook/app/pb_kits/playbook/pb_table/_table.tsx
@@ -4,11 +4,11 @@ import { buildAriaProps, buildDataProps, buildHtmlProps } from '../utilities/pro
 import { globalProps, GlobalProps } from '../utilities/globalProps'
 import PbTable from '.'
 import {
-  TableHead,
-  TableHeader,
-  TableBody,
-  TableRow,
-  TableCell,
+    TableHead,
+    TableHeader,
+    TableBody,
+    TableRow,
+    TableCell,
 } from "./subcomponents";
 
 type TableProps = {
@@ -28,6 +28,7 @@ type TableProps = {
     singleLine?: boolean,
     size?: "sm" | "md" | "lg",
     sticky?: boolean,
+    stickyLeftcolumn?: string[],
     striped?: boolean,
     tag?: "table" | "div",
     verticalBorder?: boolean,
@@ -51,6 +52,7 @@ const Table = (props: TableProps): React.ReactElement => {
         singleLine = false,
         size = 'sm',
         sticky = false,
+        stickyLeftcolumn = [],
         striped = false,
         tag = 'table',
         verticalBorder = false,
@@ -76,6 +78,7 @@ const Table = (props: TableProps): React.ReactElement => {
             'single-line': singleLine,
             'no-hover': disableHover,
             'sticky-header': sticky,
+            'sticky-left-column': stickyLeftcolumn,
             'striped': striped,
             [outerPaddingCss]: outerPadding !== '',
         },
@@ -86,32 +89,108 @@ const Table = (props: TableProps): React.ReactElement => {
     )
 
     useEffect(() => {
+        const handleStickyColumns = () => {
+            let accumulatedWidth = 0;
+    
+            stickyLeftcolumn.forEach((colId, index) => {
+                const isLastColumn = index === stickyLeftcolumn.length - 1;
+                const header = document.querySelector(`th[id="${colId}"]`);
+                const cells = document.querySelectorAll(`td[id="${colId}"]`);
+    
+                if (header) {
+                    header.classList.add('sticky');
+                    (header as HTMLElement).style.left = `${accumulatedWidth}px`;
+    
+                    if (!isLastColumn) {
+                        header.classList.add('with-border');
+                        header.classList.remove('sticky-shadow');
+                    } else {
+                        header.classList.remove('with-border');
+                        header.classList.add('sticky-shadow');
+                    }
+    
+                    accumulatedWidth += (header as HTMLElement).offsetWidth;
+                }
+    
+                cells.forEach((cell) => {
+                    cell.classList.add('sticky');
+                    (cell as HTMLElement).style.left = `${accumulatedWidth - (header as HTMLElement).offsetWidth}px`;
+                    
+                    if (!isLastColumn) {
+                        cell.classList.add('with-border');
+                        cell.classList.remove('sticky-shadow');
+                    } else {
+                        cell.classList.remove('with-border');
+                        cell.classList.add('sticky-shadow');
+                    }
+                });
+            });
+        };
+
+        setTimeout(() => {
+            handleStickyColumns();
+        }, 10);
+
+        window.addEventListener('resize', handleStickyColumns);
+
+        return () => {
+            window.removeEventListener('resize', handleStickyColumns);
+        };
+    }, [stickyLeftcolumn]);
+
+    useEffect(() => {
         const instance = new PbTable()
         instance.connect()
     }, [])
 
     return (
         <>
-            {isTableTag ? (
-                <table
-                    {...ariaProps}
-                    {...dataProps}
-                    {...htmlProps}
-                    className={classNames}
-                    id={id}
-                >
-                    {children}
-                </table>
-            ) : (
-                <div
-                    {...ariaProps}
-                    {...dataProps}
-                    {...htmlProps}
-                    className={classNames}
-                    id={id}
-                >
-                    {children}
+            {responsive === 'scroll' ? (
+                <div className='table-responsive-scroll'>
+                    {isTableTag ? (
+                        <table
+                            {...ariaProps}
+                            {...dataProps}
+                            {...htmlProps}
+                            className={classNames}
+                            id={id}
+                        >
+                            {children}
+                        </table>
+                    ) : (
+                        <div
+                            {...ariaProps}
+                            {...dataProps}
+                            {...htmlProps}
+                            className={classNames}
+                            id={id}
+                        >
+                            {children}
+                        </div>
+                    )}
                 </div>
+            ) : (
+                isTableTag ? (
+                    <table
+                        {...ariaProps}
+                        {...dataProps}
+                        {...htmlProps}
+                        className={classNames}
+                        id={id}
+                    >
+                        {children}
+                    </table>
+                ) : (
+                    <div
+                        {...ariaProps}
+                        {...dataProps}
+                        {...htmlProps}
+                        className={classNames}
+                        id={id}
+                    >
+                        {children}
+                    </div>
+                )
             )}
         </>
     )
