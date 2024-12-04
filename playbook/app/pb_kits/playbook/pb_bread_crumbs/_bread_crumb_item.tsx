dab552561c859e638a134a937d3c1fb54fc664f9
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-12-04 16:49:19 -0500

    Append bread crumb item children class

--- playbook/app/pb_kits/playbook/pb_bread_crumbs/_bread_crumb_item.tsx
+++ playbook/app/pb_kits/playbook/pb_bread_crumbs/_bread_crumb_item.tsx
@@ -1,55 +1,68 @@
-import React from 'react'
-import classnames from 'classnames'
-import { globalProps, domSafeProps } from '../utilities/globalProps'
+import React from "react"
+import classnames from "classnames"
+import { globalProps, domSafeProps } from "../utilities/globalProps"
 
 import {
   buildAriaProps,
   buildCss,
   buildDataProps,
-  buildHtmlProps } from '../utilities/props'
+  buildHtmlProps,
+} from "../utilities/props"
 
 type BreadCrumbItemProps = {
-  aria?: {[key: string]: string},
-  className?: string,
-  data?: {[key: string]: string},
-  htmlOptions?: {[key: string]: string | number | boolean | (() => void)},
-  id?: string,
-  component?: "a" | "span",
-  [x:string]: any;
+  aria?: { [key: string]: string }
+  children?: React.ReactNode
+  className?: string
+  data?: { [key: string]: string }
+  htmlOptions?: { [key: string]: string | number | boolean | (() => void) }
+  id?: string
+  component?: "a" | "span"
+  [x: string]: any
 }
 
 const BreadCrumbItem = (props: BreadCrumbItemProps): React.ReactElement => {
   const {
     aria = {},
+    children,
     className,
     data = {},
     htmlOptions = {},
     id,
-    component = 'a',
+    component = "a",
     ...rest
   } = props
   const ariaProps = buildAriaProps(aria)
   const dataProps = buildDataProps(data)
   const htmlProps = buildHtmlProps(htmlOptions)
-  const Component = component || 'span';
+  const Component = component || "span"
   const css = classnames(
-    buildCss('pb_bread_crumb_item_kit'),
+    buildCss("pb_bread_crumb_item_kit"),
     globalProps(props),
     className
   )
-  
+
+  const processedChildren = React.Children.map(children, (child) => {
+    if (React.isValidElement<{ className?: string }>(child)) {
+      return React.cloneElement(child, {
+        className: classnames(child.props.className, globalProps(props)),
+      })
+    }
+    return child
+  })
+
   return (
-    <div
-        {...ariaProps}
+    <div {...ariaProps}
         {...dataProps}
         {...htmlProps}
         className={css}
         id={id}
     >
       <Component
-          className="pb_bread_crumb_item"
+          className={classnames("pb_bread_crumb_item", globalProps(props))}
           {...domSafeProps(rest)}
-      />
+      >
+        {processedChildren}
+      </Component>
     </div>
   )
 }
