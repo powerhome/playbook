9e07f29390e1146ad3b2472db6059c9dac4ea2b3
Mark <mark@does.work>
2024-12-06 12:15:56 -0500

    Change react example

--- playbook/app/pb_kits/playbook/pb_bar_graph/docs/_bar_graph_custom.jsx
+++ playbook/app/pb_kits/playbook/pb_bar_graph/docs/_bar_graph_custom.jsx
@@ -3,20 +3,11 @@ import React from 'react'
 import BarGraph from '../_bar_graph'
 
 const chartData = [{
-  name: 'Installation',
-  data: [1475, 200, 3000, 654, 656],
+  name: 'Role',
+  data: [0, 200, 300, 654, 656],
 }, {
-  name: 'Manufacturing',
-  data: [4434, 524, 2320, 440, 500],
-}, {
-  name: 'Sales & Distribution',
-  data: [3387, 743, 1344, 434, 440],
-}, {
-  name: 'Project Development',
-  data: [3227, 878, 999, 780, 1000],
-}, {
-  name: 'Other',
-  data: [1111, 677, 3245, 500, 200],
+  name: 'Company',
+  data: [150, 524, 320, 440, 500],
 }]
 
 const barGraphOptions = {
@@ -31,16 +22,16 @@ const barGraphOptions = {
           useHTML: true,
           formatter: function() {
               switch (this.value) {
-                  case 'Jan':
-                      return `<i class="far fa-apple-whole"></i> ${this.value}`
-                  case 'Feb':
-                      return `<i class="far fa-strawberry"></i> ${this.value}`
-                  case 'Mar':
-                      return `<i class="far fa-lemon"></i> ${this.value}`
-                  case 'Apr':
-                      return `<i class="far fa-pear"></i> ${this.value}`
-                  case 'May':
-                      return `<i class="far fa-peach"></i> ${this.value}`
+                  case 'Angry':
+                      return `<i style="color: red; font-size: 18px;" class="far fa-face-angry"></i> `
+                  case 'Frown':
+                      return `<i style="color: orange; font-size: 18px;" class="far fa-face-frown"></i>`
+                  case 'Meh':
+                      return `<i style="color: grey; font-size: 18px;" class="far fa-face-meh"></i>`
+                  case 'Smile':
+                      return `<i style="color: #63E6BE; font-size: 18px;" class="far fa-face-smile"></i>`
+                  case 'Laugh':
+                      return `<i style="color: green; font-size: 18px;" class="far fa-laugh-beam sucess"></i>`
                   default:
                       return ''
               }
@@ -52,13 +43,13 @@ const barGraphOptions = {
 const BarGraphCustom = (props) => (
   <div>
     <BarGraph
-        axisTitle="Number of Employees"
         chartData={chartData}
         customOptions={barGraphOptions}
         id="bar-custom"
+        legend
         subTitle="Subtitle to replace"
         title="Bar Graph with Custom Overrides"
-        xAxisCategories={['Jan', 'Feb', 'Mar', 'Apr', 'May']}
+        xAxisCategories={['Angry', 'Frown', 'Meh', 'Smile', 'Laugh']} 
         yAxisMin={0}
         {...props}
     />
