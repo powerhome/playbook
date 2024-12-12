0d8892020f0257cde68c9a46800103a98d833b8d
Sam Duncan <samkduncan@gmail.com>
2024-12-12 11:04:04 -0600

    Added console logs to check try and check data

--- playbook/app/pb_kits/playbook/pb_date_picker/date_picker_helper.ts
+++ playbook/app/pb_kits/playbook/pb_date_picker/date_picker_helper.ts
@@ -68,7 +68,7 @@ const datePickerHelper = (config: DatePickerConfig, scrollContainer: string | HT
     timeFormat = 'at h:i K',
     yearRange,
   } = config
-
+  console.log("1 " + config);
   // ===========================================================
   // |                   Hook Definitions                      |
   // ===========================================================
@@ -148,7 +148,9 @@ const datePickerHelper = (config: DatePickerConfig, scrollContainer: string | HT
 
     // time selection
     if (enableTime) pluginList.push(timeSelectPlugin({ caption: timeCaption, showTimezone: showTimezone}))
-
+    console.log("2 " + thisRangesEndToday);
+    console.log("3 " + customQuickPickDates);
+    console.log("4 " + pluginList);
     return pluginList
   }
 
@@ -220,6 +222,7 @@ const datePickerHelper = (config: DatePickerConfig, scrollContainer: string | HT
   // Assign dynamically sourced flatpickr instance to variable
   const picker = document.querySelector<HTMLElement & { [x: string]: any }>(`#${pickerId}`)._flatpickr
   picker.innerContainer.parentElement.id = `cal-${pickerId}`
+  console.log("5 " + picker);
 
   // replace year selector with dropdown
   picker.yearElements[0].parentElement.innerHTML = `<select class="numInput cur-year" type="number" tabIndex="-1" aria-label="Year" id="year-${pickerId}"></select>`
@@ -251,7 +254,7 @@ const datePickerHelper = (config: DatePickerConfig, scrollContainer: string | HT
         picker.monthsDropdownContainer.value = picker.currentMonth
 
         /* Reset date picker to default value on form.reset() */
-        if (defaultDate && !picker.input.value){
+        if (defaultDate){
           picker.setDate(defaultDate)
           yearChangeHook(picker)
         }
