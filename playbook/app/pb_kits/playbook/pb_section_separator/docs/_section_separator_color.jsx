f5c686db6f13ca886b402c2f776e8a0d43517e56
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-12-19 10:40:16 -0500

    Update react color example for separator

--- playbook/app/pb_kits/playbook/pb_section_separator/docs/_section_separator_color.jsx
+++ playbook/app/pb_kits/playbook/pb_section_separator/docs/_section_separator_color.jsx
@@ -1,28 +1,42 @@
-import React from 'react'
-import { SectionSeparator } from 'playbook-ui'
+import React from "react"
+import { SectionSeparator, Flex, Detail, Icon } from "playbook-ui"
+
+const children = (
+  <Flex padding="xs">
+    <Icon color="primary"
+        icon="arrow-down"
+    />
+    <Detail
+        color="link"
+        size="sm"
+        text="Children"
+    />
+  </Flex>
+)
 
 const SectionSeparatorColor = (props) => {
   return (
     <div>
-      <SectionSeparator
-          text="Default Color"
+      <SectionSeparator text="Default Separator"
           {...props}
       />
-       <SectionSeparator
-           color="$primary"
-           text="Color"
+      <SectionSeparator color="primary"
+          text="Primary Separator"
+          {...props}
+      />
+      <SectionSeparator
+          color="primary"
+          lineStyle="dashed"
+          text="Primary Dashed Separator"
+          {...props}
+      />
+       <SectionSeparator color="primary"
            {...props}
-      />
-      <SectionSeparator
-          color="$primary"
-          lineStyle='dashed'
-          text="Dashed Color"
-          {...props}
-
-        />
+       >
+        {children}
+      </SectionSeparator>
     </div>
   )
 }
 
 export default SectionSeparatorColor
-
