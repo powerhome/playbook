7886616d8cacaa9fc1ab16e139ed83a4c94c4496
Carlos Lima <carloslimasd@gmail.com>
2024-12-19 09:33:01 -0300

    Adding Collapsible Trail to Rails Advanced Table

--- playbook/app/pb_kits/playbook/pb_advanced_table/table_body.rb
+++ playbook/app/pb_kits/playbook/pb_advanced_table/table_body.rb
@@ -14,6 +14,8 @@ module Playbook
                                      default: "header"
       prop :subrow_headers, type: Playbook::Props::Array,
                             default: []
+      prop :collapsible_trail, type: Playbook::Props::Boolean,
+                               default: true
 
       def render_row_and_children(row, column_definitions, current_depth, first_parent_child)
         output = ActiveSupport::SafeBuffer.new
@@ -21,7 +23,7 @@ module Playbook
 
         output << pb_rails("advanced_table/table_subrow_header", props: { row: row, column_definitions: column_definitions, depth: current_depth, subrow_header: subrow_headers[current_depth - 1] }) if is_first_child_of_subrow && enable_toggle_expansion == "all"
 
-        output << pb_rails("advanced_table/table_row", props: { id: id, row: row, column_definitions: column_definitions, depth: current_depth })
+        output << pb_rails("advanced_table/table_row", props: { id: id, row: row, column_definitions: column_definitions, depth: current_depth, collapsible_trail: collapsible_trail })
 
         if row[:children].present?
           output << content_tag(:div, class: "toggle-content", data: { advanced_table_content: row.object_id.to_s + id }) do
