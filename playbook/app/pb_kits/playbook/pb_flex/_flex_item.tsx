21de9a9b94678661d6e21997a3d5f808a518d7d3
Liam Simmons <liam.simmons@powerhrg.com>
2024-12-30 12:33:34 -0500

    Add gap to _flex_item.tsx

--- playbook/app/pb_kits/playbook/pb_flex/_flex_item.tsx
+++ playbook/app/pb_kits/playbook/pb_flex/_flex_item.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import classnames from 'classnames'
 import { buildCss, buildHtmlProps } from '../utilities/props'
-import { globalProps, GlobalProps, globalInlineProps} from '../utilities/globalProps'
+import { globalProps, GlobalProps, globalInlineProps } from '../utilities/globalProps'
+
 type FlexItemPropTypes = {
   children: React.ReactNode[] | React.ReactNode,
   fixedSize?: string,
@@ -11,7 +12,8 @@ type FlexItemPropTypes = {
   className?: string,
   order?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 'first' | 'none',
   alignSelf?: "start" | "end" | "center" | "stretch" | null,
-  displayFlex?: boolean
+  displayFlex?: boolean,
+  gap?: "space_xxs" | "space_xs" | "space_sm" | "space_md" | "space_lg" | "space_xl" | "none"
 } & GlobalProps
 
 const FlexItem = (props: FlexItemPropTypes): React.ReactElement => {
@@ -25,29 +27,46 @@ const FlexItem = (props: FlexItemPropTypes): React.ReactElement => {
     flex = 'none',
     order = 'none',
     alignSelf,
-    displayFlex
+    displayFlex,
+    gap = 'none',
   } = props
+
   const growClass = grow === true ? 'grow' : ''
   const displayFlexClass = displayFlex === true ? `display_flex_${displayFlex}` : ''
   const flexClass = flex !== 'none' ? `flex_${flex}` : ''
   const shrinkClass = shrink === true ? 'shrink' : ''
   const alignSelfClass = alignSelf ? `align_self_${alignSelf}` : ''
+  const gapClass = gap !== 'none' ? `gap_size_${gap}` : ''
   const fixedStyle =
     fixedSize !== undefined ? { flexBasis: `${fixedSize}` } : null
   const orderClass = order !== 'none' ? `order_${order}` : null
+
+  // Combine dynamic inline styles
   const dynamicInlineProps = globalInlineProps(props)
   const combinedStyles = {
     ...fixedStyle,
-    ...dynamicInlineProps
+    ...dynamicInlineProps,
   }
 
   const htmlProps = buildHtmlProps(htmlOptions)
 
-
   return (
     <div
         {...htmlProps}
-        className={classnames(buildCss('pb_flex_item_kit', growClass, shrinkClass, flexClass, displayFlexClass), orderClass, alignSelfClass, globalProps(props), className)}
+        className={classnames(
+        buildCss(
+          'pb_flex_item_kit',
+          growClass,
+          shrinkClass,
+          flexClass,
+          displayFlexClass,
+          gapClass,
+        ),
+        orderClass,
+        alignSelfClass,
+        globalProps(props),
+        className,
+      )}
         style={combinedStyles}
     >
       {children}
