e07c62ef38851aace0042f5750c06791e4fddb8e
Thomas Martin <thomas.j.martin@powerhrg.com>
2025-01-06 13:40:28 -0500

    Fix eslint warnings in Control file try 2

--- playbook/app/pb_kits/playbook/pb_typeahead/components/Control.tsx
+++ playbook/app/pb_kits/playbook/pb_typeahead/components/Control.tsx
@@ -5,16 +5,19 @@ import Flex from '../../pb_flex/_flex'
 import TextInput from '../../pb_text_input/_text_input'
 
 type Props = {
-  selectProps: any,
+  selectProps: {
+    dark?: boolean,
+    label: string,
+    error?: string,
+  },
 }
 
-const TypeaheadControl = (props: Props): JSX.Element => (
+const TypeaheadControl = (props: Props): React.ReactElement => (
   <div className="pb_typeahead_wrapper">
     <TextInput
         dark={props.selectProps.dark}
         error={props.selectProps.error}
         label={props.selectProps.label}
-        marginBottom="none"
     >
       <Flex>
         <components.Control
