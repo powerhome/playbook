e2b041158d3e962d9af9ecd91b6b82cccfe6e63e
Thomas Martin <44681574+Tomm1128@users.noreply.github.com>
2025-01-10 10:15:26 -0500

    [PLAY-1465]  Fix Linting Errors - Typeahead Kit (#4091)
    **What does this PR do?** A clear and concise description with your
    runway ticket url.
    This story fixes the typeahead kit linting errors. 
    
    Ran yarn eslint --fix for each file other than the index file and none
    of the warnings were corrected. I believe them to be unsafe changes and
    will require manual intervention.
    
    **Screenshots:** Screenshots to visualize your addition/change
    
    **How to test?** Steps to confirm the desired behavior:
    1. Go to 'Typeahead kit page
    2. Should still work as expected
    
    
    #### Checklist:
    - [x] **LABELS** Add a label: `enhancement`, `bug`, `improvement`, `new
    kit`, `deprecated`, or `breaking`. See [Changelog &
    Labels](https://github.com/powerhome/playbook/wiki/Changelog-&-Labels)
    for details.
    - [ ] **DEPLOY** I have added the `milano` label to show I'm ready for a
    review.
    - [ ] **TESTS** I have added test coverage to my code.

--- playbook/app/pb_kits/playbook/pb_typeahead/components/ClearIndicator.tsx
+++ playbook/app/pb_kits/playbook/pb_typeahead/components/ClearIndicator.tsx
@@ -1,16 +1,24 @@
 import React, { useEffect } from 'react'
 import { components } from 'react-select'
 
-const ClearContainer = (props: any) => {
+type ClearContainerProps = {
+  children: React.ReactNode,
+  selectProps?: {
+    id: string,
+  },
+  clearValue: () => void,
+}
+
+const ClearContainer = (props: ClearContainerProps): React.ReactElement => {
   const { selectProps, clearValue } = props
   useEffect(() => {
     document.addEventListener(`pb-typeahead-kit-${selectProps.id}:clear`, clearValue)
-  }, [true])
+  }, [clearValue, selectProps.id])
 
   return (
     <components.ClearIndicator
-      className="clear_indicator"
-      {...props}
+        className="clear_indicator"
+        {...props}
     />
   )
 }
--- playbook/app/pb_kits/playbook/pb_typeahead/components/Placeholder.tsx
+++ playbook/app/pb_kits/playbook/pb_typeahead/components/Placeholder.tsx
@@ -4,19 +4,26 @@ import { components } from 'react-select'
 import Flex from '../../pb_flex/_flex'
 import Icon from '../../pb_icon/_icon'
 
-const Placeholder = (props: any) => (
+type PlaceholderProps = {
+  children: React.ReactNode,
+  selectProps: {
+    plusIcon?: boolean,
+  },
+}
+
+const Placeholder = (props: PlaceholderProps): React.ReactElement => (
   <>
     <Flex
-      align="center"
-      className="placeholder"
+        align="center"
+        className="placeholder"
     >
       <components.IndicatorsContainer
-        {...props}
+          {...props}
       />
       {props.selectProps.plusIcon &&
         <Icon
-          className="typeahead-plus-icon"
-          icon="plus"
+            className="typeahead-plus-icon"
+            icon="plus"
         />
       }
     </Flex>
