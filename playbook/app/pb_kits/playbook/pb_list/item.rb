4d198c67eb1c7dcc5d25364c6cd91f20e56e496a
Sam Duncan <samkduncan@gmail.com>
2025-01-16 10:51:03 -0600

    Simplified draggable container for lists+items

--- playbook/app/pb_kits/playbook/pb_list/item.rb
+++ playbook/app/pb_kits/playbook/pb_list/item.rb
@@ -3,10 +3,17 @@
 module Playbook
   module PbList
     class Item < Playbook::KitBase
+      prop :drag_handle, type: Playbook::Props::Boolean,
+                         default: true
+      prop :drag_id, type: Playbook::Props::String
       prop :tabindex
 
       def classname
         generate_classname("pb_item_kit")
+      end
+
+      def draggable?
+        drag_id.present?
       end
     end
   end
