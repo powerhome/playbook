011dc8c1ded7a0f999da5eb36a628eeb04aedeed
markdoeswork <mark@does.work>
2025-01-20 15:34:42 -0500

    Generate beta kit

---
+++ playbook/app/pb_kits/playbook/pb_icon_button/icon_button.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module Playbook
+  module PbIconButton
+    class IconButton < ::Playbook::KitBase
+    end
+  end
+end
---
+++ playbook/spec/pb_kits/playbook/kits/icon_button_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative "../../../../app/pb_kits/playbook/pb_icon_button/icon_button"
+
+RSpec.describe Playbook::PbIcon_button::Icon_button do
+  subject { Playbook::PbIcon_button::Icon_button }
+
+  it { is_expected.to define_partial }
+
+  # Do not leave this file blank. Use other spec files for example tests.
+end
