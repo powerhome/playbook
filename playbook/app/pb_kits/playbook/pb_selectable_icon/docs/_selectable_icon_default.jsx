34c3c8817ed5a714f9e585e8f1441ae5bac0eeb2
Gabriella Massaro <gmassaro807@gmail.com>
2025-01-21 09:19:33 -0500

    Don't destructure props

--- playbook/app/pb_kits/playbook/pb_selectable_icon/docs/_selectable_icon_default.jsx
+++ playbook/app/pb_kits/playbook/pb_selectable_icon/docs/_selectable_icon_default.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import SelectableIcon from '../_selectable_icon'
 
-const SelectableIconDefault = ({ dark } ) => {
+const SelectableIconDefault = (props) => {
   const [ checkSelected, toggleSelected ] = useState(true)
   const [ checkUnselected, toggleUnselected ] = useState(false)
   const [ checkDisabled, toggleDisabled ] = useState(false)
@@ -11,30 +11,30 @@ const SelectableIconDefault = ({ dark } ) => {
     <div className="pb--doc-demo-row">
       <SelectableIcon
           checked={checkSelected}
-          dark={dark}
           icon="dollar-sign"
           inputId={10}
           onChange={() => toggleSelected(!checkSelected)}
           text="US Dollar"
+          {...props}
       />
 
       <SelectableIcon
           checked={checkUnselected}
-          dark={dark}
           icon="euro-sign"
           inputId={11}
           onChange={() => toggleUnselected(!checkUnselected)}
           text="Euro"
+          {...props}
       />
 
       <SelectableIcon
           checked={checkDisabled}
-          dark={dark}
           disabled
           icon="yen-sign"
           inputId={12}
           onChange={() => toggleDisabled(!checkDisabled)}
           text="Yen"
+          {...props}
       />
     </div>
   )
