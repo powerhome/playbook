760edbed57f8091db463bd74865deb2f815d1267
Gabriella Massaro <gmassaro807@gmail.com>
2025-01-21 14:10:40 -0500

    Update example react table with tooltip from csd example

--- playbook/app/pb_kits/playbook/pb_table/docs/_table_test_tool_tip.jsx
+++ playbook/app/pb_kits/playbook/pb_table/docs/_table_test_tool_tip.jsx
@@ -1,47 +1,95 @@
 import React from 'react'
 
-import Table from '../_table'
+import { Table, Tooltip, Icon } from "playbook-ui";
 
-const TableTestToolTip = (props) => {
+const TableTestToolTip = () => {
   return (
-    <Table
-        size="sm"
-        {...props}
-    >
-      <thead>
-        <tr>
-          <th>{'Column 1'}</th>
-          <th>{'Column 2'}</th>
-          <th>{'Column 3'}</th>
-          <th>{'Column 4'}</th>
-          <th>{'Column 5'}</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>{'Value 1'}</td>
-          <td>{'Value 2'}</td>
-          <td>{'Value 3'}</td>
-          <td>{'Value 4'}</td>
-          <td>{'Value 5'}</td>
-        </tr>
-        <tr>
-          <td>{'Value 1'}</td>
-          <td>{'Value 2'}</td>
-          <td>{'Value 3'}</td>
-          <td>{'Value 4'}</td>
-          <td>{'Value 5'}</td>
-        </tr>
-        <tr>
-          <td>{'Value 1'}</td>
-          <td>{'Value 2'}</td>
-          <td>{'Value 3'}</td>
-          <td>{'Value 4'}</td>
-          <td>{'Value 5'}</td>
-        </tr>
-      </tbody>
-    </Table>
-  )
-}
+    <Table size="sm">
+        <Table.Head>
+          <Table.Row>
+            <Table.Header>{"Column 1"}</Table.Header>
+            <Table.Header>{"Column 2"}</Table.Header>
+            <Table.Header>{"Column 3"}</Table.Header>
+            <Table.Header>{"Column 4"}</Table.Header>
+            <Table.Header>{"Column 5"}</Table.Header>
+          </Table.Row>
+        </Table.Head>
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>
+              {"Test Right Placement"}
+              <Tooltip
+                  placement={"right"}
+                  text={`Hello this is a tooltip hello hi hey HI wahts up everyone`}
+                  zIndex={10}
+              >
+                <Icon
+                    color={"primary"}
+                    icon={"info-circle"}
+                    paddingLeft={"xs"}
+                />
+              </Tooltip>
+            </Table.Cell>
+            <Table.Cell>
+              {"Test Top Placement"}
+              <Tooltip
+                  placement={"top"}
+                  text={`Hello this is a tooltip hello hi hey HI wahts up everyone`}
+                  zIndex={10}
+              >
+                <Icon
+                    color={"primary"}
+                    icon={"info-circle"}
+                    paddingLeft={"xs"}
+                />
+              </Tooltip>
+            </Table.Cell>
+            <Table.Cell>
+              {"Test Bottom Placement"}
+              <Tooltip
+                  placement={"bottom"}
+                  text={`Hello this is a tooltip hello hi hey HI wahts up everyone`}
+                  zIndex={10}
+              >
+                <Icon
+                    color={"primary"}
+                    icon={"info-circle"}
+                    paddingLeft={"xs"}
+                />
+              </Tooltip>
+            </Table.Cell>
+            <Table.Cell>
+              {"Test Left Placement"}
+              <Tooltip
+                  placement={"left"}
+                  text={`Hello this is a tooltip hello hi hey HI wahts up everyone`}
+                  zIndex={10}
+              >
+                <Icon
+                    color={"primary"}
+                    icon={"info-circle"}
+                    paddingLeft={"xs"}
+                />
+              </Tooltip>
+            </Table.Cell>
+            <Table.Cell>{"Value 5"}</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>{"Value 1"}</Table.Cell>
+            <Table.Cell>{"Value 2"}</Table.Cell>
+            <Table.Cell>{"Value 3"}</Table.Cell>
+            <Table.Cell>{"Value 4"}</Table.Cell>
+            <Table.Cell>{"Value 5"}</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>{"Value 1"}</Table.Cell>
+            <Table.Cell>{"Value 2"}</Table.Cell>
+            <Table.Cell>{"Value 3"}</Table.Cell>
+            <Table.Cell>{"Value 4"}</Table.Cell>
+            <Table.Cell>{"Value 5"}</Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+)}
 
 export default TableTestToolTip
