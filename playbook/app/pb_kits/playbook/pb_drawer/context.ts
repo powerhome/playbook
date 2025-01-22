bdc9f59b7e0314e50dffa7c4f852e8bce1211926
Jasper <jasperfurniss@gmail.com>
2025-01-22 16:21:01 -0500

    Docs Updated

--- playbook/app/pb_kits/playbook/pb_drawer/context.ts
+++ playbook/app/pb_kits/playbook/pb_drawer/context.ts
@@ -1,15 +1,11 @@
 import React from 'react'
 
-// No-op function that explicitly does nothing
-const noop = () => {
-  /* intentionally empty */
-}
+const noop = (): void => void 0
 
 type DrawerContextType = {
   onClose: () => void
 }
 
 export const DrawerContext = React.createContext<DrawerContextType>({
-  // Default no-op function - will be overridden by Drawer component
   onClose: noop,
 })
