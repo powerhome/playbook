175c9b759b0cb1fafa618b87fcc1192076763877
markdoeswork <mark@does.work>
2025-01-23 15:39:33 -0500

    Run generator

---
+++ playbook/spec/pb_kits/playbook/kits/copy_button_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative "../../../../app/pb_kits/playbook/pb_copy_button/copy_button"
+
+RSpec.describe Playbook::PbCopy_button::Copy_button do
+  subject { Playbook::PbCopy_button::Copy_button }
+
+  it { is_expected.to define_partial }
+
+  # Do not leave this file blank. Use other spec files for example tests.
+end
