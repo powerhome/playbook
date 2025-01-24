181b535422a739a33faca285b604b6afff5c27b9
Elisa Shapiro <elisashapiro@gmail.com>
2025-01-24 14:15:51 -0500

    Add rails autoclose doc example

---
+++ playbook/app/pb_kits/playbook/pb_fixed_confirmation_toast/docs/_fixed_confirmation_toast_auto_close_rails.md
@@ -0,0 +1,3 @@
+Auto close is used when you want the confirmation toast to close automatically after a certain time. `auto_close` property will be a delay number in ms.
+
+Script tag in code snippet is for demonstration purposes only.
--- playbook/app/pb_kits/playbook/pb_fixed_confirmation_toast/index.js
+++ playbook/app/pb_kits/playbook/pb_fixed_confirmation_toast/index.js
@@ -2,34 +2,45 @@ import PbEnhancedElement from '../pb_enhanced_element'
 
 export default class PbFixedConfirmationToast extends PbEnhancedElement {
   static get selector() {
-    return '.remove_toast'
+    return '.auto_close, .remove_toast'
   }
 
   connect() {
     this.self = this.element
     this.autoCloseToast(this.self)
 
-    this.self.addEventListener('click', () => {
-      this.removeToast(this.self)
-    })
+    if (this.self.classList.contains('remove_toast')) {
+      this.self.addEventListener('click', () => {
+        this.removeToast(this.self)
+      })
+    }
   }
 
   removeToast(elem) {
+    if (elem.autoCloseTimeout) {
+      clearTimeout(elem.autoCloseTimeout)
+      elem.autoCloseTimeout = null
+    }
+
     elem.parentNode.removeChild(elem)
   }
 
   autoCloseToast(element) {
-    const classListValues = element.classList.value
-    const hasAutoCloseClass = classListValues.includes('auto_close')
+    const hasAutoCloseClass = element.classList.contains('auto_close')
 
     if (hasAutoCloseClass) {
-      const classList = classListValues.split(' ')
-      const autoCloseValue = classList[classList.length - 1].split('_')[2]
-      const autoCloseIntValue = parseInt(autoCloseValue)
+      const autoCloseClass = Array.from(element.classList).find(cls => cls.startsWith('auto_close_'))
+      if (autoCloseClass) {
+        const duration = parseInt(autoCloseClass.split('_')[2])
 
-      setTimeout(() => {
-        this.removeToast(element)
-      }, autoCloseIntValue)
+        if (element.autoCloseTimeout) {
+          clearTimeout(element.autoCloseTimeout)
+        }
+
+        element.autoCloseTimeout = setTimeout(() => {
+          this.removeToast(element)
+        }, duration)
+      }
     }
   }
 }
