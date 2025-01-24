533ff2f35fd204574fc4f74ba77c82bd5f3411ff
markdoeswork <mark@does.work>
2025-01-24 13:06:18 -0500

    Add tooltip to copy button

--- playbook/app/pb_kits/playbook/pb_copy_button/copy_button.rb
+++ playbook/app/pb_kits/playbook/pb_copy_button/copy_button.rb
@@ -3,6 +3,18 @@
 module Playbook
   module PbCopyButton
     class CopyButton < ::Playbook::KitBase
+      prop :from
+      prop :text
+      prop :tooltip_position,
+           type: Playbook::Props::Enum,
+           values: %w[top right bottom left],
+           default: "top"
+      prop :tooltip_text
+      prop :value
+
+      def classname
+        generate_classname("pb_copy_button")
+      end
     end
   end
 end
