6f24ffd23f9a89f8488580ef70684be64a05c152
Elisa Shapiro <elisashapiro@gmail.com>
2025-01-29 14:46:26 -0500

    Simpler refactor of PbEnhancedElement

--- playbook/app/pb_kits/playbook/pb_fixed_confirmation_toast/index.js
+++ playbook/app/pb_kits/playbook/pb_fixed_confirmation_toast/index.js
@@ -1,24 +1,15 @@
 import PbEnhancedElement from '../pb_enhanced_element'
 
-const TOAST_WRAPPER_SELECTOR = '[data-pb-toast-wrapper]'
-const TOAST_AUTO_CLOSE_CLASS = 'auto_close'
-const TOAST_REMOVE_CLASS = 'remove_toast'
-
 export default class PbFixedConfirmationToast extends PbEnhancedElement {
   static get selector() {
-    return `${TOAST_WRAPPER_SELECTOR}, .${TOAST_AUTO_CLOSE_CLASS}, .${TOAST_REMOVE_CLASS}`
+    return '.auto_close, .remove_toast'
   }
 
   connect() {
     this.self = this.element
+    this.autoCloseToast(this.self)
 
-    if (this.self.classList.contains(TOAST_AUTO_CLOSE_CLASS) || 
-        this.self.hasAttribute('data-pb-auto-close')) {
-      this.autoCloseToast(this.self)
-    }
-
-    if (this.self.classList.contains(TOAST_REMOVE_CLASS) || 
-        this.self.hasAttribute('data-pb-remove-toast')) {
+    if (this.self.classList.contains('remove_toast')) {
       this.self.addEventListener('click', () => {
         this.removeToast(this.self)
       })
@@ -35,35 +26,21 @@ export default class PbFixedConfirmationToast extends PbEnhancedElement {
   }
 
   autoCloseToast(element) {
-    let duration
+    const hasAutoCloseClass = element.classList.contains('auto_close')
 
-    if (element.classList.contains(TOAST_AUTO_CLOSE_CLASS)) {
-      const autoCloseClass = Array.from(element.classList)
-        .find(cls => cls.startsWith('auto_close_'))
-      
+    if (hasAutoCloseClass) {
+      const autoCloseClass = Array.from(element.classList).find(cls => cls.startsWith('auto_close_'))
       if (autoCloseClass) {
-        duration = parseInt(autoCloseClass.split('_')[2])
+        const duration = parseInt(autoCloseClass.split('_')[2])
+
+        if (element.autoCloseTimeout) {
+          clearTimeout(element.autoCloseTimeout)
+        }
+
+        element.autoCloseTimeout = setTimeout(() => {
+          this.removeToast(element)
+        }, duration)
       }
-    }
-
-    if (!duration) {
-      duration = parseInt(element.getAttribute('data-pb-auto-close'))
-    }
-
-    if (duration) {
-      if (element.autoCloseTimeout) {
-        clearTimeout(element.autoCloseTimeout)
-      }
-
-      element.autoCloseTimeout = setTimeout(() => {
-        this.removeToast(element)
-      }, duration)
-    }
-  }
-
-  disconnect() {
-    if (this.element.autoCloseTimeout) {
-      clearTimeout(this.element.autoCloseTimeout)
     }
   }
 }
