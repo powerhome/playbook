be0d21442a2795718e712e4696454e6296b26402
Gabriella Massaro <gmassaro807@gmail.com>
2025-02-03 14:50:07 -0500

    Change link to value, fixes broken links when searching

--- playbook-website/app/javascript/components/MainSidebar/MenuData/GuidelinesNavItems.ts
+++ playbook-website/app/javascript/components/MainSidebar/MenuData/GuidelinesNavItems.ts
@@ -1,98 +1,98 @@
 export const VisualGuidelinesItems = [
     {
         label: "Colors",
-        link: "/visual_guidelines/colors"
+        value: "/visual_guidelines/colors"
     },
     {
         label: "Width",
-        link: "/visual_guidelines/width"
+        value: "/visual_guidelines/width"
     },
     {
         label: "Min Width",
-        link: "/visual_guidelines/min_width"
+        value: "/visual_guidelines/min_width"
     },
     {
         label: "Max Width",
-        link: "/visual_guidelines/max_width"
+        value: "/visual_guidelines/max_width"
     },
     {
         label: "Height",
-        link: "/visual_guidelines/height"
+        value: "/visual_guidelines/height"
     },
     {
         label: "Min Height",
-        link: "/visual_guidelines/min_height"
+        value: "/visual_guidelines/min_height"
     },
     {
         label : "Max Height",
-        link: "/visual_guidelines/max_height"
+        value: "/visual_guidelines/max_height"
     },
     {
         label: "Position",
-        link: "/visual_guidelines/position"
+        value: "/visual_guidelines/position"
     },
     {
         label: "Vertical Align",
-        link: "/visual_guidelines/vertical_align"
+        value: "/visual_guidelines/vertical_align"
     },
     {
         label: "Z-Index",
-        link: "/visual_guidelines/z_index"
+        value: "/visual_guidelines/z_index"
     },
     {
         label: "Line Height",
-        link: "/visual_guidelines/line_height"
+        value: "/visual_guidelines/line_height"
     },
     {
         label: "Number Spacing",
-        link: "/visual_guidelines/number_spacing"
+        value: "/visual_guidelines/number_spacing"
     },
     {
         label: "Shadows",
-        link: "/visual_guidelines/shadows"
+        value: "/visual_guidelines/shadows"
     },
     {
         label: "Spacing",
-        link: "/visual_guidelines/spacing"
+        value: "/visual_guidelines/spacing"
     },
     {
         label: "Border Radius",
-        link: "/visual_guidelines/border_radius"
+        value: "/visual_guidelines/border_radius"
     },
     {
         label: "Typography",
-        link: "/visual_guidelines/typography"
+        value: "/visual_guidelines/typography"
     },
     {
         label: "Display",
-        link: "/visual_guidelines/display"
+        value: "/visual_guidelines/display"
     },
     {
         label: "Cursor",
-        link: "/visual_guidelines/cursor"
+        value: "/visual_guidelines/cursor"
     },
     {
         label: "Flex Box",
-        link: "/visual_guidelines/flex_box"
+        value: "/visual_guidelines/flex_box"
     },
     {
         label: "Hover",
-        link: "/visual_guidelines/hover"
+        value: "/visual_guidelines/hover"
     },
     {
         label: "Group Hover",
-        link: "/visual_guidelines/group_hover"
+        value: "/visual_guidelines/group_hover"
     },
     {
         label: "Text Align",
-        link: "/visual_guidelines/text_align"
+        value: "/visual_guidelines/text_align"
     },
     {
         label: "Overflow",
-        link: "/visual_guidelines/overflow"
+        value: "/visual_guidelines/overflow"
     },
     {
         label: "Truncate",
-        link: "/visual_guidelines/truncate"
+        value: "/visual_guidelines/truncate"
     },
   ];
