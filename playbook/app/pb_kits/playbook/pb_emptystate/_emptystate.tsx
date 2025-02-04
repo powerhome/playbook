70a235d47caa4970a5380d579c5a9c69eca6e26b
Gabriella Massaro <gmassaro807@gmail.com>
2025-02-04 13:24:47 -0500

    Working commit

--- playbook/app/pb_kits/playbook/pb_emptystate/_emptystate.tsx
+++ playbook/app/pb_kits/playbook/pb_emptystate/_emptystate.tsx
@@ -3,20 +3,39 @@ import React from 'react'
 import classnames from 'classnames'
 import { buildAriaProps, buildCss, buildDataProps } from '../utilities/props'
 import { globalProps } from '../utilities/globalProps'
+import Button from '../pb_button/_button'
 
 type EmptystateProps = {
   aria?: { [key: string]: string },
+  // alignment?: "center" | "left" | "right",
   className?: string,
   data?: { [key: string]: string },
+  // description?: string,
   id?: string,
+  // imageAlt?: string,
+  // imageUrl?: string,
+  // linkButton?: string,
+  // orientation?: "horizontal" | "vertical",
+  // primaryButton?: string,
+  // size?: "sm" | "md" | "lg",
+  text?: string,
 }
 
 const Emptystate = (props: EmptystateProps) => {
   const {
     aria = {},
-  className,
-  data = {},
-  id,
+    // alignment = 'center',
+    className,
+    data = {},
+    // description,
+    id,
+    // imageAlt = '',
+    // imageUrl,
+    // linkButton,
+    // orientation = 'horizontal',
+    // primaryButton,
+    // size = "md",
+    text = '',
   } = props
 
   const ariaProps = buildAriaProps(aria)
@@ -30,7 +49,9 @@ const Emptystate = (props: EmptystateProps) => {
         className={classes}
         id={id}
     >
-      {className}
+    <Button>
+      hello
+    </Button>
     </div>
   )
 }
