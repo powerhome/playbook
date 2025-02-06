d2103d2468387ec9e2a9e89f03ca269d2b2fa3ce
Gabriella Massaro <gmassaro807@gmail.com>
2025-02-06 15:26:58 -0500

    Create lg + vertical rails emptystate default example

--- playbook/app/pb_kits/playbook/pb_emptystate/emptystate.rb
+++ playbook/app/pb_kits/playbook/pb_emptystate/emptystate.rb
@@ -3,6 +3,22 @@
 module Playbook
   module PbEmptystate
     class Emptystate < ::Playbook::KitBase
+      prop :alignment, type: Playbook::Props::Enum, values: %w[center left right], default: "center"
+      prop :description, type: Playbook::Props::String
+      prop :header, type: Playbook::Props::String
+      prop :image, type: Playbook::Props::String
+      prop :link_button, type: Playbook::Props::String
+      prop :orientation, type: Playbook::Props::Enum, values: %w[horizontal vertical], default: "vertical"
+      prop :primary_button, type: Playbook::Props::String
+      prop :size, type: Playbook::Props::Enum, values: %w[sm md lg], default: "md"
+
+      def data
+        Hash(prop(:data)).merge(pb_emptystate: true)
+      end
+
+      def classname
+        generate_classname("pb_emptystate")
+      end
     end
   end
 end
