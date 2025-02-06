9c5933d8be7d174e90cc65405073636a40292801
Gabriella Massaro <gmassaro807@gmail.com>
2025-02-06 11:12:38 -0500

    Fix failing default test

--- playbook/app/pb_kits/playbook/pb_emptystate/emptystate.test.jsx
+++ playbook/app/pb_kits/playbook/pb_emptystate/emptystate.test.jsx
@@ -1,21 +1,21 @@
 import { ensureAccessible, renderKit } from '../utilities/test-utils'
 
-import { Emptystate } from '../'
+import { Emptystate } from 'playbook-ui'
 
 /* See these resources for more testing info:
   - https://github.com/testing-library/jest-dom#usage for useage and examples
   - https://jestjs.io/docs/en/using-matchers
 */
 
-test('generated scaffold test - update me', () => {
-  const props = {
-    data: { testid: 'default' }
-  }
+const props = {
+  data: { testid: 'default' }
+}
 
+test('generated scaffold test - update me', () => {
   const kit = renderKit(Emptystate , props)
   expect(kit).toBeInTheDocument()
 })
 
 it("should be accessible", async () => {
-  ensureAccessible(Emptystate)
+  ensureAccessible(Emptystate, props)
 })
