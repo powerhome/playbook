8fd7aba290abb4cc9b6844a2ea21e04df49ea32d
Elisa Shapiro <elisashapiro@gmail.com>
2025-02-11 16:56:56 -0500

    A basic fullscreen doc ex to start with

---
+++ playbook/app/pb_kits/playbook/pb_advanced_table/docs/_advanced_table_fullscreen.jsx
@@ -0,0 +1,60 @@
+import React from "react"
+import { AdvancedTable, Button } from "playbook-ui"
+import MOCK_DATA from "./advanced_table_mock_data.json"
+
+const AdvancedTableFullscreen = (props) => {
+  const columnDefinitions = [
+    {
+      accessor: "year",
+      label: "Year",
+      cellAccessors: ["quarter", "month", "day"],
+    },
+    {
+      accessor: "newEnrollments",
+      label: "New Enrollments",
+    },
+    {
+      accessor: "scheduledMeetings",
+      label: "Scheduled Meetings",
+    },
+    {
+      accessor: "attendanceRate",
+      label: "Attendance Rate",
+    },
+    {
+      accessor: "completedClasses",
+      label: "Completed Classes",
+    },
+    {
+      accessor: "classCompletionRate",
+      label: "Class Completion Rate",
+    },
+    {
+      accessor: "graduatedStudents",
+      label: "Graduated Students",
+    },
+  ]
+
+  return (
+    <div>
+      <Button 
+          fixedWidth
+          icon="expand"
+          marginBottom="sm"
+          tabIndex={0}
+          text="Fullscreen"
+          variant="secondary"
+      />
+      <AdvancedTable
+          columnDefinitions={columnDefinitions}
+          tableData={MOCK_DATA}
+          {...props}
+      >
+          <AdvancedTable.Header enableSorting />
+          <AdvancedTable.Body />
+      </AdvancedTable>
+    </div>
+  )
+}
+
+export default AdvancedTableFullscreen
