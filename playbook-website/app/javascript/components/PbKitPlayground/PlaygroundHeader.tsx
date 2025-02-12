d0e3250ff2dc9446dcfa3c706aaab0002dc36087
Cayla Horsey <cayla.horsey@powerhrg.com>
2025-02-12 10:10:16 -0500

    Add back button functionality

--- playbook-website/app/javascript/components/PbKitPlayground/PlaygroundHeader.tsx
+++ playbook-website/app/javascript/components/PbKitPlayground/PlaygroundHeader.tsx
@@ -2,12 +2,23 @@ import React from 'react'
 import { Icon, SectionSeparator, Button, Flex, FlexItem } from 'playbook-ui'
 
 const PlaygroundHeader = () => {
+  const handleBackNavigation = (event) => {
+    event.preventDefault();
+    if (window.history.length > 1) {
+      window.history.back();
+    } else {
+      window.location.href = "https://playbook.powerapp.cloud/";
+    }
+  };
+
   return (
     <>
       <Flex orientation="row" justify="between">
+
         <Flex orientation="row">
         <FlexItem>
             <button
+              onClick={handleBackNavigation}
               style={{ background: 'none', border: 'none', cursor: 'pointer' }}
               aria-label="Go Back"
             >
