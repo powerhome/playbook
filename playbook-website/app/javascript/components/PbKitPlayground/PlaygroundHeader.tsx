32cedd79d9fdf818713a7ccc605d5354ac10352f
Cayla Horsey <cayla.horsey@powerhrg.com>
2025-02-12 13:58:07 -0500

    Adjust formatting

--- playbook-website/app/javascript/components/PbKitPlayground/PlaygroundHeader.tsx
+++ playbook-website/app/javascript/components/PbKitPlayground/PlaygroundHeader.tsx
@@ -1,43 +1,44 @@
-import { Icon, SectionSeparator, Button, Flex, FlexItem, Image} from 'playbook-ui'
-import playgroundLogo from '../../images/playground-logo.svg';
+import {
+  Icon,
+  SectionSeparator,
+  Button,
+  Flex,
+  FlexItem,
+  Image
+} from 'playbook-ui';
+import playgroundLogo from '../../images/playground-logo.svg'
 
 const PlaygroundHeader = () => {
   const handleBackNavigation = (event) => {
     event.preventDefault()
-      window.location.href = "https://playbook.powerapp.cloud/";
-  }
+    window.location.href = "https://playbook.powerapp.cloud/"
+  };
 
   return (
     <>
-      <Flex orientation="row" justify="between" marginTop='xs' marginBottom='xxs'>
+      <Flex orientation="row" justify="between" marginTop="xs">
+        <Flex orientation="row" vertical="stretch">
+          <button
+            onClick={handleBackNavigation}
+            style={{ background: 'none', border: 'none', cursor: 'pointer' }}
+            aria-label="Go Back"
+          >
+            <Icon color="primary" fixedWidth icon="chevron-left" marginLeft="sm" size="2x" />
+          </button>
 
-      <Flex orientation="row" vertical="stretch">
-            <button
-              onClick={handleBackNavigation}
-              style={{ background: 'none', border: 'none', cursor: 'pointer' }}
-              aria-label="Go Back"
-            >
-              <Icon color="primary" fixedWidth icon="chevron-left" size="2x"/>
-            </button>
-            <SectionSeparator
-              orientation="vertical"
-              inline="flex-container"
-            />
-          <Image
-              marginLeft='xs'
-              marginBottom='none'
-              url={playgroundLogo}
-          />
+          <SectionSeparator orientation="vertical" inline="flex-container" />
 
-      </Flex>
+          <Image marginLeft="xs" marginBottom="none" url={playgroundLogo} />
+        </Flex>
+
         <FlexItem justify="end">
-          <Button marginRight='sm' text="Share"/>
+          <Button marginBottom="xs" marginRight="lg" text="Share" />
         </FlexItem>
-    </Flex>
+      </Flex>
 
       <SectionSeparator />
     </>
-  );
+  )
 }
 
-export default PlaygroundHeader;
+export default PlaygroundHeader
