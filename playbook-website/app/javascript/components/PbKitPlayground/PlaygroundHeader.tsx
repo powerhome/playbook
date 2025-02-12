0d474109b780f247030fbbbe1d386512b8dade85
Cayla Horsey <cayla.horsey@powerhrg.com>
2025-02-12 11:31:25 -0500

    Add playgrounds logo

--- playbook-website/app/javascript/components/PbKitPlayground/PlaygroundHeader.tsx
+++ playbook-website/app/javascript/components/PbKitPlayground/PlaygroundHeader.tsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import { Icon, SectionSeparator, Button, Flex, FlexItem } from 'playbook-ui'
+import { Icon, SectionSeparator, Button, Flex, FlexItem, Image} from 'playbook-ui'
+import playgroundLogo from '../../images/playground-logo.svg';
 
 const PlaygroundHeader = () => {
   const handleBackNavigation = (event) => {
-    event.preventDefault();
-    if (window.history.length > 1) {
-      window.history.back();
-    } else {
+    event.preventDefault()
+    // if (window.history.length > 1) {
+    //   window.history.back();
+    // } else {
       window.location.href = "https://playbook.powerapp.cloud/";
-    }
-  };
+    // }
+  }
 
   return (
     <>
-      <Flex orientation="row" justify="between">
+      <Flex orientation="row" justify="between" marginTop='sm' marginBottom='sm'>
 
-        <Flex orientation="row">
-        <FlexItem>
+        <Flex orientation="row" vertical="stretch">
+        <FlexItem marginLeft='sm'>
             <button
               onClick={handleBackNavigation}
               style={{ background: 'none', border: 'none', cursor: 'pointer' }}
@@ -24,12 +24,21 @@ const PlaygroundHeader = () => {
             >
               <Icon color="primary" fixedWidth icon="chevron-left" size="2x"  marginTop='xxs'/>
             </button>
+          <Image
+              marginLeft='sm'
+              position="absolute"
+              url={playgroundLogo}
+          />
+            <SectionSeparator
+              orientation="vertical"
+              color='primary'
+            />
 
          </FlexItem>
 
     </Flex>
         <FlexItem justify="end">
-          <Button text="Share" />
+          <Button marginRight='sm' text="Share"/>
         </FlexItem>
     </Flex>
 
