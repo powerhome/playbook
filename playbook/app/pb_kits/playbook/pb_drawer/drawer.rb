c88d689872bf74c96ca8bc0fd5e7f0d1af978005
markdoeswork <mark@does.work>
2025-02-13 13:17:10 -0500

    Add index.js

--- playbook/app/pb_kits/playbook/pb_drawer/drawer.rb
+++ playbook/app/pb_kits/playbook/pb_drawer/drawer.rb
@@ -3,6 +3,17 @@
 module Playbook
   module PbDrawer
     class Drawer < ::Playbook::KitBase
+      prop :from
+
+      def classname
+        generate_classname("pb_drawer")
+      end
+
+      def data
+        Hash(values[:data]).merge(
+          "from": from
+        )
+      end
     end
   end
 end
