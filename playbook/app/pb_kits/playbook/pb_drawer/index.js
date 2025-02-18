66ab4b374df862364523c3a64b54c9048d8288c3
markdoeswork <mark@does.work>
2025-02-18 13:24:34 -0500

    Add push functionality

--- playbook/app/pb_kits/playbook/pb_drawer/index.js
+++ playbook/app/pb_kits/playbook/pb_drawer/index.js
@@ -9,6 +9,7 @@ export default class PbDrawer extends PbEnhancedElement {
     this.handleOpenClick = this.handleOpenClick.bind(this)
     this.handleCloseClick = this.handleCloseClick.bind(this)
     this.handleOutsideClick = this.handleOutsideClick.bind(this)
+
     this._openTriggers = Array.from(document.querySelectorAll("[data-open-drawer]"))
     this._openTriggers.forEach(el => {
       el.addEventListener("click", this.handleOpenClick)
@@ -41,16 +42,20 @@ export default class PbDrawer extends PbEnhancedElement {
     const trigger = event.currentTarget
     const drawerId = trigger.dataset.openDrawer
     const dialog = document.getElementById(drawerId)
-
     if (!dialog) return
 
     const wrapper = dialog.closest(".pb_drawer_wrapper")
     if (!wrapper) return
-
     if (dialog.open) return
 
-    wrapper.style.display = ""  // Remove display style instead of setting to block
-    wrapper.querySelector(".pb_drawer_overlay").style.display = ""  // Remove display style
+    // Check push vs. floating, plus size & placement
+    const behavior = wrapper.dataset.behavior
+    const size = wrapper.dataset.size
+    const placement = wrapper.dataset.placement
+    this.handlePushOpen(behavior, size, placement)
+
+    wrapper.style.display = ""
+    wrapper.querySelector(".pb_drawer_overlay").style.display = ""
     dialog.showModal()
   }
 
@@ -58,12 +63,14 @@ export default class PbDrawer extends PbEnhancedElement {
     const trigger = event.currentTarget
     const wrapper = trigger.closest(".pb_drawer_wrapper") || document.querySelector(".pb_drawer_wrapper")
     const dialog = wrapper.querySelector(".pb_drawer")
+    if (!dialog) return
 
-    if (dialog) {
-      wrapper.style.display = "none"
-      wrapper.querySelector(".pb_drawer_overlay").style.display = "none"
-      dialog.close()
-    }
+    const behavior = wrapper.dataset.behavior
+    this.handlePushClose(behavior)
+
+    wrapper.style.display = "none"
+    wrapper.querySelector(".pb_drawer_overlay").style.display = "none"
+    dialog.close()
   }
 
   handleOutsideClick(event) {
@@ -72,8 +79,11 @@ export default class PbDrawer extends PbEnhancedElement {
     const overlay = wrapper.querySelector(".pb_drawer_overlay")
 
     if (wrapper.dataset.overlayClick === "overlay_close" && event.target === overlay) {
+      const behavior = wrapper.dataset.behavior
+      this.handlePushClose(behavior)
+
       wrapper.style.display = "none"
-      overlay.style.display = "none"
+      wrapper.querySelector(".pb_drawer_overlay").style.display = "none"
       dialog.close()
       event.stopPropagation()
       return
@@ -87,10 +97,50 @@ export default class PbDrawer extends PbEnhancedElement {
       event.clientY > dialogRect.bottom
 
     if (clickedOutside) {
+      const behavior = wrapper.dataset.behavior
+      this.handlePushClose(behavior)
+
       wrapper.style.display = "none"
-      overlay.style.display = "none"
+      wrapper.querySelector(".pb_drawer_overlay").style.display = "none"
       dialog.close()
       event.stopPropagation()
     }
+  }
+
+  handlePushOpen(behavior, size, placement) {
+    if (behavior !== "push") return
+
+    const sizeMap = {
+      xl: "365px",
+      lg: "300px",
+      md: "250px",
+      sm: "200px",
+      xs: "64px",
+      full: "100%",
+    }
+
+    const body = document.querySelector("body")
+    if (!body) return
+
+    if (placement === "left") {
+      body.style.cssText = `margin-left: ${sizeMap[size]} !important; margin-right: '' !important;`
+    } else if (placement === "right") {
+      body.style.cssText = `margin-right: ${sizeMap[size]} !important; margin-left: '' !important;`
+    }
+    body.classList.add("PBDrawer__Body--open")
+  }
+
+  handlePushClose(behavior) {
+    if (behavior !== "push") return
+
+    const body = document.querySelector("body")
+    if (!body) return
+
+    // Just like in React
+    if (body.classList.contains("PBDrawer__Body--open")) {
+      body.classList.add("PBDrawer__Body--close")
+    }
+    body.style.cssText = ""
+    body.classList.remove("PBDrawer__Body--open")
   }
 }
