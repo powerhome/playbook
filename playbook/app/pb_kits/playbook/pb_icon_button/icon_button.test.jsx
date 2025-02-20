a6e07ba8c3545bf94c01ee32e62d24d36620978f
Gary Kang <gary.kang@powerhrg.com>
2025-02-20 14:52:34 -0500

    Add jest testing for Icon Button

--- playbook/app/pb_kits/playbook/pb_icon_button/icon_button.test.jsx
+++ playbook/app/pb_kits/playbook/pb_icon_button/icon_button.test.jsx
@@ -1,21 +1,39 @@
 import { ensureAccessible, renderKit } from '../utilities/test-utils'
 
-import { IconButton } from '../'
-
-/* See these resources for more testing info:
-  - https://github.com/testing-library/jest-dom#usage for useage and examples
-  - https://jestjs.io/docs/en/using-matchers
-*/
+import { IconButton } from 'playbook-ui'
 
 const props = {
-  data: { testid: 'default' }
+  data: { testid: 'default', icon: 'plus' }
 }
 
-test('generated scaffold test - update me', () => {
+test('default test', () => {
   const kit = renderKit(IconButton, props)
+
   expect(kit).toBeInTheDocument()
+  expect(kit).toHaveClass('pb_icon_button_kit_default')
+
+  const iconElement = kit.querySelector('.icon_button_icon')
+  expect(iconElement).toBeInTheDocument()
 })
 
 it("should be accessible", async () => {
   ensureAccessible(IconButton, props)
+})
+
+test('passes link variant prop', () => {
+  const kit = renderKit(IconButton, { ...props, variant: "link" })
+  expect(kit).toBeInTheDocument()
+  expect(kit).toHaveClass('pb_icon_button_kit_link')
+})
+
+test('should set button type to "submit" when htmlType prop is passed', () => {
+  const kit = renderKit(IconButton, { ...props, htmlType: 'submit' })
+  const buttonElement = kit.querySelector('button')
+  expect(buttonElement).toHaveAttribute('type', 'submit')
+})
+
+test('passes custom classname', () => {
+  const kit = renderKit(IconButton, { ...props, className: "extra_class" })
+  expect(kit).toBeInTheDocument()
+  expect(kit).toHaveClass('pb_icon_button_kit_default extra_class')
 })
