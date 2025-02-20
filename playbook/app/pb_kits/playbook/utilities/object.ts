16128897e4f517fd128e9380f1ec5459c21b6232
Cayla Horsey <cayla.horsey@powerhrg.com>
2025-02-20 13:36:25 -0500

    Add isEmpty function to utilities

--- playbook/app/pb_kits/playbook/utilities/object.ts
+++ playbook/app/pb_kits/playbook/utilities/object.ts
@@ -1,3 +1,3 @@
 /* 🛠️ Any commonly used lodash functions can be added here. 🤙 */
 
-export const isEmpty = (obj: Record<string, unknown>): boolean => Object.keys(obj).length < 1
+export const isEmpty = obj => [Object, Array].includes((obj || {}).constructor) && !Object.entries((obj || {})).length;
