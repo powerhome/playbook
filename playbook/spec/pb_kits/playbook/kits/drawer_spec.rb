5fd1d0a65b2e521ace41dd9bc833a944a964d464
markdoeswork <mark@does.work>
2025-02-21 10:15:49 -0500

    Remove more comments

--- playbook/spec/pb_kits/playbook/kits/drawer_spec.rb
+++ playbook/spec/pb_kits/playbook/kits/drawer_spec.rb
@@ -47,26 +47,20 @@ RSpec.describe Playbook::PbDrawer::Drawer do
 
   describe "#classname" do
     it "returns namespaced class name", :aggregate_failures do
-      # Default drawer: size "md", placement "left", no within element, no border
       expect(subject.new({}).classname.strip).to eq "pb_drawer pb_drawer_md_left"
 
-      # When within_element is true
       expect(subject.new(within_element: true).classname.strip)
         .to eq "pb_drawer pb_drawer_md_left pb_drawer_within_element_rails"
 
-      # When border is set to 'full'
       expect(subject.new(border: "full").classname.strip)
         .to eq "pb_drawer pb_drawer_md_left  drawer_border-full"
 
-      # When border is set to 'right'
       expect(subject.new(border: "right").classname.strip)
         .to eq "pb_drawer pb_drawer_md_left  drawer_border-right"
 
-      # When border is set to 'left'
       expect(subject.new(border: "left").classname.strip)
         .to eq "pb_drawer pb_drawer_md_left  drawer_border-left"
 
-      # Combination of different size and placement
       expect(subject.new(size: "lg", placement: "bottom").classname.strip)
         .to eq "pb_drawer pb_drawer_lg_bottom"
     end
