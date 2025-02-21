b2bd00464da7379b983a94b6808902f947198860
Gabriella Massaro <gmassaro807@gmail.com>
2025-02-21 11:36:45 -0500

    Recreate error with form group kit

---
+++ playbook/app/pb_kits/playbook/pb_form_group/docs/_test.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Card, FormGroup, PhoneNumberInput, Select } from "playbook-ui";
+
+const Test = () => {
+  const phoneOptions = [
+    { value: "Cell" },
+    { value: "Work" },
+    { value: "Home" },
+  ];
+
+  return (
+    <Card maxWidth="md"
+        padding="md"
+        position="sticky"
+        tag="footer"
+        zindex="10"
+    >
+      <FormGroup>
+        <PhoneNumberInput
+            error="Missing phone number."
+            id="validation"
+            initialCountry="us"
+            label="Phone Number"
+            required
+        />
+        <Select blankSelection="select"
+            label="Type"
+            options={phoneOptions}
+        />
+      </FormGroup>
+    </Card>
+  );
+};
+
+export default Test;
