a114e5dd207fe549e346152dbcd5ca5d16b36d8c
Gabriella Massaro <gmassaro807@gmail.com>
2025-02-24 15:43:30 -0500

    This creates a new token for the <% end %>, it uses the keyword class

--- playbook-website/app/javascript/components/PbKitPlayground/index.tsx
+++ playbook-website/app/javascript/components/PbKitPlayground/index.tsx
@@ -4,7 +4,7 @@
 import React, { useEffect, useState } from 'react'
 
 import Editor from 'react-simple-code-editor'
-import { highlight, languages } from 'prismjs/components/prism-core'
+import Prism, { highlight, languages } from 'prismjs/components/prism-core'
 import 'prismjs/components/prism-clike'
 import 'prismjs/components/prism-javascript'
 import 'prismjs/components/prism-markup-templating'
@@ -111,6 +111,22 @@ const PbKitPlayground = () => {
       }
     }
   }
+
+  Prism.languages.insertBefore('javascript', 'keyword', {
+    'end-operator': {
+      pattern: /<%\s*end\s*%>/,
+      greedy: true,
+      inside: {
+        'delimiter': {
+          pattern: /^<%|%>$/,
+          alias: 'punctuation'
+        },
+        'keyword': {
+          pattern: /\bend\b/
+        }
+      }
+    }
+  })
 
   return (
     <>
