b71fe868ec47c1963dff8a9c1ac485b1b06b9f1a
Jasper <jasperfurniss@gmail.com>
2025-03-04 09:06:04 -0500

    Fixed Doc Examples

--- playbook/app/pb_kits/playbook/pb_drawer/docs/_drawer_breakpoints.jsx
+++ playbook/app/pb_kits/playbook/pb_drawer/docs/_drawer_breakpoints.jsx
@@ -1,76 +1,24 @@
-import React, { useState, useEffect } from "react"
-import { Button, Drawer, Icon, Nav, NavItem } from "playbook-ui"
+import React from "react"
+import { Button, Body } from "playbook-ui"
 
 const DrawerMenu = () => {
-  const [isSmallScreen, setIsSmallScreen] = useState(false)
-  const navItems = ["Overview", "Albums", "Similar Artists"]
-
-  useEffect(() => {
-    const mediaQuery = window.matchMedia("(max-width: 992px)")
-    const updateScreen = (e) => setIsSmallScreen(e.matches)
-    updateScreen(mediaQuery)
-    mediaQuery.addEventListener('change', updateScreen)
-    return () => mediaQuery.removeEventListener('change', updateScreen)
-  }, [])
-
   return (
-    <div>
-      <Button id='sidebar'
-          padding='xs'
+    <>
+      <Body>
+        The breakpoint prop determines when the Drawer is always visible. Above
+        the specified breakpoint, the Drawer remains open on the page. Below it,
+        only the trigger element is shown, allowing you to toggle the drawer
+        open and closed. To see this in action, click the button below and
+        resize your window.
+      </Body>
+      <Button
+          link='https://8njdkc.csb.app/'
+          marginTop='md' 
+          newWindow 
       >
-        <Icon icon='bars'
-            size='2x'
-        />
+        Open Doc Example in New Tab
       </Button>
-      <Drawer
-          behavior={"push"}
-          breakpoint='md'
-          overlay={isSmallScreen ? true : false}
-          placement='left'
-          size='md'
-          triggerId='sidebar'
-      >
-        <Nav
-            link='#'
-            orientation='vertical'
-            padding={isSmallScreen ? "none" : "sm"}
-            variant='bold'
-        >
-          {navItems.map((text, index) => {
-            return (
-              <NavItem
-                  collapsible
-                  collapsibleTrail
-                  fontWeight='bolder'
-                  iconLeft='city'
-                  iconRight={["plus", "minus"]}
-                  key={index}
-                  link='#'
-                  text={text}
-              >
-                <NavItem fontSize='small'
-                    link='#'
-                    marginY='none'
-                    text='City'
-                />
-                <NavItem
-                    fontSize='small'
-                    link='#'
-                    marginY='none'
-                    text='People'
-                />
-                <NavItem
-                    fontSize='small'
-                    link='#'
-                    marginY='none'
-                    text='Business'
-                />
-              </NavItem>
-            )
-          })}
-        </Nav>
-      </Drawer>
-    </div>
+    </>
   )
 }
 
