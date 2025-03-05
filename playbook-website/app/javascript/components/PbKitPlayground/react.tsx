75efe8b8a03c2f3cdd37192a5b642dd951488510
markdoeswork <mark@does.work>
2025-03-05 15:05:04 -0500

    Use const

--- playbook-website/app/javascript/components/PbKitPlayground/react.tsx
+++ playbook-website/app/javascript/components/PbKitPlayground/react.tsx
@@ -9,14 +9,19 @@ import 'prismjs/components/prism-javascript'
 import 'prismjs/components/prism-jsx'
 import 'prismjs/themes/prism-okaidia.css'
 
-import { Avatar,Badge,Body,Button,Card,Checkbox,Drawer,Dropdown,Icon,Nav,Radio,RichTextEditor, Select,Table,Title,Tooltip } from 'playbook-ui'
+import { Avatar, Badge, Body, Button, Card, Checkbox, Drawer, Dropdown, Icon, Nav, Radio, RichTextEditor, Select, Table, Title, Tooltip } from 'playbook-ui'
 import PlaygroundHeader from './PlaygroundHeader'
 
 // Pre-import allowed components for react-live
-const scope = {  Avatar,Badge,Body,Button,Card,Checkbox,Drawer,Dropdown,Icon,Nav,Radio,RichTextEditor, Select,Table,Title,Tooltip  }
+const scope = { Avatar, Badge, Body, Button, Card, Checkbox, Drawer, Dropdown, Icon, Nav, Radio, RichTextEditor, Select, Table, Title, Tooltip }
 
 const initialCode = `
-  <Button text="Click me" onClick={() => alert("Button clicked!")} />
+const text = 'Click me'
+render (
+<>
+  <Button text={text} onClick={() => alert("Button clicked!")} />
+</>
+)
 `
 
 const PbKitPlayground = () => {
@@ -27,12 +32,13 @@ const PbKitPlayground = () => {
       <PlaygroundHeader />
       <div className="pbDocPlayground">
         <div className="pbDocPlayground-Editor">
-          <LiveProvider code={code} scope={scope}>
+          <LiveProvider code={code} scope={scope} noInline={true}>
             <LiveEditor
               onChange={setCode}
               highlight={(code) =>
                 Prism.highlight(
                   code,
+                  // Fall back to JavaScript if JSX is not defined
                   Prism.languages.jsx || Prism.languages.js,
                   'jsx'
                 )
@@ -42,7 +48,7 @@ const PbKitPlayground = () => {
           </LiveProvider>
         </div>
         <div className="pbDocPlayground-Preview">
-          <LiveProvider code={code} scope={scope}>
+          <LiveProvider code={code} scope={scope} noInline={true}>
             <LivePreview />
           </LiveProvider>
         </div>
