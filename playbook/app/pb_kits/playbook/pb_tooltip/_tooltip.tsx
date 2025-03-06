bd93c738b6c1f4f91a6640300dac8fc532555f2d
Gabriella Massaro <37029835+gabbymassaro@users.noreply.github.com>
2025-03-06 09:14:57 -0500

    [PLAY-1874] Pass Height/Width Global Props To Tooltip - React (#4335)
    **What does this PR do?** A clear and concise description with your
    runway ticket url.
    
    [PLAY-1874](https://runway.powerhrg.com/backlog_items/PLAY-1874)
    - [x] Pass Height, Width, minHeight, maxHeight, minWidth, maxWidth to
    the Tooltip Kit in React
    - [x] Create a new Doc example called 'TOOLTIP SIZING'
    
    **Screenshots:** Screenshots to visualize your addition/change
    <img width="1420" alt="Screenshot 2025-02-27 at 2 11 04 PM"
    src="https://github.com/user-attachments/assets/44a6bb05-7a7f-4de7-a929-71f52d348eac"
    />
    
    
    https://github.com/user-attachments/assets/675df546-0693-4577-ba5d-39a36dabec56
    
    
    
    **How to test?** Steps to confirm the desired behavior:
    1. Go to kits/tooltip/react
    3. Scroll down to 'Tooltip Sizing' doc
    4. See addition/change
    
    
    #### Checklist:
    - [x] **LABELS** Add a label: `enhancement`, `bug`, `improvement`, `new
    kit`, `deprecated`, or `breaking`. See [Changelog &
    Labels](https://github.com/powerhome/playbook/wiki/Changelog-&-Labels)
    for details.
    - [ ] **DEPLOY** I have added the `milano` label to show I'm ready for a
    review.
    - [ ] **TESTS** I have added test coverage to my code.

--- playbook/app/pb_kits/playbook/pb_tooltip/_tooltip.tsx
+++ playbook/app/pb_kits/playbook/pb_tooltip/_tooltip.tsx
@@ -25,12 +25,18 @@ type TooltipProps = {
   children: JSX.Element,
   data?: { [key: string]: string },
   delay?: number | Partial<{open: number; close: number}>,
+  height?: string,
   htmlOptions?: {[key: string]: string | number | boolean | (() => void)},
   icon?: string,
   interaction?: boolean,
+  maxHeight?: string,
+  maxWidth?: string,
+  minHeight?: string,
+  minWidth?: string,
   placement?: Placement,
   position?: "absolute" | "fixed";
   text: string,
+  width: string,
   showTooltip?: boolean,
   forceOpenTooltip?: boolean,
 } & GlobalProps
@@ -42,13 +48,19 @@ const Tooltip = forwardRef((props: TooltipProps, ref: ForwardedRef<unknown>): Re
     children,
     data = {},
     delay = 0,
+    height,
     htmlOptions = {},
     icon = null,
     interaction = false,
+    maxHeight,
+    maxWidth,
+    minHeight,
+    minWidth,
     placement: preferredPlacement = "top",
     position = "absolute",
     text,
     showTooltip = true,
+    width,
     zIndex,
     forceOpenTooltip = false,
     ...rest
@@ -115,6 +127,18 @@ const Tooltip = forwardRef((props: TooltipProps, ref: ForwardedRef<unknown>): Re
     top: "bottom",
   }[placement.split("-")[0]]
 
+  const tooltipSizing = () => {
+    return Object.assign(
+      {},
+      height ? { height: height } : {},
+      maxHeight ? { maxHeight: maxHeight } : {},
+      maxWidth ? { maxWidth: maxWidth } : {},
+      minHeight ? { minHeight: minHeight } : {},
+      minWidth ? { minWidth: minWidth } : {},
+      width ? { width: width } : {}
+    );
+};
+
   return (
     <>
       <div
@@ -144,6 +168,7 @@ const Tooltip = forwardRef((props: TooltipProps, ref: ForwardedRef<unknown>): Re
               ref: refs.setFloating,
               role: "tooltip",
               style: {
+                ...tooltipSizing(),
                 position: strategy,
                 top: y ?? 0,
                 left: x ?? 0,
---
+++ playbook/app/pb_kits/playbook/pb_tooltip/docs/_tooltip_sizing.md
@@ -0,0 +1,3 @@
+You can customize the `height` and `width` of the tooltip's popover.
+
+When using `maxHeight`, be sure to set a `width` as well. The text needs to truncate within the `width` prop.
