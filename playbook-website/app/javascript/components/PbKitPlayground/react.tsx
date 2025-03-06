3ae694004cc406d54f7f49199fe943f591d7a4f7
markdoeswork <mark@does.work>
2025-03-06 14:26:59 -0500

    Use Monaco editor

--- playbook-website/app/javascript/components/PbKitPlayground/react.tsx
+++ playbook-website/app/javascript/components/PbKitPlayground/react.tsx
@@ -1,41 +1,65 @@
 /* eslint-disable flowtype/no-types-missing-file-annotation */
 /* eslint-disable comma-dangle */
 /* eslint-disable react/no-danger */
-import React, { useState } from 'react'
-import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live'
+import React, { useState, useEffect } from 'react'
+import MonacoEditor from '@monaco-editor/react'
+import * as Babel from '@babel/standalone'
 
 import { Avatar, Badge, Body, Button, Card, Checkbox, Drawer, Dropdown, Icon, Nav, Radio, RichTextEditor, Select, Table, Title, Tooltip } from 'playbook-ui'
 import PlaygroundHeader from './PlaygroundHeader'
 
-// Pre-import allowed components for react-live
+// Allowed components for the code snippet
 const scope = { Avatar, Badge, Body, Button, Card, Checkbox, Drawer, Dropdown, Icon, Nav, Radio, RichTextEditor, Select, Table, Title, Tooltip }
 
 const initialCode = `
 const text = 'Click me'
-render (
-<>
-  <Button text={text} onClick={() => alert("Button clicked!")} />
-</>
+render(
+  <>
+    <Button text={text} onClick={() => alert("Button clicked!")} />
+  </>
 )
 `
 
 const PbKitPlayground = () => {
   const [code, setCode] = useState(initialCode)
+  const [error, setError] = useState(null)
+  const [Component, setComponent] = useState(null)
+
+  useEffect(() => {
+    try {
+      // Transpile JSX using Babel with the react preset
+      const transformed = Babel.transform(code, { presets: ['react'] }).code
+      // Create a new function that accepts React, scope and a render callback
+      const renderFn = new Function('React', ...Object.keys(scope), 'render', transformed)
+      let Comp = () => null
+      // The render callback captures the rendered element
+      renderFn(React, ...Object.values(scope), (element) => {
+        Comp = () => element
+      })
+      setComponent(() => Comp)
+      setError(null)
+    } catch (err) {
+      setError(err.message)
+      setComponent(null)
+    }
+  }, [code])
 
   return (
     <>
       <PlaygroundHeader />
       <div className="pbDocPlayground">
         <div className="pbDocPlayground-Editor">
-          <LiveProvider code={code} scope={scope} noInline={true}>
-            <LiveEditor onChange={setCode} />
-            <LiveError />
-          </LiveProvider>
+          <MonacoEditor
+            height="400px"
+            language="javascript"
+            value={code}
+            onChange={(value) => setCode(value)}
+            options={{ automaticLayout: true }}
+          />
+          {error && <div style={{ color: 'red' }}>{error}</div>}
         </div>
         <div className="pbDocPlayground-Preview">
-          <LiveProvider code={code} scope={scope} noInline={true}>
-            <LivePreview />
-          </LiveProvider>
+          {Component && <Component />}
         </div>
       </div>
     </>
