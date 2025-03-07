62a8a7306600da75313b443b8369319cf92fa278
Gabriella Massaro <gmassaro807@gmail.com>
2025-03-07 14:00:30 -0500

    Fix failing/empty test

--- playbook/app/pb_kits/playbook/pb_empty_state/empty_state.test.jsx
+++ playbook/app/pb_kits/playbook/pb_empty_state/empty_state.test.jsx
@@ -1,6 +1,6 @@
 import { ensureAccessible, renderKit } from '../utilities/test-utils'
 
-import { EmptyState } from '../'
+import { EmptyState } from 'playbook-ui'
 
 /* See these resources for more testing info:
   - https://github.com/testing-library/jest-dom#usage for useage and examples
