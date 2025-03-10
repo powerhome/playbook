2873aa50dbbc35a8956bf8e59e280daa900b947f
Gabriella Massaro <gmassaro807@gmail.com>
2025-03-10 11:45:18 -0400

    Create orientation doc example

--- playbook/app/pb_kits/playbook/pb_empty_state/docs/_empty_state_orientation.jsx
+++ playbook/app/pb_kits/playbook/pb_empty_state/docs/_empty_state_orientation.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { EmptyState } from 'playbook-ui'
 
-const EmptyStateDefault = (props) => (
+const EmptyStateOrientation = (props) => (
   <div>
     <EmptyState
         {...props}
@@ -9,12 +9,11 @@ const EmptyStateDefault = (props) => (
         description="Body text goes into detail with possible steps for user to take"
         header="Title Explains"
         image="https://component.gallery/static/8d36eaa25b6dcb026685101ebc379022/Empty%20state%20icon..svg"
-        linkButton="Alt Action"
-        orientation="vertical"
+        orientation="horizontal"
         primaryButton="Next Action"
-        size="sm"
+        size="lg"
     />
   </div>
 )
 
-export default EmptyStateDefault
+export default EmptyStateOrientation
--- playbook/app/pb_kits/playbook/pb_empty_state/docs/index.js
+++ playbook/app/pb_kits/playbook/pb_empty_state/docs/index.js
@@ -1,2 +1,2 @@
-export { default as EmptyStateDefault } from './_empty_state_default.jsx'
+export { default as EmptyStateOrientation } from './_empty_state_orientation.jsx'
 export { default as EmptyStateSize } from './_empty_state_size.jsx'
