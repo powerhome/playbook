30c48a760cdb7ef53898be62bada108fb9b75ade
Carlos Lima <carloslimasd@gmail.com>
2025-03-10 10:02:01 -0300

    [PBNTR-892] Implementing Dialog Enhanced Element (#4355)
    **What does this PR do?**
    Implementing Dialog Enhanced Element
    
    **How to test?** Steps to confirm the desired behavior:
    1. Go to '...'
    2. Click on '....'
    3. Scroll down to '....'
    4. See addition/change
    
    
    #### Checklist:
    - [x] **LABELS** Add a label: `enhancement`, `bug`, `improvement`, `new
    kit`, `deprecated`, or `breaking`. See [Changelog &
    Labels](https://github.com/powerhome/playbook/wiki/Changelog-&-Labels)
    for details.
    - [x] **DEPLOY** I have added the `milano` label to show I'm ready for a
    review.
    - [ ] **TESTS** I have added test coverage to my code.

--- playbook/app/entrypoints/playbook-rails.js
+++ playbook/app/entrypoints/playbook-rails.js
@@ -20,6 +20,8 @@ import datePickerHelper from 'kits/pb_date_picker/date_picker_helper'
 window.datePickerHelper = datePickerHelper
 
 // Dialog
+// Three places in Nitro depend on this function inside the window scope.
+// We will keep this code until we remove this dependency from Nitro.
 import dialogHelper from 'kits/pb_dialog/dialogHelper'
 window.dialogHelper = dialogHelper
 
@@ -73,6 +75,9 @@ PbOverlay.start()
 
 import PbSelect from 'kits/pb_select'
 PbSelect.start()
+
+import PbDialog from 'kits/pb_dialog'
+PbDialog.start()
 
 import PbDatePicker from 'kits/pb_date_picker'
 PbDatePicker.start()
