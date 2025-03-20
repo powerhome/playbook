0022ab4915b0384ede7617d73f0dbccff3abd964
George Morris <gmorris1242@gmail.com>
2025-03-20 14:46:16 -0400

    Make inline date picker icon revert back to plus after date is removed

--- playbook/app/pb_kits/playbook/pb_date_picker/docs/_date_picker_inline.jsx
+++ playbook/app/pb_kits/playbook/pb_date_picker/docs/_date_picker_inline.jsx
@@ -6,6 +6,8 @@ const DatePickerInline = (props) => {
   const showAngleDownHandler = (dateSelected) => {
     if (dateSelected) {
       document.querySelector('.inline-date-picker').classList.add('show-angle-down-icon')
+    } else {
+      document.querySelector('.inline-date-picker').classList.remove('show-angle-down-icon')
     }
   }
 
