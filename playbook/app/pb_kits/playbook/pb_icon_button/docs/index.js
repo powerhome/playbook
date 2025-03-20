a92391a94cdaf889cd34283dc426aa1799583308
Gary Kang <42440452+kangaree@users.noreply.github.com>
2025-03-20 16:18:01 -0400

    [PLAY-1963] Icon Button onClick Handler (#4412)
    **What does this PR do?** A clear and concise description with your
    runway ticket url.
    Add onClick to Icon Button kit.
    
    **Screenshots:** Screenshots to visualize your addition/change
    ![Screenshot 2025-03-20 at 3 32
    52 PM](https://github.com/user-attachments/assets/ae6c18e3-7413-4b28-a835-aa8a7c33dcde)
    
    **How to test?** Steps to confirm the desired behavior:
    1. Go to /kits/icon_button/react
    2. Check out the example
    
    #### Checklist:
    - [x] **LABELS** Add a label: `enhancement`, `bug`, `improvement`, `new
    kit`, `deprecated`, or `breaking`. See [Changelog &
    Labels](https://github.com/powerhome/playbook/wiki/Changelog-&-Labels)
    for details.
    - [x] **DEPLOY** I have added the `milano` label to show I'm ready for a
    review.

--- playbook/app/pb_kits/playbook/pb_icon_button/docs/index.js
+++ playbook/app/pb_kits/playbook/pb_icon_button/docs/index.js
@@ -1,2 +1,3 @@
 export { default as IconButtonDefault } from './_icon_button_default.jsx'
 export { default as IconButtonSizes } from './_icon_button_sizes.jsx'
+export { default as IconButtonClick } from './_icon_button_click.jsx'
