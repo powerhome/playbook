625f96d0ca21f6a548578180098d50c973171a6c
nida.ghuman <nidaqg@gmail.com>
2025-04-14 17:21:18 -0400

    Added Sticky column logic, testing

--- playbook/app/pb_kits/playbook/pb_advanced_table/docs/index.js
+++ playbook/app/pb_kits/playbook/pb_advanced_table/docs/index.js
@@ -23,3 +23,4 @@ export { default as AdvancedTableTablePropsStickyHeader } from './_advanced_tabl
 export { default as AdvancedTableColumnHeadersCustomCell } from './_advanced_table_column_headers_custom_cell.jsx'
 export { default as AdvancedTableInlineEditing } from './_advanced_table_inline_editing.jsx'
 export { default as AdvancedTableFullscreen } from './_advanced_table_fullscreen.jsx'
+export { default as AdvancedTableStickyColumns } from './_advanced_table_sticky_columns.jsx'
