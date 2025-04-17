7dfc23a6096efb5b59c4608ea896173727fc9c21
markdoeswork <mark@does.work>
2025-04-17 15:29:09 -0400

    Make tuple less confusing looking

--- playbook/app/pb_kits/playbook/pb_copy_button/docs/_copy_button_hook.jsx
+++ playbook/app/pb_kits/playbook/pb_copy_button/docs/_copy_button_hook.jsx
@@ -1,25 +1,31 @@
 import React, { useEffect } from 'react'
-import useCopy from '../../pb_copy_button/usePBCopy'
+import usePBCopy from '../../pb_copy_button/usePBCopy'
 import Body from '../../pb_body/_body'
 import Textarea from '../../pb_textarea/_textarea'
 
-const CopyButtonHook = props => {
-  const [, copy] = useCopy({ from: 'hookbody' })
+const CopyButtonHook = (props) => {
+  // eslint-disable-next-line no-unused-vars
+  const [hasCopied, copyToClipboard] = usePBCopy({ from: 'hookbody' })
+  
+  useEffect(() => {
+    const el = document.getElementById('hookbody')
+    if (!el) return
 
-  useEffect(() =>
-    document
-      .getElementById('hookbody')
-      ?.addEventListener('mouseenter', copy),
-  [copy])
-
+    el.addEventListener('mouseenter', copyToClipboard)
+    return () => {
+      el.removeEventListener('mouseenter', copyToClipboard)
+    }
+  }, [copyToClipboard])
+  // Copy to clipboard when the body is hovered
+  
   return (
     <div>
       <Body
-          cursor='pointer'
+          cursor="pointer"
           id="hookbody"
           text="Hover to copy this body text!"
       />
-      <Textarea 
+      <Textarea
           {...props}
           placeholder="Paste here"
       />
