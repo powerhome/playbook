6e8aa82ba5f16e62c572cb991fa1dd9410574f8c
markdoeswork <mark@does.work>
2025-04-21 15:27:39 -0400

    Background and outline work

--- playbook/app/pb_kits/playbook/pb_draggable/draggable.rb
+++ playbook/app/pb_kits/playbook/pb_draggable/draggable.rb
@@ -5,13 +5,25 @@ module Playbook
     class Draggable < ::Playbook::KitBase
       prop :initial_items, type: Playbook::Props::Array,
                            default: []
+      prop :drop_zone_type, type: Playbook::Props::Enum,
+                            values: %w[ghost shadow outline line],
+                            default: "shadow"
+      prop :drop_zone_color, type: Playbook::Props::Enum,
+                             values: %w[primary purple],
+                             default: "primary"
+      prop :drop_zone_orientation, type: Playbook::Props::Enum,
+                                   values: %w[horizontal vertical],
+                                   default: "vertical"
 
       def data
         Hash(prop(:data)).merge(pb_draggable: true)
       end
 
       def classname
-        generate_classname("pb_draggable")
+        generate_classname("pb_draggable",
+                           "drop_zone_#{drop_zone_type}",
+                           "drop_zone_#{drop_zone_color}",
+                           "drop_zone_#{drop_zone_orientation}")
       end
     end
   end
