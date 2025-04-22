faa9a6b543846b344dddbd4f0585f109a8a57bb8
George Morris <gmorris1242@gmail.com>
2025-04-22 12:57:26 -0400

    Remove uneeded code

--- playbook/vite.config.ts
+++ playbook/vite.config.ts
@@ -78,10 +78,6 @@ export default defineConfig({
           src: resolve(__dirname, 'app/pb_kits/playbook/pb_bar_graph/BarGraphStyles.scss'),
           dest: 'dist/pb_bar_graph/',
         },
-        {
-          src: resolve(__dirname, 'app/pb_kits/playbook/pb_gauge/GaugeStyles.scss'),
-          dest: 'dist/pb_gauge/',
-        }
       ]
     }),
     consolidate({
