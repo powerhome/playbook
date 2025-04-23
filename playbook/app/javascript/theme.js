838ca9331f629e0e5903f62e1f108b08ecd2bdad
George Morris <gmorris1242@gmail.com>
2025-04-23 15:18:49 -0400

    [PLAY-1966] Gauge Kit: Added the Ability to Use Gauge Styles w/o Gauge Kit (#4536)
    [CSB](https://codesandbox.io/p/devbox/confident-perlman-thx3lr)
    [REVIEW
    ENV](https://pr4536.playbook.beta.px.powerapp.cloud/kits/gauge/react)
    [PLAY-1966](https://runway.powerhrg.com/backlog_items/PLAY-1966)
    
    This moves all the Gauge Kit styling into a separate gaugeTheme file
    allowing users to use the gauge styles without the Gauge Kit. This only
    currently works with the default example.

--- playbook/app/javascript/theme.js
+++ playbook/app/javascript/theme.js
@@ -5,3 +5,4 @@ export {default as usePBCopy} from '../pb_kits/playbook/pb_copy_button/usePBCopy
 export {default as useDropdown } from '../pb_kits/playbook/pb_dropdown/hooks/useDropdown'
 export { highchartsTheme } from '../pb_kits/playbook/pb_dashboard/pbChartsLightTheme'
 export { highchartsDarkTheme } from '../pb_kits/playbook/pb_dashboard/pbChartsDarkTheme'
+export { default as gaugeTheme } from '../pb_kits/playbook/pb_gauge/gaugeTheme'
