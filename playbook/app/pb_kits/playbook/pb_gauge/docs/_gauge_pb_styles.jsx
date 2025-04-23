85e02ef0aff3da7159f91c8bed85f7cdb1f5b3ca
George Morris <gmorris1242@gmail.com>
2025-04-23 09:02:43 -0400

    Remove unused css file

--- playbook/app/pb_kits/playbook/pb_gauge/docs/_gauge_pb_styles.jsx
+++ playbook/app/pb_kits/playbook/pb_gauge/docs/_gauge_pb_styles.jsx
@@ -4,9 +4,6 @@ import Highcharts from "highcharts"
 import HighchartsReact from "highcharts-react-official"
 import HighchartsMore from "highcharts/highcharts-more"
 import SolidGauge from "highcharts/modules/solid-gauge"
-import '../GaugeStyles.scss'
-// Your path might look more like this
-//import "playbook-ui/dist/pb_gauge/GaugeStyles.scss";
 
 HighchartsMore(Highcharts);
 SolidGauge(Highcharts);
