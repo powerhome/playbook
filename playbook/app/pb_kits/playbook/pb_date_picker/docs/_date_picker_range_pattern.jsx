7d86ce8e886191d88449adfb150b508d8de07e8d
nida.ghuman <nidaqg@gmail.com>
2025-04-24 12:47:59 -0400

    Prop based logic for linknig 3 inputs

--- playbook/app/pb_kits/playbook/pb_date_picker/docs/_date_picker_range_pattern.jsx
+++ playbook/app/pb_kits/playbook/pb_date_picker/docs/_date_picker_range_pattern.jsx
@@ -1,53 +1,30 @@
-import React, { useState } from "react";
+import React from "react";
 import DatePicker from "../_date_picker";
 
 const DatePickerRangePattern = (props) => {
-  const [startDate, setStartDate] = useState(null);
-  const [endDate, setEndDate] = useState(null);
-
-  const handleQuickPickClose = (selectedDates) => {
-    if (selectedDates?.length === 2) {
-      setStartDate(selectedDates[0]);
-      setEndDate(selectedDates[1]);
-    }
-  };
-
-  const handleStartDateClose = (selectedDates) => {
-    if (selectedDates?.length) {
-      setStartDate(selectedDates[0]);
-      document.querySelector('#date-picker-quick-pick-range')._flatpickr.clear()
-    }
-  };
-
-  const handleEndDateClose = (selectedDates) => {
-    if (selectedDates?.length) {
-      setEndDate(selectedDates[0]);
-      document.querySelector('#date-picker-quick-pick-range')._flatpickr.clear()
-    }
-  };
-
   return (
     <>
       <DatePicker
+          controlsEndId="end"
+          controlsStartId="start"
           mode="range"
-          onClose={handleQuickPickClose}
-          pickerId="date-picker-quick-pick-range"
-          placeholder="mm/dd/yyyy to mm/dd/yyyy"
+          pickerId="quickpick-for-range"
+          placeholder="Select a Range"
           selectionType="quickpick"
           {...props}
       />
+
       <DatePicker
-          defaultDate={startDate}
-          onClose={handleStartDateClose}
-          pickerId="date-picker-start"
-          placeholder="Start Date"
+          pickerId="start-date-picker"
+          placeholder="Select a Start Date"
+          syncStartWith="quickpick-for-range"
           {...props}
       />
+
       <DatePicker
-          defaultDate={endDate}
-          onClose={handleEndDateClose}
-          pickerId="date-picker-end"
-          placeholder="End Date"
+          pickerId="end-date-picker"
+          placeholder="Select a End Date"
+          syncEndWith="quickpick-for-range"
           {...props}
       />
     </>
---
+++ playbook/app/pb_kits/playbook/pb_date_picker/docs/_date_picker_range_pattern.md
@@ -0,0 +1,14 @@
+You can link a Quickpick DatePicker to standard DatePickers using the following props:
+
+**For the Quickpick DatePicker**:
+`controlsStartId`: ID of the DatePicker that should receive the start date.
+
+`controlsEndId`: ID of the DatePicker that should receive the end date.
+
+When a quickpick option like “This Year” is selected, it automatically populates the linked start and end inputs.
+
+**For the Start/End DatePickers**:
+`syncStartWith`: ID of the quickpick this start date is synced to.
+`syncEndWith`: ID of the quickpick this end date is synced to.
+
+When a user manually edits the start or end date, it clears the selected quickpick to prevent conflicting values.
