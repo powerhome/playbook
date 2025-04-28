f8081d162c3d66ee5be094389610cb570275471b
Gary Kang <42440452+kangaree@users.noreply.github.com>
2025-04-28 10:49:09 -0400

    [PLAY-2043] Playbook Website: Add Warning Icon to Error Docs - React (#4548)
    **What does this PR do?** A clear and concise description with your
    runway ticket url.
    
    - Add warning icon next to error docs for accessibility
    - Phone Number Input, Text Input, Textarea, Typeahead, Date Picker,
    Dropdown, and Select
    
    https://runway.powerhrg.com/backlog_items/PLAY-2043
    
    **Screenshots:** Screenshots to visualize your addition/change
    ![Screenshot 2025-04-25 at 8 57
    30 AM](https://github.com/user-attachments/assets/a8c0229f-ae94-444b-b44d-f097a0fd2e16)
    
    
    **How to test?** Steps to confirm the desired behavior:
    1. Go to these docs and make sure there is a 'warning' icon next to the
    error message
    
    #### Checklist:
    - [x] **LABELS** Add a label: `enhancement`, `bug`, `improvement`, `new
    kit`, `deprecated`, or `breaking`. See [Changelog &
    Labels](https://github.com/powerhome/playbook/wiki/Changelog-&-Labels)
    for details.
    - [x] **DEPLOY** I have added the `milano` label to show I'm ready for a
    review.

--- playbook/app/pb_kits/playbook/pb_textarea/docs/_textarea_error.jsx
+++ playbook/app/pb_kits/playbook/pb_textarea/docs/_textarea_error.jsx
@@ -1,15 +1,21 @@
 import React, {useState} from 'react'
 import Textarea from '../../pb_textarea/_textarea'
+import Icon from '../../pb_icon/_icon'
 
 const TextareaError = (props) => {
   const [value, setValue] = useState('default value text')
   const handleChange = (event) => {
     setValue(event.target.value)
   }
+  const error = (
+    <>
+        <Icon icon="warning" /> This field has an error!
+    </>
+  )
   return (
     <div>
       <Textarea
-          error="This field has an error!"
+          error={error}
           label="Label"
           name="comment"
           onChange={(e)=> handleChange(e)}
