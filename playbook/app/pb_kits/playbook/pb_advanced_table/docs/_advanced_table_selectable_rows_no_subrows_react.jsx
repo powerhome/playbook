eba8d4ce495a0490491cdcccb8a800e77ff9c04f
nida.ghuman <nidaqg@gmail.com>
2025-04-30 11:37:38 -0400

    Fix import error

--- playbook/app/pb_kits/playbook/pb_advanced_table/docs/_advanced_table_selectable_rows_no_subrows_react.jsx
+++ playbook/app/pb_kits/playbook/pb_advanced_table/docs/_advanced_table_selectable_rows_no_subrows_react.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import AdvancedTable from '../_advanced_table'
+import AdvancedTable from '../../pb_advanced_table/_advanced_table'
 import MOCK_DATA from "./advanced_table_mock_data_no_subrows.json"
 
 const AdvancedTableSelectableRowsNoSubrowsReact = (props) => {
