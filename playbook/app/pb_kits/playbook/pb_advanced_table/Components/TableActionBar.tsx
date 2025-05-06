81360662206d32e19bd18c4fe0d9b679fec8b07c
nidaqg <nidaqg@gmail.com>
2025-05-06 09:05:29 -0400

    ColumnVisibility WIP

--- playbook/app/pb_kits/playbook/pb_advanced_table/Components/TableActionBar.tsx
+++ playbook/app/pb_kits/playbook/pb_advanced_table/Components/TableActionBar.tsx
@@ -1,23 +1,42 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useContext } from "react";
+
+import AdvancedTableContext from "../Context/AdvancedTableContext";
 import Card from "../../pb_card/_card";
 import Caption from "../../pb_caption/_caption";
 import Flex from "../../pb_flex/_flex";
 import FlexItem from "../../pb_flex/_flex_item";
-import { showActionBar, hideActionBar } from "../Utilities/ActionBarAnimationHelper";
+import Dropdown from "../../pb_dropdown/_dropdown";
+import DropdownContainer from "../../pb_dropdown/subcomponents/DropdownContainer";
+import DropdownTrigger from "../../pb_dropdown/subcomponents/DropdownTrigger";
+import Icon from "../../pb_icon/_icon";
+import Checkbox from "../../pb_checkbox/_checkbox";
+
+import {
+  showActionBar,
+  hideActionBar,
+} from "../Utilities/ActionBarAnimationHelper";
 
 interface TableActionBarProps {
   isVisible: boolean;
   selectedCount: number;
   actions?: React.ReactNode[] | React.ReactNode;
+  type?: string;
 }
 
 const TableActionBar: React.FC<TableActionBarProps> = ({
   isVisible,
   selectedCount,
-  actions
+  actions,
+  type = "row-selection",
 }) => {
   const cardRef = useRef(null);
+  const { table, columnVisibilityControl, columnDefinitions } =
+    useContext(AdvancedTableContext);
+  const includeIds = columnVisibilityControl?.includeIds;
 
+  const columns = table
+    .getAllLeafColumns()
+    .filter((col) => !includeIds?.length || includeIds.includes(col.id));
   useEffect(() => {
     if (cardRef.current) {
       if (isVisible) {
@@ -31,22 +50,58 @@ const TableActionBar: React.FC<TableActionBarProps> = ({
   return (
     <Card
         borderNone={!isVisible}
-        className={`${isVisible && "show-action-card row-selection-actions-card"}`}
+        className={`${
+          isVisible && "show-action-card row-selection-actions-card"
+        }`}
         htmlOptions={{ ref: cardRef as any }}
         padding={`${isVisible ? "xs" : "none"}`}
     >
       <Flex
           alignItems="center"
-          justify="between"
+          justify={type === "row-selection" ? "space-between" : "end"}
       >
-        <Caption
-            color="light"
-            paddingLeft="xs"
-            size="xs"
-        >
-          {selectedCount} Selected
-        </Caption>
-        <FlexItem>{actions}</FlexItem>
+        {type === "row-selection" ? (
+          <>
+            <Caption color="light" 
+                paddingLeft="xs" 
+                size="xs"
+            >
+              {selectedCount} Selected
+            </Caption>
+            <FlexItem>{actions}</FlexItem>
+          </>
+        ) : (
+          <Dropdown
+              className="column-visibility-dropdown-wrapper"
+              options={columnDefinitions}
+          >
+            <DropdownTrigger>
+              <Icon 
+                  color="primary" 
+                  cursor="pointer" 
+                  icon="sliders" 
+              />
+            </DropdownTrigger>
+            <DropdownContainer
+                className="column-visibility-dropdown"
+                padding="xs"
+            >
+              {columns.map((col) => (
+                <Flex cursor="pointer" 
+                    flexDirection="column" 
+                    key={col.id}
+                    padding="xs"
+                >
+                  <Checkbox
+                      checked={col.getIsVisible()}
+                      onChange={() => col.toggleVisibility()}
+                      text={col.columnDef.header}
+                  />
+                </Flex>
+              ))}
+            </DropdownContainer>
+          </Dropdown>
+        )}
       </Flex>
     </Card>
   );
--- playbook/app/pb_kits/playbook/pb_advanced_table/docs/index.js
+++ playbook/app/pb_kits/playbook/pb_advanced_table/docs/index.js
@@ -28,3 +28,4 @@ export { default as AdvancedTableStickyHeader } from './_advanced_table_sticky_h
 export { default as AdvancedTableStickyColumnsAndHeader } from './_advanced_table_sticky_columns_and_header.jsx'
 export { default as AdvancedTableExpandByDepth } from './_advanced_table_expand_by_depth.jsx'
 export { default as AdvancedTableColumnBorderColor} from './_advanced_table_column_border_color.jsx'
+export { default as AdvancedTableColumnVisibility } from './_advanced_table_column_visibility.jsx'
