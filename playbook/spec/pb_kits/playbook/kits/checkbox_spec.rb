cb8767c6d7e53ba564acbedbb08f70f2007f14ea
Gary Kang <gary.kang@powerhrg.com>
2025-05-28 15:18:10 -0400

    Add tests

--- playbook/spec/pb_kits/playbook/kits/checkbox_spec.rb
+++ playbook/spec/pb_kits/playbook/kits/checkbox_spec.rb
@@ -15,6 +15,8 @@ RSpec.describe Playbook::PbCheckbox::Checkbox do
   it { is_expected.to define_prop(:disabled).with_default(false) }
   it { is_expected.to define_boolean_prop(:checked).with_default(false) }
   it { is_expected.to define_boolean_prop(:indeterminate).with_default(false) }
+  it { is_expected.to define_boolean_prop(:indeterminate_main).with_default(false) }
+  it { is_expected.to define_prop(:indeterminate_parent) }
   it { is_expected.to define_hash_prop(:input_options).with_default({}) }
 
   describe "#classname" do
