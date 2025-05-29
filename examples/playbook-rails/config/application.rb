6325396d4a5a4a2c1d73cc0f97469f888be294cb
Sam Duncan <samkduncan@gmail.com>
2025-05-29 08:51:03 -0500

    Change to 7.1 on load_defaults

--- examples/playbook-rails/config/application.rb
+++ examples/playbook-rails/config/application.rb
@@ -11,7 +11,7 @@ Bundler.require(*Rails.groups)
 module PlaybookRails
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.2
+    config.load_defaults 7.1
 
     # Configuration for the application, engines, and railties goes here.
     #
