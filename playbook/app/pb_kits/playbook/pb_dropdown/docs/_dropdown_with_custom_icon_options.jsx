05f76a83eead1f03e625efad0cd997bcb2352c84
Elisa Shapiro <elisashapiro@gmail.com>
2025-06-12 08:44:46 -0400

    Custom Icon Options doc examples

---
+++ playbook/app/pb_kits/playbook/pb_dropdown/docs/_dropdown_with_custom_icon_options.jsx
@@ -0,0 +1,99 @@
+import React from 'react'
+import Dropdown from '../_dropdown'
+
+import Detail from '../../pb_detail/_detail'
+import Flex from '../../pb_flex/_flex'
+import Icon from '../../pb_icon/_icon'
+
+const DropdownCustomIconOptions = (props) => {
+
+  const options = [
+    { label: "Item 1", value: "item-1", id: "1" },
+    { label: "Item 2", value: "item-2", id: "2" },
+    { label: "Item 3", value: "item-3", id: "3" },
+  ]
+
+
+  return (
+  <div>
+      <Dropdown label="Multiple Icons"
+          options={options}
+          {...props}
+      >
+        {options.map((option) => (
+          <Dropdown.Option key={option.id} 
+              option={option}
+          >
+            <Flex align="center" 
+                justify="between"
+            >
+                <Flex align="center">
+                  <Icon icon="calendar" 
+                      paddingRight="xs"
+                      {...props}    
+                  />
+                  <Detail color="default" 
+                      text={option.label}
+                      {...props}
+                  />
+                </Flex>
+                <Icon icon="check" 
+                    {...props}    
+                />
+            </Flex>
+          </Dropdown.Option>
+        ))}
+      </Dropdown>
+
+      <Dropdown label="Icon on Left"
+          options={options} 
+          paddingY="md"
+          {...props}
+      >
+        {options.map((option) => (
+          <Dropdown.Option key={option.id}
+              option={option}
+          >
+            <Flex align="center">
+              <Icon icon="calendar" 
+                  paddingRight="xs"
+                  {...props}
+              />
+              <Detail color="default" 
+                  text={option.label}
+                  {...props}
+              />
+            </Flex>
+          </Dropdown.Option>
+        ))}
+      </Dropdown>
+
+      <Dropdown label="Icon on Right"
+          options={options}
+          {...props}
+      >
+        {options.map((option) => (
+          <Dropdown.Option key={option.id}
+              option={option}
+          >
+            <Flex align="center"
+                justify="between"
+            >
+              <Flex align="center">
+                <Detail color="default" 
+                    text={option.label}
+                    {...props}
+                />
+              </Flex>
+              <Icon icon="check"
+                  {...props}
+              />
+            </Flex>
+          </Dropdown.Option>
+        ))}
+      </Dropdown>
+  </div>
+  )
+}
+
+export default  DropdownCustomIconOptions
