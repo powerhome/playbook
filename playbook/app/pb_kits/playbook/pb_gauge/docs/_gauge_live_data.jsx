4296b27a1a0b7c4243fa05348e5f46647113e615
gary.kang <gary.kang@powerhrg.com>
2025-07-15 14:24:06 -0400

    Replace live data doc. TODO: investigate animation on first click

--- playbook/app/pb_kits/playbook/pb_gauge/docs/_gauge_live_data.jsx
+++ playbook/app/pb_kits/playbook/pb_gauge/docs/_gauge_live_data.jsx
@@ -1,7 +1,14 @@
 import React, { useState } from 'react'
 
 import Button from '../../pb_button/_button'
-import Gauge from '../../pb_gauge/_gauge'
+import gaugeTheme from '../gaugeTheme'
+import Highcharts from "highcharts"
+import HighchartsReact from "highcharts-react-official"
+import HighchartsMore from "highcharts/highcharts-more"
+import SolidGauge from "highcharts/modules/solid-gauge"
+
+HighchartsMore(Highcharts);
+SolidGauge(Highcharts);
 
 const GaugeLiveData = (props) => {
   const [value, setValue] = useState(50)
@@ -33,10 +40,11 @@ const GaugeLiveData = (props) => {
           text="Update Name"
           {...props}
       />
-      <Gauge
-          chartData={[{ name: name, value: value }]}
-          id="gauge-live-data"
-          {...props}
+      <HighchartsReact
+          highcharts={Highcharts}
+          options={Highcharts.merge({}, gaugeTheme, {
+            series: [{ data: [{ name: name, y: value }] }],
+          })}
       />
     </div>
   )
