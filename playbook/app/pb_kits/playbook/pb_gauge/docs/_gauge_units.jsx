2041c13ac2e1ec6d21a1b3ec27a61de1f3770040
gary.kang <gary.kang@powerhrg.com>
2025-07-15 14:13:31 -0400

    Replace units doc and add custom pb-gauge-*fix classes to use

--- playbook/app/pb_kits/playbook/pb_gauge/docs/_gauge_units.jsx
+++ playbook/app/pb_kits/playbook/pb_gauge/docs/_gauge_units.jsx
@@ -1,31 +1,70 @@
 import React from 'react'
-import Gauge from '../../pb_gauge/_gauge'
+import gaugeTheme from '../gaugeTheme'
+import Highcharts from "highcharts"
+import HighchartsReact from "highcharts-react-official"
+import HighchartsMore from "highcharts/highcharts-more"
+import SolidGauge from "highcharts/modules/solid-gauge"
+
+HighchartsMore(Highcharts);
+SolidGauge(Highcharts);
 
 const data1 = [
-  { name: 'Data Used', value: 32 },
+  { name: 'Data Used', y: 32 },
 ]
 const data2 = [
-  { name: 'Sales to Date', value: 65 },
+  { name: 'Sales to Date', y: 65 },
 ]
 
-const GaugeUnits = (props) => (
-  <div>
-    <Gauge
-        chartData={data1}
-        id="gauge-units1"
-        suffix="GB"
-        title="Data Usage"
-        {...props}
-    />
-    <Gauge
-        chartData={data2}
-        id="gauge-units2"
-        prefix="$"
-        suffix="k"
-        title="Sales Goal"
-        {...props}
-    />
-  </div>
-)
+const baseOptions1 = {
+  title: {
+    text: "Data Usage",
+  },
+  series: [{ data: data1 }],
+  plotOptions: {
+    solidgauge: {
+      dataLabels: {
+        format:
+          `<span class="fix">{y:,f}</span>` +
+          `<span class="pb-gauge-suffix">GB</span>`,
+      },
+    },
+  },
+};
+const baseOptions2 = {
+  title: {
+    text: "Sales Goal",
+  },
+  series: [{ data: data2 }],
+  plotOptions: {
+    solidgauge: {
+      dataLabels: {
+        format:
+          `<span class="pb-gauge-prefix" y="28">$</span>` +
+          `<span class="fix" y="38">{y:,f}</span>` +
+          `<span class="pb-gauge-suffix">k</span>`,
+      },
+    },
+  },
+};
 
-export default GaugeUnits
+const GaugeUnits = () => {
+  const options1 = Highcharts.merge({}, gaugeTheme, baseOptions1);
+  const options2 = Highcharts.merge({}, gaugeTheme, baseOptions2);
+
+  return (
+    <div>
+      <HighchartsReact
+          highcharts={Highcharts}
+          id="gauge-units1"
+          options={options1}
+      />
+      <HighchartsReact
+          highcharts={Highcharts}
+          id="gauge-units2"
+          options={options2}
+      />
+    </div>
+  );
+};
+
+export default GaugeUnits;
