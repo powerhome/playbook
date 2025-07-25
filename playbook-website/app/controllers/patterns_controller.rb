9ccca704a9c4b7b69e2f46e3050b0d27fc2527dc
Elisa Shapiro <elisashapiro@gmail.com>
2025-07-25 16:00:35 -0400

    Rails and React Table Filter Card Patterns

--- playbook-website/app/controllers/patterns_controller.rb
+++ playbook-website/app/controllers/patterns_controller.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+require "will_paginate"
+require "playbook/pagination_renderer"
+require "will_paginate/array"
+require "ostruct"
+
 class PatternsController < ApplicationController
   before_action :set_js
   before_action :set_pattern, only: :show
@@ -12,6 +17,20 @@ class PatternsController < ApplicationController
 
   def show
     @show_sidebar = true
+
+    # Required for table_filter_card pattern's pagination bars to appear
+    if params[:name] == "table_filter_card"
+      @users = Array.new(50) do |i|
+        OpenStruct.new(
+          id: i + 1,
+          name: %w[Ashlee Nick Nida Justin Edward Elisa Gary Sam Aaron George][i % 10],
+          title: %w[Nitro Producteer UX Engineer II Senior UX Engineer Director of User Experience Engineering UX Designer UX Engineer UX Engineer Associate Nitro Quality Ninja UX Engineer][i % 10],
+          department: "Business Technology",
+          branch: "Philadelphia",
+          start_date: Date.new(2025, 1, 1) + i.days
+        )
+      end.paginate(page: params[:page], per_page: 10)
+    end
   end
 
   def icons
