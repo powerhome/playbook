7ffd4f27f16dfdfd0157919ddd1014194c4f9781
Gary Kang <42440452+kangaree@users.noreply.github.com>
2025-08-05 11:00:11 -0400

    [PLAY-2283] Advanced Table Kit: Multi-Header Columns with Vertical Borders Doc & Fix Rails Styles (#4949)
    **What does this PR do?** A clear and concise description with your
    runway ticket url.
    
    - Add a "Multi-Header Columns with Vertical Borders" doc. 
    - Remove `chrome-styles` class from Rails Advanced Table Kit
    - This was used inconsistently for headers and cells, and also was
    **always** being added in some cases instead of checking for the browser
    
    https://runway.powerhrg.com/backlog_items/PLAY-2283
    
    **Screenshots:** Screenshots to visualize your addition/change
    <img width="1600" height="250" alt="Screenshot 2025-07-31 at 2 44 13 PM"
    src="https://github.com/user-attachments/assets/3d0ad939-260a-44b2-b7ef-4d5cfa44f18d"
    />
    
    
    **How to test?** Steps to confirm the desired behavior:
    1. Go to
    /kits/column_configuration/rails#multi-header-columns-with-vertical-borders
    2. Review React too
    
    #### Checklist:
    - [x] **LABELS** Add a label: `enhancement`, `bug`, `improvement`, `new
    kit`, `deprecated`, or `breaking`. See [Changelog &
    Labels](https://github.com/powerhome/playbook/wiki/Changelog-&-Labels)
    for details.
    - [x] **DEPLOY** I have added the `milano` label to show I'm ready for a
    review.
    - [ ] **TESTS** I have added test coverage to my code.
    - [x] **SEMVER** I have added a `minor`, `major`, or `patch` label for
    release.
    - [x] **RC** I have added an `inactive RC` label if not an active RC.

---
+++ playbook/app/pb_kits/playbook/pb_advanced_table/docs/_advanced_table_column_headers_vertical_border.jsx
@@ -0,0 +1,64 @@
+import React from "react"
+import AdvancedTable from '../../pb_advanced_table/_advanced_table'
+import MOCK_DATA from "./advanced_table_mock_data.json"
+
+const AdvancedTableColumnHeadersVerticalBorder = (props) => {
+  const columnDefinitions = [
+    {
+      accessor: "year",
+      label: "Year",
+      cellAccessors: ["quarter", "month", "day"],
+    },
+    {
+      label: "Enrollment Data",
+      columns: [
+        {
+          accessor: "newEnrollments",
+          label: "New Enrollments",
+        },
+        {
+          accessor: "scheduledMeetings",
+          label: "Scheduled Meetings",
+        },
+      ],
+    },
+    {
+      label: "Performance Data",
+      columns: [
+        {
+          accessor: "attendanceRate",
+          label: "Attendance Rate",
+        },
+        {
+          accessor: "completedClasses",
+          label: "Completed Classes",
+        },
+        {
+          accessor: "classCompletionRate",
+          label: "Class Completion Rate",
+        },
+        {
+          accessor: "graduatedStudents",
+          label: "Graduated Students",
+        },
+      ],
+    },
+  ];
+  
+  const tableProps = {
+    verticalBorder: true
+  }
+
+  return (
+    <>
+      <AdvancedTable
+          columnDefinitions={columnDefinitions}
+          tableData={MOCK_DATA}
+          tableProps={tableProps}
+          {...props}
+      />
+    </>
+  )
+}
+
+export default AdvancedTableColumnHeadersVerticalBorder
