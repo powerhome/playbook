c95cb004f85733fbfcc8c130bb4ad70d3ed31ab2
skduncan <samkduncan@gmail.com>
2025-08-15 13:05:19 -0500

    Reverting doc examples

--- playbook/app/pb_kits/playbook/pb_date_picker/docs/_date_picker_default.jsx
+++ playbook/app/pb_kits/playbook/pb_date_picker/docs/_date_picker_default.jsx
@@ -5,7 +5,6 @@ import DatePicker from '../_date_picker'
 const DatePickerDefault = (props) => (
   <div>
     <DatePicker
-        cursor="notAllowed"
         pickerId="date-picker-default"
         {...props}
     />
