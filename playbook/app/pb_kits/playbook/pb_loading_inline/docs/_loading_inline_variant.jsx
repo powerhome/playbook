b3c25f4801cc96cda674b2eb93d9adf4b1d7ff45
nickamantia <nick.amantia@powerhrg.com>
2025-08-18 11:09:42 -0400

    fixing docs

--- playbook/app/pb_kits/playbook/pb_loading_inline/docs/_loading_inline_variant.jsx
+++ playbook/app/pb_kits/playbook/pb_loading_inline/docs/_loading_inline_variant.jsx
@@ -4,15 +4,18 @@ import { LoadingInline } from 'playbook-ui'
 const LoadingInlineVariant = (props) => {
   return (
     <div>
-      <LoadingInline 
-          text="Solid Loading" 
-          variant="solid" 
-          {...props}
-      />
-      <br />
+
+  
       <LoadingInline 
           text="Dotted Loading" 
           variant="dotted" 
+          {...props}
+      />
+      <br />
+      
+      <LoadingInline
+          text="Solid Loading"
+          variant="solid"
           {...props}
       />
     </div>
