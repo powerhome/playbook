3b51095aba1c34728018bc50b82602be9fe5654f
Elisa Shapiro <elisashapiro@gmail.com>
2025-08-25 12:24:24 -0400

    Text Field Form Builder custom label is still clickable

--- playbook/lib/playbook/forms/builder/form_field_builder.rb
+++ playbook/lib/playbook/forms/builder/form_field_builder.rb
@@ -6,7 +6,7 @@ module Playbook
       class FormFieldBuilder < Module
         def initialize(method_name, kit_name:)
           define_method method_name do |name, props: {}, **options, &block|
-            props[:label] = @template.label(@object_name, name) if props[:label] == true
+            props   = props.dup
             options = Hash(options)
 
             options[:skip_default_ids] = false unless options.key?(:skip_default_ids)
@@ -20,6 +20,14 @@ module Playbook
             end
 
             input = super(name, **options, &block)
+
+            input_id = input[/\bid="([^"]+)"/, 1] || "#{@object_name}_#{name}"
+
+            if props[:label] == true
+              props[:label] = @template.label(@object_name, name)
+            elsif props[:label].is_a?(String)
+              props[:label] = @template.label_tag(input_id, props[:label])
+            end
 
             @template.pb_rails(kit_name, props: props) do
               input
