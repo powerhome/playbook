54c8e8c5121fea95650c9b7393051f1ecfbf7f53
nidaqg <nidaqg@gmail.com>
2025-08-26 14:19:21 -0400

    Hover page complete

--- playbook-website/app/javascript/components/GlobalPropsAndTokens/ExamplesPage/Examples/Hover.tsx
+++ playbook-website/app/javascript/components/GlobalPropsAndTokens/ExamplesPage/Examples/Hover.tsx
@@ -1,19 +1,58 @@
-import { Flex } from "playbook-ui";
+import { Flex, Card } from "playbook-ui";
 import ShowPage from "../../Templates/ShowPage";
 import PropsExamplesTable from "../../Templates/Subcomponents/PropsExamplesTable";
 import ExampleCodeCard from "../../Templates/Subcomponents/ExampleCodeCard";
 
 const Hover = () => {
+  const shadowValues = ["deep", "deeper", "deepest"];
+  const scaleValues = ["sm", "md", "lg"];
 
-    const shadowValues = ["deep", "deeper", "deepest"];
-    const ShadowValuesCard = () => (
-      <Flex gap="xs" wrap>
-        {shadowValues.map((value) => (
-          <ExampleCodeCard key={value} text={value} copyIcon={false} />
-        ))}
+  const ValuesCards = ({ values }: { values: string[] }) => (
+    <Flex gap="xs" wrap>
+      {values.map((value) => (
+        <ExampleCodeCard key={value} text={value} copyIcon={false} />
+      ))}
+    </Flex>
+  );
+
+  const VisualGuideExample = ({ text, hoverProp }: any) => {
+    return (
+      <>
+        <Card
+          className="visual_guide_card_hover"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          hover={hoverProp}
+          background="light"
+        >
+          {text}
+        </Card>
+      </>
+    );
+  };
+
+  const VisualGuideCard = () => {
+    return (
+      <Flex gap="sm" wrap>
+        <VisualGuideExample text="Color" hoverProp={{ color: "red" }} />
+        <VisualGuideExample
+          text="Background"
+          hoverProp={{ background: "slate" }}
+        />
+        <VisualGuideExample text="Shadow" hoverProp={{ shadow: "deep" }} />
+        <VisualGuideExample text="Scale" hoverProp={{ scale: "md" }} />
+        <VisualGuideExample
+          text="Visibility"
+          hoverProp={{ visibility: "false" }}
+        />
+        <VisualGuideExample
+          text="Underline"
+          hoverProp={{ underline: "true" }}
+        />
       </Flex>
     );
-
+  };
 
   return (
     <ShowPage
@@ -33,6 +72,7 @@ const Hover = () => {
           </a>
         </>
       }
+      VisualGuideCard={VisualGuideCard()}
     >
       <PropsExamplesTable
         headers={["Hover", "Type", "Values", "Rails Example", "React Example"]}
@@ -41,24 +81,80 @@ const Hover = () => {
             "Color",
             <ExampleCodeCard text="string" copyIcon={false} />,
             <ExampleCodeCard text="${color}" copyIcon={false} />,
-            <ExampleCodeCard id="hover-color-rails" text="hover: {color: 'red'}" />,
-            <ExampleCodeCard id="hover-color-react" text="hover= {{color:'red'}}" />,
+            <ExampleCodeCard
+              id="hover-color-rails"
+              text="hover: {color: 'red'}"
+            />,
+            <ExampleCodeCard
+              id="hover-color-react"
+              text="hover= {{color:'red'}}"
+            />,
           ],
-                    [
+          [
             "Background",
             <ExampleCodeCard text="string" copyIcon={false} />,
             <ExampleCodeCard text="${background}" copyIcon={false} />,
-            <ExampleCodeCard id="hover-background-rails" text="hover: {background: 'slate'}" />,
-            <ExampleCodeCard id="hover-background-react" text="hover= {{background:'slate'}}" />,
+            <ExampleCodeCard
+              id="hover-background-rails"
+              text="hover: {background: 'slate'}"
+            />,
+            <ExampleCodeCard
+              id="hover-background-react"
+              text="hover= {{background:'slate'}}"
+            />,
           ],
           [
             "Shadow",
             <ExampleCodeCard text="union" copyIcon={false} />,
-            <ShadowValuesCard />,
-            <ExampleCodeCard id="hover-shadow-rails" text="hover: {shadow: 'deep'}" />,
-            <ExampleCodeCard id="hover-shadow-react" text="hover= {{shadow:'deep'}}" />,
+            <ValuesCards values={shadowValues} />,
+            <ExampleCodeCard
+              id="hover-shadow-rails"
+              text="hover: {shadow: 'deep'}"
+            />,
+            <ExampleCodeCard
+              id="hover-shadow-react"
+              text="hover= {{shadow:'deep'}}"
+            />,
           ],
-
+          [
+            "Scale",
+            <ExampleCodeCard text="union" copyIcon={false} />,
+            <ValuesCards values={scaleValues} />,
+            <ExampleCodeCard
+              id="hover-scale-rails"
+              text="hover: {scale: 'md'}"
+            />,
+            <ExampleCodeCard
+              id="hover-scale-react"
+              text="hover= {{scale:'md'}}"
+            />,
+          ],
+          [
+            "Visibility",
+            <ExampleCodeCard text="boolean" copyIcon={false} />,
+            <ValuesCards values={["true", "false"]} />,
+            <ExampleCodeCard
+              id="hover-visibility-rails"
+              text="hover: {visibility: 'true'}"
+            />,
+            <ExampleCodeCard
+              id="hover-visibility-react"
+              text="hover= {{visibility:'true'}}"
+            />,
+          ],
+          [
+            "Underline",
+            <ExampleCodeCard text="boolean" copyIcon={false} />,
+            <ValuesCards values={["true", "false"]} />,
+            <ExampleCodeCard
+              id="hover-underline-rails"
+              text="hover: {underline: 'true'}"
+            />,
+            <ExampleCodeCard
+              id="hover-underline-react"
+              text="hover= {{underline:'true'}}"
+            />,
+          ],
         ]}
       />
     </ShowPage>
