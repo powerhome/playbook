723a2f301fc5dd1cc50e1cf1ea9cfe6b7596e857
nickamantia <nick.amantia@powerhrg.com>
2025-09-03 14:51:59 -0400

    failed test fix

--- playbook/app/pb_kits/playbook/pb_bar_graph/barGraph.test.js
+++ playbook/app/pb_kits/playbook/pb_bar_graph/barGraph.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '../utilities/test-utils';
-import PbBarGraph from './_pb_bar_graph';
+import BarGraph from './_bar_graph';
 
 beforeEach(() => {
   // Silences error logs within the test suite.
@@ -19,7 +19,7 @@ const testId = 'bargraph1';
 
 test('bargraph uses exact classname', () => {
   render(
-    <PbBarGraph
+    <BarGraph
         className='super_important_class'
         data={{ testid: testId }}
         id='bar-default'
