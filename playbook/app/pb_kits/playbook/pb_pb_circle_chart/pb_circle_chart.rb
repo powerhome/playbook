7ab10aff9f120dd31fc0d66892a0d2a00f6cb78a
nidaqg <nidaqg@gmail.com>
2025-09-11 12:11:20 -0400

    First rails doc

--- playbook/app/pb_kits/playbook/pb_pb_circle_chart/pb_circle_chart.rb
+++ playbook/app/pb_kits/playbook/pb_pb_circle_chart/pb_circle_chart.rb
@@ -3,6 +3,23 @@
 module Playbook
   module PbPbCircleChart
     class PbCircleChart < ::Playbook::KitBase
+      prop :options, default: {}
+      prop :container_props, default: {}
+
+      def react_props
+        {
+          options: options,
+          containerProps: container_props_hash,
+        }
+      end
+
+      def container_props_hash
+        container_props.merge({
+                                id: id,
+                                className: classname,
+                              }).compact
+      end
+
       def classname
         generate_classname("pb_pb_circle_chart")
       end
