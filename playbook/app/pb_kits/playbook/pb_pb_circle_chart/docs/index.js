d37562a1e1c2fa68143bd145928fa338fa89c05d
nidaqg <nidaqg@gmail.com>
2025-09-11 09:38:16 -0400

    Added second react doc example

--- playbook/app/pb_kits/playbook/pb_pb_circle_chart/docs/index.js
+++ playbook/app/pb_kits/playbook/pb_pb_circle_chart/docs/index.js
@@ -1,1 +1,2 @@
 export { default as PbCircleChartDefault } from './_pb_circle_chart_default.jsx'
+export { default as PbCircleChartLiveData } from './_pb_circle_chart_live_data.jsx'
--- playbook/app/pb_kits/playbook/pb_pb_circle_chart/pb_circle_chart.test.jsx
+++ playbook/app/pb_kits/playbook/pb_pb_circle_chart/pb_circle_chart.test.jsx
@@ -3,9 +3,49 @@ import { render, screen } from '../utilities/test-utils'
 
 import { PbCircleChart } from 'playbook-ui'
 
+beforeEach(() => {
+  // Silences error logs within the test suite.
+  jest.spyOn(console, 'error');
+  jest.spyOn(console, 'warn');
+  console.error.mockImplementation(() => {});
+  console.warn.mockImplementation(() => {});
+});
 
-test('generated scaffold test - update me', () => {
-  render(<PbCircleChart data={{ testid: 'default' }} />)
-  expect(screen.getByTestId('default')).toBeInTheDocument()
+afterEach(() => {
+  console.error.mockRestore();
+  console.warn.mockRestore();
+});
+
+const chartOptions = {
+  series: [
+    {
+      data: [
+        {
+          name: "Waiting for Calls",
+          y: 41,
+        },
+        {
+          name: "On Call",
+          y: 49,
+        },
+        {
+          name: "After Call",
+          y: 10,
+        },
+      ],
+    },
+  ],
+};
+
+const testId = 'pbcirclechart1';
+
+test('Kit to exist', () => {
+  render(
+  <PbCircleChart 
+      data={{testid: testId}} 
+      options={chartOptions} 
+  />
+)
+  expect(screen.getByTestId(testId)).toBeInTheDocument()
 })
 
