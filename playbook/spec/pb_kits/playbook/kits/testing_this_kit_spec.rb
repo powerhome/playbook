e15b19477a1f7cb0f4b22f044f10b0be99c37d32
nidaqg <nidaqg@gmail.com>
2025-09-12 12:02:49 -0400

    Fixes for kit generator

---
+++ playbook/spec/pb_kits/playbook/kits/testing_this_kit_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "../../../../app/pb_kits/playbook/pb_testing_this_kit/testing_this_kit"
+
+RSpec.describe Playbook::PbTestingThisKit::TestingThisKit do
+  subject { Playbook::PbTestingThisKit::TestingThisKit }
+
+  # Do not leave this file blank. Use other spec files for example tests.
+end
