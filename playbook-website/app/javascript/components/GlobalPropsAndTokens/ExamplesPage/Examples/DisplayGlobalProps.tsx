d6fcf5104fadb60a2c5f757dcb8bd3d0c66794f9
nickamantia <nick.amantia@powerhrg.com>
2025-09-23 16:38:22 -0400

    more feedback

--- playbook-website/app/javascript/components/GlobalPropsAndTokens/ExamplesPage/Examples/DisplayGlobalProps.tsx
+++ playbook-website/app/javascript/components/GlobalPropsAndTokens/ExamplesPage/Examples/DisplayGlobalProps.tsx
@@ -95,20 +95,21 @@ const sizes = {
     <Caption text="Responsiveness"/>
     <Body text="This global support responsive values using a breakpoint-based syntax that allows you to define different property values at different screen sizes. This makes it easy to create adaptive layouts without writing custom media queries."/>
     <Body marginY="md"text="Responsive values are defined using an object with screen size keys and corresponding property values. You can optionally include a default value that acts as a fallback when no breakpoint-specific value is matched. It looks like this:"/>
-    <Flex align="baseline" gap="xs">
+    <Flex align="baseline" gap="xs" marginBottom="sm">
         <Caption text="Rails"/>
         <Body>
-             <ExampleCodeCard id="display-rails" text={`display:"${values.join(' ')}"`} />
+             <ExampleCodeCard id="display-rails" text={`display:{ xs: "flex", sm: "flex", md: "flex", lg: "block", xl: "block", default: "block" }`} />
         </Body>
     </Flex>
     <Flex align="baseline" gap="xs">
         <Caption text="React"/>
         <Body  >
-            <ExampleCodeCard id="display-react" text={`display="${values.join(' ')}"`} />
+            <ExampleCodeCard id="display-react" text={`display={{ xs: "flex", sm: "flex", md: "flex", lg: "block", xl: "block", default: “block” }}`} />
         </Body>
     </Flex>
     <Body marginY="md"text="The supported screen size keys are:"/>
       <PropsExamplesTable
+        firstColumnBold={false}
         headers={[
         "Breakpoints",
         "Value",
