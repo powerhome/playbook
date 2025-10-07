a540b5c4ecc923f83c1833943f6cb7340d86abe7
nidaqg <nidaqg@gmail.com>
2025-10-07 13:41:00 -0400

    Added template example

--- playbook/app/pb_kits/playbook/pb_rich_text_editor/docs/index.js
+++ playbook/app/pb_kits/playbook/pb_rich_text_editor/docs/index.js
@@ -13,3 +13,4 @@ export { default as RichTextEditorToolbarDisabled } from './_rich_text_editor_to
 export { default as RichTextEditorAdvancedPreview } from './_rich_text_editor_advanced_preview.jsx'
 export { default as RichTextEditorAdvancedSimple } from './_rich_text_editor_advanced_simple.jsx'
 export { default as RichTextEditorAdvancedFocus } from './_rich_text_editor_advanced_focus.jsx'
+export { default as RichTextEditorAdvancedTemplates } from './_rich_text_editor_advanced_templates.jsx'
