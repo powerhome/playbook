37244fde393be284820168c080bb4414054446e4
nidaqg <nidaqg@gmail.com>
2025-10-09 11:16:24 -0400

    legend and legend position docs

--- playbook/app/pb_kits/playbook/pb_pb_line_graph/docs/index.js
+++ playbook/app/pb_kits/playbook/pb_pb_line_graph/docs/index.js
@@ -1,1 +1,3 @@
 export { default as PbLineGraphDefault } from './_pb_line_graph_default.jsx'
+export { default as PbLineGraphLegend } from './_pb_line_graph_legend.jsx'
+export { default as PbLineGraphLegendPosition } from './_pb_line_graph_legend_position.jsx'
