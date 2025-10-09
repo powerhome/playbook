d096d868c40d4dbeda659719ac9b2b5d045e5161
nidaqg <nidaqg@gmail.com>
2025-10-09 10:50:34 -0400

    Update test

--- playbook/app/pb_kits/playbook/pb_pb_line_graph/pb_line_graph.test.jsx
+++ playbook/app/pb_kits/playbook/pb_pb_line_graph/pb_line_graph.test.jsx
@@ -3,16 +3,61 @@ import { render, screen } from '../utilities/test-utils'
 
 import { PbLineGraph } from 'playbook-ui'
 
-/* See these resources for more testing info:
-  - https://github.com/testing-library/jest-dom#usage for useage and examples
-  - https://jestjs.io/docs/en/using-matchers
-*/
+beforeEach(() => {
+  // Silences error logs within the test suite.
+  jest.spyOn(console, 'error');
+  jest.spyOn(console, 'warn');
+  console.error.mockImplementation(() => {});
+  console.warn.mockImplementation(() => {});
+});
 
-test('generated scaffold test - update me', () => {
-  const props = {
-    data: { testid: 'default' }
-  }
+afterEach(() => {
+  console.error.mockRestore();
+  console.warn.mockRestore();
+});
 
-  render(<PbLineGraph {...props} />)
-    expect(screen.getByTestId('default')).toBeInTheDocument()
+const seriesData = [{
+  name: 'Installation',
+  data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175],
+}, {
+  name: 'Manufacturing',
+  data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434],
+}, {
+  name: 'Sales & Distribution',
+  data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387],
+}, {
+  name: 'Project Development',
+  data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227],
+}, {
+  name: 'Other',
+  data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111],
+}]
+
+const categories = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+const chartOptions = {
+  series: seriesData,
+  title: { text: "Solar Employment Growth by Sector, 2010-2016" },
+  subtitle: { text: "Source: thesolarfoundation.com" },
+  xAxis: {
+    categories: categories,
+  },
+  yAxis: {
+    title: {
+      text: "Number of Employees",
+    },
+  },
+}
+
+const testId = 'pblinegraph1';
+
+test('Kit to exist', () => {
+  render(
+  <PbLineGraph 
+      data={{testid: testId}} 
+      options={chartOptions} 
+  />
+)
+
+  expect(screen.getByTestId(testId)).toBeInTheDocument()
 })
