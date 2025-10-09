ca6df756aab626345bd79a0862f03c798e2ba8c4
Sam Duncan <samkduncan@gmail.com>
2025-10-09 11:37:07 -0500

    [PLAY-2437] Global Props: Create Text Align, Position and Z-Index pages (#5309)
    **What does this PR do?** A clear and concise description with your
    runway ticket url.
    [Runway Story](https://runway.powerhrg.com/backlog_items/PLAY-2437)
    
    This PR adds the following to our Global Props pages:
    * /global_props/position
    * /global_props/text_align
    * /global_props/z_index
    
    **Screenshots:** Screenshots to visualize your addition/change
    
    
    **How to test?** Steps to confirm the desired behavior:
    1. Go to '[Position Global Prop
    Page](https://pr5309.playbook.wc.beta.px.powerapp.cloud/global_props/position)'
    2. Go to '[Text Align Global Prop
    Page](https://pr5309.playbook.wc.beta.px.powerapp.cloud/global_props/text_align)'
    3. Go to '[Z-Index Global Prop
    Page](https://pr5309.playbook.wc.beta.px.powerapp.cloud/global_props/z_index)'
    
    
    #### Checklist:
    - [x] **LABELS** Add a label: `enhancement`, `bug`, `improvement`, `new
    kit`, `deprecated`, or `breaking`. See [Changelog &
    Labels](https://github.com/powerhome/playbook/wiki/Changelog-&-Labels)
    for details.
    - [x] **DEPLOY** I have added the `milano` label to show I'm ready for a
    review.
    - [x] **SEMVER** I have added a `minor`, `major`, or `patch` label for
    release.
    - [ ] **RC** I have added an `inactive RC` label if not an active RC.

--- playbook-website/app/javascript/components/GlobalPropsAndTokens/ExamplesPage/Examples/DisplayGlobalProps.tsx
+++ playbook-website/app/javascript/components/GlobalPropsAndTokens/ExamplesPage/Examples/DisplayGlobalProps.tsx
@@ -93,7 +93,7 @@ const sizes = {
 
 <Card>
     <Caption text="Responsiveness"/>
-    <Body text="This global support responsive values using a breakpoint-based syntax that allows you to define different property values at different screen sizes. This makes it easy to create adaptive layouts without writing custom media queries."/>
+    <Body text="This global prop supports responsive values using a breakpoint-based syntax that allows you to define different property values at different screen sizes. This makes it easy to create adaptive layouts without writing custom media queries."/>
     <Body marginY="md"text="Responsive values are defined using an object with screen size keys and corresponding property values. You can optionally include a default value that acts as a fallback when no breakpoint-specific value is matched. It looks like this:"/>
     <Flex align="baseline" gap="xs" marginBottom="sm">
         <Caption text="Rails"/>
---
+++ playbook-website/app/javascript/components/GlobalPropsAndTokens/ExamplesPage/Examples/PositionGlobalProps.tsx
@@ -0,0 +1,181 @@
+import { Flex, Card, Body } from "playbook-ui";
+
+import ShowPage from "../../Templates/ShowPage";
+import PropsExamplesTable from "../../Templates/Subcomponents/PropsExamplesTable";
+import ExampleCodeCard from "../../Templates/Subcomponents/ExampleCodeCard";
+import HeaderWithIcon from "../../Templates/Subcomponents/HeaderWithIcon";
+import ValueCardWithTooltip from "../../Templates/Subcomponents/ValueCardWithTooltip";
+import * as PositionImages from './PositionImages'
+
+type PositionKey = keyof typeof PositionImages;
+
+const Position = () => {
+  const VisualGuideExample = ({ position, positionSvg }: { position: any, positionSvg: PositionKey }) => {
+    return (
+      <>
+        <Flex orientation="column" alignItems="center" gap="xs">
+          <Card
+            className="visual_guide_card_position"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            padding="xxs"
+          >
+            <img
+              alt={`Display example of ${position}`}
+              src={PositionImages[positionSvg]}
+            />
+          </Card>
+          <Body text={position} />
+        </Flex>
+      </>
+    )
+  }
+
+  const PositionCard = () => {
+      return (
+        <Flex gap="xs">
+          <VisualGuideExample position="Static" positionSvg="staticSvg" />
+          <VisualGuideExample position="Relative" positionSvg="relative" />
+          <VisualGuideExample position="Absolute" positionSvg="absolute" />
+          <VisualGuideExample position="Fixed" positionSvg="fixed" />
+          <VisualGuideExample position="Sticky" positionSvg="sticky" />
+        </Flex>
+      );
+  };
+
+  const PositionValues = [
+    "relative",
+    "absolute",
+    "fixed",
+    "sticky",
+    "static",
+  ];
+
+  const SpacingValues = [
+    { name: "0"},
+    { name: "xxs", value: "4px" },
+    { name: "xs", value: "8px" },
+    { name: "sm", value: "16px" },
+    { name: "md", value: "24px" },
+    { name: "lg", value: "32px" },
+    { name: "xl", value: "40px" },
+  ];
+
+  const PositionValuesCards = () => {
+    return (
+      <Flex gap="xs" wrap>
+        {PositionValues.map((value) => (
+          <ExampleCodeCard text={value} copyIcon={false} />
+        ))}
+      </Flex>
+    );
+  };
+
+  const SpacingValuesCards = () => {
+    return (
+      <Flex gap="xs" wrap>
+        {SpacingValues.map((spacing) => (
+          <ValueCardWithTooltip
+            key={spacing.name}
+            text={spacing.name}
+            tooltipText={spacing.value}
+          />
+        ))}
+      </Flex>
+    );
+  };
+
+  return (
+    <ShowPage
+      title="Position"
+      description={
+        <>
+          The Position prop controls how an element is placed on a page and
+          how it interacts with other elements within the viewport or layout.
+          For more information on Position prop controls, refer to the{" "}
+          <a
+            href="https://developer.mozilla.org/en-US/docs/Web/CSS/position"
+            target="_blank"
+          >
+            MDN document found here.
+          </a>
+        </>
+      }
+      VisualGuideCard={PositionCard()}
+    >
+      <PropsExamplesTable
+        headers={[
+          "Prop",
+          "Type",
+          <HeaderWithIcon />,
+          "Rails Example",
+          "React Example",
+        ]}
+        rows={[
+          [
+            "Position",
+            <ExampleCodeCard text="union" copyIcon={false} />,
+            <PositionValuesCards />,
+            <ExampleCodeCard id="position-rails" text="position: 'absolute'" />,
+            <ExampleCodeCard id="position-react" text="position= 'absolute" />,
+          ],
+          [
+            "Top",
+            <ExampleCodeCard text="union" copyIcon={false} />,
+            <SpacingValuesCards />,
+            <ExampleCodeCard
+              id="top-rails"
+              text="top: 'sm'"
+            />,
+            <ExampleCodeCard
+              id="top-react"
+              text="top= 'sm'"
+            />,
+          ],
+          [
+            "Bottom",
+            <ExampleCodeCard text="union" copyIcon={false} />,
+            <SpacingValuesCards />,
+            <ExampleCodeCard
+              id="bottom-rails"
+              text="bottom: 'sm'"
+            />,
+            <ExampleCodeCard
+              id="bottom-react"
+              text="bottom= 'sm'"
+            />,
+          ],
+          [
+            "Left",
+            <ExampleCodeCard text="union" copyIcon={false} />,
+            <SpacingValuesCards />,
+            <ExampleCodeCard
+              id="left-rails"
+              text="left: 'sm'"
+            />,
+            <ExampleCodeCard
+              id="left-react"
+              text="left= 'sm'"
+            />,
+          ],
+          [
+            "Right",
+            <ExampleCodeCard text="union" copyIcon={false} />,
+            <SpacingValuesCards />,
+            <ExampleCodeCard
+              id="right-rails"
+              text="right: 'sm'"
+            />,
+            <ExampleCodeCard
+              id="right-react"
+              text="right= 'sm'"
+            />,
+          ],
+        ]}
+      />
+    </ShowPage>
+  );
+};
+
+export default Position;
