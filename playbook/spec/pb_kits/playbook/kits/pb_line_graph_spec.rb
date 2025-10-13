b196c420ae258eab5a8145118b32f51c8351ddc7
nidaqg <nidaqg@gmail.com>
2025-10-13 13:30:27 -0400

    Updated rspec tests

--- playbook/spec/pb_kits/playbook/kits/pb_line_graph_spec.rb
+++ playbook/spec/pb_kits/playbook/kits/pb_line_graph_spec.rb
@@ -5,5 +5,33 @@ require_relative "../../../../app/pb_kits/playbook/pb_pb_line_graph/pb_line_grap
 RSpec.describe Playbook::PbPbLineGraph::PbLineGraph do
   subject { Playbook::PbPbLineGraph::PbLineGraph }
 
-  # Do not leave this file blank. Use other spec files for example tests.
+  it { is_expected.to define_prop(:options) }
+
+  describe "#classname_and_id" do
+    it "returns empty string for classname since it's handled in React", :aggregate_failures do
+      expect(subject.new.classname).to eq ""
+      expect(subject.new(classname: "additional_class").classname).to eq " additional_class"
+    end
+
+    it "passes className through react_props" do
+      expect(subject.new.react_props[:className]).to eq ""
+      expect(subject.new(classname: "custom-class").react_props[:className]).to eq " custom-class"
+    end
+
+    it "attaches id as expected" do
+      expect(subject.new(id: "test-id").react_props[:id]).to eq "test-id"
+    end
+  end
+
+  describe "#react_props includes data" do
+    it "includes data in react_props" do
+      expect(subject.new(data: { test: "value" }).react_props[:data]).to eq({ test: "value" })
+    end
+  end
+
+  describe "#global props work as expected" do
+    it "global props work" do
+      expect(subject.new(margin: "lg").classname).to eq(" m_lg")
+    end
+  end
 end
