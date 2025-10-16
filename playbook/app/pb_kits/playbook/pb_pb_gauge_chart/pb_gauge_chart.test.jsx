f81d25001d8807468f0047262a3e3cb29c3215b3
nidaqg <nidaqg@gmail.com>
2025-10-16 15:40:05 -0400

    Add more jest tests

--- playbook/app/pb_kits/playbook/pb_pb_gauge_chart/pb_gauge_chart.test.jsx
+++ playbook/app/pb_kits/playbook/pb_pb_gauge_chart/pb_gauge_chart.test.jsx
@@ -31,3 +31,50 @@ test('Kit to exist', () => {
 )
   expect(screen.getByTestId(testId)).toBeInTheDocument()
 })
+
+test('Kit to apply base classname', () => {
+  render(
+    <PbGaugeChart
+        data={{testid: testId}} 
+        options={chartOptions} 
+    />
+  )
+
+  expect(screen.getByTestId(testId)).toHaveClass('pb_pb_gauge_chart')
+})
+
+test('Kit to have custom class', () => {
+  render(
+    <PbGaugeChart
+        className='custom-class'
+        data={{testid: testId}} 
+        options={chartOptions} 
+    />
+  )
+
+  expect(screen.getByTestId(testId)).toHaveClass('custom-class')
+})
+
+test('Kit to apply global props', () => {
+  render(
+    <PbGaugeChart
+        data={{testid: testId}} 
+        margin="lg"
+        options={chartOptions} 
+    />
+  )
+
+  expect(screen.getByTestId(testId)).toHaveClass('m_lg')
+})
+
+test('kit to apply id', () => {
+  render(
+    <PbGaugeChart
+        data={{testid: testId}} 
+        id='gauge-chart-id'
+        options={chartOptions} 
+    />
+  )
+
+  expect(screen.getByTestId(testId)).toHaveAttribute('id', 'gauge-chart-id')
+})
