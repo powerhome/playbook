36c52c0f3ec76e5e9dd9d811b761f86d3634e585
Elisa Shapiro <elisashapiro@gmail.com>
2025-10-17 08:17:27 -0400

    Add unique ids to the copy parts of other docs so they copy

--- playbook-website/app/javascript/components/GlobalPropsAndTokens/ExamplesPage/Examples/Z-IndexGlobalProps.tsx
+++ playbook-website/app/javascript/components/GlobalPropsAndTokens/ExamplesPage/Examples/Z-IndexGlobalProps.tsx
@@ -129,8 +129,8 @@ const ZIndex = () => {
                   "Z-Index",
                   <ExampleCodeCard text="union" copyIcon={false} />,
                   <ValuesCards />,
-                  <ExampleCodeCard text={`z_index:"5"`} />,
-                  <ExampleCodeCard text={`zIndex="5"`} />
+                  <ExampleCodeCard id="z-index-rails" text={`z_index:"5"`} />,
+                  <ExampleCodeCard id="z-index-react" text={`zIndex="5"`} />
                 ]
               ]
             }
