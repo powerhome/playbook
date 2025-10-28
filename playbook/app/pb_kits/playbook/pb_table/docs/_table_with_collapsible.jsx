412e44d3b1d26e2eb2aa44578d9fb6e29fddddec
nickamantia <nick.amantia@powerhrg.com>
2025-10-28 13:02:56 -0400

    docs updated

--- playbook/app/pb_kits/playbook/pb_table/docs/_table_with_collapsible.jsx
+++ playbook/app/pb_kits/playbook/pb_table/docs/_table_with_collapsible.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Table from '../../pb_table/_table'
 import Icon from '../../pb_icon/_icon'
 import Card from '../../pb_card/_card'
@@ -6,9 +6,15 @@ import Body from '../../pb_body/_body'
 
 const TableWithCollapsible = (props) => {
 
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggleArrows = () => {
+    setIsOpen(!isOpen)
+  }
+
   const Content = () => {
     return (
-      <Card 
+      <Card
           borderNone
           borderRadius="none"
           padding="md"
@@ -19,59 +25,65 @@ const TableWithCollapsible = (props) => {
     );
   };
 
-  return (
-      <Table
-          size="sm"
-          {...props}
-      >
-        <Table.Head>
-          <Table.Row>
-            <Table.Header>{'Column 1'}</Table.Header>
-            <Table.Header>{'Column 2'}</Table.Header>
-            <Table.Header>{'Column 3'}</Table.Header>
-            <Table.Header>{'Column 4'}</Table.Header>
-            <Table.Header>{'Column 5'}</Table.Header>
-            <Table.Header>{''}</Table.Header>
-          </Table.Row>
 
-        </Table.Head>
-        <Table.Body>
-          <Table.Row collapsible 
-              collapsibleContent={<Content/>}
-              {...props}
+  return (
+    <Table
+        size="sm"
+        {...props}
+    >
+      <Table.Head>
+        <Table.Row>
+          <Table.Header>{'Column 1'}</Table.Header>
+          <Table.Header>{'Column 2'}</Table.Header>
+          <Table.Header>{'Column 3'}</Table.Header>
+          <Table.Header>{'Column 4'}</Table.Header>
+          <Table.Header>{'Column 5'}</Table.Header>
+          <Table.Header>{''}</Table.Header>
+        </Table.Row>
+
+      </Table.Head>
+      <Table.Body>
+
+        <Table.Row collapsible
+            collapsibleContent={<Content />}
+            {...props}
+        >
+          <Table.Cell htmlOptions={{ onClick: handleToggleArrows }}>{'Value 1'}</Table.Cell>
+          <Table.Cell htmlOptions={{ onClick: handleToggleArrows }}>{'Value 2'}</Table.Cell>
+          <Table.Cell htmlOptions={{ onClick: handleToggleArrows }}>{'Value 3'}</Table.Cell>
+          <Table.Cell htmlOptions={{ onClick: handleToggleArrows }}>{'Value 4'}</Table.Cell>
+          <Table.Cell htmlOptions={{ onClick: handleToggleArrows }}>{'Value 5'}</Table.Cell>
+          <Table.Cell
+              htmlOptions={{ onClick: handleToggleArrows }}
+              textAlign="right"
           >
-            <Table.Cell>{'Value 1'}</Table.Cell>
-            <Table.Cell>{'Value 2'}</Table.Cell>
-            <Table.Cell>{'Value 3'}</Table.Cell>
-            <Table.Cell>{'Value 4'}</Table.Cell>
-            <Table.Cell>{'Value 5'}</Table.Cell>
-            <Table.Cell textAlign="right">{ 
+            {
               <Icon
                   color="primary"
                   fixedWidth
-                  icon="chevron-down"
+                  icon={isOpen ? 'chevron-up' : 'chevron-down'}
               />}
-            </Table.Cell>
+          </Table.Cell>
 
-          </Table.Row>
-          <Table.Row>
-            <Table.Cell>{'Value 1'}</Table.Cell>
-            <Table.Cell>{'Value 2'}</Table.Cell>
-            <Table.Cell>{'Value 3'}</Table.Cell>
-            <Table.Cell>{'Value 4'}</Table.Cell>
-            <Table.Cell>{'Value 5'}</Table.Cell>
-            <Table.Cell>{''}</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.Cell>{'Value 1'}</Table.Cell>
-            <Table.Cell>{'Value 2'}</Table.Cell>
-            <Table.Cell>{'Value 3'}</Table.Cell>
-            <Table.Cell>{'Value 4'}</Table.Cell>
-            <Table.Cell>{'Value 5'}</Table.Cell>
-            <Table.Cell>{''}</Table.Cell>
-          </Table.Row>
-        </Table.Body>
-      </Table>
+        </Table.Row>
+        <Table.Row>
+          <Table.Cell>{'Value 1'}</Table.Cell>
+          <Table.Cell>{'Value 2'}</Table.Cell>
+          <Table.Cell>{'Value 3'}</Table.Cell>
+          <Table.Cell>{'Value 4'}</Table.Cell>
+          <Table.Cell>{'Value 5'}</Table.Cell>
+          <Table.Cell>{''}</Table.Cell>
+        </Table.Row>
+        <Table.Row>
+          <Table.Cell>{'Value 1'}</Table.Cell>
+          <Table.Cell>{'Value 2'}</Table.Cell>
+          <Table.Cell>{'Value 3'}</Table.Cell>
+          <Table.Cell>{'Value 4'}</Table.Cell>
+          <Table.Cell>{'Value 5'}</Table.Cell>
+          <Table.Cell>{''}</Table.Cell>
+        </Table.Row>
+      </Table.Body>
+    </Table>
   )
 }
 
