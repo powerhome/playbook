70226594bedbc4ba044b5053089c42e4a9f22250
nidaqg <nidaqg@gmail.com>
2025-11-04 09:09:00 -0500

    Focus state for pills or badges in Typeahead

--- playbook/app/pb_kits/playbook/pb_typeahead/components/MultiValue.tsx
+++ playbook/app/pb_kits/playbook/pb_typeahead/components/MultiValue.tsx
@@ -12,11 +12,12 @@ type Props = {
   pillColor?: "primary" | "neutral" | "success" | "warning" | "error" | "info" | "data_1" | "data_2" | "data_3" | "data_4" | "data_5" | "data_6" | "data_7" | "data_8" | "windows" | "siding" | "roofing" | "doors" | "gutters" | "solar" | "insulation" | "accessories",
   removeProps: any,
   selectProps: any,
+  isFocused?: boolean,
 }
 
 
 const MultiValue = (props: Props) => {
-  const { removeProps } = props
+  const { removeProps, isFocused } = props
   const { imageUrl, label } = props.data
   const { dark, multiKit, pillColor, truncate, wrapped } = props.selectProps
 
@@ -29,6 +30,9 @@ const MultiValue = (props: Props) => {
 
   if (typeof imageUrl === 'string') formPillProps.avatarUrl = imageUrl
 
+  // Add className for focus state
+  const pillClassName = isFocused ? 'pb_form_pill_or_badge_focused' : ''
+
   return (
     <components.MultiValueContainer
         className="text_input_multivalue_container"
@@ -36,6 +40,7 @@ const MultiValue = (props: Props) => {
     >
       {multiKit === 'badge' &&
         <Badge
+            className={pillClassName}
             closeProps={removeProps}
             removeIcon
             text={label}
@@ -46,12 +51,14 @@ const MultiValue = (props: Props) => {
       {multiKit !== 'badge' && imageUrl &&
         <FormPill
             avatarUrl={imageUrl}
+            className={pillClassName}
             closeProps={removeProps}
             color={pillColor}
             dark={dark}
             marginRight="xs"
             name={label}
             size={multiKit === 'smallPill' ? 'small' : ''}
+            tabIndex={0}
             text=''
             truncate={truncate}
             wrapped={wrapped}
@@ -61,12 +68,14 @@ const MultiValue = (props: Props) => {
 
       {multiKit !== 'badge' && !imageUrl &&
         <FormPill
+            className={pillClassName}
             closeProps={removeProps}
             color={pillColor}
             dark={dark}
             marginRight="xs"
             name=''
             size={multiKit === 'smallPill' ? 'small' : ''}
+            tabIndex={0}
             text={label}
             truncate={truncate}
             wrapped={wrapped}
