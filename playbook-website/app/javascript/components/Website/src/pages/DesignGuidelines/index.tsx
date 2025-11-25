e1af6216a14108873feb26c89ca785438110613e
nidaqg <nidaqg@gmail.com>
2025-11-25 14:25:02 -0500

    Headers

--- playbook-website/app/javascript/components/Website/src/pages/DesignGuidelines/index.tsx
+++ playbook-website/app/javascript/components/Website/src/pages/DesignGuidelines/index.tsx
@@ -1,80 +1,89 @@
 import { useLoaderData, Link } from "react-router-dom"
 import ReactMarkdown from 'react-markdown'
-import { Flex, FlexItem, Background, Title, SectionSeparator, Layout, Card, IconCircle, Body, Icon } from "playbook-ui"
+import { Flex, Background, Title, Layout, Card, IconCircle, Body, Icon } from "playbook-ui"
+import HeaderImage from "../../../../../images/getting-started.svg"
 
 const DesignGuidelines = () => {
   const loaderData = useLoaderData() as any
   const { design_guidelines_content, design_guidelines } = loaderData
 
-  //------- remove once legacy webasite is gone --------------
+  //------- remove once legacy website is gone --------------
   let contentWithoutFrontmatter = design_guidelines_content?.replace(/^---[\s\S]*?---\n/, '') || ''
   contentWithoutFrontmatter = contentWithoutFrontmatter.replace(/<div>\s*<%=[\s\S]*?%>\s*<\/div>/g, '')
-  //------- remove once legacy webasite is gone --------------
+  //------- remove once legacy website is gone --------------
 
   return (
-    <Flex orientation="column" align="center">
-      <FlexItem className="minw0 w100">
-        <Background
-          display="flex"
-          justifyContent="center"
-          className="markdown w100"
-          backgroundColor="white"
-          paddingX="sm"
-          paddingY="md"
-        >
-          <Background maxWidth="md" backgroundColor="white">
-            <Title text="Design Guidelines" tag="h1" size={1} />
-            <SectionSeparator flexGrow={1} />
-            <div className="markdown-content">
-              <ReactMarkdown>{contentWithoutFrontmatter}</ReactMarkdown>
-            </div>
-            
-            {/* Render card grid of sub-pages */}
-            {design_guidelines?.pages && design_guidelines.pages.length > 0 && (
-              <Layout layout="collection" marginY="xl" paddingBottom="xl">
-                <Layout.Body>
-                  {design_guidelines.pages.map((page: any) => {
-                    const icon = page.frontmatter?.icon || 'files'
-                    const description = page.frontmatter?.description || ''
-                    // Convert legacy URL to beta URL
-                    const betaUrl = page.url.replace('guides/design_guidelines/', '/beta/guides/design_guidelines/')
-                    
-                    return (
-                      <Link 
-                        key={page.page_id} 
-                        to={betaUrl}
-                        className="display_flex align_items_stretch"
-                      >
-                        <Card padding="none" hover={{ shadow: "deep" }} flex={1}>
-                          <Background>
-                            <Flex justify="center" padding="xl">
-                              <IconCircle icon={icon} variant="royal" />
-                            </Flex>
-                          </Background>
-                          <Flex justify="between" align="center" padding="sm">
-                            <Title size={4} color="link" text={page.title} />
-                            <Title size={4} color="link">
-                              <Icon icon="arrow-right-long" />
-                            </Title>
-                          </Flex>
-                          <Body 
-                            text={description} 
-                            truncate="4" 
-                            color="light" 
-                            marginX="sm" 
-                            marginBottom="sm" 
-                          />
-                        </Card>
-                      </Link>
-                    )
-                  })}
-                </Layout.Body>
-              </Layout>
-            )}
-          </Background>
-        </Background>
-      </FlexItem>
-    </Flex>
+    <Background
+      flexDirection="column"
+      backgroundColor="white"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Background
+        imageUrl={HeaderImage}
+        width="100%"
+        display={{ xs: "none", sm: "none", md: "none" }}
+        height="250px"
+        backgroundColor="dark"
+        backgroundSize="contain"
+        backgroundPosition="center"
+        backgroundRepeat="no-repeat"
+      />
+      <Background
+        paddingX={{ xs: "sm", sm: "sm", md:"sm", lg: "sm", default:"none" }}
+        paddingY="md"
+        backgroundColor="white"
+        maxWidth="lg"
+      >
+        <Title size={1} text="Design Guidelines" marginBottom="sm" />
+        <div className="markdown-content">
+          <ReactMarkdown>{contentWithoutFrontmatter}</ReactMarkdown>
+        </div>
+        
+        {/* Render card grid of sub-pages */}
+        {design_guidelines?.pages && design_guidelines.pages.length > 0 && (
+          <Layout layout="collection" marginY="xl" paddingBottom="xl">
+            <Layout.Body>
+              {design_guidelines.pages.map((page: any) => {
+                const icon = page.frontmatter?.icon || 'files'
+                const description = page.frontmatter?.description || ''
+                // Convert legacy URL to beta URL
+                const betaUrl = page.url.replace('guides/design_guidelines/', '/beta/guides/design_guidelines/')
+                
+                return (
+                  <Link 
+                    key={page.page_id} 
+                    to={betaUrl}
+                  >
+                    <Card padding="none" hover={{ shadow: "deep" }} flex={1} minHeight="300px">
+                      <Background backgroundColor="light">
+                        <Flex justify="center" padding="xl">
+                          <IconCircle icon={icon} variant="royal" />
+                        </Flex>
+                      </Background>
+                      <Flex justify="between" align="center" padding="sm">
+                        <Title size={4} color="link" text={page.title} />
+                        <Title size={4} color="link">
+                          <Icon icon="arrow-right-long" />
+                        </Title>
+                      </Flex>
+                      <Body 
+                        text={description} 
+                        truncate="4" 
+                        color="light" 
+                        marginX="sm" 
+                        marginBottom="sm" 
+                      />
+                    </Card>
+                  </Link>
+                )
+              })}
+            </Layout.Body>
+          </Layout>
+        )}
+      </Background>
+    </Background>
   )
 }
 
--- playbook-website/app/javascript/components/Website/src/pages/GettingStarted/index.tsx
+++ playbook-website/app/javascript/components/Website/src/pages/GettingStarted/index.tsx
@@ -1,81 +1,101 @@
-import { useLoaderData, Link } from "react-router-dom"
-import ReactMarkdown from 'react-markdown'
-import { Flex, FlexItem, Background, Title, SectionSeparator, Layout, Card, IconCircle, Body, Icon } from "playbook-ui"
+import { useLoaderData, Link } from "react-router-dom";
+import ReactMarkdown from "react-markdown";
+import {
+  Flex,
+  Background,
+  Title,
+  Layout,
+  Card,
+  IconCircle,
+  Body,
+  Icon,
+} from "playbook-ui";
+import HeaderImage from "../../../../../images/getting-started.svg";
 
 const GettingStarted = () => {
-  const loaderData = useLoaderData() as any
-  const { getting_started_content, getting_started } = loaderData
+  const loaderData = useLoaderData() as any;
+  const { getting_started_content, getting_started } = loaderData;
 
-  //------- remove once legacy webasite is gone --------------
-  let contentWithoutFrontmatter = getting_started_content?.replace(/^---[\s\S]*?---\n/, '') || ''
-  contentWithoutFrontmatter = contentWithoutFrontmatter.replace(/<div>\s*<%=[\s\S]*?%>\s*<\/div>/g, '')
-  //------- remove once legacy webasite is gone --------------
+  //------- remove once legacy website is gone --------------
+  let contentWithoutFrontmatter =
+    getting_started_content?.replace(/^---[\s\S]*?---\n/, "") || "";
+  contentWithoutFrontmatter = contentWithoutFrontmatter.replace(
+    /<div>\s*<%=[\s\S]*?%>\s*<\/div>/g,
+    ""
+  );
+  //------- remove once legacy website is gone --------------
 
   return (
-    <Flex orientation="column" align="center">
-      <FlexItem className="minw0 w100">
-        <Background
-          display="flex"
-          justifyContent="center"
-          className="markdown w100"
-          backgroundColor="white"
-          paddingX="sm"
-          paddingY="md"
-        >
-          <Background maxWidth="md" backgroundColor="white">
-            <Title text="Getting Started" tag="h1" size={1} />
-            <SectionSeparator flexGrow={1} />
-            <div className="markdown-content">
-              <ReactMarkdown>{contentWithoutFrontmatter}</ReactMarkdown>
-            </div>
-            
-            {/* Render card grid of sub-pages */}
-            {getting_started?.pages && getting_started.pages.length > 0 && (
-              <Layout layout="collection" marginY="xl" paddingBottom="xl">
-                <Layout.Body>
-                  {getting_started.pages.map((page: any) => {
-                    const icon = page.frontmatter?.icon || 'files'
-                    const description = page.frontmatter?.description || ''
-                    // Convert legacy URL to beta URL
-                    const betaUrl = page.url.replace('guides/getting_started/', '/beta/guides/getting_started/')
-                    
-                    return (
-                      <Link 
-                        key={page.page_id} 
-                        to={betaUrl}
-                        className="display_flex align_items_stretch"
-                      >
-                        <Card padding="none" hover={{ shadow: "deep" }} flex={1}>
-                          <Background>
-                            <Flex justify="center" padding="xl">
-                              <IconCircle icon={icon} variant="royal" />
-                            </Flex>
-                          </Background>
-                          <Flex justify="between" align="center" padding="sm">
-                            <Title size={4} color="link" text={page.title} />
-                            <Title size={4} color="link">
-                              <Icon icon="arrow-right-long" />
-                            </Title>
-                          </Flex>
-                          <Body 
-                            text={description} 
-                            truncate="4" 
-                            color="light" 
-                            marginX="sm" 
-                            marginBottom="sm" 
-                          />
-                        </Card>
-                      </Link>
-                    )
-                  })}
-                </Layout.Body>
-              </Layout>
-            )}
-          </Background>
-        </Background>
-      </FlexItem>
-    </Flex>
-  )
-}
+    <Flex flexDirection="column" justifyContent="center" alignItems="center">
+      <Background
+        imageUrl={HeaderImage}
+        width="100%"
+        display={{ xs: "none", sm: "none", md: "none" }}
+        height="250px"
+        backgroundColor="dark"
+        backgroundSize="contain"
+        backgroundPosition="center"
+        backgroundRepeat="no-repeat"
+      />
+      <Background
+        paddingX={{ xs: "sm", sm: "sm", md: "sm", lg: "sm", default: "none" }}
+        paddingY="md"
+        backgroundColor="white"
+        maxWidth="lg"
+      >
+        <Title text="Getting Started" tag="h1" size={1} />
+        <div className="markdown-content">
+          <ReactMarkdown>{contentWithoutFrontmatter}</ReactMarkdown>
+        </div>
 
-export default GettingStarted
+        {/* Render card grid of sub-pages */}
+        {getting_started?.pages && getting_started.pages.length > 0 && (
+          <Layout layout="collection" marginY="xl" paddingBottom="xl">
+            <Layout.Body>
+              {getting_started.pages.map((page: any) => {
+                const icon = page.frontmatter?.icon || "files";
+                const description = page.frontmatter?.description || "";
+                // Convert legacy URL to beta URL
+                const betaUrl = page.url.replace(
+                  "guides/getting_started/",
+                  "/beta/guides/getting_started/"
+                );
+
+                return (
+                  <Link
+                    key={page.page_id}
+                    to={betaUrl}
+                    className="display_flex align_items_stretch"
+                  >
+                    <Card padding="none" hover={{ shadow: "deep" }} flex={1}>
+                      <Background>
+                        <Flex justify="center" padding="xl">
+                          <IconCircle icon={icon} variant="royal" />
+                        </Flex>
+                      </Background>
+                      <Flex justify="between" align="center" padding="sm">
+                        <Title size={4} color="link" text={page.title} />
+                        <Title size={4} color="link">
+                          <Icon icon="arrow-right-long" />
+                        </Title>
+                      </Flex>
+                      <Body
+                        text={description}
+                        truncate="4"
+                        color="light"
+                        marginX="sm"
+                        marginBottom="sm"
+                      />
+                    </Card>
+                  </Link>
+                );
+              })}
+            </Layout.Body>
+          </Layout>
+        )}
+      </Background>
+    </Flex>
+  );
+};
+
+export default GettingStarted;
