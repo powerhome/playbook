dc655c32f59bd5c2355a41b7b593d38d04960402
Elisa Shapiro <elisashapiro@gmail.com>
2025-11-26 16:08:08 -0500

    POC: TimePicker Rails as React-Rendered

--- playbook/app/pb_kits/playbook/pb_time_picker/time_picker.rb
+++ playbook/app/pb_kits/playbook/pb_time_picker/time_picker.rb
@@ -3,6 +3,30 @@
 module Playbook
   module PbTimePicker
     class TimePicker < ::Playbook::KitBase
+      prop :default_time, type: Playbook::Props::String
+      prop :label, type: Playbook::Props::String,
+                   default: "Time Picker"
+      prop :show_timezone, type: Playbook::Props::Boolean,
+                           default: false
+      prop :time_format, type: Playbook::Props::Enum,
+                         values: %w[AMPM 24hour],
+                         default: "AMPM"
+      prop :value, type: Playbook::Props::String
+
+      def classname
+        generate_classname("pb_time_picker")
+      end
+
+      def time_picker_react_props
+        {
+          defaultTime: default_time,
+          id: id,
+          label: label,
+          showTimezone: show_timezone,
+          timeFormat: time_format,
+          value: value,
+        }.compact
+      end
     end
   end
 end
