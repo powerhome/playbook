38e5dab09ab38013399702333ace888ce7807776
Nida Ghuman ✨ <nidaqg@gmail.com>
2025-12-03 09:51:41 -0500

    Revert "[PLAY-2664] Typeahead and Dialog kits: Stop mouse down event bubbling…"
    This reverts commit c5a674d0abb29d1cf5202bf4e3fafa69ebec900a.

--- playbook/app/pb_kits/playbook/pb_typeahead/components/ClearIndicator.tsx
+++ playbook/app/pb_kits/playbook/pb_typeahead/components/ClearIndicator.tsx
@@ -7,29 +7,18 @@ type ClearContainerProps = {
     id: string,
   },
   clearValue: () => void,
-  innerProps?: any,
 }
 
-const ClearContainer = (props: ClearContainerProps | any): React.ReactElement => {
-  const { selectProps, clearValue, innerProps } = props
+const ClearContainer = (props: ClearContainerProps): React.ReactElement => {
+  const { selectProps, clearValue } = props
   useEffect(() => {
     document.addEventListener(`pb-typeahead-kit-${selectProps.id}:clear`, clearValue)
   }, [clearValue, selectProps.id])
-
-  // To stop this from bubbling up when inside a dialog or other modal
-  const handleMouseDown = (event: React.MouseEvent) => {
-    event.stopPropagation()
-    innerProps?.onMouseDown?.(event)
-  }
 
   return (
     <components.ClearIndicator
         className="clear_indicator"
         {...props}
-        innerProps={{
-          ...innerProps,
-          onMouseDown: handleMouseDown,
-        }}
     />
   )
 }
