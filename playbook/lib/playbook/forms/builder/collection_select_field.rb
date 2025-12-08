33d06e0434c34cd6213fc9e4a8563a2a4d6ddb54
Elisa Shapiro <elisashapiro@gmail.com>
2025-12-08 14:00:18 -0500

    input_options for form.select, collection_select, time_zone_select_field

--- playbook/lib/playbook/forms/builder/collection_select_field.rb
+++ playbook/lib/playbook/forms/builder/collection_select_field.rb
@@ -4,10 +4,18 @@ module Playbook
   module Forms
     class Builder
       def collection_select(name, collection, value_method, text_method, options = {}, html_options = {}, props: {})
-        props[:label] = @template.label(@object_name, name) if props[:label] == true
+        props[:input_options] ||= {}
+        props[:input_options][:id] ||= "#{@object_name}_#{name}"
+
+        props[:label] = @template.label(@object_name, name, for: props[:input_options][:id]) if props[:label] == true
+
         options[:skip_default_ids] = false unless options.key?(:skip_default_ids)
         options[:prompt] = props[:blank_selection] || ""
         html_options[:required] = "required" if props[:required]
+        html_options[:id] = props[:input_options][:id]
+        html_options[:class] = props[:input_options][:class] if props[:input_options][:class]
+        html_options[:data] = (html_options[:data] || {}).merge(props[:input_options][:data] || {})
+
         input = super(name, collection, value_method, text_method, options, html_options)
 
         @template.pb_rails("select", props: props) do
