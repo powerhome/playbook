88c7ec2f4501570f69678de91a897ee9fc3afd5d
nidaqg <nidaqg@gmail.com>
2026-01-02 15:47:09 -0500

    Remove dashboard settings

--- playbook/app/javascript/dashboard.js
+++ playbook/app/javascript/dashboard.js
@@ -1,3 +1,2 @@
 // Dashboard Settings
-export { commonSettings, dataColors } from '../pb_kits/playbook/pb_dashboard/commonSettings'
 export { default as dashboardValueSettings } from '../pb_kits/playbook/pb_dashboard_value/dashboardValueSettings'
