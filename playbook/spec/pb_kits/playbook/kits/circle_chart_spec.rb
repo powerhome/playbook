d036369574503ab7f619a8535b620f7df99b516b
nidaqg <nidaqg@gmail.com>
2026-01-02 15:39:55 -0500

    Delete Legacy Highchart kits

--- playbook/spec/pb_kits/playbook/kits/circle_chart_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require_relative "../../../../app/pb_kits/playbook/pb_circle_chart/circle_chart"
-
-RSpec.describe Playbook::PbCircleChart::CircleChart do
-  subject { Playbook::PbCircleChart::CircleChart }
-
-  # Do not leave this file blank. Use other spec files for example tests.
-end
