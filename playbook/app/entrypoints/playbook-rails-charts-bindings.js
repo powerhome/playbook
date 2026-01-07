50c8a6a33370d51701bbb859b89e153e03751306
nidaqg <nidaqg@gmail.com>
2026-01-07 14:36:41 -0500

    registry issues

--- playbook/app/entrypoints/playbook-rails-charts-bindings.js
+++ playbook/app/entrypoints/playbook-rails-charts-bindings.js
@@ -1,8 +1,8 @@
 // React-Rendered Rails Chart Kit Exports =====
 // Requires: npm install highcharts highcharts-react-official
+// This file should be imported AFTER playbook-rails.js
 
 import ComponentRegistry from '../utils/componentRegistry'
-import '../utils/mountComponent'
 
 import PbBarGraph from 'kits/pb_pb_bar_graph/_pb_bar_graph'
 import PbCircleChart from 'kits/pb_pb_circle_chart/_pb_circle_chart'
@@ -15,12 +15,3 @@ ComponentRegistry.registerComponents({
   PbLineGraph,
   PbGaugeChart,
 })
-
-//export mount/unmount functions for use if needed
-export const mountPlaybookReactKits = () => {
-  ComponentRegistry.mountComponents()
-}
-
-export const unmountPlaybookReactKits = () => {
-  ComponentRegistry.unmountComponents()
-}
