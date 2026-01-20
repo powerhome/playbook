dd83b692795ff0b6f6704b63727883c7c2e4b466
Jen Kelly <jen.kelly@powerhrg.com>
2026-01-20 10:13:29 -0500

    Test GPC key

--- playbook/app/entrypoints/playbook-rails-react-bindings.js
+++ playbook/app/entrypoints/playbook-rails-react-bindings.js
@@ -1,15 +1,17 @@
 // React-Rendered Rails Kit Exports =====
 
-import ComponentRegistry from '../utils/componentRegistry'
-import '../utils/mountComponent'
+// Test
 
-import DistributionBar from 'kits/pb_distribution_bar/_distribution_bar'
-import MultiLevelSelect from 'kits/pb_multi_level_select/_multi_level_select'
-import Passphrase from 'kits/pb_passphrase/_passphrase'
-import RichTextEditor from 'kits/pb_rich_text_editor/_rich_text_editor'
-import Typeahead from 'kits/pb_typeahead/_typeahead'
-import PhoneNumberInput from 'kits/pb_phone_number_input/_phone_number_input'
-import TimePicker from 'kits/pb_time_picker/_time_picker'
+import ComponentRegistry from "../utils/componentRegistry"
+import "../utils/mountComponent"
+
+import DistributionBar from "kits/pb_distribution_bar/_distribution_bar"
+import MultiLevelSelect from "kits/pb_multi_level_select/_multi_level_select"
+import Passphrase from "kits/pb_passphrase/_passphrase"
+import RichTextEditor from "kits/pb_rich_text_editor/_rich_text_editor"
+import Typeahead from "kits/pb_typeahead/_typeahead"
+import PhoneNumberInput from "kits/pb_phone_number_input/_phone_number_input"
+import TimePicker from "kits/pb_time_picker/_time_picker"
 
 ComponentRegistry.registerComponents({
   DistributionBar,
@@ -34,4 +36,4 @@ const observer = new MutationObserver(() => {
   mountPlaybookReactKits()
 })
 
-observer.observe(document.body, { childList: true, subtree: true })
+observer.observe(document.body, {childList: true, subtree: true})
