c64c3feb0016f65f04764c0bd9fd21716d10bbd8
Jen Kelly <jen.kelly@powerhrg.com>
2026-01-20 14:46:07 -0500

    Required indicator prop

---
+++ playbook/app/pb_kits/playbook/pb_typeahead/docs/_typeahead_required_indicator.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import Typeahead from "../../pb_typeahead/_typeahead"
+
+const options = [
+  {label: "Orange", value: "#FFA500"},
+  {label: "Red", value: "#FF0000"},
+  {label: "Green", value: "#00FF00"},
+  {label: "Blue", value: "#0000FF"},
+]
+
+const TypeaheadRequiredIndicator = (props) => {
+  return (
+    <Typeahead
+        label="Colors"
+        options={options}
+        requiredIndicator
+        {...props}
+    />
+  )
+}
+
+export default TypeaheadRequiredIndicator
