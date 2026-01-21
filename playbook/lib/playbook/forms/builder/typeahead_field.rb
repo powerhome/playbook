afd5011e32fdfcd0a887069366679bb3aa064272
Jen Kelly <jen.kelly@powerhrg.com>
2026-01-21 16:14:17 -0500

    Render span inside of labelfor typeahead_field

--- playbook/lib/playbook/forms/builder/typeahead_field.rb
+++ playbook/lib/playbook/forms/builder/typeahead_field.rb
@@ -10,24 +10,7 @@ module Playbook
         props[:input_options] ||= {}
         props[:input_options][:id] ||= input_id
 
-        # props[:label] = @template.label(@object_name, name, for: props[:input_options][:id]) if props[:label] == true
-
-        if props[:label] == true
-          # If using required_indicator, extract just the text and let the template handle rendering
-          props[:label] = if props[:required_indicator]
-                            if @object && @object.class.respond_to?(:human_attribute_name)
-                              @object.class.human_attribute_name(name)
-                            else
-                              name.to_s.humanize
-                            end
-                          else
-                            # Legacy behavior (generate full label HTML) left untouched
-                            @template.label(@object_name, name, for: props[:input_options][:id])
-                          end
-        elsif props[:label].is_a?(String)
-          # If using required_indicator, keep as text; otherwise wrap in label_tag
-          props[:label] = @template.label_tag(props[:input_options][:id], props[:label]) unless props[:required_indicator]
-        end
+        props[:label] = @template.label(@object_name, name, for: props[:input_options][:id]) if props[:label] == true
 
         if props[:validation]
           validation_message = props[:validation][:message]
