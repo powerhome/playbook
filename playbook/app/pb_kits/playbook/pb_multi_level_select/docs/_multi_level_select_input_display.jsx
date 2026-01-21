b0e85dd400df5912b0e2b8ff42f0a12d579c2cdd
Nihar Patel <niharpatel52@gmail.com>
2026-01-21 14:17:18 -0500

    Add and export react example

---
+++ playbook/app/pb_kits/playbook/pb_multi_level_select/docs/_multi_level_select_input_display.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+import MultiLevelSelect from "../_multi_level_select";
+
+const treeData = [
+  {
+    label: "Power Home Remodeling",
+    value: "powerHomeRemodeling",
+    id: "powerhome1",
+    expanded: true,
+    children: [
+      {
+        label: "People",
+        value: "people",
+        id: "people1",
+        expanded: true,
+        children: [
+          {
+            label: "Talent Acquisition",
+            value: "talentAcquisition",
+            id: "talent1",
+          },
+          {
+            label: "Business Affairs",
+            value: "businessAffairs",
+            id: "business1",
+            children: [
+              {
+                label: "Initiatives",
+                value: "initiatives",
+                id: "initiative1",
+              },
+              {
+                label: "Learning & Development",
+                value: "learningAndDevelopment",
+                id: "development1",
+              },
+            ],
+          },
+          {
+            label: "People Experience",
+            value: "peopleExperience",
+            id: "experience1",
+          },
+        ],
+      },
+      {
+        label: "Contact Center",
+        value: "contactCenter",
+        id: "contact1",
+        children: [
+          {
+            label: "Appointment Management",
+            value: "appointmentManagement",
+            id: "appointment1",
+          },
+          {
+            label: "Customer Service",
+            value: "customerService",
+            id: "customer1",
+          },
+          {
+            label: "Energy",
+            value: "energy",
+            id: "energy1",
+          },
+        ],
+      },
+    ],
+  },
+];
+
+const MultiLevelSelectInputDisplay = (props) => {
+  return (
+    <>
+      <MultiLevelSelect
+          id="multi-level-select-input-display-none"
+          inputDisplay="none"
+          label="With Input Display None"
+          onSelect={(selectedNodes) =>
+            console.log("Selected Items", selectedNodes)
+          }
+          treeData={treeData}
+          {...props}
+      />
+      <br/>
+      <MultiLevelSelect
+          id="multi-level-select-input-display-pills"
+          label="With Input Display Pills (Default)"
+          onSelect={(selectedNodes) =>
+            console.log("Selected Items", selectedNodes)
+          }
+          treeData={treeData}
+          {...props}
+      />
+    </>
+  );
+};
+
+export default MultiLevelSelectInputDisplay;
