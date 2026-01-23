3cc02da29bb43e4fd3d17163d6ed798e1870ba75
nidaqg <nidaqg@gmail.com>
2026-01-23 16:27:16 -0500

    Default option to responsive flex props

--- playbook/lib/playbook/order.rb
+++ playbook/lib/playbook/order.rb
@@ -11,16 +11,26 @@ module Playbook
       selected_props = order_options.keys.select { |sk| try(sk) }
       return nil unless selected_props.present?
 
+      screen_size_values = %w[xs sm md lg xl]
+
       selected_props.map do |k|
         order_value = send(k)
         if order_value.is_a?(Hash)
-          order_value.map do |media_size, order_size_value|
-            "flex_order_#{media_size}_#{order_size_value}" if order_values.include? order_size_value.to_s
+          class_result = []
+
+          # Handle default value separately (generates base class without size prefix)
+          class_result << "flex_order_#{order_value[:default]}" if order_value.key?(:default) && order_values.include?(order_value[:default].to_s)
+
+          # Handle responsive sizes (generates classes with size prefix)
+          order_value.each do |media_size, order_size_value|
+            class_result << "flex_order_#{media_size}_#{order_size_value}" if screen_size_values.include?(media_size.to_s) && order_values.include?(order_size_value.to_s)
           end
+
+          class_result
         else
           "flex_order_#{order_value}" if order_values.include? order_value.to_s
         end
-      end.compact.join(" ")
+      end.flatten.compact.join(" ")
     end
     # rubocop:enable Style/IfInsideElse
 
