8da01589e9da4df1a64d3d7a4e0243dc46a0ca06
Nihar Patel <niharpatel52@gmail.com>
2026-01-27 15:20:04 -0500

    [PLAY-2634] `requiredIndicator` Prop for Input Fields - Passphrase + Phone Number Input (1 of 6) (#5727)
    [PLAY-2634 Runway
    Card](https://runway.powerhrg.com/backlog_items/PLAY-2634?from=active_sprint)
    
    **What does this PR do?** A clear and concise description with your
    runway ticket url.
    
    - Added the optional `requiredIndicator` prop to both the the Passphrase
    and PhoneNumberInput kits (react and rails)
    - Made it available via form builder
    - Ensured the prop usage is purely visual 
    - Added docs that state there are no logical conditions associated with
    the prop. The devs will need to ensure that the actual requirement logic
    is implemented
    - Added Spacing between "Passphrase" label and the input fields
    - Added tests
    
    
    **Screenshots:** Screenshots to visualize your addition/change
    
    <img width="1341" height="377" alt="Screenshot 2026-01-20 at 4 11 52 PM"
    src="https://github.com/user-attachments/assets/da249f20-d0f2-4311-82de-cee5713ca944"
    />
    
    
    
    <img width="1306" height="403" alt="Screenshot 2026-01-22 at 10 19
    37 AM"
    src="https://github.com/user-attachments/assets/e4c471f2-54a4-4226-ad1a-ae38b5ce8014"
    />
    
    
    
    **How to test?** Steps to confirm the desired behavior:
    
    1. Go to 'Passphrase', docs at bottom of page
    2. Go to 'Phone Number Input', docs at bottom of page
    
    
    #### Checklist:
    - [x] **LABELS** Add a label: `enhancement`, `bug`, `improvement`, `new
    kit`, `deprecated`, or `breaking`. See [Changelog &
    Labels](https://github.com/powerhome/playbook/wiki/Changelog-&-Labels)
    for details.
    - [x] **DEPLOY** I have added the `milano` label to show I'm ready for a
    review.
    - [x] **TESTS** I have added test coverage to my code.
    - [x] **SEMVER** I have added a `minor`, `major`, or `patch` label for
    release.
    - [x] **RC** I have added an `inactive RC` label if not an active RC.

--- playbook/lib/playbook/forms/builder/phone_number_field.rb
+++ playbook/lib/playbook/forms/builder/phone_number_field.rb
@@ -5,6 +5,15 @@ module Playbook
     class Builder
       def phone_number_field(name, props: {})
         props[:name] = name
+
+        if props[:label] == true && props[:required_indicator]
+          props[:label] = if @object && @object.class.respond_to?(:human_attribute_name)
+                            @object.class.human_attribute_name(name)
+                          else
+                            name.to_s.humanize
+                          end
+        end
+
         @template.pb_rails("phone_number_input", props: props)
       end
     end
