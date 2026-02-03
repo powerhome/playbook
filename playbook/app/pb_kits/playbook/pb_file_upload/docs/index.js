4ff88b4a3636d347cbb79950bb4e54f6b9ad5d68
Shantel <shantel.gray@powerhrg.com>
2026-02-03 14:27:29 -0500

    Created File Upload files

--- playbook/app/pb_kits/playbook/pb_file_upload/docs/index.js
+++ playbook/app/pb_kits/playbook/pb_file_upload/docs/index.js
@@ -1,6 +1,7 @@
-export { default as FileUploadDefault } from './_file_upload_default.jsx'
-export { default as FileUploadAccept } from './_file_upload_accept.jsx'
-export { default as FileUploadCustomMessage } from './_file_upload_custom_message.jsx'
-export { default as FileUploadCustomDescription } from './_file_upload_custom_description.jsx'
-export { default as FileUploadMaxSize } from './_file_upload_max_size.jsx'
-export { default as FileUploadError } from './_file_upload_error.jsx'
+export { default as FileUploadDefault } from "./_file_upload_default.jsx";
+export { default as FileUploadAccept } from "./_file_upload_accept.jsx";
+export { default as FileUploadCustomMessage } from "./_file_upload_custom_message.jsx";
+export { default as FileUploadCustomDescription } from "./_file_upload_custom_description.jsx";
+export { default as FileUploadMaxSize } from "./_file_upload_max_size.jsx";
+export { default as FileUploadError } from "./_file_upload_error.jsx";
+export { default as FileUploadRequiredIndicator } from "./_file_upload_required_indicator.jsx";
