78d738df52bab19acafb569c5ecbb2770040ad10
Shantel <shantel.gray@powerhrg.com>
2026-02-04 10:22:26 -0500

    Add required indicator select

---
+++ playbook/app/pb_kits/playbook/pb_select/docs/_select_required_indicator.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import { Select } from "playbook-ui";
+
+const SelectRequiredIndicator = () => {
+  const options = [
+    {
+      value: "1",
+      text: "Popcorn",
+    },
+    {
+      value: "2",
+      text: "Chips",
+    },
+    {
+      value: "3",
+      text: "Twizzlers",
+    },
+  ];
+
+  return (
+    <div>
+      <Select label="Favorite Snack"
+          name="food"
+          options={options}
+      />
+    </div>
+  );
+};
+
+export default SelectRequiredIndicator;
