6c6cb7a83dd5bdda45e27680ef10152eef115dbd
Emily Kanarek <emilyakanarek@gmail.com>
2026-02-09 12:35:09 -0500

    Undo automatic linting

--- playbook/app/pb_kits/playbook/pb_star_rating/subcomponents/_star_rating_interactive.tsx
+++ playbook/app/pb_kits/playbook/pb_star_rating/subcomponents/_star_rating_interactive.tsx
@@ -3,14 +3,12 @@ import { GlobalProps } from '../../utilities/globalProps'
 import Icon from '../../pb_icon/_icon'
 import Flex from '../../pb_flex/_flex'
 import { getStarIconObject } from "../stars/utils"
-import Caption from "../../pb_caption/_caption"
 
 type StarRatingInteractiveProps = {
     backgroundType: "fill" | "outline",
     colorOption: "yellow" | "primary" | "subtle" | "outline",
     dark: boolean,
     denominator: number,
-    label?: string,
     onClick?: (interactiveStarValue: number) => void,
     size: "xs" | "sm" | "md" | "lg",
 } & GlobalProps
@@ -21,7 +19,6 @@ const StarRatingInteractive = (props: StarRatingInteractiveProps) => {
         colorOption,
         dark,
         denominator,
-        label,
         onClick,
         size,
     } = props
@@ -47,58 +44,48 @@ const StarRatingInteractive = (props: StarRatingInteractiveProps) => {
     }
 
     return (
-        <div>
-            {label &&
-                <Caption
-                    className="pb_star_rating_kit_label"
-                    marginBottom="xs"
-                    text={label}
-                />
-            }
-            <Flex className="star_flex_area">
-                {[...Array(denominator)].map((_, index) => {
-                    const starIndex = index + 1
-                    const isFilled = starIndex <= interactiveStarValue
-                    const isHovered = hoverStarValue !== null && starIndex > interactiveStarValue && starIndex <= hoverStarValue
+        <Flex className="star_flex_area">
+            {[...Array(denominator)].map((_, index) => {
+                const starIndex = index + 1
+                const isFilled = starIndex <= interactiveStarValue
+                const isHovered = hoverStarValue !== null && starIndex > interactiveStarValue && starIndex <= hoverStarValue
 
-                    const baseClass = dark 
-                        ? starIcon[backgroundType].className.replace("empty_star_light", "empty_star_dark")
-                        : starIcon[backgroundType].className
-                    
-                    let starClass = baseClass
-                    if (isFilled) {
-                        starClass = starClass.replace(/(empty_star_light|empty_star_dark)/, '')
-                        starClass += ` ${starIcon[colorOption].className}`
-                    }
-                    if (isHovered) {
-                        starClass += " star-hovered"
-                    }
-                    if (isFilled && starIndex === interactiveStarValue) {
-                        starClass += " star-selected"
-                    }
+                const baseClass = dark 
+                    ? starIcon[backgroundType].className.replace("empty_star_light", "empty_star_dark")
+                    : starIcon[backgroundType].className
+                
+                let starClass = baseClass
+                if (isFilled) {
+                    starClass = starClass.replace(/(empty_star_light|empty_star_dark)/, '')
+                    starClass += ` ${starIcon[colorOption].className}`
+                }
+                if (isHovered) {
+                    starClass += " star-hovered"
+                }
+                if (isFilled && starIndex === interactiveStarValue) {
+                    starClass += " star-selected"
+                }
 
-                    return (
-                        <div
-                            aria-label={`Rate ${starIndex} out of ${denominator} stars`}
-                            key={index}
-                            onKeyDown={(event) => handleOnKeyDown(event, starIndex)}
+                return (
+                    <div
+                        key={index}
+                        onKeyDown={(event) => handleOnKeyDown(event, starIndex)}
+                    >
+                        <Icon
+                            className={starClass.trim()}
+                            cursor="pointer"
+                            customIcon={starIcon[backgroundType].icon as unknown as { [key: string]: SVGElement }}
+                            htmlOptions={{
+                                onClick: () => handleOnClick(starIndex),
+                                onMouseEnter: () => handleMouseEnter(starIndex),
+                                onMouseLeave: () => handleMouseLeave(),
+                            }}
                             tabIndex={0}
-                        >
-                            <Icon
-                                className={starClass.trim()}
-                                cursor="pointer"
-                                customIcon={starIcon[backgroundType].icon as unknown as { [key: string]: SVGElement }}
-                                htmlOptions={{
-                                    onClick: () => handleOnClick(starIndex),
-                                    onMouseEnter: () => handleMouseEnter(starIndex),
-                                    onMouseLeave: () => handleMouseLeave(),
-                                }}
-                            />
-                        </div>
-                    );
-                })}
-            </Flex>
-        </div>
+                        />
+                    </div>
+                );
+            })}
+        </Flex>
     )
 }
 
