6d08d1dd24572c9791a9cfa37e673ee5e5d49aa0
Emily Kanarek <emilyakanarek@gmail.com>
2026-02-12 13:33:36 -0500

    Remove unused colors import

--- playbook/app/pb_kits/playbook/pb_multi_level_select/_multi_level_select.tsx
+++ playbook/app/pb_kits/playbook/pb_multi_level_select/_multi_level_select.tsx
@@ -15,7 +15,6 @@ import Body from "../pb_body/_body";
 import Caption from "../pb_caption/_caption";
 import MultiLevelSelectOptions from "./multi_level_select_options";
 import MultiLevelSelectContext from "./context";
-import colors from "../tokens/exports/_colors.module.scss"
 
 import {
   getAncestorsOfUnchecked,
