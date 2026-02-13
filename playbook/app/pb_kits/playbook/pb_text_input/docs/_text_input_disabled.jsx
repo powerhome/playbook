62c7dab6df3443f62f0bc4c8eddd13fa231b015b
Mike <mclancy96@gmail.com>
2026-02-13 13:55:14 -0500

    Undo accidental change

--- playbook/app/pb_kits/playbook/pb_text_input/docs/_text_input_disabled.jsx
+++ playbook/app/pb_kits/playbook/pb_text_input/docs/_text_input_disabled.jsx
@@ -2,17 +2,19 @@ import React from 'react'
 
 import TextInput from '../_text_input'
 
-const TextInputDisabled = (props) => {
-  return (
-    <div>
-      <TextInput
-          disabled
-          label="Last Name"
-          placeholder="Enter last name"
-          {...props}
-      />
-    </div>
-  )
+class TextInputDisabled extends React.Component {
+  render(props) {
+    return (
+      <div>
+        <TextInput
+            disabled
+            label="Last Name"
+            placeholder="Enter last name"
+            {...props}
+        />
+      </div>
+    )
+  }
 }
 
 export default TextInputDisabled
