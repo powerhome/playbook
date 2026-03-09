2e823f1c31fa083dce1fd08d161641137af1c2cd
Nida Ghuman ✨ <nidaqg@gmail.com>
2026-03-09 16:24:06 -0400

    [PLAY-2851] RTE: Make Trix an Optional Dependency in Playbook (#5796)
    **What does this PR do?** A clear and concise description with your
    runway ticket url.
    
    [Runway
    Story](https://runway.powerhrg.com/backlog_items/PLAY-2851?from=active_sprint)
    
    ## On This PR:
    we are removing trix and react-trix from PB's package json and hence its
    dist.
    We are converting the default trix editor to work in the same way as the
    advanced editor: if using it in your repo you must install Trix and
    react-trix and then pass the Trix instance to the kit. This craetes
    consistency between the 2 versions of the RTE
    Trix is now an optional dependency so if not needed does not need to be
    installed.
    We are also removing the rails side of the kit completely. It is not
    used and therefore best to remove it now while we look at what we want
    to use instead of Trix (POC story in next sprint)
    We are also reorganizing the docs so its all the advanced editor docs up
    top with 1 trix doc at the bottom stating it is a legacy implementation
    
    ## Next Steps:
    Run this by runway devs to get their thoughts on it since they are the
    ones who will have to deal with the new way to work with Trix
    Tempo also has a few instances of Trix usage, we should work with them
    to remove those as well
    For the 4 instances in Nitro that are using Trix without using our kit
    (rails), we should look into replacing those with another library OR
    with the rails version of our RTE once we do the POC and decide on a
    direction for the Rails side of the RTE
    Once that last point is done, we can remove Trix entirely from Nitro.
    Currently it is installed within the 4 locations mentioned above.
    We should probably also look into separating out the RTE file into 2
    separate files for Trix vs Tiptap so its easier to work in that kit and
    we can be sure not to touch the legacy Trix view going forward
    
    **Screenshots:** Screenshots to visualize your addition/change
    
    
    **How to test?** Steps to confirm the desired behavior:
    1. Go to '...'
    2. Click on '....'
    3. Scroll down to '....'
    4. See addition/change
    
    
    #### Checklist:
    - [x] **LABELS** Add a label: `enhancement`, `bug`, `improvement`, `new
    kit`, `deprecated`, or `breaking`. See [Changelog &
    Labels](https://github.com/powerhome/playbook/wiki/Changelog-&-Labels)
    for details.
    - [x] **DEPLOY** I have added the `milano` label to show I'm ready for a
    review.
    - [ ] **TESTS** I have added test coverage to my code.
    - [x] **SEMVER** I have added a `minor`, `major`, or `patch` label for
    release.
    - [x] **RC** I have added an `inactive RC` label if not an active RC.
    
    ---------
    
    Co-authored-by: Elisa Shapiro <83474365+ElisaShapiro@users.noreply.github.com>

--- playbook/app/entrypoints/playbook-rails-react-bindings.js
+++ playbook/app/entrypoints/playbook-rails-react-bindings.js
@@ -6,7 +6,6 @@ import '../utils/mountComponent'
 import DistributionBar from 'kits/pb_distribution_bar/_distribution_bar'
 import MultiLevelSelect from 'kits/pb_multi_level_select/_multi_level_select'
 import Passphrase from 'kits/pb_passphrase/_passphrase'
-import RichTextEditor from 'kits/pb_rich_text_editor/_rich_text_editor'
 import Typeahead from 'kits/pb_typeahead/_typeahead'
 import PhoneNumberInput from 'kits/pb_phone_number_input/_phone_number_input'
 import TimePicker from 'kits/pb_time_picker/_time_picker'
@@ -15,7 +14,6 @@ ComponentRegistry.registerComponents({
   DistributionBar,
   MultiLevelSelect,
   Passphrase,
-  RichTextEditor,
   Typeahead,
   PhoneNumberInput,
   TimePicker,
