fbdc4856680f03a24628e60d9ce4aed19e58fcbb
nickamantia <nick.amantia@powerhrg.com>
2026-03-09 13:58:58 -0400

    feedback

--- playbook/lib/playbook/pagination_renderer.rb
+++ playbook/lib/playbook/pagination_renderer.rb
@@ -148,15 +148,15 @@ module Playbook
     private
 
       def chevron_left_svg
-        "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 30 24' fill='none' aria-hidden='true' focusable='false' class='pb_custom_icon svg-inline--fa pb_icon_kit svg_fw svg_xs'><path d='M8.20312 11.2031L17.2031 2.25C17.625 1.78125 18.3281 1.78125 18.7969 2.25C19.2188 2.67188 19.2188 3.375 18.7969 3.79688L10.5469 12L18.75 20.25C19.2188 20.6719 19.2188 21.375 18.75 21.7969C18.3281 22.2656 17.625 22.2656 17.2031 21.7969L8.20312 12.7969C7.73438 12.375 7.73438 11.6719 8.20312 11.2031Z' fill='currentColor'></path></svg>".html_safe
+        "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 30 24' fill='none' aria-hidden='true' focusable='false' class='pb_custom_icon svg-inline--fa pb_icon_kit svg_fw'><path d='M8.20312 11.2031L17.2031 2.25C17.625 1.78125 18.3281 1.78125 18.7969 2.25C19.2188 2.67188 19.2188 3.375 18.7969 3.79688L10.5469 12L18.75 20.25C19.2188 20.6719 19.2188 21.375 18.75 21.7969C18.3281 22.2656 17.625 22.2656 17.2031 21.7969L8.20312 12.7969C7.73438 12.375 7.73438 11.6719 8.20312 11.2031Z' fill='currentColor'></path></svg>".html_safe
       end
 
       def chevron_right_svg
-        "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 30 24' fill='none' aria-hidden='true' focusable='false' class='pb_custom_icon svg-inline--fa pb_icon_kit svg_fw svg_xs'><path d='M21.7969 11.2031C22.2188 11.6719 22.2188 12.375 21.7969 12.7969L12.7969 21.7969C12.3281 22.2656 11.625 22.2656 11.2031 21.7969C10.7344 21.375 10.7344 20.6719 11.2031 20.25L19.4062 12.0469L11.2031 3.79688C10.7344 3.375 10.7344 2.67188 11.2031 2.25C11.625 1.78125 12.3281 1.78125 12.75 2.25L21.7969 11.2031Z' fill='currentColor'></path></svg>".html_safe
+        "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 30 24' fill='none' aria-hidden='true' focusable='false' class='pb_custom_icon svg-inline--fa pb_icon_kit svg_fw'><path d='M21.7969 11.2031C22.2188 11.6719 22.2188 12.375 21.7969 12.7969L12.7969 21.7969C12.3281 22.2656 11.625 22.2656 11.2031 21.7969C10.7344 21.375 10.7344 20.6719 11.2031 20.25L19.4062 12.0469L11.2031 3.79688C10.7344 3.375 10.7344 2.67188 11.2031 2.25C11.625 1.78125 12.3281 1.78125 12.75 2.25L21.7969 11.2031Z' fill='currentColor'></path></svg>".html_safe
       end
 
       def chevron_down_svg
-        "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 30 24' fill='none' aria-hidden='true' focusable='false' class='pb_custom_icon svg-inline--fa pb_icon_kit svg_fw svg_xs'><path d='M21.7969 7.20312C22.2188 7.67188 22.2188 8.375 21.7969 8.79688L15.75 14.8438C15.3281 15.2656 14.625 15.2656 14.2031 14.8438L8.20312 8.79688C7.73438 8.375 7.73438 7.67188 8.20312 7.20312C8.625 6.78125 9.32812 6.78125 9.79688 7.20312L15 12.4062L20.2031 7.20312C20.6719 6.78125 21.375 6.78125 21.7969 7.20312Z' fill='currentColor'></path></svg>".html_safe
+        "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 30 24' fill='none' aria-hidden='true' focusable='false' class='pb_custom_icon svg-inline--fa pb_icon_kit svg_fw'><path d='M21.7969 7.20312C22.2188 7.67188 22.2188 8.375 21.7969 8.79688L15.75 14.8438C15.3281 15.2656 14.625 15.2656 14.2031 14.8438L8.20312 8.79688C7.73438 8.375 7.73438 7.67188 8.20312 7.20312C8.625 6.78125 9.32812 6.78125 9.79688 7.20312L15 12.4062L20.2031 7.20312C20.6719 6.78125 21.375 6.78125 21.7969 7.20312Z' fill='currentColor'></path></svg>".html_safe
       end
     end
   end
