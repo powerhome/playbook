ec4eb3ccd51f6a4aab4dc5524ce002ff1611faa9
Elisa Shapiro <elisashapiro@gmail.com>
2026-03-20 13:57:23 -0400

    Revert "Temporary doc update to show PLAY-2837 unaffected by this - to be reverted"
    This reverts commit 161713011b4d43f0891a2062fef20202a2e98c9a.

--- playbook/app/pb_kits/playbook/pb_circle_icon_button/docs/_circle_icon_button_loading.jsx
+++ playbook/app/pb_kits/playbook/pb_circle_icon_button/docs/_circle_icon_button_loading.jsx
@@ -1,102 +1,43 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 import CircleIconButton from '../_circle_icon_button'
-import Dialog from '../../pb_dialog/_dialog'
-import Button from '../../pb_button/_button'
 
-const CircleIconButtonLoading = (props) => {
+const CircleIconButtonLoading = (props) => (
+  <div>
+    <CircleIconButton
+        icon="plus"
+        loading
+        variant="primary"
+        {...props}
+    />
 
+    <br />
 
-  const [isOpen, setIsOpen] = useState(false)
-  const close = () => setIsOpen(false)
-  const open = () => setIsOpen(true)
+    <CircleIconButton
+        icon="pen"
+        loading
+        variant="secondary"
+        {...props}
+    />
 
-  return (
-    <div>
-      <CircleIconButton
-          icon="plus"
-          loading
-          variant="primary"
-          {...props}
-      />
+    <br />
 
-      <br />
+    <CircleIconButton
+        disabled
+        icon="times"
+        loading
+        {...props}
+    />
 
-      <CircleIconButton
-          icon="pen"
-          loading
-          variant="secondary"
-          {...props}
-      />
+    <br />
 
-      <br />
-
-      <CircleIconButton
-          disabled
-          icon="times"
-          loading
-          {...props}
-      />
-
-      <br />
-
-      <CircleIconButton
-          icon="user"
-          loading
-          variant="link"
-          {...props}
-      />
-          <>
-        <Button marginTop="md"
-            onClick={open}
-        >
-          {'Open Dialog to confirm PLAY-2837 fix still works for circle icon buttons'}
-        </Button>
-        <Dialog
-            onCancel={close}
-            onClose={close}
-            onConfirm={close}
-            opened={isOpen}
-            size="md"
-            title="Header Title is the Title Prop"
-        >
-          <Dialog.Body>
-              <Button
-                  aria={{ label: 'Loading' }}
-                  loading
-                  text="Button Primary"
-              />
-              <CircleIconButton
-                  icon="plus"
-                  loading
-              />
-              <div style={{height: '800px', backgroundColor: 'lightgray'}} />
-              <Button
-                  loading
-                  text="Loading..."
-              />
-              <CircleIconButton
-                  icon="plus"
-                  loading
-              />
-            </Dialog.Body>
-            <Dialog.Footer>
-              <Button
-                  loading
-                  text="Send My Issue"
-              />
-              <CircleIconButton
-                  icon="plus"
-                  loading
-              />
-              <Button  variant="link">
-                {"Back"}
-              </Button>
-            </Dialog.Footer>
-          </Dialog>
-      </>
-    </div> 
-  )
-}
+    <CircleIconButton
+        icon="user"
+        loading
+        variant="link"
+        {...props}
+    />
+  </div>
+)
 
 export default CircleIconButtonLoading
