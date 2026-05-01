c04c48918c43d31a648be41ca8b21fd909ee9118
nidaqg <nidaqg@gmail.com>
2026-05-01 10:27:13 -0400

    brakeman issue

--- playbook/app/controllers/playbook/icons_controller.rb
+++ playbook/app/controllers/playbook/icons_controller.rb
@@ -2,6 +2,8 @@
 
 module Playbook
   class IconsController < ActionController::Base
+    protect_from_forgery with: :exception
+
     def show
       icon_path = Playbook::PbIcon::Icon.resolved_icon_path(icon_name)
       return head :not_found unless icon_path
