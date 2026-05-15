6ffea3e814f93e5637ebfa92b7d374af1079f1a5
edwardsaleeby <edward.saleeby.id@gmail.com>
2026-05-15 13:25:55 -0400

    PB Website: Updated kit wrappers and use full main width

--- playbook-website/app/javascript/components/Website/src/pages/KitShow/index.tsx
+++ playbook-website/app/javascript/components/Website/src/pages/KitShow/index.tsx
@@ -96,60 +96,60 @@ const KitShow = () => {
 
   return (
     <>
-      <Flex
-        align="stretch"
-        maxWidth="xxl"
-        marginX="auto"
-        orientation="column"
-        marginTop="sm"
-      >
-        <Title
-          text={`${linkFormat(name)}`}
-          size={1}
-          marginBottom={kit_description ? undefined : "md"}
-          paddingX="xl"
-          dark={darkMode}
-        />
-        {kit_description && kit_description !== "" && (
-          <Flex paddingX="xl">
-            <Flex flex={1} minWidth={0}>
-              <Body marginTop="sm" marginBottom="md" dark={darkMode}>
-                <MarkdownContent>{kit_description}</MarkdownContent>
-              </Body>
+      <div className={`pb--kit-show ${currentKit}-kit`}>
+        <Flex
+          align="stretch"
+          minWidth={0}
+          orientation="column"
+          marginTop="sm"
+          width="100%"
+        >
+          <Title
+            text={`${linkFormat(name)}`}
+            size={1}
+            marginBottom={kit_description ? undefined : "md"}
+            dark={darkMode}
+          />
+          {kit_description && kit_description !== "" && (
+            <Flex>
+              <Flex flex={1} minWidth={0}>
+                <Body marginTop="sm" marginBottom="md" dark={darkMode}>
+                  <MarkdownContent>{kit_description}</MarkdownContent>
+                </Body>
+              </Flex>
+              <Flex
+                display={{ xs: "none", sm: "none", md: "none", lg: "none", xl: "flex" }}
+                shrink
+                htmlOptions={{ style: { width: "228px"} }}
+              />
             </Flex>
-            <Flex
-              display={{ xs: "none", sm: "none", md: "none", lg: "none", xl: "flex" }}
-              shrink
-              htmlOptions={{ style: { width: "228px"} }}
-            />
-          </Flex>
-        )}
-
-        {/* Navigation Tabs */}
-        <Nav orientation="horizontal" paddingX="xl" dark={darkMode}>
-          <NavItem
-            text="Docs"
-            active={displayTab === "docs"}
-            onClick={() => handleTabChange("docs")}
-            dark={darkMode}
-          />
-          <NavItem
-            text="Props"
-            active={displayTab === "props"}
-            onClick={() => handleTabChange("props")}
-            dark={darkMode}
-          />
-          {showPlayground && (
-            <NavItem
-              text="Playground"
-              active={displayTab === "playground"}
-              onClick={() => handleTabChange("playground")}
-              dark={darkMode}
-            />
           )}
 
-          {/* Building Blocks and References tabs, commented out until building blocks and references are implemented */}
-          {/* <NavItem
+          {/* Navigation Tabs */}
+          <Nav orientation="horizontal" dark={darkMode}>
+            <NavItem
+              text="Docs"
+              active={displayTab === "docs"}
+              onClick={() => handleTabChange("docs")}
+              dark={darkMode}
+            />
+            <NavItem
+              text="Props"
+              active={displayTab === "props"}
+              onClick={() => handleTabChange("props")}
+              dark={darkMode}
+            />
+            {showPlayground && (
+              <NavItem
+                text="Playground"
+                active={displayTab === "playground"}
+                onClick={() => handleTabChange("playground")}
+                dark={darkMode}
+              />
+            )}
+
+            {/* Building Blocks and References tabs, commented out until building blocks and references are implemented */}
+            {/* <NavItem
             text="Building Blocks"
             active={activeTab === "building-blocks"}
             onClick={() => setActiveTab("building-blocks")}
@@ -159,47 +159,38 @@ const KitShow = () => {
             active={activeTab === "references"}
             onClick={() => setActiveTab("references")}
           /> */}
-        </Nav>
-      </Flex>
+          </Nav>
+        </Flex>
+      </div>
       <SectionSeparator marginBottom="lg" dark={darkMode} />
 
-      <Flex
-        align="stretch"
-        maxWidth="xxl"
-        marginX="auto"
-        orientation="column"
-        marginBottom="lg"
-      >
-        {/* Playground Tab Content (React-only for now; hidden on Rails) */}
-        {showPlayground && displayTab === "playground" && (
-          <PlaygroundTab
-            kitSchema={kit_schema}
-            globalPropsSchema={global_props_schema}
-            kitName={currentKit}
-            defaultExample={examples?.[0]}
-            playgroundConfig={playground_config}
-          />
-        )}
+      <div className="kit-show-wrapper">
+        <Flex align="stretch" minWidth={0} orientation="column" marginBottom="lg" width="100%">
+          {/* Playground Tab Content (React-only for now; hidden on Rails) */}
+          {showPlayground && displayTab === "playground" && (
+            <PlaygroundTab
+              kitSchema={kit_schema}
+              globalPropsSchema={global_props_schema}
+              kitName={currentKit}
+              defaultExample={examples?.[0]}
+              playgroundConfig={playground_config}
+            />
+          )}
 
-        {/* Docs Tab Content */}
-        {displayTab === "docs" && (
-          <DocsTab
-            examples={examples}
-            exampleProps={exampleProps}
-            name={name}
-            sections={kit_sections}
-          />
-        )}
+          {/* Docs Tab Content */}
+          {displayTab === "docs" && (
+            <DocsTab
+              examples={examples}
+              exampleProps={exampleProps}
+              name={name}
+              sections={kit_sections}
+            />
+          )}
 
-        {/* Props Tab Content */}
-        {displayTab === "props" && <PropsTab availableProps={available_props} platform={platform} />}
-
-        {/* Building Blocks Tab Content, commented out until building blocks are implemented */}
-        {/* {activeTab === "building-blocks" && <BuildingBlocksTab />} */}
-
-        {/* References Tab Content, commented out until references are implemented */}
-        {/* {activeTab === "references" && <ReferencesTab />} */}
-      </Flex>
+          {/* Props Tab Content */}
+          {displayTab === "props" && <PropsTab availableProps={available_props} platform={platform} />}
+        </Flex>
+      </div>
     </>
   );
 };
