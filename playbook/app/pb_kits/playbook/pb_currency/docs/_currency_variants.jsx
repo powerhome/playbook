b049d2364f153da10818f7bbf04ebd0d167bbb53
Elisa Shapiro <elisashapiro@gmail.com>
2026-05-19 15:26:31 -0400

    TEMPORARY zero negative and null doc ex - to be reverted

--- playbook/app/pb_kits/playbook/pb_currency/docs/_currency_variants.jsx
+++ playbook/app/pb_kits/playbook/pb_currency/docs/_currency_variants.jsx
@@ -6,14 +6,14 @@ const CurrencyVariants = (props) => {
   return (
     <>
       <Currency
-          amount="30,327.43"
+          amount="0"
           label="Default"
           marginBottom="md"
           size="sm"
           {...props}
       />
       <Currency
-          amount="2,000.50"
+          amount="0"
           emphasized={false}
           label="Emphasized False"
           marginBottom="md"
@@ -21,7 +21,7 @@ const CurrencyVariants = (props) => {
           {...props}
       />
       <Currency
-          amount={342}
+          amount={0}
           label="Light"
           marginBottom="md"
           size="sm"
@@ -30,11 +30,123 @@ const CurrencyVariants = (props) => {
           {...props}
       />
       <Currency
-          amount="45"
+          amount="0"
           label="Bold"
+          marginBottom="md"
           size="sm"
           unit="/mo"
           variant="bold"
+          {...props}
+      />
+      <Currency
+          amount=""
+          label="Null"
+          marginBottom="md"
+          nullDisplay="--"
+          {...props}
+      />
+      <Currency
+          amount=""
+          label="Null"
+          marginBottom="md"
+          nullDisplay="$0.00"
+          {...props}
+      />
+      <Currency
+          amount=""
+          label="Null"
+          marginBottom="md"
+          nullDisplay=" "
+          {...props}
+      />
+      <Currency
+          amount="-.53"
+          label="Null"
+          marginBottom="md"
+          nullDisplay="$0.00"
+          {...props}
+      />
+      <Currency
+          amount="-.53"
+          label="Null"
+          marginBottom="md"
+          nullDisplay="0"
+          {...props}
+      />
+      <Currency
+          amount=""
+          label="Empty (no null display)"
+          marginBottom="md"
+          size="sm"
+          {...props}
+      />
+      <Currency
+          amount={0}
+          label="Numeric 0 + nullDisplay (expect $0.00)"
+          marginBottom="md"
+          nullDisplay="--"
+          size="sm"
+          {...props}
+      />
+      <Currency
+          amount="0.00"
+          label="String 0.00"
+          marginBottom="md"
+          size="sm"
+          {...props}
+      />
+      <Currency
+          amount="-123.45"
+          label="Negative + nullDisplay (expect -$123.45)"
+          marginBottom="md"
+          nullDisplay="$0.00"
+          size="sm"
+          {...props}
+      />
+      <Currency
+          amount="-123.45"
+          label="Negative sm"
+          marginBottom="md"
+          size="sm"
+          {...props}
+      />
+      <Currency
+          amount="-123.45"
+          label="Negative md"
+          marginBottom="md"
+          size="md"
+          {...props}
+      />
+      <Currency
+          amount="320.20"
+          decimals="matching"
+          label="Decimals matching"
+          marginBottom="md"
+          size="sm"
+          {...props}
+      />
+      <Currency
+          amount="0.00"
+          commaSeparator
+          label="Comma separator + zero"
+          marginBottom="md"
+          size="sm"
+          {...props}
+      />
+      <Currency
+          amount="1234567.89"
+          commaSeparator
+          label="Comma separator"
+          marginBottom="md"
+          size="sm"
+          {...props}
+      />
+      <Currency
+          amount="0.00"
+          label="Unstyled zero"
+          marginBottom="md"
+          size="sm"
+          unstyled
           {...props}
       />
     </>
