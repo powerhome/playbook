82cbe7cac52ea31022566839f12e4e296b93975c
Elisa Shapiro <elisashapiro@gmail.com>
2026-05-21 13:30:42 -0400

    Update registry to new file

--- playbook-website/app/javascript/images/kit_previews/user/index.ts
+++ playbook-website/app/javascript/images/kit_previews/user/index.ts
@@ -1,7 +1,7 @@
 import avatar from "./avatar.svg";
 import multipleUsers from "./multiple_users.svg";
 import multipleUsersStacked from "./multiple_users_stacked.svg";
-import user from "./user.svg";
+import user from "./kit_user.svg";
 
 export const userPreviewMap = {
   "user/avatar": avatar,
