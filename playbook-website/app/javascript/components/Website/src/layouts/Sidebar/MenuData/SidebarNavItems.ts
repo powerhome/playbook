a9b952e3ae597e66fed795ed69ff037386f9958a
nidaqg <nidaqg@gmail.com>
2026-06-23 08:28:53 -0400

    hide playgrounds

--- playbook-website/app/javascript/components/Website/src/layouts/Sidebar/MenuData/SidebarNavItems.ts
+++ playbook-website/app/javascript/components/Website/src/layouts/Sidebar/MenuData/SidebarNavItems.ts
@@ -48,11 +48,11 @@ export const SideBarNavItems = [
         children: false,
         leftIcon:"icons"
     },
-    {
-        name: "Playground",
-        key: "top-nav-item-9",
-        link: "/playground",
-        children: false,
-        leftIcon:"code"
-    }
+    // {
+    //     name: "Playground",
+    //     key: "top-nav-item-9",
+    //     link: "/playground",
+    //     children: false,
+    //     leftIcon:"code"
+    // }
 ]
