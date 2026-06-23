59829d50db13b1b27e3991a7d80a4e8ec1ae787a
Elisa Shapiro <elisashapiro@gmail.com>
2026-06-23 09:25:27 -0400

    Correct data import name to fix doc ex

--- playbook/app/pb_kits/playbook/pb_advanced_table/docs/_advanced_table_row_styling.jsx
+++ playbook/app/pb_kits/playbook/pb_advanced_table/docs/_advanced_table_row_styling.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import AdvancedTable from '../_advanced_table'
-import MOCK_DATA from "./advanced_table_mock_data_with_id.json"
+import MOCK_DATA_WITH_ID from "./advanced_table_mock_data_with_id.json"
 import colors from '../../tokens/exports/_colors.module.scss'
 
 
@@ -55,7 +55,7 @@ const rowStyling = [
       <AdvancedTable
           columnDefinitions={columnDefinitions}
           rowStyling={rowStyling}
-          tableData={MOCK_DATA}
+          tableData={MOCK_DATA_WITH_ID}
           {...props}
       />
     </div>
