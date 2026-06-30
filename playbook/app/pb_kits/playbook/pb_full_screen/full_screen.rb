b84f4fb5875b69509048f9573d5f45a3d820acb9
nidaqg <nidaqg@gmail.com>
2026-06-30 08:47:03 -0400

    Hide rails for now

--- playbook/app/pb_kits/playbook/pb_full_screen/full_screen.rb
+++ playbook/app/pb_kits/playbook/pb_full_screen/full_screen.rb
@@ -3,12 +3,6 @@
 module Playbook
   module PbFullScreen
     class FullScreen < ::Playbook::KitBase
-      prop :content_padding, type: Playbook::Props::Enum,
-                             values: %w[none xxs xs sm md lg xl],
-                             default: "lg"
-      prop :sticky_header, type: Playbook::Props::Boolean,
-                           default: true
-
       def classname
         generate_classname("pb_full_screen_kit")
       end
