e909588537dfe833778339a7bd2005531be65fbb
Elisa Shapiro <elisashapiro@gmail.com>
2026-07-06 13:42:21 -0400

    Bottom Row Pinning for React

--- playbook/app/pb_kits/playbook/pb_advanced_table/docs/_advanced_table_pinned_rows.jsx
+++ playbook/app/pb_kits/playbook/pb_advanced_table/docs/_advanced_table_pinned_rows.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
 import AdvancedTable from '../_advanced_table'
 import MOCK_DATA_WITH_ID from "./advanced_table_mock_data_with_id.json"
+import Caption from "../../pb_caption/_caption"
 
 const AdvancedTableRowPinning = (props) => {
   const columnDefinitions = [
@@ -35,14 +36,30 @@ const AdvancedTableRowPinning = (props) => {
     },
   ]
 
-  const [pinnedRows, setPinnedRows] = useState({top: ["8"]})
+  const [pinnedRowsTop, setPinnedRowsTop] = useState({top: ["8"]})
+  const [pinnedRowsBottom, setPinnedRowsBottom] = useState({bottom: ["8"]})
 
   return (
     <div>
+      <Caption text="Pinned Rows Top" />
       <AdvancedTable
           columnDefinitions={columnDefinitions}
           maxHeight="xs"
-          pinnedRows={{value: pinnedRows, onChange: setPinnedRows}}
+          pinnedRows={{value: pinnedRowsTop, onChange: setPinnedRowsTop}}
+          tableData={MOCK_DATA_WITH_ID}
+          tableProps={{sticky: true}}
+          {...props}
+      >
+        <AdvancedTable.Header enableSorting />
+        <AdvancedTable.Body />
+      </AdvancedTable>
+      <Caption marginTop="md"
+          text="Pinned Rows Bottom"
+      />
+      <AdvancedTable
+          columnDefinitions={columnDefinitions}
+          maxHeight="xs"
+          pinnedRows={{value: pinnedRowsBottom, onChange: setPinnedRowsBottom}}
           tableData={MOCK_DATA_WITH_ID}
           tableProps={{sticky: true}}
           {...props}
