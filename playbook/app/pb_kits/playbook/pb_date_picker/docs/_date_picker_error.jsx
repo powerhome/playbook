dde5401eb727e926effad712358c26ffd8470eb9
Elisa Shapiro <83474365+ElisaShapiro@users.noreply.github.com>
2026-08-14 14:38:33 -0400

    [PLAY-3122] Remove Icon from Error docs (#6504)
    **What does this PR do?** A clear and concise description with your
    runway ticket url.
    [PLAY-3122](https://runway.powerhrg.com/backlog_items/PLAY-3122) removes
    the Warning Icon from the error messages in our error docs - our design
    style guide has changed. This PR reverses previous work done to add
    icons to these docs, largely in PLAY-2043/PLAY-2044 (and a couple
    others).
    
    **Screenshots:** Screenshots to visualize your addition/change
    <img width="1546" height="537" alt="PLAY3112sidebyside"
    src="https://github.com/user-attachments/assets/7aebb9f7-dd0b-4b5c-b3d0-c336e8ab75d0"
    />
    
    **How to test?** Steps to confirm the desired behavior:
    1. Go to kit pages/error docs for the following kits and compare to prod
    - no more Warning icon in the error message: Rails and React updated for
    Date Picker, Dropdown, File Upload, Select, Textarea, Text Input; React
    updated for Typeahead and Phone Number Input.
    
    
    #### Checklist:
    - [x] **LABELS** Add a label: `enhancement`, `bug`, `improvement`, `new
    kit`, `deprecated`, or `breaking`. See [Changelog &
    Labels](https://github.com/powerhome/playbook/wiki/Changelog-&-Labels)
    for details.
    - [x] **DEPLOY** I have added the `milano` label to show I'm ready for a
    review.
    ~~- [ ] **TESTS** I have added test coverage to my code.~~
    ~~- [ ] **PLAYGROUND** I have added and tested Playground metadata and
    overrides for all kits and props updated in my code.~~
    - [x] **SEMVER** I have added a `minor`, `major`, or `patch` label for
    release.
    - [x] **RC** I have added an `inactive RC` label if not an active RC.

--- playbook/app/pb_kits/playbook/pb_date_picker/docs/_date_picker_error.jsx
+++ playbook/app/pb_kits/playbook/pb_date_picker/docs/_date_picker_error.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 
 import DatePicker from '../_date_picker'
-import Icon from '../../pb_icon/_icon'
 
 const DatePickerError = (props) => (
   <div>
     <DatePicker
-        error={<>
-          <Icon icon="warning" /> Invalid date. Please pick a valid date.
-        </>}
+        error="Invalid date. Please pick a valid date."
         pickerId="date-picker-error"
         {...props}
     />
