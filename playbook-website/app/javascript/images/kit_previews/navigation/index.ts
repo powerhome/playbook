f69ccaa3745476799086d2184bfa252182906e52
Elisa Shapiro <elisashapiro@gmail.com>
2026-08-24 11:14:49 -0400

    Add missing Kit Category svgs for 5 kits

--- playbook-website/app/javascript/images/kit_previews/navigation/index.ts
+++ playbook-website/app/javascript/images/kit_previews/navigation/index.ts
@@ -1,7 +1,9 @@
 import breadCrumbs from "./bread_crumbs.svg";
 import nav from "./kit_nav.svg";
+import pagination from "./pagination.svg";
 
 export const navigationPreviewMap = {
   "navigation/bread_crumbs": breadCrumbs,
   "navigation/nav": nav,
+  "navigation/pagination": pagination,
 } as const;
