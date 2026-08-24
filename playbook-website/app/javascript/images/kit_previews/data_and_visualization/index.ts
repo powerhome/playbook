4dc9ff371ea545a7d29034adbd693393e6a92861
Elisa Shapiro <83474365+ElisaShapiro@users.noreply.github.com>
2026-08-24 15:30:50 -0400

    [PLAY-3156] Playbook Website: Kit Category and Dependencies Page Updates (#6543)
    **What does this PR do?** A clear and concise description with your
    runway ticket url.
    [PLAY-3156](https://runway.powerhrg.com/backlog_items/PLAY-3156) makes
    some Playbook website updates:
    - 6 kits which did not appear on their expected Kit Category index pages
    now do (for both Rails and React: PB Circle Chart, PB Gauge Chart, PB
    Line Graph, Time Picker; for just Rails: Empty State; for just React:
    Pagination).
    - 5 of those 6 kits also needed Kit Category svgs added to the repo (all
    but Empty State which appeared on the React side).
    - Dependencies getting started page had some wacky column alignment for
    two of its tables - fixed here.
    
    **Screenshots:** Screenshots to visualize your addition/change
    <img width="1567" height="1023" alt="datavis kit cards"
    src="https://github.com/user-attachments/assets/b3873e33-48e9-49e6-a2ea-7874c66e918b"
    />
    <img width="1461" height="844" alt="side by side 1"
    src="https://github.com/user-attachments/assets/9fc7d487-1a7f-42dc-9b35-05867ee8884f"
    />
    <img width="1426" height="913" alt="side by side 2"
    src="https://github.com/user-attachments/assets/0047cf1f-4fab-495e-a9a0-5dd2787f1007"
    />
    
    
    **How to test?** Steps to confirm the desired behavior:
    1. Go to the following Kit Category Pages and see the appearance of the
    Kit Card + Image for the above MIA kits/categories: Data Visualization
    now shows PB Circle Chart, PB Gauge Chart and PB Line Graph for Rails +
    React; Form Selection now shows Time Picker for Rails and React;
    Navigation now shows Pagination for React (in addition to Rails); State
    & Progress Indicators now shows Empty State for Rails (in addition to
    React).
    2. Go to the Dependencies page and compare the Unbundled and Bundled
    Dependencies Tables to current prod - complex rows do not cause Table
    alignment issues now.
    
    
    #### Checklist:
    - [x] **LABELS** Add a label: `enhancement`, `bug`, `improvement`, `new
    kit`, `deprecated`, or `breaking`. See [Changelog &
    Labels](https://github.com/powerhome/playbook/wiki/Changelog-&-Labels)
    for details.
    - [x] **DEPLOY** I have added the `milano` label to show I'm ready for a
    review.
    ~~- [ ] **TESTS** I have added test coverage to my code.~~
    ~~- [ ] **PLAYGROUND** I have added and tested Playground metadata and
    overrides for all kits and props updated in my code.~~
    - [x] **SEMVER** I have added a `minor`, `major`, or `patch` label for
    release.
    - [x] **RC** I have added an `inactive RC` label if not an active RC.

--- playbook-website/app/javascript/images/kit_previews/data_and_visualization/index.ts
+++ playbook-website/app/javascript/images/kit_previews/data_and_visualization/index.ts
@@ -5,6 +5,9 @@ import list from "./list.svg";
 import map from "./map.svg";
 import pbBarGraph from "./pb_bar_graph.svg";
 import table from "./table.svg";
+import pbCircleChart from "./pb_circle_chart.svg";
+import pbGaugeChart from "./pb_gauge_chart.svg";
+import pbLineGraph from "./pb_line_graph.svg";
 
 export const dataAndVisualizationPreviewMap = {
   "data_visualization/distribution_bar": distributionBar,
@@ -14,4 +17,7 @@ export const dataAndVisualizationPreviewMap = {
   "data_visualization/map": map,
   "data_visualization/pb_bar_graph": pbBarGraph,
   "data_visualization/table": table,
+  "data_visualization/pb_circle_chart": pbCircleChart,
+  "data_visualization/pb_gauge_chart": pbGaugeChart,
+  "data_visualization/pb_line_graph": pbLineGraph,
 } as const;
