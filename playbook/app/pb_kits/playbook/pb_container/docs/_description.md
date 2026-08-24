281f075706c828d453be8ec0ceaa0ea2e231fe20
nidaqg <nidaqg@gmail.com>
2026-08-24 15:55:31 -0400

    Added global props doc

--- playbook/app/pb_kits/playbook/pb_container/docs/_description.md
+++ playbook/app/pb_kits/playbook/pb_container/docs/_description.md
@@ -1,1 +1,1 @@
-Container component
+Use Container in place of a custom `div` and SCSS file. Apply global props for spacing, border, radius, shadow, and more.
--- playbook/app/pb_kits/playbook/pb_container/docs/index.js
+++ playbook/app/pb_kits/playbook/pb_container/docs/index.js
@@ -1,1 +1,3 @@
 export { default as ContainerDefault } from './_container_default.jsx'
+export { default as ContainerTag } from './_container_tag.jsx'
+export { default as ContainerGlobalProps } from './_container_global_props.jsx'
