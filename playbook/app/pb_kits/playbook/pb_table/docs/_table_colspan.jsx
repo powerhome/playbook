0fec04590cce77ee3d94c420f9e1fc798956b7b0
Elisa Shapiro <elisashapiro@gmail.com>
2026-09-01 09:30:20 -0400

    Docs

---
+++ playbook/app/pb_kits/playbook/pb_table/docs/_table_colspan.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+import Table from '../_table'
+
+const TableSm = (props) => {
+  return (
+    <Table
+        size="sm"
+        {...props}
+    >
+      <Table.Head>
+        <Table.Row>
+          <Table.Header colSpan={2} 
+              text="Column 1 (spans 2 columns)" 
+              textAlign="center" 
+          />
+          <Table.Header text="Column 2" />
+          <Table.Header text="Column 3" />
+          <Table.Header text="Column 4" />
+        </Table.Row>
+      </Table.Head>
+      <Table.Body>
+        <Table.Row>
+          <Table.Cell text="Value 1" />
+          <Table.Cell text="Value 2" />
+          <Table.Cell text="Value 3" />
+          <Table.Cell text="Value 4" />
+          <Table.Cell text="Value 5" />
+        </Table.Row>
+        <Table.Row>
+          <Table.Cell text="Value 1" />
+          <Table.Cell text="Value 2" />
+          <Table.Cell text="Value 3" />
+          <Table.Cell  colSpan={2}
+              text="Value 4 (spans 2 columns)"
+              textAlign="center" 
+          />
+        </Table.Row>
+        <Table.Row>
+          <Table.Cell text="Value 1" />
+          <Table.Cell text="Value 2" />
+          <Table.Cell text="Value 3" />
+          <Table.Cell text="Value 4" />
+          <Table.Cell text="Value 5" />
+        </Table.Row>
+      </Table.Body>
+    </Table>
+  )
+}
+
+export default TableSm
